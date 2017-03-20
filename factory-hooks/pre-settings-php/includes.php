ed39972bf7d3528f92ed73748c3b2a08f912d901
Sylvain DELBOSC <sylvain.delbosc@acquia.com>
2017-03-20 16:54:38 +0530

    MMCPA-00: Fix build.

--- factory-hooks/pre-settings-php/includes.php
+++ factory-hooks/pre-settings-php/includes.php
@@ -12,5 +12,13 @@
 
 require DRUPAL_ROOT . '/../vendor/acquia/blt/settings/blt.settings.php';
 
+// Travis case.
+if (empty($config_directories)) {
+  // Configuration directories.
+  $dir = dirname(DRUPAL_ROOT);
+  $config_directories['sync'] = $dir . "/config/$site_dir";
+}
+
+// Default credentials.
 $settings['alshaya_custom_shield_default_user'] = 'alshaya_shield';
 $settings['alshaya_custom_shield_default_pass'] = 'AS_S';
