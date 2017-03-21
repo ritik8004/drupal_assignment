2a6c41d9dacd210fa07a812625527dc65c568e1f
Sylvain DELBOSC <sylvain.delbosc@acquia.com>
2017-03-21 19:31:40 +0530

    MMCPA-00: Fix site install.

--- factory-hooks/pre-sites-php/sites.php
+++ factory-hooks/pre-sites-php/sites.php
@@ -4,8 +4,5 @@
  * Customisation of sites.php for local dev env.
  */
 
-$sites['default'] = 'g';
+//$sites['default'] = 'g';
 $sites['local.alshaya.com'] = 'g';
-
-$sites['alshayasdera3rylnaka.devcloud.acquia-sites.com'] = 'g';
-$sites['alshayasderteukruhjq.devcloud.acquia-sites.com'] = 'g';
