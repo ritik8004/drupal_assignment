221ccb6f4a6d547a4d99e496175d7ca75c430c56
Swarad Mokal <swarad07@gmail.com>
2017-03-21 13:16:13 +0530

    MMCPA-288: Adding a config form for accepting the value of currency format.

---
+++ docroot/modules/commerce/acq_commerce/src/Form/CurrencySettingsForm.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\acq_commerce\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class CurrencySettingsForm
+ * @package Drupal\acq_commerce\Form
+ * @ingroup acq_commerce
+ */
+class CurrencySettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getFormId() {
+    return 'acq_commerce_currency_settings';
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getEditableConfigNames()
+  {
+    return ['acq_commerce.currency'];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $this->config('acq_commerce.currency')
+      ->set('currency_code', $form_state->getValue('currency_code'))
+      ->save();
+
+    return parent::submitForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('acq_commerce.currency');
+    $form['currency_code'] = array(
+      '#type' => 'textfield',
+      '#description' => $this->t('ISO 4217 standard currency code.'),
+      '#title' => $this->t('ISO currency code'),
+      '#required' => TRUE,
+      '#default_value' => $config->get('currency_code'),
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+}
