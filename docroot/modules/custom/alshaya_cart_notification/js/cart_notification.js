2c71103731adf4ddbca72cdb38887e9a8134f61c
Swarad Mokal <swarad07@gmail.com>
2017-03-27 11:51:09 +0530

    MMCPA-387: Adding module alshaya cart notification which adds a cart notification using dailog API.

---
+++ docroot/modules/custom/alshaya_cart_notification/js/cart_notification.js
@@ -0,0 +1,25 @@
+(function ($) {
+    Drupal.behaviors.alshaya_cart_notification = {
+        attach: function (context, settings) {
+
+            var element = document.getElementById('cart_notification');
+
+            var dialogsettings = {
+                autoOpen: true,
+                dialogClass: 'dialog-cart-notification',
+                title: '',
+                close: function() {
+                    $("#sku-base-form").append('<div id = "cart_notification"></div>');
+                }
+            };
+
+            var myDialog = Drupal.dialog(element, dialogsettings);
+
+            // Avoiding empty dialog during first page load.
+            if($(element).html().length != 0) {
+                myDialog.show();
+                myDialog.showModal();
+            }
+        }
+    };
+})(jQuery);
