501b8e70a1de4a7d8f5a24af0ff7c8e0973f9cd2
junaidmasoodi <junaidmasoodi@outlook.com>
2017-03-29 19:40:59 +0530

    MMCPA-134: Main Menu for Mobile and tablet.

--- docroot/themes/custom/alshaya_white_label/js/menu.js
+++ docroot/themes/custom/alshaya_white_label/js/menu.js
@@ -15,6 +15,8 @@
 
       $('.hamburger--menu').click(function () {
         $('.main--menu').toggle();
+        $('body').addClass('mobile--overlay');
+        $('.mobile--close').addClass('block--display');
       });
 
       $('.c-menu-primary .mobile--search').click(function () {
@@ -23,8 +25,15 @@
 
       $('.parent--level').mouseover(function () {
         $('.child--first', this).stop(true, true).show();
+        $('body').addClass('overlay');
       }).mouseout(function () {
         $('.child--first').stop(true, true).hide();
+        $('body').removeClass('overlay');
+      });
+
+      $('.mobile--close').on('click', function (e) {
+        $('.main--menu').toggle();
+        $('body').removeClass('mobile--overlay');
       });
 
     }
