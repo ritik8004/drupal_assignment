8c40da667a711ba8df287170eb7ae56a0786ecc9
Nikunj Kotecha <nikunj@axelerant.com>
2017-04-04 10:18:16 +0530

    MMCPA-0: Added more comments.

--- docroot/modules/custom/alshaya_acm/src/Plugin/Block/CheckoutSummaryBlock.php
+++ docroot/modules/custom/alshaya_acm/src/Plugin/Block/CheckoutSummaryBlock.php
@@ -194,6 +194,8 @@ class CheckoutSummaryBlock extends BlockBase implements ContainerFactoryPluginIn
    * {@inheritdoc}
    */
   public function getCacheMaxAge() {
+    // We don't want this block to be cached since it needs to show the updated
+    // details from the cart.
     return 0;
   }
 
