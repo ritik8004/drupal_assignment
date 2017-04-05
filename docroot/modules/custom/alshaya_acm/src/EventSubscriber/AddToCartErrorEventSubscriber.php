69109bac0c7ae0e1fbf67cc7bc33483370fe3e61
Swarad Mokal <swarad07@gmail.com>
2017-04-05 14:07:12 +0530

    MMCPA-386: Adding event subscriber to the alshaya_acm module.

---
+++ docroot/modules/custom/alshaya_acm/src/EventSubscriber/AddToCartErrorEventSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\alshaya_acm\EventSubscriber;
+
+use Drupal\acq_sku\AddToCartErrorEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class AddToCartErrorEventSubscriber.
+ *
+ * @package Drupal\alshaya_acm\EventSubscriber
+ */
+class AddToCartErrorEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[AddToCartErrorEvent::SUBMIT][] = ['showerrormessage', 800];
+    return $events;
+  }
+
+  /**
+   * Subscriber Callback for the event.
+   *
+   * @param \Drupal\acq_sku\AddToCartErrorEvent $event
+   *   Exception raised.
+   */
+  public function showerrormessage(AddToCartErrorEvent $event) {
+
+    $exception = $event->getEventException();
+
+    // Logs a notice.
+    \Drupal::logger('acq_sku')->notice($exception->getMessage());
+
+    // Set an error here, which can be shown to the user.
+    drupal_set_message(AddToCartErrorEvent::SUBMIT . ' | ' . $exception->getMessage(), 'error');
+  }
+
+}
