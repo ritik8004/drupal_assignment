0b89b15ca139b9d149ae68ba6ca88fbb836b788c
Swarad Mokal <swarad07@gmail.com>
2017-04-05 14:32:02 +0530

    MMCPA-386: Adding static method and variable to denote errors.

--- docroot/modules/custom/alshaya_acm/src/EventSubscriber/AddToCartErrorEventSubscriber.php
+++ docroot/modules/custom/alshaya_acm/src/EventSubscriber/AddToCartErrorEventSubscriber.php
@@ -13,11 +13,35 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class AddToCartErrorEventSubscriber implements EventSubscriberInterface {
 
   /**
+   * Flag to indicate whether we have an error.
+   *
+   * @var bool
+   */
+  public static $isErrorOnAddToCart = FALSE;
+
+  /**
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
     $events[AddToCartErrorEvent::SUBMIT][] = ['showerrormessage', 800];
     return $events;
+  }
+
+  /**
+   * Check error status in EventSubscriber.
+   *
+   * @return bool
+   *   Returns the current value of $isErrorOnAddToCart.
+   */
+  public static function getErrorStatus() {
+    return self::$isErrorOnAddToCart;
+  }
+
+  /**
+   * Sets the static variable when there is an error.
+   */
+  public static function setErrorStatus() {
+    self::$isErrorOnAddToCart = TRUE;
   }
 
   /**
@@ -30,11 +54,11 @@ class AddToCartErrorEventSubscriber implements EventSubscriberInterface {
 
     $exception = $event->getEventException();
 
+    // Set our static variable to TRUE.
+    self::setErrorStatus();
+
     // Logs a notice.
     \Drupal::logger('acq_sku')->notice($exception->getMessage());
-
-    // Set an error here, which can be shown to the user.
-    drupal_set_message(AddToCartErrorEvent::SUBMIT . ' | ' . $exception->getMessage(), 'error');
   }
 
 }
