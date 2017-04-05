2734718d998ca57f6d2aabe863243e4b53eb4cd6
Swarad Mokal <swarad07@gmail.com>
2017-04-05 10:13:14 +0530

    MMCPA-386: Adding a new event and subscriber to hook into add to cart errors.

---
+++ docroot/modules/custom/alshaya_acm/src/EventSubscriber/AddToCartErrorEventSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\alshaya_acm\EventSubscriber;
+
+use Drupal\alshaya_acm\AddToCartErrorEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class AddToCartErrorEventSubscriber.
+ *
+ * @package Drupal\alshaya_acm\EventSubscriber
+ */
+class AddToCartErrorEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[AddToCartErrorEvent::SUBMIT][] = ['showerrormessage', 800];
+    return $events;
+  }
+
+  /**
+   * Subscriber Callback for the event.
+   *
+   * @param \Drupal\alshaya_acm\AddToCartErrorEvent $event
+   *   Exception raised.
+   */
+  public function showerrormessage(AddToCartErrorEvent $event) {
+
+    $exception = $event->getEventException();
+
+    // Logs a notice.
+    \Drupal::logger('alshaya_acm')->notice('bla');
+    \Drupal::logger('alshaya_acm')->notice($exception->getMessage());
+
+    // Show user interface message here.
+  }
+
+}
