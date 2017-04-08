307b0beb9dad0f6c1b08f7a5e241c633063a88d6
Mitesh Patel <developermitesh@gmail.com>
2017-04-08 13:11:24 +0530

    MMCPA-679: Update form alter to use AlshayaProfileDeleteForm and move tempate preprocess to .theme.

--- docroot/modules/custom/alshaya_addressbook/src/Form/AlshayaProfileDeleteForm.php
+++ docroot/modules/custom/alshaya_addressbook/src/Form/AlshayaProfileDeleteForm.php
@@ -2,7 +2,12 @@
 
 namespace Drupal\alshaya_addressbook\Form;
 
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\profile\Form\ProfileDeleteForm;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\CloseModalDialogCommand;
+use Drupal\Core\Ajax\RedirectCommand;
+use Drupal\Core\Url;
 
 /**
  * Provides a confirmation form for deleting a profile entity.
@@ -67,6 +72,50 @@ class AlshayaProfileDeleteForm extends ProfileDeleteForm {
     }
 
     return $this->t('Address is deleted successfully.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildForm($form, $form_state);
+    if ($this->entity->getType() != 'address_book') {
+      return $form;
+    }
+
+    $form['actions']['submit']['#ajax'] = [
+      'callback' => '::deleteConfirmRedirect',
+    ];
+
+    $form['actions']['cancel']['#url'] = Url::fromRoute('alshaya_addressbook.close_modal');
+    $form['actions']['cancel']['#attributes']['class'][] = 'use-ajax';
+    $form['actions']['cancel']['#attributes']['data-dialog-type'] = 'modal';
+
+    return $form;
+  }
+
+  /**
+   * Submit callback for delete to close modal and reload the page.
+   */
+  public function deleteConfirmRedirect(array $form, FormStateInterface $form_state) {
+    if ($form_state->isExecuted()) {
+      $response = new AjaxResponse();
+      $response->addCommand(new CloseModalDialogCommand());
+      $response->addCommand(new RedirectCommand(Url::fromRoute('entity.profile.type.address_book.user_profile_form', [
+        'user' => $this->entity->getOwnerId(),
+        'profile_type' => 'address_book',
+      ])->toString()));
+      return $response;
+    }
+  }
+
+  /**
+   * Close modal window.
+   */
+  public function closeModal() {
+    $response = new AjaxResponse();
+    $response->addCommand(new CloseModalDialogCommand());
+    return $response;
   }
 
 }
