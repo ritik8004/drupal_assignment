b5d20b0bd80efe51288458c0a084ddccac1150d5
Murtaza Alvi <smurtazama@gmail.com>
2017-04-12 16:27:53 +0530

    MMCPA-481: Address edit form.
    Update the behavior of labels.
    Update the theme for cancel button.

--- docroot/themes/custom/alshaya_white_label/js/form.js
+++ docroot/themes/custom/alshaya_white_label/js/form.js
@@ -8,7 +8,16 @@
 
   Drupal.behaviors.formsInput = {
     attach: function (context, settings) {
-      $('.webform-submission-alshaya-contact-form input, .webform-submission-alshaya-contact-form textarea, .profile-form input').focusout(function () {
+      $('.profile-form input').each(function () {
+        if ($.trim($(this).val()).length !== 0) {
+          $(this).parent().find('label').last().addClass('active-label');
+        }
+        else {
+          $(this).parent().find('label').last().removeClass('active-label');
+        }
+      });
+
+      $('.contact-form input, .contact-form textarea, .profile-form input').focusout(function () {
         if ($(this).val() !== '') {
           $(this).parent().find('label').last().addClass('active-label');
         }
