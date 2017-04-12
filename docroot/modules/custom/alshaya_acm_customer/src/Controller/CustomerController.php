2530af6e1b0c3f44f99c1c38c6c27d6ded80ef98
Gulab Bisht <gulab.bisht@gmail.com>
2017-04-12 22:58:22 +0530

    MMCPA-735: Order print page added with custom twig files.

--- docroot/modules/custom/alshaya_acm_customer/src/Controller/CustomerController.php
+++ docroot/modules/custom/alshaya_acm_customer/src/Controller/CustomerController.php
@@ -6,6 +6,7 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Url;
 use Drupal\image\Entity\ImageStyle;
 use Drupal\user\UserInterface;
+use Com\Tecnick\Barcode\Barcode as BarcodeGenerator;
 
 /**
  * Customer controller to add/override pages for customer.
@@ -221,6 +222,171 @@ class CustomerController extends ControllerBase {
     $build['#cache'] = ['max-age' => 0];
 
     return $build;
+  }
+
+  /**
+   * Controller function for order print.
+   *
+   * @param \Drupal\user\UserInterface $user
+   *   User object for which the orders detail page is being viewed.
+   * @param string $order_id
+   *   Order id to view the detail for.
+   *
+   * @return array
+   *   Build array.
+   */
+  public function orderPrint(UserInterface $user, $order_id) {
+    \Drupal::moduleHandler()->loadInclude('alshaya_acm_product', 'inc', 'alshaya_acm_product.utility');
+    \Drupal::moduleHandler()->loadInclude('alshaya_acm_customer', 'inc', 'alshaya_acm_customer.orders');
+
+    // Get the orders to display for current user and filter applied.
+    $orders = alshaya_acm_customer_get_user_orders($user);
+
+    $order = $orders[$order_id];
+
+    if (empty($order)) {
+      throw new NotFoundHttpException();
+    }
+
+    $products = [];
+    foreach ($order['items'] as $item) {
+      $product = $item;
+      $product['total'] = number_format($item['ordered'] * $item['price'], 3);
+
+      try {
+        // Check if we can find a parent SKU for this.
+        $parentSku = alshaya_acm_product_get_parent_sku_by_sku($item['sku']);
+
+        // We will use the parent SKU name for display.
+        $product['name'] = $parentSku->label();
+
+        // Try to find attributes to display for this product.
+        $product['attributes'] = alshaya_acm_product_get_sku_configurable_values($item['sku']);
+      }
+      catch (\Exception $e) {
+        // Current SKU seems to be a simple one, we don't need to do anything.
+      }
+
+      $product['image'] = '';
+
+      // Load sku from item_id that we have in $item.
+      $media = alshaya_acm_product_get_sku_media($item['sku']);
+
+      // If we have image for the product.
+      if (!empty($media)) {
+        $image = array_shift($media);
+        $file_uri = $image->getFileUri();
+        $product['image'] = ImageStyle::load('checkout_summary_block_thumbnail')->buildUrl($file_uri);
+      }
+
+      $products[] = $product;
+    }
+
+    // Get the currency code and position.
+    $currencyCode = \Drupal::config('acq_commerce.currency')->get('currency_code');
+    $currencyCodePosition = \Drupal::config('acq_commerce.currency')->get('currency_code_position');
+
+    // Build account details array.
+    $account = [];
+    $account['first_name'] = $user->get('field_first_name')->getString();
+    $account['last_name'] = $user->get('field_last_name')->getString();
+    $account['mail'] = $user->get('mail')->getString();
+    $account['privilege_card_number'] = $user->get('field_privilege_card_number')->getString();
+
+    $build = [];
+    $build['#site_logo'] = [
+      '#theme' => 'image',
+      '#uri' => theme_get_setting('logo.url'),
+    ];
+    $build['#barcode'] = $this->getBarcode($order_id);
+    $build['#products_count'] = count($products);
+    $build['#order'] = alshaya_acm_customer_get_processed_order_summary($order_id, $order);
+    $build['#order_details'] = alshaya_acm_customer_get_processed_order_details($order);
+    $build['#products'] = $products;
+    // @TODO: MMCPA-641.
+    $build['#delivery_detail_notice'] = $this->t('Your order will be delivered between 1 and 3 days');
+    $build['#account'] = $account;
+    $build['#currency_code'] = $currencyCode;
+    $build['#currency_code_position'] = $currencyCodePosition;
+    $build['#theme'] = 'user_order_print';
+    $build['#cache'] = ['max-age' => 0];
+
+    return $build;
+  }
+
+  /**
+   * Function to get barcode.
+   *
+   * @param string $order_number
+   *   Order number for which barcode needed.
+   *
+   * @return array
+   *   Build array.
+   */
+  public function getBarcode($order_number) {
+    $build = [];
+    $settings = [
+      'type' => 'C128',
+      'value' => $order_number,
+      'color' => '#000000',
+      'height' => 90,
+      'width' => 110,
+      'padding_top' => 0,
+      'padding_right' => 0,
+      'padding_bottom' => 0,
+      'padding_left' => 0,
+      'show_value' => TRUE,
+    ];
+    $generator = new BarcodeGenerator();
+    $suffix = str_replace(
+      '+', 'plus', strtolower($settings['type'])
+    );
+    $build['barcode'] = [
+      '#theme' => 'barcode__' . $suffix,
+      '#attached' => [
+        'library' => [
+          'barcodes/' . $suffix,
+        ],
+      ],
+      '#type' => $settings['type'],
+      '#value' => $settings['value'],
+      '#width' => $settings['width'],
+      '#height' => $settings['height'],
+      '#color' => $settings['color'],
+      '#padding_top' => $settings['padding_top'],
+      '#padding_right' => $settings['padding_right'],
+      '#padding_bottom' => $settings['padding_bottom'],
+      '#padding_left' => $settings['padding_left'],
+      '#show_value' => $settings['show_value'],
+    ];
+
+    try {
+      $barcode = $generator->getBarcodeObj(
+        $settings['type'],
+        $settings['value'],
+        $settings['width'],
+        $settings['height'],
+        $settings['color'],
+        [
+          $settings['padding_top'],
+          $settings['padding_right'],
+          $settings['padding_bottom'],
+          $settings['padding_left'],
+        ]
+      );
+      $build['barcode']['#svg'] = $barcode->getSvgCode();
+    }
+    catch (\Exception $e) {
+      $this->logger->error(
+        'Error: @error, given: @value',
+        [
+          '@error' => $e->getMessage(),
+          '@value' => $settings['value'],
+        ]
+      );
+    }
+    return $build;
+
   }
 
 }
