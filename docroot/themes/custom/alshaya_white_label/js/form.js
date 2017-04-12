06224ff92961a966f4a6a79068df9c6a6824be27
Murtaza Alvi <smurtazama@gmail.com>
2017-04-12 18:24:03 +0530

    MMCPA-481: Address edit form.
    Update the removed classes.

--- docroot/themes/custom/alshaya_white_label/js/form.js
+++ docroot/themes/custom/alshaya_white_label/js/form.js
@@ -17,7 +17,7 @@
         }
       });
 
-      $('.contact-form input, .contact-form textarea, .profile-form input').focusout(function () {
+      $('.webform-submission-alshaya-contact-form input, .webform-submission-alshaya-contact-form textarea, .profile-form input').focusout(function () {
         if ($(this).val() !== '') {
           $(this).parent().find('label').last().addClass('active-label');
         }
