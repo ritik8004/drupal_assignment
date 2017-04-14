3b11eb3a77108a8af531e2fff1651c4f409dbaa5
Swarad Mokal <swarad07@gmail.com>
2017-04-14 15:41:11 +0530

    MMCPA-683: Code Refactor and comments.

--- docroot/modules/custom/alshaya_product_zoom/js/alshaya_product_zoom.js
+++ docroot/modules/custom/alshaya_product_zoom/js/alshaya_product_zoom.js
@@ -8,7 +8,7 @@
         items.parent().css('float', 'left');
       }
 
-      // Initialize lightsliders.
+      // Initialize lightSliders.
       // Slider 1 - For Desktop - Image zoom.
       $("#lightSlider").lightSlider({
         vertical: true,
@@ -18,14 +18,14 @@
         verticalHeight: 500,
       });
 
-      // Slider - 2 For Desktop - Image Gallery
+      // Slider - 2 For Desktop - Image Gallery.
       $("#product-image-gallery").lightSlider({
         vertical: true,
         item: 5,
         verticalHeight: 500,
       });
 
-      // Modal window for Slider-2, Image Gallery.
+      // Modal view for Slider-2 when clicking on big image - Image Gallery.
       var element = document.getElementById('product-image-gallery-container');
       var dialogsettings = {
         autoOpen: true,
@@ -42,67 +42,67 @@
       });
 
       ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-      // ZoomIn and out in Gallery view.
+      // ZoomIn ZoomOut in Gallery view.
       // Make sure it always starts @ zero position for below calcs to work.
-      $("#full-image").css({top: 0, left: 0});
+      if ($("#full-image-wrapper").length > 0) {
+        $("#full-image").css({top: 0, left: 0});
 
-      var maskWidth  = $("#full-image-wrapper").width();
-      var maskHeight = $("#full-image-wrapper").height();
-      var imgPos     = $("#full-image").offset();
-      var imgWidth   = $("#full-image").width();
-      console.log(imgWidth);
-      var imgHeight  = $("#full-image").height();
-      console.log(imgHeight);
-      var x1 = (imgPos.left + maskWidth) - imgWidth;
-      var y1 = (imgPos.top + maskHeight) - imgHeight;
-      var x2 = imgPos.left;
-      var y2 = imgPos.top;
+        var maskWidth  = $("#full-image-wrapper").width();
+        var maskHeight = $("#full-image-wrapper").height();
+        var imgPos     = $("#full-image").offset();
+        var imgWidth   = $("#full-image").width();
+        console.log(imgWidth);
+        var imgHeight  = $("#full-image").height();
+        console.log(imgHeight);
+        var x1 = (imgPos.left + maskWidth) - imgWidth;
+        var y1 = (imgPos.top + maskHeight) - imgHeight;
+        var x2 = imgPos.left;
+        var y2 = imgPos.top;
 
-      // Make image draggable inside the window.
-      $("#full-image").draggable({ containment: [x1,y1,x2,y2] });
-      $("#full-image").css({cursor: 'move'});
+        // Make image draggable inside the window.
+        $("#full-image").draggable({ containment: [x1,y1,x2,y2] });
+        $("#full-image").css({cursor: 'move'});
 
-      // Zoom in and Zoom out buttons.
-      var image = $('#full-image-wrapper img');
-      var imagesize = image.width();
-      var orignalwidth = imagesize;
-      $('.zoomin').on('click', function () {
-        imagesize = imagesize + 25;
-        image.width(imagesize);
-        image.height('auto');
-      });
+        // Zoom in and Zoom out buttons.
+        var image = $('#full-image-wrapper img');
+        var imagesize = image.width();
+        var orignalwidth = imagesize;
+        $('.zoomin').on('click', function () {
+          imagesize = imagesize + 25;
+          image.width(imagesize);
+          image.height('auto');
+        });
 
-      $('.zoomout').on('click', function () {
-        imagesize = imagesize - 25;
-        if(imagesize < orignalwidth) {
-          return;
-        }
-        image.width(imagesize);
-        image.height('auto');
-      });
+        $('.zoomout').on('click', function () {
+          imagesize = imagesize - 25;
+          if(imagesize < orignalwidth) {
+            return;
+          }
+          image.width(imagesize);
+          image.height('auto');
+        });
 
-      // Swap the big image inside slider-2 when clicking on thumbnail.
-      $('#product-image-gallery li').click(function () {
-        if($(this).hasClass('youtube') || $(this).hasClass('vimeo')) {
-          var href = $(this).attr('data-iframe');
-          $('#full-image-wrapper').empty();
-          $('#full-image-wrapper').html('<iframe id="player" width="480" height="480" src="'
-            + href + '" frameborder="0" allowfullscreen></iframe>');
-        }
-        else{
-          var bigImage = $(this).children('a').attr('href');
-          // Put the big image in our main container.
-          $('#full-image-wrapper img').attr('src', bigImage);
-          $('#full-image-wrapper').empty();
-          $('#full-image-wrapper').html('<img style="max-width: none;" id="full-image" src="' + bigImage + '" />');
-        }
-        // Stop the browser from loading the image in a new tab.
-        return false;
-      });
+        // Swap the big image inside slider-2 when clicking on thumbnail.
+        $('#product-image-gallery li').click(function () {
+          if($(this).hasClass('youtube') || $(this).hasClass('vimeo')) {
+            var href = $(this).attr('data-iframe');
+            $('#full-image-wrapper img').hide();
+            $('#full-image-wrapper').append('<iframe id="player" width="480" height="480" src="'
+              + href + '" frameborder="0" allowfullscreen></iframe>');
+          }
+          else{
+            var bigImage = $(this).children('a').attr('href');
+            // Put the big image in our main container.
+            $('#full-image-wrapper img').attr('src', bigImage);
+            $('#full-image-wrapper iframe').remove();
+            $('#full-image-wrapper img').show();
+          }
+          // Stop the browser from loading the image in a new tab.
+          return false;
+        });
+      }
 
-      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
       ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
       // Fetch Vimeo thumbnail.
