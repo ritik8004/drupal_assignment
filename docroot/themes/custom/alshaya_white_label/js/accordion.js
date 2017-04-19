c38e7fdf2e6684872369aa609a178c22572695d1
Swarad Mokal <swarad07@gmail.com>
2017-04-19 13:05:31 +0530

    MMCPA-607: Initializing the accordion() on footer menus, also removing CSS which was hiding the c-footer-menu.

--- docroot/themes/custom/alshaya_white_label/js/accordion.js
+++ docroot/themes/custom/alshaya_white_label/js/accordion.js
@@ -18,12 +18,38 @@
         });
       }
 
+      /**
+       * Toggles the footer accordions for desktop.
+       */
+      function toggleAccordion(){
+        var desktopView = $(document).width();
+        if(desktopView >= '768'){
+          $('.c-footer-is-accordion').accordion('disable');
+          $('.c-footer-is-accordion .ui-accordion-content').show();
+        }
+        else{
+          $('.c-footer-is-accordion').accordion('enable');
+        }
+      }
+
       if (context === document) {
         moveContextualLink('.c-accordion');
 
         $('.c-facet__blocks').accordion({
           header: '.c-accordion__title'
         });
+
+        $('.c-footer-is-accordion').accordion({
+          header: 'h2',
+          collapsible: true,
+          active: false
+        });
+        // Toggle Accordion in desktop & mobile.
+        toggleAccordion();
+        $(window).on("resize", function(){
+          toggleAccordion();
+        });
+
         if ($('.c-facet__blocks__wrapper').length) {
           var facetBlockWrapper = $('.c-facet__blocks__wrapper').clone(true, true);
           var mainBlock = $('.block-system-main-block');
