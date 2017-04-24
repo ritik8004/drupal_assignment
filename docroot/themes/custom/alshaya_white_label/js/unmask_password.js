7b7327fca053e153b03d9d3f167fc48c9c86c3d0
Gulab Bisht <gulab.bisht@gmail.com>
2017-04-24 15:38:28 +0530

    MMCPA-925: Password unmask added for register form.

--- docroot/themes/custom/alshaya_white_label/js/unmask_password.js
+++ docroot/themes/custom/alshaya_white_label/js/unmask_password.js
@@ -8,7 +8,9 @@
 
   Drupal.behaviors.unmaskPassword = {
     attach: function (context, settings) {
-      $('.form-type-password').append('<div id="unmask_password">Show</div>');
+      if (!$('.form-type-password-confirm').find('#unmask_password').length) {
+        $('.form-type-password').append('<div id="unmask_password">Show</div>');
+      }
       $('#unmask_password').on('click', function () {
         if ($('#edit-pass').attr('type') === 'password') {
           $('#edit-pass').attr('type', 'text');
@@ -33,9 +35,28 @@
         }
       });
 
+      $('.user-register-form #unmask_password').on('click', function () {
+        var current = $(this).siblings('.form-text');
+
+        if ($(current).attr('type') === 'password') {
+          $(current).attr('type', 'text');
+          $(this).html('Hide password');
+        }
+        else if ($(current).attr('type') === 'text') {
+          $(current).attr('type', 'password');
+          $(this).html('Show password');
+        }
+      });
+
       $('#edit-submit').on('click', function () {
         if ($('#edit-pass').attr('type') === 'text') {
           $('#edit-pass').attr('type', 'password');
+        }
+        if ($('#edit-pass-pass1').attr('type') === 'text') {
+          $('#edit-pass-pass1').attr('type', 'password');
+        }
+        if ($('#edit-pass-pass2').attr('type') === 'text') {
+          $('#edit-pass-pass2').attr('type', 'password');
         }
       });
     }
