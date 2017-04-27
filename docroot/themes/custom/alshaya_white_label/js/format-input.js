f4eb1f12662b9b015018274513349062fc53852a
Mitesh Patel <developermitesh@gmail.com>
2017-04-27 17:59:54 +0530

    MMCPA-1055: Privilege card validation and theming.
    **Ticket:**
    * http://jira.alshaya.com:8080/browse/MMCPA-1055
    
    **Summary:**:
    * Update to make Privilege card number field value pre-filled and disabled if
    user has already a card number saved.
    * Update to add privilge card default value.

--- docroot/themes/custom/alshaya_white_label/js/format-input.js
+++ docroot/themes/custom/alshaya_white_label/js/format-input.js
@@ -8,12 +8,28 @@
 
   Drupal.behaviors.formatInput = {
     attach: function (context, settings) {
-      var privilegeCard = $('.c-input__privilege-card');
-      privilegeCard.on('input', function () {
-        var value = $(this).val();
+      function formatPrivilegeCard(elem) {
+        var value = elem.val();
         if (value.length > 0) {
-          $(this).val(value.match(/\d{4}(?=\d{1,4})|\d+/g).join('-'));
+          elem.val(value.match(/\d{4}(?=\d{1,4})|\d+/g).join('-'));
         }
+      }
+
+      function preventDefaultValueDeletion(elem, defaultValue) {
+        setTimeout(function () {
+          if (elem.val().indexOf(defaultValue) !== 0) {
+            elem.val(defaultValue);
+          }
+        }, 1);
+      }
+
+      var privilegeCard = $('.c-input__privilege-card');
+      formatPrivilegeCard(privilegeCard);
+      var defaultValue = privilegeCard.val();
+
+      privilegeCard.on('input', function () {
+        preventDefaultValueDeletion($(this), defaultValue);
+        formatPrivilegeCard($(this));
       });
     }
   };
