83cd0afd9faf25b04b7106f88c77f73f5a090c8f
Vaibhav Jain <vaibhav.jain@acquia.com>
2017-04-27 13:26:27 +0530

    MMCPA-256: Added a Breadcrumb builder and added breadcrumb block in breadcrumb region.

---
+++ docroot/modules/custom/alshaya_search/src/AlshayaSearchBreadcrumbBuilder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\alshaya_search;
+
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Link;
+
+/**
+ * Class AlshayaSearchBreadcrumbBuilder.
+ */
+class AlshayaSearchBreadcrumbBuilder implements BreadcrumbBuilderInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $attributes) {
+    $parameters = $attributes->getParameters()->all();
+    if (!empty($parameters['view_id'])) {
+      return $parameters['view_id'] == 'search';
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $breadcrumb = new Breadcrumb();
+    $breadcrumb->addLink(Link::createFromRoute('Home', '<front>'));
+
+    $queryString = explode('&', \Drupal::request()->getQueryString());
+    foreach ($queryString as $string) {
+      $query = explode('=', $string);
+      if ($query[0] == 'keywords') {
+        $breadcrumb->addLink(Link::createFromRoute('Search results for "' . $query[1] . '"', '<none>'));
+        return $breadcrumb;
+      }
+    }
+  }
+
+}
