45626e70253b5776397687e4ecca1f6f6ee968b0
Saket Kumar <saki007ster@gmail.com>
2017-04-27 11:05:08 +0530

    MMCPA-902: Fixed javascript lint issues.

--- docroot/themes/custom/alshaya_white_label/js/sizeguide.js
+++ docroot/themes/custom/alshaya_white_label/js/sizeguide.js
@@ -9,30 +9,29 @@
   Drupal.behaviors.sizeguide = {
     attach: function (context, settings) {
 
-      if ($('#size-select').length == 0) {
+      if ($('#size-select').length === 0) {
         $('.form-item-configurables-size select').after("<ul id='size-select' />")
-          .children("option").each(function(index, el) {
+          .children('option').each(function (index, el) {
             if (index > 0) {
-              $("#size-select").append("<li><div class="+$(this).text()+">"+$(this).text()+"</div></li>");
+              $('#size-select').append('<li><div class=' + $(this).text() + '>' + $(this).text() + '</div></li>');
             }
           }
         );
-          var bh = $('.form-item-configurables-size select option:selected').text();
-        $('#size-select').before('<div><span class="size-label">Size : </span><span class="size-value">'+bh+'</span></div>');
+        var currentsize = $('.form-item-configurables-size select option:selected').text();
+        $('#size-select').before('<div><span class="size-label">Size : </span><span class="size-value">' + currentsize + '</span></div>');
       }
       $('.form-item-configurables-size select').hide();
 
-      $("#size-select li").click(function(event) {
+      $('#size-select li').click(function (event) {
         var y = $(this).text();
 
         $(this).siblings('li').removeClass('active');
         $(this).addClass('active');
 
-        $('.form-item-configurables-size select').children("option").each(function(index, el) {
+        $('.form-item-configurables-size select').children('option').each(function (index, el) {
           if (y === $(this).text()) {
             $(this).siblings('option').attr('selected', false);
             $(this).attr('selected', 'selected').change();
-
           }
         });
 
