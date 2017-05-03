0c5eba0e302f3aa3861f70d2508ed7d45bb8a788
Rohit Joshi <joshi.rohit100@gmail.com>
2017-05-03 17:59:11 +0530

    MMCPA-86: Opening info window on click.

--- docroot/modules/custom/alshaya_stores_finder/js/alshaya_store_finder.js
+++ docroot/modules/custom/alshaya_stores_finder/js/alshaya_store_finder.js
@@ -30,6 +30,24 @@
                         );
                         geolocationMap.googleMap.setCenter(newCenter);
 
+                        // Clicking the markup.
+                        var markers = geolocationMap.mapMarkers;
+                        var current_marker = {};
+                        for (var i = 0, len = markers.length; i < len; i++) {
+                            var marker = markers[i];
+                            var mapLat = marker.position.lat().toFixed(6);
+                            var mapLng = marker.position.lng().toFixed(6);
+                            var htmlLat = parseFloat($('#' + element_id + ' .lat-lng .lat').html()).toFixed(6);
+                            var htmlLng = parseFloat($('#' + element_id + ' .lat-lng .lng').html()).toFixed(6);
+                            // If markup has same latitude and longitude that we clicked.
+                            if (mapLat == htmlLat && mapLng == htmlLng) {
+                                current_marker = markers[i];
+                                break;
+                            }
+                        }
+
+                        // Trigger marker click.
+                        google.maps.event.trigger(current_marker, 'click');
                     }
 
 
