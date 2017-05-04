1f3a88ecf8239bece50516abf92e06d4e35f55ba
Saket Kumar <saki007ster@gmail.com>
2017-05-04 12:24:53 +0530

    MMCPA-776: Updating css for review comments for theming of sizeguide modal.

--- docroot/themes/custom/alshaya_white_label/js/sizeguide.js
+++ docroot/themes/custom/alshaya_white_label/js/sizeguide.js
@@ -8,7 +8,7 @@
 
   Drupal.behaviors.sizeguide = {
     attach: function (context, settings) {
-
+      // JS for converting select list to unformatted list on PDP pages.
       if ($('#block-alshaya-white-label-content #size-select').length === 0) {
         $('#block-alshaya-white-label-content .form-item-configurables-size select').after("<ul id='size-select' />")
           .children('option').each(function (index, el) {
@@ -39,6 +39,7 @@
   Drupal.behaviors.sizeguidemodal = {
     attach: function (context, settings) {
 
+      // JS for converting select list to unformatted list in the sizeguide modal popup
       if ($('#drupal-modal #size-select-modal').length === 0) {
         $('#drupal-modal .form-item-configurables-size select').after("<ul id='size-select-modal' />")
           .children('option').each(function (index, el) {
@@ -52,6 +53,7 @@
       }
       $('#drupal-modal .form-item-configurables-size select').hide();
 
+      // JS for triggering select option on clicking of list item in size list in the sizeguide modal popup
       $('#drupal-modal #size-select-modal li').click(function (event) {
         var size = $(this).text();
         $(this).siblings('li').removeClass('active');
