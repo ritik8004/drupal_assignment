278592d53ef63c24b37760a1d0b10e39dae76871
Rohit Joshi <joshi.rohit100@gmail.com>
2017-05-04 12:38:48 +0530

    MMCPA-86: Fixing js eslint errors.

--- docroot/modules/custom/alshaya_stores_finder/js/alshaya_store_finder.js
+++ docroot/modules/custom/alshaya_stores_finder/js/alshaya_store_finder.js
@@ -4,56 +4,55 @@
  */
 
 (function ($, Drupal) {
-    'use strict';
+  'use strict';
 
-    Drupal.behaviors.store_finder = {
-        attach: function (context, settings) {
+  Drupal.behaviors.store_finder = {
+    attach: function (context, settings) {
 
-            $('.set-center-location').click(function(){
-                // Id of the row.
-                var element_id = $(this).attr('id');
-                Drupal.geolocation.loadGoogle(function () {
-                    var geolocationMap = {};
+      $('.set-center-location').click(function () {
+        // Id of the row.
+        var elementID = $(this).attr('id');
+        Drupal.geolocation.loadGoogle(function () {
+          var geolocationMap = {};
 
-                    if (typeof Drupal.geolocation.maps !== 'undefined') {
-                        $.each(Drupal.geolocation.maps, function (index, map) {
-                            if (typeof map.container !== 'undefined') {
-                                geolocationMap = map;
-                            }
-                        });
-                    }
-
-                    if (typeof geolocationMap.googleMap !== 'undefined') {
-                        var newCenter = new google.maps.LatLng(
-                            $('#' + element_id + ' .lat-lng .lat').html(),
-                            $('#' + element_id + ' .lat-lng .lng').html()
-                        );
-                        geolocationMap.googleMap.setCenter(newCenter);
-
-                        // Clicking the markup.
-                        var markers = geolocationMap.mapMarkers;
-                        var current_marker = {};
-                        for (var i = 0, len = markers.length; i < len; i++) {
-                            var marker = markers[i];
-                            var mapLat = marker.position.lat().toFixed(6);
-                            var mapLng = marker.position.lng().toFixed(6);
-                            var htmlLat = parseFloat($('#' + element_id + ' .lat-lng .lat').html()).toFixed(6);
-                            var htmlLng = parseFloat($('#' + element_id + ' .lat-lng .lng').html()).toFixed(6);
-                            // If markup has same latitude and longitude that we clicked.
-                            if (mapLat == htmlLat && mapLng == htmlLng) {
-                                current_marker = markers[i];
-                                break;
-                            }
-                        }
-
-                        // Trigger marker click.
-                        google.maps.event.trigger(current_marker, 'click');
-                    }
-
-
-                });
+          if (typeof Drupal.geolocation.maps !== 'undefined') {
+            $.each(Drupal.geolocation.maps, function (index, map) {
+              if (typeof map.container !== 'undefined') {
+                geolocationMap = map;
+              }
             });
-        }
-    };
+          }
+
+          if (typeof geolocationMap.googleMap !== 'undefined') {
+            var newCenter = new google.maps.LatLng(
+              $('#' + elementID + ' .lat-lng .lat').html(),
+              $('#' + elementID + ' .lat-lng .lng').html()
+            );
+            geolocationMap.googleMap.setCenter(newCenter);
+
+            // Clicking the markup.
+            var markers = geolocationMap.mapMarkers;
+            var current_marker = {};
+            for (var i = 0, len = markers.length; i < len; i++) {
+              var marker = markers[i];
+              var mapLat = marker.position.lat().toFixed(6);
+              var mapLng = marker.position.lng().toFixed(6);
+              var htmlLat = parseFloat($('#' + elementID + ' .lat-lng .lat').html()).toFixed(6);
+              var htmlLng = parseFloat($('#' + elementID + ' .lat-lng .lng').html()).toFixed(6);
+              // If markup has same latitude and longitude that we clicked.
+              if (mapLat === htmlLat && mapLng === htmlLng) {
+                current_marker = markers[i];
+                break;
+              }
+            }
+
+            // Trigger marker click.
+            google.maps.event.trigger(current_marker, 'click');
+          }
+
+        });
+      });
+    }
+  };
 
 })(jQuery, Drupal);
