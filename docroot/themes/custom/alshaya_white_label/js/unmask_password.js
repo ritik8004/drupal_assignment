56cf3930f2117c6e56f198181a0fbba53d56215e
Swarad Mokal <swarad07@gmail.com>
2017-05-09 15:00:32 +0530

    MMCPA-1110: New BE patch by Niknuj.

--- docroot/themes/custom/alshaya_white_label/js/unmask_password.js
+++ docroot/themes/custom/alshaya_white_label/js/unmask_password.js
@@ -8,59 +8,31 @@
 
   Drupal.behaviors.unmaskPassword = {
     attach: function (context, settings) {
-      var show = Drupal.t('Show');
       var showPassword = Drupal.t('Show');
       var hidePassword = Drupal.t('Hide');
-      if (!$('.form-type-password-confirm').find('#unmask_password').length) {
-        $('.form-type-password').append('<div id="unmask_password">' + show + '</div>');
-      }
-      $('#unmask_password').on('click', function () {
-        if ($('#edit-pass').attr('type') === 'password') {
-          $('#edit-pass').attr('type', 'text');
-          $('#unmask_password').html(hidePassword);
-        }
-        else if ($('#edit-pass').attr('type') === 'text') {
-          $('#edit-pass').attr('type', 'password');
-          $('#unmask_password').html(showPassword);
+
+      $('.form-type-password').each(function () {
+        if (!$(this).find('#unmask_password').length) {
+          $(this).append('<div id="unmask_password" class="unmask-password">' + showPassword + '</div>');
         }
       });
 
-      $('.change-pwd-form #unmask_password').on('click', function () {
-        var current = $(this).siblings('.form-text');
-
-        if ($(current).attr('type') === 'password') {
-          $(current).attr('type', 'text');
+      $('.unmask-password').on('click', function () {
+        var $input = $(this).parent().find('.form-text:first');
+        if ($input.attr('type') === 'password') {
+          $input.attr('type', 'text').addClass('password-visible-input');
           $(this).html(hidePassword);
         }
-        else if ($(current).attr('type') === 'text') {
-          $(current).attr('type', 'password');
+        else {
+          $input.attr('type', 'password').removeClass('password-visible-input');
           $(this).html(showPassword);
         }
       });
 
-      $('.user-register-form #unmask_password').on('click', function () {
-        var current = $(this).siblings('.form-text');
-
-        if ($(current).attr('type') === 'password') {
-          $(current).attr('type', 'text');
-          $(this).html(hidePassword);
-        }
-        else if ($(current).attr('type') === 'text') {
-          $(current).attr('type', 'password');
-          $(this).html(showPassword);
-        }
-      });
-
-      $('#edit-submit').on('click', function () {
-        if ($('#edit-pass').attr('type') === 'text') {
-          $('#edit-pass').attr('type', 'password');
-        }
-        if ($('#edit-pass-pass1').attr('type') === 'text') {
-          $('#edit-pass-pass1').attr('type', 'password');
-        }
-        if ($('#edit-pass-pass2').attr('type') === 'text') {
-          $('#edit-pass-pass2').attr('type', 'password');
-        }
+      $('#edit-submit, .form-submit').on('click', function () {
+        $(this).parents('form:first').find('.password-visible-input').each(function () {
+          $(this).parent().find('.unmask-password').trigger('click');
+        });
       });
     }
   };
