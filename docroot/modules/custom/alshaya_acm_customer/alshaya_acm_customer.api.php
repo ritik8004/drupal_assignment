a27f282791209cd1f16dd2b0c12b1865b187ea5e
Nikunj Kotecha <nikunj@axelerant.com>
2017-05-12 15:54:12 +0530

    MMCPA-1125: Changes related to authenticating user with Magento.

---
+++ docroot/modules/custom/alshaya_acm_customer/alshaya_acm_customer.api.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Hooks specific to the alshaya_acm_customer module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Alter user account before saving user account.
+ *
+ * @param \Drupal\user\Entity\User $user
+ *   User object.
+ * @param array $customer
+ *   Customer data received from API.
+ */
+function hook_alshaya_acm_customer_update_account_alter(\Drupal\user\Entity\User $user, array &$customer) {
+
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
