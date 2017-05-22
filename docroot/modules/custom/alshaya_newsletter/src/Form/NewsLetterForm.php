783616016b14d5b96d627f5847463d4398ae5795
Nikunj Kotecha <me@nik4u.com>
2017-05-22 19:34:34 +0530

    MMCPA-282: Added messages as per requirement, changed to ajax submit.t push

--- docroot/modules/custom/alshaya_newsletter/src/Form/NewsLetterForm.php
+++ docroot/modules/custom/alshaya_newsletter/src/Form/NewsLetterForm.php
@@ -51,16 +51,33 @@ class NewsLetterForm extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $site_name = $this->config('system.site')->get('name');
 
+    $form['#prefix'] = '<div id="footer-newsletter-form-wrapper">';
+    $form['#suffix'] = '</div>';
+
     $form['email'] = [
+      '#title' => $this->t('Email address'),
+      '#title_display' => 'invisible',
       '#type' => 'email',
       '#required' => TRUE,
       '#placeholder' => $this->t('Enter your email address'),
       '#prefix' => '<div class="newsletter-block-label">' . $this->t('get email offers and the latest news from @site_name', ['@site_name' => $site_name]) . '</div>',
     ];
 
-    $form['submit'] = [
+    if ($display_message = $form_state->get('display_message')) {
+      $form['email']['#suffix'] = '<div class="subscription-status">' . $display_message . '</div>';
+    }
+
+    $form['actions']['#type'] = 'actions';
+
+    $form['actions']['submit'] = [
       '#type' => 'submit',
-      '#value' => $this->t('submit'),
+      '#value' => $this->t('sign up'),
+      '#ajax' => [
+        'event' => 'click',
+        'callback' => '::validateForm',
+        'wrapper' => 'footer-newsletter-form-wrapper',
+        'progress' => [],
+      ],
     ];
 
     return $form;
@@ -69,23 +86,33 @@ class NewsLetterForm extends FormBase {
   /**
    * {@inheritdoc}
    */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if (!$form_state->hasAnyErrors()) {
+      try {
+        $subscription = $this->apiWrapper->subscribeNewsletter($form_state->getValue('email'));
+        if ($subscription['status'] === 0) {
+          $message = '<span class="message error">' . $this->t('This email address is already subscribed.') . '</span>';
+        }
+        else {
+          $message = '<span class="message success">' . $this->t('Thank you for your subscription.') . '</span>';
+        }
+      }
+      catch (\Exception $e) {
+        $message = '<span class="message error">' . $this->t('Something went wrong, please try again later.') . '</span>';
+      }
+
+      $form_state->set('display_message', $message);
+      $form_state->setRebuild();
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    try {
-      $response = $this->apiWrapper->subscribeNewsletter($form_state->getValue('email'));
-      // @TODO: Check if want to display a different message to users who
-      // are already subscribed.
-      // Status 0 means user is already subscribed.
-      // If user is subscribed now, it will return 1.
-      if ($response['status'] === 0) {
-        drupal_set_message($this->t('Thank you for signing up to receive our emails.'), 'success');
-      }
-      else {
-        drupal_set_message($this->t('Thank you for signing up to receive our emails.'), 'success');
-      }
-    }
-    catch (\Exception $e) {
-      drupal_set_message($this->t('Something went wrong, please try again later.'), 'warning');
-    }
+
   }
 
 }
