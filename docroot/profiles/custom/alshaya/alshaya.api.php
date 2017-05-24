7a0d1f7ff58396295653dff99da37fca9a97d628
Nikunj Kotecha <me@nik4u.com>
2017-05-24 18:09:56 +0530

    MMCPA-255: Added hook to add code post profile install.

---
+++ docroot/profiles/custom/alshaya/alshaya.api.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Hooks specific to the alshaya profile.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Allow all modules to do something post profile installation is finished.
+ *
+ * @param array $modules
+ *   Modules explicitly installed in profile.
+ */
+function hook_alshaya_profile_installed(array $modules) {
+
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
