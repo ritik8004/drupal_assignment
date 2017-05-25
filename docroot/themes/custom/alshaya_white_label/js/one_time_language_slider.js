112aa1caa37b069b0a1770aca26567a98bcaa4c4
Rohit Joshi <joshi.rohit100@gmail.com>
2017-05-25 10:30:15 +0530

    MMCPA-1223: Fixing eslint error on js.

--- docroot/themes/custom/alshaya_white_label/js/one_time_language_slider.js
+++ docroot/themes/custom/alshaya_white_label/js/one_time_language_slider.js
@@ -4,19 +4,19 @@
  */
 
 (function ($, Drupal) {
-    'use strict';
+  'use strict';
 
-    Drupal.behaviors.oneTimeLanguageSlider = {
-        attach: function (context, settings) {
-            var already_visited = $.cookie('Drupal.visitor.already_visited');
-            // If visiting first time.
-            if (typeof already_visited === 'undefined') {
-                // Set expiry for 30 days.
-                $.cookie('Drupal.visitor.already_visited', '1', {path: '/', expires: 30, secure: true});
-                // Show the slider.
-                $('.only-first-time').show();
-            }
-        }
-    };
+  Drupal.behaviors.oneTimeLanguageSlider = {
+    attach: function (context, settings) {
+      var already_visited = $.cookie('Drupal.visitor.already_visited');
+      // If visiting first time.
+      if (typeof already_visited === 'undefined') {
+        // Set expiry for 30 days.
+        $.cookie('Drupal.visitor.already_visited', '1', {path: '/', expires: 30, secure: true});
+        // Show the slider.
+        $('.only-first-time').show();
+      }
+    }
+  };
 
 })(jQuery, Drupal);
