b576535637e4eec7ef7d6d33d69e0840338192f7
Rohit Joshi <joshi.rohit100@gmail.com>
2017-05-25 10:05:04 +0530

    MMCPA-1223: Using core jquery-cookie library.

--- docroot/themes/custom/alshaya_white_label/js/one_time_language_slider.js
+++ docroot/themes/custom/alshaya_white_label/js/one_time_language_slider.js
@@ -8,38 +8,15 @@
 
     Drupal.behaviors.oneTimeLanguageSlider = {
         attach: function (context, settings) {
-            var already_visited = getOneTimeCookie("Drupal.visitor.already_visited");
+            var already_visited = $.cookie('Drupal.visitor.already_visited');
             // If visiting first time.
-            if (already_visited == "") {
+            if (typeof already_visited === 'undefined') {
                 // Set expiry for 30 days.
-                setOneTimeCookie("Drupal.visitor.already_visited", 1, 30);
+                $.cookie('Drupal.visitor.already_visited', '1', {path: '/', expires: 30, secure: true});
                 // Show the slider.
                 $('.only-first-time').show();
             }
         }
-    };
-
-    var setOneTimeCookie = function (name, value, expiry) {
-        var d = new Date();
-        d.setTime(d.getTime() + (expiry*24*60*60*1000));
-        var expires = "expires="+ d.toUTCString();
-        document.cookie = name + "=" + value + ";" + expires + ";path=/";
-    };
-
-    var getOneTimeCookie = function (name) {
-        var name = name + "=";
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var ca = decodedCookie.split(';');
-        for(var i = 0; i <ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
     };
 
 })(jQuery, Drupal);
