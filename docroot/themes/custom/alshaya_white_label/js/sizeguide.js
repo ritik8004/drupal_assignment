37a0e96613502ad7bf4464364ef64f6b40181be6
Nikunj Kotecha <me@nik4u.com>
2017-06-15 19:04:21 +0530

    MMCPA-1633: Fix issues related to size select20.

--- docroot/themes/custom/alshaya_white_label/js/sizeguide.js
+++ docroot/themes/custom/alshaya_white_label/js/sizeguide.js
@@ -8,30 +8,15 @@
 
   Drupal.behaviors.sizeguide = {
     attach: function (context, settings) {
+
       // JS for converting select list for size to unformatted list on PDP pages.
-      if ($(window).width() > 1024) {
-        if ($('.acq-content-product .form-item-configurables-size .select2Option').length === 0) {
-          $('.acq-content-product .form-item-configurables-size select').select2Option();
-        }
+      $('.acq-content-product .form-item-configurables-size, .acq-content-product-modal .form-item-configurables-size').once('bind-events').each(function () {
+        $('select', $(this)).select2Option();
 
-        if ($('.acq-content-product-modal .form-item-configurables-size .select2Option').length === 0) {
-          $('.acq-content-product-modal .form-item-configurables-size select').select2Option();
-        }
-      }
-    }
-  };
-
-  Drupal.behaviors.sizeguideclick = {
-    attach: function (context, settings) {
-      if ($(window).width() > 1024) {
-        var clickedOption = $('.acq-content-product .form-item-configurables-size .select2Option li a.picked');
-        $('.acq-content-product .form-item-configurables-size .select2Option').find('.list-title .selected-text').remove();
-        $('.acq-content-product .form-item-configurables-size .select2Option').find('.list-title').append('<span class="selected-text">' + clickedOption.text() + '</span>');
-
-        var clickedOption2 = $('.acq-content-product-modal .form-item-configurables-size .select2Option li a.picked');
-        $('.acq-content-product-modal .form-item-configurables-size .select2Option').find('.list-title .selected-text').remove();
-        $('.acq-content-product-modal .form-item-configurables-size .select2Option').find('.list-title').append('<span class="selected-text">' + clickedOption2.text() + '</span>');
-      }
+        var clickedOption = $('.select2Option li a.picked', $(this));
+        $('.select2Option', $(this)).find('.list-title .selected-text').remove();
+        $('.select2Option', $(this)).find('.list-title').append('<span class="selected-text">' + clickedOption.text() + '</span>');
+      });
     }
   };
 
