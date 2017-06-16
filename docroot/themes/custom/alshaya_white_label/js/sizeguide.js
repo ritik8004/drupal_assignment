21643e82f15b61988bc0c077d84e12cd970b343b
Bassam Ismail <skippednote@gmail.com>
2017-06-16 19:14:35 +0530

    Merge pull request #1392 from saki007ster/MMCPA-1483
    MMCPA-1483: Fixing a bug in select to option js for resize.

--- docroot/themes/custom/alshaya_white_label/js/sizeguide.js
+++ docroot/themes/custom/alshaya_white_label/js/sizeguide.js
@@ -8,18 +8,6 @@
 
   Drupal.behaviors.sizeguide = {
     attach: function (context, settings) {
-      // JS for converting select list for size to unformatted list on PDP pages.
-      function select2OptionConvert() {
-        if ($(window).width() > 1024) {
-          if ($('.acq-content-product .form-item-configurables-size .select2Option').length === 0) {
-            $('.acq-content-product .form-item-configurables-size select').select2Option();
-          }
-          if ($('.acq-content-product-modal .form-item-configurables-size .select2Option').length === 0) {
-            $('.acq-content-product-modal .form-item-configurables-size select').select2Option();
-          }
-        }
-      }
-
       // Show mobile slider only on mobile resolution.
       select2OptionConvert();
       $(window).on('resize', function (e) {
@@ -27,13 +15,17 @@
       });
 
       // JS for converting select list for size to unformatted list on PDP pages.
-      $('.acq-content-product .form-item-configurables-size, .acq-content-product-modal .form-item-configurables-size').once('bind-events').each(function () {
-        $('select', $(this)).select2Option();
+      function select2OptionConvert() {
+        if ($(window).width() > 1024) {
+          $('.acq-content-product .form-item-configurables-size, .acq-content-product-modal .form-item-configurables-size').once('bind-events').each(function () {
+            $('select', $(this)).select2Option();
 
-        var clickedOption = $('.select2Option li a.picked', $(this));
-        $('.select2Option', $(this)).find('.list-title .selected-text').remove();
-        $('.select2Option', $(this)).find('.list-title').append('<span class="selected-text">' + clickedOption.text() + '</span>');
-      });
+            var clickedOption = $('.select2Option li a.picked', $(this));
+            $('.select2Option', $(this)).find('.list-title .selected-text').remove();
+            $('.select2Option', $(this)).find('.list-title').append('<span class="selected-text">' + clickedOption.text() + '</span>');
+          });
+        }
+      }
     }
   };
 
