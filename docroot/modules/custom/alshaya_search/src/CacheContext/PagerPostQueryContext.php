92ccddc4afdbfec303856ad5120e383e2ad9a00b
piyuesh23 <piyuesh23@qed42.com>
2017-06-25 19:13:53 +0530

    MMCPA-1079: Optimizing search result page load more.

---
+++ docroot/modules/custom/alshaya_search/src/CacheContext/PagerPostQueryContext.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\alshaya_search\CacheContext;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Cache\Context\CacheContextInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Class PagerPostQueryContext.
+ *
+ * @package Drupal\alshaya_search
+ */
+class PagerPostQueryContext implements CacheContextInterface {
+
+  /**
+   * Symfony\Component\HttpFoundation\RequestStack definition.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * Constructs a new PagerPostQueryContext object.
+   */
+  public function __construct(RequestStack $request_stack) {
+    $this->requestStack = $request_stack;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getLabel() {
+    drupal_set_message('Lable of cache context');
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @throws \InvalidArgumentException
+   */
+  public function getContext() {
+    $current_request = $this->requestStack->getCurrentRequest();
+    $page_num = $current_request->request->get('page');
+    return $page_num;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheableMetadata() {
+    return new CacheableMetadata();
+  }
+
+}
