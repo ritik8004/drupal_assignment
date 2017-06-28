9a94967dfbfeeeb4b91579e3bbbd9e44087c5925
piyuesh23 <piyuesh23@qed42.com>
2017-06-28 16:01:51 +0530

    MMCPA-255: Render promo link as text if on the same promo page.

--- docroot/modules/custom/alshaya_acm_product/src/Plugin/Field/FieldFormatter/SkuGalleryFormatter.php
+++ docroot/modules/custom/alshaya_acm_product/src/Plugin/Field/FieldFormatter/SkuGalleryFormatter.php
@@ -9,6 +9,7 @@ use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\CurrentRouteMatch;
 use Drupal\node\Entity\Node;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -33,6 +34,13 @@ class SkuGalleryFormatter extends SKUFieldFormatter implements ContainerFactoryP
   protected $skuManager;
 
   /**
+   * The current route matcher service.
+   *
+   * @var \Drupal\Core\Routing\CurrentRouteMatch
+   */
+  protected $currentRouteMatch;
+
+  /**
    * SkuGalleryFormatter constructor.
    *
    * @param string $plugin_id
@@ -49,6 +57,8 @@ class SkuGalleryFormatter extends SKUFieldFormatter implements ContainerFactoryP
    *   The view mode.
    * @param array $third_party_settings
    *   Any third party settings.
+   * @param \Drupal\Core\Routing\CurrentRouteMatch $currentRouteMatch
+   *   Current route matcher service.
    * @param \Drupal\alshaya_acm_product\SkuManager $skuManager
    *   Sku Manager service.
    */
@@ -59,9 +69,11 @@ class SkuGalleryFormatter extends SKUFieldFormatter implements ContainerFactoryP
                               $label,
                               $view_mode,
                               array $third_party_settings,
+                              CurrentRouteMatch $currentRouteMatch,
                               SkuManager $skuManager) {
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
     $this->skuManager = $skuManager;
+    $this->currentRouteMatch = $currentRouteMatch;
   }
 
   /**
@@ -124,12 +136,21 @@ class SkuGalleryFormatter extends SKUFieldFormatter implements ContainerFactoryP
         }
       }
 
-      $promotions = $this->skuManager->getPromotionsFromSkuId($sku, TRUE);
-
-      if (!empty($promotions)) {
-        $promotions = [
-          '#markup' => implode(', ', $promotions),
-        ];
+      $promotions = $this->skuManager->getPromotionsFromSkuId($sku);
+      $current_route_name = $this->currentRouteMatch->getRouteName();
+      $current_node = $this->currentRouteMatch->getParameter('node');
+      // Check if current page is promotion page,
+      // render current promotion as text.
+      if (($current_route_name === 'entity.node.canonical') &&
+      ($current_node->bundle() === 'acq_promotion')) {
+        foreach ($promotions as $key => $promotion) {
+          if ((int) $current_node->id() === $key) {
+            $promotions[$key]['render_link'] = FALSE;
+          }
+          else {
+            $promotions[$key]['render_link'] = TRUE;
+          }
+        }
       }
 
       $sku_gallery = [
@@ -194,6 +215,7 @@ class SkuGalleryFormatter extends SKUFieldFormatter implements ContainerFactoryP
       $configuration['label'],
       $configuration['view_mode'],
       $configuration['third_party_settings'],
+      $container->get('current_route_match'),
       $container->get('alshaya_acm_product.skumanager')
     );
   }
