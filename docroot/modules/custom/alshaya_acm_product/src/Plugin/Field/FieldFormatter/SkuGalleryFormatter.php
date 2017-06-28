5379d8c0723d1dd780b7ff48c9cb2a3b2a130541
piyuesh23 <piyuesh23@qed42.com>
2017-06-28 16:06:45 +0530

    MMCPA-255: Updating sku cache context.

--- docroot/modules/custom/alshaya_acm_product/src/Plugin/Field/FieldFormatter/SkuGalleryFormatter.php
+++ docroot/modules/custom/alshaya_acm_product/src/Plugin/Field/FieldFormatter/SkuGalleryFormatter.php
@@ -172,6 +172,7 @@ class SkuGalleryFormatter extends SKUFieldFormatter implements ContainerFactoryP
         '#promotions' => $promotions,
         '#cache' => [
           'tags' => ['sku:' . $sku->id()],
+          'contexts' => ['url'],
         ],
       ];
 
