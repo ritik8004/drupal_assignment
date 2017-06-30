6896c1d9475bf53bffed200a0f265c73e0fe7144
Sylvain DELBOSC <sylvain.delbosc@acquia.com>
2017-06-30 14:33:30 +0200

    Merge pull request #1585 from vbouchet31/fix-magento-path
    MMCPA-00: Update the magento_host path to match the magento behind conductor

--- factory-hooks/pre-settings-php/includes.php
+++ factory-hooks/pre-settings-php/includes.php
@@ -64,7 +64,7 @@ switch ($env) {
   default:
     $config['acq_commerce.conductor']['url'] = 'https://uat.dev.alshaya.acm.acquia.io/';
 
-    $config['alshaya_api.settings']['magento_host'] = 'https://staging-api.mothercare.com.kw.c.z3gmkbwmwrl4g.ent.magento.cloud';
+    $config['alshaya_api.settings']['magento_host'] = 'https://master-7rqtwti-z3gmkbwmwrl4g.eu.magentosite.cloud';
     $config['alshaya_api.settings']['magento_lang_prefix'] = 'kwt_';
     $config['alshaya_api.settings']['magento_api_base'] = 'rest/V1';
     $config['alshaya_api.settings']['verify_ssl'] = 0;
