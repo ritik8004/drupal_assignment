4a050a3f7af5085344f631467fc9a6a58503c3e7
Vaibhav Jain <vaibhav.jain@acquia.com>
2017-07-11 17:19:48 +0530

    MMCPA-1723: Created a new method in user service to provide formatted number, and removed old code.

--- docroot/modules/custom/alshaya_user/src/AlshayaUserInfo.php
+++ docroot/modules/custom/alshaya_user/src/AlshayaUserInfo.php
@@ -5,6 +5,8 @@ namespace Drupal\alshaya_user;
 use Drupal\Core\Session\AccountProxy;
 use Drupal\Core\Entity\EntityRepository;
 use Drupal\user\Entity\User;
+use Drupal\mobile_number\MobileNumberUtil;
+use libphonenumber\PhoneNumberFormat;
 
 /**
  * Class AlshayaUserInfo.
@@ -35,17 +37,27 @@ class AlshayaUserInfo {
   public $userObject;
 
   /**
+   * The Mobile number util service object.
+   *
+   * @var \Drupal\mobile_number\MobileNumberUtil
+   */
+  public $mobileUtil;
+
+  /**
    * AlshayaUserInfo constructor.
    *
    * @param \Drupal\Core\Session\AccountProxy $currentUser
    *   The current account object.
    * @param \Drupal\Core\Entity\EntityRepository $entityRepository
    *   The entity repository service.
+   * @param \Drupal\mobile_number\MobileNumberUtil $mobileNumberUtil
+   *   The Mobile number util service object.
    */
-  public function __construct(AccountProxy $currentUser, EntityRepository $entityRepository) {
+  public function __construct(AccountProxy $currentUser, EntityRepository $entityRepository, MobileNumberUtil $mobileNumberUtil) {
     $this->currentUser = $currentUser;
     $this->entityRepository = $entityRepository;
     $this->userObject = $this->entityRepository->getTranslationFromContext(User::load($this->currentUser->id()));
+    $this->mobileUtil = $mobileNumberUtil;
   }
 
   /**
@@ -80,6 +92,22 @@ class AlshayaUserInfo {
   public function getName($glue = ' ') {
     if (!empty($this->getFirstName())) {
       return implode($glue, [$this->getFirstName(), $this->getLastName()]);
+    }
+    return '';
+  }
+
+  /**
+   * Get formatter mobile number.
+   *
+   * @return mixed|string
+   *   Formatted mobile number string or empty if it does not exist.
+   */
+  public function getFormattedMobileNumber() {
+    $mobileNumber = explode(', ', $this->userObject->get('field_mobile_number')->getString())[0];
+    if (!empty($mobileNumber)) {
+      $mobile_value = $this->mobileUtil->getMobileNumber($mobileNumber);
+      $mobile_number = $this->mobileUtil->libUtil()->format($mobile_value, PhoneNumberFormat::INTERNATIONAL);
+      return str_replace(' ', ' - ', $mobile_number);
     }
     return '';
   }
