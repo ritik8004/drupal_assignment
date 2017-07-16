1f388ab0c9febca0989965e413a229a11ebe23a5
Saket Kumar <saki007ster@gmail.com>
2017-07-16 10:57:49 +0300

    MMCPA-0: Fixing RTL issues for PDP cross0sell and upsell.

--- docroot/themes/custom/alshaya_white_label/js/sell.js
+++ docroot/themes/custom/alshaya_white_label/js/sell.js
@@ -40,6 +40,46 @@
         $('.block-basket-horizontal-recommendation .owl-carousel').owlCarousel(options);
       }
 
+      var optionsPdp = {
+        loop: true,
+        responsiveClass: true,
+        dots: true,
+        responsive: {
+          0: {
+            items: 1,
+            nav: false
+          },
+          768: {
+            items: 2,
+            nav: true
+          },
+          1025: {
+            items: 3,
+            nav: true
+          }
+        }
+      };
+
+      if (isRTL()) {
+        $('.horizontal-crossell .owl-carousel').attr('dir', 'rtl');
+        $('.horizontal-crossell .owl-carousel').owlCarousel(
+          $.extend({}, optionsPdp, {rtl: true})
+        );
+      }
+      else {
+        $('.horizontal-crossell .owl-carousel').owlCarousel(optionsPdp);
+      }
+
+      if (isRTL()) {
+        $('.horizontal-upell .owl-carousel').attr('dir', 'rtl');
+        $('.horizontal-upell .owl-carousel').owlCarousel(
+          $.extend({}, optionsPdp, {rtl: true})
+        );
+      }
+      else {
+        $('.horizontal-upell .owl-carousel').owlCarousel(optionsPdp);
+      }
+
       $('.owl-carousel').owlCarousel({
         loop: true,
         responsiveClass: true,
@@ -59,7 +99,6 @@
           }
         }
       });
-
     }
   };
 })(jQuery, Drupal);
