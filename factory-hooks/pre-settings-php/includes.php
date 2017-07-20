44c0e9476a78678e5fa83574ff38aaad24f8b8d3
Rohit Joshi <joshi.rohit100@gmail.com>
2017-07-20 16:21:49 +0300

    MMCPA-001: Adding backslah to the item.

--- factory-hooks/pre-settings-php/includes.php
+++ factory-hooks/pre-settings-php/includes.php
@@ -46,7 +46,7 @@ switch ($env) {
 
   case '01dev':
   case '01test':
-    $config['acq_commerce.conductor']['url'] = 'https://agent.dev.alshaya.acm.acquia.io';
+    $config['acq_commerce.conductor']['url'] = 'https://agent.dev.alshaya.acm.acquia.io/';
 
     $config['alshaya_api.settings']['magento_host'] = 'https://master-7rqtwti-z3gmkbwmwrl4g.eu.magentosite.cloud';
     $config['alshaya_api.settings']['magento_lang_prefix'] = 'kwt_';
