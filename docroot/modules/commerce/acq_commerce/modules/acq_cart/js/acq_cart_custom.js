2456bf330763c914727470b95b92802d1769098d
piyuesh23 <piyuesh23@qed42.com>
2017-07-21 16:06:34 +0530

    MMCPA-00: Reverting incomplete commits.

--- docroot/modules/commerce/acq_commerce/modules/acq_cart/js/acq_cart_custom.js
+++ docroot/modules/commerce/acq_commerce/modules/acq_cart/js/acq_cart_custom.js
@@ -1,17 +1,36 @@
 (function ($, Drupal) {
   'use strict';
 
+  var acq_cart_mini_cart_xhr;
+  var acq_cart_mini_cart_timeout;
+
   Drupal.behaviors.acq_cart_js = {
     attach: function (context, settings) {
-      if (context === document) {
-        $.ajax({
-          url: Drupal.url('mini-cart'),
-          success: function (result) {
-            $('#mini-cart-wrapper').html(result);
-          }
-        });
+      // Do the processing only if we have mini cart available in DOM.
+      if ($('body').find('#mini-cart-wrapper').length) {
+        // Clear time out, behaviors can be called multiple times.
+        if (acq_cart_mini_cart_timeout) {
+          clearTimeout(acq_cart_mini_cart_timeout);
+        }
+
+        // Let other JS operations finish.
+        acq_cart_mini_cart_timeout = setTimeout(Drupal.updateMiniCart, 50);
       }
     }
+  };
+
+  Drupal.updateMiniCart = function () {
+    // Abort the AJAX operation if already started before starting new one.
+    if (acq_cart_mini_cart_xhr && acq_cart_mini_cart_xhr.readyState !== 4) {
+      acq_cart_mini_cart_xhr.abort();
+    }
+
+    acq_cart_mini_cart_xhr = $.ajax({
+      url: Drupal.url('mini-cart'),
+      success: function (result) {
+        $('#mini-cart-wrapper').html(result);
+      }
+    });
   };
 
 })(jQuery, Drupal);
