1ff404a520e6b456c6a7ba8751366c45c3427b85
piyuesh23 <piyuesh23@qed42.com>
2017-08-07 17:26:59 +0530

    MMCPA-1189: Overriding brand value for configurable skus to make it consistent.

--- docroot/modules/custom/alshaya_seo/src/AlshayaGtmManager.php
+++ docroot/modules/custom/alshaya_seo/src/AlshayaGtmManager.php
@@ -294,6 +294,7 @@ class AlshayaGtmManager {
 
     if ($parent_sku = alshaya_acm_product_get_parent_sku_by_sku($skuId)) {
       $attributes['gtm-sku-type'] = $parent_sku->bundle();
+      $attributes['gtm-brand'] = $parent_sku->get('attr_product_brand')->getString() ?: 'Mothercare Kuwait';
     }
 
     return $attributes;
