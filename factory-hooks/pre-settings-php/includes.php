f5aa310289fb3c114058a29ea07bcd382965e8c0
Sylvain DELBOSC <sylvain.delbosc@acquia.com>
2017-08-07 20:42:12 +0200

    Revert "Merge pull request #2098 from acquia-pso/revert-2086-conductor-update"
    This reverts commit 7f79b3640c5e2387e6ad4d8669a07e8e0b2a8a2f, reversing
    changes made to e58fba196f5bcc0ea2ad6b00f504b4ccb635dc7f.

--- factory-hooks/pre-settings-php/includes.php
+++ factory-hooks/pre-settings-php/includes.php
@@ -20,15 +20,37 @@ if (empty($config_directories)) {
 }
 
 // Default credentials.
+// TODO: Security.
 $settings['alshaya_custom_shield_default_user'] = 'alshaya_shield';
 $settings['alshaya_custom_shield_default_pass'] = 'AS_S';
+
+// TODO: Security.
+$settings['alshaya_acm_user_username'] = 'alshaya_acm';
+$settings['alshaya_acm_user_email'] = 'noreply-acm@alshaya.com';
+$settings['alshaya_acm_user_password'] = 'AlShAyA_AcM';
 
 // Conductor settings.
 $env = isset($_ENV['AH_SITE_ENVIRONMENT']) ? $_ENV['AH_SITE_ENVIRONMENT'] : 'local';
 
 // Set the knet resource path which should be outside GIT root.
-$config['alshaya_acm_knet.settings']['resource_path'] = '/home/alshaya/knet-resource/' . $env . '/mckw/';
+$knet_resource_dir = $env == 'local' ? '/home/vagrant/knet-resource/' : '/home/alshaya/knet-resource/' . $env . '/mckw/';
+$config['alshaya_acm_knet.settings']['resource_path'] = $knet_resource_dir;
 $config['alshaya_acm_knet.settings']['use_secure_response_url'] = 0;
+
+// TODO: Security.
+$soauth_key_dir = $env == 'local' ? '/var/www/alshaya/box/' : '/home/alshaya/simple-oauth/' . $env . '/';
+$settings['alshaya_acm_soauth_public_key'] = $soauth_key_dir . 'alshaya_acm.pub';
+$settings['alshaya_acm_soauth_private_key'] = $soauth_key_dir . 'alshaya_acm';
+$settings['alshaya_acm_soauth_client_secret'] = 'AlShAyA';
+$settings['alshaya_acm_soauth_client_uuid'] = '35b9a28a-939f-4e2b-be55-9445c5b6549e';
+
+// Common conductor settings.
+$config['alshaya_api.settings']['magento_lang_prefix'] = 'kwt_';
+$config['alshaya_api.settings']['magento_api_base'] = 'rest/V1';
+$config['alshaya_api.settings']['verify_ssl'] = 0;
+
+$config['alshaya_api.settings']['username'] = 'acquiaapi';
+$config['alshaya_api.settings']['password'] = 'gF2Fkndy8Erb';
 
 // Set the debug dir of conductor.
 $config['acq_commerce.conductor']['debug_dir'] = '/home/alshaya/' . $env;
@@ -40,33 +62,23 @@ switch ($env) {
     global $_alshaya_acm_disable_stock_check;
     $_alshaya_acm_disable_stock_check = TRUE;
 
-    // Set the knet resource path which should be outside GIT root.
-    $config['alshaya_acm_knet.settings']['resource_path'] = '/home/vagrant/knet-resource/';
     $config['acq_commerce.conductor']['debug'] = FALSE;
+
+    $config['acq_commerce.conductor']['url'] = 'https://alshaya-dev.prod.acm.acquia.io/';
+    $config['acq_commerce.conductor']['hmac_id'] = 'uAfqsl!BMf5xd8Z';
+    $config['acq_commerce.conductor']['hmac_secret'] = 'eS#8&0@XyegNUO';
+    $config['alshaya_api.settings']['magento_host'] = 'https://master-7rqtwti-z3gmkbwmwrl4g.eu.magentosite.cloud';
+    break;
 
   case '01dev':
   case '01test':
     $config['acq_commerce.conductor']['url'] = 'https://uat.dev.alshaya.acm.acquia.io/';
-
     $config['alshaya_api.settings']['magento_host'] = 'https://master-7rqtwti-z3gmkbwmwrl4g.eu.magentosite.cloud';
-    $config['alshaya_api.settings']['magento_lang_prefix'] = 'kwt_';
-    $config['alshaya_api.settings']['magento_api_base'] = 'rest/V1';
-    $config['alshaya_api.settings']['verify_ssl'] = 0;
-
-    $config['alshaya_api.settings']['username'] = 'acquiaapi';
-    $config['alshaya_api.settings']['password'] = 'gF2Fkndy8Erb';
   break;
 
   default:
     $config['acq_commerce.conductor']['url'] = 'https://pprod.dev.alshaya.acm.acquia.io/';
-
     $config['alshaya_api.settings']['magento_host'] = 'https://staging-api.mothercare.com.kw.c.z3gmkbwmwrl4g.ent.magento.cloud';
-    $config['alshaya_api.settings']['magento_lang_prefix'] = 'kwt_';
-    $config['alshaya_api.settings']['magento_api_base'] = 'rest/V1';
-    $config['alshaya_api.settings']['verify_ssl'] = 0;
-
-    $config['alshaya_api.settings']['username'] = 'acquiaapi';
-    $config['alshaya_api.settings']['password'] = 'gF2Fkndy8Erb';
     break;
 }
 
