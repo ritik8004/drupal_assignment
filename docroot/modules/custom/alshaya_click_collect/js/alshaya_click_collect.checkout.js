4c5e83f0be8dd04a42fae198101b35fe10e6522f
Swarad Mokal <swarad07@gmail.com>
2017-08-09 21:03:00 +0300

    MMCPA-2100: Revert checkout click and collect js file.

--- docroot/modules/custom/alshaya_click_collect/js/alshaya_click_collect.checkout.js
+++ docroot/modules/custom/alshaya_click_collect/js/alshaya_click_collect.checkout.js
@@ -256,29 +256,6 @@
     selectedOnMap.execute();
   };
 
-  function offsetCenter(map, latlng, offsetx, offsety) {
-
-    // latlng is the apparent centre-point
-    // offsetx is the distance you want that point to move to the right, in pixels
-    // offsety is the distance you want that point to move upwards, in pixels
-    // offset can be negative
-    // offsetx and offsety are both optional
-
-    var scale = Math.pow(2, map.getZoom());
-
-    var worldCoordinateCenter = map.getProjection().fromLatLngToPoint(latlng);
-    var pixelOffset = new google.maps.Point((offsetx/scale) || 0,(offsety/scale) ||0);
-
-    var worldCoordinateNewCenter = new google.maps.Point(
-      worldCoordinateCenter.x - pixelOffset.x,
-      worldCoordinateCenter.y + pixelOffset.y
-    );
-
-    var newCenter = map.getProjection().fromPointToLatLng(worldCoordinateNewCenter);
-
-    map.setCenter(newCenter);
-  }
-
   // View selected store on map
   Drupal.checkoutClickCollect.storeViewOnMapSelected = function (StoreObj, makerIndex) {
     // Create/Get map object.
@@ -292,9 +269,7 @@
     // Get the lat/lng of current store to center the map.
     var newLocation = new google.maps.LatLng(parseFloat(StoreObj.lat), parseFloat(StoreObj.lng));
     // Set the google map center.
-    // map.googleMap.setCenter(newLocation);
-
-    offsetCenter(map.googleMap, newLocation, 100, 500);
+    map.googleMap.setCenter(newLocation);
   };
 
   // Display All the stores on map.
