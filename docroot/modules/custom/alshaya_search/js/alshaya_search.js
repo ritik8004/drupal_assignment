b84cbbe7c452cf8c5343a855c70155c8e8fd37aa
piyuesh23 <piyuesh23@qed42.com>
2017-08-10 19:42:36 +0530

    MMCPA-00: Reverting accidental changes.

--- docroot/modules/custom/alshaya_search/js/alshaya_search.js
+++ docroot/modules/custom/alshaya_search/js/alshaya_search.js
@@ -162,11 +162,10 @@
               .attr('rel'));
         }
       );
+
+      $.fn.alshayaAttachSearchSlider = function () {
+        Drupal.attachBehaviors(context);
+      };
     }
   };
-
-  $.fn.alshayaAttachSearchSlider = function () {
-    Drupal.attachBehaviors(context);
-  };
-
 })(jQuery);
