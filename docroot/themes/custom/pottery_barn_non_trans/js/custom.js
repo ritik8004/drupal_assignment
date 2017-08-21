ea2767891a885b0317f2f72eb7858dad18c837bf
Mac Central_Team <>
2017-08-21 16:22:06 +0300

    MMCPA-000: PBKSA defect fixing and intrgrated language switcher in mobile view. Add MailChimp form.

--- docroot/themes/custom/pottery_barn_non_trans/js/custom.js
+++ docroot/themes/custom/pottery_barn_non_trans/js/custom.js
@@ -3,32 +3,64 @@
  * Custom js file.
  */
 
- (function ($, Drupal) {
-   'use strict';
+(function ($, Drupal) {
+  'use strict';
 
   // Home page email sign up form popup.
-   var SignupField = $('.form--signup-elements');
+  var SignupField = $('.form--signup-elements');
 
-   if (SignupField.hasClass('form-item--error')) {
-     $('.signup-popup').show();
-   }
-   else {
-     $('.signup-popup').hide();
-   }
+  if (SignupField.hasClass('form-item--error')) {
+    $('.signup-popup').show();
+  }
+  else {
+    $('.signup-popup').hide();
+  }
 
-   $('#contact, .email-signup').on('click', function (e) {
-     $('.signup-popup').show();
-     e.preventDefault();
-   });
+  $('#contact, .email-signup').on('click', function (e) {
+    $('.signup-popup').show();
+    e.preventDefault();
+  });
 
-   $('.c-footer__copy a').on('click', function (e) {
-     $('.privacy-popup').show();
-     e.preventDefault();
-   });
+  $('.c-footer__copy a').on('click', function (e) {
+    $('.privacy-popup').show();
+    e.preventDefault();
+  });
 
-   $('.close-popup').on('click', function () {
-     $('.signup-popup, .privacy-popup').hide();
-     $('.messages--status').hide();
-   });
+  $('.close-popup').on('click', function () {
+    $('.signup-popup, .privacy-popup').hide();
+    $('.messages--status').hide();
+  });
 
- })(jQuery, Drupal);
+  Drupal.behaviors.potteryBarnStoreFinder = {
+    attach: function (context, settings) {
+      // Store Finder, opening hours toggle.
+      // @todo: Remove when the site is reinstalled, or configs are imported.
+      $('.view-id-stores_finder, .geolocation-common-map-container')
+        .on('click', '.hours--wrapper', function () {
+          $(this).find('.hours--label').toggleClass('open');
+        });
+    }
+  };
+
+  // Mobile Language Toggle
+  // Language Settings In Mobile View
+  if ($(window).width() < 767) {
+    setTimeout(function () {
+      $('body').addClass('mobile-language-toggle-active');
+    }, 1000);
+
+    $(window).scroll(function () {
+      if ($(window).scrollTop() + $(window).height() > $(document)
+          .height() - 100) {
+        $('body').removeClass('mobile-language-toggle-active');
+      }
+    });
+  }
+
+  $('.close-lang-toggle').click(function () {
+    setTimeout(function () {
+      $('body').removeClass('mobile-language-toggle-active');
+    }, 1000);
+  });
+
+})(jQuery, Drupal);
