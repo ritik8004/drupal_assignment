9a306c461f0ab1a69e5abc0ac5154b0c147e9857
vbouchet <vincent.bouchet.31@gmail.com>
2017-08-24 12:41:03 +0300

    ACR2-00: Review how bash is called.

--- blt/scripts/frontend-build.sh
+++ blt/scripts/frontend-build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # This file runs during the frontend build.
 
 set -e
--- blt/scripts/frontend-setup.sh
+++ blt/scripts/frontend-setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # This file runs during the frontend setup.
 
 set -e
