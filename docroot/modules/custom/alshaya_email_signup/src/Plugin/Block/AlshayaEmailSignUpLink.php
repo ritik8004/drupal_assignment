0196c425f8242c3778fca4f588b05da913e47822
Mitesh Patel <developermitesh@gmail.com>
2017-09-04 14:24:14 +0530

    ACR2-90: Create email signup link block and place it in header.

---
+++ docroot/modules/custom/alshaya_email_signup/src/Plugin/Block/AlshayaEmailSignUpLink.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\alshaya_email_signup\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Url;
+
+/**
+ * Provides a block to display 'custom link' elements.
+ *
+ * @Block(
+ *   id = "alshaya_email_signup_link",
+ *   admin_label = @Translation("Email signup Link")
+ * )
+ */
+class AlshayaEmailSignUpLink extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#type' => 'link',
+      '#title' => $this->t('Email sign up'),
+      '#url' => Url::fromRoute('<front>'),
+      '#attributes' => [
+        'class' => ['use-ajax'],
+        'data-dialog-type' => 'modal',
+        'data-dialog-options' => json_encode([
+          'width' => 700,
+        ]),
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheTags() {
+    return Cache::mergeTags(parent::getCacheTags(), ['email-signup-link']);
+  }
+
+}
