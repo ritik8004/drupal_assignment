2bd8fa76948e384a397c6b114e9c0714d7097e3f
nikunj <me@nik4u.com>
2017-09-04 11:03:38 +0530

    MMCPA-2250: Adding settings for non-buyable texts and field in sku.

--- docroot/modules/custom/alshaya_acm_product/src/Form/ProductSettingsForm.php
+++ docroot/modules/custom/alshaya_acm_product/src/Form/ProductSettingsForm.php
@@ -31,6 +31,8 @@ class ProductSettingsForm extends ConfigFormBase {
     $config = $this->config('alshaya_acm_product.settings');
     $config->set('brand_logo_base_path', $form_state->getValue('brand_logo_base_path'));
     $config->set('brand_logo_extension', $form_state->getValue('brand_logo_extension'));
+    $config->set('non_buyable_message', $form_state->getValue('non_buyable_message'));
+    $config->set('non_buyable_help_text', $form_state->getValue('non_buyable_help_text'));
     $config->set('size_guide_link', $form_state->getValue('size_guide_link'));
     $config->set('size_guide_modal_content', $form_state->getValue('size_guide_modal_content'));
     $config->save();
@@ -60,6 +62,20 @@ class ProductSettingsForm extends ConfigFormBase {
       '#description' => $this->t('Do not include leading dots.'),
       '#required' => TRUE,
       '#default_value' => $config->get('brand_logo_extension'),
+    ];
+
+    $form['non_buyable_message'] = [
+      '#type' => 'text_format',
+      '#format' => 'rich_text',
+      '#title' => $this->t('Non-buyable product message'),
+      '#default_value' => $config->get('non_buyable_message.value'),
+    ];
+
+    $form['non_buyable_help_text'] = [
+      '#type' => 'text_format',
+      '#format' => 'rich_text',
+      '#title' => $this->t('Non-buyable product help text'),
+      '#default_value' => $config->get('non_buyable_help_text.value'),
     ];
 
     $form['size_guide_link'] = [
