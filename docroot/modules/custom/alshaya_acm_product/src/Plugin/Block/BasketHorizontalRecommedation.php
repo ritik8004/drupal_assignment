d44568e0e4c50fe1c2360997f03e958412b85b5d
Rohit Joshi <joshi.rohit100@gmail.com>
2017-09-07 14:13:45 +0530

    MMCPA-2243: Adding chnages for upsell and cross sell products.

--- docroot/modules/custom/alshaya_acm_product/src/Plugin/Block/BasketHorizontalRecommedation.php
+++ docroot/modules/custom/alshaya_acm_product/src/Plugin/Block/BasketHorizontalRecommedation.php
@@ -5,6 +5,7 @@ namespace Drupal\alshaya_acm_product\Plugin\Block;
 use Drupal\acq_sku\Entity\SKU;
 use Drupal\Core\Block\BlockBase;
 use Drupal\acq_cart\CartStorageInterface;
+use Drupal\acq_sku\AcqSkuLinkedSku;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 
@@ -26,6 +27,13 @@ class BasketHorizontalRecommedation extends BlockBase implements ContainerFactor
   protected $cartStorage;
 
   /**
+   * The linked sku service.
+   *
+   * @var \Drupal\acq_sku\AcqSkuLinkedSku
+   */
+  protected $linkedSkus;
+
+  /**
    * Constructor.
    *
    * @param array $configuration
@@ -36,10 +44,13 @@ class BasketHorizontalRecommedation extends BlockBase implements ContainerFactor
    *   The plugin implementation definition.
    * @param \Drupal\acq_cart\CartStorageInterface $cart_storage
    *   The cart session.
+   * @param \Drupal\acq_sku\AcqSkuLinkedSku $linked_skus
+   *   Linked sku service.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, CartStorageInterface $cart_storage) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, CartStorageInterface $cart_storage, AcqSkuLinkedSku $linked_skus) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->cartStorage = $cart_storage;
+    $this->linkedSkus = $linked_skus;
   }
 
   /**
@@ -50,7 +61,8 @@ class BasketHorizontalRecommedation extends BlockBase implements ContainerFactor
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('acq_cart.cart_storage')
+      $container->get('acq_cart.cart_storage'),
+      $container->get('acq_sku.linked_sku')
     );
   }
 
@@ -76,19 +88,14 @@ class BasketHorizontalRecommedation extends BlockBase implements ContainerFactor
         }
       }
 
-      foreach ($skus as $sku) {
-        if ($sku_entity = SKU::loadFromSku($sku)) {
-          $cross_sell_skus = $sku_entity->getCrossSell();
-          foreach ($cross_sell_skus as $cross_sell_sku) {
-            $view_skus[] = $cross_sell_sku['value'];
+      if (!empty($skus)) {
+        foreach ($skus as $sku) {
+          if ($sku_entity = SKU::loadFromSku($sku)) {
+            // Get crosssell skus.
+            $view_skus += $this->linkedSkus->getLinkedSKus($sku_entity, 'crosssell');
           }
         }
       }
-    }
-
-    if (!empty($view_skus)) {
-      // Get all cross sell SKU.
-      $view_skus = array_diff($view_skus, $skus);
     }
 
     if (!empty($view_skus)) {
