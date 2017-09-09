8d5bd28112a0e2e459c3ab85c416998ca187bfcc
nikunj <me@nik4u.com>
2017-09-09 15:56:23 +0530

    MMCPA-1799: Adding conditions to avoid memcache in travis.

--- factory-hooks/post-settings-php/memcache.php
+++ factory-hooks/post-settings-php/memcache.php
@@ -8,9 +8,6 @@
 
 // We don't do anything on travis.
 if (!getenv('TRAVIS')) {
-  // Enable memcache.
-  $settings['additional_modules'][] = 'memcache';
-
   // Default settings.
   $settings['memcache']['extension'] = 'Memcached';
   $settings['memcache']['stampede_protection'] = TRUE;
@@ -18,7 +15,6 @@ if (!getenv('TRAVIS')) {
   // Set default cache backend to memcache.
   $settings['cache']['default'] = 'cache.backend.memcache';
 
-  // Enable pcb_memcache for stock and set it by default.
-  $settings['additional_modules'][] = 'pcb_memcache';
+  // Use pcb_memcache for stock.
   $settings['cache']['bins']['stock'] = 'cache.backend.permanent_memcache';
 }
