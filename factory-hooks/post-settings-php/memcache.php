c6c26d494fa174feb3c0df112373f3e03ba32279
nikunj <me@nik4u.com>
2017-09-09 16:08:12 +0530

    MMCPA-1799: Enable memcache on travis.

--- factory-hooks/post-settings-php/memcache.php
+++ factory-hooks/post-settings-php/memcache.php
@@ -6,15 +6,12 @@
  * @see https://docs.acquia.com/site-factory/tiers/paas/workflow/hooks
  */
 
-// We don't do anything on travis.
-if (!getenv('TRAVIS')) {
-  // Default settings.
-  $settings['memcache']['extension'] = 'Memcached';
-  $settings['memcache']['stampede_protection'] = TRUE;
+// Default settings.
+$settings['memcache']['extension'] = 'Memcached';
+$settings['memcache']['stampede_protection'] = TRUE;
 
-  // Set default cache backend to memcache.
-  $settings['cache']['default'] = 'cache.backend.memcache';
+// Set default cache backend to memcache.
+$settings['cache']['default'] = 'cache.backend.memcache';
 
-  // Use pcb_memcache for stock.
-  $settings['cache']['bins']['stock'] = 'cache.backend.permanent_memcache';
-}
+// Use pcb_memcache for stock.
+$settings['cache']['bins']['stock'] = 'cache.backend.permanent_memcache';
