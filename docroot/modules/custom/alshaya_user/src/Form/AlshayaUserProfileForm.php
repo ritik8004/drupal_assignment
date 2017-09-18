5408ce958ce0fc56ef7b23eadfec8d1f0c084d78
nikunj <me@nik4u.com>
2017-09-19 00:32:55 +0530

    MMCPA-1939: Overridden user entity ProfileForm class to set custom message.

---
+++ docroot/modules/custom/alshaya_user/src/Form/AlshayaUserProfileForm.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\alshaya_user\Form;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\user\ProfileForm;
+
+/**
+ * Form controller for user forms (edit).
+ */
+class AlshayaUserProfileForm extends ProfileForm {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    $account = $this->entity;
+    $account->save();
+    $form_state->setValue('uid', $account->id());
+
+    drupal_set_message($this->t('Contact details changes have been saved.'));
+  }
+
+}
