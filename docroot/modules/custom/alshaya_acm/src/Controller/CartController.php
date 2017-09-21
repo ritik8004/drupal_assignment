1aa8d254f63d76dbe969f4180b077e9bae8f4640
nikunj <me@nik4u.com>
2017-09-21 15:59:28 +0530

    MMCPA-2312: Enable CSRF Token validation for removing items from cart.

--- docroot/modules/custom/alshaya_acm/src/Controller/CartController.php
+++ docroot/modules/custom/alshaya_acm/src/Controller/CartController.php
@@ -2,9 +2,11 @@
 
 namespace Drupal\alshaya_acm\Controller;
 
+use Drupal\Core\Access\CsrfTokenGenerator;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\acq_cart\CartStorageInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * Class CartController.
@@ -26,14 +28,24 @@ class CartController extends ControllerBase {
   protected $cart;
 
   /**
+   * CSRF Token generator object.
+   *
+   * @var \Drupal\Core\Access\CsrfTokenGenerator
+   */
+  protected $csrfTokenGenerator;
+
+  /**
    * Constructor.
    *
    * @param \Drupal\acq_cart\CartStorageInterface $cart_storage
    *   The cart storage.
+   * @param \Drupal\Core\Access\CsrfTokenGenerator $csrf_token
+   *   CSRF Token generator object.
    */
-  public function __construct(CartStorageInterface $cart_storage) {
+  public function __construct(CartStorageInterface $cart_storage, CsrfTokenGenerator $csrf_token) {
     $this->cartStorage = $cart_storage;
     $this->cart = $this->cartStorage->getCart();
+    $this->csrfTokenGenerator = $csrf_token;
   }
 
   /**
@@ -41,15 +53,20 @@ class CartController extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('acq_cart.cart_storage')
+      $container->get('acq_cart.cart_storage'),
+      $container->get('csrf_token')
     );
   }
 
   /**
    * Handler for cart/remove/{sku}.
    */
-  public function cartRemoveSku($sku) {
+  public function cartRemoveSku($sku, $token) {
     if (!empty($sku)) {
+      $token_value = $this->cart->id() . '/' . $sku;
+      if (!$this->csrfTokenGenerator->validate($token, $token_value)) {
+        throw new AccessDeniedHttpException();
+      }
 
       // If there is a coupon applied on cart.
       if (!empty($this->cart->getCoupon())) {
@@ -70,6 +87,7 @@ class CartController extends ControllerBase {
     else {
       drupal_set_message($this->t('Oops, something went wrong.'), 'error');
     }
+
     return $this->redirect('acq_cart.cart');
   }
 
