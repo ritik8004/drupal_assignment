b252bef798fb779a1dcd38d274d4e8475c8ba0f2
Murtaza Alvi <smurtazama@gmail.com>
2017-09-26 22:54:34 +0530

    ACR2-610: Sharethis overlay product list.
    Add theme to sharethis on product list.
    Add the patternlab yml files.
    Add the behaviour for sharethis.

---
+++ docroot/themes/custom/whitelabel/components/_patterns/01-molecules/06-content/field-product-listing/node-product-item.js
@@ -0,0 +1,12 @@
+(function ($) {
+  'use strict';
+
+  $(document).ready(function () {
+    var $share_label = $('.view-product-item__share--label');
+    var $product = $('.view-product-item__inner-container');
+
+    $($share_label).on('click', function () {
+      $(this).closest($product).toggleClass('share-overlay');
+    });
+  });
+})(jQuery);
