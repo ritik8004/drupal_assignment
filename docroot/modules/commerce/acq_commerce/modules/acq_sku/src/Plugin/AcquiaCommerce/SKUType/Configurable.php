d95033b6f30c53daf1a00667b74631448de09c45
nikunj <me@nik4u.com>
2017-10-03 10:13:28 +0530

    MMCPA-1137: Handle API outage.

--- docroot/modules/commerce/acq_commerce/modules/acq_sku/src/Plugin/AcquiaCommerce/SKUType/Configurable.php
+++ docroot/modules/commerce/acq_commerce/modules/acq_sku/src/Plugin/AcquiaCommerce/SKUType/Configurable.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\acq_sku\Plugin\AcquiaCommerce\SKUType;
 
+use Drupal\acq_commerce\Conductor\APIWrapper;
 use Drupal\acq_sku\AcquiaCommerce\SKUPluginBase;
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Form\FormStateInterface;
@@ -180,7 +181,7 @@ class Configurable extends SKUPluginBase {
       // Call above is to create cart if empty, we except a new or old cart here
       // and it can be empty if server is not working or in maintenance mode.
       if (empty($cart)) {
-        $e = new \Exception(acq_commerce_api_down_global_error_message(), 500);
+        $e = new \Exception(acq_commerce_api_down_global_error_message(), APIWrapper::API_DOWN_ERROR_CODE);
 
         // Dispatch event so action can be taken.
         $dispatcher = \Drupal::service('event_dispatcher');
