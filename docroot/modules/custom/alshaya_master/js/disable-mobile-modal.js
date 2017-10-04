cb0c817298d92ef9d6f23e08395811627d63ffe1
Mitesh Patel <developermitesh@gmail.com>
2017-10-04 15:47:09 +0530

    ACR2-765: Disable opening link in modal for mobile.

---
+++ docroot/modules/custom/alshaya_master/js/disable-mobile-modal.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * Globaly required scripts to disable modal for mobile.
+ */
+
+(function ($, Drupal) {
+  'use strict';
+
+  if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    $('a[data-dialog-type="modal"]').each(function () {
+      var href = $(this).attr('href');
+      $(this).click(function () {
+        window.open(Drupal.settings.basePath + href);
+        return false;
+      })
+    })
+  }
+
+})(jQuery, Drupal);
+
