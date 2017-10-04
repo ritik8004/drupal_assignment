0e4e30d82f60454ab6672517baf40fbd7d1d184d
prashant114606 <prashant114606@gmail.com>
2017-10-04 15:32:32 +0530

    ACR2-483: H&M basic theme setup.

---
+++ docroot/themes/custom/alshaya_hnm/js/address_book.js
@@ -0,0 +1,45 @@
+/**
+ * @file
+ * Address Book.
+ */
+
+(function ($, Drupal) {
+  'use strict';
+
+  Drupal.behaviors.address = {
+    attach: function (context, settings) {
+
+      function toggleOverlay(button, className) {
+        $(button).click(function () {
+          $('body').removeClass(className);
+        });
+      }
+
+      $('.address--delete a').click(function () {
+        $('body').addClass('modal-overlay');
+
+        $(document).ajaxComplete(function () {
+          toggleOverlay('.ui-dialog-titlebar-close', 'modal-overlay');
+          toggleOverlay('.ui-dialog-buttonpane .dialog-cancel', 'modal-overlay');
+        });
+      });
+
+      // On dialog close.
+      $(window).on('dialog:afterclose', function (e, dialog, $element) {
+        // If body has overlay class, remove it.
+        if ($('body').hasClass('modal-overlay')) {
+          $('body').removeClass('modal-overlay');
+        }
+      });
+
+      $(window).on('resize', function (e) {
+        if ($(window).width() > 768) {
+          $('.back-link').click(function (event) {
+            event.preventDefault();
+          });
+        }
+      });
+    }
+  };
+
+})(jQuery, Drupal);
