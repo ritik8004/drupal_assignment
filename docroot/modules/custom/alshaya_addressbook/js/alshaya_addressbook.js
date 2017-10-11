156cf4418dc1c2c091b166259ef553bd2fd9ed3c
Swarad Mokal <swarad07@gmail.com>
2017-10-11 15:00:31 +0530

    ACR2-762: Correcting units and removing log.

--- docroot/modules/custom/alshaya_addressbook/js/alshaya_addressbook.js
+++ docroot/modules/custom/alshaya_addressbook/js/alshaya_addressbook.js
@@ -3,7 +3,6 @@
 
   $.fn.correctFloorFieldLabel = function () {
     if ($.trim($('.form-item-field-address-floor').val()).length !== 0) {
-      console.log($('.form-item-field-address-floor').length);
       var label = $('.form-item-field-address-floor').parent().find('label:not(".error")');
       label.addClass('active-label');
     }
