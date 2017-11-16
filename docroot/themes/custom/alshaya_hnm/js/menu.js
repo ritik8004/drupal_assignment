21255638120a2f69c4b4e7f99efc95cad5437d6f
Syed Murtaza Mairaj Alvi <smurtazama@gmail.com>
2017-11-16 14:27:07 +0530

    ACR2-1538: Mega menu Flickers.
    Update the theme and behaviour of mega menu to fix the menu flickering.

--- docroot/themes/custom/alshaya_hnm/js/menu.js
+++ docroot/themes/custom/alshaya_hnm/js/menu.js
@@ -127,11 +127,8 @@
 
       var header_timer;
       $('.main--menu').hover(function () {
-        header_timer = setTimeout(function () {
-          $('body').addClass('overlay');
-        }, 300);
+        $('body').addClass('overlay');
       }, function () {
-        clearTimeout(header_timer);
         $('body').removeClass('overlay');
       });
 
@@ -190,9 +187,7 @@
         var parent = $('.block-alshaya-main-menu li.menu--one__list-item');
 
         $('.block-alshaya-main-menu').mouseenter(function () {
-          setTimeout(function () {
-            $(parent).parent().addClass('active--menu--links');
-          }, 310);
+          $(parent).parent().addClass('active--menu--links');
         });
 
         $('.block-alshaya-main-menu').mouseleave(function () {
