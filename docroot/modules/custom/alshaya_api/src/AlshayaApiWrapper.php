9ad1b3d309199c1f19234f31990f0f7aa6605e8f
Vaibhav Jain <vaibhav.jain@acquia.com>
2017-11-17 09:35:33 -0500

    ACR2-371: Change the field to drop down, based on API response.

--- docroot/modules/custom/alshaya_api/src/AlshayaApiWrapper.php
+++ docroot/modules/custom/alshaya_api/src/AlshayaApiWrapper.php
@@ -422,4 +422,23 @@ class AlshayaApiWrapper {
     return $mapping[$this->langcode];
   }
 
+  /**
+   * Function to get customer address form.
+   *
+   * @param string $form_id
+   *   The form ID to fetch.
+   *
+   * @return array
+   *   The Form array from API response OR empty array.
+   */
+  public function getCustomerAddressForm($form_id = 'customer_address_edit') {
+    $endpoint = 'deliverymatrix/customerAddressAttributeMetadata/form/' . $form_id;
+    $response = $this->invokeApi($endpoint, [], 'GET', FALSE);
+    $form = json_decode($response, TRUE);
+    if (is_array($form) && !empty($form)) {
+      return $form;
+    }
+    return [];
+  }
+
 }
