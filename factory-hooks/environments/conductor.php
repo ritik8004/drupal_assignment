9e5aede2b8a9931dcada61bb8ef9cfb1b34da8bb
vbouchet <vincent.bouchet.31@gmail.com>
2017-11-20 17:34:21 +0100

    ACR2-00: Declare H&M Dev conductor.

--- factory-hooks/environments/conductor.php
+++ factory-hooks/environments/conductor.php
@@ -8,6 +8,12 @@ function alshaya_get_conductor_host_data() {
       'hmac_id' => '139fbcb466984b39aea5fd200984a2af',
       'hmac_secret' => 'oMSt6AXgn3TqlMVj5D8A3Q'
     ],
+    // H&M.
+    'hm_dev' => [
+        'url' => 'https://alshaya-hm-dev.eu-west-1.prod.acm.acquia.io/',
+        'hmac_id' => '139fbcb466984b39aea5fd200984a2af',
+        'hmac_secret' => 'oMSt6AXgn3TqlMVj5D8A3Q',
+    ],
   ];
 }
 
