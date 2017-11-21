f31167367df32ba721b8c01d2d1b783b10a9a3ab
vbouchet <vincent.bouchet.31@gmail.com>
2017-11-21 09:14:09 +0100

    ACR2-00: Add hm prod magento.

--- factory-hooks/environments/magento.php
+++ factory-hooks/environments/magento.php
@@ -16,6 +16,7 @@ function alshaya_get_magento_host_data() {
     'hm_qa' => 'https://qa-dc3i3ua-zbrr3sobrsb3o.eu.magentosite.cloud',
     'hm_uat' => 'https://uat-irjkrqa-zbrr3sobrsb3o.eu.magentosite.cloud',
     'hm_swatch' => 'https://acr2-27-o7wcoxy-zbrr3sobrsb3o.eu.magentosite.cloud',
+    'hm_prod' => 'http://hm.store.alshaya.com.c.zbrr3sobrsb3o.ent.magento.cloud',
   ];
 }
 
