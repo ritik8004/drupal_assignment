76bd06c7d8e7a0b3f4d7597213b304737ed9eb65
nikunj <me@nik4u.com>
2017-11-21 19:22:31 +0530

    ACR2-371: Use term ids instead of name for area/governate.

--- docroot/modules/custom/alshaya_addressbook/js/alshaya_addressbook.js
+++ docroot/modules/custom/alshaya_addressbook/js/alshaya_addressbook.js
@@ -8,4 +8,19 @@
     }
   };
 
+  $.fn.updateAreaList = function (areas) {
+    var emptyOption = $('.area-list-dropdown option[value=""]');
+    $('.area-list-dropdown option').remove();
+    $('.area-list-dropdown').append(emptyOption);
+
+    for (var i in areas) {
+      var option = $('<option />');
+      option.attr('value', i);
+      option.html(areas[i]);
+      $('.area-list-dropdown').append(option);
+    }
+
+    $('.area-list-dropdown').trigger('change');
+  }
+
 })(jQuery, Drupal);
