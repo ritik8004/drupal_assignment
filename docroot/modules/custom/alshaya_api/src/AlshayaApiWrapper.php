940b67151e8dc6e6ff93d420cbab071c5a19c778
Amey Mudras <amey.mudras@acquia.com>
2017-11-23 12:11:20 +0300

    ACR2-1583: Handling empty conidtion on a array.

--- docroot/modules/custom/alshaya_api/src/AlshayaApiWrapper.php
+++ docroot/modules/custom/alshaya_api/src/AlshayaApiWrapper.php
@@ -169,11 +169,14 @@ class AlshayaApiWrapper {
    * @param float $lon
    *   Longitude.
    *
-   * @return array
+   * @return mixed
    *   Stores array.
    */
   public function getSkuStores($sku, $lat, $lon) {
     $stores = $this->getProductStores($sku, $lat, $lon);
+    if (empty($stores)) {
+      return;
+    }
 
     // Add missing information to store data.
     array_walk($stores, function (&$store) use (&$index) {
