f96ed52dedc724b615d5a3ab68f2fb9271d3eaf9
nikunj <me@nik4u.com>
2017-11-30 13:24:20 +0530

    ACR2-00: Add support for multiple transac sites in local.

--- factory-hooks/pre-sites-php/sites.php
+++ factory-hooks/pre-sites-php/sites.php
@@ -8,5 +8,7 @@ if (!isset($_ENV['AH_SITE_ENVIRONMENT'])) {
   $sites['default'] = 'g';
   $sites['127.0.0.1'] = 'g';
   $sites['local.alshaya.com'] = 'g';
+  $sites['local.alshaya-hm.com'] = 'g';
+  $sites['local.alshaya-mcksa.com'] = 'g';
   $sites['local.non-transac.com'] = 'g';
 }
