6eabd4a06c7d5af5f3fdb09a5abd534dd7e8e8c6
Sylvain DELBOSC <sylvain.delbosc@acquia.com>
2017-12-01 15:15:34 +0100

    MMCPA-0: fix merge issues.

--- blt/scripts/frontend-build.sh
+++ blt/scripts/frontend-build.sh
@@ -5,7 +5,7 @@ set -e
 
 docrootDir="$1"
 
-themes=( "whitelabel" "whitelabel_transac" "whitelabel_non_transac" "victoria_secret" "alshaya_white_label" "pottery_barn_non_trans" "alshaya_hnm" "bath_body_works" )
+themes=( "debenhams" "whitelabel" "whitelabel_transac" "whitelabel_non_transac" "victoria_secret" "alshaya_white_label" "pottery_barn_non_trans" "alshaya_hnm" "bath_body_works" )
 
 for i in "${themes[@]}"
 do
