a36590f7c6d9b32518644d79e6a62e57a04e4226
Sylvain DELBOSC <sylvain.delbosc@acquia.com>
2017-12-01 13:26:23 +0100

    MMCPA-0: use new conductors.

--- factory-hooks/environments/conductor.php
+++ factory-hooks/environments/conductor.php
@@ -7,27 +7,27 @@ function alshaya_get_conductor_host_data() {
   return [
     // Mothercare.
     'mc_dev' => [
-      'url' => 'https://alshaya-dev.eu-west-1.prod.acm.acquia.io/',
+      'url' => 'https://alshaya-mckw-dev.eu-west-1.prod.acm.acquia.io/',
       'hmac_id' => 'c37e4ed2d937425db29385d08491d53a',
       'hmac_secret' => 'dZWSbz_TyTbyaJoBmIyNcA',
     ],
     'mc_test' => [
-      'url' => 'https://alshaya-test.eu-west-1.prod.acm.acquia.io/',
+      'url' => 'https://alshaya-mckw-test.eu-west-1.prod.acm.acquia.io/',
       'hmac_id' => 'edda8c2a78af42b9af1e42221145fd01',
       'hmac_secret' => 'hTVYIu3SDzLh3BwNI6ZEjw',
     ],
     'mc_uat' => [
-      'url' => 'https://alshaya-uat.eu-west-1.prod.acm.acquia.io/',
+      'url' => 'https://alshaya-mckw-uat.eu-west-1.prod.acm.acquia.io/',
       'hmac_id' => 'ec11fb2f54d34b2f9d35ec1d3575b89e',
       'hmac_secret' => 'gpW7PQFKKDU-qPrcIgaYNQ',
     ],
     'mc_pprod' => [
-      'url' => 'https://alshaya-pprod.eu-west-1.prod.acm.acquia.io/',
+      'url' => 'https://alshaya-mckw-pprod.eu-west-1.prod.acm.acquia.io/',
       'hmac_id' => '676f2059d53d407791472c31783ae32c',
       'hmac_secret' => '-2Ok7ywndwcpsraYIIZ__w',
     ],
     'mc_prod' => [
-      'url' => 'https://alshaya-prod.eu-west-1.prod.acm.acquia.io/',
+      'url' => 'https://alshaya-mckw-prod.eu-west-1.prod.acm.acquia.io/',
       'hmac_id' => '3d136846d24040099a7eed6c1f4e80b9',
       'hmac_secret' => 'zUt1psyEWi5xO-glHlH_tw',
     ],
