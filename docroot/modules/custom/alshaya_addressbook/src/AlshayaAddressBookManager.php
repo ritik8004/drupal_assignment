37d027c91f8bfef917190184dfac94792c890ecb
nikunj <me@nik4u.com>
2017-12-06 13:07:04 +0530

    ACR2-371: Populate area_parent if empty.

--- docroot/modules/custom/alshaya_addressbook/src/AlshayaAddressBookManager.php
+++ docroot/modules/custom/alshaya_addressbook/src/AlshayaAddressBookManager.php
@@ -559,8 +559,12 @@ class AlshayaAddressBookManager {
             $magento_address[$attribute_code] = _alshaya_acm_checkout_clean_address_phone($address[$field_code]);
             break;
 
-          case 'administrative_area':
           case 'area_parent':
+            if (empty($address['area_parent']) && !empty($address['administrative_area'])) {
+              $parent = $this->getLocationParentTerm($address['administrative_area']);
+              $address[$field_code] = $parent ? $parent->id() : NULL;
+            }
+          case 'administrative_area':
             $term = $this->termStorage->load($address[$field_code]);
 
             if (empty($term)) {
