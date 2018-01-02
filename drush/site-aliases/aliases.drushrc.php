1f57039bae9a75772d6e1d22147badfccb567873
Abhishek Anand <fly2abhishek@gmail.com>
2018-01-02 12:47:40 +0530

    ACR2-1938: Updating box/config.yml

--- drush/site-aliases/aliases.drushrc.php
+++ drush/site-aliases/aliases.drushrc.php
@@ -359,14 +359,18 @@
 #    'uri' => 'http://example.com',
 #    'root' => $aliases['dev']['root'],
 #  ) + $aliases['server'];
-// [vagrant_machine_name].local
+
+// Local environment.
 $aliases['alshaya.local'] = array(
-  // /var/www/[vagrant_machine_name]/docroot
   'root' => '/var/www/alshaya/docroot',
-  // vagrant_hostname
   'uri' => 'http://local.alshaya.com',
-  // vagrant_hostname
-  'remote-host' => 'local.alshaya.com',
-  'remote-user' => 'vagrant',
-  'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
-);
+  );
+// Add remote connection options when alias is used outside VM.
+if ('vagrant' != $_SERVER['USER']) {
+  $aliases['alshaya.local'] += array(
+    'remote-host' => 'local.alshaya.com',
+    'remote-user' => 'vagrant',
+    'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+  );
+}
+
