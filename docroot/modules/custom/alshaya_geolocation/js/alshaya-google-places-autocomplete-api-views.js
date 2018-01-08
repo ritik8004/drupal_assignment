7bc75a2b945d0e734496d958c0e5d2eceedf1368
Mitesh Patel <developermitesh@gmail.com>
2018-01-08 16:08:55 +0530

    ACR2-2621: Refactor stores finder autocomplete submit to show progress indicator on store-finder page.

--- docroot/modules/custom/alshaya_geolocation/js/alshaya-google-places-autocomplete-api-views.js
+++ docroot/modules/custom/alshaya_geolocation/js/alshaya-google-places-autocomplete-api-views.js
@@ -70,6 +70,11 @@
     $("input[name='geolocation_geocoder_google_places_api_state']").val(stateValue);
 
     if (!$.isEmptyObject(coords)) {
+      // Show progress bar on store-finder page.
+      if (!$('[data-drupal-selector="edit-list-view"]').hasClass('hidden')) {
+        var pregress_element = $('<div class="ajax-progress ajax-progress-fullscreen">&nbsp;</div>');
+        $('body').after(pregress_element);
+      }
       $(field).parents('form').find('input[type="submit"]').click();
     }
   };
