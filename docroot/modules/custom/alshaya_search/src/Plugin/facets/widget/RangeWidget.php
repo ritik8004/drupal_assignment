8e86886636d12dbbd1b10c3e08986decb717cfaf
piyuesh23 <piyuesh23@qed42.com>
2018-01-10 11:30:29 +0530

    ACR2-2674: Updating range widget based on update to facets Beta version.

--- docroot/modules/custom/alshaya_search/src/Plugin/facets/widget/RangeWidget.php
+++ docroot/modules/custom/alshaya_search/src/Plugin/facets/widget/RangeWidget.php
@@ -47,10 +47,10 @@ class RangeWidget extends CheckboxWidget {
   /**
    * {@inheritdoc}
    */
-  public function getQueryType(array $query_types) {
+  public function getQueryType() {
     // The `numeric` type maps to `search_api_granular` which will correctly
     // handle ranges.
-    return $query_types['numeric'];
+    return 'numeric';
   }
 
 }
