17d3a30261da625cb6b4a5ff87108a61cd9b40a7
Mitesh Patel <developermitesh@gmail.com>
2018-01-11 20:53:11 +0530

    ACR2-2675: Add comments to custom script.

--- scripts/wbm2cm.sh
+++ scripts/wbm2cm.sh
@@ -8,17 +8,22 @@ alias="$1"
 # "local.alshaya-mckw.com" or "local.alshaya-hmkw.com".
 site="-l $2"
 
+# Clear cache before running updb.
 echo "==== Clear cache ===="
 drush $alias $site cr
 
+# Run updb.
 echo "==== Running updb on: $site ===="
 drush $alias $site updb -y
 
+# Enable wbm2cm to switch from workbench_moderation to content_moderation.
 echo "==== Enable wbm2cm module ===="
 drush $alias $site en -y wbm2cm
 
+# Run drush cmd available from wbm2cm to migrate workbench_moderation content.
 echo "==== Running wbm2cm-migrate as the command is available. ===="
 drush $alias $site wbm2cm-migrate
 
+# After successful migration uninstall module.
 echo "==== Disable wbm2cm module ===="
 drush $alias $site pmu -y wbm2cm
