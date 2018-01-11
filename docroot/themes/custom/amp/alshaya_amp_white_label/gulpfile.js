0a2b1ba20c505f62c308c89ce4b46ceda73d70c1
prashant114606 <prashant114606@gmail.com>
2018-01-11 14:58:55 +0530

    ACR2-2105: Removed dead code.

--- docroot/themes/custom/amp/alshaya_amp_white_label/gulpfile.js
+++ docroot/themes/custom/amp/alshaya_amp_white_label/gulpfile.js
@@ -85,10 +85,6 @@ var paths = {
     source: 'sass/',
     destination: 'css/'
   }
-  // ,
-  // scripts: 'js/',
-  // images: 'img/',
-  // styleGuide: 'styleguide'
 };
 
 // These are passed to each task.
@@ -110,61 +106,14 @@ var options = {
     destination: paths.styles.destination
   },
 
-  // ----- JS ----- //
-  // js: {
-  //   files: paths.scripts + '**/*.js',
-  //   destination: paths.scripts
-
-  // },
-
-  // ----- Images ----- //
-  // images: {
-  //   files: paths.images + '**/*.{png,gif,jpg,svg}',
-  //   destination: paths.images
-  // },
-
-  // ----- eslint ----- //
-  // jsLinting: {
-  //   files: {
-  //     theme: [
-  //       paths.scripts + '**/*.js',
-  //       '!' + paths.scripts + '**/*.min.js'
-  //     ],
-  //     gulp: [
-  //       'gulpfile.js',
-  //       'gulp-tasks/**/*'
-  //     ]
-  //   }
-
-  // },
-
-  // ----- KSS Node ----- //
-  // styleGuide: {
-  //   source: [
-  //     paths.styles.source
-  //   ],
-  //   builder: 'builder/twig',
-  //   destination: 'styleguide/',
-  //   css: [
-  //     path.relative(paths.styleGuide, paths.styles.destination + 'styles.css'),
-  //     path.relative(paths.styleGuide, paths.styles.destination + 'style-guide-only/kss-only.css')
-  //   ],
-  //   js: [],
-  //   homepage: 'style-guide-only/homepage.md',
-  //   title: 'Living Style Guide'
-  // }
-
 };
 
 // Tasks
 require('./gulp-tasks/build')(gulp, plugins, options);
 require('./gulp-tasks/clean')(gulp, plugins, options);
 require('./gulp-tasks/clean-css')(gulp, plugins, options);
-// require('./gulp-tasks/clean-styleguide')(gulp, plugins, options);
 require('./gulp-tasks/compile-sass')(gulp, plugins, options);
-// require('./gulp-tasks/compile-styleguide')(gulp, plugins, options);
 require('./gulp-tasks/default')(gulp, plugins, options);
-// require('./gulp-tasks/lint-js')(gulp, plugins, options);
 require('./gulp-tasks/lint-css')(gulp, plugins, options);
 require('./gulp-tasks/minify-css')(gulp, plugins, options);
 require('./gulp-tasks/test-css')(gulp, plugins, options);
