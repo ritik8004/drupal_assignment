afc3a90351854c28c862ab518bc3d5e679b43275
Rohit Joshi <rohit.joshi@Rohits-MacBook-Pro.local>
2018-01-11 19:42:44 +0530

    ACR2-2702: Updating amp block to be shown only when enable on node.

--- docroot/modules/custom/alshaya_amp/src/Plugin/Block/AlshayaAmpMenuBlock.php
+++ docroot/modules/custom/alshaya_amp/src/Plugin/Block/AlshayaAmpMenuBlock.php
@@ -7,6 +7,7 @@ use Drupal\Core\Cache\Cache;
 use Drupal\Core\Database\Connection;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -41,6 +42,13 @@ class AlshayaAmpMenuBlock extends BlockBase implements ContainerFactoryPluginInt
   protected $languageManager;
 
   /**
+   * Route matcher.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
    * AlshayaMegaMenuBlock constructor.
    *
    * @param array $configuration
@@ -53,11 +61,14 @@ class AlshayaAmpMenuBlock extends BlockBase implements ContainerFactoryPluginInt
    *   Database connection.
    * @param \Drupal\Core\Language\LanguageManagerInterface $language
    *   The Language manager.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route matcher.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, Connection $connection, LanguageManagerInterface $language) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Connection $connection, LanguageManagerInterface $language, RouteMatchInterface $route_match) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->connection = $connection;
     $this->languageManager = $language;
+    $this->routeMatch = $route_match;
   }
 
   /**
@@ -69,7 +80,8 @@ class AlshayaAmpMenuBlock extends BlockBase implements ContainerFactoryPluginInt
       $plugin_id,
       $plugin_definition,
       $container->get('database'),
-      $container->get('language_manager')
+      $container->get('language_manager'),
+      $container->get('current_route_match')
     );
   }
 
@@ -78,9 +90,23 @@ class AlshayaAmpMenuBlock extends BlockBase implements ContainerFactoryPluginInt
    */
   public function build() {
     $data = [];
+
+    /* @var \Drupal\node\Entity\Node $node */
+    $node = $this->routeMatch->getParameter('node');
+    if ($node) {
+      $display_amp_menu = $node->get('field_display_amp_menu')->getValue();
+      // If not show the amp menu on node.
+      if (!empty($display_amp_menu) && $display_amp_menu[0]['value'] == 0) {
+        return [];
+      }
+    }
+
     $query = $this->connection->select('taxonomy_term_field_data', 'tfd');
     $query->fields('tfd', ['tid', 'name']);
     $query->innerJoin('taxonomy_term_hierarchy', 'tth', 'tth.tid=tfd.tid');
+    $query->innerJoin('taxonomy_term__field_category_include_menu', 'ttrm', 'ttrm.entity_id=tfd.tid');
+    $query->condition('ttrm.field_category_include_menu_value', 1);
+    $query->condition('ttrm.langcode', $this->languageManager->getCurrentLanguage()->getId());
     $query->condition('tth.parent', 0);
     $query->condition('tfd.vid', $this->vid);
     $query->condition('tfd.langcode', $this->languageManager->getCurrentLanguage()->getId());
@@ -104,7 +130,8 @@ class AlshayaAmpMenuBlock extends BlockBase implements ContainerFactoryPluginInt
    * {@inheritdoc}
    */
   public function getCacheTags() {
-    return Cache::mergeTags(parent::getCacheTags(), ['term_list']);
+    $nid = $this->routeMatch->getParameter('node')->id();
+    return Cache::mergeTags(parent::getCacheTags(), ['term_list', 'node:' . $nid]);
   }
 
 }
