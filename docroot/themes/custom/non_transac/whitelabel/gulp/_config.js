8e03dfc9a5646867ea6f6ae54c6fa7eae6e95cc1
Swarad Mokal <swarad07@gmail.com>
2018-03-07 10:22:19 +0530

    CORE-665: Correcting gulp destination paths.

--- docroot/themes/custom/non_transac/whitelabel/gulp/_config.js
+++ docroot/themes/custom/non_transac/whitelabel/gulp/_config.js
@@ -9,7 +9,7 @@ module.exports = {
       'sass/**/*.ltr.scss',
       'components/**/*.ltr.scss'
     ],
-    destination: 'dist/css/',
+    destination: 'components/dist/css/',
     options: {
       outputStyle: 'expanded',
       includePaths: [
@@ -26,7 +26,7 @@ module.exports = {
       'components/js/**/*.js',
       'components/_patterns/**/*.js'
     ],
-    destination: 'dist/js/'
+    destination: 'components/dist/js/'
   },
   svg: {
     source: 'images/icons/svg/**/*.svg',
