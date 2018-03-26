0a7337d248ba2033d8c47c7b6e71498191c94065
Mitesh Patel <developermitesh@gmail.com>
2018-03-26 15:56:40 +0530

    AAVS-151: Update alshayamainmenu block to render second level category menu.

--- docroot/modules/custom/alshaya_main_menu/src/Plugin/Block/AlshayaMainMenuBlock.php
+++ docroot/modules/custom/alshaya_main_menu/src/Plugin/Block/AlshayaMainMenuBlock.php
@@ -5,11 +5,8 @@ namespace Drupal\alshaya_main_menu\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Cache\Cache;
-use Drupal\Core\Routing\RouteMatchInterface;
-use Drupal\Core\Database\Connection;
 use Drupal\alshaya_main_menu\ProductCategoryTree;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -45,27 +42,6 @@ class AlshayaMainMenuBlock extends BlockBase implements ContainerFactoryPluginIn
   protected $cacheTags = [];
 
   /**
-   * Route match service.
-   *
-   * @var \Drupal\Core\Routing\RouteMatchInterface
-   */
-  protected $routeMatch;
-
-  /**
-   * Database connection.
-   *
-   * @var \Drupal\Core\Database\Connection
-   */
-  protected $connection;
-
-  /**
-   * Language Manager.
-   *
-   * @var \Drupal\Core\Language\LanguageManagerInterface
-   */
-  protected $languageManager;
-
-  /**
    * Product category tree.
    *
    * @var \Drupal\alshaya_main_menu\ProductCategoryTree
@@ -83,21 +59,12 @@ class AlshayaMainMenuBlock extends BlockBase implements ContainerFactoryPluginIn
    *   Plugin defination.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_manager
    *   The entity type manager.
-   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
-   *   Route match service.
-   * @param \Drupal\Core\Database\Connection $connection
-   *   Database connection.
-   * @param \Drupal\Core\Language\LanguageManagerInterface $language
-   *   The Language manager.
    * @param \Drupal\alshaya_main_menu\ProductCategoryTree $product_category_tree
    *   Product category tree.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_manager, RouteMatchInterface $route_match, Connection $connection, LanguageManagerInterface $language, ProductCategoryTree $product_category_tree) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_manager, ProductCategoryTree $product_category_tree) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->termStorage = $entity_manager->getStorage('taxonomy_term');
-    $this->routeMatch = $route_match;
-    $this->connection = $connection;
-    $this->languageManager = $language;
     $this->productCateoryTree = $product_category_tree;
   }
 
@@ -110,9 +77,6 @@ class AlshayaMainMenuBlock extends BlockBase implements ContainerFactoryPluginIn
       $plugin_id,
       $plugin_definition,
       $container->get('entity_type.manager'),
-      $container->get('current_route_match'),
-      $container->get('database'),
-      $container->get('language_manager'),
       $container->get('alshaya_main_menu.product_category_tree')
     );
   }
@@ -122,7 +86,6 @@ class AlshayaMainMenuBlock extends BlockBase implements ContainerFactoryPluginIn
    */
   public function defaultConfiguration() {
     return [
-      'top_level_category' => 0,
       'follow_category_term' => 0,
       'default_parent' => NULL,
     ];
@@ -140,12 +103,6 @@ class AlshayaMainMenuBlock extends BlockBase implements ContainerFactoryPluginIn
       // Open if not set to defaults.
       '#open' => $defaults['top_level_category'] !== $this->configuration['top_level_category'] || $defaults['follow_category_term'] !== $this->configuration['follow_category_term'],
       '#process' => [[get_class(), 'processMenuConfigParents']],
-    ];
-
-    $form['menu_config']['top_level_category'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Display only top level category items'),
-      '#default_value' => $this->configuration['top_level_category'] ? $this->configuration['top_level_category'] : $defaults['top_level_category'],
     ];
 
     $form['menu_config']['follow_category_term'] = [
@@ -197,7 +154,6 @@ class AlshayaMainMenuBlock extends BlockBase implements ContainerFactoryPluginIn
    * {@inheritdoc}
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
-    $this->configuration['top_level_category'] = (int) $form_state->getValue('top_level_category');
     $this->configuration['follow_category_term'] = (int) $form_state->getValue('follow_category_term');
     if ($form_state->getValue('default_parent')) {
       $this->configuration['default_parent'] = (int) $form_state->getValue('default_parent');
@@ -208,15 +164,12 @@ class AlshayaMainMenuBlock extends BlockBase implements ContainerFactoryPluginIn
    * {@inheritdoc}
    */
   public function build() {
+    // Get the term object from current route.
     $term = $this->productCateoryTree->getTermFromRoute();
 
-    // If the block is configured to display only top level terms.
-    if ($this->configuration['top_level_category']) {
-      $term_data = $this->productCateoryTree->getTopLevelCategory();
-    }
     // Get the term id from the current path, and display only the related
     // second level child terms.
-    elseif ($this->configuration['follow_category_term'] && !empty($this->configuration['default_parent'])) {
+    if ($this->configuration['follow_category_term'] && !empty($this->configuration['default_parent'])) {
       // If term is of 'acq_product_category' vocabulary.
       if (is_object($term) && $parents = $this->productCateoryTree->getParentsFromTerm($term)) {
         // Get the top level parent id if parent exists.
@@ -254,19 +207,10 @@ class AlshayaMainMenuBlock extends BlockBase implements ContainerFactoryPluginIn
       }
     }
 
-    // Use different template for top level menu item.
-    if ($this->configuration['top_level_category']) {
-      return [
-        '#theme' => 'alshaya_main_menu_top_level',
-        '#term_tree' => $term_data,
-      ];
-    }
-    else {
-      return [
-        '#theme' => 'alshaya_main_menu_level1',
-        '#term_tree' => $term_data,
-      ];
-    }
+    return [
+      '#theme' => 'alshaya_main_menu_level1',
+      '#term_tree' => $term_data,
+    ];
   }
 
   /**
