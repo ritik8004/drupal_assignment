30b837e968f74c712f9f712c957e4559113a0790
Mitesh Patel <developermitesh@gmail.com>
2018-04-09 19:38:54 +0530

    AAVS-309: Update js for promo panel and some classes.

--- docroot/modules/custom/alshaya_promo_panel/js/alshaya-promo-panel.js
+++ docroot/modules/custom/alshaya_promo_panel/js/alshaya-promo-panel.js
@@ -8,15 +8,15 @@
 
   Drupal.behaviors.promoPanel = {
     attach: function (context, settings) {
-      var $offer_toggler = $('.block-content--block-content-promo-panel h2');
-      var $offer_content = $('.block-content--block-content-promo-panel .field-paragraph-content');
+      var $offer_toggler = $('.block-content--block-content-promo-panel > .promo-panel-label');
+      var $offer_content = $('.block-content--block-content-promo-panel > div');
       var $overlay_content = $('.empty-overlay');
       var $body = $('body');
 
       $($offer_toggler).on('click', function () {
-        $(this).parent().toggleClass('active-offer');
+        $(this).parent().toggleClass('active-promo-panel');
         $($offer_content).slideToggle('500');
-        $($body).toggleClass('active-todays-offer');
+        $($body).toggleClass('active-promo-panel-content');
         $($overlay_content).toggleClass('overlay-content');
       });
 
@@ -24,14 +24,14 @@
        * Promo panel sticky on scroll.
        */
       function checkOffset() {
-        if ($('.block-content--block-content-promo-panel h2').offset().top + $('.block-content--block-content-promo-panel h2').height() >= $('.block-content--block-content-promo-panel').offset().top - 30) {
-          $('.block-content--block-content-promo-panel h2').addClass('label-not-fixed'); // restore on scroll down
-          $('.block-content--block-content-promo-panel').removeClass('todays-offer-fixed'); // restore on scroll down
+        if ($('.block-content--block-content-promo-panel > .promo-panel-label').offset().top + $('.block-content--block-content-promo-panel > .promo-panel-label').height() >= $('.block-content--block-content-promo-panel').offset().top - 30) {
+          $('.block-content--block-content-promo-panel > .promo-panel-label').addClass('label-not-fixed'); // restore on scroll down
+          $('.block-content--block-content-promo-panel').removeClass('promo-panel-fixed'); // restore on scroll down
         }
 
-        if ($(document).scrollTop() + window.innerHeight < $('.block-todays-offer').offset().top) {
-          $('.block-content--block-content-promo-panel h2').removeClass('label-not-fixed');
-          $('.block-content--block-content-promo-panel').addClass('todays-offer-fixed');
+        if ($(document).scrollTop() + window.innerHeight < $('.block-content--block-content-promo-panel').offset().top) {
+          $('.block-content--block-content-promo-panel > .promo-panel-label').removeClass('label-not-fixed');
+          $('.block-content--block-content-promo-panel').addClass('promo-panel-fixed');
         }
       }
 
