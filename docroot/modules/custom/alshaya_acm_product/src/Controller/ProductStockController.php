98a64c11c45f5894c58228786dddeb3c0b729212
nikunj <me@nik4u.com>
2018-04-11 18:48:55 +0530

    CORE-2170: Make add to cart fast, page load even faster.

--- docroot/modules/custom/alshaya_acm_product/src/Controller/ProductStockController.php
+++ docroot/modules/custom/alshaya_acm_product/src/Controller/ProductStockController.php
@@ -5,18 +5,15 @@ namespace Drupal\alshaya_acm_product\Controller;
 use Drupal\acq_sku\Entity\SKU;
 use Drupal\acq_sku\Form\AcqSkuFormBuilder;
 use Drupal\Component\Plugin\PluginManagerInterface;
-use Drupal\Component\Serialization\Json;
 use Drupal\Core\Ajax\AjaxResponse;
-use Drupal\Core\Ajax\RedirectCommand;
+use Drupal\Core\Ajax\HtmlCommand;
 use Drupal\Core\Cache\CacheableJsonResponse;
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\Core\Render\BubbleableMetadata;
 use Drupal\Core\Render\Renderer;
 use Drupal\node\Entity\Node;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -151,95 +148,29 @@ class ProductStockController extends ControllerBase {
    * @param \Drupal\Core\Entity\EntityInterface $entity
    *   Entity related to SKU for which we checking the stock.
    *
-   * @return \Drupal\Core\Cache\CacheableJsonResponse|\Symfony\Component\HttpFoundation\JsonResponse
+   * @return \Drupal\Core\Ajax\AjaxResponse
    *   Response object returning cart form or out of stock message.
    */
   public function getCartForm($view_mode, EntityInterface $entity) {
-    if ($entity instanceof Node) {
-      $sku = $entity->get('field_skus')->first()->getString();
-      $sku_entity = SKU::loadFromSku($sku);
+    $response = new AjaxResponse();
+
+    $wrapper = 'article[data-skuid="' . $entity->id() . '"]:visible';
+
+    if (!alshaya_acm_product_is_buyable($entity)) {
+      $response->addCommand(new HtmlCommand($wrapper, ''));
     }
-    elseif ($entity instanceof SKU) {
-      $sku_entity = $entity;
-    }
-    // For someone trying to trick the system with different entity type.
-    // This will be good as we will be using this with GET now.
-    else {
-      throw new NotFoundHttpException();
-    }
-
-    $build = [];
-
-    if (!alshaya_acm_product_is_buyable($sku_entity)) {
-      // @TODO: This is to avoid adding out of stock classes. Needs refactoring.
-      $build['max_quantity'] = 100;
-      $build['html'] = '';
-
-      $response = new CacheableJsonResponse($build, 200);
-      $response->addCacheableDependency($sku_entity);
-      $response->addcacheabledependency(['url.path']);
-
-      // Adding cacheability metadata, so whenever, cache invalidates, this
-      // url's cached response also gets invalidate.
-      $cacheMeta = new CacheableMetadata();
-
-      // Adding cache tags.
-      $cacheMeta->addCacheTags(['acq_sku:' . $sku_entity->id()]);
-      $response->addCacheableDependency($cacheMeta);
-
-      return $response;
-    }
-    elseif ($max_quantity = alshaya_acm_get_product_stock($sku_entity)) {
-      $build['max_quantity'] = $max_quantity;
-
-      $form = $this->fetchAddCartForm($sku_entity, $view_mode);
-      $rendered_form = $this->renderer->renderRoot($form);
-
-      // Get the data from BubbleMetaData.
-      $data = BubbleableMetadata::createFromRenderArray($form);
-
-      // Retrieve the attachments from the $data.
-      $attachments = $data->getAttachments();
-
-      // Generate the settings to be sent back with the ajax response.
-      $settings = '';
-      if (!empty($attachments['drupalSettings'])) {
-        $settings .= '<script type="text/javascript">jQuery.extend(drupalSettings, ';
-        $settings .= Json::encode($attachments['drupalSettings']);
-        $settings .= ');</script>';
-      }
-
-      $build['html'] = $rendered_form . $settings;
-
-      return new JsonResponse($build);
+    elseif (alshaya_acm_get_product_stock($entity)) {
+      $form = $this->fetchAddCartForm($entity, $view_mode);
+      $response->addCommand(new HtmlCommand($wrapper, $form));
     }
     else {
-      // We process add to cart form if we have stock or request is POST.
-      // Stock might become zero after user has loaded the page.
-      // In such cases we just reload the page to show updated status.
-      if ($this->currentRequest->getMethod() == 'POST') {
-        $response = new AjaxResponse();
-        $response->addCommand(new RedirectCommand($this->currentRequest->server->get('HTTP_REFERER')));
-        return $response;
-      }
-      else {
-        $build['max_quantity'] = 0;
-        $build['html'] = '<span class="out-of-stock">' . $this->t('out of stock')->render() . '</span>';
-
-        $response = new CacheableJsonResponse($build, 200);
-        $response->addCacheableDependency($sku_entity);
-        $response->addcacheabledependency(['url.path']);
-
-        // Adding cacheability metadata, so whenever, cache invalidates, this
-        // url's cached response also gets invalidate.
-        $cacheMeta = new CacheableMetadata();
-
-        // Adding cache tags.
-        $cacheMeta->addCacheTags(['acq_sku:' . $sku_entity->id()]);
-        $response->addCacheableDependency($cacheMeta);
-        return $response;
-      }
+      $html = [
+        '#markup' => '<span class="out-of-stock">' . $this->t('out of stock')->render() . '</span>',
+      ];
+      $response->addCommand(new HtmlCommand($wrapper, $html));
     }
+
+    return $response;
   }
 
   /**
@@ -254,10 +185,7 @@ class ProductStockController extends ControllerBase {
    *   Add to cart form for the sku.
    */
   protected function fetchAddCartForm(SKU $sku_entity, $view_mode) {
-    $plugin_definition = $this->pluginManager->pluginFromSKU($sku_entity);
-
-    $class = $plugin_definition['class'];
-    $plugin = new $class();
+    $plugin = $sku_entity->getPluginInstance();
 
     $form['add_to_cart'] = $this->skuFormBuilder->getForm($plugin, $sku_entity);
     $form['add_to_cart']['#weight'] = 50;
