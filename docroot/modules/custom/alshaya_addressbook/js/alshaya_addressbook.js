a6c7ae2fb7bfd64272f299cacfeb5298d2002c3e
Rohit Joshi <joshi.rohit100@gmail.com>
2018-04-13 16:39:00 +0530

    CORE-2153: Error fix for address book edit/new address page.

--- docroot/modules/custom/alshaya_addressbook/js/alshaya_addressbook.js
+++ docroot/modules/custom/alshaya_addressbook/js/alshaya_addressbook.js
@@ -8,6 +8,24 @@
     }
   };
 
+  // Focus on first error.
+  $.fn.firstErrorFocus = function () {
+    // We doing this as at this point of time, process is not fully completed
+    // so we relying on this ajaxComplete. This will only be called when there
+    // is error on address book form.
+    $(document).ajaxComplete(function(event, xhr, settings) {
+      var focusElement = $('#address-book-form-wrapper input.error:first');
+      var stickyHeaderHeight = $('.branding__menu').height();
+
+      focusElement.focus();
+
+      // Scroll to the first element with error.
+      $('html, body').animate({
+          scrollTop: focusElement.offset().top - parseInt(stickyHeaderHeight)
+      });
+    });
+  };
+
   /**
    * Updates area list options based on options provided in argument.
    *
