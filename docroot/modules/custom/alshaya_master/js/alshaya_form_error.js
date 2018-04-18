204ac518630db7302e59eaff206442ff034753be
Sylvain DELBOSC <sylvain.delbosc@acquia.com>
2018-04-18 11:59:04 +0200

    Revert "Merge pull request #5550 from piyuesh23/CORE-2490"
    This reverts commit 92ebe63599f60a877748cd1f5b37e73c2519d7ce, reversing
    changes made to 0603f48fd572cd5a2cd2de98db31292d7c799ec2.

--- docroot/modules/custom/alshaya_master/js/alshaya_form_error.js
+++ docroot/modules/custom/alshaya_master/js/alshaya_form_error.js
@@ -23,8 +23,7 @@ Drupal.alshayaFormError = Drupal.alshayaFormError || {};
 
       // Scroll to the first element with error.
       if (scroll) {
-        // Sticky header is not on cart/checkout/* pages.
-        var stickyHeaderHeight = ($('.branding__menu').length > 0) ? $('.branding__menu').height() : 0;
+        var stickyHeaderHeight = $('.branding__menu').height();
         $('html, body').animate({
             scrollTop: focusElement.offset().top - parseInt(stickyHeaderHeight)
         });
