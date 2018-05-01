d969a299542d3e932f93f233a366405c5fe20f6d
Lubomir Culen <15381639+mirsoftacquia@users.noreply.github.com>
2018-05-01 09:17:58 +0200

    CORE-2380: Added better comments to the code.

--- docroot/modules/custom/alshaya_search/src/Event/AlshayaAcsfDuplicationScrubConfigurationHandler.php
+++ docroot/modules/custom/alshaya_search/src/Event/AlshayaAcsfDuplicationScrubConfigurationHandler.php
@@ -19,8 +19,15 @@ class AlshayaAcsfDuplicationScrubConfigurationHandler extends AcsfEventHandler {
 
     $site_hash = $config->get('site_hash');
 
+    // If the site has site_hash, it must be re-generated so it uses a different
+    // search index from the original one.
     if ($site_hash) {
       global $base_url;
+
+      // This basically uses the same hashing method as in
+      // Drupal\search_api_solr\Utility::getSiteHash() method, but it cannot
+      // be called directly as it probably uses different namespace for
+      // configuration so the variable was not saved to the new site.
       $hash = substr(base_convert(sha1(uniqid($base_url, TRUE)), 16, 36), 0, 6);
       $config->set('site_hash', $hash);
 
