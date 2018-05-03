a6a5dc8638cc1d5900897a50cece70e9e4bd9210
Rohit Joshi <joshi.rohit100@gmail.com>
2018-05-03 20:50:21 +0530

    CORE-2902: Adding facet processor to remove current term.

---
+++ docroot/modules/custom/alshaya_product/src/Plugin/facets/processor/AlshayaRemoveCurrentTermProcessor.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\alshaya_product\Plugin\facets\processor;
+
+use Drupal\facets\FacetInterface;
+use Drupal\facets\Processor\BuildProcessorInterface;
+use Drupal\facets\Processor\ProcessorPluginBase;
+
+/**
+ * Removes other terms (except child terms of current term) from facet items.
+ *
+ * @FacetsProcessor(
+ *   id = "alshaya_remove_current_term",
+ *   label = @Translation("Alshaya remove the current term"),
+ *   description = @Translation("Removes the current term from the facet items (Only for PLP)."),
+ *   stages = {
+ *     "build" = 50
+ *   }
+ * )
+ */
+class AlshayaRemoveCurrentTermProcessor extends ProcessorPluginBase implements BuildProcessorInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(FacetInterface $facet, array $results) {
+    if (!empty($results)) {
+      // For PLP page, the result url will always point to current plp page, so
+      // we assuming the first result will give the current plp page tid.
+      $current_term = $results[0]->getUrl()->getRouteParameters()['taxonomy_term'];
+
+      // If no term, means no PLP page. So we not process further.
+      if (!$current_term) {
+        return $results;
+      }
+
+      // Children of current term.
+      $children = $facet->getHierarchyInstance()->getNestedChildIds($current_term);
+
+      foreach ($results as $key => $result) {
+        // If term is not the child of current term.
+        if (!in_array($result->getRawValue(), $children)) {
+          unset($results[$key]);
+        }
+      }
+    }
+
+    // Return the results.
+    return $results;
+  }
+
+}
