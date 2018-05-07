b1ed9e31af5f25c713c6120699effec0d11dd57e
Akanksha Singh <akankshasingh@Akankshas-MacBook-Air.local>
2018-05-07 14:50:11 +0530

    CORE-2937: GTM - Product Click event not triggered on PLP page post release.

--- docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.js
+++ docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.js
@@ -899,30 +899,31 @@
   Drupal.alshaya_seo_gtm_push_product_clicks = function (element, currencyCode, listName, position) {
     // Don't trigger product click event for items in cross-sell on Mobile.
     if (((element.closest('.owl-item').length !== 0) ||
-        (element.closest('.no-carousel').length !== 0)) &&
-      ($(window).width() > 320)) {
-
-      var product = Drupal.alshaya_seo_gtm_get_product_values(element);
-      product.variant = '';
-      if (position) {
-        product.position = position;
-      }
-
-      var data = {
-        event: 'productClick',
-        ecommerce: {
-          currencyCode: currencyCode,
-          click: {
-            actionField: {
-              list: listName
-            },
-            products: [product]
-          }
-        }
-      };
-
-      dataLayer.push(data);
+            (element.closest('.no-carousel').length == 0)) &&
+        ($(window).width() < 320)) {
+      return;
     }
+
+    var product = Drupal.alshaya_seo_gtm_get_product_values(element);
+    product.variant = '';
+    if (position) {
+      product.position = position;
+    }
+
+    var data = {
+      event: 'productClick',
+      ecommerce: {
+        currencyCode: currencyCode,
+        click: {
+          actionField: {
+            list: listName
+          },
+          products: [product]
+        }
+      }
+    };
+
+    dataLayer.push(data);
   };
 
   /**
