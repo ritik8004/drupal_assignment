f5304fef61d5cb3c680362a54120e08acfcb55c8
Mitesh Patel <developermitesh@gmail.com>
2018-05-10 20:15:14 +0530

    CORE-316: Make form element type changes to render correct keypad for mobile.

--- docroot/modules/custom/alshaya_search/js/disable_keyword_ajax.js
+++ docroot/modules/custom/alshaya_search/js/disable_keyword_ajax.js
@@ -5,6 +5,11 @@
       $('#block-keywordsearchblock #edit-submit-search', context).off().on('click', function(e){
         $(this).closest('form').submit();
       });
+      // Convert search textfield to search to prompt keypaad with search button
+      // For mobile.
+      if ($(window).width() > 767) {
+        $('#block-keywordsearchblock .form-autocomplete', context).attr('type', 'search');
+      }
     }
   };
 }(jQuery));
