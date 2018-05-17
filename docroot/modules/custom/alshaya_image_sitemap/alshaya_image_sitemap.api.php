3e9c268b963e9b886f2ab1c4fc67aeb52203ddc4
Akanksha Singh <akankshasingh@community.icann.org>
2018-05-17 12:43:46 +0530

    CORE-3017: Added module to generate image sitemap.

---
+++ docroot/modules/custom/alshaya_image_sitemap/alshaya_image_sitemap.api.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Hooks specific to the alshaya_seo_transac module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Alter media array.
+ *
+ * @param array $media
+ *   The media array.
+ * @param int $nid
+ *   Nid of the product.
+ */
+function hook_media_image_sitemap_alter(array &$media, &$nid) {
+
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
---
+++ docroot/modules/custom/alshaya_image_sitemap/src/Controller/AlshayaImageSitemapController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Drupal\alshaya_image_sitemap\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\alshaya_image_sitemap\AlshayaImageSitemapGenerator;
+use Drupal\Core\State\StateInterface;
+
+/**
+ * Class AlshayaImageSitemapController.
+ *
+ * @package Drupal\alshaya_image_sitemap\Controller
+ */
+class AlshayaImageSitemapController extends ControllerBase {
+
+  /**
+   * Drupal\Core\State\StateInterface definition.
+   *
+   * @var Drupal\Core\State\StateInterface
+   */
+  protected $stateInt;
+
+  /**
+   * Drupal\alshaya_image_sitemap\AlshayaImageSitemapGenerator.
+   *
+   * @var Drupal\alshaya_image_sitemap\AlshayaImageSitemapGenerator
+   */
+  protected $generator;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(StateInterface $stateInt, AlshayaImageSitemapGenerator $generator) {
+    $this->stateInt = $stateInt;
+    $this->generator = $generator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('state'),
+      $container->get('alshaya_image_sitemap.generator')
+    );
+  }
+
+  /**
+   * List.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function listImageSitemap() {
+    $output = '';
+    $header = [
+      $this->t('SITEMAP URL'),
+      $this->t('CREATED DATE'),
+      $this->t('TOTAL LINKS'),
+      $this->t('ACTIONS'),
+    ];
+    $rows = [];
+    $url = 'public://alshaya_image_sitemap/image_sitemap.xml';
+    $url = file_create_url($url);
+
+    // Rows of table.
+    $image_sitemap_created = $this->stateInt->get('alshaya_image_sitemap.last_generated');
+    $image_sitemap_number_of_urls = $this->stateInt->get('alshaya_image_sitemap.url_count');
+    if (isset($image_sitemap_created) && isset($image_sitemap_number_of_urls)) {
+      $rows[] = [
+        Link::fromTextAndUrl($url, Url::fromUri($url)),
+        date('d-M-Y ', $image_sitemap_created),
+        $image_sitemap_number_of_urls,
+        Link::fromTextAndUrl($this->t('Re-generate'), Url::fromRoute('alshaya_image_sitemap.alshaya_image_sitemap_batch_controller_generate'))
+          ->toString(),
+      ];
+    }
+
+    $output = [
+      '#theme' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+      '#empty' => Link::fromTextAndUrl($this->t('Add a new image sitemap'), Url::fromRoute('alshaya_image_sitemap.alshaya_image_sitemap_batch_controller_generate')),
+    ];
+    return $output;
+  }
+
+  /**
+   * Generate site map.
+   */
+  public function generate() {
+    $this->generator->getSitemapReady();
+    $batch = [
+      'operations' => [],
+      'finished' => [__CLASS__, 'batchFinishedCallback'],
+      'title' => $this->t('Create Image Sitemap'),
+      'init_message' => $this->t('Starting sitemap creation.....'),
+      'progress_message' => $this->t('Completed @current step of @total.'),
+      'error_message' => $this->t('Sitemap creation has encountered an error.'),
+    ];
+
+    $nids = $this->generator->getNodes();
+    $nid_chunks = array_chunk($nids, 500);
+    foreach ($nid_chunks as $nid_chunk) {
+      $batch['operations'][] = [
+        [__CLASS__, 'batchStartCallback'],
+        [$nid_chunk],
+      ];
+    }
+
+    // Initialize the batch.
+    batch_set($batch);
+
+    return batch_process('admin/config/search/alshaya_image_sitemap');
+  }
+
+  /**
+   * Batch start callback.
+   *
+   * @param array $nids
+   *   Array of nids.
+   * @codingStandardsIgnoreStart
+   */
+  public static function batchStartCallback(array $nids) {
+    \Drupal::service('alshaya_image_sitemap.generator')->process($nids);
+    // @codingStandardsIgnoreEnd
+  }
+
+  /**
+   * Batch finished callback.
+   *
+   * @param bool $success
+   *   Success or fail import.
+   * @param array $results
+   *   Result array.
+   * @param array $operations
+   *   Operation array.
+   * @codingStandardsIgnoreStart
+   */
+  public static function batchFinishedCallback($success, array $results = [], array $operations = []) {
+    if ($success) {
+      \Drupal::service('alshaya_image_sitemap.generator')
+        ->sitemapGenerateFinished();
+      \Drupal::state()->set('alshaya_image_sitemap.last_generated', REQUEST_TIME);
+      // @codingStandardsIgnoreEnd
+      $message = ('Image Sitemap Generated Successfully.');
+      drupal_set_message($message, 'success');
+    }
+    else {
+      $message = t('There was some error while importing redirects.');
+      drupal_set_message($message, 'error');
+    }
+  }
+
+}
