c71092f20738d251b7314c5c9259c3b5495696fe
Rohit Joshi <joshi.rohit100@gmail.com>
2018-05-24 17:21:54 +0530

    CORE-2150: Adding filter/sort for the back_to_list.

--- docroot/modules/custom/alshaya_search/js/back_to_list.js
+++ docroot/modules/custom/alshaya_search/js/back_to_list.js
@@ -4,102 +4,141 @@
     attach: function (context, settings) {
       // On product click, store the product position.
       $('.views-infinite-scroll-content-wrapper .c-products__item').on('click', function () {
-        // Set key/value where key is url and value is position of product.
-        localStorage.setItem(window.location.href, $(this).position().top);
+        // Prepare object to store details.
+        var storage_details = {
+          nid: $(this).find('article:first').attr('data-nid'),
+          filter: $('.block-views-exposed-filter-blockalshaya-product-list-block-1 form .facets-hidden-container').html(),
+          sort: $('select[name="sort_bef_combine"]').val(),
+          facet_summary: $('.block-facets-summary').html()
+        };
+
+        // As local storage only supports string key/value pair.
+        localStorage.setItem(window.location.href, JSON.stringify(storage_details));
       });
 
-      // Here we just clearing the storage.
-      $(window).on('scroll', function() {
-        if (localStorage.getItem(window.location.href)) {
-          // Doing this due to conflict.
-          setTimeout(function(){
-            // Current scroll position.
-            var y_scroll_pos = window.pageYOffset;
-            // Position where we need to scroll.
-            var product_position = localStorage.getItem(window.location.href);
-            // If windows scroll position is greater that product position, it means
-            // we have reached/scrolled to the product and thus clear the storage.
-            if(y_scroll_pos >= parseInt(product_position)) {
-              localStorage.removeItem(window.location.href);
-            }
-          }, 1000);
-        }
-      });
-
-      // On page load, check if needs to scroll to appropriate position.
-      $(window).on('load', function(event) {
-        if (localStorage.getItem(window.location.href)) {
-          // Scroll to top first for consistent behavior.
-          window.scrollTo(0, 0);
-          // Doing this as back button loads page so fast causing conflict.
-          setTimeout(function() {
-            // Scroll to appropriate position.
-            checkAndScrollTo(localStorage.getItem(window.location.href));
-          }, 1000);
-        }
-      });
-
-      // If scroll position is set in storage.
-      if (localStorage.getItem(window.location.href)) {
-        // Get the position.
-        var gScroltop = localStorage.getItem(window.location.href);
-        // Doing this on ajax complete as the load more button might not
-        // available due to lazy loading.
-        $(document).ajaxComplete(function (event, xhr, settings) {
-          // Scroll to appropriate position.
-          checkAndScrollTo(gScroltop);
-        });
-      }
-
-      /**
-       * Scroll to the appropriate product position.
-       *
-       * @param product_pos
-       */
-       function scrollToProduct(product_pos) {
-        // Get the height of brand menu.
-        var stickyHeaderHeight = ($('.branding__menu').length > 0) ? $('.branding__menu').height() + 40 : 40;
-        var product_height = $('.views-infinite-scroll-content-wrapper .c-products__item').height();
-
-        // Once load more button is not less than the product scroll position,
-        // we scroll then to appropriate position.
-        $(window).scrollTop(product_pos + parseInt(stickyHeaderHeight) + parseInt(product_height));
-      }
-
-      /**
-       * Whether load more needs to be clicked or not.
-       *
-       * @param product_pos
-       * @returns {boolean}
-       */
-      function triggerLoadMoreClick(product_pos) {
-        // If load more button exists.
-        if ($('.js-pager__items').length > 0) {
-          var load_more_button_position = $('.js-pager__items').position();
-          // If load more button position is less what than the product we clicked,
-          // means we need to click the load more button.
-          if (load_more_button_position.top <= product_pos) {
-            return true;
+      // On page load, apply filter/sort if any.
+      $(window).on('load', function() {
+        var storage_value = getStorageValues();
+        if (typeof storage_value !== 'undefined' && storage_value !== null) {
+          if (typeof storage_value.nid !== 'undefined') {
+            window.scrollTo(0, 0);
+            // Doing this due to conflict.
+            setTimeout(function(){
+              // Apply the filter/sort if any.
+              applyFilterSort();
+            }, 1000);
           }
+        }
+      });
+
+      // Doing this on ajax complete as the load more button might not
+      // available due to lazy loading.
+      $(document).ajaxComplete(function (event, xhr, settings) {
+        // Scroll to appropriate position.
+        var storage_value = getStorageValues();
+        if (typeof storage_value !== 'undefined' && storage_value !== null) {
+          if (typeof storage_value.nid !== 'undefined') {
+            // If product is not available on the page, means we need to scroll more.
+            if ($('.views-infinite-scroll-content-wrapper .c-products__item article[data-nid="' + storage_value.nid + '"]').length < 1) {
+              $('.js-pager__items a').trigger("click");
+            }
+            else {
+              setTimeout(function() {
+                var ee = $('.views-infinite-scroll-content-wrapper article[data-nid="' + storage_value.nid + '"]:visible:first');
+                $('html, body').animate({
+                  scrollTop: ($(ee).offset().top)
+                }, 100);
+
+                // Once scroll to product, clear the storage.
+                localStorage.removeItem(window.location.href);
+                // Sometimes loader is not hiding, just hide it.
+                $('.ajax-progress').hide();
+              }, 1000);
+            }
+          }
+        }
+      });
+
+      /**
+       * Get the storage values.
+       *
+       * @returns {null}
+       */
+      function getStorageValues() {
+        if (localStorage.getItem(window.location.href)) {
+          return JSON.parse(localStorage.getItem(window.location.href));
+        }
+
+        return null;
+      }
+
+      /**
+       * Apply facet sort/filter.
+       */
+      function applyFilterSort() {
+        var storage_value = getStorageValues();
+        // If facet filter is available, then apply them + sort.
+        if (typeof storage_value !== 'undefined' && storage_value !== null) {
+          if (typeof storage_value.filter !== 'undefined' && storage_value.filter.length > 0) {
+            // Fill facet hidden input.
+            $('.block-views-exposed-filter-blockalshaya-product-list-block-1 form .facets-hidden-container').append(storage_value.filter);
+            // Fill facet summary block.
+            $('.block-facets-summary').append(storage_value.facet_summary);
+            // Fill sort value.
+            $('select[name="sort_bef_combine"]').val(storage_value.sort);
+
+            // If desktop device.
+            if (!checkNotDesktop()) {
+              // Apply sort here.
+              $('#edit-sort-bef-combine').val(storage_value['sort']);
+              var sort_text = $('select[name="sort_bef_combine"][value="' + storage_value['sort'] + '"]').text();
+              $('.select2-selection__rendered').html(sort_text);
+              $('.select2-selection__rendered').attr('title', sort_text);
+            }
+
+            // Clear values from storage so that not work for second time.
+            delete storage_value['sort'];
+            delete storage_value['filter'];
+            delete storage_value['facet_summary'];
+
+            // Update storage value.
+            localStorage.setItem(window.location.href, JSON.stringify(storage_value));
+            // Trigger apply button click.
+            $('#edit-submit-alshaya-product-list').trigger('click');
+          }
+          // Only if sort is selected.
+          else if (typeof storage_value.sort !== 'undefined' && storage_value.sort.length > 0) {
+            // If desktop device.
+            if (!checkNotDesktop()) {
+              // Apply sort here.
+              $('#edit-sort-bef-combine').val(storage_value['sort']);
+              var sort_text = $('select[name="sort_bef_combine"][value="' + storage_value['sort'] + '"]').text();
+              $('.select2-selection__rendered').html(sort_text);
+              $('.select2-selection__rendered').attr('title', sort_text);
+            }
+
+            // Clear sort from storage.
+            delete storage_value['sort'];
+
+            // Update storage value.
+            localStorage.setItem(window.location.href, JSON.stringify(storage_value));
+            // Trigger apply button click.
+            $('#edit-submit-alshaya-product-list').trigger('click');
+          }
+        }
+      }
+
+      /**
+       * Check if device is not desktop.
+       *
+       * @returns {Array|{index: number, input: string}}
+       */
+      function checkNotDesktop() {
+        if (navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)) {
+          return true;
         }
 
         return false;
-      }
-
-      /**
-       * Check and click and scroll to position.
-       *
-       * @param product_pos
-       */
-      function checkAndScrollTo(product_pos) {
-        if (triggerLoadMoreClick(product_pos)) {
-          $('.js-pager__items a').trigger("click");
-        }
-        // Scroll to appropriate position if no ajax call.
-        if (localStorage.getItem(window.location.href)) {
-          // Scroll to appropriate product.
-          scrollToProduct(product_pos);
-        }
       }
     }
   }
