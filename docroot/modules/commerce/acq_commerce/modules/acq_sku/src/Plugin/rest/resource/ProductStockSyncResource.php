3a185cdcb5ec6292472cafd81a679acfdc14e500
nikunj <me@nik4u.com>
2018-06-28 17:24:22 +0530

    CORE-3963: Add new stock quantity to logs.

--- docroot/modules/commerce/acq_commerce/modules/acq_sku/src/Plugin/rest/resource/ProductStockSyncResource.php
+++ docroot/modules/commerce/acq_commerce/modules/acq_sku/src/Plugin/rest/resource/ProductStockSyncResource.php
@@ -150,13 +150,16 @@ class ProductStockSyncResource extends ResourceBase {
 
     /** @var \Drupal\acq_sku\Entity\SKU $sku */
     if ($sku = SKU::loadFromSku($stock['sku'], $langcode)) {
-      $this->logger->info('Updating stock for SKU @sku.', ['@sku' => $stock['sku']]);
-
       if (isset($stock['is_in_stock']) && empty($stock['is_in_stock'])) {
         $stock['qty'] = 0;
       }
 
       $quantity = isset($stock['qty']) ? $stock['qty'] : 0;
+
+      $this->logger->info('Updating stock for SKU @sku. New quantity: @quantity', [
+        '@sku' => $stock['sku'],
+        '@quantity' => $quantity,
+      ]);
 
       if ($quantity != $sku->get('stock')->getString()) {
         $sku->get('stock')->setValue($quantity);
