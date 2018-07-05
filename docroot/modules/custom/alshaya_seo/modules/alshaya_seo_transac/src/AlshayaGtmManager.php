21ff09a3601cae8d66b5bf16ddefb128bb426b1c
akanksha-hp <akanksha@axelerant.com>
2018-07-05 13:42:53 +0530

    CORE-3415: Updated the getGtmCurrency function.

--- docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/src/AlshayaGtmManager.php
+++ docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/src/AlshayaGtmManager.php
@@ -1342,12 +1342,13 @@ class AlshayaGtmManager {
    *   GTM currency code.
    */
   public function getGtmCurrency() {
-    if (($this->configFactory->get('acq_commerce.currency')->get('gtm_currency_code')) == '' || ($this->configFactory->get('acq_commerce.currency')->get('gtm_currency_code')) == NULL) {
-      return $this->configFactory->get('acq_commerce.currency')->getRawData()['currency_code'];
+    if (!empty($gtm_currency_code = $this->configFactory->get('acq_commerce.currency')
+      ->get('gtm_currency_code'))) {
+      return $gtm_currency_code;
     }
-    else {
-      return $this->configFactory->get('acq_commerce.currency')->getRawData()['gtm_currency_code'];
-    }
+
+    return $this->configFactory->get('acq_commerce.currency')
+      ->get('currency_code');
   }
 
 }
