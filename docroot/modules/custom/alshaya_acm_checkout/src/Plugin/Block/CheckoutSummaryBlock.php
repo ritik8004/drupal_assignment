b2d68432914307a00062faf641a5cdcf8b3c29f9
Mitesh Patel <developermitesh@gmail.com>
2018-07-18 19:52:18 +0530

    CORE-318: Use helper function to preprocess address display.

--- docroot/modules/custom/alshaya_acm_checkout/src/Plugin/Block/CheckoutSummaryBlock.php
+++ docroot/modules/custom/alshaya_acm_checkout/src/Plugin/Block/CheckoutSummaryBlock.php
@@ -341,6 +341,7 @@ class CheckoutSummaryBlock extends BlockBase implements ContainerFactoryPluginIn
         }
 
         $shipping_address = $this->addressBookManager->getAddressArrayFromMagentoAddress($shipping_address);
+        $shipping_address = $this->addressBookManager->getAddressWithPrefix($shipping_address);
 
         $comma = $this->t(',')->render();
 
