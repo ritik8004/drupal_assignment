04dadaeb718bd75e7b7d45693cff417199fd5bd3
Mitesh Patel <developermitesh@gmail.com>
2018-07-21 22:56:31 +0530

    CORE-3755: Update new image resolution for H&M.

--- docroot/modules/brands/alshaya_hm/alshaya_hm_images/src/SkuAssetManager.php
+++ docroot/modules/brands/alshaya_hm/alshaya_hm_images/src/SkuAssetManager.php
@@ -231,18 +231,18 @@ class SkuAssetManager {
 
       // Use res attribute in place of width & height while calculating images
       // for swatches.
-      if (empty($style)) {
-        $set['width'] = "width[" . $alshaya_hm_images_settings->get('dimensions')[$location_image]['width'] . "]";
-        $set['height'] = "height[" . $alshaya_hm_images_settings->get('dimensions')[$location_image]['height'] . "]";
-      }
-      else {
-        $detect = new MobileDetect();
-        $set['res'] = "res[z]";
+//      if (empty($style)) {
+//        $set['width'] = "width[" . $alshaya_hm_images_settings->get('dimensions')[$location_image]['width'] . "]";
+//        $set['height'] = "height[" . $alshaya_hm_images_settings->get('dimensions')[$location_image]['height'] . "]";
+//      }
+//      else {
 
-        if ($detect->isMobile()) {
-          $set['res'] = "res[y]";
-        }
+      $set['res'] = "res[" . $alshaya_hm_images_settings->get('dimensions')[$location_image]['desktop'] ."]";
+      $detect = new MobileDetect();
+      if ($detect->isMobile()) {
+        $set['res'] = "res[" . $alshaya_hm_images_settings->get('dimensions')[$location_image]['mobile'] ."]";
       }
+      //}
 
       // Check for overrides for style identifiers & dimensions.
       $config_overrides = $this->overrideConfig($sku, $page_type);
@@ -253,12 +253,15 @@ class SkuAssetManager {
           $image_location_identifier = $config_overrides['style_identifiers'][$location_image];
         }
 
-        if ((!empty($style)) && isset($config_overrides['dimensions'][$location_image]['width'])) {
-          $set['width'] = "width[" . $config_overrides['dimensions'][$location_image]['width'] . "]";
+        if ((!empty($style)) && isset($config_overrides['dimensions'][$location_image]['desktop'])) {
+          $set['res'] = "res[" . $config_overrides['dimensions'][$location_image]['desktop'] ."]";
         }
 
-        if ((!empty($style)) && isset($config_overrides['dimensions'][$location_image]['height'])) {
-          $set['height'] = "height[" . $config_overrides['dimensions'][$location_image]['height'] . "]";
+        $detect = new MobileDetect();
+        if ($detect->isMobile()) {
+          if ((!empty($style)) && isset($config_overrides['dimensions'][$location_image]['mobile'])) {
+            $set['res'] = "res[" . $config_overrides['dimensions'][$location_image]['mobile'] . "]";
+          }
         }
       }
     }
