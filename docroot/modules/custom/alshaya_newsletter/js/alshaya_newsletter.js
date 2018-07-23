252d4aaf0963d07a22bd9a3ad31253838aa3876f
Rohit Joshi <joshi.rohit100@gmail.com>
2018-07-23 15:42:30 +0300

    CORE-4257: Remove error message for newsletter on empty field.

--- docroot/modules/custom/alshaya_newsletter/js/alshaya_newsletter.js
+++ docroot/modules/custom/alshaya_newsletter/js/alshaya_newsletter.js
@@ -27,7 +27,25 @@
           $('#alshaya-newsletter-subscribe .form-type-email input').val('');
           setTimeout(Drupal.clearNewsletterForm, parseInt(data.interval));
         }
+        else {
+          // Add error class to the field.
+          // We are doing this as we have made the field non-required as this
+          // causing conflict/problem with the 'cv.jquery.validate.js' and thus
+          // we need to add the class back.
+          $('#alshaya-newsletter-subscribe .form-type-email input').addClass('error');
+        }
       };
+
+      // On focus out on button click.
+      $('.block-alshaya-newsletter-subscription button.edit-newsletter').on('focusout', function() {
+        // If email field is empty.
+        if ($('.block-alshaya-newsletter-subscription input[name="email"]').val().length < 1) {
+          // If there was an error due to ajax response, then remove it.
+          if ($('.block-alshaya-newsletter-subscription #footer-newsletter-form-wrapper span.message').hasClass('error')) {
+            $('.block-alshaya-newsletter-subscription #footer-newsletter-form-wrapper .subscription-status').remove()
+          }
+        }
+      });
     }
   };
 
