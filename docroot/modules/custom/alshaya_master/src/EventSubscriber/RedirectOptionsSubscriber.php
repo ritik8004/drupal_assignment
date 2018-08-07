5b2b6cce5cb9f25c705e14ed1244b94a80209c59
piyuesh23 <piyuesh23@qed42.com>
2018-08-07 13:12:32 +0530

    CORE-4686: Updating contrib modules: consumers, paragraphs, r4032login, redirect, robotstxt, shield, taxonomy_access_fix, taxonomy_term_depth, webform, address.

--- docroot/modules/custom/alshaya_master/src/EventSubscriber/RedirectOptionsSubscriber.php
+++ docroot/modules/custom/alshaya_master/src/EventSubscriber/RedirectOptionsSubscriber.php
@@ -6,7 +6,7 @@ use Drupal\r4032login\Event\RedirectEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
- * Class RedirectOptionsSubscriber
+ * Class RedirectOptionsSubscriber.
  *
  * Remove query parameters from redirect options.
  */
@@ -15,20 +15,8 @@ class RedirectOptionsSubscriber implements EventSubscriberInterface {
   /**
    * Returns an array of event names this subscriber wants to listen to.
    *
-   * The array keys are event names and the value can be:
-   *
-   *  * The method name to call (priority defaults to 0)
-   *  * An array composed of the method name to call and the priority
-   *  * An array of arrays composed of the method names to call and respective
-   *    priorities, or 0 if unset
-   *
-   * For instance:
-   *
-   *  * array('eventName' => 'methodName')
-   *  * array('eventName' => array('methodName', $priority))
-   *  * array('eventName' => array(array('methodName1', $priority), array('methodName2')))
-   *
-   * @return array The event names to listen to
+   * @return array
+   *   The event names to listen to
    */
   public static function getSubscribedEvents() {
     $events[RedirectEvent::EVENT_NAME] = ['onRedirect', 100];
@@ -39,7 +27,7 @@ class RedirectOptionsSubscriber implements EventSubscriberInterface {
   /**
    * Alter redirect url before the url is perform.
    *
-   * @param \Drupal\r4032login\Event\RedirectEvent
+   * @param \Drupal\r4032login\Event\RedirectEvent $event
    *   The Event to process.
    */
   public function onRedirect(RedirectEvent $event) {
