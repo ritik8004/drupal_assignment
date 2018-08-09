57c8b18854aff917006b31b59283f3fc0cc7c742
Mitesh Patel <developermitesh@gmail.com>
2018-08-09 18:55:28 +0530

    CORE-4716: Make sorting options labels on search pages translatable.

---
+++ docroot/modules/custom/alshaya_search/src/Form/AlshayaSrpSortLabelSettingsForm.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Drupal\alshaya_search\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class AlshayaPlpSortSettingsForm.
+ */
+class AlshayaSrpSortLabelSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'alshaya_srp_sort_label_settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEditableConfigNames() {
+    return ['alshaya_search.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildForm($form, $form_state);
+
+    $form['sort_options'] = [
+      '#type' => 'table',
+      '#header' => [
+        $this->t('Admin Label'),
+        $this->t('Label'),
+      ],
+    ];
+
+    // Sort options from config.
+    $position_settings = $this->config('alshaya_search.settings');
+    $sort_options_label = $position_settings->get('sort_options_labels');
+
+    foreach ($sort_options_label as $key => $value) {
+      $form['sort_options'][$key]['admin_label'] = [
+        '#plain_text' => $key,
+      ];
+
+      $form['sort_options'][$key]['label'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('label for @title', ['@title' => $key]),
+        '#title_display' => 'invisible',
+        // @codingStandardsIgnoreLine
+        '#default_value' => $this->t($value),
+      ];
+    }
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Get form values.
+    $sort_options = $form_state->getValue('sort_options');
+    $labels = array_map(function ($sort_option) {
+      return $sort_option['label'];
+    }, $sort_options);
+
+    $config = $this->config('alshaya_search.settings');
+    $config->set('sort_options_labels', $labels);
+    $config->save(TRUE);
+    return parent::submitForm($form, $form_state);
+  }
+
+}
