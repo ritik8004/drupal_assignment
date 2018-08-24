75684ca38bfcaea302ab9c7d76125ec54d935631
nikunj <me@nik4u.com>
2018-08-24 14:50:02 +0530

    CORE-0000: Manage ACM V2 queues from Drupal Admin.

---
+++ docroot/modules/commerce/acq_commerce/src/Form/PurgeQueueForm.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Drupal\acq_commerce\Form;
+
+use Drupal\acq_commerce\Conductor\APIWrapperInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class PurgeQueueForm.
+ *
+ * @package Drupal\acm\Form
+ *
+ * @ingroup acm
+ */
+class PurgeQueueForm extends FormBase {
+
+  /**
+   * Connector Agent API Helper.
+   *
+   * @var \Drupal\acq_commerce\Conductor\APIWrapperInterface
+   */
+  private $api;
+
+  /**
+   * PurgeQueueForm constructor.
+   *
+   * @param \Drupal\acq_commerce\Conductor\APIWrapperInterface $api
+   *   APIWrapper object.
+   */
+  public function __construct(APIWrapperInterface $api) {
+    $this->api = $api;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('acq_commerce.agent_api')
+    );
+  }
+
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormId() {
+    return 'acm_purge_queue';
+  }
+
+  /**
+   * Define the form used for settings.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   An associative array containing the current state of the form.
+   *
+   * @return array
+   *   Form definition array.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['total'] = [
+      '#type' => 'markup',
+      '#markup' => $this->t('Currently in queue: @total', [
+        '@total' => $this->api->getQueueStatus(),
+      ]),
+    ];
+
+    $form['actions']['#type'] = 'actions';
+
+    $form['actions']['purge_queue'] = [
+      '#type' => 'submit',
+      '#value' => t('Purge queue'),
+    ];
+
+    return ($form);
+  }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   An associative array containing the current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    try {
+      $this->api->purgeQueue();
+      drupal_set_message($this->t('Queue purged.'));
+    }
+    catch (\Exception $e) {
+      drupal_set_message($this->t('Got error when purging queue: @error.', [
+        '@error' => $e->getMessage(),
+      ]), 'error');
+    }
+  }
+
+}
