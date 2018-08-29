2b4e944e0691cd5819cbf205dacfac96fe278804
vbouchet <vincent.bouchet.31@gmail.com>
2018-08-29 14:10:42 +0200

    CORE-000: Add more logging to get insights.

--- factory-hooks/post-install/post-install.sh
+++ factory-hooks/post-install/post-install.sh
@@ -8,20 +8,20 @@ country_code="$4"
 cd `drush8 sa @alshaya.$target_env | grep root | cut -d"'" -f4`
 
 ## Push the updb logs on Slack channel.
-FILE=/home/alshaya/slack_settings
+FILE=$HOME/slack_settings
 
-echo "this is in the bash" >> /home/alshaya/debug.txt
-echo $HOME >> /home/alshaya/debug.txt
+echo "this is in the bash" >> $HOME/debug.txt
+echo $HOME >> $HOME/debug.txt
 
 if [ -f $FILE ]; then
   # Load the Slack webhook URL (which is not stored in this repo).
-  . /home/alshaya/slack_settings
+  . $HOME/slack_settings
 
-  echo "Sending error notification to Slack channel."
-  curl -X POST --data-urlencode "payload={\"username\": \"Acquia Cloud\", \"txt\": \" New site $brand_code $country_code installed on $target_env.\", \"icon_emoji\": \":acquiacloud:\"}" /home/alshaya/slack_settings
+  echo "Sending error notification to Slack channel." >> $HOME/debug.txt
+  curl -X POST --data-urlencode "payload={\"username\": \"Acquia Cloud\", \"txt\": \" New site $brand_code $country_code installed on $target_env.\", \"icon_emoji\": \":acquiacloud:\"}" $HOME/slack_settings
 else
-  echo "File /home/alshaya/slack_settings does not exist."
+  echo "File $HOME/slack_settings does not exist." >> $HOME/debug.txt
 fi
 
 # @TODO: Get the logs from setup-fresh-site.sh and send these to Slack.
-./.../scripts/setup/setup-fresh-site.sh "$target_env" "$site" "$brand_code" "$country_code"
+./.../scripts/setup/setup-fresh-site.sh "$target_env" "$site" "$brand_code" "$country_code" >> $HOME/debug.txt
