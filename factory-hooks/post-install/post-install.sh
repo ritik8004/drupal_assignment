e5d3b5c12f956a86bbf35838903d2522e210046c
vbouchet <vincent.bouchet.31@gmail.com>
2018-08-29 12:00:13 +0200

    CORE-000: Update post-install scripts to run more operations.

--- factory-hooks/post-install/post-install.sh
+++ factory-hooks/post-install/post-install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-site="alshaya"
 target_env="$1"
-brand_code="$2"
-country_code="$3"
+site="$2"
+brand_code="$3"
+country_code="$4"
 
-cd `drush8 sa @$site.$target_env | grep root | cut -d"'" -f4`
+cd `drush8 sa @alshaya.$target_env | grep root | cut -d"'" -f4`
 
 ## Push the updb logs on Slack channel.
 FILE=/home/alshaya/slack_settings
 
 echo "this is in the bash" >> /home/alshaya/debug.txt
+echo $HOME >> /home/alshaya/debug.txt
 
 if [ -f $FILE ]; then
   # Load the Slack webhook URL (which is not stored in this repo).
@@ -21,3 +22,6 @@ if [ -f $FILE ]; then
 else
   echo "File /home/alshaya/slack_settings does not exist."
 fi
+
+# @TODO: Get the logs from setup-fresh-site.sh and send these to Slack.
+./.../scripts/setup/setup-fresh-site.sh "$target_env" "$site" "$brand_code" "$country_code"
