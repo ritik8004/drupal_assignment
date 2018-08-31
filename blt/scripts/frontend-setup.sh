0734c6f7515cbb72ecf3ce1f2b13df55a56053b0
vbouchet <vincent.bouchet.31@gmail.com>
2018-08-31 16:29:19 +0200

    CORE-000: Fix issue with setup detection.

--- blt/scripts/frontend-setup.sh
+++ blt/scripts/frontend-setup.sh
@@ -11,8 +11,6 @@ isTravisPr=0
 isTravisMerge=0
 diff=""
 
-echo $TRAVIS_PULL_REQUEST
-
 # Determine if we are on Travis.
 if [[ $TRAVIS && $TRAVIS == "true" ]]; then
   isTravis=1
@@ -33,44 +31,42 @@ echo "isTravisPr: $isTravisPr"
 echo "isTravisMerge: $isTravisMerge"
 
 # We only setup themes on if we are not on Travis or if themes have changed.
-if ([ $isTravis == 0 ]) || ([[ $(echo "$diff" | grep /themes/) ]])
-then
-  for dir in $(find $docrootDir/themes/custom -mindepth 1 -maxdepth 1 -type d)
-  do
-    theme_type_dir=${dir##*/}
+for dir in $(find $docrootDir/themes/custom -mindepth 1 -maxdepth 1 -type d)
+do
+  theme_type_dir=${dir##*/}
 
-    forceSetup=0
-    if ([ $isTravisMerge == 1 ])
-    then
-      for subdir in $(find $docrootDir/themes/custom/$theme_type_dir -mindepth 1 -maxdepth 1 -type d)
-      do
-        theme_dir=${subdir##*/}
+  # We build the theme if:
+    # - We are outside Travis context.
+    # - The theme has changed.
+    # - We are merging but the theme (css) does not exist on deploy directory.
+  setup=0
+  if [ $isTravisMerge == 1 ]; then
+    for subdir in $(find $docrootDir/themes/custom/$theme_type_dir -mindepth 1 -maxdepth 1 -type d)
+    do
+      theme_dir=${subdir##*/}
 
-        if [[ $(echo "$diff" | grep themes/custom/$theme_type_dir/$theme_dir) && ! -d "$docrootDir/themes/custom/$theme_type_dir/$theme_dir/css" ]]
-        then
-          forceSetup=1
-        fi
-      done
-    fi
+      if [[ $(echo "$diff" | grep themes/custom/$theme_type_dir/$theme_dir) || ! -d "$docrootDir/themes/custom/$theme_type_dir/$theme_dir/css" ]]
+      then
+        setup=1
+      fi
+    done
+  elif [ $isTravis == 0 ]; then
+    setup=1
+  elif [ $(echo "$diff" | grep themes/custom/$theme_type_dir) ]; then
+    setup=1
+  fi
 
-    echo "forceSetup: $forceSetup"
+  echo "setup: $setup"
 
-    # We build the theme if:
-      # - We are outside Travis context.
-      # - The theme has changed.
-      # - We are merging but the theme (css) does not exist on deploy directory.
-    if ([ $isTravis == 0 ]) || ([ $forceSetup == 1 ]) || ([[ $(echo "$diff" | grep themes/custom/$theme_type_dir) ]])
-    then
-      echo -en "travis_fold:start:FE-$theme_type_dir-Setup\r"
-      echo -en "Start - Installing npm for $theme_type_dir themes"
-      cd $docrootDir/themes/custom/$theme_type_dir
-      npm run install-tools
-      echo -en "End - Installing npm for $theme_type_dir themes"
-      echo -en "travis_fold:end:FE-$theme_type_dir-Setup\r"
-    else
-      echo "No need to setup $theme_type_dir frontend. There is no change in any $theme_type_dir themes."
-    fi
-  done
-else
-  echo "No need to setup any frontend. There is no frontend change at all."
-fi
+  if ([ $setup == 1 ])
+  then
+    echo -en "travis_fold:start:FE-$theme_type_dir-Setup\r"
+    echo -en "Start - Installing npm for $theme_type_dir themes"
+    cd $docrootDir/themes/custom/$theme_type_dir
+    npm run install-tools
+    echo -en "End - Installing npm for $theme_type_dir themes"
+    echo -en "travis_fold:end:FE-$theme_type_dir-Setup\r"
+  else
+    echo "No need to setup $theme_type_dir frontend. There is no change in any $theme_type_dir themes."
+  fi
+done
