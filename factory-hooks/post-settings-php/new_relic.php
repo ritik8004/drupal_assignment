2dca19d9885237a895bfc26df2d7612c25949dac
Rohit Joshi <joshi.rohit100@gmail.com>
2018-09-05 10:51:12 +0530

    CORE-4844: Disable newrelic agent injection for amp pages.

--- factory-hooks/post-settings-php/new_relic.php
+++ factory-hooks/post-settings-php/new_relic.php
@@ -7,6 +7,13 @@
  */
 
 if (extension_loaded('newrelic')) {
-  global $acsf_site_name;
-  newrelic_set_appname("$acsf_site_name;alshaya.01live", '', 'true');
+  global $site_name;
+  $is_amp_page = isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], '?amp') !== false;
+  if ($is_amp_page) {
+    // Disable newrelic for amp pages.
+    newrelic_disable_autorum();
+  }
+  else {
+    newrelic_set_appname("$site_name;alshaya.01live", '', 'true');
+  }
 }
