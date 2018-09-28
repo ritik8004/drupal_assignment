58e6db29a83ab686c2ecdd4b0fbd1d6bf5e60876
Rohit Joshi <joshi.rohit100@gmail.com>
2018-09-28 15:37:52 +0530

    CORE-5272: Adding config for the layout options and adding field in product category vocab.

--- docroot/modules/custom/alshaya_acm_product/src/Form/ProductSettingsForm.php
+++ docroot/modules/custom/alshaya_acm_product/src/Form/ProductSettingsForm.php
@@ -267,14 +267,16 @@ class ProductSettingsForm extends ConfigFormBase {
       '#default_value' => !empty($config->get('product_default_image')) ? [$config->get('product_default_image')] : [],
     ];
 
+    $pdp_layout_options = [];
+    // Prepare PDP layout select options.
+    foreach ($config->get('pdp_layout_options') as $option_key => $option_label) {
+      $pdp_layout_options[$option_key] = $option_label;
+    }
     $form['pdp_layout'] = [
       '#type' => 'select',
       '#title' => $this->t('PDP layout'),
       '#description' => $this->t('This will change the layout/appearence of the PDP page.'),
-      '#options' => [
-        'default' => $this->t('Default layout'),
-        'magzine' => $this->t('Magzine layout'),
-      ],
+      '#options' => $pdp_layout_options,
       '#default_value' => $config->get('pdp_layout'),
     ];
 
