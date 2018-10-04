a27e69f0f90212c09ebb4ae2697afa661a30748b
Vincent <vincent.bouchet@acquia.com>
2018-10-04 09:34:44 +0200

    Merge pull request #8263 from miteshmap/MAPP-38-simple-page
    MAPP-38: Create end point for simple pages.

--- docroot/modules/custom/alshaya_mobile_app/src/Service/MobileAppUtility.php
+++ docroot/modules/custom/alshaya_mobile_app/src/Service/MobileAppUtility.php
@@ -5,11 +5,21 @@ namespace Drupal\alshaya_mobile_app\Service;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\taxonomy\TermInterface;
 use Drupal\node\NodeInterface;
+use Drupal\file\FileInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Path\AliasManagerInterface;
+use Drupal\Core\Url;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Utilty Class.
  */
 class MobileAppUtility {
+
+  use StringTranslationTrait;
 
   /**
    * Cache Backend service for alshaya.
@@ -19,13 +29,57 @@ class MobileAppUtility {
   protected $cache;
 
   /**
+   * The language manager.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
+   * The request stack service.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * The path alias manager.
+   *
+   * @var \Drupal\Core\Path\AliasManagerInterface
+   */
+  protected $aliasManager;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
    * Utility constructor.
    *
    * @param \Drupal\Core\Cache\CacheBackendInterface $cache
    *   Cache backend instance to use.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   *   The language manager.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request stack service.
+   * @param \Drupal\Core\Path\AliasManagerInterface $alias_manager
+   *   The path alias manager.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
    */
-  public function __construct(CacheBackendInterface $cache) {
+  public function __construct(CacheBackendInterface $cache,
+                              LanguageManagerInterface $language_manager,
+                              RequestStack $request_stack,
+                              AliasManagerInterface $alias_manager,
+                              EntityTypeManagerInterface $entity_type_manager) {
     $this->cache = $cache;
+    $this->languageManager = $language_manager;
+    $this->requestStack = $request_stack->getCurrentRequest();
+    $this->aliasManager = $alias_manager;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
@@ -51,6 +105,101 @@ class MobileAppUtility {
     }
 
     return $return;
+  }
+
+  /**
+   * Get the alias language.
+   *
+   * @param string $alias
+   *   The alias string.
+   *
+   * @return string
+   *   Return the string of language code.
+   */
+  private function getAliasLang($alias) {
+    $alias_lang = NULL;
+    if ($this->languageManager->getCurrentLanguage()->getId() == 'ar' && !preg_match("/\p{Arabic}/u", $alias)) {
+      $alias_lang = $this->languageManager->getDefaultLanguage()->getId();
+    }
+    elseif ($this->languageManager->getCurrentLanguage()->getId() == 'en' && preg_match("/\p{Arabic}/u", $alias)) {
+      // Get the correct language, based on user input.
+      $languages = $this->languageManager->getLanguages();
+      if (count($languages) > 1 && array_key_exists('ar', $languages)) {
+        $alias_lang = $languages['ar']->getId();
+      }
+    }
+    return $alias_lang;
+  }
+
+  /**
+   * Get node entity object from given alias.
+   *
+   * @param string $alias
+   *   The alias to use to get entity.
+   * @param string $bundle
+   *   (optional) The bundle to validate entity against.
+   *
+   * @return \Drupal\node\NodeInterface|bool
+   *   The node object, or FALSE if nothing found.
+   */
+  public function getNodeFromAlias($alias, $bundle = '') {
+    // Get the internal path of given alias and get route parameters.
+    $internal_path = $this->aliasManager->getPathByAlias('/' . $alias, $this->getAliasLang($alias));
+    // Throw page not found error if internal path doesn't contain node path.
+    if (strpos($internal_path, 'node') === FALSE) {
+      return FALSE;
+    }
+    // Get the parameters, to get node id from internal path.
+    $params = Url::fromUri("internal:" . $internal_path)->getRouteParameters();
+
+    if (!empty($params['node']) && $node = $this->entityTypeManager->getStorage('node')->load($params['node'])) {
+      if ($node instanceof NodeInterface && $node->bundle() == $bundle) {
+        $langcode = $this->languageManager->getCurrentLanguage()->getId();
+        if ($langcode !== $this->languageManager->getDefaultLanguage()->getId()) {
+          if ($node->hasTranslation($langcode)) {
+            $node = $node->getTranslation($langcode);
+          }
+        }
+        return $node;
+      }
+    }
+    return FALSE;
+  }
+
+  /**
+   * Prepare multiple images array for given entity on given fieldname.
+   *
+   * @param object $entity
+   *   The entity object.
+   * @param string $field_name
+   *   The field name from which it needs to create images array.
+   *
+   * @return array
+   *   The array containing information of images.
+   */
+  public function getImages($entity, $field_name) {
+    $images = [];
+    if (!empty($entity->get($field_name)->getValue())) {
+      foreach ($entity->get($field_name)->getValue() as $key => $value) {
+        if (($file = $entity->get($field_name)->get($key)->entity) && $file instanceof FileInterface) {
+          $images[] = [
+            'url' => file_create_url($file->getFileUri()),
+            'alt' => !empty($value['alt']) ? $value['alt'] : '',
+            'title' => !empty($value['title']) ? $value['title'] : '',
+          ];
+        }
+      }
+    }
+    return $images;
+  }
+
+  /**
+   * Helper function to throw an error.
+   *
+   * @throws Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+   */
+  public function throwException() {
+    throw new NotFoundHttpException($this->t("page not found"));
   }
 
 }
