8eef0f58874ebe9bb8a06cb126f1362e75304ae4
Rohit Joshi <joshi.rohit100@gmail.com>
2018-10-15 13:38:27 +0530

    CORE-5789: Fixing fatal error on parent sku sync.

--- docroot/modules/commerce/acq_commerce/modules/acq_sku/src/Entity/SKU.php
+++ docroot/modules/commerce/acq_commerce/modules/acq_sku/src/Entity/SKU.php
@@ -426,6 +426,16 @@ class SKU extends ContentEntityBase implements SKUInterface {
 
     $sku_entity = array_shift($skus);
 
+    /**
+     * Example - If we load the SKU in AR lang code while the SKUs default
+     * lang code is EN and thus while loading SKU, we will always have EN as the
+     * lang code and in early processing, we remove this and thus we have this
+     * as empty value.
+     */
+    if (empty($sku_entity)) {
+      return NULL;
+    }
+
     if ($is_multilingual) {
       if ($sku_entity->hasTranslation($langcode)) {
         $sku_entity = $sku_entity->getTranslation($langcode);
