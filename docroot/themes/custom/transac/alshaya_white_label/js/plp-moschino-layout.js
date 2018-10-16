e3df984410ae8d19addee80cd72361694786ba08
Swarad Mokal <swarad07@gmail.com>
2018-10-16 19:01:19 +0530

    CORE-5715: Theming menu content.

---
+++ docroot/themes/custom/transac/alshaya_white_label/js/plp-moschino-layout.js
@@ -0,0 +1,51 @@
+/**
+ * @file
+ * Custom js for videos on PLP page.
+ */
+
+(function ($, Drupal) {
+  'use strict';
+  Drupal.behaviors.alshayaPLPVideos = {
+    attach: function (context, settings) {
+      if ($('.plp-video').length > 1) {
+        $('#plp-video-player').ready(function () {
+
+          // Store the video object
+          var plpPlayer = $(this);
+
+          plpPlayer.play();
+
+          // Set click functions.
+          $('.video-js, .plp-mute-button').click(function () {
+            if (plpPlayer.muted()) {
+              plpPlayer.muted(false);
+              $('.plp-mute-button').removeClass('plp-video-muted');
+            }
+            else {
+              plpPlayer.muted(true);
+              $('.plp-mute-button').addClass('plp-video-muted');
+            }
+          });
+        });
+      }
+
+      // Accordion for submenu links.
+      $('.field--name-field-plp-menu').find('.mos-menu-item').each(function () {
+        $(this).once('accordion-init').accordion({
+          heightStyle: 'content',
+          collapsible: true,
+          active: false
+        });
+      });
+
+      // Show the sub menu on click of the sub menu btn.
+      $('.moschino-layout-submenu-icon .sub-menu-btn').on('click', function () {
+        $('.moschino-sub-menu-content').toggle();
+      });
+
+      $('.moschino-sub-menu-content .close-btn').on('click', function () {
+        $('.moschino-sub-menu-content').toggle();
+      });
+    }
+  };
+})(jQuery, Drupal);
