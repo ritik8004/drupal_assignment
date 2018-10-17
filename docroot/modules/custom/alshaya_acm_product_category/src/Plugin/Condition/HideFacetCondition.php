a5ca676b2925d59e2d5a4f8ec2ee1a3221093ee9
akanksha-hp <akanksha@axelerant.com>
2018-10-17 20:03:44 +0530

    CORE-5650: Fixing review comments for PLP Moschino.

--- docroot/modules/custom/alshaya_acm_product_category/src/Plugin/Condition/HideFacetCondition.php
+++ docroot/modules/custom/alshaya_acm_product_category/src/Plugin/Condition/HideFacetCondition.php
@@ -35,25 +35,6 @@ class HideFacetCondition extends ConditionPluginBase implements ContainerFactory
   }
 
   /**
-   * Creates a new HideFacetCondition object.
-   *
-   * @param array $configuration
-   *   The plugin configuration, i.e. an array with configuration values keyed
-   *   by configuration option name. The special key 'context' may be used to
-   *   initialize the defined contexts by setting it to an array of context
-   *   values keyed by context names.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   *  @codingStandardsIgnoreStart
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    // @codingStandardsIgnoreEnd
-  }
-
-  /**
    * {@inheritdoc}
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
