de6d07b81f1cc2d8dcfc1346763a33075cb3b53c
nikunj <me@nik4u.com>
2018-10-23 18:29:44 +0530

    CORE-5863: Log messaes for INFO and above.

--- factory-hooks/post-settings-php/loggly.php
+++ factory-hooks/post-settings-php/loggly.php
@@ -10,6 +10,7 @@
 if ($settings['env'] !== 'local') {
   global $site_name;
 
+  $config['logs_http.settings']['severity_level'] = 6;
   $config['logs_http.settings']['uuid'] = $settings['env'] . '-' . $site_name;
   $config['logs_http.settings']['url'] = 'http://logs-01.loggly.com/inputs/dfab01a8-9cf2-423e-a1af-d6aa5ebe37a0/tag/http/';
 }
