96639763d4cde0178aecf02c273480c1d5a765f5
nikunj <me@nik4u.com>
2018-10-23 16:40:55 +0530

    CORE-5863: Enable logs_http module and configure loggly.

---
+++ factory-hooks/post-settings-php/loggly.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Implementation of Loggly.
+ *
+ * @see https://www.loggly.com/blog/logs-for-drupal-why-you-need-them-and-how-to-do-it/
+ */
+
+// Do it for all envs except local for now.
+if ($settings['env'] !== 'local') {
+  global $site_name;
+
+  $config['logs_http.settings']['uuid'] = $settings['env'] . '-' . $site_name;
+  $config['logs_http.settings']['url'] = 'http://logs-01.loggly.com/inputs/dfab01a8-9cf2-423e-a1af-d6aa5ebe37a0/tag/http/';
+}
