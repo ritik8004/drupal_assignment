610b371a773fb384093093b99dd51e23989a799e
akanksha-hp <akanksha@axelerant.com>
2018-10-23 18:45:14 +0530

    CORE-5861: Correct filename for download.

--- docroot/modules/custom/alshaya_acm_product/src/Controller/ProductReportController.php
+++ docroot/modules/custom/alshaya_acm_product/src/Controller/ProductReportController.php
@@ -8,6 +8,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Datetime\DateFormatterInterface;
 
 /**
  * Class ProductReportController.
@@ -31,12 +33,28 @@ class ProductReportController extends ControllerBase {
   protected $fileSystem;
 
   /**
+   * Current time service.
+   *
+   * @var \Drupal\Component\Datetime\TimeInterface
+   */
+  protected $currentTime;
+
+  /**
+   * Date formatter service.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  protected $dateFormatter;
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('database'),
-      $container->get('file_system')
+      $container->get('file_system'),
+      $container->get('datetime.time'),
+      $container->get('date.formatter')
     );
   }
 
@@ -47,10 +65,19 @@ class ProductReportController extends ControllerBase {
    *   Database object.
    * @param \Drupal\Core\File\FileSystemInterface $fileSystem
    *   The filesystem service.
+   * @param \Drupal\Component\Datetime\TimeInterface $current_time
+   *   Current time service.
+   * @param \Drupal\Core\Datetime\DateFormatterInterface $date_formatter
+   *   Date formatter service.
    */
-  public function __construct(Connection $database, FileSystemInterface $fileSystem) {
+  public function __construct(Connection $database,
+                              FileSystemInterface $fileSystem,
+                              TimeInterface $current_time,
+                              DateFormatterInterface $date_formatter) {
     $this->database = $database;
     $this->fileSystem = $fileSystem;
+    $this->currentTime = $current_time;
+    $this->dateFormatter = $date_formatter;
   }
 
   /**
@@ -72,7 +99,11 @@ class ProductReportController extends ControllerBase {
     $skus = $select->execute()->fetchAll(\PDO::FETCH_ASSOC);
 
     $path = file_create_url($this->fileSystem->realpath("temporary://"));
-    $fp = fopen($path . '/export-sku-data.csv', 'w');
+    $site_name = $this->config('system.site')->get('name');
+    $time_format = $this->dateFormatter->format($this->currentTime->getRequestTime(), 'custom', 'Ymd');
+    $filename = 'product-report-' . $site_name . '-' . $time_format . '.csv';
+
+    $fp = fopen($path . '/' . $filename, 'w');
     fwrite($fp, 'SKU, Type, Language, Product ID, Stock, Price, Final Price, Special Price' . "\n");
     foreach ($skus as $sku) {
       fwrite($fp, $sku['sku'] . ',' . $sku['type'] . ',' . $sku['langcode'] . ',' . $sku['product_id'] . ',' . $sku['stock'] . ',' . $sku['price'] . ',' . $sku['final_price'] . ',' . $sku['special_price'] . "\n");
@@ -80,12 +111,11 @@ class ProductReportController extends ControllerBase {
 
     fclose($fp);
 
-    $filename = $path . '/export-sku-data.csv';
-    $response = new BinaryFileResponse($filename);
-    $site_name = $this->config('system.site')->get('name');
+    $response = new BinaryFileResponse($path . '/' . $filename);
+
     $disposition = $response->headers->makeDisposition(
       ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-      'Product-report' . $site_name . '.csv'
+      $filename
     );
     $response->headers->set('Content-Disposition', $disposition);
     return $response;
