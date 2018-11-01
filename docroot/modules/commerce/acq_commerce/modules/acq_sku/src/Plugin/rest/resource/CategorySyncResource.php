d54bc341ad2e5372069a45f058166007c8251f2d
Rohit Joshi <joshi.rohit100@gmail.com>
2018-11-01 12:13:26 +0530

    CORE-6028: Category sync improvement.

--- docroot/modules/commerce/acq_commerce/modules/acq_sku/src/Plugin/rest/resource/CategorySyncResource.php
+++ docroot/modules/commerce/acq_commerce/modules/acq_sku/src/Plugin/rest/resource/CategorySyncResource.php
@@ -107,7 +107,12 @@ class CategorySyncResource extends ResourceBase {
       $categories
     );
 
-    $response['success'] = (bool) (($response['created'] > 0) || ($response['updated'] > 0));
+    $response['success'] = (bool) ((count($response['created']) > 0) || (count($response['updated']) > 0));
+
+    $this->logger->info('New categories created: @new_cat  Old Cat: @old_cat', [
+      '@new_cat' => json_encode($response['created']),
+      '@old_cat' => json_encode($response['updated']),
+    ]);
 
     return (new ModifiedResourceResponse($response));
   }
