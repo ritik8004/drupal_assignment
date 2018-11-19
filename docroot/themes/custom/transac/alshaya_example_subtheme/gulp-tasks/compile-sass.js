252d77684a79cb1d3b270e652685d769dcf52235
prashant114606 <prashant114606@gmail.com>
2018-11-19 12:29:32 +0530

    CORE-6035: Adding feature specific gulp task and directory inside example subtheme.

--- docroot/themes/custom/transac/alshaya_example_subtheme/gulp-tasks/compile-sass.js
+++ docroot/themes/custom/transac/alshaya_example_subtheme/gulp-tasks/compile-sass.js
@@ -10,6 +10,12 @@ module.exports = function (gulp, plugins, options) {
     return gulp.src([
       options.sass.files
     ])
+      .pipe(plugins.plumber({
+        errorHandler: function (e) {
+          console.log(e.messageFormatted);
+          this.emit('end');
+        }
+      }))
       .pipe(plugins.sassGlob())
       .pipe(plugins.sass({
         errLogToConsole: true,
@@ -28,7 +34,7 @@ module.exports = function (gulp, plugins, options) {
       options.sass.files
     ])
       .pipe(plugins.plumber({
-        errorHandler: function(e) {
+        errorHandler: function (e) {
           console.log(e.messageFormatted);
           this.emit('end');
         }
@@ -46,5 +52,62 @@ module.exports = function (gulp, plugins, options) {
       .pipe(plugins.sourcemaps.write())
       .pipe(plugins.plumber.stop())
       .pipe(gulp.dest(options.sass.destination));
+  });
+
+  gulp.task('compile:module-component-libraries-ltr', function () {
+    return gulp.src([
+      options.sass.directionalSource
+    ])
+      .pipe(plugins.plumber({
+        errorHandler: function (e) {
+          console.log(e.messageFormatted);
+          this.emit('end');
+        }
+      }))
+      .pipe(plugins.sourcemaps.init())
+      .pipe(plugins.sassGlob())
+      .pipe(plugins.sass({
+        errLogToConsole: true,
+        outputStyle: 'expanded'
+      }))
+      .pipe(plugins.autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      }))
+      .pipe(plugins.rename({
+        suffix: '.ltr'
+      }))
+      .pipe(plugins.sourcemaps.write())
+      .pipe(plugins.plumber.stop())
+      .pipe(gulp.dest(options.sass.directionalDestination));
+  });
+
+  gulp.task('compile:module-component-libraries-rtl', function () {
+    return gulp.src([
+      options.sass.directionalSource
+    ])
+      .pipe(plugins.plumber({
+        errorHandler: function (e) {
+          console.log(e.messageFormatted);
+          this.emit('end');
+        }
+      }))
+      .pipe(plugins.sourcemaps.init())
+      .pipe(plugins.sassGlob())
+      .pipe(plugins.sassVariables(options.sass.variables))
+      .pipe(plugins.sass({
+        errLogToConsole: true,
+        outputStyle: 'expanded'
+      }))
+      .pipe(plugins.autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      }))
+      .pipe(plugins.rename({
+        suffix: '.rtl'
+      }))
+      .pipe(plugins.sourcemaps.write())
+      .pipe(plugins.plumber.stop())
+      .pipe(gulp.dest(options.sass.directionalDestination));
   });
 };
