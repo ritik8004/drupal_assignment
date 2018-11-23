f2ee35d280c59b70f76561f3e62c5a76ca39ab81
nikunj <me@nik4u.com>
2018-11-23 15:48:40 +0530

    CORE-6327: Refactor code to make code more simpler.

--- factory-hooks/post-settings-php/cybersource.php
+++ factory-hooks/post-settings-php/cybersource.php
@@ -6,10 +6,9 @@
  * @see https://docs.acquia.com/site-factory/tiers/paas/workflow/hooks
  */
 
+// Set cybersource env to test on all non-prod envs.
+$settings['acq_cybersource.settings']['env'] = 'test';
+
 if (in_array($settings['env'], ['01live', '01update'])) {
   $settings['acq_cybersource.settings']['env'] = 'prod';
-}
-else {
-  // Set cybersource env to test on all non-prod envs.
-  $settings['acq_cybersource.settings']['env'] = 'test';
 }
