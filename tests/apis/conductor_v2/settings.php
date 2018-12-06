da5f3e3ac93d59c6c6a2ad396bc6322ec9b843ca
vbouchet <vincent.bouchet.31@gmail.com>
2018-12-06 09:51:54 +0300

    CORE-000: Provide a script to loop through all the ACM queues.

--- tests/apis/conductor_v2/settings.php
+++ tests/apis/conductor_v2/settings.php
@@ -11,6 +11,6 @@ global $config;
 $config = [
   'org_id' => 1,
   'url' => 'https://api.eu-west-1.prod.acm.acquia.io/v2/',
-  'hmac_key' => 'xxxxxxxxxxx',
-  'hmac_secret' => 'xxxxxxxxxxxx',
+  'hmac_key' => '55f360f632174015aafeb460d7acf3ec',
+  'hmac_secret' => 'M2M3NWZlYjItMDM2Ny00',
 ];
