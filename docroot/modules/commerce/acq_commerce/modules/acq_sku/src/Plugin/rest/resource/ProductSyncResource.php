4d716f6655bf1a86253b30ebf24c76eb58a66478
nikunj <me@nik4u.com>
2018-12-06 13:04:13 +0300

    CORE-6382: Log diff of categories when saving product node.

--- docroot/modules/commerce/acq_commerce/modules/acq_sku/src/Plugin/rest/resource/ProductSyncResource.php
+++ docroot/modules/commerce/acq_commerce/modules/acq_sku/src/Plugin/rest/resource/ProductSyncResource.php
@@ -383,6 +383,8 @@ class ProductSyncResource extends ResourceBase {
             $node = $this->createDisplayNode($product, $langcode);
           }
 
+          $existingCategories = $node->get('field_category')->getValue();
+
           $node->get('title')->setValue(html_entity_decode($product['name']));
 
           $description = (isset($product['attributes']['description'])) ? $product['attributes']['description'] : '';
@@ -409,6 +411,17 @@ class ProductSyncResource extends ResourceBase {
           \Drupal::moduleHandler()->alter('acq_sku_product_node', $node, $product);
 
           $node->save();
+
+          // Log the diff if there were any existing categories in the node.
+          // This will avoid logging any diffs when we are creating node.
+          if ($existingCategories) {
+            $updatedCategories = $node->get('field_category')->getValue();
+            $this->logger->info('Categories diff for @sku for @langcode: @diff', [
+              '@sku' => $sku->getSku(),
+              '@langcode' => $langcode,
+              '@diff' => self::getArrayDiff($existingCategories, $updatedCategories),
+            ]);
+          }
         }
         else {
           try {
