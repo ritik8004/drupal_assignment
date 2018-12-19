becc459e13b61f07ad3b4b46e70509b2a2161ace
Moneesh Koundal <moneesh.koundal@acquia.com>
2018-12-19 14:18:18 +0530

    CORE-000: Moved the clear cache with db restore.

--- scripts/utilities/reset-from-post-stage-dumps.sh
+++ scripts/utilities/reset-from-post-stage-dumps.sh
@@ -45,10 +45,10 @@ do
     drush -l $site.$env-alshaya.acsitefactory.com sql-drop -y
     `drush -l $site.$env-alshaya.acsitefactory.com sql-connect` < ~/backup/$target_env/post-stage/$site.sql
     rm ~/backup/$target_env/post-stage/$site.sql
-  fi
 
-  ## Clearing cache before running updb to the site.
-  drush -l $site.$env-alshaya.acsitefactory.com cr
+    ## Clearing cache before running updb to the site.
+    drush -l $site.$env-alshaya.acsitefactory.com cr
+  fi
 
   ## Apply the database updates to the site.
   echo "Executing updb."
