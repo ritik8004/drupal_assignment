3de605a445dd74abe9d1fe38ef9962b2cd3e53ba
prashant114606 <prashant114606@gmail.com>
2018-12-21 10:32:43 +0530

    CORE-4130: updating the class name inside the attribute.js

--- docroot/themes/custom/transac/alshaya_white_label/js/attribute.js
+++ docroot/themes/custom/transac/alshaya_white_label/js/attribute.js
@@ -43,7 +43,7 @@
       var clickedOption = $('select option:selected', that);
       if (!clickedOption.is(':disabled')) {
         if ($(this).parent().hasClass('form-item-configurables-article-castor-id')) {
-          Drupal.alshaya_liquid_pixel_images_update_selected_swatch_label();
+          Drupal.alshaya_color_swatch_update_selected_label();
         }
         else {
           $('.select2Option', that).find('.list-title .selected-text').html(clickedOption.text());
