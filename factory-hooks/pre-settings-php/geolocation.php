9ac56a60366727bdf2e3313a87ee3c5c88634bf7
Rohit Joshi <joshi.rohit100@gmail.com>
2019-01-03 09:18:07 +0530

    CORE-6780: Add check to not revert google map key for prod env.

--- factory-hooks/pre-settings-php/geolocation.php
+++ factory-hooks/pre-settings-php/geolocation.php
@@ -6,5 +6,8 @@
  * @see https://docs.acquia.com/site-factory/tiers/paas/workflow/hooks
  */
 
-// Set google map api key.
-$settings['geolocation.settings']['google_map_api_key'] = 'AIzaSyCx3OwGGlm7KqnrbZQIQ3FQLJgWKb3p5LI';
+// If not prod env.
+if (!in_array($settings['env'], ['01live', '01update'])) {
+  // Set google map api key.
+  $settings['geolocation.settings']['google_map_api_key'] = 'AIzaSyCx3OwGGlm7KqnrbZQIQ3FQLJgWKb3p5LI';
+}
