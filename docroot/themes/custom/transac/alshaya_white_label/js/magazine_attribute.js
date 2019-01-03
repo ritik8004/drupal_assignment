d0259ed375134ee4d9e0cfa61132d4936569fc34
Saket Kumar <saki007ster@gmail.com>
2019-01-03 17:48:25 +0530

    CORE-6794: Fixing mobile sticky div behaviour.

--- docroot/themes/custom/transac/alshaya_white_label/js/magazine_attribute.js
+++ docroot/themes/custom/transac/alshaya_white_label/js/magazine_attribute.js
@@ -134,52 +134,6 @@
   };
 
   /**
-   * Helper function to compute height of add to cart button and make it sticky.
-   *
-   * @param {String} direction The scroll direction.
-   * @param {string} state The moment when function is called, initial/after.
-   */
-  function mobileMagazineSticky(direction, state) {
-    // Sticky Section.
-    var stickyDiv = $('.magazine-layout .content__title_wrapper');
-    // This is the wrapper that holds delivery options.
-    var mobileContentWrapper = $('.c-pdp .mobile-content-wrapper');
-    var windowBottom;
-    var mobileCWBottom;
-    if (state === 'initial') {
-      // Button top.
-      var stickyDivTop = stickyDiv.offset().top + stickyDiv.height();
-      // Screen bottom.
-      windowBottom = $(window).scrollTop() + $(window).height();
-      if (stickyDivTop > windowBottom) {
-        stickyDiv.addClass('fixed');
-      }
-      else {
-        stickyDiv.removeClass('fixed');
-      }
-      return;
-    }
-    else {
-      // mobileContentWrapper bottom, based on direction we have to factor in
-      // the height of button if it is already fixed.
-      mobileCWBottom = mobileContentWrapper.offset().top + mobileContentWrapper.height();
-      if (direction === 'up') {
-        mobileCWBottom = mobileContentWrapper.offset().top + mobileContentWrapper.height() + stickyDiv.outerHeight() - 60;
-      }
-
-      // Screen scroll offset.
-      windowBottom = $(window).scrollTop() + $(window).height();
-      // Hide button when we are below delivery wrapper.
-      if (windowBottom > mobileCWBottom && mobileContentWrapper.length) {
-        stickyDiv.removeClass('fixed');
-      }
-      else {
-        stickyDiv.addClass('fixed');
-      }
-    }
-  }
-
-  /**
    *
    * JS function to move mobile colors to bellow of PDP main image in
    * product description section.
@@ -292,43 +246,20 @@
     attach: function (context, settings) {
       // Only on mobile.
       if ($(window).width() < 768) {
-        // Select the node that will be observed for mutations.
-        var targetNode = document.querySelector('.acq-content-product .sku-base-form');
-        // Options for the observer (which mutations to observe).
-        var config = {attributes: true, childList: false, subtree: false};
-        // Callback function to execute when mutations are observed.
-        var callback = function (mutationsList, observer) {
-          mutationsList.forEach(function (mutation) {
-            if ((mutation.type === 'attributes') &&
-              (mutation.attributeName === 'class') &&
-              (!mutation.target.classList.contains('visually-hidden'))) {
-              var buttonHeight = $('.c-pdp .mobile-content-wrapper .basic-details-wrapper .edit-add-to-cart').outerHeight();
-              var mobileContentWrapper = $('.c-pdp .mobile-content-wrapper .basic-details-wrapper');
-              mobileContentWrapper.css('height', 'auto');
-              mobileContentWrapper.css('height', mobileContentWrapper.height() + buttonHeight - 8);
-              observer.disconnect();
-            }
-          });
-        };
-        // Create an observer instance linked to the callback function.
-        var observer = new MutationObserver(callback);
-        // Start observing the target node for configured mutations.
-        observer.observe(targetNode, config);
-        mobileMagazineSticky('bottom', 'initial');
-        var lastScrollTop = 0;
+        var stickyDiv = $('.magazine-layout .content__title_wrapper');
+        var mobileContentWrapper = $('.c-pdp .mobile-content-wrapper');
+        stickyDiv.addClass('fixed');
         $(window).on('scroll', function () {
-          var windowScrollTop = $(this).scrollTop();
-          var direction = 'bottom';
-          if (windowScrollTop > lastScrollTop) {
-            direction = 'bottom';
+          // Screen bottom.
+          var mobileCWBottom = mobileContentWrapper.offset().top + mobileContentWrapper.height() + stickyDiv.height() + 64;
+          var windowBottom = $(this).scrollTop() + $(this).height();
+          if (mobileCWBottom > windowBottom) {
+            stickyDiv.addClass('fixed');
           }
           else {
-            direction = 'up';
+            stickyDiv.removeClass('fixed');
           }
-          lastScrollTop = windowScrollTop;
-          mobileMagazineSticky(direction, 'after');
         });
-
       }
     }
   };
