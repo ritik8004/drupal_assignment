5880797f9790c285a8125d75ab8f3ddaae5a9be3
nikunj <me@nik4u.com>
2019-01-07 21:37:19 +0530

    CORE-6865: Correct index data.

---
+++ docroot/modules/custom/alshaya_search_api/src/Commands/AlshayaSearchApiCommands.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Drupal\alshaya_search_api\Commands;
+
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Logger\LoggerChannelInterface;
+use Drush\Commands\DrushCommands;
+
+/**
+ * Class AlshayaSearchApiCommands.
+ *
+ * @package Drupal\alshaya_search_api\Commands
+ */
+class AlshayaSearchApiCommands extends DrushCommands {
+
+  /**
+   * Database Connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  private $connection;
+
+  /**
+   * The date time service.
+   *
+   * @var \Drupal\Component\Datetime\TimeInterface
+   */
+  private $dateTime;
+
+  /**
+   * AlshayaSearchApiCommands constructor.
+   *
+   * @param \Drupal\Core\Database\Connection $connection
+   *   Database Connection.
+   * @param \Drupal\Component\Datetime\TimeInterface $date_time
+   *   The Date Time service.
+   * @param \Drupal\Core\Logger\LoggerChannelInterface $logger
+   *   Logger.
+   */
+  public function __construct(Connection $connection,
+                              TimeInterface $date_time,
+                              LoggerChannelInterface $logger) {
+    $this->connection = $connection;
+    $this->dateTime = $date_time;
+    $this->setLogger($logger);
+  }
+
+  /**
+   * Correct index data.
+   *
+   * @command alshaya_search_api:correct-index-data
+   *
+   * @aliases correct-index-data
+   */
+  public function correctIndexData() {
+    $query = $this->connection->query('SELECT product.item_id FROM {search_api_db_product} product 
+      LEFT JOIN {search_api_item} item ON item.item_id = product.item_id 
+      WHERE item.item_id IS NULL');
+
+    $item_ids = $query->fetchAll();
+
+    if (empty($item_ids)) {
+      $this->say('Every-thing good.');
+      return;
+    }
+
+    $indexes = ['acquia_search_index', 'product'];
+    $requested_time = $this->dateTime->getRequestTime();
+
+    $item_ids = array_column($item_ids, 'item_id');
+
+    foreach ($item_ids as $item_id) {
+      foreach ($indexes as $index) {
+        $this->connection->merge('search_api_item')
+          ->key([
+            'item_id' => $item_id,
+            'index_id' => $index,
+          ])
+          ->fields([
+            'datasource' => 'entity:node',
+            'status' => 1,
+            'changed' => $requested_time,
+          ])
+          ->execute();
+      }
+
+      $this->io()->writeln('Index data corrected for: ' . $item_id);
+      $this->logger->info('Index data corrected for: @item_id', [
+        '@item_id' => $item_id,
+      ]);
+    }
+  }
+
+}
