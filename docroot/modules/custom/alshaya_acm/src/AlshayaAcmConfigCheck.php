3d7fc5c4f242350146067e44e30c57ec513b8acf
Rohit Joshi <joshi.rohit100@gmail.com>
2019-01-07 14:53:54 +0530

    CORE-6780: Correcting the param.

--- docroot/modules/custom/alshaya_acm/src/AlshayaAcmConfigCheck.php
+++ docroot/modules/custom/alshaya_acm/src/AlshayaAcmConfigCheck.php
@@ -148,15 +148,18 @@ class AlshayaAcmConfigCheck {
     ];
 
     if (in_array($config_reset, $reset)) {
+      $reset = !empty($config_reset) ? [$config_reset] : $reset;
       // Reset the settings.
-      $config = $this->configFactory->getEditable($config_reset);
-      $settings = Settings::get($config_reset);
+      foreach ($reset as $config_key) {
+        $config = $this->configFactory->getEditable($config_key);
+        $settings = Settings::get($config_key);
 
-      foreach ($settings as $key => $value) {
-        $config->set($key, $value);
+        foreach ($settings as $key => $value) {
+          $config->set($key, $value);
+        }
+
+        $config->save();
       }
-
-      $config->save();
     }
 
     // Re-configure Simple Oauth.
