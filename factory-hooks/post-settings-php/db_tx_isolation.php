f4aff7c34edffa3a21741cb685b7ffb8fd1b90fb
Rohit Joshi <joshi.rohit100@gmail.com>
2019-01-21 16:08:36 +0530

    CORE-6839: Adding check for the ACSF.

--- factory-hooks/post-settings-php/db_tx_isolation.php
+++ factory-hooks/post-settings-php/db_tx_isolation.php
@@ -11,7 +11,11 @@
 // to `READ-COMMITTED` to avoid/minimize the deadlocks.
 // @see https://support.acquia.com/hc/en-us/articles/360005253954-Fixing-database-deadlocks
 // for reference.
-$databases['default']['default']['init_commands'] = [
-  'isolation' => "SET SESSION tx_isolation='READ-COMMITTED'",
-];
-acquia_hosting_db_choose_active($conf['acquia_hosting_site_info']['db'], 'default', $databases, $conf);
+
+// Only for the ACSF environment.
+if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
+  $databases['default']['default']['init_commands'] = [
+    'isolation' => "SET SESSION tx_isolation='READ-COMMITTED'",
+  ];
+  acquia_hosting_db_choose_active($conf['acquia_hosting_site_info']['db'], 'default', $databases, $conf);
+}
