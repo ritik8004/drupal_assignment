e414ceaa90e9fb5476553a23b5c257984fe3790c
Rohit Joshi <joshi.rohit100@gmail.com>
2019-01-21 16:56:35 +0530

    CORE-6839: Correcting the condition.

--- factory-hooks/post-settings-php/db_tx_isolation.php
+++ factory-hooks/post-settings-php/db_tx_isolation.php
@@ -12,10 +12,9 @@
 // @see https://support.acquia.com/hc/en-us/articles/360005253954-Fixing-database-deadlocks
 // for reference.
 
-// Only for the ACSF environment.
-if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
-  $databases['default']['default']['init_commands'] = [
-    'isolation' => "SET SESSION tx_isolation='READ-COMMITTED'",
-  ];
+$databases['default']['default']['init_commands'] = [
+  'isolation' => "SET SESSION tx_isolation='READ-COMMITTED'",
+];
+if (file_exists('/var/www/site-php')) {
   acquia_hosting_db_choose_active($conf['acquia_hosting_site_info']['db'], 'default', $databases, $conf);
 }
