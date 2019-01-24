0e48eb6db3ee604d80ef39bac7343c31791a66ed
Mitesh Patel <developermitesh@gmail.com>
2019-01-24 11:20:44 +0530

    CORE-7064: Refactor label for config page.

--- docroot/modules/custom/alshaya_search_api/src/Form/AlshayaListingSettingsForm.php
+++ docroot/modules/custom/alshaya_search_api/src/Form/AlshayaListingSettingsForm.php
@@ -44,7 +44,7 @@ class AlshayaListingSettingsForm extends ConfigFormBase {
 
     $form['filter_oos_product'] = [
       '#type' => 'checkbox',
-      '#title' => $this->t('Display out of stock products in listing pages.'),
+      '#title' => $this->t('Filter out of stock products in listing pages.'),
       '#default_value' => $config->get('filter_oos_product'),
     ];
 
