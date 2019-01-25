dfdd8d0fa5de6369947a398bc324bec9bebb7277
Akanksha Singh <akanksha@axelerant.com>
2019-01-25 13:00:43 +0530

    MAPP-634: PR feedback for get user status endpoint.

--- docroot/modules/custom/alshaya_mobile_app/src/Plugin/rest/resource/UserGetStatusResource.php
+++ docroot/modules/custom/alshaya_mobile_app/src/Plugin/rest/resource/UserGetStatusResource.php
@@ -3,9 +3,7 @@
 namespace Drupal\alshaya_mobile_app\Plugin\rest\resource;
 
 use Drupal\rest\Plugin\ResourceBase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\user\UserInterface;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\rest\ModifiedResourceResponse;
 
 /**
@@ -21,31 +19,16 @@ use Drupal\rest\ModifiedResourceResponse;
  */
 class UserGetStatusResource extends ResourceBase {
 
-  use StringTranslationTrait;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->getParameter('serializer.formats'),
-      $container->get('logger.factory')->get('alshaya_mobile_app')
-    );
-  }
-
   /**
    * Receive GET request with email information to send status of the user.
    *
    * @param string $email
    *   Email address for which we need to return status.
    *
-   * @return \Drupal\rest\ResourceResponse
+   * @return \Drupal\rest\ModifiedResourceResponse
    *   The response containing status of the user.
    */
-  public function get($email) {
+  public function get(string $email) {
     $response_data = [
       'status' => FALSE,
     ];
