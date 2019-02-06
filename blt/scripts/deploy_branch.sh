d2586f4c6ec8adaaf8a59bf63f654a3bdbf951d0
vbouchet <vincent.bouchet.31@gmail.com>
2019-02-06 12:42:29 +0100

    CORE-000: Update travis deploy_branch flow.

--- blt/scripts/deploy_branch.sh
+++ blt/scripts/deploy_branch.sh
@@ -5,7 +5,7 @@ set -ev
 if [[ ! $TRAVIS_BRANCH =~ ^revert-.* ]]; then
   source $BLT_DIR/scripts/travis/deploy_branch;
 else
-  echo "We don't deploy because it is a revert branch.";
+  echo ">>>>>>> We don't deploy because it is a revert branch.";
 fi
 
 set +v
