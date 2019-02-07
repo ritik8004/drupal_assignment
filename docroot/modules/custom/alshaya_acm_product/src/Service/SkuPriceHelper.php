7bbce3acfb0f9c42bce65c53e89cadae257c7c80
nikunj <me@nik4u.com>
2019-02-08 00:55:18 +0530

    CORE-7417: CORE-7418: CORE-7419: From to price display.

--- docroot/modules/custom/alshaya_acm_product/src/Service/SkuPriceHelper.php
+++ docroot/modules/custom/alshaya_acm_product/src/Service/SkuPriceHelper.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\alshaya_acm_product\Service;
 
+use Drupal\acq_sku\Entity\SKU;
 use Drupal\alshaya_acm_product\SkuManager;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
  * Class SkuPriceHelper.
@@ -11,6 +13,8 @@ use Drupal\Core\Config\ConfigFactoryInterface;
  * @package Drupal\alshaya_acm_product\Service
  */
 class SkuPriceHelper {
+
+  use StringTranslationTrait;
 
   const PRICE_DISPLAY_MODE_SIMPLE = 'simple';
 
@@ -31,6 +35,20 @@ class SkuPriceHelper {
   private $displayMode;
 
   /**
+   * Decimal points to show for price.
+   *
+   * @var int
+   */
+  private $decimalPoints;
+
+  /**
+   * Build array updated across functions.
+   *
+   * @var array
+   */
+  private $build;
+
+  /**
    * SkuPriceHelper constructor.
    *
    * @param \Drupal\alshaya_acm_product\SkuManager $sku_manager
@@ -42,8 +60,194 @@ class SkuPriceHelper {
                               ConfigFactoryInterface $config_factory) {
     $this->skuManager = $sku_manager;
 
-    $config = $config_factory->get('alshaya_acm_product.display_settings');
-    $this->displayMode = $config->get('price_display_mode') ?? self::PRICE_DISPLAY_MODE_SIMPLE;
+    $this->displayMode = $config_factory
+      ->get('alshaya_acm_product.display_settings')
+      ->get('price_display_mode') ?? self::PRICE_DISPLAY_MODE_SIMPLE;
+
+    $this->decimalPoints = (int) $config_factory
+      ->get('acq_commerce.currency')
+      ->get('decimal_points');
+  }
+
+  /**
+   * Get price block for specific sku.
+   *
+   * @param \Drupal\acq_sku\Entity\SKU $sku
+   *   Product sku for which we want the price block.
+   * @param array $options
+   *   Additional flags like vat text is required or not.
+   *
+   * @return array
+   *   Build array.
+   */
+  public function getPriceBlockForSku(SKU $sku, array $options = ['with_vat' => 1]):array {
+    $this->build = [
+      '#theme' => 'product_price_block',
+    ];
+
+    if (empty($options['color'])) {
+      $options['color'] = '';
+    }
+
+    $case = $sku->bundle() == 'configurable' ? $this->displayMode : 'simple';
+    switch ($case) {
+      case self::PRICE_DISPLAY_MODE_FROM_TO:
+        $this->buildPriceBlockFromTo($sku, $options['color']);
+        break;
+
+      case self::PRICE_DISPLAY_MODE_SIMPLE:
+      default:
+        $this->buildPriceBlockSimple($sku, $options['color']);
+        break;
+    }
+
+    if (!empty($options['with_vat'])) {
+      $this->build['#vat_text'] = $this->skuManager->getVatText();
+    }
+
+    return $this->build;
+  }
+
+  /**
+   * Update build array to display price in simple mode.
+   *
+   * @param \Drupal\acq_sku\Entity\SKU $sku
+   *   Product sku for which we want the price block.
+   * @param string $color
+   *   Color value for limiting scope of variants.
+   */
+  private function buildPriceBlockSimple(SKU $sku, string $color = '') {
+    $prices = $this->skuManager->getMinPrices($sku, $color);
+    $price = $prices['price'];
+    $final_price = $prices['final_price'];
+
+    if ($price) {
+      $this->build['#price'] = [
+        '#theme' => 'acq_commerce_price',
+        '#price' => $price,
+      ];
+
+      // Get the discounted price.
+      if ($final_price) {
+        // Final price could be same as price, we dont need to show discount.
+        if ($final_price >= $price) {
+          return;
+        }
+
+        $this->build['#final_price'] = [
+          '#theme' => 'acq_commerce_price',
+          '#price' => $final_price,
+        ];
+
+        // Get discount if discounted price available.
+        $this->build['#discount'] = [
+          '#markup' => $this->skuManager->getDiscountedPriceMarkup($price, $final_price),
+        ];
+      }
+    }
+    elseif ($final_price) {
+      $this->build['#price'] = [
+        '#theme' => 'acq_commerce_price',
+        '#price' => $final_price,
+      ];
+    }
+  }
+
+  /**
+   * Update build array to display price in from-to mode.
+   *
+   * @param \Drupal\acq_sku\Entity\SKU $sku
+   *   Product sku for which we want the price block.
+   * @param string $color
+   *   Color value for limiting scope of variants.
+   */
+  private function buildPriceBlockFromTo(SKU $sku, string $color = '') {
+    $prices = $this->skuManager->getMinPrices($sku, $color);
+
+    $child_prices = array_column($prices['children'], 'price');
+    $child_final_prices = array_column($prices['children'], 'final_price');
+    $discounts = array_column($prices['children'], 'discount');
+
+    if (count($prices['children']) <= 1
+      || count(array_unique(array_filter($child_prices))) == 1
+      || count(array_unique(array_filter($child_final_prices))) == 1) {
+
+      return $this->buildPriceBlockSimple($sku);
+    }
+
+    if (count(array_filter($child_prices)) == count(array_filter($child_final_prices))) {
+      $this->build['#price'] = [
+        '#theme' => 'price_range',
+        '#price' => $this->getMinMax($child_prices),
+      ];
+
+      $this->build['#final_price'] = [
+        '#theme' => 'price_range',
+        '#price' => $this->getMinMax($child_final_prices),
+      ];
+    }
+    else {
+      $selling_prices = array_column($prices['children'], 'price');
+      $this->build['#price'] = [
+        '#theme' => 'price_range',
+        '#price' => $this->getMinMax($selling_prices),
+      ];
+    }
+
+    $this->build['#discount'] = [
+      '#markup' => $this->getDiscountedPriceMarkup(max($discounts)),
+    ];
+  }
+
+  /**
+   * Wrapper function to get min - max string.
+   *
+   * @param array $prices
+   *   Prices array.
+   *
+   * @return string
+   *   String with min - max or just min.
+   */
+  private function getMinMax(array $prices):string {
+    $min = min($prices);
+    $max = max($prices);
+
+    if ($min == $max) {
+      return $this->getFormattedPrice($min);
+    }
+
+    return (string) $this->t('@min - @max', [
+      '@min' => $this->getFormattedPrice($min),
+      '@max' => $this->getFormattedPrice($max),
+    ]);
+  }
+
+  /**
+   * Wrapper function to get formatted price.
+   *
+   * @param string|float $price
+   *   Price.
+   *
+   * @return string
+   *   Formatted price.
+   */
+  private function getFormattedPrice($price):string {
+    return (string) number_format($price, $this->decimalPoints);
+  }
+
+  /**
+   * Get Discounted Price markup.
+   *
+   * @param float $discount
+   *   Discount percent.
+   *
+   * @return string
+   *   Price markup.
+   */
+  public function getDiscountedPriceMarkup($discount):string {
+    return $discount > 0
+      ? (string) $this->t('Save upto @discount%', ['@discount' => $discount])
+      : '';
   }
 
 }
