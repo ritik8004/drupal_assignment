de553e2706ff836d43fd6f47f2f47f29a91f3057
prashant114606 <prashant114606@gmail.com>
2019-02-14 10:46:53 +0530

    CORE-7528: Fixing Social share icon should not be clickable for OOS product.

--- docroot/themes/custom/transac/alshaya_white_label/js/socialshare_popup.js
+++ docroot/themes/custom/transac/alshaya_white_label/js/socialshare_popup.js
@@ -14,18 +14,23 @@
         });
       }
 
-      $('.magazine-layout .modal-share-this .share-icon').click(function () {
-        $('body').addClass('social-modal-overlay');
-        var top_height = ($(window).height() - $('.social-modal-overlay .sharethis-container').height()) / 2;
-        var left_width = ($(window).width() - $('.social-modal-overlay .sharethis-container').innerWidth()) / 2;
-        // Make popup content in center of the window
-        $('.sharethis-container').css({top: top_height, left: left_width});
-        modalOverlay('.close-icon', 'social-modal-overlay');
-
-        $(document).ajaxComplete(function () {
+      if ($('.sharethis-wrapper').hasClass('out-of-stock')) {
+        $('.modal-share-this').addClass('social-share-out-of-stock');
+      }
+      else {
+        $('.magazine-layout .modal-share-this .share-icon').on('click', function (e) {
+          $('body').addClass('social-modal-overlay');
+          var top_height = ($(window).height() - $('.social-modal-overlay .sharethis-container').height()) / 2;
+          var left_width = ($(window).width() - $('.social-modal-overlay .sharethis-container').innerWidth()) / 2;
+          // Make popup content in center of the window.
+          $('.sharethis-container').css({top: top_height, left: left_width});
           modalOverlay('.close-icon', 'social-modal-overlay');
+
+          $(document).ajaxComplete(function () {
+            modalOverlay('.close-icon', 'social-modal-overlay');
+          });
         });
-      });
+      }
     }
   };
 
