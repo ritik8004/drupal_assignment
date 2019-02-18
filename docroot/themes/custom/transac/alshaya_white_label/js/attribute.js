c745ed6c6bcb83586aba8251aea3f9ae70558552
nikunj <me@nik4u.com>
2019-02-19 00:49:36 +0530

    CORE-7428: Fix JSLint error and remember selection after ajax calls.

--- docroot/themes/custom/transac/alshaya_white_label/js/attribute.js
+++ docroot/themes/custom/transac/alshaya_white_label/js/attribute.js
@@ -6,6 +6,10 @@
 (function ($, Drupal) {
   'use strict';
 
+  var group_wrapper;
+  var group_anchor_wrapper;
+  var group_selected = {};
+
   Drupal.select2OptionConvert = function () {
     // Process configurable attributes which need to be shown as grouped.
     $('.form-item-configurable-select-group').once('bind-js').each(function () {
@@ -17,10 +21,10 @@
       $(this).hide();
 
       // We will add all groups inside a wrapper to show only one at a time.
-      var group_wrapper = $('<div class="group-wrapper" />');
+      group_wrapper = $('<div class="group-wrapper" />');
 
       // We will add all group anchors inside another wrapper to show together.
-      var group_anchor_wrapper = $('<div class="group-anchor-wrapper" />');
+      group_anchor_wrapper = $('<div class="group-anchor-wrapper" />');
 
       // Get alternates from last option.
       var alternates = JSON.parse($(this).find('option:last').attr('group-data'));
@@ -28,65 +32,15 @@
       // Loop through all alternates to add anchor and dropdown for each.
       for (var i in alternates) {
         if (alternates.hasOwnProperty(i)) {
-          // Select needs to have some classes for styling and boxes JS.
-          var select = $('<select class="form-item-configurable-select form-select" />');
-
-          // Copy some other attributes.
-          select.attr('data-selected-title', $(this).attr('data-selected-title'));
-          select.attr('data-default-title', $(this).attr('data-default-title'));
-
-          // Add each option with different display label based on alternate.
-          $(this).find('option').each(function () {
-            var option = $(this).clone();
-
-            var group_data = $(this).attr('group-data');
-            if ((typeof group_data == 'undefined')) {
-              option.html($(this).html());
-            }
-            else {
-              var option_alternates = JSON.parse(group_data);
-              option.html(option_alternates[i]['value'])
-            }
-
-            select.append(option);
-          });
-
-          // Bind to events and trigger same for original dropdown.
-          select.on('change', function () {
-            that.val($(this).val());
-            that.trigger('change');
-          });
-
-          // Using group class to link anchor to its group.
-          var group_class = 'group-' + alternates[i].label.toLowerCase();
-
-          // Adding each group inside its own wrapper to let box JS work as is.
-          var group = $('<div class="group ' + group_class + '" />');
-          group.append(select);
-          group_wrapper.append(group);
-
-          // Adding anchor for each group.
-          var anchor = $('<a href="#" />');
-          anchor.html(alternates[i].label);
-          anchor.on('click', function (event) {
-            event.preventDefault();
-
-            // Remove active class from both anchor and group.
-            $(this).closest('.form-type-select').find('.active').removeClass('active');
-
-            // Add active class to anchor.
-            $(this).addClass('active');
-
-            // Add active class to linked group.
-            var group_class = 'group-' + $(this).html().toLowerCase();
-            $(this).closest('.form-type-select').find('.' + group_class).addClass('active');
-          });
-
-          group_anchor_wrapper.append(anchor);
+          Drupal.processAlternateForGroupedSelected(that, i, alternates[i]);
         }
       }
 
-      if (group_wrapper.find('.group.active').length == 0) {
+      if (typeof group_selected[$(this).attr('name')] !== 'undefined') {
+        group_wrapper.find('.' + group_selected[$(this).attr('name')]).addClass('active');
+        group_anchor_wrapper.find('.' + group_selected[$(this).attr('name')]).addClass('active');
+      }
+      else {
         group_wrapper.find('.group:first').addClass('active');
         group_anchor_wrapper.find('a:first').addClass('active');
       }
@@ -133,6 +87,66 @@
         $('.select2Option', that).find('.list-title .selected-text').html(clickedOption.text());
       }
     });
+  };
+
+  Drupal.processAlternateForGroupedSelected = function (that, i, alternate) {
+    // Select needs to have some classes for styling and boxes JS.
+    var select = $('<select class="form-item-configurable-select form-select" />');
+
+    // Copy some other attributes.
+    select.attr('data-selected-title', that.attr('data-selected-title'));
+    select.attr('data-default-title', that.attr('data-default-title'));
+
+    // Add each option with different display label based on alternate.
+    that.find('option').each(function () {
+      var option = $(this).clone();
+
+      var group_data = $(this).attr('group-data');
+      if ((typeof group_data == 'undefined')) {
+        option.html($(this).html());
+      }
+      else {
+        var option_alternates = JSON.parse(group_data);
+        option.html(option_alternates[i]['value']);
+      }
+
+      select.append(option);
+    });
+
+    // Bind to events and trigger same for original dropdown.
+    select.on('change', function () {
+      that.val($(this).val());
+      that.trigger('change');
+    });
+
+    // Using group class to link anchor to its group.
+    var group_class = 'group-' + alternate.label.toLowerCase();
+
+    // Adding each group inside its own wrapper to let box JS work as is.
+    var group = $('<div class="group ' + group_class + '" />');
+    group.append(select);
+    group_wrapper.append(group);
+
+    // Adding anchor for each group.
+    var anchor = $('<a href="#" class="' + group_class + '" />');
+    anchor.html(alternate.label);
+    anchor.on('click', function (event) {
+      event.preventDefault();
+
+      var group_class = 'group-' + $(this).html().toLowerCase();
+      group_selected[$(this).parents('.form-type-configurable-select-group').find('.form-item-configurable-select-group').attr('name')] = group_class;
+
+      // Remove active class from both anchor and group.
+      $(this).closest('.form-type-select').find('.active').removeClass('active');
+
+      // Add active class to anchor.
+      $(this).addClass('active');
+
+      // Add active class to linked group.
+      $(this).closest('.form-type-select').find('.' + group_class).addClass('active');
+    });
+
+    group_anchor_wrapper.append(anchor);
   };
 
   Drupal.behaviors.configurableAttributeBoxes = {
