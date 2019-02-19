29b227d41ba8fe2245ce4d812ae622bdd5a5cb72
Mitesh Patel <developermitesh@gmail.com>
2019-02-19 18:59:27 +0530

    CORE-7542: create simple sitemap plugin for taxonomy_term to use custom status field to filter out links for acq_product_category.

---
+++ docroot/modules/custom/alshaya_sitemap/src/Plugin/simple_sitemap/UrlGenerator/AlshayaTaxonomyTermUrlGenerator.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\alshaya_sitemap\Plugin\simple_sitemap\UrlGenerator;
+
+use Drupal\simple_sitemap\Plugin\simple_sitemap\UrlGenerator\EntityUrlGenerator;
+
+/**
+ * Class AlshayaTaxonomyTermUrlGenerator.
+ *
+ * @package Drupal\simple_sitemap\Plugin\simple_sitemap\UrlGenerator
+ *
+ * @UrlGenerator(
+ *   id = "entity_taxonomy_term",
+ *   title = @Translation("Taxonomy term URL generator"),
+ *   description = @Translation("Generates Taxononmy Term URLs by overriding the 'entity' URL generator."),
+ *   weight = 5,
+ *   settings = {
+ *     "instantiate_for_each_data_set" = true,
+ *     "overrides_entity_type" = "taxonomy_term",
+ *   },
+ * )
+ */
+class AlshayaTaxonomyTermUrlGenerator extends EntityUrlGenerator {
+
+  /**
+   * Add custom status field for acq_product_category to filter out links.
+   *
+   * @inheritdoc
+   */
+  public function getDataSets() {
+    $data_sets = [];
+    $sitemap_entity_types = $this->entityHelper->getSupportedEntityTypes();
+
+    $bundle_settings = $this->generator->getBundleSettings();
+    if (!empty($bundle_settings['taxonomy_term'])) {
+      foreach ($bundle_settings['taxonomy_term'] as $bundle_name => $settings) {
+        if ($settings['index']) {
+          $keys = $sitemap_entity_types['taxonomy_term']->getKeys();
+          if ($bundle_name == 'acq_product_category') {
+            $keys += ['status' => 'field_commerce_status'];
+          }
+
+          $data_sets[] = [
+            'bundle_settings' => $bundle_settings,
+            'bundle_name' => $bundle_name,
+            'entity_type_name' => 'taxonomy_term',
+            'keys' => $keys,
+          ];
+        }
+      }
+    }
+
+    return $data_sets;
+  }
+
+}
