15ff0706498091e29bb5f36ac9c65e8809c4221b
Mohammad Rashid <mohammad.rashid@alshaya.com>
2019-03-04 09:01:53 +0000

    CORE-7661: New pr to avoid travis failing issue in pr 10383.

--- docroot/modules/custom/alshaya_acm_product/src/ProductHelper.php
+++ docroot/modules/custom/alshaya_acm_product/src/ProductHelper.php
@@ -5,6 +5,8 @@ namespace Drupal\alshaya_acm_product;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Component\Utility\Unicode;
+use Drupal\acq_commerce\SKUInterface;
+use Detection\MobileDetect;
 
 /**
  * Class ProductHelper.
@@ -16,29 +18,39 @@ class ProductHelper {
   use StringTranslationTrait;
 
   /**
-   * Max length to show in short description.
+   * Config Factory service object.
    *
-   * @var string
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
    */
-  protected $shortDescMaxLength;
+  protected $configFactory;
 
   /**
-   * Use text summary to generate short description.
+   * SKU Manager service object.
    *
-   * @var bool
+   * @var \Drupal\alshaya_acm_product\SkuManager
    */
-  protected $useTextSummary;
+  protected $skuManager;
 
   /**
-   * SkuManager constructor.
+   * Product display settings (alshaya_acm_product.display_settings).
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $productDisplaySettings;
+
+  /**
+   * SkuImagesManager constructor.
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   Config Factory service object.
+   * @param \Drupal\alshaya_acm_product\SkuManager $sku_manager
+   *   SKU Manager service object.
    */
-  public function __construct(ConfigFactoryInterface $config_factory) {
-    $display_settings = $config_factory->get('alshaya_acm_product.display_settings');
-    $this->shortDescMaxLength = $display_settings->get('short_desc_characters');
-    $this->useTextSummary = $display_settings->get('short_desc_text_summary');
+  public function __construct(ConfigFactoryInterface $config_factory,
+                              SkuManager $sku_manager) {
+    $this->configFactory = $config_factory;
+    $this->skuManager = $sku_manager;
+    $this->productDisplaySettings = $this->configFactory->get('alshaya_acm_product.display_settings');
   }
 
   /**
@@ -53,7 +65,6 @@ class ProductHelper {
     $short_desc['label'] = [
       '#markup' => $this->t('Short Description'),
     ];
-
     $desc = $this->createShortDescription($html);
     // It is very unlikely but description might be too short to process.
     if (!isset($desc['read_more'])) {
@@ -64,7 +75,6 @@ class ProductHelper {
     $short_desc['value'] = [
       '#markup' => $desc['html'],
     ];
-
     $build['short_desc'] = $short_desc;
   }
 
@@ -72,7 +82,7 @@ class ProductHelper {
    * Create short description from given html.
    */
   public function createShortDescription($html, $limit = NULL) {
-    $limit = ($limit == NULL) ? $this->shortDescMaxLength : $limit;
+    $limit = ($limit == NULL) ? $this->productDisplaySettings->get('short_desc_characters') : $limit;
     $desc_stripped = strip_tags($html);
     // It is very unlikely but description might be too short to process.
     if (Unicode::strlen($html) <= $limit || Unicode::strlen($desc_stripped) <= $limit) {
@@ -81,10 +91,9 @@ class ProductHelper {
       ];
     }
     else {
-      $desc = ($this->useTextSummary)
+      $desc = ($this->productDisplaySettings->get('short_desc_text_summary'))
         ? text_summary($html, NULL, $limit)
         : Unicode::truncate($desc_stripped, $limit, TRUE, FALSE);
-
       $return = [
         'html' => $desc . '...',
         'read_more' => TRUE,
@@ -110,12 +119,10 @@ class ProductHelper {
     if (strip_tags($short_desc) == $short_desc) {
       return $short_desc;
     }
-
     // Remove the ellipses appended at last if there any.
     if (Unicode::substr($short_desc, -3) == '...') {
       $short_desc = Unicode::substr($short_desc, 0, -3);
     }
-
     // To suppress errors by the DomDocument.
     libxml_use_internal_errors(TRUE);
     $dom = new \DOMDocument();
@@ -129,10 +136,44 @@ class ProductHelper {
       }
       $last_child = &$last_child->lastChild;
     }
-
     $short_desc = trim($dom->saveHTML());
-
     return $short_desc;
+  }
+
+  /**
+   * Display swatches on PLP/Search/Promo.
+   *
+   * @param array $build
+   *   Build array to modify.
+   * @param \Drupal\acq_commerce\SKUInterface $sku
+   *   SKU entity.
+   */
+  public function updateSwatches(array &$build, SKUInterface $sku) {
+    // We do not display swatches for color nodes.
+    $color = $build['#color'] ?? '';
+    // Display swatches only if enabled in configuration and not color node.
+    if ($this->productDisplaySettings->get('color_swatches') && empty($color)) {
+      // Get swatches for this product from media.
+      $swatches = $this->skuManager->getSwatches($sku);
+      if (!empty($swatches)) {
+        // Display the colors count for mobile only if different variants images
+        // being shown in gallery on PLP.
+        $detect = new MobileDetect();
+        if ($detect->isMobile() && $this->productDisplaySettings->get('show_variants_thumbnail_plp_gallery')) {
+          $build['#swatch_color_count'] = count($swatches) > 1
+            ? t('@swatch_count colors', ['@swatch_count' => count($swatches)])
+            : t('@swatch_count color', ['@swatch_count' => count($swatches)]);
+        }
+        elseif (!$this->productDisplaySettings->get('show_variants_thumbnail_plp_gallery')) {
+          // Show only first 'X' swatches as defined in configuration.
+          $swatch_plp_limit = $this->productDisplaySettings->get('swatch_plp_limit');
+          $build['#swatches'] = array_slice($swatches, 0, $swatch_plp_limit, TRUE);
+          $build['#swatch_more_text'] = count($swatches) > $swatch_plp_limit
+            ? t('+ @swatch_count colors', ['@swatch_count' => count($swatches) - $swatch_plp_limit])
+            : FALSE;
+        }
+      }
+    }
   }
 
 }
