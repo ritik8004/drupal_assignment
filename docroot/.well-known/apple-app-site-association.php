5c36c096411e2d3a258fa8569046532f41dfde86
Akanksha Singh <akanksha@axelerant.com>
2019-03-08 13:49:16 +0530

    MAPP-793: Secure mobile app specific files.

--- docroot/.well-known/apple-app-site-association.php
+++ docroot/.well-known/apple-app-site-association.php
@@ -1,9 +1,31 @@
 <?php
-header('Content-Type: application/json');
-$url = 'https://' . $_SERVER['SERVER_NAME'] . '/en/well-known/apple-app-site-association';
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($curl, CURLOPT_URL, $url);
-print curl_exec($curl);
+
+/**
+ * @file
+ * Contains install, update, uninstall hooks for alshaya_acm_product module.
+ */
+
+// Set the variable as false initially so
+// that file cannot be accessed till server is verified.
+$security_check = FALSE;
+
+if (($map = gardens_site_data_load_file()) && isset($map['sites'])) {
+  foreach ($map['sites'] as $domain => $site_details) {
+    // Match domain names with the site in SERVER.
+    if ($_SERVER['SERVER_NAME'] == $domain) {
+      $security_check = TRUE;
+      break;
+    }
+  }
+}
+
+if ($security_check) {
+  header('Content-Type: application/json');
+  $url = 'https://' . $_SERVER['SERVER_NAME'] . '/en/well-known/apple-app-site-association';
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($curl, CURLOPT_URL, $url);
+  print curl_exec($curl);
+}
