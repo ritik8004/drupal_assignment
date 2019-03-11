9f1aeb9e6214246f0e2e237569feeb52fc189ec8
prashant114606 <prashant114606@gmail.com>
2019-03-11 17:19:57 +0530

    CORE-7652: Calculating the height of menu when header gets fixed.

--- docroot/themes/custom/transac/alshaya_victoria_secret/js/custom.js
+++ docroot/themes/custom/transac/alshaya_victoria_secret/js/custom.js
@@ -6,11 +6,23 @@
 (function ($, Drupal) {
   'use strict';
 
-  if ($(window).width() < 1025) {
-    var TopHeight = $('.block-alshaya-super-category-menu').outerHeight() + $('.mobile-slug-banner').outerHeight();
+  Drupal.behaviors.TogglemenuWithSlugBanner = {
+    attach: function (context, settings) {
+      if ($(window).width() < 1025) {
+        $('.hamburger--menu').once().on('click', function () {
+          if ($('body').hasClass('header--fixed')) {
+            $('.toggle--sign, .mobile--close').css('top', $('.block-alshaya-super-category-menu').outerHeight());
+            $('.menu--one__list').css('top', $('.block-alshaya-super-category-menu').outerHeight() + $('.toggle--sign').outerHeight());
+          }
+          else {
+            var TopHeight = $('.block-alshaya-super-category-menu').outerHeight() + $('.mobile-slug-banner').outerHeight();
 
-    $('.toggle--sign, .mobile--close').css('top', TopHeight);
-    $('.menu--one__list').css('top', TopHeight + $('.toggle--sign').outerHeight());
-  }
+            $('.toggle--sign, .mobile--close').css('top', TopHeight);
+            $('.menu--one__list').css('top', TopHeight + $('.toggle--sign').outerHeight());
+          }
+        });
+      }
+    }
+  };
 
 })(jQuery, Drupal);
