9d2566c72215de3da0beb29e6746d973c55ae10a
Rohit Joshi <joshi.rohit100@gmail.com>
2019-03-19 15:20:16 +0530

    CORE-8052: Redirecting echo in log file as well.

--- scripts/cron/promo-sync.sh
+++ scripts/cron/promo-sync.sh
@@ -6,20 +6,20 @@
 cd /var/www/html/${AH_SITE_NAME}/docroot
 
 cartPromoSyncTime=`date`
-echo "Running cart promotion sync on" ${cartPromoSyncTime}
+echo "Running cart promotion sync on" ${cartPromoSyncTime} &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
 drush acsf-tools-ml --profiles=alshaya_transac acspm --types='cart' --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
 
 catalogPromSyncTime=`date`
-echo "Running catalog promotion sync on" ${catalogPromSyncTime}
+echo "Running catalog promotion sync on" ${catalogPromSyncTime} &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
 drush acsf-tools-ml --profiles=alshaya_transac acspm --types='category' --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
 
 attachQueueTime=`date`
-echo "Running attach queue on" ${attachQueueTime}
+echo "Running attach queue on" ${attachQueueTime} &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
 drush acsf-tools-ml queue-run acq_promotion_attach_queue --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
 
 detachQueueTime=`date`
-echo "Running detach queue on" ${detachQueueTime}
+echo "Running detach queue on" ${detachQueueTime} &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
 drush acsf-tools-ml queue-run acq_promotion_detach_queue --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
 
 promoProcessEndTime=`date`
-echo "Promo sync and queue processing is completed on" ${promoProcessEndTime}
+echo "Promo sync and queue processing is completed on" ${promoProcessEndTime} &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
