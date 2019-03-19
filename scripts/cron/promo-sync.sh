09d860d578ec6cc890c49a3209641ece4e8ecebb
Rohit Joshi <joshi.rohit100@gmail.com>
2019-03-19 15:07:44 +0530

    CORE-8052: Adding catalog promo rule as well.

--- scripts/cron/promo-sync.sh
+++ scripts/cron/promo-sync.sh
@@ -5,17 +5,21 @@
 
 cd /var/www/html/${AH_SITE_NAME}/docroot
 
-promoSyncTime=`date`
-echo "Running cart promotion sync on" ${promoSyncTime}
-drush acsf-tools-ml --profiles=alshaya_transac acspm --types='cart' --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync-cart.log
+cartPromoSyncTime=`date`
+echo "Running cart promotion sync on" ${cartPromoSyncTime}
+drush acsf-tools-ml --profiles=alshaya_transac acspm --types='cart' --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
+
+catalogPromSyncTime=`date`
+echo "Running catalog promotion sync on" ${catalogPromSyncTime}
+drush acsf-tools-ml --profiles=alshaya_transac acspm --types='category' --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
 
 attachQueueTime=`date`
 echo "Running attach queue on" ${attachQueueTime}
-drush acsf-tools-ml queue-run acq_promotion_attach_queue --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync-cart.log
+drush acsf-tools-ml queue-run acq_promotion_attach_queue --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
 
 detachQueueTime=`date`
 echo "Running detach queue on" ${detachQueueTime}
-drush acsf-tools-ml queue-run acq_promotion_detach_queue --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync-cart.log
+drush acsf-tools-ml queue-run acq_promotion_detach_queue --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
 
 promoProcessEndTime=`date`
 echo "Promo sync and queue processing is completed on" ${promoProcessEndTime}
