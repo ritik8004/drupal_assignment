a6aa54943c0519bafce2665a16ab2f0f02a2bf0e
Rohit Joshi <joshi.rohit100@gmail.com>
2019-03-19 14:52:06 +0530

    CORE-8052: Move logs to file.

--- scripts/cron/promo-sync.sh
+++ scripts/cron/promo-sync.sh
@@ -7,12 +7,15 @@ cd /var/www/html/${AH_SITE_NAME}/docroot
 
 promoSyncTime=`date`
 echo "Running cart promotion sync on" ${promoSyncTime}
-drush acsf-tools-ml --profiles=alshaya_transac acspm --types='cart' --verbose
+drush acsf-tools-ml --profiles=alshaya_transac acspm --types='cart' --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync-cart.log
 
 attachQueueTime=`date`
 echo "Running attach queue on" ${attachQueueTime}
-drush acsf-tools-ml queue-run acq_promotion_attach_queue --verbose
+drush acsf-tools-ml queue-run acq_promotion_attach_queue --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync-cart.log
 
 detachQueueTime=`date`
 echo "Running detach queue on" ${detachQueueTime}
-drush acsf-tools-ml queue-run acq_promotion_detach_queue --verbose
+drush acsf-tools-ml queue-run acq_promotion_detach_queue --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync-cart.log
+
+promoProcessEndTime=`date`
+echo "Promo sync and queue processing is completed on" ${promoProcessEndTime}
