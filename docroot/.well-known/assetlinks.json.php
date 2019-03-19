c1255c180e3aae0b5e380ca6244d0e3cc1f9584c
Akanksha Singh <akanksha@axelerant.com>
2019-03-19 14:50:49 +0530

    MAPP-793: Secure mobile app specific files.

--- docroot/.well-known/assetlinks.json.php
+++ docroot/.well-known/assetlinks.json.php
@@ -6,27 +6,19 @@
 
 include_once '../sites/g/sites.inc';
 
-// Set the variable as false initially so
-// that file cannot be accessed till server is verified.
-$security_check = FALSE;
-
 if (($map = gardens_site_data_load_file()) && isset($map['sites'])) {
   foreach ($map['sites'] as $domain => $site_details) {
     // Match domain names with the site in SERVER.
     if ($_SERVER['SERVER_NAME'] == $domain) {
-      $security_check = TRUE;
+      header('Content-Type: application/json');
+      $url = 'https://' . $_SERVER['SERVER_NAME'] . '/en/well-known/assetlinks.json';
+      $curl = curl_init();
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+      curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+      curl_setopt($curl, CURLOPT_URL, $url);
+      print curl_exec($curl);
       break;
     }
   }
-}
-
-if ($security_check) {
-  header('Content-Type: application/json');
-  $url = 'https://' . $_SERVER['SERVER_NAME'] . '/en/well-known/assetlinks.json';
-  $curl = curl_init();
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-  curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-  curl_setopt($curl, CURLOPT_URL, $url);
-  print curl_exec($curl);
 }
