e5f2d709b02bc5a7aa4f49365502aa150f8a9c35
Piyuesh Kumar <piyuesh23@qed42.com>
2019-03-21 09:19:10 +0530

    CORE-7270: Redirecting old alias to new node ids.

--- docroot/modules/brands/alshaya_hm/src/Commands/AlshayaHmCommands.php
+++ docroot/modules/brands/alshaya_hm/src/Commands/AlshayaHmCommands.php
@@ -7,10 +7,10 @@ use Drupal\alshaya_acm_product\SkuManager;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Path\AliasManager;
 use Drupal\Core\State\StateInterface;
+use Drupal\redirect\Entity\Redirect;
 use Drush\Commands\DrushCommands;
 use Drupal\Core\Database\Connection;
 use Drupal\node\NodeInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Class AlshayaHmCommands.
@@ -57,13 +57,6 @@ class AlshayaHmCommands extends DrushCommands {
   protected $state;
 
   /**
-   * Request stack object.
-   *
-   * @var \Symfony\Component\HttpFoundation\Request|null
-   */
-  protected $request;
-
-  /**
    * AlshayaHmCommands constructor.
    *
    * @param \Drupal\Core\Database\Connection $connection
@@ -76,31 +69,27 @@ class AlshayaHmCommands extends DrushCommands {
    *   Config Factory service.
    * @param \Drupal\Core\State\StateInterface $state
    *   State Manager service.
-   * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
-   *   Request stack object.
    */
   public function __construct(Connection $connection,
                               SkuManager $skuManager,
                               AliasManager $aliasManager,
                               ConfigFactoryInterface $configFactory,
-                              StateInterface $state,
-                              RequestStack $requestStack) {
+                              StateInterface $state) {
     $this->connection = $connection;
     $this->skuManager = $skuManager;
     $this->aliasManager = $aliasManager;
     $this->configFactory = $configFactory;
     $this->state = $state;
-    $this->request = $requestStack->getCurrentRequest();
   }
 
   /**
    * Store url alias of all config products in the system before deleting them.
    *
-   * @command alshaya_hm:store_temp_alias
+   * @command alshaya_hm:store_old_alias_sku_map
    *
    * @validate-module-enabled alshaya_hm
    *
-   * @aliases alshaya_hm_store_temp_alias
+   * @aliases alshaya_hm_store_old_alias_sku_map
    */
   public function storeTempAlias() {
     $mappings = [];
@@ -109,8 +98,9 @@ class AlshayaHmCommands extends DrushCommands {
     $query->fields('asfd', ['sku', 'langcode']);
     $rows = $query->execute()->fetchAll();
 
+    // Fetch Alias - first child sku mapping for all configurable products.
     foreach ($rows as $row) {
-      if (!empty($color_mapping = $this->fetchAliasColorCodeMapping($row->sku, $row->langcode))) {
+      if (!empty($color_mapping = $this->fetchAliasFirstChildSkuMapping($row->sku, $row->langcode))) {
         $mappings[] = $color_mapping;
       }
     }
@@ -119,7 +109,7 @@ class AlshayaHmCommands extends DrushCommands {
   }
 
   /**
-   * Helper function to create mapping between alias & color code.
+   * Helper function to create mapping between alias & first child sku.
    *
    * @param string $sku
    *   SKU code for which mapping needs to be derived.
@@ -129,7 +119,7 @@ class AlshayaHmCommands extends DrushCommands {
    * @return array
    *   Returns mapping array between color code & alias.
    */
-  protected function fetchAliasColorCodeMapping($sku, $langcode) {
+  protected function fetchAliasFirstChildSkuMapping($sku, $langcode) {
     $sku_entity = SKU::loadFromSku($sku, $langcode);
 
     if (($node = $this->skuManager->getDisplayNode($sku, FALSE)) &&
@@ -144,14 +134,12 @@ class AlshayaHmCommands extends DrushCommands {
       }
 
       // Fetch first child SKU from the parent SKU & store its color attribute.
-      // Do the same for both languages. Skip this condition for child SKUs
-      // without a color label value.
+      // Do the same for both languages.
       if (($firt_child_sku = $this->skuManager->getFirstChildForSku($sku_entity, 'article_castor_id')) &&
-        ($firt_child_sku instanceof SKU) &&
-        ($color_code = $firt_child_sku->get('attr_color_label')->getString())) {
+        ($firt_child_sku instanceof SKU)) {
         return [
           'alias' => $this->aliasManager->getAliasByPath('/node/' . $node->id(), $node->language()->getId()),
-          'color_code' => $color_code,
+          'child_sku' => $firt_child_sku->getSku(),
           'langcode' => $node->language()->getId(),
         ];
       }
@@ -167,25 +155,23 @@ class AlshayaHmCommands extends DrushCommands {
    *
    * @validate-module-enabled alshaya_hm
    *
-   * @aliases alshaya_hm_store_set_redirects
+   * @aliases alshaya_hm_set_redirects
    */
   public function addAliasRedirects() {
     $mapping = unserialize($this->state->get(self::TEMP_ALIAS_COLOR_MAPPING_STATE_KEY));
-    $host = $this->request->getHost();
-    $redirect_map = [];
 
-    if ($mapping) {
-      foreach ($mapping as $map) {
-        $sub_path = '/' . $map['langcode'] . $map['alias'];
-        $host_key = str_replace('.', ':', $host);
-        $redirect_map[$host_key][] = [
-          'sub_path' => $sub_path,
-          'destination' => $host . $sub_path . '-' . $map['color_code'],
-        ];
-      }
+    foreach ($mapping as $map) {
+      $node = $this->skuManager->getDisplayNode($map['child_sku']);
+
+      // Create redirect from old alias to new nodes.
+      Redirect::create([
+        'redirect_source' => $map['alias'],
+        'redirect_redirect' => '/node/' . $node->id(),
+        'language' => $map['langcode'],
+        'status_code' => '301',
+      ])->save();
     }
 
-    $this->configFactory->getEditable('redirect_domain.domains')->set('domain_redirects', $redirect_map)->save();
   }
 
 }
