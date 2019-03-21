b1fd5ce8bb7cc7f06c7fc0b925a7db366c243414
DELBOSC <sylvain.delbosc@acquia.com>
2019-03-21 13:42:20 +0100

    CORE-8052: fix promo sync commands.

--- scripts/cron/promo-sync.sh
+++ scripts/cron/promo-sync.sh
@@ -7,11 +7,11 @@ cd /var/www/html/${AH_SITE_NAME}/docroot
 
 cartPromoSyncTime=`date`
 echo "Running cart promotion sync on" ${cartPromoSyncTime} &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
-drush acsf-tools-ml --profiles=alshaya_transac acspm --types='cart' --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
+drush acsf-tools-ml --profiles=alshaya_transac acspm "'--types=cart'" --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
 
 catalogPromSyncTime=`date`
 echo "Running catalog promotion sync on" ${catalogPromSyncTime} &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
-drush acsf-tools-ml --profiles=alshaya_transac acspm --types='category' --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
+drush acsf-tools-ml --profiles=alshaya_transac acspm "'--types=category'" --verbose &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
 
 attachQueueTime=`date`
 echo "Running attach queue on" ${attachQueueTime} &>> /var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-promo-sync.log
