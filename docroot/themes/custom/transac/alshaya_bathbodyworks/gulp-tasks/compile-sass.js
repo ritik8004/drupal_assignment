299367bd00366870e8e058c868fe9580e050ce7d
prashant114606 <prashant114606@gmail.com>
2019-03-25 19:01:18 +0530

    CORE-5173: Theming of fragrance filter.

--- docroot/themes/custom/transac/alshaya_bathbodyworks/gulp-tasks/compile-sass.js
+++ docroot/themes/custom/transac/alshaya_bathbodyworks/gulp-tasks/compile-sass.js
@@ -10,6 +10,12 @@ module.exports = function (gulp, plugins, options) {
     return gulp.src([
       options.sass.files
     ])
+      .pipe(plugins.plumber({
+        errorHandler: function (e) {
+          console.log(e.messageFormatted);
+          this.emit('end');
+        }
+      }))
       .pipe(plugins.sassGlob())
       .pipe(plugins.sass({
         errLogToConsole: true,
@@ -29,7 +35,7 @@ module.exports = function (gulp, plugins, options) {
       options.sass.files
     ])
       .pipe(plugins.plumber({
-        errorHandler: function(e) {
+        errorHandler: function (e) {
           console.log(e.messageFormatted);
           this.emit('end');
         }
@@ -47,5 +53,62 @@ module.exports = function (gulp, plugins, options) {
       .pipe(plugins.sourcemaps.write())
       .pipe(plugins.plumber.stop())
       .pipe(gulp.dest(options.sass.destination));
+  });
+
+  gulp.task('compile:module-component-libraries-ltr', function () {
+    return gulp.src([
+      options.sass.directionalSource
+    ])
+      .pipe(plugins.plumber({
+        errorHandler: function (e) {
+          console.log(e.messageFormatted);
+          this.emit('end');
+        }
+      }))
+      .pipe(plugins.sourcemaps.init())
+      .pipe(plugins.sassGlob())
+      .pipe(plugins.sass({
+        errLogToConsole: true,
+        outputStyle: 'expanded'
+      }))
+      .pipe(plugins.autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      }))
+      .pipe(plugins.rename({
+        suffix: '.ltr'
+      }))
+      .pipe(plugins.sourcemaps.write())
+      .pipe(plugins.plumber.stop())
+      .pipe(gulp.dest(options.sass.directionalDestination));
+  });
+
+  gulp.task('compile:module-component-libraries-rtl', function () {
+    return gulp.src([
+      options.sass.directionalSource
+    ])
+      .pipe(plugins.plumber({
+        errorHandler: function (e) {
+          console.log(e.messageFormatted);
+          this.emit('end');
+        }
+      }))
+      .pipe(plugins.sourcemaps.init())
+      .pipe(plugins.sassGlob())
+      .pipe(plugins.sassVariables(options.sass.variables))
+      .pipe(plugins.sass({
+        errLogToConsole: true,
+        outputStyle: 'expanded'
+      }))
+      .pipe(plugins.autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      }))
+      .pipe(plugins.rename({
+        suffix: '.rtl'
+      }))
+      .pipe(plugins.sourcemaps.write())
+      .pipe(plugins.plumber.stop())
+      .pipe(gulp.dest(options.sass.directionalDestination));
   });
 };
