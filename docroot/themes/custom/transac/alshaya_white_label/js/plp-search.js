63aa46175f3f6ee988f289949e3e84c2ffbd7165
Swarad <swarad07@gmail.com>
2019-04-03 11:37:08 +0530

    CORE-6701: Fixing conflicts in my own code.

--- docroot/themes/custom/transac/alshaya_white_label/js/plp-search.js
+++ docroot/themes/custom/transac/alshaya_white_label/js/plp-search.js
@@ -344,10 +344,6 @@
         // Remove if clear all button is clicked on the filter pane.
         $('.shop-by-size-clear-container').remove();
       }
-
-      $('.c-facet__title.c-accordion__title').once().on('click', function () {
-        $(this).toggleClass('active');
-      });
     }
   };
 })(jQuery);
