07defc8ebd974c83bcbf855651526b624a8321ab
Rohit Joshi <joshi.rohit100@gmail.com>
2019-04-03 14:57:15 +0530

    CORE-6701: Updating category facet to only able to select one item and adding 'All' value.

---
+++ docroot/modules/custom/alshaya_search_api/src/Plugin/facets/processor/AlshayaFacetAllOption.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Drupal\alshaya_search_api\Plugin\facets\processor;
+
+use Drupal\facets\FacetInterface;
+use Drupal\facets\Processor\BuildProcessorInterface;
+use Drupal\facets\Processor\ProcessorPluginBase;
+use Drupal\facets\Result\Result;
+
+/**
+ * Show `All` option in category facet.
+ *
+ * @FacetsProcessor(
+ *   id = "alshaya_facet_all_option",
+ *   label = @Translation("Alshaya Facet - All- option"),
+ *   description = @Translation("Shows the -All- option in the facet."),
+ *   stages = {
+ *     "build" = 50
+ *   }
+ * )
+ */
+class AlshayaFacetAllOption extends ProcessorPluginBase implements BuildProcessorInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(FacetInterface $facet, array $results) {
+    // If there are results in the facet.
+    if (!empty($results)) {
+      // Get total count from all items in this facet.
+      $max_items = array_sum(array_map(function ($item) {
+        return $item->getCount();
+      }, $results));
+
+      // Prepare `All` result facet item.
+      $all_result_item = new Result(0, $this->t('All'), $max_items);
+
+      // Get active items for this facet.
+      $active_item = $facet->getActiveItems();
+      // If any active item in this facet.
+      if ($active_item) {
+        // Get any one result item from current facet, get its url and then use
+        // this url for `all` result with some changes.
+        $one_result = array_pop($results);
+        if ($one_result instanceof Result) {
+          // Get the query parameters from url.
+          $url = $one_result->getUrl();
+          $query = $url->getOption('query');
+
+          if ($query && !empty($options = $query['f'])) {
+            foreach ($options as $key => $option) {
+              // Unset all active items of this facet to prepare url for the
+              // `All` facet item.
+              if (strpos($option, $facet->getUrlAlias()) !== FALSE) {
+                unset($options[$key]);
+              }
+            }
+
+            $query['f'] = $options;
+            $url->setOption('query', $query);
+            $all_result_item->setUrl($url);
+          }
+        }
+      }
+
+      $results = [0 => $all_result_item] + $results;
+    }
+
+    // Return the results.
+    return $results;
+  }
+
+}
