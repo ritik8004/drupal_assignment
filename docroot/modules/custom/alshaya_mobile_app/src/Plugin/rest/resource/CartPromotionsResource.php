30eb22259ee3442c782f327c4d9bb317dde8db93
Akanksha Singh <akanksha@axelerant.com>
2019-04-05 09:32:40 +0530

    MAPP-712: Notifications are not displaying on Bag page.

--- docroot/modules/custom/alshaya_mobile_app/src/Plugin/rest/resource/CartPromotionsResource.php
+++ docroot/modules/custom/alshaya_mobile_app/src/Plugin/rest/resource/CartPromotionsResource.php
@@ -7,12 +7,8 @@ use Drupal\rest\ModifiedResourceResponse;
 use Drupal\rest\Plugin\ResourceBase;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Entity\EntityRepository;
-use Drupal\Core\Language\LanguageManager;
-use Drupal\node\Entity\Node;
 use Drupal\alshaya_acm_promotion\AlshayaPromotionsManager;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\node\NodeInterface;
 use Drupal\alshaya_acm\ApiHelper;
 
 /**
@@ -42,20 +38,6 @@ class CartPromotionsResource extends ResourceBase {
    * @var \Drupal\alshaya_acm_promotion\AlshayaPromotionsManager
    */
   protected $alshayaAcmPromotionManager;
-
-  /**
-   * The Language Manager service.
-   *
-   * @var \Drupal\Core\Language\LanguageManager
-   */
-  protected $languageManager;
-
-  /**
-   * The Entity repository service.
-   *
-   * @var \Drupal\Core\Entity\EntityRepository
-   */
-  protected $entityRepository;
 
   /**
    * The entity type manager.
@@ -88,10 +70,6 @@ class CartPromotionsResource extends ResourceBase {
    *   The mobile app utility service.
    * @param \Drupal\alshaya_acm_promotion\AlshayaPromotionsManager $alshaya_acm_promotion_manager
    *   The alshaya promotion manager service.
-   * @param \Drupal\Core\Entity\EntityRepository $entityRepository
-   *   The entity repository service.
-   * @param \Drupal\Core\Language\LanguageManager $languageManager
-   *   The language manager service.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
    * @param \Drupal\alshaya_acm\ApiHelper $api_helper
@@ -104,15 +82,11 @@ class CartPromotionsResource extends ResourceBase {
                               LoggerInterface $logger,
                               MobileAppUtility $mobile_app_utility,
                               AlshayaPromotionsManager $alshaya_acm_promotion_manager,
-                              EntityRepository $entityRepository,
-                              LanguageManager $languageManager,
                               EntityTypeManagerInterface $entity_type_manager,
                               ApiHelper $api_helper) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
     $this->mobileAppUtility = $mobile_app_utility;
     $this->alshayaAcmPromotionManager = $alshaya_acm_promotion_manager;
-    $this->languageManager = $languageManager;
-    $this->entityRepository = $entityRepository;
     $this->entityTypeManager = $entity_type_manager;
     $this->apiHelper = $api_helper;
   }
@@ -129,8 +103,6 @@ class CartPromotionsResource extends ResourceBase {
       $container->get('logger.factory')->get('alshaya_mobile_app'),
       $container->get('alshaya_mobile_app.utility'),
       $container->get('alshaya_acm_promotion.manager'),
-      $container->get('entity.repository'),
-      $container->get('language_manager'),
       $container->get('entity_type.manager')
     );
   }
@@ -151,12 +123,12 @@ class CartPromotionsResource extends ResourceBase {
    */
   public function get(string $cart_id) {
     $cart_id = (int) $cart_id;
-    $promotions = [];
 
     if (empty($cart_id)) {
       $this->mobileAppUtility->throwException();
     }
 
+    // Get all selected rules in block.
     $blocks = $this->entityTypeManager->getStorage('block')
       ->loadByProperties([
         'plugin' => 'alshaya_cart_promotions_block',
@@ -165,79 +137,11 @@ class CartPromotionsResource extends ResourceBase {
     $block = reset($blocks);
     $selected_promotions = array_filter($block->get('settings')['promotions']);
 
-    if (!empty($selected_promotions)) {
-      foreach ($selected_promotions as $promotion_rule_id) {
-        if ($promotion_rule_id) {
-          $node = $this->alshayaAcmPromotionManager->getPromotionByRuleId($promotion_rule_id);
-
-          if ($node instanceof NodeInterface && $node->isPublished()) {
-            // Get translation if available.
-            $node = $this->entityRepository->getTranslationFromContext($node);
-
-            $message = $node->get('field_acq_promotion_label')->getString();
-
-            if ($message) {
-              $promotions[$promotion_rule_id] = ['#markup' => $message];
-            }
-          }
-        }
-      }
-    }
-
-    $cart = $this->apiHelper->getCart($cart_id);
-
     // Get all the rules applied in cart.
+    $cart = $this->apiHelper->getCart($cart_id);
     $cartRulesApplied = $cart->get('cart_rules');
 
-    // Load all the promotions of the three specific types we check below.
-    // We load only published promotions.
-    $subTypePromotions = $this->alshayaAcmPromotionManager->getAllPromotions([
-      [
-        'field' => 'field_alshaya_promotion_subtype',
-        'value' => [
-          AlshayaPromotionsManager::SUBTYPE_FIXED_PERCENTAGE_DISCOUNT_ORDER,
-          AlshayaPromotionsManager::SUBTYPE_FIXED_AMOUNT_DISCOUNT_ORDER,
-          AlshayaPromotionsManager::SUBTYPE_FREE_SHIPPING_ORDER,
-        ],
-        'operator' => 'IN',
-      ],
-      [
-        'field' => 'status',
-        'value' => Node::PUBLISHED,
-      ],
-    ]);
-
-    foreach ($subTypePromotions as $subTypePromotion) {
-      $message = '';
-
-      $promotion_rule_id = $subTypePromotion->get('field_acq_promotion_rule_id')
-        ->getString();
-      $sub_type = $subTypePromotion->get('field_alshaya_promotion_subtype')
-        ->getString();
-
-      // Special condition for free shipping type promotion.
-      if ($sub_type == AlshayaPromotionsManager::SUBTYPE_FREE_SHIPPING_ORDER) {
-        // For free shipping, we only show if it is applied.
-        if (in_array($promotion_rule_id, $cartRulesApplied)) {
-          $message = $this->t('Your order qualifies for free delivery.');
-        }
-      }
-      // For the rest, we show only if they are not applied.
-      elseif (!in_array($promotion_rule_id, $cartRulesApplied)) {
-        // Get translation if available.
-        $subTypePromotion = $this->entityRepository->getTranslationFromContext($subTypePromotion);
-
-        // Get message from magento data stored in drupal.
-        $message = $subTypePromotion->get('field_acq_promotion_label')
-          ->getString();
-      }
-
-      if ($message) {
-        $promotions[$promotion_rule_id] = [$message];
-      }
-    }
-
-    $promotions = array_filter($promotions);
+    $promotions = $this->alshayaAcmPromotionManager->getAllCartPromotions($selected_promotions, $cartRulesApplied);
 
     return new ModifiedResourceResponse($promotions);
   }
