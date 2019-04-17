38158eef69a640dbb7296dbfd6a6160c3905fc5b
Akanksha Singh <akanksha@axelerant.com>
2019-04-17 11:28:54 +0530

    CORE-8570: Push customer’s Mobile number in the datalayers.

--- docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.js
+++ docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.js
@@ -1144,6 +1144,7 @@
   Drupal.setUserDetailsInStorage = function () {
     var userDetails = {};
     userDetails.userID = drupalSettings.user.uid;
+    userDetails.userPhone = '';
     userDetails.userEmailID = '';
     userDetails.userName = '';
     userDetails.userType = 'Guest User';
