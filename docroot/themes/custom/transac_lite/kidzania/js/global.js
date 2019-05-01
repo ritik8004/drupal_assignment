b4cad6b8bc276f25f77760985ac66d7701ccf65e
sreekanth.sreedharan <sreekanth.sreedharan@alshaya.com>
2019-05-01 13:55:54 +0300

    KOTBP-20: KZKW - Header UI.

--- docroot/themes/custom/transac_lite/kidzania/js/global.js
+++ docroot/themes/custom/transac_lite/kidzania/js/global.js
@@ -2,3 +2,13 @@
  * @file
  * Globaly required scripts.
  */
+
+(function ($) {
+  'use strict';
+
+  $('.menu-toggle').on('click', function (e) {
+    $('.menu-navigation').toggleClass('show-menu');
+    e.preventDefault();
+  });
+
+})(jQuery);
