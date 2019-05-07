44d8599a33d95dc1803417b967529134068a1600
Swarad <swarad07@gmail.com>
2019-05-07 15:52:02 +0530

    CORE-7701: Adding lazy load for all promo block and banner images in HP and DP.

--- docroot/themes/custom/transac/alshaya_white_label/js/alshaya_image_lazyload.js
+++ docroot/themes/custom/transac/alshaya_white_label/js/alshaya_image_lazyload.js
@@ -3,28 +3,15 @@
  * Provides blazy lazyloading.
  */
 
+/* global Blazy */
+
 (function ($, Drupal) {
   'use strict';
-
-  /* global Blazy */
 
   Drupal.behaviors.blazy = {
     attach: function (context, settings) {
       // Initialize.
-      var bLazy = new Blazy({
-        success: function () {
-          updateCounter();
-        }
-      });
-
-      // Not needed, only here to illustrate amount of loaded images.
-      var imageLoaded = 0;
-      console.log(imageLoaded);
-
-      function updateCounter() {
-        imageLoaded++;
-        console.log(imageLoaded);
-      }
+      var bLazy = new Blazy();
     }
   };
 })(jQuery, Drupal, drupalSettings);
