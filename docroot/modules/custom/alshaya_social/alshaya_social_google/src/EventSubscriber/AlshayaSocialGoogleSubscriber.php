43a1a91bf10f8c13381a3524b1dd8845cddc54f7
Mitesh Patel <developermitesh@gmail.com>
2019-05-13 23:37:20 +0530

    CORE-8960: Create alshaya_social_google module, configure settings and enable it.

---
+++ docroot/modules/custom/alshaya_social/alshaya_social_google/src/EventSubscriber/AlshayaSocialGoogleSubscriber.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Drupal\alshaya_social_google\EventSubscriber;
+
+use Drupal\social_auth\AuthManager\OAuth2ManagerInterface;
+use Drupal\social_api\Plugin\NetworkManager;
+use Drupal\social_auth\Event\SocialAuthEvents;
+use Drupal\social_auth\Event\SocialAuthUserFieldsEvent;
+use Drupal\social_auth\SocialAuthDataHandler;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\alshaya_acm_customer\CustomerHelper;
+use Drupal\Core\Logger\LoggerChannelFactory;
+
+/**
+ * Event subscriber to fill first and last name for user using google.
+ */
+class AlshayaSocialGoogleSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The data handler.
+   *
+   * @var \Drupal\social_auth\SocialAuthDataHandler
+   */
+  private $dataHandler;
+
+  /**
+   * The network plugin manager.
+   *
+   * @var \Drupal\social_api\Plugin\NetworkManager
+   */
+  private $networkManager;
+
+  /**
+   * The Facebook authentication manager.
+   *
+   * @var \Drupal\social_auth\AuthManager\OAuth2ManagerInterface
+   */
+  protected $providerAuth;
+
+  /**
+   * The customer helper.
+   *
+   * @var \Drupal\alshaya_acm_customer\CustomerHelper
+   */
+  protected $customerHelper;
+
+  /**
+   * The logger.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactory
+   */
+  protected $logger;
+
+  /**
+   * AlshayaSocialGoogleSubscriber constructor.
+   *
+   * @param \Drupal\social_auth\SocialAuthDataHandler $data_handler
+   *   Used to manage session variables.
+   * @param \Drupal\social_api\Plugin\NetworkManager $network_manager
+   *   An instance of social_auth_facebook network plugin.
+   * @param \Drupal\social_auth\AuthManager\OAuth2ManagerInterface $provider_auth
+   *   The provider auth manager.
+   * @param \Drupal\alshaya_acm_customer\CustomerHelper $customer_helper
+   *   The customer helper.
+   * @param \Drupal\Core\Logger\LoggerChannelFactory $logger_factory
+   *   LoggerChannelFactory object.
+   */
+  public function __construct(
+    SocialAuthDataHandler $data_handler,
+    NetworkManager $network_manager,
+    OAuth2ManagerInterface $provider_auth,
+    CustomerHelper $customer_helper,
+    LoggerChannelFactory $logger_factory
+  ) {
+    $this->dataHandler = $data_handler;
+    $this->networkManager = $network_manager;
+    $this->providerAuth = $provider_auth;
+    $this->customerHelper = $customer_helper;
+    $this->logger = $logger_factory->get('alshaya_social_google');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[SocialAuthEvents::USER_FIELDS][] = ['onUserFields', 100];
+    return $events;
+  }
+
+  /**
+   * Add first name and last name info from google profile.
+   *
+   * @param \Drupal\social_auth\Event\SocialAuthUserFieldsEvent $event
+   *   The social auth user fields event object.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginException
+   */
+  public function onUserFields(SocialAuthUserFieldsEvent $event) {
+    if ($event->getPluginId() !== 'social_auth_google' || empty($this->dataHandler->get('access_token'))) {
+      return;
+    }
+
+    $google = $this->networkManager->createInstance($event->getPluginId())->getSdk();
+    $this->providerAuth->setClient($google)->setAccessToken($this->dataHandler->get('access_token'));
+
+    // Gets user's google profile.
+    if ($google_profile = $this->providerAuth->getUserInfo()) {
+      $fields = $event->getUserFields();
+      $fields['field_first_name'] = $google_profile->getFirstName();
+      $fields['field_last_name'] = $google_profile->getLastName();
+
+      try {
+        $customer = $this->customerHelper->updateCustomer(
+          NULL,
+          $fields['mail'],
+          $fields['field_first_name'],
+          $fields['field_last_name'],
+          $fields['pass']
+        );
+      }
+      catch (\Exception $e) {
+        // Do nothing except for downtime exception, we will do other
+        // validations after try/catch.
+        if (acq_commerce_is_exception_api_down_exception($e)) {
+          $this->logger->error('Error occurred during customer registration @message', [
+            '@message' => $e->getMessage(),
+          ]);
+        }
+        else {
+          $this->logger->error('Error occurred during customer registration @message', [
+            '@message' => $e->getMessage(),
+          ]);
+        }
+        return;
+      }
+      $fields['acq_customer_id'] = $customer['customer_id'];
+      $event->setUserFields($fields);
+    }
+  }
+
+}
