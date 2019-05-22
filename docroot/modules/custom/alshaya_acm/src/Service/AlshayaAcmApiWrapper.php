38485f2420581f924311377c9a7d9cf67c880995
nikunj <coder@nikunj.dev>
2019-05-22 15:43:59 +0530

    CORE-8823: Add lower timeout to get linked skus api call.

---
+++ docroot/modules/custom/alshaya_acm/src/Service/AlshayaAcmApiWrapper.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\alshaya_acm\Service;
+
+use Drupal\acq_commerce\Conductor\APIWrapper;
+use Drupal\acq_commerce\Conductor\RouteException;
+use Drupal\acq_commerce\Connector\ConnectorException;
+use Drupal\Core\Site\Settings;
+
+/**
+ * Class AlshayaAcmApiWrapper.
+ *
+ * @package Drupal\alshaya_acm\Service
+ */
+class AlshayaAcmApiWrapper extends APIWrapper {
+
+  /**
+   * Get linked skus for a given sku by linked type.
+   *
+   * @param string $sku
+   *   The sku id.
+   * @param string $type
+   *   Linked type. Like - related/crosssell/upsell.
+   *
+   * @return array|mixed
+   *   All linked skus of given type.
+   *
+   * @throws \Drupal\acq_commerce\Conductor\RouteException
+   */
+  public function getLinkedskus($sku, $type = LINKED_SKU_TYPE_ALL) {
+    $sku = urlencode($sku);
+    $endpoint = $this->apiVersion . "/agent/product/$sku/related/$type";
+
+    $doReq = function ($client, $opt) use ($endpoint) {
+      // Allow overriding the timeout value from settings.
+      $opt['timeout'] = (int) Settings::get('linked_skus_timeout', 3);
+      return ($client->get($endpoint, $opt));
+    };
+
+    try {
+      $result = $this->tryAgentRequest($doReq, 'linkedSkus', 'related');
+    }
+    catch (ConnectorException $e) {
+      throw new RouteException(__FUNCTION__, $e->getMessage(), $e->getCode(), $this->getRouteEvents());
+    }
+
+    return $result;
+  }
+
+}
