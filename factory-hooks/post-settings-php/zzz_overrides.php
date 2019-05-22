5df9003804156c180359f6eed1f33f26b41a74ec
nikunj <coder@nikunj.dev>
2019-05-22 20:01:15 +0530

    CORE-9358: Fix broken overrides after last change. Update module and patch for block. Fix issue in config reset to merge settings into config instead of replacing.

---
+++ factory-hooks/post-settings-php/zzz_overrides.php
@@ -0,0 +1,28 @@
+<?php
+// @codingStandardsIgnoreFile
+
+/**
+ * @file
+ * Implementation of ACSF post-settings-php hook to allow overriding settings.
+ *
+ * @see https://docs.acquia.com/site-factory/tiers/paas/workflow/hooks
+ */
+
+global $acsf_site_name;
+$acsf_site_code = substr($acsf_site_name, 0, -2);
+$country_code = substr($acsf_site_name, -2);
+
+// Allow overriding settings and config to set secret info directly from
+// include files on server which can be per brand or brand country combination.
+$settings_path = $_SERVER['HOME'] . DIRECTORY_SEPARATOR . 'settings' . DIRECTORY_SEPARATOR . 'settings-';
+
+$brand_country_file = $settings_path . $acsf_site_code . $country_code . '.php';
+if (file_exists($brand_country_file)) {
+  include_once $brand_country_file;
+}
+
+$brand_file = $settings_path . $acsf_site_code . '.php';
+
+if (file_exists($brand_file)) {
+  include_once $brand_file;
+}
