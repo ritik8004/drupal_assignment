df3b6684a8fcb0c2bcc0590a50872aaf07a77119
nikunj <coder@nikunj.dev>
2019-06-06 19:14:00 +0530

    CORE-9163: Remove dependency and alter class of late runtime.

--- docroot/modules/custom/alshaya_performance/src/AlshayaPerformanceServiceProvider.php
+++ docroot/modules/custom/alshaya_performance/src/AlshayaPerformanceServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\alshaya_performance;
 
+use Drupal\alshaya_performance\EventSubscriber\AlshayaLateRuntimeProcessor;
 use Drupal\alshaya_performance\Logger\AlshayaPerformanceSysLog;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceModifierInterface;
@@ -21,6 +22,12 @@ class AlshayaPerformanceServiceProvider extends ServiceProviderBase implements S
       $syslog = $container->getDefinition('logger.syslog');
       if ($syslog) {
         $syslog->setClass(AlshayaPerformanceSysLog::class);
+      }
+
+      // Override the syslog logger class.
+      $purge_late_runtime = $container->getDefinition('purge_processor_lateruntime.processor');
+      if ($purge_late_runtime) {
+        $purge_late_runtime->setClass(AlshayaLateRuntimeProcessor::class);
       }
     }
     catch (\Exception $e) {
