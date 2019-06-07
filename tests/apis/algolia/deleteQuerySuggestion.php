9d2a2a2b09d4ec4e024b6f188b92bb8c9a7a9b1e
nikunj <coder@nikunj.dev>
2019-06-07 18:56:01 +0530

    CORE-8367: Final update as per feedbacks.

--- tests/apis/algolia/deleteQuerySuggestion.php
+++ tests/apis/algolia/deleteQuerySuggestion.php
@@ -9,8 +9,7 @@
 /**
  * How to use this:
  *
- * Usage: php createIndex.php [prefix]
- * Example: php createIndex.php mckw_01dev
+ * Usage: php createIndex.php [brand] [env] [app_id] [app_secret_admin]
  *
  * Ensure settings.php is updated with proper application id and admin secret
  * key. Once that is done, please go through all the arrays here:
