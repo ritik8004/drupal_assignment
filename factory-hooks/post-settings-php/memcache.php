c88c2b421b72a6da9011ee4ef23773cfaf8b8bd4
nikunj <coder@nikunj.dev>
2019-06-25 18:17:40 +0530

    CORE-10228: Use memcache for cart history cache.

--- factory-hooks/post-settings-php/memcache.php
+++ factory-hooks/post-settings-php/memcache.php
@@ -83,6 +83,7 @@ if ($memcache_module_is_present && ($memcache_exists || $memcached_exists)) {
     $settings['cache']['bins']['hm_product_configurations'] = 'cache.backend.permanent_memcache';
     $settings['cache']['bins']['orders_count'] = 'cache.backend.permanent_memcache';
     $settings['cache']['bins']['product_processed_data'] = 'cache.backend.permanent_memcache';
+    $settings['cache']['bins']['cart_history'] = 'cache.backend.permanent_memcache';
 
     // Enable stampede protection.
     $settings['memcache']['stampede_protection'] = TRUE;
