d31ed5d14dafdb995aab7bb5f62599e59ed13118
Mitesh Patel <developermitesh@gmail.com>
2019-07-04 10:14:18 +0530

    CORE-10435: create iso_currency_code key for currency and delete gtm_currency_code.

--- docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/src/AlshayaGtmManager.php
+++ docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/src/AlshayaGtmManager.php
@@ -1323,13 +1323,10 @@ class AlshayaGtmManager {
    *   GTM currency code.
    */
   public function getGtmCurrency() {
-    if (!empty($gtm_currency_code = $this->configFactory->get('acq_commerce.currency')
-      ->get('gtm_currency_code'))) {
-      return $gtm_currency_code;
-    }
-
-    return $this->configFactory->get('acq_commerce.currency')
-      ->get('currency_code');
+    $currency_code = $this->configFactory->get('acq_commerce.currency');
+    return (!$this->configFactory->get('alshaya_seo_transac.meta_variables')->get('use_iso_code'))
+      ? $currency_code->get('currency_code')
+      : $currency_code->get('iso_currency_code');
   }
 
 }
