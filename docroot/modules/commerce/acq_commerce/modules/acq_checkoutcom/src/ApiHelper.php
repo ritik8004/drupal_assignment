818f7ec2db9943da884272c1a47868fba6ead63a
Mitesh Patel <developermitesh@gmail.com>
2019-07-08 07:31:54 +0530

    CORE-9293: add todo to remove config.

--- docroot/modules/commerce/acq_commerce/modules/acq_checkoutcom/src/ApiHelper.php
+++ docroot/modules/commerce/acq_commerce/modules/acq_checkoutcom/src/ApiHelper.php
@@ -78,6 +78,7 @@ class ApiHelper {
     $keys = [
       'public_key' => 'pk_test_ed88f0cd-e9b1-41b7-887e-de794963921f',
       'secret_key' => 'sk_test_863d1545-5253-4387-b86b-df6a86797baa',
+      // @todo: Remove config once api is in place.
       'verify_3dsecure' => $this->configFactory->get('acq_checkoutcom.settings')->get('verify_3dsecure'),
     ];
 
