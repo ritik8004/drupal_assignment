1e1aecda2bb6864a2106457ad2950be2396ea0df
nikunj <coder@nikunj.dev>
2019-07-12 13:53:13 +0530

    CORE-10625: Fix validations issue.

--- docroot/modules/commerce/acq_commerce/modules/acq_checkoutcom/js/acq_checkoutcom.form.js
+++ docroot/modules/commerce/acq_commerce/modules/acq_checkoutcom/js/acq_checkoutcom.form.js
@@ -10,6 +10,12 @@
     attach: function attach(context) {
       $('#edit-actions-next', context).once('checkout-click').on('click', function (e) {
         e.preventDefault();
+
+        var form = $(this).closest('form');
+        if (!$(form).valid()) {
+          return;
+        }
+
         var paymentForm = $(this).parents('form');
 
         CheckoutKit.createCardToken({
@@ -19,6 +25,8 @@
           "expiryYear": cardYear.value,
           "cvv": cardCvv.value
         },function(data){
+          $('#payment_details_checkout_com').hide();
+
           cardToken.value = data.id;
           cardBin.value = data.card.bin;
           cardName.value = '';
