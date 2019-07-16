ede7b049ff6ed3f31e5e07126076f795e0151506
nikunj <coder@nikunj.dev>
2019-07-16 23:15:40 +0530

    CORE-000: Fix bug for query suggesion in update downstream. Update more configs in update index.

--- tests/apis/algolia/updateDownStreamIndexes.php
+++ tests/apis/algolia/updateDownStreamIndexes.php
@@ -43,7 +43,7 @@ foreach ($languages as $language) {
   $settingsSource[$language] = $indexSource[$language]->getSettings();
   $rules[$language] = algolia_get_rules($indexSource[$language]);
   $sourceQueries = algolia_get_query_suggestions($app_id, $app_secret_admin, $source_name);
-  $sourceQuery = reset($sourceQueries);
+  $sourceQuery[$language] = reset($sourceQueries);
   $sourceSynonyms[$language] = algolia_get_synonyms($indexSource[$language]);
 }
 
@@ -56,8 +56,8 @@ foreach ($sandbox_envs as $sandbox_env) {
 
     $queries = algolia_get_query_suggestions($sandbox_app_id, $sandbox_app_secret_admin, $name);
     $query = reset($queries);
-    $query['sourceIndices'][0]['facets'] = $sourceQuery['sourceIndices'][0]['facets'];
-    $query['sourceIndices'][0]['generate'] = $sourceQuery['sourceIndices'][0]['generate'];
+    $query['sourceIndices'][0]['facets'] = $sourceQuery[$language]['sourceIndices'][0]['facets'];
+    $query['sourceIndices'][0]['generate'] = $sourceQuery[$language]['sourceIndices'][0]['generate'];
     algolia_add_query_suggestion($sandbox_app_id, $sandbox_app_secret_admin, $query['indexName'], json_encode($query));
     // Clear before creating.
     $index->clearSynonyms(TRUE);
@@ -74,8 +74,8 @@ foreach ($prod_envs as $prod_env) {
 
     $queries = algolia_get_query_suggestions($app_id, $app_secret_admin, $name);
     $query = reset($queries);
-    $query['sourceIndices'][0]['facets'] = $sourceQuery['sourceIndices'][0]['facets'];
-    $query['sourceIndices'][0]['generate'] = $sourceQuery['sourceIndices'][0]['generate'];
+    $query['sourceIndices'][0]['facets'] = $sourceQuery[$language]['sourceIndices'][0]['facets'];
+    $query['sourceIndices'][0]['generate'] = $sourceQuery[$language]['sourceIndices'][0]['generate'];
     algolia_add_query_suggestion($app_id, $app_secret_admin, $query['indexName'], json_encode($query));
     // Clear before creating.
     $index->clearSynonyms(TRUE);
