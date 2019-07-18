d29dc4ad0610568ebb92277631c886ca16b940db
Rohit Joshi <joshi.rohit100@gmail.com>
2019-07-18 18:02:08 +0530

    CORE-5268: Log the message only when message is override.

--- docroot/modules/commerce/acq_commerce/src/Connector/ConnectorException.php
+++ docroot/modules/commerce/acq_commerce/src/Connector/ConnectorException.php
@@ -15,16 +15,22 @@ class ConnectorException extends \UnexpectedValueException {
    * {@inheritdoc}
    */
   public function __construct($message = '', $code = 0, \Throwable $e = NULL) {
-    // Log the error message.
-    \Drupal::logger('acq_commerce')->error($message);
+    $message_overridden = FALSE;
     // If error contains any HTML, or contains 'magento' string, use global
     // error message.
     if ($message != strip_tags($message) || $position = stripos($message, 'magento')) {
       $message = acq_commerce_api_down_global_error_message();
+      $message_overridden = TRUE;
     }
     elseif ($position = stripos($message, 'Backend server error:')) {
       $prefix = 'Backend server error:';
       $message = trim(substr($message, $position + strlen($prefix)));
+      $message_overridden = TRUE;
+    }
+
+    // Log the message if changed.
+    if ($message_overridden) {
+      \Drupal::logger('acq_commerce')->error($message);
     }
 
     return parent::__construct($message, $code, $e);
