96b7b8d644fc5500b17d69ad423bd5bbbf45adde
Rohit Joshi <joshi.rohit100@gmail.com>
2019-07-18 17:28:32 +0530

    CORE-5268: Make ACM/MDC error/exception messages more user friendly.

--- docroot/modules/commerce/acq_commerce/src/Connector/ConnectorException.php
+++ docroot/modules/commerce/acq_commerce/src/Connector/ConnectorException.php
@@ -10,4 +10,22 @@ namespace Drupal\acq_commerce\Connector;
  * @ingroup acq_commerce
  */
 class ConnectorException extends \UnexpectedValueException {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct($message = '', $code = 0, \Throwable $e = NULL) {
+    // If error contains any HTML, or contains 'magento' string, use global
+    // error message.
+    if ($message != strip_tags($message) || $position = stripos($message, 'magento')) {
+      $message = acq_commerce_api_down_global_error_message();
+    }
+    elseif ($position = stripos($message, 'Backend server error:')) {
+      $prefix = 'Backend server error:';
+      $message = trim(substr($message, $position + strlen($prefix)));
+    }
+
+    return parent::__construct($message, $code, $e);
+  }
+
 }
