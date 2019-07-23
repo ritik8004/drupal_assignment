3c3a8a2a4461cdfc0083c95167cb00bcf721b2e2
Piyuesh Kumar <piyuesh23@qed42.com>
2019-07-23 22:07:14 +0100

    CORE-10370: Adding alshaya_rum module.

--- docroot/modules/custom/alshaya_rum/src/Form/AlshayaRumConfigForm.php
+++ docroot/modules/custom/alshaya_rum/src/Form/AlshayaRumConfigForm.php
@@ -5,14 +5,20 @@ namespace Drupal\alshaya_rum\Form;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
+/**
+ * Configure Alshaya Real User Monitoring settings for this site.
+ */
 class AlshayaRumConfigForm extends ConfigFormBase {
 
   /**
-   * Gets the configuration names that will be editable.
-   *
-   * @return array
-   *   An array of configuration object names that are editable if called in
-   *   conjunction with the trait's config() method.
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'alshaya_rum_config_form';
+  }
+
+  /**
+   * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
     return [
@@ -21,44 +27,34 @@ class AlshayaRumConfigForm extends ConfigFormBase {
   }
 
   /**
-   * Returns a unique string identifying the form.
-   *
-   * The returned ID should be a unique string that can be a valid PHP function
-   * name, since it's used in hook implementation names such as
-   * hook_form_FORM_ID_alter().
-   *
-   * @return string
-   *   The unique string identifying the form.
-   */
-  public function getFormId() {
-    return 'alshaya_rum_config_form';
-  }
-
-  /**
-   * @inheritDoc
+   * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $alshaya_rum_config = $this->config('alshaya_rum.settings');
+
     $form['rum_user_id'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('RUM user id'),
-      '#default_value' => $this->config('alshaya_rum.settings')->get('rum_user_id'),
+      '#title' => $this->t('Alshaya RUM user id'),
+      '#default_value' => $alshaya_rum_config->get('rum_user_id'),
     ];
 
     $form['whitelisted_rum_paths'] = [
       '#type' => 'textarea',
-      '#title' => $this->t('List of paths on which we need to load rum script.'),
-      '#default_value' => $this->config('alshaya_rum.settings')->get('whitelisted_rum_paths'),
+      '#title' => $this->t('List of pages on which RUM script should load:'),
+      '#default_value' => $alshaya_rum_config->get('whitelisted_rum_paths'),
     ];
 
     return parent::buildForm($form, $form_state);
   }
 
   /**
-   * @inheritDoc
+   * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->config('alshaya_rum.settings')->set('rum_user_id', $form_state->get('rum_user_id'));
-    $this->config('alshaya_rum.settings')->set('whitelisted_rum_paths', $form_state->get('whitelisted_rum_paths'));
+    $this->config('alshaya_rum.settings')
+      ->set('rum_user_id', $form_state->getValue('rum_user_id'))
+      ->set('whitelisted_rum_paths', $form_state->getValue('whitelisted_rum_paths'))
+      ->save();
     parent::submitForm($form, $form_state);
   }
 
