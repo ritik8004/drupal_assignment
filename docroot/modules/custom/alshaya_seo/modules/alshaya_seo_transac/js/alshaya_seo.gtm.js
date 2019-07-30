d1947e03c2428efb709afd78c83cba390753537a
Ashutosh Gupta <ashutosh.gupta@acquia.com>
2019-07-30 12:41:06 +0530

    CORE-11173: Adding promotionImpression for homepage promo banner.

--- docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.js
+++ docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.js
@@ -719,6 +719,11 @@
           Drupal.alshaya_seo_gtm_push_promotion_impressions($('.field--name-body'), gtmPageType, 'promotionImpression');
         }
 
+      // Tracking view of main promotion banner on BBW homepage.
+      $('.paragraph--type--banner').each(function () {
+        Drupal.alshaya_seo_gtm_push_promotion_impressions($(this), gtmPageType, 'promotionImpression');
+      });
+
       // Tracking of homepage banner.
       $('.c-content__slider .field--name-field-banner').each(function () {
         $(this).once('js-event').on('click', function () {
