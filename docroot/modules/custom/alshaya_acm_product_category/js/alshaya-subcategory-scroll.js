5514ee2281dc4ba6b0057674ed2927eedb72c028
Prashant Kumar <prashant114606@gmail.com>
2019-07-31 18:03:14 +0530

    CORE-9042: Initial commit related to panty guide feedback.

--- docroot/modules/custom/alshaya_acm_product_category/js/alshaya-subcategory-scroll.js
+++ docroot/modules/custom/alshaya_acm_product_category/js/alshaya-subcategory-scroll.js
@@ -8,19 +8,6 @@
 
   Drupal.behaviors.alshayaAcmProductCategorySubCategoryScroll = {
     attach: function () {
-      $('.sub-category').once().on('click', function() {
-        var tid = $(this).attr('data-tid');
-        scrollToCategoryHeader(tid);
-      });
-
-      // Hide the divs that do not have results.
-      $('div.plp-subcategory-block').once().find('div.sub-category').each(function(){
-        var tid = $(this).attr('data-tid');
-        if($('term#' + tid).length === 0) {
-          $(this).hide();
-        }
-      });
-
       /**
        * Animate & Scroll to subcategory header.
        *
@@ -28,10 +15,63 @@
        */
       function scrollToCategoryHeader(tid) {
         var element = '#' + tid;
+        var stickyFilterPosition;
+
+        if ($(window).width() < 768) {
+          stickyFilterPosition = $('#block-supercategorymenu').outerHeight() + $('#block-mobilenavigation').outerHeight() + $('.show-all-filters').outerHeight();
+          if ($('#block-subcategoryblock').hasClass('mobile-sticky-sub-category')) {
+            console.log(stickyFilterPosition);
+            if ($('.plp-subcategory-block').offset().top > $(element).offset().top) {
+              stickyFilterPosition = stickyFilterPosition + $('.mobile-sticky-sub-category').outerHeight();
+            }
+            else {
+              stickyFilterPosition = stickyFilterPosition - $('.mobile-sticky-sub-category').outerHeight();
+            }
+          }
+        }
+        else if ($(window).width() > 767 && $(window).width() < 1024) {
+          stickyFilterPosition = 60;
+          if ($('#block-subcategoryblock').hasClass('mobile-sticky-sub-category')) {
+            console.log(stickyFilterPosition);
+            if ($('.plp-subcategory-block').offset().top > $(element).offset().top) {
+              stickyFilterPosition = stickyFilterPosition + $('.mobile-sticky-sub-category').outerHeight();
+            }
+            else {
+              stickyFilterPosition = stickyFilterPosition - $('.mobile-sticky-sub-category').outerHeight();
+            }
+          }
+        }
+        else {
+          if ($('.sticky-filter-wrapper').hasClass('show-sub-category') && $('.plp-subcategory-block').offset().top > $(element).offset().top) {
+            stickyFilterPosition = $('.show-sub-category').outerHeight();
+          }
+          else {
+            stickyFilterPosition = $('#block-subcategoryblock + .block-views-exposed-filter-blockalshaya-product-list-block-1').outerHeight();
+          }
+        }
+
         $('html, body').animate({
-          scrollTop: ($(element).offset().top - $('.branding__menu').outerHeight() - 10)
+          scrollTop: ($(element).offset().top - stickyFilterPosition)
         }, 500);
       }
+      $('.sub-category').once().on('click', function() {
+        var tid = $(this).attr('data-tid');
+        setTimeout(function() {
+          scrollToCategoryHeader(tid);
+        }, 300);
+      });
+
+      if ($('#block-subcategoryblock').length > 0) {
+        $('body').addClass('subcategory-listing-enabled');
+      }
+
+      // Hide the divs that do not have results.
+      $('div.plp-subcategory-block').find('div.sub-category').each(function(){
+        var tid = $(this).attr('data-tid');
+        if($('term#' + tid).length === 0) {
+          $(this).hide();
+        }
+      });
     }
   };
 })(jQuery, Drupal);
