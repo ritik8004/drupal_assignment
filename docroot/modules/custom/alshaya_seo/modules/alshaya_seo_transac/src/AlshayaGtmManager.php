2326777afb6c08aa3e6a6b8df9620b8ea83bab2b
Ashutosh Gupta <ashutosh.gupta@acquia.com>
2019-07-31 12:24:43 +0530

    CORE-9520: Updating shipping value with amount_with_tax value.

--- docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/src/AlshayaGtmManager.php
+++ docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/src/AlshayaGtmManager.php
@@ -937,7 +937,7 @@ class AlshayaGtmManager {
       'affiliation' => 'Online Store',
       'revenue' => alshaya_master_convert_amount_to_float($order['totals']['grand']),
       'tax' => alshaya_master_convert_amount_to_float($order['totals']['tax']) ?: 0.00,
-      'shipping' => alshaya_master_convert_amount_to_float($order['shipping']['method']['amount']) ?: 0.00,
+      'shipping' => alshaya_master_convert_amount_to_float($order['shipping']['method']['amount_with_tax']) ?: 0.00,
       'coupon' => $order['coupon'],
     ];
 
