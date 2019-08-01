bdc3cb5095b796ea75a4c9b8d832543ea9d77462
Ashutosh Gupta <ashutosh.gupta@acquia.com>
2019-08-01 18:28:04 +0530

    CORE-11114: Add promotion class filter in js.

--- docroot/modules/custom/alshaya_acm_product/js/show_color_images_on_filter.js
+++ docroot/modules/custom/alshaya_acm_product/js/show_color_images_on_filter.js
@@ -64,7 +64,7 @@
 
         // Update all href to have selected param.
         selectedUrl += activeSpan.attr('data--id');
-        $(this).parents('article').find('a[href]:not(.swatch-url)').attr('href', selectedUrl);
+        $(this).parents('article').find('a[href]:not(.swatch-url, .sku-promotion-link)').attr('href', selectedUrl);
       }
     });
 
