1905dba17f91b28c0d105fc30bc7c2adc870c82b
Ashutosh Gupta <ashutosh.gupta@acquia.com>
2019-08-02 16:08:49 +0530

    CORE-11114: Updating link only for specific added class.

--- docroot/modules/custom/alshaya_acm_product/js/show_color_images_on_filter.js
+++ docroot/modules/custom/alshaya_acm_product/js/show_color_images_on_filter.js
@@ -64,7 +64,7 @@
 
         // Update all href to have selected param.
         selectedUrl += activeSpan.attr('data--id');
-        $(this).parents('article').find('a[href]:not(.not-update-href)').attr('href', selectedUrl);
+        $(this).parents('article').find('a.list-product-gallery').attr('href', selectedUrl);
       }
     });
 
