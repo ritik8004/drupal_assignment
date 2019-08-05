8002a1f59cc3b0939716c45a825e54e84d80dfb6
Ponraj-Paulraj <ponraj.paulraj@alshaya.com>
2019-08-05 22:51:27 +0300

    fix for 'Line indented incorrectly' build error.

--- docroot/modules/custom/alshaya_click_collect/alshaya_click_collect.api.php
+++ docroot/modules/custom/alshaya_click_collect/alshaya_click_collect.api.php
@@ -5,12 +5,12 @@
  * Hooks specific to the alshaya_click_collect module.
  */
 
- /**
-  * Alter click and collect config.
-  *
-  * @param array $config_data
-  *   Array of config data.
-  */
+/**
+ * Alter click and collect config.
+ *
+ * @param array $config_data
+ *   Array of config data.
+ */
 function hook_alshaya_click_and_collect_config_alter(array &$config_data) {
 
 }
