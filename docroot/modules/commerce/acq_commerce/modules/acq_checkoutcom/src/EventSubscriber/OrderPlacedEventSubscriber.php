3dcf5653a5c15a2dbd1f543b55a52c1973ded46b
Mitesh Patel <developermitesh@gmail.com>
2019-08-08 21:01:42 +0530

    CORE-11274: Remove session data on order placed.

---
+++ docroot/modules/commerce/acq_commerce/modules/acq_checkoutcom/src/EventSubscriber/OrderPlacedEventSubscriber.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\acq_checkoutcom\EventSubscriber;
+
+use Drupal\acq_commerce\Event\OrderPlacedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+/**
+ * Class OrderPlacedEventSubscriber.
+ *
+ * @package Drupal\acq_checkoutcom\EventSubscriber
+ */
+class OrderPlacedEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Checkout Helper.
+   *
+   * @var \Drupal\alshaya_acm_checkout\CheckoutHelper
+   */
+  protected $session;
+
+  /**
+   * OrderPlacedEventSubscriber constructor.
+   *
+   * @param \Symfony\Component\HttpFoundation\Session\SessionInterface $session
+   *   Used for reading data from and writing data to session.
+   */
+  public function __construct(SessionInterface $session) {
+    $this->session = $session;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events = [];
+    $events[OrderPlacedEvent::EVENT_NAME][] = ['onOrderPlaced', 100];
+    return $events;
+  }
+
+  /**
+   * Process post order placed.
+   *
+   * @param \Drupal\acq_commerce\Event\OrderPlacedEvent $event
+   *   Event object.
+   *
+   * @throws \Exception
+   */
+  public function onOrderPlaced(OrderPlacedEvent $event) {
+    $this->session->remove('checkout_com_payment_card');
+  }
+
+}
