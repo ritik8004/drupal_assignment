26037b49127793e330a21b9c7c268162f46b3fa6
Akanksha Singh <akanksha@axelerant.com>
2019-08-27 18:08:50 +0530

    CORE-11926: Add processor for clear filters.

---
+++ docroot/modules/custom/alshaya_facets_pretty_paths/src/Plugin/facets_summary/processor/AlshayaResetFacetsProcessor.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\alshaya_facets_pretty_paths\Plugin\facets_summary\processor;
+
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Drupal\facets_summary\FacetsSummaryInterface;
+use Drupal\facets_summary\Plugin\facets_summary\processor\ResetFacetsProcessor;
+
+/**
+ * Provides a processor that allows to reset facet filters.
+ *
+ * @SummaryProcessor(
+ *   id = "alshaya_reset_facets",
+ *   label = @Translation("Adds reset facets link."),
+ *   description = @Translation("When checked, this facet will add a link to reset enabled facets."),
+ *   stages = {
+ *     "build" = 50
+ *   }
+ * )
+ */
+class AlshayaResetFacetsProcessor extends ResetFacetsProcessor {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(FacetsSummaryInterface $facets_summary, array $build, array $facets) {
+    $conf = $facets_summary->getProcessorConfigs()[$this->getPluginId()];
+
+    // Do nothing if there are no selected facets or reset text is empty.
+    if (empty($build['#items']) || empty($conf['settings']['link_text'])) {
+      return $build;
+    }
+
+    $request = \Drupal::requestStack()->getMasterRequest();
+    $query_params = $request->query->all();
+    unset($query_params['f']);
+
+    // Remove all pretty facets.
+    $uri = $request->getRequestUri();
+    if (strpos($uri, '/--') !== FALSE) {
+      $uri = substr($uri, 0, strrpos($uri, '/--'));
+    }
+    $url = Url::fromUserInput($uri);
+    $url->setOptions(['query' => $query_params]);
+
+    // @codingStandardsIgnoreLine
+    $item = (new Link($this->t($conf['settings']['link_text']), $url))->toRenderable();
+    array_unshift($build['#items'], $item);
+    return $build;
+  }
+
+}
