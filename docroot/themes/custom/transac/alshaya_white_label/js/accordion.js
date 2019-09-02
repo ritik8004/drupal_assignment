f84d597aeb3f8ae09e41028900a8671a632fcbc6
sreekanth.sreedharan <sreekanth.sreedharan@alshaya.com>
2019-09-02 12:14:41 +0300

    CORE-11815: Move the cart promotion accordion script to seperate JS.

--- docroot/themes/custom/transac/alshaya_white_label/js/accordion.js
+++ docroot/themes/custom/transac/alshaya_white_label/js/accordion.js
@@ -8,25 +8,6 @@
 
   Drupal.behaviors.privilegeCardAccordion = {
     attach: function (context, settings) {
-      var applyCoupon = $('#coupon-button');
-      if (context === document) {
-        applyCoupon.prev().addBack().wrapAll('<div class="card__content">');
-      }
-      $('.coupon-code-wrapper, .alias--cart #details-privilege-card-wrapper').each(function () {
-        if (context === document) {
-          var error = $(this).find('.form-item--error-message');
-          var active = false;
-          if (error.length > 0) {
-            active = 0;
-          }
-          $(this).accordion({
-            header: '.card__header',
-            collapsible: true,
-            heightStyle: 'content',
-            active: active
-          });
-        }
-      });
 
       $('.alias--user-register #details-privilege-card-wrapper').each(function () {
         if (context === document) {
