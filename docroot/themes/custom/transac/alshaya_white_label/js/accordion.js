78c657cf1e42e7fa9b9a88bf369e82e8ea30a870
sreekanth.sreedharan <sreekanth.sreedharan@alshaya.com>
2019-09-05 14:47:03 +0300

    CORE-11815: Order confirmation, myaccount recent order list js moved to alshaya_acm_customer module.

--- docroot/themes/custom/transac/alshaya_white_label/js/accordion.js
+++ docroot/themes/custom/transac/alshaya_white_label/js/accordion.js
@@ -54,41 +54,12 @@
         });
 
       /**
-       * Toggles the Expand Order Accordions.
-       */
-
-      if ($('.recent__orders--list .order-summary-row').length) {
-        var parentOrder = $('.recent__orders--list .order-summary-row');
-        var listOrder = $('.recent__orders--list .order-item-row');
-
-        $(listOrder).hide();
-
-        $(parentOrder).click(function () {
-          var $ub = $(this).nextAll().stop(true, true).fadeToggle('slow');
-          listOrder.not($ub).hide();
-          $ub.parent().toggleClass('open--accordion');
-          listOrder.not($ub).parent().removeClass('open--accordion');
-        });
-      }
-
-      /**
        * Toggles the Tabs.
        */
       if ($('.checkout .multistep-checkout').length) {
         $('.multistep-checkout legend').click(function () {
           $(this).next('.fieldset-wrapper').slideToggle();
         });
-      }
-
-      /**
-       * Toggles the Search on Order list.
-       */
-      if ($('.alshaya-acm-customer-order-list-search').length) {
-        $('.alshaya-acm-customer-order-list-search label')
-          .on('click', function () {
-            $('.alshaya-acm-customer-order-list-search')
-              .toggleClass('active--search');
-          });
       }
     }
   };
