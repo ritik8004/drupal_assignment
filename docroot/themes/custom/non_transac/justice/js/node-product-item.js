ed83a70b9be80412e4485263d58cc0427a1c5f55
Sanjib Dutta <sanjib.dutta@backs-MacBook-Pro-2.local>
2019-09-23 13:27:30 +0300

    [develop-justice]JS-23:Configure share this on product listing page.

---
+++ docroot/themes/custom/non_transac/justice/js/node-product-item.js
@@ -0,0 +1,29 @@
+(function ($) {
+  'use strict';
+
+  $(document).ready(function () {
+    var bodyEle = $('body');
+    var shareIconEle = $('div.view-product-item__share--label');
+    var productEle = $('.view-product-item__inner-container');
+    // var productEleOverlay = $('div.view-product-item__inner-container.share-overlay');
+    var overlayClass = 'share-overlay';
+
+    bodyEle.on('click', function (event) {
+      if ($(event.target).is(shareIconEle)) {
+        var local = $(event.target).closest(productEle);
+        if (local.hasClass(overlayClass)) {
+          local.removeClass(overlayClass);
+        }
+        else {
+          bodyEle.find(productEle).removeClass(overlayClass);
+          local.addClass(overlayClass);
+        }
+      }
+      else {
+        if (!$(event.target).hasClass(overlayClass)) {
+          bodyEle.find(productEle).removeClass(overlayClass);
+        }
+      }
+    });
+  });
+})(jQuery);
