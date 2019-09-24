7c5085629c6a6f7900df253b7b391519769a928a
Sanjib Dutta <sanjib.dutta@backs-MacBook-Pro-2.local>
2019-09-24 14:50:51 +0300

    JS-23: adding drupal behaviours in js file

--- docroot/themes/custom/non_transac/justice/js/node-product-item.js
+++ docroot/themes/custom/non_transac/justice/js/node-product-item.js
@@ -1,28 +1,35 @@
-(function ($) {
+/**
+ * @file
+ * plp share product.
+ */
+
+(function ($, Drupal) {
   'use strict';
 
-  $(document).ready(function () {
-    var bodyEle = $('body');
-    var shareIconEle = $('div.view-product-item__share--label');
-    var productEle = $('.view-product-item__inner-container');
-    var overlayClass = 'share-overlay';
+  Drupal.behaviors.shareProduct = {
+    attach: function (context, settings) {
+      var bodyEle = $('body');
+      var shareIconEle = $('div.view-product-item__share--label');
+      var productEle = $('.view-product-item__inner-container');
+      var overlayClass = 'share-overlay';
 
-    bodyEle.on('click', function (event) {
-      if ($(event.target).is(shareIconEle)) {
-        var local = $(event.target).closest(productEle);
-        if (local.hasClass(overlayClass)) {
-          local.removeClass(overlayClass);
+      bodyEle.on('click', function (event) {
+        if ($(event.target).is(shareIconEle)) {
+          var local = $(event.target).closest(productEle);
+          if (local.hasClass(overlayClass)) {
+            local.removeClass(overlayClass);
+          }
+          else {
+            bodyEle.find(productEle).removeClass(overlayClass);
+            local.addClass(overlayClass);
+          }
         }
         else {
-          bodyEle.find(productEle).removeClass(overlayClass);
-          local.addClass(overlayClass);
+          if (!$(event.target).hasClass(overlayClass)) {
+            bodyEle.find(productEle).removeClass(overlayClass);
+          }
         }
-      }
-      else {
-        if (!$(event.target).hasClass(overlayClass)) {
-          bodyEle.find(productEle).removeClass(overlayClass);
-        }
-      }
-    });
-  });
-})(jQuery);
+      });
+    }
+  };
+})(jQuery, Drupal);
