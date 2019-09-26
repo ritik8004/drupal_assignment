ee972f8394cfd3adeb5a0b5acc6f51c9b9e0eb19
Mitesh Patel <developermitesh@gmail.com>
2019-09-26 23:34:13 +0530

    CORE-12497: update class and data attributes for node articles.

--- docroot/modules/react/alshaya_algolia_react/js/Components/teaser/Teaser.js
+++ docroot/modules/react/alshaya_algolia_react/js/Components/teaser/Teaser.js
@@ -9,26 +9,24 @@ const Teaser = ({hit}) => {
 
   return (
     <div className="c-products__item views-row">
-      <article data-sku={hit.sku} gtm-view-mode="search_result">
-        <div className="content">
-          <div className="field field--name-field-skus field--type-sku field--label-hidden field__items">
-            <a
-              href={hit.url}
-              data--original-url={hit.url}
-              className="list-product-gallery product-selected-url">
-              <Gallery media={hit.media} title={hit.title} />
-            </a>
-            <div className="product-plp-detail-wrapper">
-              <h2 className="field--name-name">
-                <a href={hit.url} className="product-selected-url">{hit.title}</a>
-              </h2>
-              <PriceContainer price={hit.original_price} final_price={hit.final_price}/>
-              <PromotionsContainer promotions={hit.promotions}/>
-              {swatches}
-            </div>
+      <article data-sku={hit.sku} data-vmode="search_result">
+        <div className="field field--name-field-skus field--type-sku field--label-hidden field__items">
+          <a
+            href={hit.url}
+            data--original-url={hit.url}
+            className="list-product-gallery product-selected-url">
+            <Gallery media={hit.media} title={hit.title} />
+          </a>
+          <div className="product-plp-detail-wrapper">
+            <h2 className="field--name-name">
+              <a href={hit.url} className="product-selected-url">{hit.title}</a>
+            </h2>
+            <PriceContainer price={hit.original_price} final_price={hit.final_price}/>
+            <PromotionsContainer promotions={hit.promotions}/>
+            {swatches}
           </div>
-          <LabelsContainer labels={hit.product_labels} sku={hit.sku} />
         </div>
+        <LabelsContainer labels={hit.product_labels} sku={hit.sku} />
       </article>
     </div>
   );
