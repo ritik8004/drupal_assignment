277c9aa71db4ee00eec500f1beede9630099b6de
Swarad <swarad07@gmail.com>
2019-09-27 17:10:30 +0530

    CORE-10015: Adding FE for alshaya react algolia search block.

--- docroot/modules/react/alshaya_algolia_react/js/Components/InstantSearchComponent.js
+++ docroot/modules/react/alshaya_algolia_react/js/Components/InstantSearchComponent.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {
   InstantSearch,
+  Configure
 } from 'react-instantsearch-dom';
 import {searchClient} from '../Config/SearchClient';
 
 const InstantSearchComponent = ({ children, indexName}) => (
-  <InstantSearch searchClient={searchClient} indexName={indexName}>{children}</InstantSearch>
+  <InstantSearch searchClient={searchClient} indexName={indexName}>
+    {children}
+    <Configure hitsPerPage="6"/>
+  </InstantSearch>
 );
 
 export default InstantSearchComponent;
