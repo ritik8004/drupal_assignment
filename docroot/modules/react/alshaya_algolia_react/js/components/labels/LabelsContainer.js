b3be4a10b69dcbd885674ea86f39d14e62327d5d
Mitesh Patel <developermitesh@gmail.com>
2019-10-01 12:58:11 +0530

    CORE-12545: return null when no there are no labels in record.

--- docroot/modules/react/alshaya_algolia_react/js/components/labels/LabelsContainer.js
+++ docroot/modules/react/alshaya_algolia_react/js/components/labels/LabelsContainer.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ImageElement from '../gallery/imageHelper/ImageElement';
 
 const LabelsContainer = ({labels, sku}) => {
+  if (labels.length === 0) {
+    return (null);
+  }
   const labelItems = labels.map(({image, position}) => (
     <div class={`label ${position}`}>
       <ImageElement src={image.url} alt={image.alt} title={image.title} />
