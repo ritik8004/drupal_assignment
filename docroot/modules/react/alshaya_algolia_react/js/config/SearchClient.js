dfefc7d031bfeb51e0b4bd056a6321ba3ee1729b
Mitesh Patel <developermitesh@gmail.com>
2019-10-04 17:21:11 +0530

    CORE-12545: Update search client to make request only for search query.

--- docroot/modules/react/alshaya_algolia_react/js/config/SearchClient.js
+++ docroot/modules/react/alshaya_algolia_react/js/config/SearchClient.js
@@ -1,6 +1,22 @@
 import algoliasearch from 'algoliasearch/lite';
 
-export const searchClient = algoliasearch(
+const algoliaClient = algoliasearch(
   drupalSettings.algoliaSearch.application_id,
   drupalSettings.algoliaSearch.api_key
 );
+
+export const searchClient = {
+  search(requests) {
+    if (requests.every(({ indexName, params }) => indexName === drupalSettings.algoliaSearch.indexName && !params.query)) {
+      return Promise.resolve({
+        results: requests.map(() => ({
+          hits: [],
+          nbHits: 0,
+          nbPages: 0,
+          processingTimeMS: 0,
+        })),
+      });
+    }
+    return algoliaClient.search(requests);
+  },
+};
