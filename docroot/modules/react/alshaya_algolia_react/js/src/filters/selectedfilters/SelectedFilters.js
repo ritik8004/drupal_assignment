defeacb6b476c8e66560892a5eacf527f5d087fd
Mitesh Patel <developermitesh@gmail.com>
2019-10-09 13:20:52 +0530

    CORE-12545: Show/hide selected filters when values exists.

--- docroot/modules/react/alshaya_algolia_react/js/src/filters/selectedfilters/SelectedFilters.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/filters/selectedfilters/SelectedFilters.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect }  from 'react';
 import { connectCurrentRefinements } from 'react-instantsearch-dom';
 import FiltersLabels from './FiltersLabels';
 import ClearRefinements from './ClearFilters';
@@ -57,8 +57,24 @@ const CustomCurrentFilters =  connectCurrentRefinements(({ items, refine }) => {
 });
 
 const SelectedFilters = (props) => {
+  const selectedFilterRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      // Hide selected filters div when theere are not selected filters.
+      const selectedFilters = selectedFilterRef.current;
+      if (selectedFilters.querySelector('li') === null) {
+        selectedFilters.style.display = 'none';
+      }
+      else {
+        selectedFilters.style.display = 'block';
+      }
+
+    }, 500);
+  });
+
   return (
-    <div id="block-filterbar" className="block block-facets-summary block-facets-summary-blockfilter-bar">
+    <div id="block-filterbar" className="block block-facets-summary block-facets-summary-blockfilter-bar" ref={selectedFilterRef} style={{display: 'none'}}>
       <span className="filter-list-label">Selected Filters</span>
       <CustomCurrentFilters />
     </div>
