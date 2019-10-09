cdb9c00759d2065c958337007a542bfd62e73327
Mohammad Rashid <mohammad.rashid@alshaya.com>
2019-10-09 13:08:19 +0000

    MAPP-1029: Removed unwanted depdencies and code cleanup.

--- docroot/modules/custom/alshaya_mobile_app/src/Plugin/rest/resource/ProductLinkedSkusResource.php
+++ docroot/modules/custom/alshaya_mobile_app/src/Plugin/rest/resource/ProductLinkedSkusResource.php
@@ -5,18 +5,11 @@ namespace Drupal\alshaya_mobile_app\Plugin\rest\resource;
 use Drupal\acq_commerce\SKUInterface;
 use Drupal\acq_sku\AcqSkuLinkedSku;
 use Drupal\acq_sku\Entity\SKU;
-use Drupal\acq_sku\ProductInfoHelper;
-use Drupal\alshaya_acm_product\SkuImagesManager;
-use Drupal\alshaya_acm_product\Service\SkuInfoHelper;
-use Drupal\alshaya_acm_product\SkuManager;
 use Drupal\alshaya_mobile_app\Service\MobileAppUtility;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\acq_sku\ProductOptionsManager;
-use Drupal\Core\Extension\ModuleHandlerInterface;
 
 /**
  * Provides a resource to get product details with linked skus.
@@ -32,74 +25,11 @@ use Drupal\Core\Extension\ModuleHandlerInterface;
 class ProductLinkedSkusResource extends ResourceBase {
 
   /**
-   * SKU Manager.
-   *
-   * @var \Drupal\alshaya_acm_product\SkuManager
-   */
-  private $skuManager;
-
-  /**
-   * SKU Images Manager.
-   *
-   * @var \Drupal\alshaya_acm_product\SkuImagesManager
-   */
-  private $skuImagesManager;
-
-  /**
-   * Product Info helper.
-   *
-   * @var \Drupal\acq_sku\ProductInfoHelper
-   */
-  private $productInfoHelper;
-
-  /**
-   * Node Storage.
-   *
-   * @var \Drupal\node\NodeStorageInterface
-   */
-  private $nodeStorage;
-
-  /**
-   * Term Storage.
-   *
-   * @var \Drupal\taxonomy\TermStorageInterface
-   */
-  private $termStorage;
-
-  /**
    * The mobile app utility service.
    *
    * @var \Drupal\alshaya_mobile_app\Service\MobileAppUtility
    */
   private $mobileAppUtility;
-
-  /**
-   * Production Options Manager service object.
-   *
-   * @var \Drupal\acq_sku\ProductOptionsManager
-   */
-  protected $productOptionsManager;
-
-  /**
-   * Store cache tags and contexts to be added in response.
-   *
-   * @var array
-   */
-  private $cache;
-
-  /**
-   * Module handler.
-   *
-   * @var \Drupal\Core\Extension\ModuleHandlerInterface
-   */
-  protected $moduleHandler;
-
-  /**
-   * Sku info helper.
-   *
-   * @var \Drupal\alshaya_acm_product\Service\SkuInfoHelper
-   */
-  protected $skuInfoHelper;
 
   /**
    * ProductResource constructor.
@@ -114,22 +44,8 @@ class ProductLinkedSkusResource extends ResourceBase {
    *   Serializer formats.
    * @param \Psr\Log\LoggerInterface $logger
    *   Logger channel.
-   * @param \Drupal\alshaya_acm_product\SkuManager $sku_manager
-   *   SKU Manager.
-   * @param \Drupal\alshaya_acm_product\SkuImagesManager $sku_images_manager
-   *   SKU Images Manager.
-   * @param \Drupal\acq_sku\ProductInfoHelper $product_info_helper
-   *   Product Info helper.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   Entity Type Manager.
    * @param \Drupal\alshaya_mobile_app\Service\MobileAppUtility $mobile_app_utility
    *   The mobile app utility service.
-   * @param \Drupal\acq_sku\ProductOptionsManager $product_options_manager
-   *   Production Options Manager service object.
-   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
-   *   Module handler.
-   * @param \Drupal\alshaya_acm_product\Service\SkuInfoHelper $sku_info_helper
-   *   Sku info helper object.
    */
   public function __construct(
     array $configuration,
@@ -137,29 +53,10 @@ class ProductLinkedSkusResource extends ResourceBase {
     $plugin_definition,
     array $serializer_formats,
     LoggerInterface $logger,
-    SkuManager $sku_manager,
-    SkuImagesManager $sku_images_manager,
-    ProductInfoHelper $product_info_helper,
-    EntityTypeManagerInterface $entity_type_manager,
-    MobileAppUtility $mobile_app_utility,
-    ProductOptionsManager $product_options_manager,
-    ModuleHandlerInterface $module_handler,
-    SkuInfoHelper $sku_info_helper
+    MobileAppUtility $mobile_app_utility
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
-    $this->skuManager = $sku_manager;
-    $this->skuImagesManager = $sku_images_manager;
-    $this->productInfoHelper = $product_info_helper;
-    $this->nodeStorage = $entity_type_manager->getStorage('node');
-    $this->termStorage = $entity_type_manager->getStorage('taxonomy_term');
     $this->mobileAppUtility = $mobile_app_utility;
-    $this->productOptionsManager = $product_options_manager;
-    $this->cache = [
-      'tags' => [],
-      'contexts' => [],
-    ];
-    $this->moduleHandler = $module_handler;
-    $this->skuInfoHelper = $sku_info_helper;
   }
 
   /**
@@ -172,14 +69,7 @@ class ProductLinkedSkusResource extends ResourceBase {
       $plugin_definition,
       $container->getParameter('serializer.formats'),
       $container->get('logger.factory')->get('alshaya_mobile_app'),
-      $container->get('alshaya_acm_product.skumanager'),
-      $container->get('alshaya_acm_product.sku_images_manager'),
-      $container->get('acq_sku.product_info_helper'),
-      $container->get('entity_type.manager'),
-      $container->get('alshaya_mobile_app.utility'),
-      $container->get('acq_sku.product_options_manager'),
-      $container->get('module_handler'),
-      $container->get('alshaya_acm_product.sku_info')
+      $container->get('alshaya_mobile_app.utility')
     );
   }
 
@@ -208,14 +98,6 @@ class ProductLinkedSkusResource extends ResourceBase {
 
     $response = new ResourceResponse($data);
     $cacheableMetadata = $response->getCacheableMetadata();
-
-    if (!empty($this->cache['contexts'])) {
-      $cacheableMetadata->addCacheContexts($this->cache['contexts']);
-    }
-
-    if (!empty($this->cache['tags'])) {
-      $cacheableMetadata->addCacheTags($this->cache['tags']);
-    }
 
     $response->addCacheableDependency($cacheableMetadata);
 
