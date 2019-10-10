0fd194c719ad9986519dd49722fe03d8fc526cad
Mitesh Patel <developermitesh@gmail.com>
2019-10-10 18:09:10 +0530

    CORE-12545: Remove searchclient related customization as it prevents default functionality to work properly.

--- docroot/modules/react/alshaya_algolia_react/js/src/config/SearchClient.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/config/SearchClient.js
@@ -1,22 +1,6 @@
 import algoliasearch from 'algoliasearch/lite';
 
-const algoliaClient = algoliasearch(
+export const searchClient = algoliasearch(
   drupalSettings.algoliaSearch.application_id,
   drupalSettings.algoliaSearch.api_key
 );
-
-export const searchClient = {
-  search(requests) {
-    if (requests.every(({ indexName, params }) => indexName === drupalSettings.algoliaSearch.indexName && !params.query)) {
-      return Promise.resolve({
-        results: requests.map(() => ({
-          hits: [],
-          nbHits: 0,
-          nbPages: 0,
-          processingTimeMS: 0,
-        })),
-      });
-    }
-    return algoliaClient.search(requests);
-  },
-};
