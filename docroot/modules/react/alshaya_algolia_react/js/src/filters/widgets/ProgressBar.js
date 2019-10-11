936d67ad32b751bef47ab258fc0644f9744e5403
Swarad <swarad07@gmail.com>
2019-10-11 14:54:45 +0530

    CORE-12734: FE for Algolia Pager.

--- docroot/modules/react/alshaya_algolia_react/js/src/filters/widgets/ProgressBar.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/filters/widgets/ProgressBar.js
@@ -6,22 +6,18 @@ class ProgressBar extends React.Component {
 
   static defaultProps = {
     completed: 0,
-    color: '#0BD318',
     animation: 200,
-    height: 10
-  }
+  };
 
   static throwError() {
     return new Error(...arguments);
   }
 
   render () {
-    const {color, completed, animation, height, className, children, ...rest} = this.props;
+    const {completed, animation, className, children, ...rest} = this.props;
     const style = {
-      backgroundColor: color,
       width: completed + '%',
       transition: `width ${animation}ms`,
-      height: height
     };
 
     return (
