13fc170c075d84ee2b4d9a10e51d7d8e36f5a510
Rohit Joshi <joshi.rohit100@gmail.com>
2019-10-15 11:41:49 +0530

    CORE-11576: Adding properties to cart component.

--- docroot/modules/react/alshaya_spc/js/cart/components/cart/index.js
+++ docroot/modules/react/alshaya_spc/js/cart/components/cart/index.js
@@ -8,12 +8,29 @@ export default class Cart extends React.Component {
     super(props);
     this.state = {
       'wait': true,
-      'cart': null
+      'items': [],
+      'totals': [],
+      'recommended_products': [],
+      'total_items': null
     };
   }
 
+  componentDidMount() {
+    // Listen to `refreshCart` event triggered from `mini-cart/index.js`.
+    document.addEventListener('refreshCart', (e) => {
+      var data = e.detail.data();
+      this.setState(state => ({
+        items: { },
+        totals: data.totals,
+        recommended_products: { },
+        total_items: data.items_qty,
+        wait: false
+      }));
+    }, false);
+  };
+
   render() {
-      if (this.state.wait || !this.state.cart) {
+      if (this.state.wait) {
         return <EmptyCart></EmptyCart>
       }
 
