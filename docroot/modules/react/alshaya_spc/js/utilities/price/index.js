b08bd0b97996d975a1318407b3d02c1a3b446731
Rohit Joshi <joshi.rohit100@gmail.com>
2019-10-17 18:03:31 +0530

    CORE-11576: Adding total subtotal component.

--- docroot/modules/react/alshaya_spc/js/utilities/price/index.js
+++ docroot/modules/react/alshaya_spc/js/utilities/price/index.js
@@ -3,8 +3,9 @@ import React from 'react';
 export default class Price extends React.Component {
 
   render() {
-    const price = this.props.price.toFixed(this.props.decimal_position);
-    return <span>{price}</span>
+    const currency_config = window.drupalSettings.alshaya_spc.currency_config;
+    const price = this.props.price.toFixed(currency_config.decimal_points);
+    return <span>{currency_config.currency_code} {price}</span>
   }
 
 }
