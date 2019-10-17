38d98d97ad3c7f0dc1f2ab7790d496bf787d8099
Swarad <swarad07@gmail.com>
2019-10-17 12:33:07 +0530

    CORE-12545: Fixing JS window scroll feedback - adding once().

--- docroot/themes/custom/transac/alshaya_white_label/conditional-sass/algolia-search/js/algolia-search.js
+++ docroot/themes/custom/transac/alshaya_white_label/conditional-sass/algolia-search/js/algolia-search.js
@@ -29,7 +29,7 @@
           filterPosition = $('.branding__menu').outerHeight() + superCategoryMenuHeight + slugBannerHeight;
         }
 
-        $(window).on('scroll', function () {
+        $(window).once('algoliaReactSearch').on('scroll', function () {
           if ($(window).width() < 768) {
             if ($(this).scrollTop() > filterPosition) {
               $('body').addClass('Sticky-algolia-search');
