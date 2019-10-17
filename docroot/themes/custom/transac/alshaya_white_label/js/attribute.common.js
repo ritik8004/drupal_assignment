446aed6bd3168d7096936ab3da6baa31dcd1c917
nikunj <coder@nikunj.dev>
2019-10-17 13:49:19 +0530

    CORE-10531: Apply changes to magazine layout as well.

---
+++ docroot/themes/custom/transac/alshaya_white_label/js/attribute.common.js
@@ -0,0 +1,59 @@
+/**
+ * @file
+ * Common code for attributes.
+ */
+
+(function ($, Drupal) {
+  'use strict';
+
+  /**
+   * JS for converting select list for size to unformatted list on PDP pages.
+   *
+   * @param {object} element
+   *   The HTML element inside which we want to convert select list into unformatted list.
+   */
+  Drupal.convertSelectListtoUnformattedList = function (element) {
+    element.once('convert-select-list-to-unformatted-list').each(function () {
+      $(this).on('refresh', function () {
+        var that = $(this).parent();
+
+        $(this).select2Option();
+        $(this).find('.list-title .selected-text').html('');
+
+        var clickedOption = $('option:selected', this);
+        if (!clickedOption.is(':disabled')) {
+          if ($(this).parent().hasClass('form-item-configurables-article-castor-id')) {
+            Drupal.alshaya_color_swatch_update_selected_label();
+          }
+          else {
+            var selectedText = clickedOption.attr('selected-text')
+              ? clickedOption.attr('selected-text')
+              : clickedOption.text();
+            $('.select2Option', that).find('.list-title .selected-text').html(selectedText);
+          }
+        }
+      });
+
+      $(this).trigger('refresh');
+    });
+  };
+
+  Drupal.behaviors.configurableAttributeBoxes = {
+    attach: function (context, settings) {
+      $('.form-item-configurable-swatch').parent().addClass('configurable-swatch');
+      $('.form-item-configurable-select').parent().addClass('configurable-select');
+
+      // Show mobile slider only on mobile resolution.
+      Drupal.select2OptionConvert();
+      $(window).on('resize', function (e) {
+        Drupal.select2OptionConvert();
+      });
+
+      if ($(window).width() <= drupalSettings.show_configurable_boxes_after) {
+        $('.form-item-configurable-select, .form-item-configurable-swatch').on('change', function () {
+          $(this).closest('form').find('div.error, label.error, span.error').remove();
+        });
+      }
+    }
+  };
+})(jQuery, Drupal);
--- docroot/themes/custom/transac/alshaya_white_label/js/magazine_attribute.js
+++ docroot/themes/custom/transac/alshaya_white_label/js/magazine_attribute.js
@@ -49,27 +49,6 @@
   };
 
   /**
-   * JS for converting select list for size to unformatted list on PDP pages.
-   *
-   * @param {object} element
-   *   The HTML element inside which we want to convert select list into unformatted list.
-   */
-  Drupal.convertSelectListtoUnformattedList = function (element) {
-    element.once('bind-events').each(function () {
-      var that = $(this).parent();
-      $('select', that).select2Option();
-
-      $('.select2Option', that).find('.list-title .selected-text').html('');
-
-      var clickedOption = $('select option:selected', that);
-      if (!clickedOption.is(':disabled')) {
-        $('.select2Option', that).find('.list-title .selected-text').html(clickedOption.text());
-        Drupal.alshaya_color_swatch_update_selected_label();
-      }
-    });
-  };
-
-  /**
    * Implementation of view more/less colour for swatches.
    */
   Drupal.magazine_swatches_count = function () {
@@ -164,22 +143,6 @@
       $(this).hide();
       $('.show-more-color').show();
     });
-  };
-
-  Drupal.behaviors.configurableAttributeBoxes = {
-    attach: function (context, settings) {
-      $('.form-item-configurable-swatch').parent().addClass('configurable-swatch');
-      $('.form-item-configurable-select').parent().addClass('configurable-select');
-
-      // Show mobile slider only on mobile resolution.
-      Drupal.select2OptionConvert(context);
-
-      if ($(window).width() <= drupalSettings.show_configurable_boxes_after) {
-        $('.form-item-configurable-select, .form-item-configurable-swatch').on('change', function () {
-          $(this).closest('.sku-base-form').find('div.error, label.error, span.error').remove();
-        });
-      }
-    }
   };
 
   /**
