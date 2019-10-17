6d94593db17590987ff0ac8c365f248cd60454a1
Mitesh Patel <developermitesh@gmail.com>
2019-10-17 16:44:49 +0530

    CORE-10016: display top results when user enters query.

--- docroot/modules/react/alshaya_algolia_react/js/src/components/Portal/Portal.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/components/Portal/Portal.js
@@ -1,11 +1,13 @@
 import { useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 
-const Portal = ({query, children, ...attr}) => {
+const Portal = ({query, children, conditional = false, ...attr}) => {
   const el = useRef(document.createElement('div'));
   const [dynamic] = useState(!el.current.parentElement);
   useEffect(() => {
-    if (dynamic) {
+    const createPortal = (conditional === true) ? (query !== '') : true;
+
+    if (dynamic && createPortal) {
       for (const [property, value] of Object.entries(attr)) {
         el.current[property] = value;
       }
