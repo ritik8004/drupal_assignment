0b4783358515092a5083424310508cc185d7c7c5
Mitesh Patel <developermitesh@gmail.com>
2019-10-18 01:04:54 +0530

    CORE-12923: update product title to show query highlight.

--- docroot/modules/react/alshaya_algolia_react/js/src/AppAutocomplete.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/AppAutocomplete.js
@@ -72,10 +72,10 @@ class AppAutocomplete extends React.Component {
   };
 
   render() {
-    const { query, categories } = this.state;
+    const { query } = this.state;
     // Display search results when wrapper is present on page.
     const searchWrapper = document.getElementById('alshaya-algolia-search');
-    const searchResultsDiv = (typeof searchWrapper != 'undefined' && searchWrapper != null)
+    const searchResultsDiv = (typeof searchWrapper != 'undefined' && searchWrapper != null) && query != ''
       ? (<SearchResultsRender query={query} />)
       : '';
 
