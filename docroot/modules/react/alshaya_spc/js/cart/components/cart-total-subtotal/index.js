a806ba8bab0028d405dc52d4fdf2f2f4c5c7eb33
Rohit Joshi <joshi.rohit100@gmail.com>
2019-10-18 11:28:16 +0530

    CORE-11576: Passing stock status to total component.

--- docroot/modules/react/alshaya_spc/js/cart/components/cart-total-subtotal/index.js
+++ docroot/modules/react/alshaya_spc/js/cart/components/cart-total-subtotal/index.js
@@ -6,10 +6,10 @@ import VatText from '../../../utilities/vat-text';
 export default class CartTotalSubTotal extends React.Component {
 
   render() {
+    const in_stock = this.props.in_stock;
     const vat_text = window.drupalSettings.alshaya_spc.vat_text;
     const discount = this.props.totals.discount_amount;
 
-    console.log(this.props.totals);
     return (
       <div>
         <h2>{Drupal.t('order summary')}</h2>
