43843415d8f78bd506571fdde081c5c78f6a84b8
Your Name <you@example.com>
2019-10-22 19:40:09 +0530

    Updates for PR feedback.

--- docroot/modules/brands/alshaya_ve/modules/alshaya_ve_non_transac/js/alshaya_ve_non_transac_store_finder.js
+++ docroot/modules/brands/alshaya_ve/modules/alshaya_ve_non_transac/js/alshaya_ve_non_transac_store_finder.js
@@ -50,7 +50,8 @@
                   $.each(results[0].address_components, function( index, value ) {
                     if (value.types[0] == 'country') {
                       var cookie_options = {path: '/', expires: 7200, secure: true};
-                      $.cookie('alshaya_client_country_code', value.short_name, cookie_options);
+                      $.cookie('alshaya_client_country_code', value.short_name.toLowerCase(), cookie_options);
+                      // drupalSettings.geolocation.geocoder.googlePlacesAPI.restrictions.country = [value.short_name.toLowerCase()];
                     }
                   });
                 }
@@ -58,6 +59,37 @@
             });
           });
         }
+      }
+    }
+  };
+
+
+  Drupal.behaviors.storeFinderUpdateCountry = {
+    attach: function (context, settings) {
+      var country_code = $.cookie('alshaya_client_country_code');
+      var name = GetParameterValues('country');
+      var options = $('#views-exposed-form-stores-finder-page-2 select option').map(function() { return $(this).val(); }).get();
+      if (typeof name === 'undefined') {
+        if(jQuery.cookie('alshaya_client_country_code')) {
+          if(jQuery.inArray(jQuery.cookie('alshaya_client_country_code').toLowerCase(), options) !== -1) {
+            drupalSettings.geolocation.geocoder.googlePlacesAPI.restrictions.country = [jQuery.cookie('alshaya_client_country_code').toLowerCase()];
+          }
+          else {
+            drupalSettings.geolocation.geocoder.googlePlacesAPI.restrictions.country = options.slice(1);
+          }
+        }
+        else {
+          drupalSettings.geolocation.geocoder.googlePlacesAPI.restrictions.country = options.slice(1);
+        }
+      }
+      else {
+        if (name == 'All') {
+          drupalSettings.geolocation.geocoder.googlePlacesAPI.restrictions.country = options.slice(1);
+        }
+        else {
+          drupalSettings.geolocation.geocoder.googlePlacesAPI.restrictions.country = [name];
+        }
+        
       }
     }
   };
@@ -73,9 +105,26 @@ function defaultSelectCountryOption() {
     var countryCode = drupalSettings.geolocation.geocoder.googlePlacesAPI.restrictions.country[0];
     var optionExists = (jQuery("#views-exposed-form-stores-finder-page-2 select option[value=" + countryCode + "]").length > 0);
     if (optionExists) {
-      jQuery("#views-exposed-form-stores-finder-page-2 select option[value=" + countryCode + "]").prop('selected',true);
+      var name = GetParameterValues('country');
+      if (typeof name === 'undefined') {
+        jQuery("#views-exposed-form-stores-finder-page-2 select option[value=" + countryCode + "]").prop('selected',true);
+        setTimeout(function () {
+          jQuery('#views-exposed-form-stores-finder-page-2 [id^="edit-submit-stores-finder"]').trigger('click');
+        }, 500);
+      }
     }
   }
+}
+
+// Function to check url parameters.
+function GetParameterValues(param) {  
+  var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');  
+  for (var i = 0; i < url.length; i++) {  
+    var urlparam = url[i].split('=');  
+    if (urlparam[0] == param) {  
+      return urlparam[1];  
+    }  
+  } 
 }
 
 
