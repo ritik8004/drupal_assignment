dbae5d552b5df5e5a9eaefa6dd2206c734f90551
Rohit Joshi <joshi.rohit100@gmail.com>
2019-10-24 10:42:04 +0530

    CORE-12288: Adding my shopping bag component.

--- docroot/modules/react/alshaya_spc/js/cart/components/cart/index.js
+++ docroot/modules/react/alshaya_spc/js/cart/components/cart/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import EmptyCart from '../empty-cart';
+import CartShoppingBag from '../cart-shopping-bag';
 import CartTotalSubTotal from '../cart-total-subtotal';
 import CartOutOfStock from '../cart-oos';
 import CartRecommendedProducts from '../recommended-products';
@@ -15,6 +16,7 @@ export default class Cart extends React.Component {
       'totals': [],
       'recommended_products': [],
       'total_items': null,
+      'amount': null,
       'in_stock': true
     };
   }
@@ -36,6 +38,7 @@ export default class Cart extends React.Component {
         totals: data.totals,
         recommended_products: data.recommended_products,
         total_items: data.items_qty,
+        amount: data.cart_total,
         wait: false,
         in_stock: in_stock
       }));
@@ -50,6 +53,7 @@ export default class Cart extends React.Component {
       return (
         <div>
           <CartOutOfStock in_stock={this.state.in_stock} />
+          <CartShoppingBag qty={this.state.total_items} total={this.state.amount} />
           <CartTotalSubTotal totals={this.state.totals} in_stock={this.state.in_stock}></CartTotalSubTotal>
           <CartRecommendedProducts recommended_products={this.state.recommended_products} />
         </div>
