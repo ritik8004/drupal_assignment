1f4b22b27a493d1d4f3c753f68caa555d4a6add9
Mitesh Patel <developermitesh@gmail.com>
2019-10-30 15:21:38 +0530

    CORE-13249: add key to component in array map.

--- docroot/modules/react/alshaya_algolia_react/js/src/components/promotions/PromotionsContainer.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/components/promotions/PromotionsContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Promotion from '../promotions/Promotion';
 
 const PromotionsContainer = ({promotions}) => {
-  const promotionList = (promotions) ? promotions.map(promotion => <Promotion promotion={promotion} />) : '';
+  const promotionList = (promotions) ? promotions.map(promotion => <Promotion key={promotion.text} promotion={promotion} />) : '';
   if (promotionList !== '' && promotionList !== 'null') {
     return <div className="promotions">{promotionList}</div>;
   }
