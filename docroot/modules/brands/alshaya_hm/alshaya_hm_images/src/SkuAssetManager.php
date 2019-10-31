a25f1e576d25cc155624ef318fa90409b2bd6bc2
Rohit Joshi <joshi.rohit100@gmail.com>
2019-10-31 13:55:54 +0530

    CORE-13273: Using the shared asset directory.

--- docroot/modules/brands/alshaya_hm/alshaya_hm_images/src/SkuAssetManager.php
+++ docroot/modules/brands/alshaya_hm/alshaya_hm_images/src/SkuAssetManager.php
@@ -316,7 +316,7 @@ class SkuAssetManager {
     $pims_directory = $this->hmImageSettings->get('pims_directory');
 
     // Prepare the directory path.
-    $directory = 'public://assets/' . trim($data['path'], '/');
+    $directory = 'public://assets-shared/' . trim($data['path'], '/');
     $target = $directory . DIRECTORY_SEPARATOR . $data['filename'];
 
     // Check if file already exists in the directory.
@@ -397,7 +397,7 @@ class SkuAssetManager {
     }
 
     // Prepare the directory path.
-    $directory = 'public://assets-lp/' . trim(dirname($asset['Data']['FilePath']), '/');
+    $directory = 'public://assets-lp-shared/' . trim(dirname($asset['Data']['FilePath']), '/');
     $target = $directory . DIRECTORY_SEPARATOR . basename($asset['Data']['FilePath']);
 
     // Check if file already exists in the directory.
