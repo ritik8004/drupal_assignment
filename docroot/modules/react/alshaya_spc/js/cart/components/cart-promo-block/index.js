a03a4faf1d322ec5a6e440bf7ba4881ff06028a2
Rohit Joshi <joshi.rohit100@gmail.com>
2019-11-08 15:40:23 +0530

    CORE-11576: Cart promotion apply/remove.

--- docroot/modules/react/alshaya_spc/js/cart/components/cart-promo-block/index.js
+++ docroot/modules/react/alshaya_spc/js/cart/components/cart-promo-block/index.js
@@ -1,31 +1,82 @@
 import React from 'react';
-import Axios from 'axios';
+
+import {applyRemovePromo} from '../../../utilities/update_cart';
 import CheckoutSectionTitle from "../spc-checkout-section-title";
 
 export default class CartPromoBlock extends React.Component {
 
-  promoAction = (action) => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'promo_applied': false,
+      'promo_code': '',
+      'button_text': Drupal.t('Apply')
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.coupon_code.length > 0) {
+      this.setState({
+        promo_applied: true,
+        promo_code: this.props.coupon_code,
+        button_text: Drupal.t('Remove')
+      });
+
+      document.getElementById('promo-code').value = this.props.coupon_code;
+    }
+  }
+
+  promoAction = (promo_applied) => {
     var promo_value = document.getElementById('promo-code').value.trim();
     // If empty promo text.
-    if (promo_value.length === 0) {
+    if (this.state.promo_applied === false && promo_value.length === 0) {
       document.getElementById('promo-error-message').innerHTML = Drupal.t('Please enter promo code.');
       document.getElementById('promo-code').classList.add('error');
       return;
     }
 
-    this.promoApplyRemove(action, promo_value);
-  };
+    var action = (promo_applied === true) ? 'remove coupon' : 'apply coupon';
 
-  promoApplyRemove = (action, code) => {
-    let api_url = window.drupalSettings.alshaya_spc.middleware_url + '/cart/' + cart;
+    var cart_data = applyRemovePromo(action, promo_value);
+    if (cart_data instanceof Promise) {
+      cart_data.then((result) => {
+        // If coupon is not valid.
+        if (result.response_message.status === 'error_coupon') {
+          document.getElementById('promo-error-message').innerHTML = result.response_message.msg;
+          document.getElementById('promo-code').classList.add('error');
+        }
+        else if(result.response_message.status === 'success') {
+          document.getElementById('promo-error-message').innerHTML = result.response_message.msg;
+          document.getElementById('promo-code').classList.add('success');
+          // I initially promo_applied was false, means promo is applied now.
+          if (promo_applied === false) {
+            this.setState({
+              promo_applied: true,
+              promo_code: promo_value,
+              button_text: Drupal.t('Remove')
+            });
+          }
+          else {
+            // It means promo is removed.
+            this.setState({
+              promo_applied: false,
+              promo_code: '',
+              button_text: Drupal.t('Apply')
+            });
 
-    return Axios.get(api_url)
-      .then(response => {
-        return response.data
-      })
-      .catch(error => {
-        // Processing of error here.
+            document.getElementById('promo-code').value = '';
+          }
+
+          // Refreshing mini-cart.
+          var event = new CustomEvent('refreshMiniCart', {bubbles: true, detail: { data: () => result }});
+          document.dispatchEvent(event);
+
+          // Refreshing cart components..
+          var event = new CustomEvent('refreshCart', {bubbles: true, detail: { data: () => result }});
+          document.dispatchEvent(event);
+        }
       });
+    }
   };
 
   render() {
@@ -34,8 +85,7 @@ export default class CartPromoBlock extends React.Component {
         <CheckoutSectionTitle>{Drupal.t('have a promo code?')}</CheckoutSectionTitle>
         <div className="block-content">
           <input id="promo-code" type="text" placeholder={Drupal.t('Enter your promo code here')} />
-          <button className="promo-remove" onClick={()=>{this.promoAction('remove')}}/>
-          <button className="promo-submit" onClick={()=>{this.promoAction('apply')}}>{Drupal.t('Apply')}</button>
+          <button className="promo-submit" onClick={()=>{this.promoAction(this.state.promo_applied)}}>{this.state.button_text}</button>
           <div id="promo-error-message"/>
         </div>
       </div>
