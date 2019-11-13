0fd80056ddefee3d77e346b99b093f885abff176
nikunj <coder@nikunj.dev>
2019-11-13 16:11:49 +0530

    CORE-13527: Allow only rsync to be done separately in dedicated script.

--- scripts/utilities/stack-migration.sh
+++ scripts/utilities/stack-migration.sh
@@ -2,57 +2,57 @@
 #
 # This script migrates given site between stacks.
 #
-# ./scripts/utilities/stack-migration.sh 01live vskw 02live vskw2 prepare
-# ./scripts/utilities/stack-migration.sh 01live vskw 02live vskw2 doit
+# ./scripts/utilities/stack-migration.sh 01live vskw 02live vskw2
 
 source_env="$1"
 source_site="$2"
 target_env="$3"
 target_site="$4"
 
-type="$5"
-if [[ -z "$type" ]]; then
-  type="prepare"
-fi
-
 if [[ -z "$source_env" ]]; then
-  echo "Usage: ./scripts/utilities/stack-migration.sh SOURCE_ENV SOURCE_SITE_CODE TARGET_ENV TARGET_SITE_CODE (prepare|doit)"
-  echo "Example: ./scripts/utilities/stack-migration.sh 01live vskw 02live vskw2 prepare"
+  echo "Usage: ./scripts/utilities/stack-migration.sh SOURCE_ENV SOURCE_SITE_CODE TARGET_ENV TARGET_SITE_CODE"
+  echo "Example: ./scripts/utilities/stack-migration.sh 01live vskw 02live vskw2"
   exit
 fi
 
 if [[ -z "$source_site" ]]; then
-  echo "Usage: ./scripts/utilities/stack-migration.sh SOURCE_ENV SOURCE_SITE_CODE TARGET_ENV TARGET_SITE_CODE (prepare|doit)"
-  echo "Example: ./scripts/utilities/stack-migration.sh 01live vskw 02live vskw2 prepare"
+  echo "Usage: ./scripts/utilities/stack-migration.sh SOURCE_ENV SOURCE_SITE_CODE TARGET_ENV TARGET_SITE_CODE"
+  echo "Example: ./scripts/utilities/stack-migration.sh 01live vskw 02live vskw2"
   exit
 fi
 
 if [[ -z "$target_env" ]]; then
-  echo "Usage: ./scripts/utilities/stack-migration.sh SOURCE_ENV SOURCE_SITE_CODE TARGET_ENV TARGET_SITE_CODE (prepare|doit)"
-  echo "Example: ./scripts/utilities/stack-migration.sh 01live vskw 02live vskw2 prepare"
+  echo "Usage: ./scripts/utilities/stack-migration.sh SOURCE_ENV SOURCE_SITE_CODE TARGET_ENV TARGET_SITE_CODE"
+  echo "Example: ./scripts/utilities/stack-migration.sh 01live vskw 02live vskw2"
   exit
 fi
 
 if [[ -z "$target_site" ]]; then
-  echo "Usage: ./scripts/utilities/stack-migration.sh SOURCE_ENV SOURCE_SITE_CODE TARGET_ENV TARGET_SITE_CODE (prepare|doit)"
-  echo "Example: ./scripts/utilities/stack-migration.sh 01live vskw 02live vskw2 prepare"
+  echo "Usage: ./scripts/utilities/stack-migration.sh SOURCE_ENV SOURCE_SITE_CODE TARGET_ENV TARGET_SITE_CODE"
+  echo "Example: ./scripts/utilities/stack-migration.sh 01live vskw 02live vskw2"
   exit
 fi
 
-if [ ${target_env:0:2} != "02" ]; then
+if [ ${target_env:0:2} == "02" ]; then
   target_var="alshaya202live"
   target="alshaya2.02live@web-4238.enterprise-g1.hosting.acquia.com"
-else
+elif [ ${target_env:0:2} == "01" ]; then
   target_var="alshaya01live"
   target="alshaya.01live@web-1503.enterprise-g1.hosting.acquia.com"
+else
+  echo "Invalid target env $target_env"
+  exit
 fi
 
-if [ ${source_env:0:2} != "02" ]; then
+if [ ${source_env:0:2} == "02" ]; then
   source_var="alshaya202live"
   source="alshaya2.02live@web-4238.enterprise-g1.hosting.acquia.com"
-else
+elif [ ${source_env:0:2} == "01" ]; then
   source_var="alshaya01live"
   source="alshaya.01live@web-1503.enterprise-g1.hosting.acquia.com"
+else
+  echo "Invalid source env $source_env"
+  exit
 fi
 
 cd /var/www/html/${source_var}/docroot
@@ -67,12 +67,6 @@ echo "Syncing files with target env for $source_site"
 source_files_folder=`drush -l $source_site.factory.alshaya.com status | grep Public | cut -d":" -f2 | sed 's/ //g'`
 target_files_folder=`ssh -t $target "cd /var/www/html/$target_var/docroot; drush -l $target_site.factory.alshaya.com status | grep Public | cut -d":" -f2 | sed 's/ //g'"`
 screen -S rsync_${source_site}_${target_site} -dm bash -c "rsync -a $source_files_folder $target:$target_files_folder"
-
-if [[ "$type" == "prepare" ]]; then
-  echo "Not doing full migration as we are just preparing for now. Please pass 5th argument as 'doit' if you want to do full migration"
-  echo
-  exit
-fi
 
 echo
 echo "Dumping database..."
