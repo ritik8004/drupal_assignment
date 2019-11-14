d0b07b4f628aab492fa4b20e7d93c54505ddd020
Rohit Joshi <joshi.rohit100@gmail.com>
2019-11-14 12:13:42 +0530

    CORE-0001: Using guzzle client for the drupal.

--- docroot/middleware/src/Service/Drupal/Drupal.php
+++ docroot/middleware/src/Service/Drupal/Drupal.php
@@ -37,7 +37,7 @@ class Drupal {
     $client = $this->drupalInfo->getDrupalApiClient();
     $data = [];
     foreach ($skus as $sku) {
-      $url = $this->drupalInfo->getDrupalUrl() . '/' . sprintf('rest/v1/product/%s', $sku) . '?context=cart';
+      $url = '/' . sprintf('rest/v1/product/%s', $sku) . '?context=cart';
       $response = $client->request('GET', $url, ['verify' => FALSE]);
       $result = $response->getBody()->getContents();
       $data[$sku] = json_decode($result, TRUE);
@@ -59,7 +59,7 @@ class Drupal {
     $client = $this->drupalInfo->getDrupalApiClient();
     $data = [];
     foreach ($skus as $sku) {
-      $url = $this->drupalInfo->getDrupalUrl() . '/' . sprintf('rest/v1/product/%s/linked', $sku);
+      $url = '/' . sprintf('rest/v1/product/%s/linked', $sku);
       $response = $client->request('GET', $url, ['verify' => FALSE]);
       $result = $response->getBody()->getContents();
       $data[$sku] = json_decode($result, TRUE);
