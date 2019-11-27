8b56abce53376cc0f48f1fd1214ead2228fbcc37
Sylvain DELBOSC <sylvain.delbosc@acquia.com>
2019-11-27 12:51:27 +0100

    Merge pull request #14147 from purushotamrai/CORE-13546-settings-develop
    [4.8.0] CORE-13546: Update alshaya stack 01 env settings to stack independent.

--- factory-hooks/post-settings-php/new_relic.php
+++ factory-hooks/post-settings-php/new_relic.php
@@ -8,8 +8,17 @@
  */
 
 if (extension_loaded('newrelic')) {
+  $env = 'local';
+
+  if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
+    $env = $_ENV['AH_SITE_ENVIRONMENT'];
+  }
+  elseif (getenv('TRAVIS')) {
+    $env = 'travis';
+  }
+
   global $_acsf_site_name;
-  newrelic_set_appname("$_acsf_site_name;alshaya.01live", '', 'true');
+  newrelic_set_appname("$_acsf_site_name;alshaya.$env", '', 'true');
   // Disable newrelic for all pages.
   newrelic_disable_autorum();
 }
