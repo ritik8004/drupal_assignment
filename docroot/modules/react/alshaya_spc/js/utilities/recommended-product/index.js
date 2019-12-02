4c82feed77561840862525b076b9244676963ed7
Rohit Joshi <joshi.rohit100@gmail.com>
2019-12-02 18:04:03 +0530

    CORE-11577: Cart recommended products.

--- docroot/modules/react/alshaya_spc/js/utilities/recommended-product/index.js
+++ docroot/modules/react/alshaya_spc/js/utilities/recommended-product/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import CartPromotion from '../../cart/components/cart-promotion';
+
 export default class RecommendedProduct extends React.Component {
 
   render() {
     const item = this.props.item;
     return (
-      <div>
-        <img src={item.medias.images[0].url} />
+      <a href={"product-quick-view/" + item.nid + "/nojs"} className='use-ajax above-mobile-block' data-dialog-type='modal'>
+        <img src={item.extra_data.cart_image.url} alt={item.extra_data.cart_image.alt} title={item.extra_data.cart_image.title} />
         <div>{item.title}</div>
-      </div>
+        <div className="spc-promotions">
+          {item.promo.map((key, val) =>
+            <CartPromotion key={val} promo={key} />
+          )}
+        </div>
+      </a>
     );
   }
 }
