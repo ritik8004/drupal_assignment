7a9305fc924171bd1d9d00c30a5d15d4d0447d89
Rohit Joshi <joshi.rohit100@gmail.com>
2019-12-05 17:07:45 +0530

    CORE-13955: Correcting the condition.

--- docroot/modules/react/alshaya_spc/js/utilities/free-delivery-text/index.js
+++ docroot/modules/react/alshaya_spc/js/utilities/free-delivery-text/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class FreeDeliveryText extends React.Component {
 
   render() {
-    if (this.props.freeDelivery) {
+    if (!this.props.freeDelivery) {
       return <span className="delivery-prefix">{this.props.text}</span>
     }
 
