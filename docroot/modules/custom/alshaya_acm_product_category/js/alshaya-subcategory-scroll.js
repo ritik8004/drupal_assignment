7cc1a09a8eea2f2332f3a8f0bac27f954c8fde35
Prashant Kumar <prashant114606@gmail.com>
2019-12-05 12:22:02 +0530

    CORE-13959: Fixing the page is scrolling with different padding for different secnario.

--- docroot/modules/custom/alshaya_acm_product_category/js/alshaya-subcategory-scroll.js
+++ docroot/modules/custom/alshaya_acm_product_category/js/alshaya-subcategory-scroll.js
@@ -19,7 +19,7 @@
     var stickyFilterPosition;
 
     if ($(window).width() < 768) {
-      stickyFilterPosition = $('#block-supercategorymenu').outerHeight() + $('#block-mobilenavigation').outerHeight() + $('.show-all-filters').outerHeight() + 20;
+      stickyFilterPosition = $('#block-supercategorymenu').outerHeight() + $('#block-mobilenavigation').outerHeight() + $('.show-all-filters').outerHeight();
       if ($('#block-subcategoryblock').hasClass('mobile-sticky-sub-category')) {
         // If target is above of the current view point in that case subcategory will be visible.
         if ($('.plp-subcategory-block').offset().top > $(element).offset().top) {
@@ -32,15 +32,15 @@
     }
     else if ($(window).width() > 767 && $(window).width() < 1024) {
       // Height of sticky filter.
-      stickyFilterPosition = 55;
+      stickyFilterPosition = $('.container-without-product').outerHeight();
       if ($('#block-subcategoryblock').hasClass('mobile-sticky-sub-category')) {
         if ($('.plp-subcategory-block').offset().top > $(element).offset().top) {
           stickyFilterPosition = stickyFilterPosition + $('.mobile-sticky-sub-category').outerHeight() - 10;
         }
       }
       else if (!$('body').hasClass('header-sticky-filter') && !$('#block-subcategoryblock').hasClass('mobile-sticky-sub-category')) {
-        // On page load when filters are not sticky.
-        stickyFilterPosition = 10;
+        // On page load when filters are not sticky and adding 60 as fixed width for sticky filters which becomes sticky on scroll.
+        stickyFilterPosition = -$('.show-all-filters').outerHeight() + 60;
       }
     }
     else {
@@ -48,7 +48,7 @@
         stickyFilterPosition = $('.show-sub-category').outerHeight();
       }
       else if ($('.region__content').hasClass('filter-fixed-top') && $('.plp-subcategory-block').offset().top < $(element).offset().top) {
-        stickyFilterPosition = $('.site-brand-home').outerHeight();
+        stickyFilterPosition = $('.site-brand-home').outerHeight() + 10;
       }
       else {
         // Removing extra added 20px for top margin here when filters are not sticky.
@@ -56,14 +56,9 @@
       }
     }
 
-    // Adding padding from the sticky filters wrapper.
-    var topPadding = 0;
-    if ($(window).width() > 1023) {
-      topPadding = 20;
-    }
-    // Adding 10px of margin from top so spacing doesn't look tight between term title and sticky facet filters.
+    // Adding 20px of margin from top so spacing doesn't look tight between term title and sticky facet filters.
     $('html, body').animate({
-      scrollTop: ($(element).offset().top - stickyFilterPosition - topPadding)
+      scrollTop: ($(element).offset().top - stickyFilterPosition - 20)
     }, 500);
   }
 
