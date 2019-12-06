ed44e8a2f9f60f486dd05d5feb09b5f70576447e
Rohit Joshi <joshi.rohit100@gmail.com>
2019-12-06 11:05:45 +0530

    CORE-13975: Fixing PR feedback.

--- docroot/modules/custom/alshaya_acm_promotion/src/Plugin/rest/resource/PromotionsResource.php
+++ docroot/modules/custom/alshaya_acm_promotion/src/Plugin/rest/resource/PromotionsResource.php
@@ -181,7 +181,7 @@ class PromotionsResource extends ResourceBase {
           'promote' => in_array($node->get('field_acq_promotion_rule_id')->first()->getString(), $active_promotions),
           'promo_sub_tpe' => $node->get('field_alshaya_promotion_subtype')->first()->getString(),
           'promo_desc' => $node->get('field_acq_promotion_description')->first() ? $node->get('field_acq_promotion_description')->first()->getValue()['value'] : '',
-          'promo_label' => $node->get('field_acq_promotion_label')->first() ? $node->get('field_acq_promotion_label')->first()->getString() : '',
+          'promo_label' => $node->get('field_acq_promotion_label')->getString(),
         ];
 
         $this->moduleHandler->alter('alshaya_acm_promo_resource', $data, $node);
