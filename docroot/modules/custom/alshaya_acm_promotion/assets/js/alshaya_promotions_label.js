5258d2871e247278f62e508c3e8054cf723304f3
Prashant Kumar <prashant114606@gmail.com>
2019-12-06 16:29:15 +0530

    CORE-13830: Promo label is updating dynamically and also changing as per colour variant selection.

--- docroot/modules/custom/alshaya_acm_promotion/assets/js/alshaya_promotions_label.js
+++ docroot/modules/custom/alshaya_acm_promotion/assets/js/alshaya_promotions_label.js
@@ -109,16 +109,21 @@
       });
 
       // Cut the Dynamic promotion wrapper and insert it after add to cart button.
-      if ($(window).width() < 768 && $('.promotions .sku-dynamic-promotion-link').length > 0) {
-        var dynamicPromotionWrapper = $('.promotions .promotions-dynamic-label').clone();
-        if ($('.basic-details-wrapper .promotions-dynamic-label').length < 1) {
-          dynamicPromotionWrapper.once('bind-promotions-dynamic-label-events').insertAfter($('.edit-add-to-cart'));
+      if ($(window).width() < 768) {
+        if ($('.promotions .sku-dynamic-promotion-link').length > 0) {
+          var dynamicPromotionWrapper = $('.promotions .promotions-dynamic-label').clone();
+          if ($('.basic-details-wrapper .promotions-dynamic-label').length < 1) {
+            dynamicPromotionWrapper.once('bind-promotions-dynamic-label-events').insertAfter($('.edit-add-to-cart'));
+          } else {
+            // Replace the same promotion wrapper with updated dynamic label.
+            $('.basic-details-wrapper .promotions-dynamic-label').replaceWith(dynamicPromotionWrapper);
+          }
         }
         else {
-          // Replace the same promotion wrapper with updated dynamic label.
-          $('.basic-details-wrapper .promotions-dynamic-label').replaceWith(dynamicPromotionWrapper);
+          $('.basic-details-wrapper .promotions-dynamic-label').remove();
         }
       }
+
 
       // Go ahead and display dynamic promotions.
       $('.acq-content-product .content__title_wrapper .promotions .promotions-dynamic-label', context).once('update-promo-label-pdp').each(function () {
