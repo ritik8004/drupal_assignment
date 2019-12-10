2e3bcc795bd999050b591eca28a969dadf0a7e7e
Rohit Joshi <joshi.rohit100@gmail.com>
2019-12-10 12:58:28 +0530

    CORE-0001: Adding small fixes for spc.

--- docroot/modules/react/alshaya_spc/js/cart/components/total-line-items/index.js
+++ docroot/modules/react/alshaya_spc/js/cart/components/total-line-items/index.js
@@ -7,10 +7,10 @@ class TotalLineItems extends React.Component {
   render() {
     return (
       <div className="totals">
-        <TotalLineItem name="sub-total" title={Drupal.t('sub total')} value={this.props.totals.subtotal_incl_tax}/>
-        <TotalLineItem tooltip={true} tooltipContent={Drupal.t('Discount applied')} name="discount-total" title={Drupal.t('discount')} value={this.props.totals.discount_amount}/>
+        <TotalLineItem name="sub-total" title={Drupal.t('Subtotal')} value={this.props.totals.subtotal_incl_tax}/>
+        <TotalLineItem tooltip={true} tooltipContent={Drupal.t('Discount applied')} name="discount-total" title={Drupal.t('Discount')} value={this.props.totals.discount_amount}/>
         <div className="hero-total">
-          <TotalLineItem name="grand-total" title={Drupal.t('order total')} value={this.props.totals.base_grand_total}/>
+          <TotalLineItem name="grand-total" title={Drupal.t('Order Total')} value={this.props.totals.base_grand_total}/>
           <div className="delivery-vat">
             <FreeDeliveryText freeDelivery={this.props.totals.free_delivery} text={Drupal.t('excluding delivery')} />
             <VatText />
