2a916c003b83e76dbeee72f4ed5c0ae6bfee3742
Mitesh Patel <developermitesh@gmail.com>
2019-12-17 11:56:11 +0530

    CORE-14115: revalidate blazy on hiding search result container.

--- docroot/modules/react/alshaya_algolia_react/js/src/utils/SearchUtility.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/utils/SearchUtility.js
@@ -32,6 +32,7 @@ function hideSearchResultContainer() {
   searchResultDiv.style.display = 'none';
   searchResultDiv.classList.remove('show-algolia-result');
   pageStandard.className = defaultClasses;
+  Drupal.blazyRevalidate();
 }
 
 function toggleSearchResultsContainer(query) {
