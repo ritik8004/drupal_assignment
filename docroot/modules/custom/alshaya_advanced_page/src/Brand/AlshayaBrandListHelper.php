15d6fc184f087925f1fa21be1860aa4ac1d4ca7c
Brajendra Singh <brajendra.singh@pimcore.com>
2019-12-26 10:42:45 +0530

    CORE-1227: Create and store brand original image from MDC in drupal and use in brand carousel.

--- docroot/modules/custom/alshaya_advanced_page/src/Brand/AlshayaBrandListHelper.php
+++ docroot/modules/custom/alshaya_advanced_page/src/Brand/AlshayaBrandListHelper.php
@@ -48,8 +48,8 @@ class AlshayaBrandListHelper {
     $query = $query = $this->connection->select('taxonomy_term_field_data', 'ttfd');
     $query->fields('ttfd', ['tid', 'name']);
     $query->innerJoin('taxonomy_term__field_sku_attribute_code', 'ttac', 'ttac.entity_id = ttfd.tid');
-    $query->innerJoin('taxonomy_term__field_attribute_swatch_image', 'ttasi', 'ttasi.entity_id = ttfd.tid');
-    $query->innerJoin('file_managed', 'fm', 'ttasi.field_attribute_swatch_image_target_id = fm.fid AND fm.status = 1');
+    $query->innerJoin('taxonomy_term__field_attribute_swatch_org_image', 'ttasoi', 'ttasoi.entity_id = ttfd.tid');
+    $query->innerJoin('file_managed', 'fm', 'ttasoi.field_attribute_swatch_org_image_target_id = fm.fid AND fm.status = 1');
     $query->addField('fm', 'uri');
     $query->condition('ttac.field_sku_attribute_code_value', self::BRAND_ATTRIBUTE_CODE);
     $query->condition('ttfd.default_langcode', 1);
--- docroot/modules/custom/alshaya_advanced_page/src/Plugin/Block/AlshayaBrandCarouselBlock.php
+++ docroot/modules/custom/alshaya_advanced_page/src/Plugin/Block/AlshayaBrandCarouselBlock.php
@@ -4,9 +4,11 @@ namespace Drupal\alshaya_advanced_page\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\alshaya_advanced_page\Brand\AlshayaBrandListHelper;
 use Drupal\Core\Cache\Cache;
+use Drupal\Core\Language\LanguageManagerInterface;
 
 /**
  * Provides alshaya brand carousel block.
@@ -26,11 +28,27 @@ class AlshayaBrandCarouselBlock extends BlockBase implements ContainerFactoryPlu
   protected $brandList;
 
   /**
+   * Module Handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * THe language manager service.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
    * {@inheritdoc}
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, AlshayaBrandListHelper $brand_list) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, AlshayaBrandListHelper $brand_list, ModuleHandlerInterface $module_handler, LanguageManagerInterface $languageManager) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->brandList = $brand_list;
+    $this->moduleHandler = $module_handler;
+    $this->languageManager = $languageManager;
   }
 
   /**
@@ -41,7 +59,9 @@ class AlshayaBrandCarouselBlock extends BlockBase implements ContainerFactoryPlu
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('alshaya.brand_list_helper')
+      $container->get('alshaya.brand_list_helper'),
+      $container->get('module_handler'),
+      $container->get('language_manager')
     );
   }
 
@@ -54,11 +74,16 @@ class AlshayaBrandCarouselBlock extends BlockBase implements ContainerFactoryPlu
     $brand_images = [];
 
     if (!empty($terms)) {
+      $langcode = $this->languageManager->getCurrentLanguage()->getId();
+      $link = '/' . $langcode . '/search?f[0]=product_brand:';
+      if ($this->moduleHandler->moduleExists('alshaya_search_algolia')) {
+        $link = '/' . $langcode . '/#query= &refinementList[attr_product_brand][0]=';
+      }
       foreach ($terms as $term) {
         $brand_images[$term->tid] = [
           'image' => $term->uri,
           'title' => $term->name,
-          'link' => '/search?f[0]=product_brand:' . $term->name,
+          'link' => $link . $term->name,
         ];
       }
     }
