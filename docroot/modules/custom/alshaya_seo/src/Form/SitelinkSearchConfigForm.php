f43d8a9e08c2bff6fe720e3d26cbecdcac421d3d
Brajendra Singh <brajendra.singh@pimcore.com>
2020-01-06 18:18:18 +0530

    CORE-11394: Updated as per PR feedback.

--- docroot/modules/custom/alshaya_seo/src/Form/SitelinkSearchConfigForm.php
+++ docroot/modules/custom/alshaya_seo/src/Form/SitelinkSearchConfigForm.php
@@ -58,13 +58,11 @@ class SitelinkSearchConfigForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildForm($form, $form_state);
     $config = $this->config('alshaya_seo.sitelink_search');
-    $settingsMode = ($this->moduleHandler->moduleExists('search_api')) && (alshaya_is_env_prod()) ? FALSE : TRUE;
     $form['enable_sitelink_searchbox'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Enable Sitelink Searchbox'),
       '#required' => FALSE,
       '#default_value' => $config->get('enable_sitelink_searchbox'),
-      '#disabled' => $settingsMode,
       '#description' => $this->t('This setting will work only with search enabled production sites.'),
     ];
     $form['sitelink_searchbox_url'] = [
@@ -73,7 +71,6 @@ class SitelinkSearchConfigForm extends ConfigFormBase {
       '#title' => $this->t('Search Url for Sitelink Searchbox'),
       '#default_value' => $config->get('sitelink_searchbox_url'),
       '#description' => $this->t('Enter site search url eg. search?keywords={search_term_string}, #query={search_term_string}'),
-      '#disabled' => $settingsMode,
     ];
     return $form;
   }
