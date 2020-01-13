fc58e79c43f25ad9b21e0c01277c8d3293d19e1f
nikitaaswani <nikita.aswani@qed42.com>
2020-01-13 17:16:31 +0530

    CORE-5741-menu-algo-uat: Updated styling for dynamic mega menu after server side algorithm implementation.

--- docroot/themes/custom/transac/alshaya_white_label/conditional-sass/megamenu-dynamic-layout/js/megamenu-dynamic-layout.js
+++ docroot/themes/custom/transac/alshaya_white_label/conditional-sass/megamenu-dynamic-layout/js/megamenu-dynamic-layout.js
@@ -22,39 +22,14 @@
     $this.once('MegaMenuDynamicLayout').each(function(){
       var eleL2Wrapper = $(this).children('.menu--two__list');
       var eleL2LinksWrapper = eleL2Wrapper.find('.menu__links__wrapper');
-      var eleL2HighlightWrapper = eleL2Wrapper.find('.term-image__wrapper');
+      var eleL2HighlightWrapper = eleL2Wrapper.find('.term-image__wrapper .highlights');
       var eleMainMenu = $('.megamenu-dynamic-layout');
 
       if (eleL2Wrapper.length > 0) {
         $(this).css('position', 'relative');
-        // Target the first L2 list.
-        var eleFirstList = eleL2Wrapper.find('.menu__links__wrapper .menu--two__list-item:eq(0)');
-        // Target the last L2 list.
-        var eleLastList = eleL2Wrapper.find('.menu__links__wrapper .menu--two__list-item:last-child');
-
-        // Left position of first L2 list.
-        var posFirstList = eleFirstList.offset().left;
-        // Left position of last L2 list.
-        var posLastList = eleLastList.offset().left;
-
-        // Set the Width L2 wrapper for Arabic layout.
-        if (isRTL()) {
-          if (posLastList < posFirstList) {
-            var listWidth = eleL2LinksWrapper.offset().left - posLastList;
-            eleL2LinksWrapper.css('min-width', listWidth + eleL2LinksWrapper.outerWidth());
-          }
-        } else {
-          // Checking the Left position of first and last L2.
-          if (posFirstList < posLastList) {
-            var listWidth = posLastList - eleL2LinksWrapper.offset().left ;
-            eleL2LinksWrapper.css('min-width', listWidth + eleLastList.outerWidth());
-          }
-        }
-
         var eleL2LinksWrapperWidth = eleL2LinksWrapper.outerWidth();
         var highlightWrapperWidth = eleL2HighlightWrapper.outerWidth();
-        var l2WrapperWidth = (eleL2LinksWrapperWidth + highlightWrapperWidth);
-
+        var l2WrapperWidth = (eleL2LinksWrapperWidth + highlightWrapperWidth + 24);
         // Assigning the width to the L2 wrapper
         eleL2Wrapper.css('width', l2WrapperWidth);
 
