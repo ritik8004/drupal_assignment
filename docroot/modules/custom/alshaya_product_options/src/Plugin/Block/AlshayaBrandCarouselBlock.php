9b26a21f38635382f5de772462f9536476938c0a
Brajendra Singh <brajendra.singh@pimcore.com>
2020-01-20 15:18:10 +0530

    CORE-14117: Removing unused dependency as per feedback.

--- docroot/modules/custom/alshaya_product_options/src/Plugin/Block/AlshayaBrandCarouselBlock.php
+++ docroot/modules/custom/alshaya_product_options/src/Plugin/Block/AlshayaBrandCarouselBlock.php
@@ -4,11 +4,9 @@ namespace Drupal\alshaya_product_options\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Extension\ModuleHandlerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\alshaya_product_options\Brand\AlshayaBrandListHelper;
 use Drupal\Core\Cache\Cache;
-use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\alshaya_options_list\AlshayaOptionsListHelper;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Session\AccountInterface;
@@ -32,20 +30,6 @@ class AlshayaBrandCarouselBlock extends BlockBase implements ContainerFactoryPlu
   protected $brandList;
 
   /**
-   * Module Handler.
-   *
-   * @var \Drupal\Core\Extension\ModuleHandlerInterface
-   */
-  protected $moduleHandler;
-
-  /**
-   * The language manager service.
-   *
-   * @var \Drupal\Core\Language\LanguageManagerInterface
-   */
-  protected $languageManager;
-
-  /**
    * Brand Carousel config.
    *
    * @var \Drupal\Core\Config\ImmutableConfig
@@ -62,11 +46,9 @@ class AlshayaBrandCarouselBlock extends BlockBase implements ContainerFactoryPlu
   /**
    * {@inheritdoc}
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, AlshayaBrandListHelper $brand_list, ModuleHandlerInterface $module_handler, LanguageManagerInterface $languageManager, ConfigFactoryInterface $config_factory, AlshayaOptionsListHelper $alshaya_options_service) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, AlshayaBrandListHelper $brand_list, ConfigFactoryInterface $config_factory, AlshayaOptionsListHelper $alshaya_options_service) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->brandList = $brand_list;
-    $this->moduleHandler = $module_handler;
-    $this->languageManager = $languageManager;
     $this->config = $config_factory->get('alshaya_brand_carousel.settings');
     $this->alshayaOptionsService = $alshaya_options_service;
   }
@@ -80,8 +62,6 @@ class AlshayaBrandCarouselBlock extends BlockBase implements ContainerFactoryPlu
       $plugin_id,
       $plugin_definition,
       $container->get('alshaya_product_options.brand_list_helper'),
-      $container->get('module_handler'),
-      $container->get('language_manager'),
       $container->get('config.factory'),
       $container->get('alshaya_options_list.alshaya_options_service')
     );
