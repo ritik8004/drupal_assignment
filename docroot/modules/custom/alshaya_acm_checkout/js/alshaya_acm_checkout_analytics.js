f8bcb7334e10116c378ac7d632af69a99439d19a
nikunj <coder@nikunj.dev>
2020-01-22 19:33:10 +0530

    CORE-13235: Use better conditions to check if ga is loaded.

--- docroot/modules/custom/alshaya_acm_checkout/js/alshaya_acm_checkout_analytics.js
+++ docroot/modules/custom/alshaya_acm_checkout/js/alshaya_acm_checkout_analytics.js
@@ -16,28 +16,23 @@
   Drupal.behaviors.alshayaAcmCheckoutAnalytics = {
     attach: function (context, settings) {
       Drupal.alshayaPopulateDataFromGA();
-
-      // Do it again once after page load finishes.
-      // At times GA script loads late.
-      $(window).once('alshayaAcmCheckoutAnalytics').on('load', function () {
-        Drupal.alshayaPopulateDataFromGA();
-      })
     }
   };
 
   Drupal.alshayaPopulateDataFromGA = function () {
-    if (typeof ga == 'undefined') {
-      setTimeout(Drupal.alshayaPopulateDataFromGA, 500);
+    // Check if ga is loaded.
+    if (typeof window.ga === 'function' && window.ga.loaded) {
+      // Use GA function queue.
+      ga(function () {
+        $('#acm-ga-client-id').val(ga.getAll()[0].get('clientId'));
+        $('#acm-ga-tracking-id').val(ga.getAll()[0].get('trackingId'));
+      });
+
       return;
     }
 
-    try {
-      $('#acm-ga-client-id').val(ga.getAll()[0].get('clientId'));
-      $('#acm-ga-tracking-id').val(ga.getAll()[0].get('trackingId'));
-    } catch (e) {
-      // Try to do again if we face an error.
-      setTimeout(Drupal.alshayaPopulateDataFromGA, 1000);
-    }
+    // Try to read again.
+    setTimeout(Drupal.alshayaPopulateDataFromGA, 500);
   };
 
 })(jQuery, Drupal);
