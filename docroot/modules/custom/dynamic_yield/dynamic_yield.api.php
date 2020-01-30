b5a2f50304965334665d74bf20ded85272dca789
Avinash Shukla <avinash.shukla@acquia.com>
2020-01-30 14:48:35 +0530

    CORE-10955: Added context alter hook in dy api. Altered context in alshaya seo module.

---
+++ docroot/modules/custom/dynamic_yield/dynamic_yield.api.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Hooks specific to the dynamic_yield module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Allow other modules to alter DY context.
+ *
+ * @param array $context
+ *   DY context.
+ */
+function hook_dynamic_yield_context_alter(array &$context) {
+
+}
