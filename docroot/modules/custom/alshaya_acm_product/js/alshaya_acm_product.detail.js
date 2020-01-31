ba597358b6cfb5b2df1735ed4aa6cc0fcf433526
Sonam Singh <sonam@Sonams-MacBook-Air.local>
2020-01-31 13:17:24 +0530

    CORE-14068: Add 'each' from twig.

--- docroot/modules/custom/alshaya_acm_product/js/alshaya_acm_product.detail.js
+++ docroot/modules/custom/alshaya_acm_product/js/alshaya_acm_product.detail.js
@@ -203,19 +203,20 @@
         Drupal.getRelatedProductPosition();
       });
 
-      // Add 'each' with price on change of quantity.
-      $('select.edit-quantity').once('product-edit-quantity').on('change', function () {
-        var quantity = $(this).val();
-        var identifier = drupalSettings.productInfo[$(this).closest('form').attr('data-sku')].identifier;
-        var eachSelector = $('.price-block-' + identifier + ' .price-suffix');
-        var eachSelectorLength = eachSelector.length;
+      // Add 'each' with price on change of quantity if matchback is enabled.
+      if ($('.price-suffix-matchback').length) {
+        $('select.edit-quantity').once('product-edit-quantity').on('change', function () {
+          var quantity = $(this).val();
+          var productKey = ($(this).parents('article.entity--type-node').attr('data-vmode') == 'matchback') ? 'matchback' : 'productInfo';
+          var eachSelector = $('.price-block-' + drupalSettings[productKey][$(this).closest('form').attr('data-sku')].identifier + ' .price-suffix-matchback');
 
-        if (quantity > 1 && !eachSelectorLength) {
-          $('.price-block-' + identifier + ' .price-amount').append('<span class=price-suffix> each</span>');
-        } else if (quantity <= 1 && eachSelectorLength) {
-          eachSelector.remove();
-        }
-      });
+          if (quantity > 1) {
+            eachSelector.show();
+          } else if (quantity <= 1) {
+            eachSelector.hide();
+          }
+        });
+      }
     }
   };
 
