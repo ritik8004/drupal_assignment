36c3f591ddd2efe0b160d846c30d9b1e6804ea24
Brajendra Singh <brajendra.singh@pimcore.com>
2020-02-08 00:13:01 +0530

    CORE-15011: Push visible product impressions whenever filter selected on PLP..

--- docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.product_list.js
+++ docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.product_list.js
@@ -5,16 +5,26 @@
 
 (function ($, Drupal) {
   'use strict';
+
   Drupal.behaviors.seoGoogleTagManagerProductList = {
     attach: function (context, settings) {
-    	// Don't allow for aloglia search.
+      // Don't allow for aloglia search.
       if(window.location.href.match(/#query/g)) {
-      	return;
+        return;
       }
+      // Trigger incase of filter selected.
+      drupalSettings.alshayaSeoGtmFilTertriggered = false;
+      $(context).once('alshaya-seo-gtm-filter-search').ready(function(context, settings) {
+        if ( drupalSettings.alshayaSeoGtmFilTertriggered ) {
+          return;
+        }
+        drupalSettings.alshayaSeoGtmFilTertriggered = true;
+        Drupal.alshaya_seo_gtm_prepare_and_push_product_impression(context, settings);
+      });
 
-		  $(window).on('load scroll', function (event) {
-		  	Drupal.alshaya_seo_gtm_prepare_and_push_product_impression(context, settings);
-		  });
+      $(window).on('load scroll', function (event) {
+        Drupal.alshaya_seo_gtm_prepare_and_push_product_impression(context, settings);
+      });
     }
   };
 })(jQuery, Drupal);
