3e2897801f7c75a95399c3638e7b746d68091b61
Nikunj Kotecha <coder@nikunj.dev>
2020-02-07 08:51:10 +0530

    Revert "[4.11.0] CORE-14251: Removed BE theme functions and helper functions around cloudzoom and gallery."

--- docroot/modules/custom/alshaya_acm_product/src/SkuImagesManager.php
+++ docroot/modules/custom/alshaya_acm_product/src/SkuImagesManager.php
@@ -801,6 +801,9 @@ class SkuImagesManager {
             '#type' => 'pdp',
           ];
 
+          // Add PDP slider position class in template.
+          $pdp_image_slider_position = $this->skuManager->getImageSliderPosition($sku);
+
           $library_array = [
             'alshaya_product_zoom/cloud_zoom_pdp_gallery',
             'alshaya_product_zoom/product.cloud_zoom',
@@ -820,7 +823,9 @@ class SkuImagesManager {
             '#mainImage' => $main_image,
             '#thumbnails' => $thumbnails,
             '#pager_flag' => $pager_flag,
+            '#properties' => $this->getRelCloudZoom($settings),
             '#labels' => $labels,
+            '#image_slider_position_pdp' => 'slider-position-' . $pdp_image_slider_position,
             '#attached' => [
               'library' => $library_array,
             ],
@@ -834,6 +839,7 @@ class SkuImagesManager {
 
         // If thumbnails available.
         if (!empty($thumbnails)) {
+          $settings = $this->getCloudZoomDefaultSettings();
           $config_name = ($context == 'modal') ? 'pdp_slider_items_settings.pdp_slider_items_number_cs_us' : 'pdp_gallery_pager_limit';
           $pdp_gallery_pager_limit = $this->configFactory->get('alshaya_acm_product.settings')
             ->get($config_name);
@@ -859,8 +865,10 @@ class SkuImagesManager {
 
           $gallery['alshaya_magazine'] = [
             '#theme' => 'alshaya_magazine',
+            '#sku' => $sku,
             '#thumbnails' => $thumbnails,
             '#pager_flag' => $pager_flag,
+            '#properties' => $this->getRelCloudZoom($settings),
             '#labels' => $labels,
             '#attached' => [
               'library' => [
@@ -888,7 +896,41 @@ class SkuImagesManager {
       'slide_style' => 'product_zoom_medium_606x504',
       'zoom_style' => 'product_zoom_large_800x800',
       'thumb_style' => 'pdp_gallery_thumbnail',
+      'zoom_width' => 'auto',
+      'zoom_height' => 'auto',
+      'zoom_position' => 'right',
+      'adjust_x' => '0',
+      'adjust_y' => '0',
+      'tint' => '',
+      'tint_opacity' => '0.25',
+      'lens_opacity' => '0.85',
+      'soft_focus' => 'false',
+      'smooth_move' => '3',
     ];
+  }
+
+  /**
+   * Get the rel attribute for Alshaya Product zoom.
+   *
+   * @param array $settings
+   *   Product CloudZoom settings.
+   *
+   * @return string
+   *   return the rel attribute.
+   */
+  protected function getRelCloudZoom(array $settings) {
+    $string = '';
+    $string .= "zoomWidth:'" . $settings['zoom_width'] . "'";
+    $string .= ",zoomHeight:'" . $settings['zoom_height'] . "'";
+    $string .= ",position:'" . $settings['zoom_position'] . "'";
+    $string .= ",adjustX:'" . $settings['adjust_x'] . "'";
+    $string .= ",adjustY:'" . $settings['adjust_y'] . "'";
+    $string .= ",tint:'" . $settings['tint'] . "'";
+    $string .= ",tintOpacity:'" . $settings['tint_opacity'] . "'";
+    $string .= ",lensOpacity:'" . $settings['lens_opacity'] . "'";
+    $string .= ",softFocus:" . $settings['soft_focus'];
+    $string .= ",smoothMove:'" . $settings['smooth_move'] . "'";
+    return $string;
   }
 
   /**
