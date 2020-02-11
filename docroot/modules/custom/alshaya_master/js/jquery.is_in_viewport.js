3cfda90d9d834be7164c1fbb62c5410c6c44bf5b
Brajendra Singh <brajendra.singh@pimcore.com>
2020-02-11 15:52:17 +0530

    CORE-15011: Combine horizontal view port detection in existing one and other minor PR feedbacks changes.

--- docroot/modules/custom/alshaya_master/js/jquery.is_in_viewport.js
+++ docroot/modules/custom/alshaya_master/js/jquery.is_in_viewport.js
@@ -17,22 +17,6 @@
       var viewportTop = $(window).scrollTop();
       var viewportBottom = viewportTop + $(window).height();
 
-      return elementTop >= viewportTop && elementBottom <= viewportBottom;
-    }
-    catch (e) {
-      return false;
-    }
-  }
-
-  /**
-   * Check if element is fully visible in viewport horizontally or not.
-   *
-   * @param offset
-   *
-   * @returns {boolean}
-   */
-  $.fn.isElementInViewPortHorizontally = function (offset) {
-    try {
       // Get element top and bottom.
       var elementLeft = $(this).offset().left - offset;
       var elementRight = elementLeft + $(this).outerWidth();
@@ -41,7 +25,10 @@
       var viewportLeft = $(window).scrollLeft();
       var viewportRight = viewportLeft + $(window).width();
 
-      return elementLeft >= viewportLeft && elementRight <= viewportRight;
+      return elementTop >= viewportTop
+      && elementBottom <= viewportBottom
+      && elementLeft >= viewportLeft
+      && elementRight <= viewportRight;
     }
     catch (e) {
       return false;
