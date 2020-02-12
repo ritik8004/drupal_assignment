5b6faee2238065741ea1ede1fcb5eebdbf3e4411
Mohammad Rashid <mohammad.rashid@alshaya.com>
2020-02-12 11:12:19 +0000

    CORE-15542: Conductor cofigurations for westelm prod env.

--- factory-hooks/environments/magento.php
+++ factory-hooks/environments/magento.php
@@ -398,6 +398,22 @@ $magentos = [
       'store_id' => ['en' => 21, 'ar' => 18],
     ],
   ],
+  'we_prod' => [
+    'url' => 'http://wes.store.alshaya.com',
+    'magento_secrets' => [],
+    'kw' => [
+      'store_id' => ['en' => 1, 'ar' => 14],
+    ],
+    'sa' => [
+      'store_id' => ['en' => 2, 'ar' => 5],
+    ],
+    'ae' => [
+      'store_id' => ['en' => 8, 'ar' => 11],
+    ],
+    'eg' => [
+      'store_id' => ['en' => 20, 'ar' => 17],
+    ],
+  ],
   'default' => [
     'kw' => [
       'magento_lang_prefix' => [
