b5c7945d476019f28f81fa42b1e9b6d63093c80c
Avinash Shukla <avinash.shukla@acquia.com>
2020-02-12 18:05:42 +0530

    CORE-15425: Fixed GTM event filter for color and included filterType key.

--- docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.js
+++ docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.js
@@ -857,20 +857,27 @@
 
         // Track facet filters.
         $('li.facet-item').once('js-event').on('click', function () {
-          if ($(this).find('input.facets-checkbox').attr('checked') === undefined) {
-            var selectedVal = $(this).find('label>span.facet-item__value').text();
-            var facetTitle = $(this).parent('ul').siblings('h3.c-facet__title').text();
-            var filterValue = facetTitle + ':' + selectedVal;
-            filterValue = filterValue.trim();
-
-            var data = {
-              event: 'filter',
-              siteSection: section.trim(),
-              filterValue: filterValue
-            };
-
-            dataLayer.push(data);
+          var selectedVal = '';
+          var facetTitle = '';
+          if ($(this).find('input.facets-checkbox').length > 0) {
+            // Select value of facets other than color and remove item count.
+            selectedVal = $(this).find('label>span.facet-item__value').children().remove().end().text();
           }
+          else {
+            // Select value for color facet filtered and remove item count.
+            selectedVal = $(this).find('a>span.facet-item__value').children().remove().end().text();
+          }
+          facetTitle = $(this).parent('ul').siblings('h3.c-facet__title').children().remove().end().text();
+          var filterValue = selectedVal;
+          filterValue = filterValue.trim();
+          var data = {
+            event: 'filter',
+            siteSection: section.trim(),
+            filterType: facetTitle,
+            filterValue: filterValue
+          };
+
+          dataLayer.push(data);
         });
 
         // Track sorts.
