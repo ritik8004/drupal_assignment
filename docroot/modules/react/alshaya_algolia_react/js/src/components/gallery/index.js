21dc139e7dd476f95bc5c629361aa62edc00db28
vivekpan27 <vivek.panicker@innoraft.com>
2020-02-17 06:21:26 +0000

    CORE-14845: SingleImageGalley component created and modified Gallery component.

--- docroot/modules/react/alshaya_algolia_react/js/src/components/gallery/index.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/components/gallery/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import AssetGallery from './variants/AssetGallery';
 import SearchGallery from './variants/SearchGallery';
+import SingleImageGallery from './variants/SingleImageGallery';
 import { isMobile } from '../../utils';
-import ImageElement from './imageHelper/ImageElement';
 
 const Gallery = (props) => {
   if (typeof props.media === 'undefined') {
@@ -10,31 +10,17 @@ const Gallery = (props) => {
   }
 
   if (isMobile()) {
-    const images = [...props.media];
-    const mainImage = images.length > 0 ? images.shift() : {};
-
-    return (
-      <div className="alshaya_search_gallery">
-        <div className='alshaya_search_mainimage'>
-          <ImageElement
-            src={drupalSettings.reactTeaserView.gallery.lazy_load_placeholder}
-            data-src={ typeof mainImage.url != 'undefined' ? mainImage.url : '' }
-            title={props.title}
-            className='b-lazy'
-          />
-        </div>
-      </div>
-    );
+    return (<SingleImageGallery {...props}/>);
   }
   else {
     if (drupalSettings.reactTeaserView.gallery.showHoverImage) {
       return (<AssetGallery {...props}/>);
     }
-    else if (drupalSettings.reactTeaserView.gallery.showThumbnails){
+    else if (drupalSettings.reactTeaserView.gallery.showThumbnails) {
       return (<SearchGallery {...props}/>);
     }
     else {
-      return (<AssetGallery {...props} />);
+      return (<SingleImageGallery {...props} />);
     }
   }
 
