c09148d4ce08b4f166c0228d26bea653251ab225
vivekpan27 <vivek.panicker@innoraft.com>
2020-02-26 14:35:03 +0000

    CORE-15650: Fixed some condition checking code in Swatch component.

--- docroot/modules/react/alshaya_algolia_react/js/src/components/swatch/index.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/components/swatch/index.js
@@ -20,34 +20,37 @@ const Swatches = (props) => {
   if ((props.swatches === undefined)) {
     return null;
   }
+
+  // Display the colors count for mobile only if different variants images
+  // being shown in gallery on PLP.
+  const show_variants_thumbnail_plp_gallery = drupalSettings.reactTeaserView.showVariantsThumbnail;
+  // Display the configured number of swatches.
   const limit = drupalSettings.reactTeaserView.swatches.swatchPlpLimit;
   const total_no_of_swatches = props.swatches.length;
   const diff = total_no_of_swatches - limit;
-  var swatch_more_text = (diff > 0) ? diff + ' colors' : null;
+  const swatch_color_count = total_no_of_swatches + (total_no_of_swatches > 1 ? ' colors' : ' color')
+  const swatch_more_text = (diff > 0) ? diff + ' colors' : null;
 
   return (
     <React.Fragment>
-      {props.swatches.length > 0 ?
+      {total_no_of_swatches > 0 && !show_variants_thumbnail_plp_gallery ?
         <div className="swatches">
           {props.swatches.slice(0, limit).map((swatch, key) => <Swatch swatch={swatch} key={key} url={props.url} />)}
-          {swatch_more_text ?
-            <a className="swatch-more-link product-selected-url" href={props.url}>+ {swatch_more_text}</a>
+          {swatch_more_text ? <a className="swatch-more-link product-selected-url" href={props.url}>+ {swatch_more_text}</a> : null}
+        </div>
+        :
+        <React.Fragment>
+          {total_no_of_swatches > 0 ?
+            <div className="swatches">
+              <div className="swatch-color-count-wrapper mobile-only-block">
+                <a className="swatch-color-count product-selected-url"
+                  href={props.url}>{swatch_color_count}</a>
+              </div>
+            </div>
             :
             null
-          }
-        </div>
-        :
-        null
-      }
-      {props.swatches.swatch_color_count ?
-        <div className="swatches">
-          <div className="swatch-color-count-wrapper mobile-only-block">
-            <a className="swatch-color-count product-selected-url"
-              href="{$`{props.url}`}">{$`{props.swatches.swatch_color_count }`}</a>
-          </div>
-        </div>
-        :
-        null
+        }
+        </React.Fragment>
       }
     </React.Fragment>
   )
