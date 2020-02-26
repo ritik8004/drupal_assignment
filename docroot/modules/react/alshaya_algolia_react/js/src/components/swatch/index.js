66e789b5ad3752153cf296fd8821ef5f88422971
vivekpan27 <vivek.panicker@innoraft.com>
2020-02-26 09:27:01 +0000

    CORE-15650: Added Swatch section to Algolia React Teaser view.

---
+++ docroot/modules/react/alshaya_algolia_react/js/src/components/swatch/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ImageElement from '../gallery/imageHelper/ImageElement';
+
+const Swatch = (props) => {
+  const selected_image = props.url + '?selected=' + props.swatch.child_id;
+  return (
+    <a href={selected_image}>
+      <span className='swatch-block swatch-image'>
+        {props.url ?
+          <ImageElement data-sku-image={props.swatch.image_url} src={props.swatch.image_url} />
+          :
+          <ImageElement src={props.swatch.image_url} />
+        }
+      </span>
+    </a>
+  )
+}
+
+const Swatches = (props) => {
+  if ((props.swatches === undefined)) {
+    return null;
+  }
+  const limit = drupalSettings.reactTeaserView.swatches.swatchPlpLimit;
+  const total_no_of_swatches = props.swatches.length;
+  const diff = total_no_of_swatches - limit;
+  var swatch_more_text = (diff > 0) ? diff + ' colors' : null;
+
+  return (
+    <React.Fragment>
+      {props.swatches.length > 0 ?
+        <div className="swatches">
+          {props.swatches.slice(0, limit).map((swatch, key) => <Swatch swatch={swatch} key={key} url={props.url} />)}
+          {swatch_more_text ?
+            <a className="swatch-more-link product-selected-url" href={props.url}>+ {swatch_more_text}</a>
+            :
+            null
+          }
+        </div>
+        :
+        null
+      }
+      {props.swatches.swatch_color_count ?
+        <div className="swatches">
+          <div className="swatch-color-count-wrapper mobile-only-block">
+            <a className="swatch-color-count product-selected-url"
+              href="{$`{props.url}`}">{$`{props.swatches.swatch_color_count }`}</a>
+          </div>
+        </div>
+        :
+        null
+      }
+    </React.Fragment>
+  )
+}
+
+export default Swatches;
