a3b94e1005f2c5019143e37d3e2c05e2e33cdd72
Brajendra Singh <brajendra.singh@pimcore.com>
2020-02-27 16:59:50 +0530

    CORE-14831: Changes as per PR feedback.

--- docroot/modules/custom/alshaya_acm_product_category/modules/alshaya_super_category/src/EventSubscriber/SuperCategoryMetaImageEventSubscriber.php
+++ docroot/modules/custom/alshaya_acm_product_category/modules/alshaya_super_category/src/EventSubscriber/SuperCategoryMetaImageEventSubscriber.php
@@ -8,6 +8,9 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Drupal\taxonomy\TermInterface;
 use Drupal\Component\Utility\Html;
 use Drupal\Component\Utility\Unicode;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\alshaya_acm_product_category\ProductCategoryTree;
+use Drupal\Core\Theme\ThemeManagerInterface;
 
 /**
  * Class SuperCtegoryMetaImageEventSubscriber.
@@ -24,13 +27,43 @@ class SuperCategoryMetaImageEventSubscriber implements EventSubscriberInterface 
   protected $routeMatch;
 
   /**
+   * Language manager.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
+   * Product category tree manager.
+   *
+   * @var \Drupal\alshaya_acm_product_category\ProductCategoryTree
+   */
+  private $productCategoryTree;
+
+  /**
+   * Theme Manager service.
+   *
+   * @var \Drupal\Core\Theme\ThemeManagerInterface
+   */
+  protected $themeManager;
+
+  /**
    * DefaultMetaImageEventSubscriber constructor.
    *
    * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
    *   Route Match Object.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $languageManager
+   *   Language manager.
+   * @param \Drupal\alshaya_acm_product_category\ProductCategoryTree $productCategoryTree
+   *   Product category tree manager.
+   * @param \Drupal\Core\Theme\ThemeManagerInterface $theme_manager
+   *   Theme Manager.
    */
-  public function __construct(RouteMatchInterface $route_match) {
+  public function __construct(RouteMatchInterface $route_match, LanguageManagerInterface $language_manager, ProductCategoryTree $productCategoryTree, ThemeManagerInterface $theme_manager) {
     $this->routeMatch = $route_match;
+    $this->languageManager = $language_manager;
+    $this->productCategoryTree = $productCategoryTree;
+    $this->themeManager = $theme_manager;
   }
 
   /**
@@ -48,12 +81,11 @@ class SuperCategoryMetaImageEventSubscriber implements EventSubscriberInterface 
    *   The dispatch event.
    */
   public function setSuperCategoryLogoMetaImage(MetaImageRenderEvent $event) {
-    $product_category_tree = \Drupal::service('alshaya_acm_product_category.product_category_tree');
-    $term = $product_category_tree->getCategoryTermFromRoute();
+    $term = $this->productCategoryTree->getCategoryTermFromRoute();
     if (empty($term)) {
       return;
     }
-    $parents = $product_category_tree->getCategoryTermParents($term);
+    $parents = $this->productCategoryTree->getCategoryTermParents($term);
     if (!empty($parents)) {
       $parent = end($parents);
     }
@@ -63,15 +95,12 @@ class SuperCategoryMetaImageEventSubscriber implements EventSubscriberInterface 
     // Create a name without spaces and any special character.
     $term_clean_name = Html::cleanCssIdentifier(Unicode::strtolower($parent->label()));
     // Set the language suffix for logo based on current language.
-    $langcode = \Drupal::service('language_manager')
-      ->getCurrentLanguage()
-      ->getId();
+    $langcode = $this->languageManager->getCurrentLanguage()->getId();
 
     $langcode = ($langcode != 'en') ? '-' . $langcode : '';
     // Current active theme object.
-    $theme = \Drupal::service('theme.manager')->getActiveTheme();
     // Set the logo path based on term name and current language.
-    $logo_path = '/' . $theme->getPath() . '/imgs/logos/' . $term_clean_name . '-logo';
+    $logo_path = '/' . $this->themeManager->getActiveTheme()->getPath() . '/imgs/logos/' . $term_clean_name . '-logo';
     $logo_relative_path = DRUPAL_ROOT . $logo_path;
     // Check logo fallback.
     $logo = '';
