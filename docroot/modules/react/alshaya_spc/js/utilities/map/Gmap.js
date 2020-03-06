d8f7bda84fb7d34036cec0f170110e9fbaa243bf
Mitesh Patel <developermitesh@gmail.com>
2020-03-06 18:09:59 +0530

    CORE-16193: fix eslint coding standard issues.

--- docroot/modules/react/alshaya_spc/js/utilities/map/Gmap.js
+++ docroot/modules/react/alshaya_spc/js/utilities/map/Gmap.js
@@ -1,6 +1,7 @@
 import _isEmpty from 'lodash/isEmpty';
 import { isRTL } from '../rtl';
 import { dispatchCustomEvent } from '../events';
+import { getDefaultMapCenter } from '../checkout_util';
 
 export default class Gmap {
   constructor() {
@@ -40,27 +41,30 @@ export default class Gmap {
       maxZoom: this.map.settings.maxZoom,
       minZoom: this.map.settings.minZoom,
       fullscreenControl: this.map.settings.fullscreenControl,
-      mapTypeControlOptions: {
-        position: isRTL() === true ? google.maps.ControlPosition.TOP_RIGHT : google.maps.ControlPosition.TOP_LEFT
-      },
-      mapTypeId: google.maps.MapTypeId['ROADMAP'],
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
       mapTypeControl: this.map.settings.mapTypeControl,
       mapTypeControlOptions: {
-        position: isRTL() === true ? google.maps.ControlPosition.RIGHT_BOTTOM : google.maps.ControlPosition.LEFT_BOTTOM
+        position: isRTL() === true
+          ? google.maps.ControlPosition.RIGHT_BOTTOM
+          : google.maps.ControlPosition.LEFT_BOTTOM,
       },
       zoomControl: this.map.settings.zoomControl,
       zoomControlOptions: {
         style: google.maps.ZoomControlStyle.SMALL,
-        position: isRTL() === true ? google.maps.ControlPosition.RIGHT_BOTTOM : google.maps.ControlPosition.LEFT_BOTTOM,
+        position: isRTL() === true
+          ? google.maps.ControlPosition.RIGHT_BOTTOM
+          : google.maps.ControlPosition.LEFT_BOTTOM,
       },
       streetViewControl: this.map.settings.streetViewControl,
       streetViewControlOptions: {
-        position: isRTL() === true ? google.maps.ControlPosition.RIGHT_CENTER : google.maps.ControlPosition.LEFT_CENTER
+        position: isRTL() === true
+          ? google.maps.ControlPosition.RIGHT_CENTER
+          : google.maps.ControlPosition.LEFT_CENTER,
       },
       scrollwheel: this.map.settings.scrollwheel,
       disableDoubleClickZoom: this.map.settings.disableDoubleClickZoom,
       draggable: this.map.settings.draggable,
-      gestureHandling: this.map.settings.gestureHandling
+      gestureHandling: this.map.settings.gestureHandling,
     });
 
     return this.map.googlemap;
@@ -69,9 +73,7 @@ export default class Gmap {
   /**
    * Get map object.
    */
-  getMapObj = () => {
-    return this.map.googleMap;
-  }
+  getMapObj = () => this.map.googleMap;
 
   /**
    * Set map object.
@@ -90,9 +92,10 @@ export default class Gmap {
       return;
     }
 
-    if (typeof drupalSettings.map.center !== 'undefined' && !_isEmpty(drupalSettings.map.center)) {
-      let {latitude, longitude} = drupalSettings.map.center;
-      let position = new google.maps.LatLng(parseFloat(latitude), parseFloat(longitude));
+    const defaultLocation = getDefaultMapCenter();
+    if (!_isEmpty(defaultLocation)) {
+      const { latitude, longitude } = defaultLocation();
+      const position = new google.maps.LatLng(parseFloat(latitude), parseFloat(longitude));
       this.map.googleMap.setCenter(position);
       this.map.googleMap.setZoom(this.map.settings.zoom);
       return;
@@ -100,15 +103,15 @@ export default class Gmap {
 
     this.map.geoCoder.geocode({
       componentRestrictions: {
-        country: window.drupalSettings.country_code
-      }
-    }, function (results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
+        country: drupalSettings.country_code,
+      },
+    }, (results, status) => {
+      if (status === google.maps.GeocoderStatus.OK) {
         // Just center the map and don't do anything.
         window.spcMap.googleMap.setCenter(results[0].geometry.location);
         window.spcMap.googleMap.setZoom(window.spcMap.map.settings.zoom);
         if (callBackFunc) {
-          callBackFunc.call(results)
+          callBackFunc.call(results);
         }
       }
     });
@@ -127,47 +130,50 @@ export default class Gmap {
    * Pass second parameter true to not show infowindo and
    * false to add infowindow and it's close event.
    */
-  setMapMarker = (markerSettings, showInfoWindow) => {
+  setMapMarker = (markerSettings, showInfoWindow = false) => {
     this.map.mapMarkers = this.map.mapMarkers || [];
-    showInfoWindow = showInfoWindow || false;
+    const currentMarkerSettings = { ...markerSettings };
 
-    let { icon: marker_icon_path, label_position } = this.map.settings.map_marker;
+    const { icon: markerIconPath, labelPosition } = this.map.settings.map_marker;
 
-    if (typeof marker_icon_path === 'string') {
+    if (typeof markerIconPath === 'string') {
       // Add the marker icon.
-      markerSettings.icon = {
-        url: marker_icon_path,
-        labelOrigin: new google.maps.Point(label_position.x, label_position.y),
-        scaledSize: new google.maps.Size(31, 48)
+      currentMarkerSettings.icon = {
+        url: markerIconPath,
+        labelOrigin: new google.maps.Point(labelPosition.x, labelPosition.y),
+        scaledSize: new google.maps.Size(31, 48),
       };
 
       // If only single digit move them closer to the center.
-      if (drupalSettings.path.currentLanguage === 'ar' && markerSettings.label.length === 1) {
-        markerSettings.icon.labelOrigin = new google.maps.Point(label_position.single_x, label_position.single_y);
+      if (drupalSettings.path.currentLanguage === 'ar' && currentMarkerSettings.label.length === 1) {
+        currentMarkerSettings.icon.labelOrigin = new google.maps.Point(
+          labelPosition.single_x,
+          labelPosition.single_y,
+        );
       }
     }
 
-    if (!markerSettings.map) {
-      markerSettings.map = this.map.googleMap;
+    if (!currentMarkerSettings.map) {
+      currentMarkerSettings.map = this.map.googleMap;
     }
 
     // Add the marker to the map.
     /** @type {GoogleMarker} */
-    let currentMarker = new google.maps.Marker(markerSettings);
+    const currentMarker = new google.maps.Marker(currentMarkerSettings);
     let currentInfoWindow = null;
 
     if (showInfoWindow === true) {
       // Set the info popup text.
       currentInfoWindow = new google.maps.InfoWindow({
-        content: markerSettings.infoWindowContent,
+        content: currentMarkerSettings.infoWindowContent,
         maxWidth: 209,
-        disableAutoPan: this.map.disableAutoPan
+        disableAutoPan: this.map.settings.disableAutoPan,
       });
     }
 
-    let map = this.map;
-    currentMarker.addListener('click', function () {
-      if (markerSettings.infoWindowSolitary && showInfoWindow === true) {
+    const { map } = this;
+    currentMarker.addListener('click', () => {
+      if (currentMarkerSettings.infoWindowSolitary && showInfoWindow === true) {
         if (typeof map.infoWindow !== 'undefined') {
           map.infoWindow.close();
         }
@@ -179,11 +185,11 @@ export default class Gmap {
       // Set the marker to center of the map on click.
       map.googleMap.setCenter(currentMarker.getPosition());
       map.googleMap.setZoom(12);
-      dispatchCustomEvent('markerClick', { marker: currentMarker,  markerSettings });
+      dispatchCustomEvent('markerClick', { marker: currentMarker, currentMarkerSettings });
     });
 
     if (showInfoWindow === true) {
-      google.maps.event.addListener(currentInfoWindow, 'closeclick', function () {
+      google.maps.event.addListener(currentInfoWindow, 'closeclick', () => {
         // Auto zoom.
         map.googleMap.fitBounds(map.googleMap.bounds);
         // Auto center.
@@ -191,7 +197,7 @@ export default class Gmap {
       });
 
       if (map.settings.info_auto_display) {
-        google.maps.event.addListenerOnce(map.googleMap, 'tilesloaded', function () {
+        google.maps.event.addListenerOnce(map.googleMap, 'tilesloaded', () => {
           google.maps.event.trigger(currentMarker, 'click');
         });
       }
@@ -207,14 +213,14 @@ export default class Gmap {
    */
   removeMapMarker = (map = null) => {
     map = map || this.map;
-    map.mapMarkers.forEach(function (marker) {
+    map.mapMarkers.forEach((marker) => {
       marker.setMap();
     });
   };
 
   closeAllInfoWindow = (map = null) => {
     map = map || this.map;
-    map.mapMarkers.forEach(function(marker) {
+    map.mapMarkers.forEach((marker) => {
       marker.infoWindow.close(map, marker);
     });
   }
