36e45bb7f50d4111bb9047acabd5937c4d8029b3
Srijeeta <srijeeta.ghosh@innoraft.com>
2020-03-12 13:27:03 +0000

    CORE-16215: Mobile cart preview and recommended products JS files issue fixes.

--- docroot/modules/react/alshaya_spc/js/cart/components/recommended-products/index.js
+++ docroot/modules/react/alshaya_spc/js/cart/components/recommended-products/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 
 import RecommendedProduct from '../../../utilities/recommended-product';
-import SectionTitle from "../../../utilities/section-title";
-import { isRTL } from "../../../utilities/rtl";
+import SectionTitle from '../../../utilities/section-title';
+import { isRTL } from '../../../utilities/rtl';
 
 export default class CartRecommendedProducts extends React.Component {
-
   listHorizontalScroll = (direction) => {
     // Lets try native scroll for now using scroll-snap from CSS
     // if doesnt work out this has to be a slider.
-    var container = document.querySelector('.spc-recommended-products .block-content');
+    const container = document.querySelector('.spc-recommended-products .block-content');
     if (direction === 'next') {
       container.scrollBy({
         top: 0,
         left: isRTL() === true ? -320 : 320,
         behavior: 'smooth',
       });
-    }
-    else {
+    } else {
       container.scrollBy({
         top: 0,
         left: isRTL() === true ? 320 : -320,
@@ -27,8 +25,7 @@ export default class CartRecommendedProducts extends React.Component {
   };
 
   render() {
-    const recommended_products = this.props.recommended_products;
-    const sectionTitle = this.props.sectionTitle;
+    const { recommended_products, sectionTitle } = this.props;
 
     // If recommended products available.
     if (Object.keys(recommended_products).length > 0) {
@@ -36,19 +33,17 @@ export default class CartRecommendedProducts extends React.Component {
         <React.Fragment>
           <SectionTitle>{sectionTitle}</SectionTitle>
           <div className="spc-recommended-products">
-            <button className="nav-prev" onClick={() => {this.listHorizontalScroll('prev')}}/>
+            <button className="nav-prev" type="button" onClick={() => { this.listHorizontalScroll('prev'); }}/>
             <div className="block-content">
-              {Object.keys(recommended_products).map(function(key) {
-                return <RecommendedProduct key={key} item={recommended_products[key]}/>
+              { Object.keys(recommended_products).map(function(key) {
+                return <RecommendedProduct key={key} item={recommended_products[key]} />
               })}
             </div>
-            <button className="nav-next" onClick={() => {this.listHorizontalScroll('next')}}/>
+            <button className="nav-next" type="button" onClick={() => { this.listHorizontalScroll('next'); }} />
           </div>
         </React.Fragment>
       );
     }
-
     return (null);
   }
-
 }
