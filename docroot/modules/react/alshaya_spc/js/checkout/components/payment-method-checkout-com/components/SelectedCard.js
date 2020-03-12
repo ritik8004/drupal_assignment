c6fb8c1ec069ca24c459fdd79100ed7255e58430
Mitesh Patel <developermitesh@gmail.com>
2020-03-13 00:20:41 +0530

    CORE-15516: format code of SelectedCard component.

--- docroot/modules/react/alshaya_spc/js/checkout/components/payment-method-checkout-com/components/SelectedCard.js
+++ docroot/modules/react/alshaya_spc/js/checkout/components/payment-method-checkout-com/components/SelectedCard.js
@@ -2,36 +2,37 @@ import React from 'react';
 import ConditionalView from '../../../../common/components/conditional-view';
 import ToolTip from '../../../../utilities/tooltip';
 
-const cvvText = Drupal.t('This code is a three or four digit number printed on the front or back of the credit card');
-
 const SelectedCard = ({
   cardInfo, openStoreListModal, labelEffect, handleCardCvvChange, onExistingCardSelect, selected = true,
-}) => (
-  <div>
-    <span onClick={() => onExistingCardSelect(cardInfo.public_hash)}>
-      {Drupal.t('card no.') + ' ' + cardInfo.maskedCC}
-      {Drupal.t('expires') +  ' ' + cardInfo.expirationDate}
-    </span>
-    <div className="spc-add-new-address-btn" onClick={openStoreListModal}>
-      {Drupal.t('change')}
-    </div>
-    <ConditionalView condition={selected && cardInfo.mada === true}>
-      <div className="spc-type-textfield spc-type-cvv">
-        <input
-          type="tel"
-          id="spc-cc-cvv"
-          pattern="\d{3,4}"
-          required
-          onChange={handleCardCvvChange}
-          onBlur={(e) => labelEffect(e, 'blur')}
-        />
-        <div className="c-input__bar" />
-        <label htmlFor="spc-cc-cvv">{Drupal.t('CVV')}</label>
-        <div id="spc-cc-cvv-error" className="error" />
-        <ToolTip content={cvvText} enable question/>
+}) => {
+  const cvvText = Drupal.t('This code is a three or four digit number printed on the front or back of the credit card');
+  return (
+    <div>
+      <span onClick={() => onExistingCardSelect(cardInfo.public_hash)}>
+        {Drupal.t('card no.') + ' ' + cardInfo.maskedCC}
+        {Drupal.t('expires') +  ' ' + cardInfo.expirationDate}
+      </span>
+      <div className="spc-add-new-address-btn" onClick={openStoreListModal}>
+        {Drupal.t('change')}
       </div>
-    </ConditionalView>
-  </div>
-);
+      <ConditionalView condition={selected && cardInfo.mada === true}>
+        <div className="spc-type-textfield spc-type-cvv">
+          <input
+            type="tel"
+            id="spc-cc-cvv"
+            pattern="\d{3,4}"
+            required
+            onChange={handleCardCvvChange}
+            onBlur={(e) => labelEffect(e, 'blur')}
+          />
+          <div className="c-input__bar" />
+          <label htmlFor="spc-cc-cvv">{Drupal.t('CVV')}</label>
+          <div id="spc-cc-cvv-error" className="error" />
+          <ToolTip content={cvvText} enable question />
+        </div>
+      </ConditionalView>
+    </div>
+  );
+}
 
 export default SelectedCard;
