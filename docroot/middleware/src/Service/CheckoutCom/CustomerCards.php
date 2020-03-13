af875a7c2bb15cabaa76262e96f57c4f80e91c4e
Mitesh Patel <developermitesh@gmail.com>
2020-03-13 15:44:28 +0530

    CORE-15516: remove unnecessary code for customer cards.

--- docroot/middleware/src/Service/CheckoutCom/CustomerCards.php
+++ docroot/middleware/src/Service/CheckoutCom/CustomerCards.php
@@ -2,7 +2,6 @@
 
 namespace App\Service\CheckoutCom;
 
-use App\Service\Drupal\DrupalInfo;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -18,20 +17,6 @@ class CustomerCards {
   protected $helper;
 
   /**
-   * Service to get Drupal Info.
-   *
-   * @var \App\Service\Drupal\DrupalInfo
-   */
-  protected $drupalInfo;
-
-  /**
-   * Mada Validator.
-   *
-   * @var \App\Service\CheckoutCom\MadaValidator
-   */
-  protected $madaValidator;
-
-  /**
    * Logger service.
    *
    * @var \Psr\Log\LoggerInterface
@@ -39,41 +24,39 @@ class CustomerCards {
   protected $logger;
 
   /**
-   * Credit card type map.
-   *
-   * @var array
-   */
-  protected $ccTypesMap = [
-    'AE' => 'amex',
-    'VI' => 'visa',
-    'MC' => 'mastercard',
-    'DI' => 'discover',
-    'JCB' => 'jcb',
-    'DN' => 'dinersclub',
-  ];
-
-  /**
    * APIWrapper constructor.
    *
    * @param \App\Service\CheckoutCom\Helper $helper
    *   Checkout.com Helper.
-   * @param \App\Service\Drupal\DrupalInfo $drupal_info
-   *   Service to get Drupal Info.
-   * @param \App\Service\CheckoutCom\MadaValidator $mada_validator
-   *   Mada Validator.
    * @param \Psr\Log\LoggerInterface $logger
    *   Logger service.
    */
   public function __construct(
     Helper $helper,
-    DrupalInfo $drupal_info,
-    MadaValidator $mada_validator,
     LoggerInterface $logger
   ) {
     $this->helper = $helper;
-    $this->drupalInfo = $drupal_info;
-    $this->madaValidator = $mada_validator;
     $this->logger = $logger;
+  }
+
+  /**
+   * Get given card hash info for given customer.
+   *
+   * @param int $customer_id
+   *   The customer id.
+   * @param string $card_hash
+   *   The card hash.
+   *
+   * @return mixed|null
+   *   Return array of card data or null.
+   */
+  public function getGivenCardInfo(int $customer_id, string $card_hash) {
+    $cards = $this->getCustomerCards($customer_id);
+    $decode_hash = $this->deocodePublicHash($card_hash);
+    if (isset($cards[$decode_hash])) {
+      return $cards[$decode_hash];
+    }
+    return NULL;
   }
 
   /**
@@ -82,8 +65,7 @@ class CustomerCards {
    * @return array
    *   Return array of customer cards or empty array.
    */
-  public function getCustomerCards($customer_id) {
-
+  public function getCustomerCards(int $customer_id) {
     $response = $this->helper->getCustomerCards($customer_id);
 
     if (!empty($response) && isset($response['message'])) {
@@ -91,13 +73,9 @@ class CustomerCards {
       return [];
     }
 
-    $cards = !empty($response['items'])
+    return !empty($response['items'])
       ? $this->extractCardInfo($response['items'])
       : [];
-
-    // Sort cards by last saved first.
-    $cards = $this->sortCardsByDate($cards);
-    return $cards;
   }
 
   /**
@@ -117,28 +95,13 @@ class CustomerCards {
     $card_list = [];
     foreach ($cards as $card) {
       $token_details = json_decode($card['token_details'], TRUE);
-      $card['paymentMethod'] = $this->getCardType($token_details['type']);
       // @todo: Remove if we are already receiving mada:true/false.
       $token_details['mada'] = isset($token_details['mada']) && $token_details['mada'] == 'Y';
       // Encode public hash.
       // https://github.com/acquia-pso/alshaya/pull/13267#discussion_r311886591.
-      $card['public_hash'] = $this->encodePublicHash($card['public_hash']);
       $card_list[$card['public_hash']] = array_merge($card, $token_details);
     }
     return $card_list;
-  }
-
-  /**
-   * Get card type based on code.
-   *
-   * @param string $type
-   *   Card type code.
-   *
-   * @return string|null
-   *   Return card type name or null.
-   */
-  public function getCardType($type): ?string {
-    return $this->ccTypesMap[$type] ?? NULL;
   }
 
   /**
@@ -165,22 +128,6 @@ class CustomerCards {
    */
   public function deocodePublicHash(string $public_hash) {
     return base64_decode($public_hash);
-  }
-
-  /**
-   * Sort cards by last saved dates first.
-   *
-   * @param array $cards
-   *   The array of saved cards.
-   *
-   * @return array
-   *   Return sorted array of cards.
-   */
-  protected function sortCardsByDate(array $cards): array {
-    uasort($cards, function ($a, $b) {
-      return (strtotime($a['created_at']) > strtotime($b['created_at'])) ? -1 : 1;
-    });
-    return $cards;
   }
 
 }
