133e2a039ade07c013ea7ccb72e184d840135a1f
vivekpan27 <vivek.panicker@innoraft.com>
2020-03-13 13:34:26 +0000

    CORE-15437: Added customer/notCustom Algolia analytics tags to search params.

--- docroot/modules/react/alshaya_algolia_react/js/src/config/SearchClient.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/config/SearchClient.js
@@ -4,10 +4,14 @@ export const searchClient = algoliasearch(
   drupalSettings.algoliaSearch.application_id,
   drupalSettings.algoliaSearch.api_key
 );
-
 export const algoliaSearchClient = {
   search(requests) {
+
     if (window.algoliaSearchActivityStarted || requests[0].params.query.length > 0) {
+      requests[0].params['analyticsTags'] = drupalSettings.user.isCustomer === false
+        ? ['notCustomer']
+        : ['customer'];
+
       return searchClient.search(requests);
     }
 
