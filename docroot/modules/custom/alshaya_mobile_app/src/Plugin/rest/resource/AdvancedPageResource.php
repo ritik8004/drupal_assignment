dedc7d7839c1bf8e5a8f63d4bd1231f1252f4429
DELBOSC <sylvain.delbosc@acquia.com>
2020-03-16 13:49:19 +0100

    t push origin uat open https://github.com/acquia-pso/alshaya/compare/uat...sdelbosc:uat?pull_request[title]=Revert%20from%20UATRevert "Merge pull request #15190 from sonamsingh28/MCP-1650_Dynamic-way-get-Home-simple-page-URLs-uat"
    This reverts commit 79a6ac50b7de30bf42bd6799e2917d24350336d9, reversing
    changes made to a54e31b1105d1ed00117561983ebe4095ea6006b.

--- docroot/modules/custom/alshaya_mobile_app/src/Plugin/rest/resource/AdvancedPageResource.php
+++ docroot/modules/custom/alshaya_mobile_app/src/Plugin/rest/resource/AdvancedPageResource.php
@@ -126,9 +126,8 @@ class AdvancedPageResource extends ResourceBase {
    *   The response containing list of categories.
    */
   public function get() {
-    $page = $this->requestStack->query->get('page');
     // Path alias of advanced page.
-    $alias = $this->configFactory->get('alshaya_mobile_app.settings')->get('static_page_mappings.' . $page);
+    $alias = $this->requestStack->query->get('url');
     $node = $this->mobileAppUtility->getNodeFromAlias($alias, self::NODE_TYPE);
 
     if (!$node instanceof NodeInterface) {
@@ -203,7 +202,7 @@ class AdvancedPageResource extends ResourceBase {
     $response->addCacheableDependency(CacheableMetadata::createFromRenderArray([
       '#cache' => [
         'contexts' => [
-          'url.query_args:page',
+          'url.query_args:url',
         ],
         'tags' => array_merge([
           ProductCategoryTree::CACHE_TAG,
