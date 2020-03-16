4db4561f8f88ac22ab5ff37b0126eb5287916ed3
Rohit Joshi <joshi.rohit100@gmail.com>
2020-03-16 13:30:24 +0530

    CORE-14019: Adding more translations for cart page.

--- docroot/modules/react/alshaya_spc/js/utilities/total-line-items/index.js
+++ docroot/modules/react/alshaya_spc/js/utilities/total-line-items/index.js
@@ -52,7 +52,7 @@ class TotalLineItems extends React.Component {
           />
         </ConditionalView>
 
-        <TotalLineItem tooltip tooltipContent={discount_tooltip} name="discount-total" title={Drupal.t('discount')} value={this.props.totals.discount_amount} />
+        <TotalLineItem tooltip tooltipContent={discount_tooltip} name="discount-total" title={Drupal.t('Discount')} value={this.props.totals.discount_amount} />
 
         <div className="hero-total">
           <TotalLineItem name="grand-total" title={Drupal.t('Order Total')} value={this.props.totals.base_grand_total} />
