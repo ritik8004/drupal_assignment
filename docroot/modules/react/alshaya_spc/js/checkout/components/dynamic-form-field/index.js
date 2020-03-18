9e48e4732c24f559ea7f0002132d81b019dcfcc1
Srijeeta <srijeeta.ghosh@innoraft.com>
2020-03-18 06:36:58 +0000

    CORE-16215: Dynamic form field camel case changes.

--- docroot/modules/react/alshaya_spc/js/checkout/components/dynamic-form-field/index.js
+++ docroot/modules/react/alshaya_spc/js/checkout/components/dynamic-form-field/index.js
@@ -4,26 +4,26 @@ import AreaSelect from '../area-select';
 import ParentAreaSelect from '../parent-area-select';
 import TextField from '../../../utilities/textfield';
 
-export default class DynamicFormField extends React.Component {
-  render() {
-    let default_val = '';
-    if (this.props.default_val.length !== 0
-      && this.props.default_val.length !== 'undefined') {
-      default_val = this.props.default_val;
-    }
-
-    const {
-      field_key, field, area_list, areasUpdate,
-    } = this.props;
-    if (field_key === 'administrative_area') {
-      return <AreaSelect cityChanged={this.props.cityChanged} default_val={default_val} area_list={area_list} field_key={field_key} field={field} />;
-    }
-    if (field_key === 'area_parent') {
-      return <ParentAreaSelect default_val={default_val} field_key={field_key} field={field} areasUpdate={areasUpdate} />;
-    }
-
-    return (
-      <TextField isAddressField required={this.props.field.required} id={this.props.field_key} type="text" label={this.props.field.label} name={this.props.field_key} defaultValue={default_val !== '' ? default_val[this.props.field.key] : ''} />
-    );
+const DynamicFormField = (props) => {
+  let defaultVal = '';
+  if (this.props.default_val.length !== 0
+    && this.props.default_val.length !== 'undefined') {
+    defaultVal = this.props.default_val;
   }
-}
+
+  const {
+    field_key, field, area_list, areasUpdate,
+  } = this.props;
+  if (field_key === 'administrative_area') {
+    return <AreaSelect cityChanged={this.props.cityChanged} default_val={defaultVal} area_list={area_list} field_key={field_key} field={field} />;
+  }
+  if (field_key === 'area_parent') {
+    return <ParentAreaSelect default_val={defaultVal} field_key={field_key} field={field} areasUpdate={areasUpdate} />;
+  }
+
+  return (
+    <TextField isAddressField required={this.props.field.required} id={this.props.field_key} type="text" label={this.props.field.label} name={this.props.field_key} defaultValue={defaultVal !== '' ? defaultVal[this.props.field.key] : ''} />
+  );
+};
+
+export default DynamicFormField;
