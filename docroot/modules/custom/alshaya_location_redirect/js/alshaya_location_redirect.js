bb3e900900a1aebdcf1f12aca3acc03ce2836d92
Iain Potter <nospam@fore-front.co.uk>
2020-03-19 14:02:53 +0000

    CORE-16852: updates 'site' to 'brand' and 'market' to 'country'.

--- docroot/modules/custom/alshaya_location_redirect/js/alshaya_location_redirect.js
+++ docroot/modules/custom/alshaya_location_redirect/js/alshaya_location_redirect.js
@@ -1,6 +1,6 @@
 /**
  * @file
- * Detect user location, and potentially redirect to an appropriate market.
+ * Detect user location, and potentially redirect to an appropriate country.
  */
 
 (function ($, Drupal, drupalSettings) {
@@ -15,7 +15,7 @@
   // Check if user location already exists in an existing local storage item.
   var user_location = localStorage.getItem(storage_key);
 
-  // Mapping of sites to markets.
+  // Mapping of brands to countries..
   var mapping = {
     'bbw' : {
       'kw': 'https://www.bathandbodyworks.com.kw',
@@ -48,15 +48,15 @@
     },
   };
 
-  if (drupalSettings.alshaya_location_redirect.site) {
-    var site = drupalSettings.alshaya_location_redirect.site;
+  if (drupalSettings.alshaya_location_redirect.brand) {
+    var brand = drupalSettings.alshaya_location_redirect.brand;
   }
   else {
     return;
   }
 
-  if (drupalSettings.alshaya_location_redirect.market) {
-    var current_market = drupalSettings.alshaya_location_redirect.market;
+  if (drupalSettings.alshaya_location_redirect.country) {
+    var current_country = drupalSettings.alshaya_location_redirect.country;
   }
   else {
     return;
@@ -65,8 +65,8 @@
   // Determine if we already have a value for the user location.
   if (user_location) {
     // If the user location already exists, decide whether or not to redirect
-    // the user if that location doesn't match the current market.
-    if (user_location !== current_market) {
+    // the user if that location doesn't match the current country.
+    if (user_location !== current_country) {
       redirect = true;
     }
   }
@@ -74,28 +74,34 @@
     var worker_url = drupalSettings.alshaya_location_redirect.worker_url;
     user_location = 'xx';
 
-    // Call cloudflare worker to get user country.
-    $.getJSON(worker_url, function (data){
-      $.each( data, function( key, val ) {
-        user_location = val;
-      });
+    $.ajax({
+      type: 'GET',
+      url: worker_url,
+      dataType: 'json',
+      success: function(data) {
+        $.each( data, function(key, val) {
+          user_location = val;
+        });
+      },
+      data: {},
+      async: false
     });
 
     // Add the user location to local storage.
     localStorage.setItem(storage_key, user_location);
 
     if (user_location === 'xx') {
-      // Did not receive a valid market to redirect to.
+      // Did not receive a valid country to redirect to.
       return;
     }
 
-    if (user_location !== current_market) {
+    if (user_location !== current_country) {
       redirect = true;
     }
   }
 
-  if (redirect && mapping.hasOwnProperty(site) && mapping[site].hasOwnProperty(user_location)) {
-    var alternate_url = mapping[site][user_location];
+  if (redirect && mapping.hasOwnProperty(brand) && mapping[brand].hasOwnProperty(user_location)) {
+    var alternate_url = mapping[brand][user_location];
 
     // Perform the redirect.
     window.location = alternate_url;
