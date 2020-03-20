0e2c0603f56121bad547f08d7c5bd76e6be6527b
Srijeeta <srijeeta.ghosh@innoraft.com>
2020-03-20 14:10:10 +0000

    CORE-16215: Destructuring props assignments for mini cart components.

--- docroot/modules/react/alshaya_spc/js/minicart/components/mini-cart-content/index.js
+++ docroot/modules/react/alshaya_spc/js/minicart/components/mini-cart-content/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import PriceElement from '../../../utilities/special-price/PriceElement';
 
-class MiniCartContent extends React.Component {
-  render() {
-    return (
-      <>
-        <a className="cart-link-total" href={Drupal.url('cart')}>
-          <PriceElement amount={this.props.amount} />
-        </a>
-        <a className="cart-link" href={Drupal.url('cart')}>
-          <span className="quantity">{this.props.qty}</span>
-        </a>
-      </>
-    );
-  }
-}
+const MiniCartContent = (props) => {
+  const { amount, qty } = props;
+  return (
+    <>
+      <a className="cart-link-total" href={Drupal.url('cart')}>
+        <PriceElement amount={amount} />
+      </a>
+      <a className="cart-link" href={Drupal.url('cart')}>
+        <span className="quantity">{qty}</span>
+      </a>
+    </>
+  );
+};
 
 export default MiniCartContent;
