5812477ff791cc681500bf1d30b01c3b6d662f12
Srijeeta <srijeeta.ghosh@innoraft.com>
2020-03-20 06:30:38 +0000

    CORE-16215: Destructuring props assignement.

--- docroot/modules/react/alshaya_spc/js/cart/components/item-low-quantity/index.js
+++ docroot/modules/react/alshaya_spc/js/cart/components/item-low-quantity/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const ItemLowQuantity = (props) => {
-  if (props.in_stock && props.stock < props.qty) {
+  const {
+    in_stock: inStock,
+    stock,
+    qty,
+  } = props;
+  if (inStock && stock < qty) {
     return <div>{Drupal.t('This product is not available in selected quantity. Please adjust the quantity to proceed.')}</div>;
   }
 
