771c3bc6a78c5050ea556acfe8a985ca77e9a9fd
vivekpan27 <vivek.panicker@innoraft.com>
2020-03-24 07:24:24 +0000

    CORE-16224: Correct stock amount count on PLP.

--- docroot/modules/react/alshaya_algolia_react/js/src/components/panels/GridAndCount.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/components/panels/GridAndCount.js
@@ -3,7 +3,7 @@ import GridButtons from '../grid-buttons';
 
 export default function GridAndCount(props) {
   return (
-    <div className="block block-alshaya-search-api block-alshaya-grid-count-block">
+    <div className="block block-alshaya-grid-count-block">
       <div className="total-result-count">
         <div className="view-header search-count tablet">
           {props.children}
