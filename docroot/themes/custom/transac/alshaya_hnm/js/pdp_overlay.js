e3943c3aec10b5a294b7148cfc47d58ee5d48498
Mohammad Rashid <mohammad.rashid@alshaya.com>
2020-03-25 16:34:59 +0000

    CORE-16517: Adding js to show overlay in pdp.

---
+++ docroot/themes/custom/transac/alshaya_hnm/js/pdp_overlay.js
@@ -0,0 +1,32 @@
+/**
+ * @file
+ * Custom JS for HM brand to show attributes details in overlay.
+ */
+
+(function ($, Drupal) {
+  'use strict';
+
+  Drupal.behaviors.pdp_overlay = {
+    attach: function (context, settings) {
+      $('.popup-details').once('attribute-sliderbar').on('click', function () {
+        $('.attribute-sliderbar').addClass('attribute-sliderbar-active');
+        $('.attribute-sliderbar').css('visibility', 'visible');
+
+        if ($(window).width() > 1023) {
+          $('html').addClass('all-filters-overlay');
+        }
+        else {
+          $('body').addClass('mobile--overlay');
+        }
+      });
+
+      // close the 'details overlay'.
+      $('.attribute-sliderbar__close').once('attribute-sliderbar').on('click', function () {
+        $('.attribute-sliderbar').removeClass('attribute-sliderbar-active');
+        $('body').removeClass('mobile--overlay');
+        $('html').removeClass('all-filters-overlay');
+      });
+    }
+  };
+
+})(jQuery, Drupal);
