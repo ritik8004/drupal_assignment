e8ff66b7faca01a5a0f18a419c65e6fd1cf7f219
Rohit Joshi <joshi.rohit100@gmail.com>
2020-03-30 12:48:09 +0530

    CORE-17051: Using parse instead dangerous function.

--- docroot/modules/react/alshaya_spc/js/utilities/tooltip/index.js
+++ docroot/modules/react/alshaya_spc/js/utilities/tooltip/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import parse from 'html-react-parser';
 
 const ToolTip = ({ children, enable, question }) => {
   const iconClass = question === true ? ' question' : ' info';
   if (enable) {
     return (
       <div className={`tooltip-anchor${iconClass}`}>
-        <div
-          className="tooltip-box"
-          /* eslint-disable react/no-danger */
-          dangerouslySetInnerHTML={{ __html: children }}
-        />
+        <div className="tooltip-box">
+          {parse(children)}
+        </div>
       </div>
     );
   }
