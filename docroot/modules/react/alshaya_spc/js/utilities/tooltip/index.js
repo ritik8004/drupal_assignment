f0a8312e8823ba076c8e9a552383b228798f1e40
Rohit Joshi <joshi.rohit100@gmail.com>
2020-03-30 15:21:31 +0530

    CORE-17067: Handle tooltip different for string and object.

--- docroot/modules/react/alshaya_spc/js/utilities/tooltip/index.js
+++ docroot/modules/react/alshaya_spc/js/utilities/tooltip/index.js
@@ -4,10 +4,13 @@ import parse from 'html-react-parser';
 const ToolTip = ({ children, enable, question }) => {
   const iconClass = question === true ? ' question' : ' info';
   if (enable) {
+    const toolTip = (typeof children === 'string')
+      ? parse(children)
+      : children;
     return (
       <div className={`tooltip-anchor${iconClass}`}>
         <div className="tooltip-box">
-          {parse(children)}
+          {toolTip}
         </div>
       </div>
     );
