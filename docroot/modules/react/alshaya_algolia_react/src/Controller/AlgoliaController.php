08f26c396d8ecb9208effefa4e4459cc4d795c57
Sylvain DELBOSC <sylvain.delbosc@acquia.com>
2020-04-02 11:35:46 +0200

    Merge pull request #15611 from nikunjkotecha/MCP-2118
    MCP-2118: Add page to provide settings for Algolia in JSON.

--- docroot/modules/react/alshaya_algolia_react/src/Controller/AlgoliaController.php
+++ docroot/modules/react/alshaya_algolia_react/src/Controller/AlgoliaController.php
@@ -2,7 +2,11 @@
 
 namespace Drupal\alshaya_algolia_react\Controller;
 
+use Drupal\block\BlockViewBuilder;
+use Drupal\Core\Cache\CacheableJsonResponse;
+use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Customer controller to add front page.
@@ -27,6 +31,42 @@ class AlgoliaController extends ControllerBase {
         ],
       ],
     ];
+  }
+
+  /**
+   * Callback to get Algolia settings.
+   *
+   * @return \Drupal\Core\Cache\CacheableJsonResponse
+   *   Settings as JSON.
+   */
+  public function getSettings() {
+    /** @var \Drupal\block\BlockInterface $block */
+    $block = $this->entityTypeManager()->getStorage('block')->load('alshayaalgoliareactautocomplete');
+    if (empty($block)) {
+      throw new NotFoundHttpException();
+    }
+
+    /** @var \Drupal\block\BlockViewBuilder $builder */
+    $builder = $this->entityTypeManager()->getViewBuilder($block->getEntityTypeId());
+    $lazy_build = $builder->view($block);
+    $lazy_build['#block'] = $block;
+    $build = BlockViewBuilder::preRender($lazy_build);
+    $dependency = CacheableMetadata::createFromRenderArray($lazy_build);
+
+    $drupalSettings = $build['content']['#attached']['drupalSettings'];
+
+    $settings = [];
+    $settings['application_id'] = $drupalSettings['algoliaSearch']['application_id'];
+    $settings['api_key'] = $drupalSettings['algoliaSearch']['api_key'];
+    $settings['indexName'] = $drupalSettings['algoliaSearch']['indexName'];
+    $settings['filters'] = $drupalSettings['algoliaSearch']['filters'];
+    $settings['gallery']['showHoverImage'] = $drupalSettings['reactTeaserView']['gallery']['showHoverImage'];
+    $settings['gallery']['showThumbnails'] = $drupalSettings['reactTeaserView']['gallery']['showThumbnails'];
+    $settings['swatches'] = $drupalSettings['reactTeaserView']['swatches'];
+
+    $response = new CacheableJsonResponse($settings);
+    $response->addCacheableDependency($dependency);
+    return $response;
   }
 
 }
