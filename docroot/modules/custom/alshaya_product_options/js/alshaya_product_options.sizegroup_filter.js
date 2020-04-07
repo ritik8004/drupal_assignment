0d1044a97c16f0cba27b44afc074ef43811dc4a8
anjali vijay <anjali.vijay@axelerant.com>
2020-04-07 19:52:43 +0530

    CORE-12569: FE - Size Filter Grouped by Product Type Attribute on Search page and scss fixes on PDP page.

--- docroot/modules/custom/alshaya_product_options/js/alshaya_product_options.sizegroup_filter.js
+++ docroot/modules/custom/alshaya_product_options/js/alshaya_product_options.sizegroup_filter.js
@@ -16,26 +16,27 @@
       if(active_facet_sort_elements.length > 1) {
         for(var i = 0; i < active_facet_sort_elements.length ; i++) {
           // Normal execution in case of the facet block selector.
-          if($('.all-filters #' + active_facet_sort_elements[i]).hasClass('block-facets')) {
-            $('.all-filters #' + active_facet_sort_elements[i]).addClass('show-facet');  
+          if($('.all-filters #' + active_facet_sort_elements[i]).hasClass('block-facets')
+            || $('.all-filters-algolia #' + active_facet_sort_elements[i]).hasClass('c-facet')) {
+            $('.all-filters #'  + active_facet_sort_elements[i] + ', .all-filters-algolia #' + active_facet_sort_elements[i]).addClass('show-facet');
           } else { // Add class to the parent of selected children if the selector is not block facet.
-            $('.all-filters #block-plpsize >ul >li').hide();
-            $('.all-filters [id=' + active_facet_sort_elements[i] + ']').parent().addClass('show-facet');  
+            $('.all-filters #block-plpsize >ul >li, .all-filters-algolia #attr_size >ul >li').hide();
+            $('.all-filters [id=' + active_facet_sort_elements[i] + '], .all-filters-algolia [id=' + active_facet_sort_elements[i] + ']').parent().addClass('show-facet');
           }
         }
       }
     },
+
     attach: function (context, settings) {
       // Attach on attach behaviour call.
       Drupal.behaviors.sizegroupFilter.openSelectedSizeGroupFilter();
 
       // Override reopen facet logic.
-      $('.show-all-filters').once('facet-show-all-for-sizegroup-processed').on('click', function() {
+      $('.sticky-filter-wrapper').once('facet-show-all-for-sizegroup-processed').on('click', '.show-all-filters, .show-all-filters-algolia', function() {
         Drupal.behaviors.sizegroupFilter.openSelectedSizeGroupFilter();
       });
 
-
-      $('.sticky-filter-wrapper #block-plpsize ul li').once('show-size-filter').on('click tap', function (context) {
+      $('.sticky-filter-wrapper').once('product-option-show-size-filter').on('click tap', '#block-plpsize ul li, #attr_size ul li', function (context) {
         var $oldOpenedEl = $('.is-open', $(this).parent());
         if($(this).hasClass('is-open')) {
           $oldOpenedEl.removeClass('is-open');
@@ -46,44 +47,52 @@
         $(this).addClass('is-open');
       });
 
-      $('.all-filters #block-plpsize >ul >li').on('click', function(event) {
+      $('.all-filters, .all-filters-algolia').on('click', '#block-plpsize >ul >li, #attr_size >ul >li', function (event) {
         // Update the title on click of facet.
         var facet_title = $(event.target).text();
+
+        if($(event.target).closest('.sizegroup').length > 0) {
+          facet_title = $(event.target).closest('.sizegroup').parent().children('span').text();
+          $('.filter-sort-title').html(facet_title);
+
+          // Return for normal execution.
+          return;
+        }
 
         $('.filter-sort-title').html(facet_title);
         // Only show current facet and hide all others.
         $(this).removeClass('show-facet');
-        $('.all-filters #block-plpsize >ul >li').hide();
+        $('.all-filters #block-plpsize >ul >li, .all-filters-algolia #attr_size >ul >li').hide();
         $(this).addClass('show-facet');
         // Pass comma separated ids for the elements to make visible.
         // This is separate by , so that other JS logic bypasses it
         // and this will be handled by this behaviour specifically
         // See openSelectedSizeGroupFilter function.
-        $('#all-filter-active-facet-sort').val($(this).children('ul').attr('id') + ',' + $(this).closest('.block-facets').attr('id'));
+        $('#all-filter-active-facet-sort').val($(this).children('ul').attr('id') + ',' + $(this).closest('.c-facet').attr('id'));
 
         // Stop event bubbling and normal execution.
         event.stopPropagation();
         event.preventDefault();
       });
 
-      $('.facet-all-back').once('facet-all-back-for-sizegroup-processed').on('click', function() {
-        var $selectedGrandChild = $('.all-filters #block-plpsize >ul >li.show-facet');
+      $('.all-filters, .all-filters-algolia').once('facet-all-back-for-sizegroup-processed').on('click', '.facet-all-back, .back-facet-list', function() {
+        var $selectedGrandChild = $('.all-filters #block-plpsize >ul >li.show-facet, .all-filters-algolia #attr_size >ul >li.show-facet');
 
         // If any grand child is open go to this custom logic.
         if($selectedGrandChild.length) {
           // Reset what is done in the above click handler to reset the state.
-          $('.all-filters #block-plpsize >ul >li').show();
+          $('.all-filters #block-plpsize >ul >li, .all-filters-algolia #attr_size >ul >li').show();
           // Select it's parent (child of main grand parent) and trigger click on it.
           // This is done in order to mimmick the back state.
-          $selectedGrandChild.closest('.block-facets').find('.c-facet__title').trigger('click');
+          $selectedGrandChild.closest('.c-facet').find('.c-facet__title').trigger('click');
           // Reset the show facet class to reset to original state.
-          $('.all-filters #block-plpsize >ul >li.show-facet').removeClass('show-facet');
+          $('.all-filters #block-plpsize >ul >li.show-facet, .all-filters-algolia #attr_size >ul >li.show-facet').removeClass('show-facet');
         }
 
         // Stop event bubbling and normal execution.
         event.stopPropagation();
         event.preventDefault();
       });
-	  }
-	}
+    }
+  }
 })(jQuery, Drupal);
