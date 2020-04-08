b211ac14e27f57d719f755466229d20d9ce165cc
Brajendra Singh <brajendra.singh@pimcore.com>
2020-04-08 15:38:23 +0530

    CORE-12569: Fixes as PR feedback.

--- docroot/modules/react/alshaya_algolia_react/js/src/components/algolia/widgets/SizeGroupFilter.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/components/algolia/widgets/SizeGroupFilter.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import connectRefinementList from '../connectors/connectRefinementList';
-// import SwatchList from './SwatchList';
 
 // Creating size grouping filter.
-const SizeGroupFilter = ({ items, refine, searchForItems, isFromSearch, ...props }) => {
-  var searchForm = (null);
-  if (isFromSearch) {
-    searchForm = (
-      <li>
-        <input
-          type="search"
-          onChange={event => searchForItems(event.currentTarget.value)}
-        />
-      </li>
-    );
-  }
+const SizeGroupFilter = ({ items, refine, ...props }) => {
 
   if (typeof props.itemCount != 'undefined') {
     props.itemCount(props.attribute, items.length);
@@ -30,26 +18,28 @@ const SizeGroupFilter = ({ items, refine, searchForItems, isFromSearch, ...props
     groupedItems[item[0]].push(items[i]);
   }
 
+  console.log(items);
+  console.log(groupedItems);
+
   return (
     <ul>
-      {searchForm}
       {groupedItems.map((item, group) => {
         return (
           <li key={group}>
-            <span className="sizegroup-filter">{ group }</span>
-            <ul className="sizegroup" id={ group }>
+            <span className="sizegroup-filter">{group}</span>
+            <ul className="sizegroup" id={group}>
               {item.map(clild => {
                 return (
                   <li
-                    key={group + '-' + clild.label.split(":").pop()}
-                    className={"facet-item " + (clild.isRefined ? 'is-active' : '')}
+                    key={`$(group)-$(clild.label.split(":").pop())`}
+                    className={`facet-item $((clild.isRefined ? 'is-active' : ''))`}
                     datadrupalfacetlabel={props.name}
                     onClick={event => {
                       event.preventDefault();
                       refine(clild.label);
                     }}
                   >
-                    <span className="facet-item__value">{Drupal.t(clild.label.split(":").pop())}
+                    <span className="facet-item__value">{`$(clild.label.split(":").pop().trim())`}
                       <span className="facet-item__count">({clild.count})</span>
                     </span>
                   </li>
