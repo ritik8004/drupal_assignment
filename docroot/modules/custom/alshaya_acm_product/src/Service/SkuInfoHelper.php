1d73fdad3b9709eb56779a9ffaa5c18267c21460
vivekpan27 <vivek.panicker@innoraft.com>
2020-04-10 09:09:11 +0000

    CORE-16939: Optimized SkuInfoHelper::getAttributes().

--- docroot/modules/custom/alshaya_acm_product/src/Service/SkuInfoHelper.php
+++ docroot/modules/custom/alshaya_acm_product/src/Service/SkuInfoHelper.php
@@ -232,20 +232,18 @@ class SkuInfoHelper {
    *   Attributes.
    */
   public function getAttributes(SKUInterface $sku, array $unused_options = []): array {
-    $skuData = $sku->toArray();
-
+    $skuData = $sku->get('attributes')->getValue();
     $attributes = [];
-    foreach ($skuData['attributes'] as $row) {
+    foreach ($skuData as $row) {
       if (!empty($unused_options) && in_array($row['key'], $unused_options)) {
         continue;
       }
-
-      // Can not use data from $skuData['attributes'] as it is key_value
-      // field type, and value is varchar field with limit of 255, Which strips
-      // the text beyond the limit for description, and some of fields have key
-      // stored instead of value, value is saved in it's separate table.
-      if (isset($skuData["attr_{$row['key']}"])) {
-        $row['value'] = $sku->get("attr_{$row['key']}")->getString();
+      $field_name = "attr_{$row['key']}";
+      // This is done to fetch values of fields like entity reference fields.
+      // Otherwise we would only get raw values, like the reference ID in case
+      // of entity reference fields.
+      if ($sku->hasField($field_name)) {
+        $row['value'] = $sku->get($field_name)->getString();
       }
       // Remove un-wanted description key.
       unset($row['description']);
