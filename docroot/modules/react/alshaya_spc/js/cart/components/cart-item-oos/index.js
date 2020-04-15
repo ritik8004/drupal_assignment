aac49b684f0c9e865f5cd57d870656a45c877672
Mitesh Patel <developermitesh@gmail.com>
2020-04-15 13:33:15 +0530

    CORE-16195: CORE-17132: Revert memoization as getting warning for unique key for dev build.

--- docroot/modules/react/alshaya_spc/js/cart/components/cart-item-oos/index.js
+++ docroot/modules/react/alshaya_spc/js/cart/components/cart-item-oos/index.js
@@ -1,7 +1,5 @@
-import React from 'react';
-
 const CartItemOOS = ({ inStock }) => (
   (inStock !== true) ? Drupal.t('This product is out of stock. Please remove to proceed.') : null
 );
 
-export default React.memo(CartItemOOS);
+export default CartItemOOS;
