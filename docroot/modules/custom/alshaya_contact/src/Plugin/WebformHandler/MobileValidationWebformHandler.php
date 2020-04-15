0add204a1b04b581c500e2db4374c9732097dd6a
vagrant <vagrant@local.alshaya.com>
2020-04-15 05:58:35 +0000

    Solving PR comments.

--- docroot/modules/custom/alshaya_contact/src/Plugin/WebformHandler/MobileValidationWebformHandler.php
+++ docroot/modules/custom/alshaya_contact/src/Plugin/WebformHandler/MobileValidationWebformHandler.php
@@ -29,6 +29,8 @@ class MobileValidationWebformHandler extends WebformHandlerBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
     $webform_data = $webform_submission->getData();
+
+    // mobile_number_full only available on API. Validation only allow API.
     if (array_key_exists("mobile_number_full", $webform_data)) {
       $mobile_number = $webform_submission->getElementData('mobile_number_full');
       $preference_channel = $webform_submission->getElementData('select_your_preference_of_channel_of_communication');
