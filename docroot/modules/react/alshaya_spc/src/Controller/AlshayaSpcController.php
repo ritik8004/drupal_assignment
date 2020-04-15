8cbe71a9af0a201a368110f797bedee7177e10e5
Rohit Joshi <joshi.rohit100@gmail.com>
2020-04-15 16:57:42 +0530

    CORE-16774: Show notification for location outside country for HD.

--- docroot/modules/react/alshaya_spc/src/Controller/AlshayaSpcController.php
+++ docroot/modules/react/alshaya_spc/src/Controller/AlshayaSpcController.php
@@ -255,6 +255,11 @@ class AlshayaSpcController extends ControllerBase {
       'value' => $this->t('Access to your location access has been denied by your browser. You can reenable location services in your browser settings.'),
     ];
 
+    $strings[] = [
+      'key' => 'location_outside_country_hd',
+      'value' => '<span class="font-bold">' . $this->t('You are browsing outside @country', ['@country' => 'EG']) . '</span><br/>' . $this->t('We don\'t support delivery outside @country. Please enter an address with in country @country below to continue.', ['@country' => 'HH']),
+    ];
+
     $build = [
       '#theme' => 'spc_checkout',
       '#areas' => $areas,
