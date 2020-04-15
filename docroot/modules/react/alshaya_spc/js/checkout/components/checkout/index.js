99d8cdfba011e414c1482141e284318649e25092
nikunj <coder@nikunj.dev>
2020-04-15 16:58:42 +0530

    CORE-17445: Redirect to home page if cart empty.

--- docroot/modules/react/alshaya_spc/js/checkout/components/checkout/index.js
+++ docroot/modules/react/alshaya_spc/js/checkout/components/checkout/index.js
@@ -118,6 +118,8 @@ export default class Checkout extends React.Component {
             }
           });
         });
+      } else {
+        window.location = Drupal.url('');
       }
     } catch (error) {
       // In case of error, do nothing.
