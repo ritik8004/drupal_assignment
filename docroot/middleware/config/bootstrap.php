08823f85ab8d0f42e166325950919d509738de71
nikunj <coder@nikunj.dev>
2020-04-21 12:21:44 +0530

    CORE-17382: Revert changes for APP_ENV. As per symfony doc for PROD it does not try to write on runtime. For us whole code directory is readonly so we dont want code to write anything.

--- docroot/middleware/config/bootstrap.php
+++ docroot/middleware/config/bootstrap.php
@@ -22,6 +22,6 @@ if (file_exists($home . '/settings/.env')) {
 }
 
 $_SERVER += $_ENV;
-$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? NULL) ?: 'dev';
+$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? NULL) ?: 'prod';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
