ff3c20942f24fcee2cdec3135777cb43efed7733
Srijeeta <srijeeta.ghosh@innoraft.com>
2020-04-22 16:11:25 +0000

    CORE-17377: Reverted the code removed for impression_position.

--- docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.js
+++ docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.js
@@ -211,6 +211,49 @@
         });
       });
 
+      /**
+       * Impressions tracking on listing pages with Products.
+       */
+      if ((gtmPageType === 'product detail page') || (gtmPageType === 'cart page')) {
+        var count_pdp_items = 1;
+        if (!drupalSettings.hasOwnProperty('impressions_position')) {
+          drupalSettings.impressions_position = [];
+        }
+
+        productLinkSelector.each(function () {
+          // Fetch attributes for this product.
+          var impression = Drupal.alshaya_seo_gtm_get_product_values($(this));
+          // Keep variant empty for impression pages. Populated only post add to cart action.
+          impression.variant = '';
+
+          var pdpListName = '';
+          var upSellCrossSellSelector = $(this).closest('.view-product-slider').parent('.views-element-container').parent();
+          if (!$(this).closest('.owl-item').hasClass('cloned') && !upSellCrossSellSelector.hasClass('mobile-only-block')) {
+            // Check whether the product is in US or CS region & update list accordingly.
+            if (listName.indexOf('placeholder') > -1) {
+              if (upSellCrossSellSelector.hasClass('horizontal-crossell')) {
+                pdpListName = listName.replace('placeholder', 'CS');
+              }
+              else if (upSellCrossSellSelector.hasClass('horizontal-upell')) {
+                pdpListName = listName.replace('placeholder', 'US');
+              }
+              else if (upSellCrossSellSelector.hasClass('horizontal-related')) {
+                pdpListName = listName.replace('placeholder', 'RELATED');
+              }
+            }
+
+            impression.list = pdpListName;
+            impression.position = count_pdp_items;
+            impressions.push(impression);
+            drupalSettings.impressions_position[$(this).attr('data-nid') + '-' + pdpListName] = count_pdp_items;
+            count_pdp_items++;
+          }
+        });
+
+        Drupal.alshaya_seo_gtm_push_impressions(currencyCode, impressions);
+      }
+
+
       // If we receive an empty page type, set page type as not defined.
       if (gtmPageType === 'not defined') {
         if (document.location.pathname.indexOf('/' + drupalSettings.path.currentLanguage + '/user') == 0) {
@@ -673,6 +716,7 @@
           }
 
           currentListName = subListName;
+          position = drupalSettings.impressions_position[that.attr('data-nid') + '-' + subListName];
           Drupal.alshaya_seo_gtm_push_product_clicks(that, currencyCode, subListName, position);
         });
       });
