ec8f9b15b79eaf710b797d1f4cd5af66f53f517d
Sonam Singh <sonam.singh@innoraft.com>
2020-04-22 14:21:17 +0530

    CORE-17821: Add query to fetch all asset types.

--- docroot/modules/brands/alshaya_hm/alshaya_hm_images/src/EventSubscriber/ProductInfoRequestedEventSubscriber.php
+++ docroot/modules/brands/alshaya_hm/alshaya_hm_images/src/EventSubscriber/ProductInfoRequestedEventSubscriber.php
@@ -106,7 +106,7 @@ class ProductInfoRequestedEventSubscriber implements EventSubscriberInterface {
 
         $return = [];
         foreach ($media as $item) {
-          $asset_type = $this->skuManager->getAssetType($item);
+          $asset_type = $this->skuManager->getAssetType($item, $sku);
 
           switch ($asset_type) {
             case 'image':
