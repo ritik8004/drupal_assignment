f6149eb54bbfe771ad01562b527ff6d3fe2de140
vagrant <vagrant@local.alshaya.com>
2020-04-28 15:33:30 +0000

    MCP-3092: Merge parent and child on product carousels

--- docroot/modules/custom/alshaya_acm_product/src/Plugin/rest/resource/ProductLinkedSkusResource.php
+++ docroot/modules/custom/alshaya_acm_product/src/Plugin/rest/resource/ProductLinkedSkusResource.php
@@ -109,24 +109,42 @@ class ProductLinkedSkusResource extends ResourceBase {
    *   The response containing linked skus of the given sku.
    */
   public function get(string $sku) {
-    $skuEntity = SKU::loadFromSku($sku);
+    $skuIds[] = $sku;
 
-    // Check parameter is empty or not.
+    // Check parameter is equal to 1.
     if ($this->request->query->get('use_parent') == 1) {
       // Get parent SKU entity.
       $skuEntity = $this->sku_manager->getParentSkuBySku($sku);
-    }
-
-    if (!$skuEntity instanceof SKUInterface) {
-      throw new NotFoundHttpException($this->t("page not found"));
+      if (!$skuEntity instanceof SKUInterface) {
+        throw new NotFoundHttpException($this->t("page not found"));
+      }
+      $skuIds[] = $skuEntity->getSku();
     }
 
     $data = [];
-    foreach (AcqSkuLinkedSku::LINKED_SKU_TYPES as $linked_type) {
-      $data['linked'][] = [
-        'link_type' => $linked_type,
-        'skus' => $this->getLinkedSkus($skuEntity, $linked_type),
-      ];
+    foreach ($skuIds as $sku) {
+      $skuEntity = SKU::loadFromSku($sku);
+
+      if (!$skuEntity instanceof SKUInterface) {
+        throw new NotFoundHttpException($this->t("page not found"));
+      }
+
+      foreach (AcqSkuLinkedSku::LINKED_SKU_TYPES as $key => $linked_type) {
+        // Check equal to link_type value.
+        if ($data['linked'][$key]['link_type'] == $linked_type) {
+          $result = $this->getLinkedSkus($skuEntity, $linked_type);
+          // Check is not empty.
+          if (!empty($result)) {
+            // Array merge and remove duplicate array.
+            $data['linked'][$key]['skus'] = array_map("unserialize", array_unique(array_map("serialize", array_merge($data['linked'][$key]['skus'], $result))));
+          }
+          continue;
+        }
+        $data['linked'][] = [
+          'link_type' => $linked_type,
+          'skus' => $this->getLinkedSkus($skuEntity, $linked_type),
+        ];
+      }
     }
 
     $response = new ResourceResponse($data);
