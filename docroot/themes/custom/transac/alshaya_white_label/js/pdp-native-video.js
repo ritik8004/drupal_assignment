102f95f296a2445e82c9761c2f34252d1859b96a
Swarad <swarad07@gmail.com>
2020-04-28 11:12:26 +0530

    CORE-17822: Adding modal for video playing on mobile.

--- docroot/themes/custom/transac/alshaya_white_label/js/pdp-native-video.js
+++ docroot/themes/custom/transac/alshaya_white_label/js/pdp-native-video.js
@@ -10,6 +10,7 @@
    * Play/Pause videos.
    *
    * @param {*} element
+   * The video element.
    */
   function playPause(element) {
     if (element.get(0).paused) {
@@ -20,18 +21,40 @@
     }
   }
 
+  /**
+   * Make video play in fullscreen modal.
+   *
+   * @param {*} element
+   * The video element.
+   */
+  function playFullScreen(element) {
+    var parent = element.parent();
+    parent.addClass('go-fullscreen');
+  }
+
   Drupal.behaviors.pdpVideoPlayer = {
     attach: function (context) {
+      $('.pdp-video-close').once('video-fullscreen-control').on('click', function () {
+        playPause($(this).next());
+        $(this).parent().removeClass('go-fullscreen');
+      });
+
+      // Video player controls to play pause.
       $('video.gallery-video').once('video-load').on('click', function () {
         var videoElement = $(this);
         playPause(videoElement);
+        if ($(window).width() < 768) {
+          playFullScreen(videoElement);
+        }
       });
 
+      // When video ends or is paused.
       $('video.gallery-video').once('video-player-ended').on('ended pause', function () {
         $(this).removeClass('playing');
         $(this).removeClass('hide-controls');
       });
 
+      // When video is playng.
       $('video.gallery-video').once('video-player-playing').on('playing', function () {
         $(this).addClass('playing');
         $(this).addClass('hide-controls');
