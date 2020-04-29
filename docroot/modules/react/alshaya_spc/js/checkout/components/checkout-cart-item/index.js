8e6385365a7db9deb26b43a275aa17872d38ed8d
Rohit Joshi <joshi.rohit100@gmail.com>
2020-04-29 12:15:43 +0530

    CORE-17762: Handle checkout cart items block.

--- docroot/modules/react/alshaya_spc/js/checkout/components/checkout-cart-item/index.js
+++ docroot/modules/react/alshaya_spc/js/checkout/components/checkout-cart-item/index.js
@@ -5,51 +5,87 @@ import CheckoutConfigurableOption from '../../../utilities/checkout-configurable
 import SpecialPrice from '../../../utilities/special-price';
 import ConditionalView from '../../../common/components/conditional-view';
 
-const CheckoutCartItem = (props) => {
-  const {
-    item: {
-      id,
-      title,
-      relative_link: relativeLink,
-      configurable_values: configurableValues,
-      extra_data, // eslint-disable-line camelcase
-      original_price: originalPrice,
-      final_price: finalPrice,
-    },
-  } = props;
-
-  // @Todo: Handle this in CORE-17764.
-  if (relativeLink === undefined) {
-    return (null);
+export default class CheckoutCartItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      wait: true,
+      productInfo: null,
+    };
   }
 
-  return (
-    <div className="product-item">
-      <div className="spc-product-image">
-        <ConditionalView condition={extra_data.cart_image !== null}>
-          <CheckoutItemImage img_data={extra_data.cart_image} />
-        </ConditionalView>
-      </div>
-      <div className="spc-product-meta-data">
-        <div className="spc-product-title-price">
-          <div className="spc-product-title">
-            <ConditionalView condition={relativeLink.length > 0}>
-              <a href={relativeLink}>{title}</a>
-            </ConditionalView>
-            <ConditionalView condition={relativeLink.length === 0}>
-              {title}
-            </ConditionalView>
-          </div>
-          <div className="spc-product-price">
-            <SpecialPrice price={originalPrice} final_price={finalPrice} />
-          </div>
-        </div>
-        <div className="spc-product-attributes">
-          { configurableValues.map((key) => <CheckoutConfigurableOption key={`${key.label}-${id}`} label={key} />) }
-        </div>
-      </div>
-    </div>
-  );
-};
+  componentDidMount() {
+    const { item } = this.props;
+    // Key will be like 'product:en:testsku'
+    Drupal.alshayaSpc.getProductData(item.sku, this.productDataCallback);
+  }
 
-export default CheckoutCartItem;
+  /**
+   * Call back to get product data from storage.
+   */
+  productDataCallback = (productData) => {
+    // If sku info available.
+    if (productData !== null && productData.sku !== undefined) {
+      this.setState({
+        wait: false,
+        productInfo: productData,
+      });
+    }
+  };
+
+  render() {
+    const { wait } = this.state;
+    if (wait === true) {
+      return (null);
+    }
+
+    const {
+      item: {
+        id,
+        finalPrice,
+      },
+    } = this.props;
+
+    const {
+      productInfo: {
+        image,
+        options: configurableValues,
+        title,
+        url: relativeLink,
+        price: originalPrice,
+      },
+    } = this.state;
+
+    const cartImage = {
+      url: image,
+      alt: title,
+      title,
+    };
+
+    return (
+      <div className="product-item">
+        <div className="spc-product-image">
+          <CheckoutItemImage img_data={cartImage} />
+        </div>
+        <div className="spc-product-meta-data">
+          <div className="spc-product-title-price">
+            <div className="spc-product-title">
+              <ConditionalView condition={relativeLink.length > 0}>
+                <a href={relativeLink}>{title}</a>
+              </ConditionalView>
+              <ConditionalView condition={relativeLink.length === 0}>
+                {title}
+              </ConditionalView>
+            </div>
+            <div className="spc-product-price">
+              <SpecialPrice price={originalPrice} final_price={finalPrice} />
+            </div>
+          </div>
+          <div className="spc-product-attributes">
+            { configurableValues.map((key) => <CheckoutConfigurableOption key={`${key.label}-${id}`} label={key} />) }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
