a81ef63965886bd618a2ad67b99d8a1a3f1046fc
nikunj <coder@nikunj.dev>
2020-05-01 22:47:43 +0530

    CORE-18036: Add APIs to access cloudflare KV namespace data and update it.

---
+++ tests/apis/cloudflare/common.php
@@ -0,0 +1,9 @@
+<?php
+// @codingStandardsIgnoreFile
+
+require_once __DIR__ . '/settings.php';
+
+$namespace_identifier = 'f21217b14e244ad7ba54f11c6b70e087';
+$account_identifier = '712b5dd353dfc12aca7b342b28d5118f';
+
+$url = 'https://api.cloudflare.com/client/v4/';
