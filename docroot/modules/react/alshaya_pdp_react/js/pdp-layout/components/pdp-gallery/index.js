463e99e148388ad4aad3c811730412f5a2886e28
anjali vijay <anjali.vijay@axelerant.com>
2020-05-05 20:29:39 +0530

    CORE-17783: React slick added for image gallery on mobile.

--- docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-gallery/index.js
+++ docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-gallery/index.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import PdpAsset from '../pdp-asset';
+import Slider from 'react-slick';
 
 export default class PdpGallery extends React.Component {
   constructor(props) {
     super(props);
 
-    const { skuCode } = props;
-
-    this.images = skuCode ? drupalSettings.pdpGallery[skuCode]['#thumbnails'] : [];
-
   }
 
   render() {
-    const {images} = this;
+    const { skuCode } = this.props;
+    const images = skuCode ? drupalSettings.pdpGallery[skuCode]['#thumbnails'] : [];
+    const sliderSettings = {
+      dots: true,
+      infinite: false,
+      arrows: false,
+    };
 
     return (
+
       <div className="magv2-pdp-gallery">
-        <div className="magazine__gallery--container">
+        <div className="magazine__gallery--container-desktop">
           {images.map((image, key) => {
             return (
               <PdpAsset
@@ -29,6 +33,16 @@ export default class PdpGallery extends React.Component {
             />);
           })}
         </div>
+        <div className="magazine__gallery--container-mobile">
+          <Slider {...sliderSettings}>
+            {images.map((image, key) => {
+              return (
+                <img key={key} src={image.mediumurl} />
+              );
+            })}
+          </Slider>
+        </div>
+
       </div>
     );
   }
