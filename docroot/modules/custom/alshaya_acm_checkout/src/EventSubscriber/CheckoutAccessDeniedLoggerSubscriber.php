fe0c56804953ac088fb27cb9ec3db8208ccfd4f8
nikunj <coder@nikunj.dev>
2020-05-07 17:07:30 +0530

    CORE-17440: Add more logs with verbosity level.

--- docroot/modules/custom/alshaya_acm_checkout/src/EventSubscriber/CheckoutAccessDeniedLoggerSubscriber.php
+++ docroot/modules/custom/alshaya_acm_checkout/src/EventSubscriber/CheckoutAccessDeniedLoggerSubscriber.php
@@ -2,9 +2,11 @@
 
 namespace Drupal\alshaya_acm_checkout\EventSubscriber;
 
+use Drupal\acq_commerce\Response\NeedsRedirectException;
 use Drupal\Core\EventSubscriber\HttpExceptionSubscriberBase;
 use Drupal\Core\Logger\LoggerChannelTrait;
 use Drupal\Core\Routing\RouteMatchInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 /**
@@ -42,7 +44,7 @@ class CheckoutAccessDeniedLoggerSubscriber extends HttpExceptionSubscriberBase {
    * {@inheritdoc}
    */
   protected static function getPriority() {
-    return 10;
+    return 999;
   }
 
   /**
@@ -53,19 +55,45 @@ class CheckoutAccessDeniedLoggerSubscriber extends HttpExceptionSubscriberBase {
    */
   public function on403(GetResponseEvent $event) {
     if ($this->routeMatch->getRouteName() === 'acq_checkout.form') {
-      $exception = $event->getException();
+      $this->logDetails($event->getException(), '403');
+    }
+  }
 
-      try {
-        $this->getLogger()->warning('403 error was thrown from line @line on file @file', [
-          '@line' => $exception->getLine(),
-          '@file' => $exception->getFile(),
-        ]);
-      }
-      catch (\Throwable $e) {
-        $this->getLogger()->warning('403 error was thrown and failed to get line and file for type: @type', [
-          '@type' => get_class($exception),
-        ]);
-      }
+  /**
+   * {@inheritdoc}
+   */
+  public function onException(GetResponseForExceptionEvent $event) {
+    $exception = $event->getException();
+
+    if ($exception instanceof NeedsRedirectException) {
+      $this->logDetails($exception, '302');
+    }
+
+    parent::onException($event);
+  }
+
+  /**
+   * Log details for exception.
+   *
+   * @param \Exception $exception
+   *   Exception.
+   * @param string $error
+   *   Error identifier.
+   */
+  protected function logDetails(\Exception $exception, string $error) {
+    try {
+      $this->getLogger('CheckoutLogger')->warning('@error error was thrown from line @line on file @file', [
+        '@error' => $error,
+        '@line' => $exception->getLine(),
+        '@file' => $exception->getFile(),
+      ]);
+    }
+    catch (\Throwable $e) {
+      $this->getLogger('CheckoutLogger')->warning('@error error was thrown and failed to get line and file for type: @type. Message: @message', [
+        '@error' => $error,
+        '@type' => get_class($exception),
+        '@message' => $e->getMessage(),
+      ]);
     }
   }
 
