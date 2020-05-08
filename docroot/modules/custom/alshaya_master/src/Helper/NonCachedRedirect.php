19b0bb89eb24968fb1d4ddd694fac12aa25470c9
Rohit Joshi <joshi.rohit100@gmail.com>
2020-05-08 16:13:47 +0530

    Merge pull request #16226 from nikunjkotecha/CORE-17440
    [4.17.3] CORE-17440: Use non-cached redirect instead of access denied.

---
+++ docroot/modules/custom/alshaya_master/src/Helper/NonCachedRedirect.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\alshaya_master\Helper;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Cache\CacheableRedirectResponse;
+
+/**
+ * Class NonCachedRedirect.
+ *
+ * @package Drupal\alshaya_master\Helper
+ */
+class NonCachedRedirect {
+
+  /**
+   * Wrapper function to redirect the user.
+   *
+   * It does all the required operations and adds header to explicitly
+   * disable caching of the redirect.
+   *
+   * @param string $url
+   *   URL to redirect to.
+   */
+  public static function redirect(string $url) {
+    $response = new CacheableRedirectResponse($url);
+
+    $request = \Drupal::request();
+    // Save the session so things like messages get saved.
+    $request->getSession()->save();
+    $response->prepare($request);
+
+    // Make response non-cacheable.
+    $cacheable_metadata = new CacheableMetadata();
+    $cacheable_metadata->setCacheMaxAge(0);
+    $response->addCacheableDependency($cacheable_metadata);
+
+    // Make sure to trigger kernel events.
+    \Drupal::service('kernel')->terminate($request, $response);
+
+    $response->send();
+    exit;
+  }
+
+}
