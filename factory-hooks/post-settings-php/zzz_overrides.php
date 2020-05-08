d57e02b0a11e1d78b76d8aea872dddedd77cf590
nikunj <coder@nikunj.dev>
2020-05-08 14:15:11 +0530

    CORE-17440: Revert changes for stack level overrides.

--- factory-hooks/post-settings-php/zzz_overrides.php
+++ factory-hooks/post-settings-php/zzz_overrides.php
@@ -14,19 +14,15 @@ $country_code = substr($_acsf_site_name, -2);
 
 // Allow overriding settings and config to set secret info directly from
 // include files on server which can be per brand or brand country combination.
-$settings_path = $settings['server_home_dir'] . DIRECTORY_SEPARATOR . 'settings' . DIRECTORY_SEPARATOR . 'settings';
+$settings_path = $settings['server_home_dir'] . DIRECTORY_SEPARATOR . 'settings' . DIRECTORY_SEPARATOR . 'settings-';
 
-$stack_file = $settings_path . '.php';
-if (file_exists($stack_file)) {
-  include_once $stack_file;
-}
-
-$brand_country_file = $settings_path . '-' . $acsf_site_code . $country_code . '.php';
+$brand_country_file = $settings_path . $acsf_site_code . $country_code . '.php';
 if (file_exists($brand_country_file)) {
   include_once $brand_country_file;
 }
 
-$brand_file = $settings_path . '-' . $acsf_site_code . '.php';
+$brand_file = $settings_path . $acsf_site_code . '.php';
+
 if (file_exists($brand_file)) {
   include_once $brand_file;
 }
