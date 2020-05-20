22cc718adaa59dd49e6fc134854a8238ef1a29e8
Mitesh Patel <developermitesh@gmail.com>
2020-05-20 17:10:12 +0530

    CORE-19048: extract all pollyfills for ie11 in a common library.

--- docroot/modules/react/alshaya_spc/webpack.config.js
+++ docroot/modules/react/alshaya_spc/webpack.config.js
@@ -3,15 +3,6 @@ var buildPath = '/modules/react/alshaya_spc/dist/';
 
 var config = {
   entry: {
-    number: ["core-js/modules/es.number.is-integer", "core-js/modules/es.number.is-nan"],
-    promise: ["core-js/modules/es.promise", "core-js/modules/es.array.iterator"],
-    object: [
-      "core-js/modules/es.object.entries",
-      "core-js/modules/es.object.keys",
-      "core-js/modules/es.object.values",
-      "core-js/modules/es.object.assign",
-      "core-js/modules/es.array.for-each",
-    ],
     cart: './js/cart',
     minicart: './js/minicart',
     checkout: './js/checkout',
