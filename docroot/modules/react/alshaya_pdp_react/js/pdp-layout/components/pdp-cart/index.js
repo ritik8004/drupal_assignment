57b0fac703685967202531c69cca1e5e6b2a88dd
Srijeeta <srijeeta.ghosh@innoraft.com>
2020-05-28 05:27:42 +0000

    CORE-18721: Removed timeout for add to cart.

--- docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-cart/index.js
+++ docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-cart/index.js
@@ -10,123 +10,121 @@ import {
 class PdpCart extends React.Component {
   addToCart = (e) => {
     e.preventDefault();
-    setTimeout(() => {
-      const { configurableCombinations, skuCode, productInfo } = this.props;
-      const options = [];
-      let qty = 1;
-      if (configurableCombinations) {
-        const attributes = configurableCombinations[skuCode].configurables;
-        Object.keys(attributes).forEach((key) => {
-          const option = {
-            option_id: attributes[key].attribute_id,
-            option_value: document.getElementById(key).value,
-          };
+    const { configurableCombinations, skuCode, productInfo } = this.props;
+    const options = [];
+    let qty = 1;
+    if (configurableCombinations) {
+      const attributes = configurableCombinations[skuCode].configurables;
+      Object.keys(attributes).forEach((key) => {
+        const option = {
+          option_id: attributes[key].attribute_id,
+          option_value: document.getElementById(key).value,
+        };
 
-          // Skipping the psudo attributes.
-          if (drupalSettings.psudo_attribute === undefined
-            || drupalSettings.psudo_attribute !== option.option_id) {
-            options.push(option);
+        // Skipping the psudo attributes.
+        if (drupalSettings.psudo_attribute === undefined
+          || drupalSettings.psudo_attribute !== option.option_id) {
+          options.push(option);
+        }
+      });
+    }
+    const cartAction = 'add item';
+    const cartData = getCartData();
+    const cartId = (cartData) ? cartData.cart_id : null;
+    const variantSelected = document.getElementById('pdp-add-to-cart-form').getAttribute('variantselected');
+    qty = document.getElementById('qty') ? document.getElementById('qty').value : 1;
+
+    const postData = {
+      action: cartAction,
+      sku: variantSelected,
+      quantity: qty,
+      cart_id: cartId,
+      options,
+    };
+
+    const productData = {
+      quantity: qty,
+      parentSku: skuCode,
+      sku: variantSelected,
+      variant: variantSelected,
+    };
+
+    productData.productName = configurableCombinations
+      ? productInfo[skuCode].variants[variantSelected].cart_title
+      : productInfo[skuCode].cart_title;
+    productData.image = configurableCombinations
+      ? productInfo[skuCode].variants[variantSelected].cart_image
+      : productInfo[skuCode].cart_image;
+
+    const cartEndpoint = productInfo[skuCode].cart_update_endpoint;
+
+    updateCart(cartEndpoint, postData).then(
+      (response) => {
+        // If there any error we throw from middleware.
+        if (response.error === true) {
+          if (response.error_code === '400') {
+            clearCartData();
           }
-        });
-      }
-      const cartAction = 'add item';
-      const cartData = getCartData();
-      const cartId = (cartData) ? cartData.cart_id : null;
-      const variantSelected = document.getElementById('pdp-add-to-cart-form').getAttribute('variantselected');
-      qty = document.getElementById('qty') ? document.getElementById('qty').value : 1;
-
-      const postData = {
-        action: cartAction,
-        sku: variantSelected,
-        quantity: qty,
-        cart_id: cartId,
-        options,
-      };
-
-      const productData = {
-        quantity: qty,
-        parentSku: skuCode,
-        sku: variantSelected,
-        variant: variantSelected,
-      };
-
-      productData.productName = configurableCombinations
-        ? productInfo[skuCode].variants[variantSelected].cart_title
-        : productInfo[skuCode].cart_title;
-      productData.image = configurableCombinations
-        ? productInfo[skuCode].variants[variantSelected].cart_image
-        : productInfo[skuCode].cart_image;
-
-      const cartEndpoint = productInfo[skuCode].cart_update_endpoint;
-
-      updateCart(cartEndpoint, postData).then(
-        (response) => {
-          // If there any error we throw from middleware.
-          if (response.error === true) {
-            if (response.error_code === '400') {
-              clearCartData();
-            }
-          } else if (response.cart_id) {
-            if (response.response_message.status === 'success'
-                && (typeof response.items[productData.variant] !== 'undefined'
-                  || typeof response.items[productData.parentSku] !== 'undefined')) {
-              const cartItem = typeof response.items[productData.variant] !== 'undefined' ? response.items[productData.variant] : response.items[productData.parentSku];
-              productData.totalQty = cartItem.qty;
-            }
-
-            let configurables = [];
-            let productUrl = productInfo[skuCode].url;
-            let price = productInfo[skuCode].priceRaw;
-            let promotions = productInfo[skuCode].promotionsRaw;
-            let productDataSKU = productInfo[skuCode].sku;
-            let parentSKU = productInfo[skuCode].sku;
-            let { maxSaleQty } = productInfo[skuCode];
-            let maxSaleQtyParent = productInfo[skuCode].max_sale_qty_parent;
-            const gtmAttributes = productInfo[skuCode].gtm_attributes;
-
-            if (configurableCombinations) {
-              const productVariantInfo = productInfo[skuCode].variants[productData.variant];
-              productDataSKU = productData.variant;
-              price = productVariantInfo.priceRaw;
-              parentSKU = productVariantInfo.parent_sku;
-              promotions = productVariantInfo.promotionsRaw;
-              configurables = productVariantInfo.configurableOptions;
-              maxSaleQty = productVariantInfo.maxSaleQty;
-              maxSaleQtyParent = productVariantInfo.max_sale_qty_parent;
-
-              if (productVariantInfo.url !== undefined) {
-                const langcode = document.getElementsByTagName('html')[0].getAttribute('lang');
-                productUrl = productVariantInfo.url[langcode];
-              }
-            }
-
-            storeProductData({
-              sku: productDataSKU,
-              parentSKU,
-              title: productData.product_name,
-              url: productUrl,
-              image: productData.image,
-              price,
-              configurables,
-              promotions,
-              maxSaleQty,
-              maxSaleQtyParent,
-              gtmAttributes,
-            });
-
-            // Triggering event to notify react component.
-            const refreshMiniCartEvent = new CustomEvent('refreshMiniCart', { bubbles: true, detail: { data() { return response; }, productData } });
-            document.dispatchEvent(refreshMiniCartEvent);
-
-            const refreshCartEvent = new CustomEvent('refreshCart', { bubbles: true, detail: { data() { return response; } } });
-            document.dispatchEvent(refreshCartEvent);
+        } else if (response.cart_id) {
+          if (response.response_message.status === 'success'
+              && (typeof response.items[productData.variant] !== 'undefined'
+                || typeof response.items[productData.parentSku] !== 'undefined')) {
+            const cartItem = typeof response.items[productData.variant] !== 'undefined' ? response.items[productData.variant] : response.items[productData.parentSku];
+            productData.totalQty = cartItem.qty;
           }
-        },
-      )
-        .catch((error) => {
-          console.log(error.response);
-        });
-    }, 20);
+
+          let configurables = [];
+          let productUrl = productInfo[skuCode].url;
+          let price = productInfo[skuCode].priceRaw;
+          let promotions = productInfo[skuCode].promotionsRaw;
+          let productDataSKU = productInfo[skuCode].sku;
+          let parentSKU = productInfo[skuCode].sku;
+          let { maxSaleQty } = productInfo[skuCode];
+          let maxSaleQtyParent = productInfo[skuCode].max_sale_qty_parent;
+          const gtmAttributes = productInfo[skuCode].gtm_attributes;
+
+          if (configurableCombinations) {
+            const productVariantInfo = productInfo[skuCode].variants[productData.variant];
+            productDataSKU = productData.variant;
+            price = productVariantInfo.priceRaw;
+            parentSKU = productVariantInfo.parent_sku;
+            promotions = productVariantInfo.promotionsRaw;
+            configurables = productVariantInfo.configurableOptions;
+            maxSaleQty = productVariantInfo.maxSaleQty;
+            maxSaleQtyParent = productVariantInfo.max_sale_qty_parent;
+
+            if (productVariantInfo.url !== undefined) {
+              const langcode = document.getElementsByTagName('html')[0].getAttribute('lang');
+              productUrl = productVariantInfo.url[langcode];
+            }
+          }
+
+          storeProductData({
+            sku: productDataSKU,
+            parentSKU,
+            title: productData.product_name,
+            url: productUrl,
+            image: productData.image,
+            price,
+            configurables,
+            promotions,
+            maxSaleQty,
+            maxSaleQtyParent,
+            gtmAttributes,
+          });
+
+          // Triggering event to notify react component.
+          const refreshMiniCartEvent = new CustomEvent('refreshMiniCart', { bubbles: true, detail: { data() { return response; }, productData } });
+          document.dispatchEvent(refreshMiniCartEvent);
+
+          const refreshCartEvent = new CustomEvent('refreshCart', { bubbles: true, detail: { data() { return response; } } });
+          document.dispatchEvent(refreshCartEvent);
+        }
+      },
+    )
+      .catch((error) => {
+        console.log(error.response);
+      });
   }
 
   render() {
