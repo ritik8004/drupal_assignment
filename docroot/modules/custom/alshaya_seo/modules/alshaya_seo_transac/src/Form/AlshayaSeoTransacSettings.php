2111fe722b60bf7eab8b5dc9f913f5c2502558ec
vivekpan27 <vivek.panicker@innoraft.com>
2020-05-29 15:45:13 +0000

    CORE-17872: Added settings form and menu link for Alshaya Seo Transac module.

---
+++ docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/src/Form/AlshayaSeoTransacSettings.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\alshaya_seo_transac\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure Alshaya SEO Transac settings for this site.
+ */
+class AlshayaSeoTransacSettings extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'alshaya_seo_transac_alshaya_seo_transac_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['alshaya_seo_transac.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['product_impression_queue_size'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Product impression GTM event queue size'),
+      '#default_value' => $this->config('alshaya_seo_transac.settings')->get('product_impression_queue_size'),
+    ];
+    $form['product_impression_timer_time'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Product Impression GTM event timer time'),
+      // Convert time from milliseconds to seconds.
+      '#default_value' => ($this->config('alshaya_seo_transac.settings')->get('product_impression_timer_time') / 1000),
+      '#suffix' => 'seconds',
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if ($form_state->getValue('product_impression_queue_size') < 1) {
+      $form_state->setErrorByName('product_impression_queue_size', $this->t('Please enter a positive value for queue size.'));
+    }
+    if ($form_state->getValue('product_impression_timer_time') < 1) {
+      $form_state->setErrorByName('product_impression_timer_time', $this->t('Please enter a positive value for timer time.'));
+    }
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('alshaya_seo_transac.settings')
+      ->set('product_impression_queue_size', (int) $form_state->getValue('product_impression_queue_size'))
+      // Convert time from seconds to milliseconds.
+      ->set('product_impression_timer_time', ((int) $form_state->getValue('product_impression_timer_time') * 1000))
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
+
+}
