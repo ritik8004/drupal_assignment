93471396d3245ff397e10736e5edb55a2d984451
vivekpan27 <vivek.panicker@innoraft.com>
2020-06-01 13:27:02 +0000

    CORE-17872: Removed product slider changes as a separate ticket has been raised for the same.

--- docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.product_slider.js
+++ docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.product_slider.js
@@ -5,16 +5,14 @@
 
 (function ($, Drupal, debounce) {
   'use strict';
-  var listClassName = '';
-
   Drupal.behaviors.seoGoogleTagManagerProductSliderList = {
     attach: function (context, settings) {
       $(window).once('product-carousel-load-scroll').on('load scroll', debounce(function (event) {
-        Drupal.alshaya_seo_gtm_process_carousel(context, settings, event);
+        Drupal.alshaya_seo_gtm_process_carousel(context, settings);
       }, 500));
 
-      $(document).once('product-slider-prev-next').on('click', '.view-product-slider .slick-prev, .view-product-slider .slick-next', function (event) {
-        Drupal.alshaya_seo_gtm_process_carousel(context, settings, event);
+      $(document).once('product-slider-prev-next').on('click', '.view-product-slider .slick-prev, .view-product-slider .slick-next', function () {
+        Drupal.alshaya_seo_gtm_process_carousel(context, settings);
       });
     }
   };
@@ -26,10 +24,11 @@
    *
    * @param settings
    */
-  Drupal.alshaya_seo_gtm_process_carousel = function (context, settings, event) {
+  Drupal.alshaya_seo_gtm_process_carousel = function (context, settings) {
     var body = $('body');
     var gtmPageType = body.attr('gtm-container');
     var listName = body.attr('gtm-list-name');
+    var listClassName = '';
 
     if ((gtmPageType === 'product detail page') || (gtmPageType === 'cart page')) {
       // Check whether the product is in US or CS region & update list accordingly.
@@ -37,27 +36,27 @@
         if ($('div').hasClass('horizontal-crossell') && (!$(context).find('.horizontal-crossell').hasClass('mobile-only-block'))) {
           var crossellSelecter = $('.horizontal-crossell .view-product-slider');
           if ((!crossellSelecter.closest('.owl-item').hasClass('cloned'))) {
-            Drupal.alshaya_seo_gtm_prepare_and_push_product_impression(Drupal.alshaya_seo_gtm_push_carousel_product_impression, crossellSelecter, settings, event);
+            Drupal.alshaya_seo_gtm_prepare_and_push_carousel_product_impression(crossellSelecter, settings, 'horizontal-crossell');
           }
         }
 
         if ($('div').hasClass('horizontal-upell') && (!$(context).find('.horizontal-upell').hasClass('mobile-only-block'))) {
           var upellSelecter = $('.horizontal-upell .view-product-slider');
           if (!upellSelecter.closest('.owl-item').hasClass('cloned')) {
-            Drupal.alshaya_seo_gtm_prepare_and_push_product_impression(Drupal.alshaya_seo_gtm_push_carousel_product_impression, upellSelecter, settings, event);
+            Drupal.alshaya_seo_gtm_prepare_and_push_carousel_product_impression(upellSelecter, settings, 'horizontal-upell');
           }
         }
 
         if ($('div').hasClass('horizontal-related') && !$(context).find('.horizontal-related').hasClass('mobile-only-block')) {
           var relatedSelecter = $('.horizontal-related .view-product-slider');
           if (!relatedSelecter.closest('.owl-item').hasClass('cloned')) {
-            Drupal.alshaya_seo_gtm_prepare_and_push_product_impression(Drupal.alshaya_seo_gtm_push_carousel_product_impression, relatedSelecter, settings, event);
+            Drupal.alshaya_seo_gtm_prepare_and_push_carousel_product_impression(relatedSelecter, settings, 'horizontal-related');
           }
         }
       }
     }
     else {
-      Drupal.alshaya_seo_gtm_prepare_and_push_product_impression(Drupal.alshaya_seo_gtm_push_carousel_product_impression, $('.view-product-slider'), settings, event);
+      Drupal.alshaya_seo_gtm_prepare_and_push_carousel_product_impression($('.view-product-slider'), settings, listClassName);
     }
   }
 
@@ -68,7 +67,7 @@
    *
    * @param settings
    */
-  Drupal.alshaya_seo_gtm_push_carousel_product_impression = function (context, eventType, currentQueueSize) {
+  Drupal.alshaya_seo_gtm_prepare_and_push_carousel_product_impression = function (context, settings, listClassName) {
     var impressions = [];
     var body = $('body');
     var currencyCode = body.attr('gtm-currency');
@@ -110,9 +109,10 @@
           count++;
         }
       });
+      if (impressions.length > 0) {
+        Drupal.alshaya_seo_gtm_push_impressions(currencyCode, impressions);
+      }
     }
-
-    return impressions;
   };
 
 })(jQuery, Drupal, Drupal.debounce);
