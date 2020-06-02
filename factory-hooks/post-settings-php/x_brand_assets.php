97e5c48c243418b4ff5ecf8d6fb2a88c36474953
Sonam Singh <sonam.singh@innoraft.com>
2020-06-02 13:04:09 +0530

    CORE-18517: Add comment for s3fs configs.

--- factory-hooks/post-settings-php/x_brand_assets.php
+++ factory-hooks/post-settings-php/x_brand_assets.php
@@ -17,5 +17,7 @@ $settings['alshaya_brand_shared_folder'] = 'sites/g/files/' . $acsf_site_code;
 
 $settings['media_download_timeout_video'] = 200;
 
+// s3fs module will remove the config field for access_key and secret_key
+// from '8.x-3.0-beta1' and read from settings so details are added here.
 $settings['s3fs.access_key'] = 'AKIARGKNHDDL5AKRBP7S';
 $settings['s3fs.secret_key'] = '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';
