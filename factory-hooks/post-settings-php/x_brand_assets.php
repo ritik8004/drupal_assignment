4b6017c9f6c810c4b04eacaafaa4fcb3e806cf55
Sonam Singh <sonam.singh@innoraft.com>
2020-06-04 16:15:56 +0530

    CORE-18517: Reset s3fs settings on reset and move config to settings file.

--- factory-hooks/post-settings-php/x_brand_assets.php
+++ factory-hooks/post-settings-php/x_brand_assets.php
@@ -21,3 +21,31 @@ $settings['media_download_timeout_video'] = 200;
 // from '8.x-3.0-beta1' and read from settings so details are added here.
 $settings['s3fs.access_key'] = 'AKIARGKNHDDL5AKRBP7S';
 $settings['s3fs.secret_key'] = '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';
+
+$env = alshaya_get_site_environment();
+// This is to remove `01/02` etc from env name.
+if (substr($env, 0, 1) == '0') {
+  $env = substr($env, 2);
+}
+
+switch ($env) {
+  case 'uat':
+    $bucket = 'als-ecom-drupal-stopgap-uat-s3';
+    break;
+
+  case 'dev':
+  case 'dev2':
+  case 'dev3':
+  case 'qa2':
+  case 'test':
+  case 'local':
+    $bucket = 'als-ecom-drupal-stopgap-dev-s3';
+    break;
+
+  default:
+    $bucket = '';
+}
+
+// Setting s3fs bucket and root folder.
+$settings['s3fs.settings']['bucket'] = $bucket;
+$settings['s3fs.settings']['root_folder'] = $env . '/' . $acsf_site_code . '/shared';
