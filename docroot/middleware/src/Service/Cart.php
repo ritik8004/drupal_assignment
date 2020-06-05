65571939a11a5ce54dfa252e3794db795e07ce44
Mitesh Patel <developermitesh@gmail.com>
2020-06-05 13:49:16 +0530

    CORE-18624: move sorting helper method to a class as static method.

--- docroot/middleware/src/Service/Cart.php
+++ docroot/middleware/src/Service/Cart.php
@@ -12,6 +12,7 @@ use App\Service\Magento\CartActions;
 use App\Service\CheckoutCom\CustomerCards;
 use Drupal\alshaya_spc\Helper\SecureText;
 use Psr\Log\LoggerInterface;
+use Drupal\alshaya_master\helper\SortUtility;
 
 /**
  * Class Cart.
@@ -1319,7 +1320,7 @@ class Cart {
       }
 
       // Sort the stores first by distance and then by name.
-      $this->sortStores($stores, 'rnc_available', 'desc', 'distance', 'asc');
+      SortUtility::sortByMultipleKey($stores, 'rnc_available', 'desc', 'distance', 'asc');
       return $stores;
     }
     catch (\Exception $e) {
@@ -1328,63 +1329,6 @@ class Cart {
         '@cart_id' => $cart_id,
         '@response' => $e->getMessage(),
       ]);
-    }
-  }
-
-  /**
-   * Sort an array based on a specific key.
-   *
-   * @param array $array
-   *   Array to sort.
-   * @param string $key
-   *   Key of the array or object to sort with.
-   * @param string $order
-   *   Sorting order asc/desc.
-   * @param string $key2
-   *   Optional secondary key for sorting when first key has equal value.
-   * @param string $order2
-   *   Sorting order asc/desc.
-   */
-  protected function sortStores(array &$array, $key, $order = 'desc', $key2 = NULL, $order2 = 'asc') {
-    usort($array, function ($item_1, $item_2) use ($key, $order, $key2) {
-      $val1 = is_array($item_1) ? $item_1[$key] : $item_1->$key;
-      $val2 = is_array($item_2) ? $item_2[$key] : $item_2->$key;
-
-      $order = $this->sortCheckOrders($val1, $val2, $order);
-
-      // Add check for secondary key.
-      if ($key2 && $order === 0) {
-        $val3 = is_array($item_1) ? $item_1[$key2] : $item_1->$key2;
-        $val4 = is_array($item_2) ? $item_2[$key2] : $item_2->$key2;
-        return $this->sortCheckOrders($val3, $val4, $order);
-      }
-
-      return $order;
-    });
-  }
-
-  /**
-   * Helper function to get order between two values.
-   *
-   * @param mixed $val1
-   *   First value.
-   * @param mixed $val2
-   *   Second value.
-   * @param string $order
-   *   Ascending or descending.
-   *
-   * @return int
-   *   Returns the order value - 0, 1 or -1.
-   */
-  protected function sortCheckOrders($val1, $val2, $order) {
-    if ($val1 == $val2) {
-      return 0;
-    }
-    if ($order == 'asc') {
-      return ($val1 < $val2) ? -1 : 1;
-    }
-    elseif ($order == 'desc') {
-      return ($val1 > $val2) ? -1 : 1;
     }
   }
 
