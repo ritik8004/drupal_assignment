decb50ae28b980ee13750a5f8d95d6d7c4e46629
vivekpan27 <vivek.panicker@innoraft.com>
2020-06-08 19:39:18 +0000

    CORE-17756: Enabled super category optional filters for top suggestions on mobile.

--- docroot/modules/react/alshaya_algolia_react/js/src/App.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/App.js
@@ -59,6 +59,22 @@ class App extends React.PureComponent {
       ? (<SearchResults query={query} />)
       : '';
 
+    function getSuperCategory() {
+      let activeMenuItem = document.querySelector('.main--menu .menu--one__link.active');
+      if (activeMenuItem !== null) {
+        return activeMenuItem.getAttribute('data-super-category-label');
+      }
+      return null;
+    }
+    // Uses the Algolia optionalFilters feature.
+    // Super Category is currently the only optional filter in use.
+    // We want to promote the products belonging to current page super category
+    // to the top of the search results.
+    let supercategory = getSuperCategory();
+    let optionalFilters = drupalSettings.superCategory && supercategory
+      ? `${drupalSettings.superCategory.search_facet}:${supercategory}`
+      : null
+
     return (
       <div>
         <InstantSearch indexName={ `${drupalSettings.algoliaSearch.indexName}_query` } searchClient={algoliaSearchClient}>
@@ -73,6 +89,7 @@ class App extends React.PureComponent {
           <Portal id="top-results" conditional query={query}>
             <span className="top-suggestions-title">{Drupal.t('top suggestions')}</span>
             <InstantSearch indexName={drupalSettings.algoliaSearch.indexName} searchClient={algoliaSearchClient}>
+              {optionalFilters ? <Configure optionalFilters={optionalFilters} /> : null}
               <Configure hitsPerPage={drupalSettings.autocomplete.hits} query={query}/>
               <Hits hitComponent={Teaser}/>
             </InstantSearch>
