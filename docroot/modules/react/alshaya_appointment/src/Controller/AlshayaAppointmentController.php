d3a4f90d21172495207f3f2b87520686d192b57d
Sonam Singh <sonam.singh@innoraft.com>
2020-06-16 19:39:05 +0530

    CORE-18012: Add steps wizard component.

--- docroot/modules/react/alshaya_appointment/src/Controller/AlshayaAppointmentController.php
+++ docroot/modules/react/alshaya_appointment/src/Controller/AlshayaAppointmentController.php
@@ -18,12 +18,41 @@ class AlshayaAppointmentController extends ControllerBase {
    *   Return array of markup with react lib attached.
    */
   public function appointment() {
+    $steps = [
+      0 => [
+        'step' => 1,
+        'stepTitle' => $this->t('appointment type'),
+      ],
+      1 => [
+        'step' => 2,
+        'stepTitle' => $this->t('select store'),
+      ],
+      2 => [
+        'step' => 3,
+        'stepTitle' => $this->t('select time slot'),
+      ],
+      3 => [
+        'step' => 4,
+        'stepTitle' => $this->t('login / guest'),
+      ],
+      4 => [
+        'step' => 5,
+        'stepTitle' => $this->t('customer details'),
+      ],
+      5 => [
+        'step' => 6,
+        'stepTitle' => $this->t('confirmation'),
+      ],
+    ];
     return [
       '#type' => 'markup',
       '#markup' => '<div id="appointment-booking"></div>',
       '#attached' => [
         'library' => [
           'alshaya_appointment/alshaya_appointment',
+        ],
+        'drupalSettings' => [
+          'step_labels' => $steps,
         ],
       ],
     ];
