640afd11f51b2da3717896ab78b02e5151e290a5
vivekpan27 <vivek.panicker@innoraft.com>
2020-06-17 11:52:13 +0000

    CORE-19430: Added google social auth settings for local.

--- factory-hooks/environments/settings.php
+++ factory-hooks/environments/settings.php
@@ -293,6 +293,16 @@ function alshaya_get_additional_settings($site_code, $country_code, $env) {
         ],
       ],
     ],
+    'pbk' => [
+      'default' => [
+        'default' => [
+          'social_auth_google.settings' => [
+            'client_id' => '702222333032-0htvmbqgnfjh8a7l0mashbnpmbnua446.apps.googleusercontent.com',
+            'client_secret' => 'aJhYUR-qa9yRWUUj8L_wNK8k',
+          ],
+        ],
+      ],
+    ],
     'default' => [
       'default' => [
         'default' => [
