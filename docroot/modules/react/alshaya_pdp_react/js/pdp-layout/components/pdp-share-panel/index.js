b60a497f1d29d118a6b7ed6fecd02f7ac548e8d0
anjali vijay <anjali.vijay@axelerant.com>
2020-06-25 11:06:30 +0530

    CORE-19666: Share panel theming on new pdp.

--- docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-share-panel/index.js
+++ docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-share-panel/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import parse from 'html-react-parser';
 import CopyPageLink from './copy-page-link';
+import PdpSectionTitle from '../utilities/pdp-section-title';
 
 const PdpSharePanel = () => {
   const { sharethis } = drupalSettings;
 
+  const showSharePanel = () => {
+    document.querySelector('.magv2-pdp-share-wrapper').classList.toggle('show-share-panel');
+  }
+
   return (
-    <>
+    <div className="magv2-pdp-share-wrapper card">
+      <div className="magv2-share-title-wrapper">
+        <PdpSectionTitle>
+          {Drupal.t('Share this page')}
+        </PdpSectionTitle>
+        <div className="magv2-accordion" onClick={() => showSharePanel()} />
+      </div>
       <div className="pdp-share-panel">
         <div className="sharethis-wrapper">
           {parse(sharethis.content)}
         </div>
         <CopyPageLink />
       </div>
-    </>
+    </div>
   );
 };
 
