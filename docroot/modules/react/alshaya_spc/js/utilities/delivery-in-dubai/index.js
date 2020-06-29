65a698cb9f21e240fa8236b33dc811301a93dad6
Sanjib-Dutta <sanjib.dutta@alshaya.com>
2020-06-29 11:30:02 +0300

    [5.2.0]CORE-21506: pdp, basket and checkout is complete. left with address field delivery in Dubai

--- docroot/modules/react/alshaya_spc/js/utilities/delivery-in-dubai/index.js
+++ docroot/modules/react/alshaya_spc/js/utilities/delivery-in-dubai/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DeliveryInDubai = () => {
   const { delivery_in_dubai_text: DeliveryInDubaiText } = window.drupalSettings.alshaya_spc;
   if (DeliveryInDubaiText !== undefined) {
-    return <span className="delivery-in-dubai-text fadeInUp" style={{ animationDelay: '0.4s' }}>{DeliveryInDubaiText}</span>;
+    return <div className="delivery-in-dubai-text fadeInUp" style={{ animationDelay: '0.4s' }}>{DeliveryInDubaiText}</div>;
   }
 
   return (null);
