13409221009eac8ef649d9c1bf2186648161aeb3
anjali vijay <anjali.vijay@axelerant.com>
2020-07-01 17:26:04 +0530

    CORE-21538: Product swatches theming.

--- docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-cart/swatch-select-option/index.js
+++ docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-cart/swatch-select-option/index.js
@@ -1,50 +1,15 @@
 import React from 'react';
-import AvailableSelectOptions from '../available-select-options';
-import DefaultSelectOptions from '../default-select-options';
 
-const SwatchSelectOption = (props) => {
-  const {
-    configurables,
-    code,
-    nextCode,
-    nextValues,
-    handleLiClick,
-    selected,
-  } = props;
+const SwatchSelectOption = ({ configurables, code }) => (
+  <div className="non-groupped-attr">
+    <ul id={code} className="select-attribute">
+      {Object.keys(configurables.values).map((attr) => (
+        <li key={attr}>
+          <a href="#" style={{ backgroundImage: `url(${configurables.values[attr].swatch_image})` }} />
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
-  return (
-    <>
-      <div className="non-groupped-attr">
-        <ul id={code} className="select-attribute">
-          {Object.keys(configurables.values).map((attr) => {
-            if (code === nextCode) {
-              return (
-                <AvailableSelectOptions
-                  nextValues={nextValues}
-                  attr={attr}
-                  value={configurables.values[attr].swatch_image}
-                  key={attr}
-                  selected={selected}
-                  code={code}
-                  handleLiClick={handleLiClick}
-                />
-              );
-            }
-            // Show the default options.
-            return (
-              <DefaultSelectOptions
-                attr={attr}
-                value={configurables.values[attr].swatch_image}
-                key={attr}
-                selected={selected}
-                code={code}
-                handleLiClick={handleLiClick}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    </>
-  );
-};
 export default SwatchSelectOption;
