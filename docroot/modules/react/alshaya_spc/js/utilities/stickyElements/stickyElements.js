d09e616ea5b2312505a724e3e43b0d36e0271e3c
Swarad <swarad07@gmail.com>
2020-07-02 17:47:53 +0530

    CORE-21552: Fixing offset for VS SPC cart preview post minimalistic header.

--- docroot/modules/react/alshaya_spc/js/utilities/stickyElements/stickyElements.js
+++ docroot/modules/react/alshaya_spc/js/utilities/stickyElements/stickyElements.js
@@ -60,7 +60,7 @@ function stickyMobileCartPreview() {
   const superMenu = document.getElementById('block-supermenu');
 
   // Check branding menu height.
-  const brandingMenuHeight = document.getElementsByClassName('branding__menu')[0].offsetHeight || 0;
+  const brandingMenuHeight = document.getElementsByClassName('branding__menu')[0].offsetHeight;
 
   let menuHeight = 0;
 
@@ -76,7 +76,9 @@ function stickyMobileCartPreview() {
 
   // Menu offset.
   if (superCategoryMenu && superCategoryMenu.offsetHeight) {
-    menuHeight = superCategoryMenu.offsetHeight + document.getElementById('block-mobilenavigation').offsetHeight;
+    // In super category menu, we allow the super category menu to scroll after
+    // minimalistic header, hence factor only menu nav bar height.
+    menuHeight = document.getElementById('block-mobilenavigation').offsetHeight;
   } else if (superMenu && superMenu.offsetHeight) {
     menuHeight = superMenu.offsetHeight + brandingMenuHeight;
   } else {
@@ -87,7 +89,6 @@ function stickyMobileCartPreview() {
     // Mobile cart sticky header.
     if (window.innerWidth < 768) {
       const cartOffsetTop = menuHeight + breadCrumbHeight + preContentHeight - cartPreviewOffset;
-
       if (window.pageYOffset > cartOffsetTop) {
         if (!cartPreview[0].classList.contains('sticky')) {
           cartPreview[0].classList.add('sticky');
