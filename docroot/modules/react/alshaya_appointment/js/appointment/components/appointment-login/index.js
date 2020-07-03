7ab817e20f16b4976160df3d7910359639251c77
Avinash Shukla <avinash.shukla@acquia.com>
2020-07-03 11:33:32 +0530

    CORE-18017: Fixed redirects after login.

--- docroot/modules/react/alshaya_appointment/js/appointment/components/appointment-login/index.js
+++ docroot/modules/react/alshaya_appointment/js/appointment/components/appointment-login/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import {setStorageInfo} from "../../../utilities/storage";
 
 export default class AppointmentLogin extends React.Component {
   constructor(props) {
     super(props);
+  }
 
+  handleBack = (step) => {
+    const { handleBack } = this.props;
+    handleBack(step);
+  }
+
+  handleSubmit = () => {
+    setStorageInfo(this.state);
+    const { handleSubmit } = this.props;
+    handleSubmit();
   }
 
   render() {
+
+    const social_login_enabled = drupalSettings.alshaya_appointment.social_login_enabled;
+
     return (
       <div className="appointment-loginn-wrapper">
         <div className="appointment-with-account">
@@ -19,15 +33,75 @@ export default class AppointmentLogin extends React.Component {
               { Drupal.t('Sign in') }
             </a>
           </div>
+          { social_login_enabled &&
           <div className="appointment-social-login">
             <p>{ Drupal.t('Or Sign in with') }</p>
+            <div>
+              <div className="alshaya-social">
+                <a className="social_auth_facebook auth-link" href="#"
+                   social-auth-link="/en/user/login/facebook">
+                  <span
+                    className="social-network-text">sign up with Facebook</span>
+                </a>
+              </div>
+              <div className="alshaya-social">
+                <a className="social_auth_google auth-link" href="#"
+                   social-auth-link="/en/user/login/google">
+                  <span
+                    className="social-network-text">sign up with Google</span>
+                </a>
+              </div>
+            </div>
           </div>
+          }
         </div>
         <div className="appointment-without-account">
           <div>
             <h2>{ Drupal.t('I don\'t have an account') }</h2>
             <p>{ Drupal.t('Log in for faster booking and to manage your appointments online') }</p>
           </div>
+          <div>
+            <a href="/user/register?destination=/appointment/booking">
+              { Drupal.t('Register') }
+            </a>
+            <button
+              className="appointment-store-button select-store"
+              type="button"
+              onClick={() => this.handleSubmit}
+            >
+              {Drupal.t('Continue as Guest')}
+            </button>
+          </div>
+          { social_login_enabled &&
+          <div className="appointment-social-login">
+            <p>{ Drupal.t('Or Sign in with') }</p>
+            <div>
+              <div className="alshaya-social">
+                <a className="social_auth_facebook auth-link" href="#"
+                   social-auth-link="/en/user/login/facebook">
+                  <span
+                    className="social-network-text">sign up with Facebook</span>
+                </a>
+              </div>
+              <div className="alshaya-social">
+                <a className="social_auth_google auth-link" href="#"
+                   social-auth-link="/en/user/login/google">
+                  <span
+                    className="social-network-text">sign up with Google</span>
+                </a>
+              </div>
+            </div>
+          </div>
+          }
+        </div>
+        <div>
+          <button
+            className="appointment-store-button back"
+            type="button"
+            onClick={() => this.handleBack('select-time-slot')}
+          >
+            {Drupal.t('BACK')}
+          </button>
         </div>
       </div>
     );
