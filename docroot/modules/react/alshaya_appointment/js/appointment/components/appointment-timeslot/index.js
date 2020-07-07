115c88535e3f42c687a40143b5d4e7b04c974e41
anjali vijay <anjali.vijay@axelerant.com>
2020-07-07 16:40:16 +0530

    CORE-18027: Booking wizard time slot theming.

--- docroot/modules/react/alshaya_appointment/js/appointment/components/appointment-timeslot/index.js
+++ docroot/modules/react/alshaya_appointment/js/appointment/components/appointment-timeslot/index.js
@@ -84,9 +84,6 @@ export default class AppointmentTimeSlot extends React.Component {
 
   render() {
     const { date, timeSlots } = this.state;
-    const arg = {
-      '!date': moment().format('dddd DD MMMM'),
-    };
     return (
       <div className="appointment-store-wrapper">
         <div className="appointment-store-inner-wrapper">
@@ -96,9 +93,10 @@ export default class AppointmentTimeSlot extends React.Component {
             *
           </div>
           <div className="timeslot-latest-available">
-            <p>
-              {Drupal.t('The first available appointment is on !date', arg)}
-            </p>
+            <span>
+              {Drupal.t('The first available appointment is on ')}
+            </span>
+            <span className="starting-timeslot">{Drupal.t(moment().format('dddd DD MMMM'))}</span>
           </div>
           <div className="appointment-datepicker">
             <AppointmentCalendar
