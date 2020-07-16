c8d20bd0ecf9cc4de90080b93fa661c60de717ee
vagrant <vagrant@local.alshaya.com>
2020-07-16 06:57:48 +0000

    CORE-19331: Added image urls for banner and thumbanil fields.

--- docroot/modules/custom/alshaya_mobile_app/src/Service/MobileAppUtilityParagraphs.php
+++ docroot/modules/custom/alshaya_mobile_app/src/Service/MobileAppUtilityParagraphs.php
@@ -612,6 +612,13 @@ class MobileAppUtilityParagraphs extends MobileAppUtility {
     foreach ($paragraph_fields as $field_name) {
       if (empty($row = $this->processParagraphReferenceField($entity, $field_name))) {
         $row = $entity->get($field_name)->getValue();
+        if ($field_name == 'field_banner' || $field_name == 'thumbnail') {
+          if (!empty($row)) {
+            $image_file = $this->fileStorage->load($row[0]['target_id']);
+            $image_url = file_create_url($image_file->getFileUri());
+            $row[0]['url'] = $image_url;
+          }
+        }
       }
       $data[$field_name] = $row;
     }
