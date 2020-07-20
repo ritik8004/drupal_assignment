e786414d4400ff9c9d483e5c84fd73279865196c
Sanket Jain <sanketjain@Sankets-MacBook-Pro.local>
2020-07-20 18:31:45 +0530

    CORE-19331: Updated code as per feedback.

--- docroot/modules/custom/alshaya_mobile_app/src/Service/MobileAppUtilityParagraphs.php
+++ docroot/modules/custom/alshaya_mobile_app/src/Service/MobileAppUtilityParagraphs.php
@@ -24,7 +24,7 @@ use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\redirect\RedirectRepository;
 use Drupal\acq_commerce\Conductor\APIWrapper;
 use Drupal\Core\Block\BlockManagerInterface;
-use Drupal\file\FileInterface;
+use Drupal\file\Entity\File;
 use Drupal\Core\Entity\EntityInterface;
 
 /**
@@ -621,7 +621,7 @@ class MobileAppUtilityParagraphs extends MobileAppUtility {
         if ($field_name == 'field_banner' || $field_name == 'thumbnail') {
           if (!empty($row)) {
             $image_file = $this->fileStorage->load($row[0]['target_id']);
-            if ($image_file instanceof FileInterface) {
+            if ($image_file instanceof File) {
               $row[0]['url'] = file_create_url($image_file->getFileUri());
             }
           }
