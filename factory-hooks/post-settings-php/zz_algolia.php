df380d5f94ba030ddecd72f5401295e399b3f587
vivekpan27 <vivek.panicker@innoraft.com>
2020-07-24 07:29:01 +0000

    CORE-22563: Configured Algolia to use new PBK dev application settings.

--- factory-hooks/post-settings-php/zz_algolia.php
+++ factory-hooks/post-settings-php/zz_algolia.php
@@ -9,6 +9,7 @@
  */
 
 global $_acsf_site_name;
+global $acsf_site_code;
 
 $algolia_env = $settings['env'];
 
@@ -38,8 +39,24 @@ $config['search_api.index.acquia_search_index']['options']['algolia_index_name']
 // This will need to be overridden in brand specific settings files on each
 // env using prod app for each brand.
 if (!in_array($algolia_env, ['01test', '01uat', '01pprod', '01live'])) {
-  $config['search_api.server.algolia']['backend_config']['application_id'] = 'testing24192T8KHZ';
-  $config['search_api.server.algolia']['backend_config']['api_key'] = '628e74a9b6f3817cdd868278c8b8656e';
-  $config['block.block.alshayaalgoliareactautocomplete']['settings']['application_id'] = 'testing24192T8KHZ';
-  $config['block.block.alshayaalgoliareactautocomplete']['settings']['search_api_key'] = 'afeb84ab13757e11fbe8765142e2d7ad';
+  // Default values of old sandbox.
+  $algolia_settings = [
+    'app_id' => 'testing24192T8KHZ',
+    'write_api_key' => '1a3473b08a7e58f0b808fe4266e08187',
+    'search_api_key' => '950ad607b0d79914702c82849af9a63f',
+  ];
+
+  // Here we add the brand specific overrides.
+  switch ($acsf_site_code) {
+    case 'pbk':
+      $algolia_settings = [
+        'app_id' => 'KBYTOTQY6T',
+        'write_api_key' => 'bc6a377733b1f8812c094d709580faa6',
+        'search_api_key' => '3f0b012a52119eb8e95b7ec359d3e881',
+      ];
+  }
+  $config['search_api.server.algolia']['backend_config']['application_id'] = $algolia_settings['app_id'];
+  $config['search_api.server.algolia']['backend_config']['api_key'] = $algolia_settings['write_api_key'];
+  $config['block.block.alshayaalgoliareactautocomplete']['settings']['application_id'] = $algolia_settings['app_id'];
+  $config['block.block.alshayaalgoliareactautocomplete']['settings']['search_api_key'] = $algolia_settings['search_api_key'];
 }
