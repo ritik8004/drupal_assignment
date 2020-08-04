b9536732e65f46c08fcbb211865acda944e7594e
vivekpan27 <vivek.panicker@innoraft.com>
2020-08-04 13:10:47 +0000

    CORE-22563: Fetched algolia settings from factory hooks settings file.

--- factory-hooks/post-settings-php/zz_algolia.php
+++ factory-hooks/post-settings-php/zz_algolia.php
@@ -38,44 +38,8 @@ $config['search_api.index.acquia_search_index']['options']['algolia_index_name']
 // This will need to be overridden in brand specific settings files on each
 // env using prod app for each brand.
 if (!in_array($algolia_env, ['01test', '01uat', '01pprod', '01live'])) {
-  global $acsf_site_code;
-
-  /**
-   * Return the Algolia keys for the specified brand.
-   *
-   * To note: These keys are for the Sanbox(dev) app for each brand.
-   * For the test/uat/pprod/live envs, we use the prod Application and we add
-   * the settings for those applications on ~/settings/settings-brandcode.php
-   * on the server for each brand.
-   *
-   * @param string $site_code
-   *   The site code. Eg: pbk, hm, mc, etc.
-   *
-   * @return array
-   *   The Algolia app keys for the brand.
-   */
-  function alshaya_algolia_get_sandbox_brand_key_mapping(string $site_code) {
-    // @todo Remove the 'default' once when sandbox app keys have been added for
-    // all the brands.
-    $mapping = [
-      'default' => [
-        'app_id' => 'testing24192T8KHZ',
-        'write_api_key' => '1a3473b08a7e58f0b808fe4266e08187',
-        'search_api_key' => '950ad607b0d79914702c82849af9a63f',
-      ],
-      'pbk' => [
-        'app_id' => 'KBYTOTQY6T',
-        'write_api_key' => 'bc6a377733b1f8812c094d709580faa6',
-        'search_api_key' => '3f0b012a52119eb8e95b7ec359d3e881',
-      ],
-    ];
-
-    return $mapping[$site_code] ?? $mapping['default'];
-  }
-
-  $algolia_settings = alshaya_algolia_get_sandbox_brand_key_mapping($acsf_site_code);
-  $config['search_api.server.algolia']['backend_config']['application_id'] = $algolia_settings['app_id'];
-  $config['search_api.server.algolia']['backend_config']['api_key'] = $algolia_settings['write_api_key'];
-  $config['block.block.alshayaalgoliareactautocomplete']['settings']['application_id'] = $algolia_settings['app_id'];
-  $config['block.block.alshayaalgoliareactautocomplete']['settings']['search_api_key'] = $algolia_settings['search_api_key'];
+  $config['search_api.server.algolia']['backend_config']['application_id'] = $settings['algolia_sandbox.settings']['app_id'];
+  $config['search_api.server.algolia']['backend_config']['api_key'] = $settings['algolia_sandbox.settings']['write_api_key'];
+  $config['block.block.alshayaalgoliareactautocomplete']['settings']['application_id'] = $settings['algolia_sandbox.settings']['app_id'];
+  $config['block.block.alshayaalgoliareactautocomplete']['settings']['search_api_key'] = $settings['algolia_sandbox.settings']['search_api_key'];
 }
