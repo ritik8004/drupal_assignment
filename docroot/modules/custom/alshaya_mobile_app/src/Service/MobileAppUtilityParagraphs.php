656fe62ee947213770cedf3f02778f83b89d56b9
Nikunj Kotecha <coder@nikunj.dev>
2020-08-04 09:53:22 +0000

    Merge pull request #17393 from sanket-jain10/CORE-22685
    [5.2.0] CORE-22685: Added parent entity type to paragraph entities in api.

--- docroot/modules/custom/alshaya_mobile_app/src/Service/MobileAppUtilityParagraphs.php
+++ docroot/modules/custom/alshaya_mobile_app/src/Service/MobileAppUtilityParagraphs.php
@@ -611,6 +611,12 @@ class MobileAppUtilityParagraphs extends MobileAppUtility {
       ? $entity->bundle()
       : $entity->getEntityTypeId(),
     ];
+
+    // Get parent entity types for paragraph entities.
+    if ($entity->getEntityTypeId() == 'paragraph') {
+      $data['parent_type'] = $entity->getParentEntity()->bundle();
+    }
+
     // Get configured fields of entity, we don't require base fields.
     $paragraph_fields = $this->getConfiguredFields($entity);
     foreach ($paragraph_fields as $field_name) {
