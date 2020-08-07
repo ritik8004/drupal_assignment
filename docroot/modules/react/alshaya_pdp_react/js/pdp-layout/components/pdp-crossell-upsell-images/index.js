8e0348b9b287a928e6ddb678d37d335fc0d7f566
Swarad <swarad07@gmail.com>
2020-08-07 12:21:03 +0530

    CORE-22982: Using native image lazyloading instead of react package.

--- docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-crossell-upsell-images/index.js
+++ docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-crossell-upsell-images/index.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import LazyLoad from 'react-lazy-load';
 import PdpInfo from '../pdp-info';
 
 const PdpCrossellUpsellImage = ({
   imageUrl, alt, title, pdpProductPrice, finalPrice, productUrl,
 }) => (
   <a className="magv2-pdp-crossell-upsell-image-wrapper" href={productUrl}>
-    <LazyLoad
-      debounce={false}
-      throttle={250}
-      offsetTop={0}
-    >
-      <img
-        src={imageUrl}
-        alt={alt}
-        title={title}
-      />
-    </LazyLoad>
+    <img
+      src={imageUrl}
+      alt={alt}
+      title={title}
+      loading="lazy"
+    />
     <PdpInfo
       title={title}
       finalPrice={finalPrice}
