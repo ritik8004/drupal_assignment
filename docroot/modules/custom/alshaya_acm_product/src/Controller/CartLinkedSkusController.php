abd8ce9355bd2ed7802f5bdeada87e0e407f3e33
vivekpan27 <vivek.panicker@innoraft.com>
2020-08-11 14:42:26 +0000

    CORE-22531: Removed code which fetches data from localstorage to get the parent.

--- docroot/modules/custom/alshaya_acm_product/src/Controller/CartLinkedSkusController.php
+++ docroot/modules/custom/alshaya_acm_product/src/Controller/CartLinkedSkusController.php
@@ -110,7 +110,7 @@ class CartLinkedSkusController extends ControllerBase {
           if ($sku_entity->bundle() === 'simple') {
             $sku_entity = $this->skuManager->getParentSkuBySku($sku_entity);
             if (($sku_entity instanceof SKUInterface)
-              && (!in_array($sku_entity->get('sku')->getString(), $querySkus))) {
+              && (!in_array($sku_entity->getSku(), $querySkus))) {
               $cache_tags = array_merge($cache_tags, $sku_entity->getCacheTags());
               $cross_sell_skus += $this->skuManager->getLinkedSkus($sku_entity, $queryParams['type']);
             }
