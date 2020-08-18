b8a3229c7f96054db1370f0f7496db8c37755026
Srijeeta <srijeeta.ghosh@innoraft.com>
2020-08-18 08:01:05 +0000

    CORE-22733: Product labels and promotion on cs/us section.

--- docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-crossell-upsell-images/index.js
+++ docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-crossell-upsell-images/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import PdpInfo from '../pdp-info';
 
 const PdpCrossellUpsellImage = ({
-  imageUrl, alt, title, pdpProductPrice, finalPrice, productUrl,
+  imageUrl, alt, title, pdpProductPrice, finalPrice, productUrl, productLabels, productPromotions,
 }) => (
   <a className="magv2-pdp-crossell-upsell-image-wrapper" href={productUrl}>
+    {productLabels ? (
+      <div className="product-labels">
+        {Object.keys(productLabels).map((key) => (
+          <div className={`labels ${productLabels[key].position}`}>
+            <img
+              src={productLabels[key].image.url}
+              alt={productLabels[key].image.alt}
+              title={productLabels[key].image.title}
+            />
+          </div>
+        ))}
+      </div>
+    ) : null}
     <div className="magv2-pdp-crossell-upsell-img">
       <img
         src={imageUrl}
@@ -13,6 +26,13 @@ const PdpCrossellUpsellImage = ({
         loading="lazy"
       />
     </div>
+    {productPromotions ? (
+      <div className="promo-labels">
+        {Object.keys(productPromotions).map((key) => (
+          <p><a href={productPromotions[key].promo_web_url}>{productPromotions[key].text}</a></p>
+        ))}
+      </div>
+    ) : null}
     <PdpInfo
       title={title}
       finalPrice={finalPrice}
