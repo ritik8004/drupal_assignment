0dfd767833a4570460a4bc5b7eee5b27b2507c36
anjali vijay <anjali.vijay@axelerant.com>
2020-08-19 18:33:41 +0530

    CORE-23295: Sticky header visibility fixes.

--- docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-cart/index.js
+++ docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-cart/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import ConfigurableProductForm from './configurable-product-form';
 import SimpleProductForm from './simple-product-form';
 
 const PdpCart = ({
   configurableCombinations, skuCode, productInfo, pdpRefresh, childRef, pdpLabelRefresh,
 }) => {
+  const wrapper = useRef();
+
+  useEffect(() => {
+    if (childRef) {
+      childRef(wrapper);
+    }
+  },
+  []);
+
   if (configurableCombinations) {
     return (
-      <div className="pdp-cart-form fadeInUp notInMobile" style={{ animationDelay: '0.6s' }} ref={childRef}>
+      <div className="pdp-cart-form fadeInUp notInMobile" style={{ animationDelay: '0.6s' }} ref={wrapper}>
         <ConfigurableProductForm
           configurableCombinations={configurableCombinations}
           skuCode={skuCode}
