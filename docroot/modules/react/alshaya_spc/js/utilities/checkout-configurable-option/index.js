fab2a6f8f19a59923eeae8035c2102b99a149471
Sonam Singh <sonam.singh@innoraft.com>
2020-08-28 01:06:22 +0530

    CORE-22708: Add code to hide lpn attribute from FE.

--- docroot/modules/react/alshaya_spc/js/utilities/checkout-configurable-option/index.js
+++ docroot/modules/react/alshaya_spc/js/utilities/checkout-configurable-option/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 const CheckoutConfigurableOption = (props) => {
   const { label } = props;
+  if (drupalSettings.lpn.hide_lpn_attribute && label.attribute_id === `attr_${drupalSettings.lpn.lpn_attribute}`) {
+    return null;
+  }
 
   return (
     <>
