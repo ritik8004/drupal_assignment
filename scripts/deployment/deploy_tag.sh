73ad867ff527de6562d373eb56775f45deca813c
nikunj <coder@nikunj.dev>
2020-09-07 17:06:07 +0530

    CORE-4541: Validate tag and output in console too.

--- scripts/deployment/deploy_tag.sh
+++ scripts/deployment/deploy_tag.sh
@@ -7,13 +7,13 @@
 
 tag="$1"
 mode="$2"
+stack=`whoami`
+repo="$stack@svn-25.enterprise-g1.hosting.acquia.com:$stack.git"
 
 log_file=/var/log/sites/${AH_SITE_NAME}/logs/$(hostname -s)/alshaya-deployments.log
 
-echo "Starting deployment for tag $tag in mode $mode" &>> ${log_file}
-
-stack=`whoami`
-repo="$stack@svn-25.enterprise-g1.hosting.acquia.com:$stack.git"
+echo "Starting deployment in mode $mode. Tag $tag, Stack $stack" &>> ${log_file}
+echo "Starting deployment in mode $mode. Tag $tag, Stack $stack"
 
 backup_directory="~/$AH_SITE_ENVIRONMENT/backup/pre-$tag"
 directory="~/$AH_SITE_ENVIRONMENT/repo"
@@ -21,7 +21,8 @@ directory="~/$AH_SITE_ENVIRONMENT/repo"
 # Create folder and clone if not available
 if [ ! -d "$directory" ]
 then
-  echo "Repo directory not available, creating and cloning." &>> ${log_file}
+  echo "Repo directory not available, creating and cloning. Tag $tag, Stack $stack" &>> ${log_file}
+  echo "Repo directory not available, creating and cloning. Tag $tag, Stack $stack"
   mkdir $directory
   cd $directory
   git clone $repo
@@ -39,8 +40,14 @@ git branch -D main
 # Fetch all tags.
 git fetch --tags
 
+# Validate if tag exists.
+if [ ! $(git tag -l "$tag") ]; then
+  echo "Error: Tag not found, aborting. Tag $tag, Stack $stack" &>> ${log_file}
+  echo "Error: Tag not found, aborting. Tag $tag, Stack $stack"
+  exit
+fi
+
 # Create "main" branch from tag.
-# @TODO: Validate the tag exists.
 git branch main $tag
 
 # Checkout the new "main" branch.
@@ -52,7 +59,8 @@ cd /var/www/html/$AH_SITE_NAME/docroot
 if [ "$mode" = "updb" ]
 then
   drush sfmlc alshaya-enable-maintenance &>> ${log_file}
-  echo "Sites put offline for tag $tag" &>> ${log_file}
+  echo "Sites put offline. Tag $tag, Stack $stack" &>> ${log_file}
+  echo "Sites put offline. Tag $tag, Stack $stack"
 fi
 
 # Take backup now just before deployment
@@ -65,7 +73,8 @@ cd "$directory/$stack"
 # Force the push to avoid issues with previous commit history.
 git push origin main -f
 
-echo "Code deployment finished for tag $tag" &>> ${log_file}
+echo "Code deployment finished. Tag $tag, Stack $stack" &>> ${log_file}
+echo "Code deployment finished. Tag $tag, Stack $stack"
 
 # Go to docroot to do site operations again.
 cd /var/www/html/$AH_SITE_NAME/docroot
@@ -74,5 +83,6 @@ if [ "$mode" = "updb" ]
 then
   drush sfmlc updb &>> ${log_file}
   drush sfmlc alshaya-disable-maintenance &>> ${log_file}
-  echo "UPDB done and sites put back online for tag $tag" &>> ${log_file}
+  echo "UPDB done and sites put back online. Tag $tag, Stack $stack" &>> ${log_file}
+  echo "UPDB done and sites put back online. Tag $tag, Stack $stack"
 fi
