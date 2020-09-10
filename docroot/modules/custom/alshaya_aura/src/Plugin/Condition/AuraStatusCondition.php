97fca17ca9a019f878fb370d6a9e115bdcbdee5c
Sonam Singh <sonam.singh@innoraft.com>
2020-09-10 22:31:01 +0530

    CORE-23891: Update AURA status final class.

--- docroot/modules/custom/alshaya_aura/src/Plugin/Condition/AuraStatusCondition.php
+++ docroot/modules/custom/alshaya_aura/src/Plugin/Condition/AuraStatusCondition.php
@@ -4,20 +4,20 @@ namespace Drupal\alshaya_aura\Plugin\Condition;
 
 use Drupal\Core\Condition\ConditionPluginBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\alshaya_aura\Helper\AuraStatusList;
+use Drupal\alshaya_aura\Helper\AuraStatus;
 
 /**
- * Provides a 'AURA Status' condition.
+ * Provides a AURA Status Condition.
  *
  * @Condition(
- *   id = "aura_status",
- *   label = @Translation("AURA Status"),
+ *   id = "aura_status_condition",
+ *   label = @Translation("Aura Status Condition"),
  *   context_definitions = {
  *     "user" = @ContextDefinition("entity:user", label = @Translation("User"))
  *   }
  * )
  */
-class AuraStatus extends ConditionPluginBase {
+class AuraStatusCondition extends ConditionPluginBase {
 
   /**
    * {@inheritdoc}
@@ -27,9 +27,10 @@ class AuraStatus extends ConditionPluginBase {
       '#type' => 'checkboxes',
       '#title' => $this->t('When the user has the following AURA status'),
       '#default_value' => $this->configuration['status'],
-      '#options' => array_map('\Drupal\Component\Utility\Html::escape', AuraStatusList::getAllAuraStatus()),
+      '#options' => $this->getAllAuraStatus(),
       '#description' => $this->t('If you select no status, the condition will evaluate to TRUE for all users.'),
     ];
+
     return parent::buildConfigurationForm($form, $form_state);
   }
 
@@ -65,7 +66,7 @@ class AuraStatus extends ConditionPluginBase {
    * {@inheritdoc}
    */
   public function summary() {
-    $aura_statuses = array_intersect_key(AuraStatusList::getAllAuraStatus(), $this->configuration['status']);
+    $aura_statuses = array_intersect_key($this->getAllAuraStatus(), $this->configuration['status']);
     if (count($aura_statuses) > 1) {
       $aura_statuses = implode(', ', $aura_statuses);
     }
@@ -78,6 +79,19 @@ class AuraStatus extends ConditionPluginBase {
     else {
       return $this->t('The user has AURA status in @aura_statuses', ['@aura_statuses' => $aura_statuses]);
     }
+  }
+
+  /**
+   * Get all AURA Status.
+   */
+  public function getAllAuraStatus() {
+    $all_aura_status = [
+      AuraStatus::AURA_LINKED_VERIFIED => $this->t('Linked Verified'),
+      AuraStatus::AURA_LINKED_NOT_VERIFIED => $this->t('Linked Not Verified'),
+      AuraStatus::AURA_NOT_LINKED => $this->t('Not Linked'),
+    ];
+
+    return $all_aura_status;
   }
 
 }
