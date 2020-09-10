16dee54242cc0d0e8e77cfe2b782430367c593f9
Sonam Singh <sonam.singh@innoraft.com>
2020-09-10 14:27:37 +0530

    CORE-23891: Add a new block condition plugin for AURA status.

---
+++ docroot/modules/custom/alshaya_aura/src/Plugin/Condition/AuraStatus.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\alshaya_aura\Plugin\Condition;
+
+use Drupal\Core\Condition\ConditionPluginBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'AURA Status' condition.
+ *
+ * @Condition(
+ *   id = "aura_status",
+ *   label = @Translation("AURA Status"),
+ *   context_definitions = {
+ *     "user" = @ContextDefinition("entity:user", label = @Translation("User"))
+ *   }
+ * )
+ */
+class AuraStatus extends ConditionPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form['status'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('When the user has the following AURA status'),
+      '#default_value' => $this->configuration['status'],
+      '#options' => array_map('\Drupal\Component\Utility\Html::escape', $this->getAuraStatusValues()),
+      '#description' => $this->t('If you select no status, the condition will evaluate to TRUE for all users.'),
+    ];
+    return parent::buildConfigurationForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'status' => [],
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $this->configuration['status'] = array_filter($form_state->getValue('status'));
+    parent::submitConfigurationForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function evaluate() {
+    if (empty($this->configuration['status']) && !$this->isNegated()) {
+      return TRUE;
+    }
+    $user = $this->getContextValue('user');
+    return (bool) array_intersect($this->configuration['status'], [$user->field_aura_loyalty_status->value]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function summary() {
+    $aura_statuses = array_intersect_key($this->getAuraStatusValues(), $this->configuration['status']);
+    if (count($aura_statuses) > 1) {
+      $aura_statuses = implode(', ', $aura_statuses);
+    }
+    else {
+      $aura_statuses = reset($aura_statuses);
+    }
+    if (!empty($this->configuration['negate'])) {
+      return $this->t('The user do not have AURA status in @aura_statuses', ['@aura_statuses' => $aura_statuses]);
+    }
+    else {
+      return $this->t('The user has AURA status in @aura_statuses', ['@aura_statuses' => $aura_statuses]);
+    }
+  }
+
+  /**
+   * Helper to get AURA status values.
+   */
+  public function getAuraStatusValues() {
+    // @TODO: Will remove the hardcoded values once we have the actual data.
+    $auraStatusValues = ['registered' => 'Registered', 'approved' => 'Approved'];
+
+    return $auraStatusValues;
+  }
+
+}
