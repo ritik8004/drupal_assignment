4c06ef539b35fbc464482759c71506f4f8ce70fa
vidit-anjaria <vidit.27@gmail.com>
2020-09-14 20:47:48 +0530

    CORE-23920: [Blocked UX] As a guest user I should see a link in top section with short description.

--- docroot/themes/custom/transac/alshaya_white_label/js/custom.js
+++ docroot/themes/custom/transac/alshaya_white_label/js/custom.js
@@ -291,4 +291,24 @@
       }
     }
   };
+
+  Drupal.behaviors.auraHeaderPopup = {
+    attach: function (context) {
+      $('.aura-header-link a', context).on('click', function(e) {
+        e.preventDefault();
+        $('.aura-header-popup-wrapper').toggle();
+        e.stopPropagation();
+      });
+
+      $(document, context).on('click', function(e) {
+        const displayState = $('.aura-header-popup-wrapper').css('display');
+
+        if (displayState !== 'none') {
+          if (!($(e.target).closest('.aura-header-popup-wrapper').length)) {
+            $('.aura-header-popup-wrapper').hide();
+          }
+        }
+      });
+    }
+  };
 })(jQuery, Drupal, drupalSettings);
