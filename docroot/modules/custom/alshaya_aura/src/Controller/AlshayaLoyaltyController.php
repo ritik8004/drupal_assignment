6e3cb4cc0052899975f7cbf23f48fa6e6285a7b9
Sonam Singh <sonam.singh@innoraft.com>
2020-09-15 13:18:42 +0530

    CORE-24031: Create Loyalty Club page with static content.

---
+++ docroot/modules/custom/alshaya_aura/src/Controller/AlshayaLoyaltyController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\alshaya_aura\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class AlshayaLoyaltyController.
+ *
+ * @package Drupal\alshaya_aura\Controller
+ */
+class AlshayaLoyaltyController extends ControllerBase {
+
+  /**
+   * View AURA Loyalty Club details.
+   */
+  public function loyaltyClub() {
+    return [
+      '#theme' => 'loyalty_club',
+    ];
+  }
+
+  /**
+   * Returns page title.
+   */
+  public function getLoyaltyClubTitle() {
+    return $this->t('My Loyalty Club');
+  }
+
+}
