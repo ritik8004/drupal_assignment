bc7c58e0c63ee3bc7d870cb5572005c0ebafa900
vivekpan27 <vivek.panicker@innoraft.com>
2020-09-22 06:08:08 +0000

    CORE-23922: Replaced jquery Ajax call with Drupal ajax call.

--- docroot/modules/custom/alshaya_aura/js/aura-header.js
+++ docroot/modules/custom/alshaya_aura/js/aura-header.js
@@ -6,7 +6,9 @@
 (function ($, Drupal) {
   'use strict';
 
-  $(document).ready(function() {
+  Drupal.aura = Drupal.aura || {};
+
+  Drupal.aura.rewardsHeader = function() {
     // Decide what to show in the Aura header in desktop and in the primary menu
     // in mobile.
     const auraHeader = $('.block-aura-rewards-header');
@@ -14,6 +16,7 @@
     const mobilePopup = $('.block-alshaya-main-menu .menu__list .aura-header-popup-wrapper');
 
     if ($('body').hasClass('logged-out')) {
+      // Following code is executed for anonymous users.
       // Desktop mode.
       auraHeader.find('.aura-header-link').removeClass('hidden');
       auraHeader.find('.aura-header-popup-wrapper').removeClass('hidden');
@@ -21,62 +24,66 @@
       mobilePopup.removeClass('hidden');
     }
     else {
-      $.ajax({
+      // Following code is executed for logged in users.
+      var userPointsAjaxFetch = Drupal.ajax({
         type: 'GET',
+        progress: {type: 'throbber'},
         url: '/' + drupalSettings.path.currentLanguage + drupalSettings.aura.user_points_route,
-        success: function(data) {
-          if (data.aura_user.points >= 0 && data.aura_user.is_loyalty_linked == 1) {
-            // Desktop mode.
-            auraHeader.find('.aura-general-pages .name').html(data.aura_user.name);
-            auraHeader.find('.aura-general-pages .points').html(data.aura_user.points + Drupal.t(' Points'));
-            auraHeader.find('.aura-general-pages .badge').addClass('badge-' + data.aura_user.tier);
+      });
+      const userPointsSuccessCallback = function (data) {
+        if (data.aura_user.points >= 0 && data.aura_user.is_loyalty_linked == 1) {
+          // Desktop mode.
+          auraHeader.find('.aura-general-pages .name').html(data.aura_user.name);
+          auraHeader.find('.aura-general-pages .points').html(data.aura_user.points + Drupal.t(' Points'));
+          auraHeader.find('.aura-general-pages .badge').addClass('badge-' + data.aura_user.tier);
 
+          // Mobile mode.
+          main_menu_element.find('.name').html(data.aura_user.name);
+          main_menu_element.find('.points').html(data.aura_user.points).removeClass('hidden');
+          main_menu_element.find('.badge').addClass('badge-' + data.aura_user.tier).removeClass('hidden');
+        }
+        else if (data.aura_user.is_loyalty_linked == 1) {
+          // Loyalty card is linked, but points not there in the API response.
+          // Then show the same things as for anonymous users.
+          // Desktop mode.
+          auraHeader.find('.aura-header-link').removeClass('hidden');
+          auraHeader.find('.aura-header-popup-wrapper').removeClass('hidden');
+          // Mobile mode.
+          mobilePopup.removeClass('hidden');
+        }
+        else {
+          // Loyalty card is not linked.
+          main_menu_element.find('.name').html(data.aura_user.name);
+        }
+
+        // We show different content in the header depending on whether the user is
+        // logged in or anonymous.
+        if ($('body').hasClass('aura-my-account')) {
+          $('.aura-my-account-page').removeClass('hidden');
+          if (data.aura_user.is_loyalty_linked == 0) {
             // Mobile mode.
-            main_menu_element.find('.name').html(data.aura_user.name);
-            main_menu_element.find('.points').html(data.aura_user.points).removeClass('hidden');
-            main_menu_element.find('.badge').addClass('badge-' + data.aura_user.tier).removeClass('hidden');
+            mobilePopup.removeClass('hidden');
           }
-          else if (data.aura_user.is_loyalty_linked == 1) {
-            // Loyalty card is linked, but points not there in the API response.
-            // Then show the same things as for anonymous users.
+        }
+        else {
+          if (data.aura_user.is_loyalty_linked == 1) {
+            // We show the user points data.
+            $('.aura-general-pages').removeClass('hidden')
+          }
+          else {
+            // We do not show the user points data.
             // Desktop mode.
             auraHeader.find('.aura-header-link').removeClass('hidden');
             auraHeader.find('.aura-header-popup-wrapper').removeClass('hidden');
             // Mobile mode.
             mobilePopup.removeClass('hidden');
           }
-          else {
-            // Loyalty card is not linked.
-            main_menu_element.find('.name').html(data.aura_user.name);
-          }
-
-          // We show different content in the header depending on whether the user is
-          // logged in or anonymous.
-          if ($('body').hasClass('aura-my-account')) {
-            $('.aura-my-account-page').removeClass('hidden');
-            if (data.aura_user.is_loyalty_linked == 0) {
-              // Mobile mode.
-              mobilePopup.removeClass('hidden');
-            }
-          }
-          else {
-            if (data.aura_user.is_loyalty_linked == 1) {
-              // We show the user points data.
-              $('.aura-general-pages').removeClass('hidden')
-            }
-            else {
-              // We do not show the user points data.
-              // Desktop mode.
-              auraHeader.find('.aura-header-link').removeClass('hidden');
-              auraHeader.find('.aura-header-popup-wrapper').removeClass('hidden');
-              // Mobile mode.
-              mobilePopup.removeClass('hidden');
-            }
-          }
         }
-      });
+      }
+      userPointsAjaxFetch.options.success = userPointsSuccessCallback;
+      userPointsAjaxFetch.execute();
     }
-  });
+  };
 
   Drupal.behaviors.auraHeaderPopup = {
     attach: function (context) {
@@ -98,6 +105,10 @@
           }
         });
       }
+
+      $(window).on('load', function() {
+        Drupal.aura.rewardsHeader();
+      });
     }
   };
 
