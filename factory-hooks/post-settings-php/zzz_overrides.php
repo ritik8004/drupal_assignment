c9d9e489e917a0ab4a62f3f3fa13273e01fa9ff3
vivekpan27 <vivek.panicker@innoraft.com>
2020-09-25 13:37:21 +0000

    CORE-24408: Looping over server PHP and YAML server settings file.

--- factory-hooks/post-settings-php/zzz_overrides.php
+++ factory-hooks/post-settings-php/zzz_overrides.php
@@ -31,41 +31,34 @@ $country_code = $site_country_code['country_code'];
 
 $settings_path = $home . DIRECTORY_SEPARATOR . 'settings' . DIRECTORY_SEPARATOR . 'settings';
 
-// Allow overriding settings to set data directly from YAML files on server
-// which can be for stack or per brand or brand country combination.
-$stack_file = $settings_path . '.yml';
-$overridden_settings = [];
-if (file_exists($stack_file)) {
-  $overridden_settings = Yaml::decode(file_get_contents($stack_file));
-}
+// Allow overriding settings and config to set secret info directly from
+// include files on server which can be for stack or per brand or brand
+// country combination.
+$overridding_settings_files = [
+  $settings_path,
+  $settings_path . '-' . $acsf_site_code,
+  $settings_path . '-' . $acsf_site_code . $country_code,
+];
+$extensions = ['yml', 'php'];
 
-$brand_country_file = $settings_path . '-' . $acsf_site_code . $country_code . '.yml';
-if (file_exists($brand_country_file)) {
-  $overridden_settings = array_merge($overridden_settings, Yaml::decode(file_get_contents($brand_country_file)));
-}
-
-$brand_file = $settings_path . '-' . $acsf_site_code . '.yml';
-if (file_exists($brand_file)) {
-  $overridden_settings = array_merge($overridden_settings, Yaml::decode(file_get_contents($brand_file)));
+foreach ($extensions as $extension) {
+  foreach ($overridding_settings_files as $file) {
+    $file = $file . ".$extension";
+    switch ($extension) {
+      case 'yml':
+        if (file_exists($file)) {
+          $overridden_settings = Yaml::decode(file_get_contents($file));
+        }
+        break;
+      case 'php':
+        if (file_exists($file)) {
+          include_once $file;
+        }
+        break;
+    }
+  }
 }
 
 $settings = (!empty($overridden_settings))
   ? array_replace_recursive($settings, $overridden_settings)
   : $settings;
-
-// Allow overriding settings and config to set secret info directly from
-// include files on server which can be per brand or brand country combination.
-$stack_file = $settings_path . '.php';
-if (file_exists($stack_file)) {
-  include_once $stack_file;
-}
-
-$brand_country_file = $settings_path . '-' . $acsf_site_code . $country_code . '.php';
-if (file_exists($brand_country_file)) {
-  include_once $brand_country_file;
-}
-
-$brand_file = $settings_path . '-' . $acsf_site_code . '.php';
-if (file_exists($brand_file)) {
-  include_once $brand_file;
-}
