7419261aa3d6341ecf5aa6f2d69c68ad2fd69eb8
nikunj <coder@nikunj.dev>
2020-09-29 22:05:08 +0530

    CORE-24432: Process full catalog in single go. Add more logs to show updates and time.

--- scripts/utilities/migrate_images_to_shared.php
+++ scripts/utilities/migrate_images_to_shared.php
@@ -12,6 +12,8 @@
 
 use Drupal\Core\File\FileSystemInterface;
 
+$time_start = microtime(TRUE);
+
 $logger = \Drupal::logger('migrate_images_to_shared');
 
 $db = \Drupal::database();
@@ -19,82 +21,110 @@ $db = \Drupal::database();
 /* @var \Drupal\Core\File\FileSystem $file_system */
 $file_system = \Drupal::service('file_system');
 
-// Get all the files currently in use.
+// Get count of skus products public files.
 $query = $db->select('acq_sku_field_data', 'sku');
-$query->addField('sku', 'sku');
-$query->addField('sku', 'attr_assets__value');
 $query->condition('default_langcode', 1);
 $query->condition('attr_assets__value', '%public://%', 'LIKE');
-$query->orderBy('changed', 'DESC');
-$query->range(0, 1000);
-$result = $query->execute()->fetchAll();
+$count = $query->countQuery()->execute()->fetchCol()[0];
 
-foreach ($result as $row) {
-  $update = FALSE;
+if (empty($count)) {
+  $logger->notice('No products found for migration.');
+  exit;
+}
 
-  $media = unserialize($row->attr_assets__value);
-  foreach ($media as $index => $item) {
-    if (empty($item['drupal_uri']) || strpos($item['drupal_uri'], 'public://') === FALSE) {
-      continue;
-    }
-    $update = TRUE;
+$processed = 0;
 
-    $source = $file_system->realpath($item['drupal_uri']);
-    if (empty($source)) {
-      unset($item['fid']);
-      unset($item['drupal_uri']);
+do {
+  // Get all the public files currently in use.
+  $query = $db->select('acq_sku_field_data', 'sku');
+  $query->addField('sku', 'sku');
+  $query->addField('sku', 'attr_assets__value');
+  $query->condition('default_langcode', 1);
+  $query->condition('attr_assets__value', '%public://%', 'LIKE');
+  $query->orderBy('changed', 'DESC');
+  $query->range(0, 100);
+  $result = $query->execute()->fetchAll();
+
+  foreach ($result as $row) {
+    $update = FALSE;
+
+    $media = unserialize($row->attr_assets__value);
+    foreach ($media as $index => $item) {
+      if (empty($item['drupal_uri']) || strpos($item['drupal_uri'], 'public://') === FALSE) {
+        continue;
+      }
+      $update = TRUE;
+
+      $source = $file_system->realpath($item['drupal_uri']);
+      if (empty($source)) {
+        unset($item['fid']);
+        unset($item['drupal_uri']);
+        $media[$index] = $item;
+
+        continue;
+      }
+
+      $item['drupal_uri'] = str_replace('public://', 'brand://', $item['drupal_uri']);
+
+      $directory = pathinfo($item['drupal_uri'], PATHINFO_DIRNAME);
+      if (!$file_system->prepareDirectory($directory)) {
+        $file_system->mkdir($directory, 0755, TRUE);
+      }
+
+      try {
+        $file_system->move(
+          $source,
+          $directory,
+          FileSystemInterface::EXISTS_REPLACE
+        );
+
+        $db->update('file_managed')
+          ->fields(['uri' => $item['drupal_uri']])
+          ->condition('fid', $item['fid'])
+          ->execute();
+
+        $logger->notice(dt('File successfully moved. SKU: @sku; Source: @source; Destination: @destination', [
+          '@sku' => $row->sku,
+          '@source' => $source,
+          '@destination' => $item['drupal_uri'],
+        ]));
+      }
+      catch (\Exception $e) {
+        $logger->notice(dt('File no longer available, removing from asset. SKU: @sku; Source: @source; Destination: @destination', [
+          '@sku' => $row->sku,
+          '@source' => $source,
+          '@destination' => $item['drupal_uri'],
+        ]));
+
+        unset($item['fid']);
+        unset($item['drupal_uri']);
+      }
+
       $media[$index] = $item;
-
-      continue;
     }
 
-    $item['drupal_uri'] = str_replace('public://', 'brand://', $item['drupal_uri']);
-
-    $directory = pathinfo($item['drupal_uri'], PATHINFO_DIRNAME);
-    if (!$file_system->prepareDirectory($directory)) {
-      $file_system->mkdir($directory, 0755, TRUE);
-    }
-
-    try {
-      $file_system->move(
-        $source,
-        $directory,
-        FileSystemInterface::EXISTS_REPLACE
-      );
-
-      $db->update('file_managed')
-        ->fields(['uri' => $item['drupal_uri']])
-        ->condition('fid', $item['fid'])
+    if ($update) {
+      $db->update('acq_sku_field_data')
+        ->fields(['attr_assets__value' => serialize($media)])
+        ->condition('sku', $row->sku)
         ->execute();
 
-      $logger->notice(dt('File successfully moved. SKU: @sku; Source: @source; Destination: @destination', [
+      $logger->notice(dt('Updated from public to brand for sku @sku', [
         '@sku' => $row->sku,
-        '@source' => $source,
-        '@destination' => $item['drupal_uri'],
       ]));
     }
-    catch (\Exception $e) {
-      $logger->notice(dt('File no longer available, removing from asset. SKU: @sku; Source: @source; Destination: @destination', [
-        '@sku' => $row->sku,
-        '@source' => $source,
-        '@destination' => $item['drupal_uri'],
-      ]));
-
-      unset($item['fid']);
-      unset($item['drupal_uri']);
-    }
-
-    $media[$index] = $item;
   }
 
-  if ($update) {
-    $db->update('acq_sku_field_data')
-      ->fields(['attr_assets__value' => serialize($media)])
-      ->condition('sku', $row->sku)
-      ->execute();
+  $processed = $processed + count($result);
+  $logger->notice(dt('Processed @processed out of total @count.', [
+    '@processed' => $processed,
+    '@count' => $count,
+  ]));
+} while ($result);
 
-    $logger->notice(dt('Updated from public to brand for sku @sku', [
-      '@sku' => $row->sku,
-    ]));
-  }
-}
+$time_end = microtime(TRUE);
+
+$logger->notice(dt('Finished migration. Total time taken: @time, count: @count', [
+  '@time' => ($time_end - $time_start),
+  '@count' => $count,
+]));
