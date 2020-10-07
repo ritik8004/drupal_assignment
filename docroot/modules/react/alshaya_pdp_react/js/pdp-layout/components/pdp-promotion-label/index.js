fb6855f654f062955e177f92090b72c4048ca294
vagrant <vagrant@local.alshaya.com>
2020-10-07 11:32:57 +0000

    CORE-22895: Pdp promo label refresh on sku code change.

--- docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-promotion-label/index.js
+++ docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-promotion-label/index.js
@@ -1,20 +1,49 @@
 import React from 'react';
+import axios from 'axios';
 import PdpDynamicPromotions from '../pdp-dynamic-promotions';
 
-const PdpPromotionLabel = (props) => {
-  const {
-    skuMainCode, cartDataValue, promotions,
-  } = props;
+class PdpPromotionLabel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      promotionsRawData: null,
+    };
+  }
 
-  return (promotions) ? (
-    <>
-      {Object.keys(promotions).map((key) => (
-        <p><a href={promotions[key].promo_web_url}>{promotions[key].text}</a></p>
-      ))}
-      <div id="dynamic-promo-labels">
-        <PdpDynamicPromotions skuMainCode={skuMainCode} cartDataValue={cartDataValue} />
-      </div>
-    </>
-  ) : null;
-};
+  getRelatedProductsInfo = (promotionsRawData, url) => {
+    // If product promotion data is already processed.
+    if (promotionsRawData === null) {
+      axios.get(url).then((response) => {
+        if (response.data.length !== 0) {
+          console.log(response.data);
+          this.setState({
+            promotionsRawData: response.data.promotionsRaw,
+          });
+        }
+      });
+    }
+  }
+
+
+  render() {
+    const {
+      skuMainCode,
+      cartDataValue,
+    } = this.props;
+    const { promotionsRawData } = this.state;
+    const url = Drupal.url(`rest/v1/product/${skuMainCode}?pdp=magazinev2`);
+    this.getRelatedProductsInfo(promotionsRawData, url);
+
+    return (promotionsRawData) ? (
+      <>
+        {Object.keys(promotionsRawData).map((key) => (
+          <p><a href={promotionsRawData[key].promo_web_url}>{promotionsRawData[key].text}</a></p>
+        ))}
+        <div id="dynamic-promo-labels">
+          <PdpDynamicPromotions skuMainCode={skuMainCode} cartDataValue={cartDataValue} />
+        </div>
+      </>
+    ) : null;
+  }
+}
 export default PdpPromotionLabel;
