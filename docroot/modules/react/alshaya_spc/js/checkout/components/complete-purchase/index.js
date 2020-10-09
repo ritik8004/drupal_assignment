b046249c9f0439978f58ea10627dad7fdf93c6c2
anjali vijay <anjali.vijay@axelerant.com>
2020-10-09 10:24:57 +0530

    CORE-24713: Fixed linting issues.

--- docroot/modules/react/alshaya_spc/js/checkout/components/complete-purchase/index.js
+++ docroot/modules/react/alshaya_spc/js/checkout/components/complete-purchase/index.js
@@ -3,7 +3,6 @@ import {
   placeOrder,
   isDeliveryTypeSameAsInCart,
 } from '../../../utilities/checkout_util';
-import PriceElement from '../../../utilities/special-price/PriceElement';
 import dispatchCustomEvent from '../../../utilities/events';
 import { smoothScrollTo } from '../../../utilities/smoothScroll';
 import ConditionalView from '../../../common/components/conditional-view';
