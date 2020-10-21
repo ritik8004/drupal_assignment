f69df75f42d9668131c5c2b04018614161b76909
vivekpan27 <vivek.panicker@innoraft.com>
2020-10-21 06:28:54 +0000

    CORE-24726: Added check for _dyid cookie also before sending AJAX request.

--- docroot/modules/custom/alshaya_dynamic_yield/js/alshaya_dynamic_yield.intelligent_tracking.js
+++ docroot/modules/custom/alshaya_dynamic_yield/js/alshaya_dynamic_yield.intelligent_tracking.js
@@ -2,12 +2,15 @@
   $(window).on('load', function() {
     // This is being done on "load" since only at this time we have the
     // _dyid cookie available which will be required in the controller.
-    var dyIdServerCookie = document.cookie.split(';')
+    var dyIdCookie = document.cookie.split(';')
       .find(function(row) {
-        return row.match(/_dyid_server/);
+        return row.match(/_dyid_server|_dyid/);
       });
 
-    if (typeof dyIdServerCookie !== 'undefined') {
+    if ((typeof dyIdCookie !== 'undefined')
+      && ((dyIdCookie.indexOf('_dyid') < 0)
+      || (dyIdCookie.indexOf('_dyid_server') > -1))
+    ) {
       return;
     }
 
