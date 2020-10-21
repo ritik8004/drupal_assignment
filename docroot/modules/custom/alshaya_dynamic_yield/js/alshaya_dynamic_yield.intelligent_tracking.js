a40764422b035b14c8d6eb9a77a655c34b02ef96
vivekpan27 <vivek.panicker@innoraft.com>
2020-10-21 08:23:49 +0000

    CORE-24726: Directly fetching object keys instead of using loop.

--- docroot/modules/custom/alshaya_dynamic_yield/js/alshaya_dynamic_yield.intelligent_tracking.js
+++ docroot/modules/custom/alshaya_dynamic_yield/js/alshaya_dynamic_yield.intelligent_tracking.js
@@ -2,20 +2,9 @@
   $(window).on('load', function() {
     // This is being done on "load" since only at this time we have the
     // _dyid cookie available which will be required in the controller.
-    var isDyIdCookieSet = false;
-    var isDyIdServerCookieSet = false;
-    $.each($.cookie(), function(key) {
-      if (!isDyIdCookieSet && key.indexOf('_dyid') > -1) {
-        isDyIdCookieSet = true;
-      }
-      if (!isDyIdServerCookieSet && key.indexOf('_dyid_server') > -1) {
-        isDyIdServerCookieSet = true;
-      }
-      if (isDyIdCookieSet && isDyIdServerCookieSet) {
-        // Break from loop.
-        return false;
-      }
-    });
+    var allCookies = $.cookie();
+    var isDyIdCookieSet = '_dyid' in allCookies;
+    var isDyIdServerCookieSet = '_dyid_server' in allCookies;
 
     if (!isDyIdCookieSet || isDyIdServerCookieSet) {
       return;
