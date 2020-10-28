b0d498310b4e76098acddd519642f14da06cf5f8
vivekpan27 <vivek.panicker@innoraft.com>
2020-10-28 11:01:03 +0000

    CORE-24974: Corrected response times for API calls.

--- factory-hooks/pre-settings-php/alshaya_backend_calls_options.settings.php
+++ factory-hooks/pre-settings-php/alshaya_backend_calls_options.settings.php
@@ -11,58 +11,58 @@
 $settings['alshaya_backend_calls_options'] = [
   'middleware' => [
     'order_place' => [
-      'timeout' => 64,
+      'timeout' => 60,
     ],
     'cart_update' => [
-      'timeout' => 12,
+      'timeout' => 15,
     ],
     'cart_get' => [
-      'timeout' => 7,
+      'timeout' => 10,
     ],
     'cart_create' => [
       'timeout' => 5,
     ],
     'cart_associate' => [
-      'timeout' => 11,
+      'timeout' => 15,
     ],
     'cart_estimate_shipping' => [
-      'timeout' => 11,
+      'timeout' => 15,
     ],
     'cart_payment_methods' => [
-      'timeout' => 7,
-    ],
-    'cart_selected_payment' => [
-      'timeout' => 6,
-    ],
-    'cart_search' => [
-      'timeout' => 8,
-    ],
-    'order_search' => [
-      'timeout' => 9,
-    ],
-    'cybersource_token_get' => [
       'timeout' => 10,
     ],
+    'cart_selected_payment' => [
+      'timeout' => 10,
+    ],
+    'cart_search' => [
+      'timeout' => 10,
+    ],
+    'order_search' => [
+      'timeout' => 5,
+    ],
+    'cybersource_token_get' => [
+      'timeout' => 15,
+    ],
     'cybersource_token_process' => [
-      'timeout' => 11,
+      'timeout' => 15,
     ],
     'customer_create' => [
-      'timeout' => 8,
+      'timeout' => 10,
     ],
     'customer_search' => [
-      'timeout' => 4,
+      'timeout' => 10,
     ],
     'checkoutcom_config_get' => [
-      'timeout' => 4,
+      'timeout' => 10,
     ],
     'checkoutcom_token_get' => [
-      'timeout' => 4,
+      'timeout' => 5,
     ],
     'checkoutcom_token_list' => [
       'timeout' => 4,
     ],
     'cnc_check' => [
-      'timeout' => 6,
+      'timeout' => 10,
     ],
     'customer_me_get' => [
       'timeout' => 10,
