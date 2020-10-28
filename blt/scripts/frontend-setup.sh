47d70febb3ecd4ad10a4a6469f3f440be1fe50b9
Nikita Aswani <nikitaaswani@Nikitas-MacBook-Pro-2.local>
2020-10-28 20:45:39 +0530

    CORE-24888: Fixed syntax error in frontend-setup.sh file.

--- blt/scripts/frontend-setup.sh
+++ blt/scripts/frontend-setup.sh
@@ -80,6 +80,7 @@ do
         gulp lint:css-with-fail
         gulp lint:js-with-fail
         if (-d $docrootDir/themes/custom/$theme_type_dir/$theme_dir/conditional-sass/)
+        then
           gulp lint:module-component-libraries-css-with-fail
         fi
       done
