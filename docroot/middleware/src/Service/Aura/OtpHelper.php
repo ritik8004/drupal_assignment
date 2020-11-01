a33be04c6d33dc980070d6193a879cee540ec691
Sonam Singh <sonam.singh@innoraft.com>
2020-11-01 16:05:22 +0530

    CORE-25357: Add mobile already exists validation in otp modal.

---
+++ docroot/middleware/src/Service/Aura/OtpHelper.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Service\Aura;
+
+use App\Service\Magento\MagentoApiWrapper;
+use App\Service\Utility;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Helper for aura otp related APIs.
+ *
+ * @package App\Service\Aura
+ */
+class OtpHelper {
+
+  /**
+   * Magento API Wrapper service.
+   *
+   * @var \App\Service\Magento\MagentoApiWrapper
+   */
+  protected $magentoApiWrapper;
+
+  /**
+   * Logger service.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * Utility service.
+   *
+   * @var \App\Service\Utility
+   */
+  protected $utility;
+
+  /**
+   * OtpHelper constructor.
+   *
+   * @param \App\Service\Magento\MagentoApiWrapper $magento_api_wrapper
+   *   Magento API wrapper service.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   Logger service.
+   * @param \App\Service\Utility $utility
+   *   Utility Service.
+   */
+  public function __construct(
+    MagentoApiWrapper $magento_api_wrapper,
+    LoggerInterface $logger,
+    Utility $utility
+  ) {
+    $this->magentoApiWrapper = $magento_api_wrapper;
+    $this->logger = $logger;
+    $this->utility = $utility;
+  }
+
+  /**
+   * Send OTP.
+   *
+   * @return array
+   *   Return API response/error.
+   */
+  public function sendOtp($mobile) {
+    try {
+      $endpoint = sprintf('/sendotp/phonenumber/%s', $mobile);
+      $response = $this->magentoApiWrapper->doRequest('GET', $endpoint);
+      $responseData = [
+        'status' => $response,
+      ];
+      return $responseData;
+    }
+    catch (\Exception $e) {
+      $this->logger->notice('Error while trying to send otp on mobile number @mobile. Message: @message', [
+        '@mobile' => $mobile,
+        '@message' => $e->getMessage(),
+      ]);
+      return $this->utility->getErrorResponse($e->getMessage(), $e->getCode());
+    }
+  }
+
+}
