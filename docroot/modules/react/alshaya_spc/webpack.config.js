c3008f1f278d79253b85de37a94791bbeb4f991f
Sonam Singh <sonam.singh@innoraft.com>
2020-11-11 17:38:29 +0530

    CORE-24234: Fix npm build issue.

--- docroot/modules/react/alshaya_spc/webpack.config.js
+++ docroot/modules/react/alshaya_spc/webpack.config.js
@@ -27,7 +27,12 @@ var config = {
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        use: ['babel-loader']
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env',
+            '@babel/react',{
+              'plugins': ['@babel/plugin-proposal-class-properties']}]
+        }
       }
     ]
   }
