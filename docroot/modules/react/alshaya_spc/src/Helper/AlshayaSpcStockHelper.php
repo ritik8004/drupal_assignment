78a9e7a504ea144bcb1fcb486575e8be88aca9bb
Wouter Adem <wouter.adem@gmail.com>
2020-11-18 10:50:46 +0100

    Revert "Merge pull request #18727 from sonamsingh28/CORE-25685_Checkout-Event-Refresh-stock-only-when-required-in-unique-queue"
    This reverts commit 414712e722c2cc64270c005aadf8d317ad3660ed.

--- docroot/modules/react/alshaya_spc/src/Helper/AlshayaSpcStockHelper.php
+++ docroot/modules/react/alshaya_spc/src/Helper/AlshayaSpcStockHelper.php
@@ -4,7 +4,6 @@ namespace Drupal\alshaya_spc\Helper;
 
 use Drupal\acq_commerce\SKUInterface;
 use Drupal\acq_sku\Entity\SKU;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 
 /**
  * Class Alshaya Spc Stock Helper.
@@ -12,24 +11,6 @@ use Drupal\Core\Logger\LoggerChannelFactoryInterface;
  * @package Drupal\alshaya_spc\Helper
  */
 class AlshayaSpcStockHelper {
-  /**
-   * Logger service.
-   *
-   * @var \Drupal\Core\Logger\LoggerChannelInterface
-   */
-  protected $logger;
-
-  /**
-   * AlshayaSpcStockHelper constructor.
-   *
-   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
-   *   Logger Factory service.
-   */
-  public function __construct(
-    LoggerChannelFactoryInterface $logger_factory
-  ) {
-    $this->logger = $logger_factory->get('alshaya_spc');
-  }
 
   /**
    * Refresh stock cache and Drupal cache of products in cart.
@@ -67,9 +48,6 @@ class AlshayaSpcStockHelper {
 
         if ($stock === 0) {
           $response['stock'][$sku] = FALSE;
-          $this->logger->info('Refresh Stock skipped for SKU: @sku.', [
-            '@sku' => $sku,
-          ]);
           continue;
         }
 
