b26f3fa9b9264eb7e7f878cdd654d5aaacffcacf
vivekpan27 <vivek.panicker@innoraft.com>
2020-11-27 10:44:46 +0000

    CORE-23349: Placed code inside dialog event handler to execute it after dialog open.

--- docroot/modules/custom/alshaya_acm_promotion/assets/js/alshaya_promotions_label.basket.js
+++ docroot/modules/custom/alshaya_acm_promotion/assets/js/alshaya_promotions_label.basket.js
@@ -13,17 +13,19 @@
         $('#promo-action-button').trigger('click');
       });
 
-      // Create event for select button.
-      if ($('.free-gift-listing-modal #select-add-free-gift').length > 0) {
-        var selectFreeGiftModal = new CustomEvent('selectFreeGiftModalEvent', {bubbles: true, detail: {}});
-        document.dispatchEvent(selectFreeGiftModal);
-      }
+      $(window).once('dialogopened').on('dialog:aftercreate', function (event) {
+        // Create event for select button.
+        if ($('.free-gift-listing-modal #select-add-free-gift').length > 0) {
+          var selectFreeGiftModal = new CustomEvent('selectFreeGiftModalEvent', {bubbles: true, detail: {}});
+          document.dispatchEvent(selectFreeGiftModal);
+        }
 
-      // Event for Add Free gift button.
-      if ($('.free-gift-detail-modal #add-free-gift').length > 0) {
-        var productDetailFreeGiftModal = new CustomEvent('openFreeGiftModalEvent', {bubbles: true, detail: {}});
-        document.dispatchEvent(productDetailFreeGiftModal);
-      }
+        // Event for Add Free gift button.
+        if ($('.free-gift-detail-modal #add-free-gift').length > 0) {
+          var productDetailFreeGiftModal = new CustomEvent('openFreeGiftModalEvent', {bubbles: true, detail: {}});
+          document.dispatchEvent(productDetailFreeGiftModal);
+        }
+      });
     }
   };
 
