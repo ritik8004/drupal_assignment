f8505d566c073f25532e21b77569d3fe0362dec0
Nikita Aswani <nikitaaswani@Nikitas-MacBook-Pro-2.local>
2020-12-01 13:30:43 +0530

    CORE-25491: Fixed input border and label positioning with and without focus.

--- docroot/modules/react/alshaya_fit_calculator/js/utilities/textfield/index.js
+++ docroot/modules/react/alshaya_fit_calculator/js/utilities/textfield/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
-const TextField = ({ name, label, focusClass }) => (
-  <div className="fitCalc-type-textfield">
-    <input
-      type="text"
-      id={name}
-      name={name}
-      className={focusClass}
-    />
-    <div className="c-input__bar" />
-    <label>{label}</label>
-  </div>
-);
+const TextField = ({ name, label, focusClass }) => {
+  const handleEvent = (e, handler) => {
+    if (handler === 'blur') {
+      if (e.currentTarget.value.length > 0) {
+        e.currentTarget.classList.add('focus');
+      } else {
+        e.currentTarget.classList.remove('focus');
+      }
+    }
+  };
+
+  return (
+    <div className="fitCalc-type-textfield">
+      <input
+        type="text"
+        onBlur={(e) => handleEvent(e, 'blur')}
+        id={name}
+        name={name}
+        className={focusClass}
+      />
+      <div className="c-input__bar" />
+      <label>{label}</label>
+    </div>
+  );
+};
 
 export default TextField;
