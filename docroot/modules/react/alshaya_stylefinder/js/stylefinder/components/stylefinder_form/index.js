f5cbcb8e1df7d39ef1427affcad1e130bfb1a1c8
vagrant <vagrant@local.alshaya.com>
2020-12-02 10:25:57 +0000

    CORE-25494: Added StyleFinder Sub-Module for Alshaya Quiz.

---
+++ docroot/modules/react/alshaya_stylefinder/js/stylefinder/components/stylefinder_form/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class StyleFinder extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>Placeholder...</div>
+    );
+  }
+}
