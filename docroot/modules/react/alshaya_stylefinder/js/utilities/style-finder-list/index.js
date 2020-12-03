0ab79b2bdca36432ee9b9e1c686911797372d07b
vidit-anjaria <vidit.27@gmail.com>
2020-12-03 14:00:09 +0530

    CORE-25501: React | Style finder static markup.

---
+++ docroot/modules/react/alshaya_stylefinder/js/utilities/style-finder-list/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const StyleFinderList = ({ children, className = '' }) => <ul className={`style-finder-list ${className}`}>{children}</ul>;
+
+export default StyleFinderList;
---
+++ docroot/modules/react/alshaya_stylefinder/js/utilities/style-finder-paragraph/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const StyleFinderPara = ({ children, className = '' }) => <p className={`style-finder-text ${className}`}>{children}</p>;
+
+export default StyleFinderPara;
