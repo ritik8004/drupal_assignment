cf89b0095ac2c5fcad99130bffb8054b47e6ceb2
Nikita Aswani <nikitaaswani@Nikitas-MacBook-Pro-2.local>
2020-12-06 17:36:55 +0530

    CORE-25501: Style finder theming.

--- docroot/modules/react/alshaya_stylefinder/js/utilities/style-finder-paragraph/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const StyleFinderPara = ({ children, className = '' }) => <p className={`style-finder-text ${className}`}>{children}</p>;
-
-export default StyleFinderPara;
