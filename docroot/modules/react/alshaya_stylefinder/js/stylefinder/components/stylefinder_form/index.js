accf9ced890eda6c4a29aed2aeccc7219fc7c66e
Avinash Shukla <avinash.shukla@acquia.com>
2020-12-08 13:01:51 +0530

    CORE-25503: Render style finder quiz opions from drupalsettings.

--- docroot/modules/react/alshaya_stylefinder/js/stylefinder/components/stylefinder_form/index.js
+++ docroot/modules/react/alshaya_stylefinder/js/stylefinder/components/stylefinder_form/index.js
@@ -3,123 +3,141 @@ import StyleFinderTitle from '../../../utilities/style-finder-title';
 import StyleFinderDesc from '../../../utilities/style-finder-description';
 import StyleFinderSteps from '../../../utilities/style-finder-steps';
 import StyleFinderSubTitle from '../../../utilities/style-finder-subtitle';
+import StyleFinderListItem from '../../../utilities/style-finder-list-item';
 
 export default class StyleFinder extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      step: [],
+      answerSelected: [],
+    };
+  }
+
+  componentDidMount() {
+    const { quizDetails } = drupalSettings.styleFinder;
+    const step = [quizDetails.question[0]];
+    this.setState({
+      step,
+    });
+  }
+
+  findAncestor = (el, cls) => {
+    const element = el;
+    let e = el;
+    while (!e.classList.contains(cls)) {
+      e = e.parentElement;
+    }
+    return (el === null) ? element : e;
+  }
+
+  handleStepSubmit = (e, answer, choice, counter) => {
+    e.preventDefault();
+    const element = this.findAncestor(e.target, 'list-item');
+    if (element !== undefined) {
+      element.parentElement.childNodes.forEach((child) => {
+        child.classList.remove('active');
+      });
+      element.classList.add('active');
+    }
+    let { step, answerSelected } = this.state;
+    step.length = counter;
+    answerSelected.length = counter - 1;
+    this.setState((state) => {
+      answerSelected = state.answerSelected.concat(choice);
+      return {
+        answerSelected,
+      };
+    });
+    const stepDetails = step[step.length - 1];
+    if (stepDetails.answer[answer].question !== undefined
+      && stepDetails.answer[answer].question.length > 0) {
+      this.setState((state) => {
+        step = state.step.concat(stepDetails.answer[answer].question[0]);
+        return {
+          step,
+        };
+      });
+    } else {
+      this.setState({
+        step,
+      });
+    }
+  };
+
+  renderStep = (stepDetails, counter) => {
+    const { answer } = stepDetails;
+    let optionListClass = 'style-finder-lining-list';
+    let optionList = '';
+    optionList = Object.keys(answer).map(function listOptionsStep1(item) {
+      return (
+        <StyleFinderListItem
+          key={item.nid}
+          answer={answer[item]}
+          handleStepSubmit={this.handleStepSubmit}
+          counter={counter}
+        />
+      );
+    }, this);
+    if (counter === 1) {
+      optionListClass = 'style-finder-type-list';
+      optionList = Object.keys(answer).map(function listOptionsStep(index) {
+        return (
+          <li
+            className="list-item"
+            key={index.nid}
+            onClick={(e) => this.handleStepSubmit(e, index, answer[index].choice, 1)}
+          >
+            {answer[index].title}
+          </li>
+        );
+      }, this);
+    } else if (counter === 2) {
+      optionListClass = 'style-finder-lining-list';
+    } else if (counter === 3) {
+      optionListClass = 'style-finder-bra-coverage-list';
+    }
+
+    return (
+      <div className="style-finder-step-wrapper" key={stepDetails.ques_instruction}>
+        <StyleFinderSteps>
+          {stepDetails.ques_instruction}
+        </StyleFinderSteps>
+
+        <StyleFinderSubTitle className="style-finder-choose-lining-level">
+          {stepDetails.title}
+        </StyleFinderSubTitle>
+
+        <ul className={`style-finder-list ${optionListClass}`}>
+          {optionList}
+        </ul>
+      </div>
+    );
   }
 
   render() {
+    const { quizDetails } = drupalSettings.styleFinder;
+    const { step } = this.state;
+    const otherSteps = [];
+    let j = 1;
+    if (step.length > 0) {
+      for (let i = 0; i < step.length; i++) {
+        otherSteps.push(this.renderStep(step[i], j));
+        j += 1;
+      }
+    }
+
     return (
       <section className="style-finder-wrapper">
         <div className="style-finder-heading-wrapper">
           <StyleFinderTitle>
-            {Drupal.t('Style Finder')}
+            {quizDetails.quiz_title}
           </StyleFinderTitle>
-
           <StyleFinderDesc>
-            {Drupal.t('Complete the steps below to find your best-fitting bra.')}
+            {quizDetails.quiz_instruction}
           </StyleFinderDesc>
         </div>
-        <div className="style-finder-step-wrapper">
-          <StyleFinderSteps>
-            {Drupal.t('Steps 1')}
-          </StyleFinderSteps>
-
-          <StyleFinderSubTitle className="style-finder-choose-type">
-            {Drupal.t('Choose your type')}
-          </StyleFinderSubTitle>
-
-          <ul className="style-finder-list style-finder-type-list">
-            <li className="style-type">{Drupal.t('Bras')}</li>
-            <li className="style-type">{Drupal.t('Sports Bras')}</li>
-          </ul>
-        </div>
-        <div className="style-finder-step-wrapper">
-          <StyleFinderSteps>
-            {Drupal.t('Steps 2')}
-          </StyleFinderSteps>
-
-          <StyleFinderSubTitle className="style-finder-choose-lining-level">
-            {Drupal.t('Choose your lining level')}
-          </StyleFinderSubTitle>
-
-          <ul className="style-finder-list style-finder-lining-list">
-            <li className="style-finder-list-item">
-              <div className="style-finder-list-image">
-                <img src="https://www.victoriassecret.com/p/280x373/tif/59/19/59196aba1c544b40aa9444744794727c/111820074Z6W_OF_F.jpg" />
-              </div>
-              <div className="style-finder-list-title">
-                {Drupal.t('Unlined')}
-              </div>
-              <div className="style-finder-list-text">
-                {Drupal.t('Nothing comes between you and your bra, but you\'ll still feel supported.')}
-              </div>
-            </li>
-            <li className="style-finder-list-item">
-              <div className="style-finder-list-image">
-                <img src="https://www.victoriassecret.com/p/280x373/tif/f4/67/f4679a9b946e452e9a244c78e9741f15/111782234X4O_OF_F.jpg" />
-              </div>
-              <div className="style-finder-list-title">
-                {Drupal.t('Lightly Lined')}
-              </div>
-              <div className="style-finder-list-text">
-                {Drupal.t('Just a little something for smooth shape, no show-through and plenty of support.')}
-              </div>
-            </li>
-            <li className="style-finder-list-item">
-              <div className="style-finder-list-image">
-                <img src="https://www.victoriassecret.com/p/280x373/tif/7b/2d/7b2d2355995241b3b3219ba161c6becd/1118200251MM_OF_F.jpg" />
-              </div>
-              <div className="style-finder-list-title">
-                {Drupal.t('Push-Up')}
-              </div>
-              <div className="style-finder-list-text">
-                {Drupal.t('Comfortable, state-of-the-art padding for extra support and a subtle to sultry lift.')}
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div className="style-finder-step-wrapper style-finder-step-coverage-wrapper">
-          <StyleFinderSteps>
-            {Drupal.t('Steps 3')}
-          </StyleFinderSteps>
-
-          <StyleFinderSubTitle className="style-finder-choose-coverage">
-            {Drupal.t('Choose your coverage')}
-          </StyleFinderSubTitle>
-
-          <ul className="style-finder-list style-finder-step-coverage-wrapper">
-            <li className="style-coverage">
-              <div className="style-finder-list-image">
-                <img src="https://www.victoriassecret.com/assets/cms-components/vs-bra-finder/assets/0818-bra-guide-coverage-perfect-coverage.aa99069e.png" />
-              </div>
-              <div className="style-finder-list-title">{Drupal.t('Full Coverage')}</div>
-              <div className="style-finder-list-text">
-                {Drupal.t('Complete coverage and comfort, maximum support.')}
-              </div>
-            </li>
-            <li className="style-coverage">
-              <div className="style-finder-list-image">
-                <img src="https://www.victoriassecret.com/assets/cms-components/vs-bra-finder/assets/0818-bra-guide-coverage-demi.17572faa.png" />
-              </div>
-              <div className="style-finder-list-title">{Drupal.t('Demi')}</div>
-              <div className="style-finder-list-text">
-                {Drupal.t('A little less coverage in a subtle shape.')}
-              </div>
-            </li>
-            <li className="style-coverage">
-              <div className="style-finder-list-image">
-                <img src="https://www.victoriassecret.com/assets/cms-components/vs-bra-finder/assets/0818-bra-guide-coverage-plunge.fe3c2cc4.png" />
-              </div>
-              <div className="style-finder-list-title">{Drupal.t('Plunge')}</div>
-              <div className="style-finder-list-text">
-                {Drupal.t('A deep neckline that\'s the most revealing')}
-              </div>
-            </li>
-          </ul>
-        </div>
+        {otherSteps.map((item) => item)}
       </section>
     );
   }
---
+++ docroot/modules/react/alshaya_stylefinder/js/utilities/style-finder-list-item/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default class StyleFinderListItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  handleStepSubmit = (e, answer, choice, counter) => {
+    const { handleStepSubmit } = this.props;
+    handleStepSubmit(e, answer, choice, counter);
+  };
+
+  render() {
+    const { answer, counter } = this.props;
+
+    return (
+      <>
+        {answer
+          && (
+          <li
+            onClick={(e) => this.handleStepSubmit(e, answer.nid, answer.choice, counter)}
+            className="list-item style-finder-list-item"
+          >
+            <div className="style-finder-list-image">
+              <img src={answer.image_url} />
+            </div>
+            <div className="style-finder-list-title">
+              {answer.title}
+            </div>
+            <div className="style-finder-list-text">
+              {answer.description}
+            </div>
+          </li>
+          )}
+      </>
+    );
+  }
+}
