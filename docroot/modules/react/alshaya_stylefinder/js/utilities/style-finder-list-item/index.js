2346619c5959974811cb29ea8844d7e8fe8e7098
Avinash Shukla <avinash.shukla@acquia.com>
2020-12-11 15:27:37 +0530

    CORE-25499: Fixed component with class-less component.

--- docroot/modules/react/alshaya_stylefinder/js/utilities/style-finder-list-item/index.js
+++ docroot/modules/react/alshaya_stylefinder/js/utilities/style-finder-list-item/index.js
@@ -1,44 +1,36 @@
 import React from 'react';
 
-export default class StyleFinderListItem extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+const handleStep = (e, props) => {
+  const { answer, counter, handleStepSubmit } = props;
+  handleStepSubmit(e, answer.nid, answer.attrCode, answer.choice, counter);
+};
 
-  handleStepSubmit = (e, answer, attrCode, choice, counter) => {
-    const { handleStepSubmit } = this.props;
-    handleStepSubmit(e, answer, attrCode, choice, counter);
-  };
-
-  render() {
-    const { answer, counter } = this.props;
-
-    return (
-      <>
-        {answer
-          && (
-          <li
-            onClick={(e) => this.handleStepSubmit(
-              e,
-              answer.nid,
-              answer.attrCode,
-              answer.choice, counter,
-            )}
-            className="list-item style-finder-list-item"
-          >
-            <div className="style-finder-list-image">
-              <img src={answer.image_url} />
-            </div>
-            <div className="style-finder-list-title">
-              {answer.title}
-            </div>
-            <div className="style-finder-list-text">
-              {answer.description}
-            </div>
-          </li>
+const StyleFinderListItem = (props) => {
+  const { answer } = props;
+  return (
+    <>
+      {answer
+      && (
+        <li
+          onClick={(e) => handleStep(
+            e,
+            props,
           )}
-      </>
-    );
-  }
-}
+          className="list-item style-finder-list-item"
+        >
+          <div className="style-finder-list-image">
+            <img src={answer.image_url} />
+          </div>
+          <div className="style-finder-list-title">
+            {answer.title}
+          </div>
+          <div className="style-finder-list-text">
+            {answer.description}
+          </div>
+        </li>
+      )}
+    </>
+  );
+};
+
+export default StyleFinderListItem;
