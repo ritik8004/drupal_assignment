933f3935331e83b379e41b839f64aacd3e06979b
Rohit Joshi <joshi.rohit100@gmail.com>
2020-12-15 13:58:01 +0530

    CORE-26382: Adding missing file.

---
+++ docroot/middleware/src/Service/CartErrorCodes.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Service;
+
+/**
+ * Class CartErrorCodes.
+ *
+ * Class contains error codes we send to FE.
+ */
+final class CartErrorCodes {
+
+  /**
+   * Error code when cart has OOS item.
+   */
+  const CART_HAS_OOS_ITEM = 506;
+
+}
