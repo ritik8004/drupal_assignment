53779d426771647f0f0cd4128aa6d4e5078a2db4
Srijeeta <srijeeta.ghosh@innoraft.com>
2020-12-18 07:39:17 +0000

    CORE-23349: Remove promo error message on promo coupon application.

--- docroot/modules/react/alshaya_spc/js/cart/components/cart-promo-block/index.js
+++ docroot/modules/react/alshaya_spc/js/cart/components/cart-promo-block/index.js
@@ -239,6 +239,15 @@ export default class CartPromoBlock extends React.Component {
       }
     }
 
+    // Remove any promo coupons errors on promo
+    // coupon application success.
+    if (promoApplied) {
+      const promoError = document.querySelector('#promo-message.error');
+      if (promoError !== null) {
+        promoError.outerHTML = '<div id="promo-message" />';
+      }
+    }
+
     return (
       <div className="spc-promo-code-block fadeInUp" style={{ animationDelay: '0.4s' }}>
         <SectionTitle>{Drupal.t('have a promo code?')}</SectionTitle>
