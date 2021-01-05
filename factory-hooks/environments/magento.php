08f31a404ebd4c31d930238bc40e451cb83f083c
Sanjib-Dutta <sanjib.dutta@alshaya.com>
2021-01-05 12:44:22 +0300

    [5.14.4]CORE-26573:[Drupal FE] :  URL instance creation for WES UAT bahrain / Egypt / Qatar Market

--- factory-hooks/environments/magento.php
+++ factory-hooks/environments/magento.php
@@ -685,6 +685,12 @@ $magentos = [
     'eg' => [
       'store_id' => ['en' => 21, 'ar' => 18],
     ],
+    'bh' => [
+      'store_id' => ['en' => 25, 'ar' => 22],
+    ],
+    'qa' => [
+      'store_id' => ['en' => 31, 'ar' => 28],
+    ],
   ],
   'we_oms_uat' => [
     'url' => 'https://wes-uatoms.store.alshaya.com',
@@ -736,6 +742,12 @@ $magentos = [
     ],
     'eg' => [
       'store_id' => ['en' => 20, 'ar' => 17],
+    ],
+    'bh' => [
+      'store_id' => ['en' => 26, 'ar' => 23],
+    ],
+    'qa' => [
+      'store_id' => ['en' => 32, 'ar' => 29],
     ],
   ],
   'aeo_qa' => [
