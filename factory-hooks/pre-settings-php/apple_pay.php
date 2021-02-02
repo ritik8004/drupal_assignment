b8bfa5819aa73d3d1939a4396c84ed7a3a5f1e86
Avinash Shukla <avinash.shukla@acquia.com>
2021-02-02 15:23:13 +0530

    CORE-24815: Fixed apple pay ssl path for local environment and react component condition fixed.

--- factory-hooks/pre-settings-php/apple_pay.php
+++ factory-hooks/pre-settings-php/apple_pay.php
@@ -7,7 +7,13 @@
  * @see https://docs.acquia.com/site-factory/tiers/paas/workflow/hooks
  */
 
-$dir = $_SERVER['HOME'] . DIRECTORY_SEPARATOR . 'apple-pay-resources' . DIRECTORY_SEPARATOR;
+require_once DRUPAL_ROOT . '/../factory-hooks/environments/environments.php';
+$env = alshaya_get_site_environment();
+
+// Set home directory as per environment.
+($env === 'local') ? $home = '/home/vagrant' : $home = $_SERVER['HOME'];
+
+$dir = $home . DIRECTORY_SEPARATOR . 'apple-pay-resources' . DIRECTORY_SEPARATOR;
 $settings['apple_pay_secret_info']['merchantCertificateKey'] = $dir . 'merchant_id.key';
 $settings['apple_pay_secret_info']['merchantCertificatePem'] = $dir . 'merchant_id.pem';
 $settings['apple_pay_secret_info']['merchantCertificatePass'] = '';
