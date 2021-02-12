17195a97302e217a594054a95b2c34161534675d
vivekpan27 <vivek.panicker@innoraft.com>
2021-02-12 05:34:29 +0000

    CORE-27572: Purged cache tags on varnish on stock refresh.

--- docroot/modules/react/alshaya_spc/src/EventSubscriber/StockEventSubscriber.php
+++ docroot/modules/react/alshaya_spc/src/EventSubscriber/StockEventSubscriber.php
@@ -5,7 +5,10 @@ namespace Drupal\alshaya_spc\EventSubscriber;
 use Drupal\acq_commerce\SKUInterface;
 use Drupal\acq_sku\Entity\SKU;
 use Drupal\Core\Cache\Cache;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\purge\Plugin\Purge\Invalidation\InvalidationsServiceInterface;
+use Drupal\purge\Plugin\Purge\Processor\ProcessorsServiceInterface;
+use Drupal\purge\Plugin\Purge\Purger\PurgersServiceInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\PostResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -29,20 +32,53 @@ class StockEventSubscriber implements EventSubscriberInterface {
   /**
    * The logger service.
    *
-   * @var \Drupal\Core\Logger\LoggerChannelInterface
+   * @var \Psr\Log\LoggerInterface
    */
   protected $logger;
 
   /**
+   * The purgers service.
+   *
+   * @var \Drupal\purge\Plugin\Purge\Purger\PurgersServiceInterface
+   */
+  protected $purgers;
+
+  /**
+   * The purge processors service.
+   *
+   * @var \Drupal\purge\Plugin\Purge\Processor\ProcessorInterface
+   */
+  protected $purgeProcessor;
+
+  /**
+   * The purge invalidations factory service.
+   *
+   * @var \Drupal\purge\Plugin\Purge\Invalidation\InvalidationsServiceInterface
+   */
+  protected $purgeInvalidationsFactory;
+
+  /**
    * The class constructor.
    *
-   * @param Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
+   * @param \Psr\Log\LoggerInterface $logger
    *   The logger factory service.
+   * @param \Drupal\purge\Plugin\Purge\Purger\PurgersServiceInterface $purgers
+   *   The purgers service.
+   * @param \Drupal\purge\Plugin\Purge\Processor\ProcessorsServiceInterface $processors
+   *   The purge processors service.
+   * @param \Drupal\purge\Plugin\Purge\Invalidation\InvalidationsServiceInterface $purge_invalidations_factory
+   *   The purge invalidations factory service.
    */
   public function __construct(
-    LoggerChannelFactoryInterface $logger_factory
+    LoggerInterface $logger,
+    PurgersServiceInterface $purgers,
+    ProcessorsServiceInterface $processors,
+    InvalidationsServiceInterface $purge_invalidations_factory
   ) {
-    $this->logger = $logger_factory->get('alshaya_spc');
+    $this->logger = $logger;
+    $this->purgers = $purgers;
+    $this->purgeProcessor = $processors->get('lateruntime');
+    $this->purgeInvalidationsFactory = $purge_invalidations_factory;
   }
 
   /**
@@ -81,6 +117,7 @@ class StockEventSubscriber implements EventSubscriberInterface {
     }
 
     $cache_tags = [];
+    $purge_tags = [];
 
     foreach (self::$skusWithRefreshedStock as $sku) {
       $sku_entity = SKU::loadFromSku($sku);
@@ -89,10 +126,29 @@ class StockEventSubscriber implements EventSubscriberInterface {
       }
     }
 
+    if (empty($cache_tags)) {
+      return;
+    }
+
     Cache::invalidateTags($cache_tags);
-    $this->logger->info('Cleared cache tags for skus with refreshed stock: @skus', [
-      '@skus' => implode(',', self::$skusWithRefreshedStock),
-    ]);
+
+    // Now prepare data to purge the varnish cache.
+    foreach ($cache_tags as $cache_tag) {
+      $purge_tags[] = $this->purgeInvalidationsFactory->get('tag', $cache_tag);
+    }
+
+    try {
+      $this->purgers->invalidate($this->purgeProcessor, $purge_tags);
+      $this->logger->info('Invalidated cache tags on stock refresh for the following skus: @skus', [
+        '@skus' => implode(',', self::$skusWithRefreshedStock),
+      ]);
+    }
+    catch (\Exception $e) {
+      $this->logger->notice('Exception occurred while invalidating cache tags on stock refresh: @exception, for skus @skus', [
+        '@exception' => $e->getMessage(),
+        '@skus' => implode(',', self::$skusWithRefreshedStock),
+      ]);
+    }
   }
 
 }
