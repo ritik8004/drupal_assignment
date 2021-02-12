bca5df40e7acf6e0353771f47cbe82e048df5030
vikasmishra <vikaskumar.mishra@infosys.com>
2021-02-12 18:58:30 +0530

    CORE-19551: updated code as per the new approach and removed unwanted files.

--- docroot/modules/custom/alshaya_olapic/js/alshaya_olapic_widget.js
+++ docroot/modules/custom/alshaya_olapic/js/alshaya_olapic_widget.js
@@ -9,12 +9,36 @@
 
   Drupal.behaviors.alshayaOlapic = {
     attach: function (context) {
-      if (typeof drupalSettings.olapic_keys != "undefined") {
-        $('script[data-olapic="olapic_specific_widget"]').attr( 'data-apikey',drupalSettings.olapic_keys.data_apikey);
-        $('script[data-olapic="olapic_specific_widget"]').attr( 'data-instance',drupalSettings.olapic_keys.data_instance);
-        $('script[data-olapic="olapic_specific_widget"]').attr( 'data-lang',drupalSettings.olapic_keys.data_lang);
+        onOlapicLoad();
 
-      }
     }
   };
 })(jQuery, Drupal, drupalSettings);
+
+function doRender(options){
+  console.log(options);
+  window.olapic.prepareWidget(options, {
+      'renderNow' : true,
+      'force' : true // optional - overwrites the widget index on load
+  });
+};
+function onOlapicLoad() {
+  if (typeof drupalSettings.olapic_keys.data_apikey != "undefined") {
+    OlapicSDK.conf.set('apikey', drupalSettings.olapic_keys.data_apikey);
+    language = drupalSettings.olapic_keys.lang;
+    if(drupalSettings.olapic_keys.development_mode == 1){
+      OlapicSDK.conf.set('mode', 'development');
+    }
+  }
+  window.olapic = window.olapic || new OlapicSDK.Olapic( function (o) {
+      window.olapic = o;
+      $("div[id^='olapic-']").each(function () {
+        doRender({
+       'id': $(this).attr("data-instance"), //required
+       'wrapper': $(this).attr("id"),
+       'lang':language, //optional - only used when using translation engine
+        });
+    });
+
+  });
+};
