00a1ad11998dca3636ca8a9986533c4e2888d63f
anjali vijay <anjali.vijay@axelerant.com>
2021-02-12 16:58:11 +0530

    CORE-27314: Reviews component props converted to camelcase.

--- docroot/modules/react/alshaya_bazaar_voice/js/reviews/components/review-attributes/index.js
+++ docroot/modules/react/alshaya_bazaar_voice/js/reviews/components/review-attributes/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const ReviewAttributes = ({
-  ReviewAttributesData,
+  reviewAttributesData,
 }) => {
-  if (ReviewAttributesData !== undefined) {
+  if (reviewAttributesData !== undefined) {
     return (
       <div className="review-attributes">
         <div className="review-attributes-wrapper">
-          {Object.keys(ReviewAttributesData).map((item) => (
-            <div className="review-attributes-details" key={ReviewAttributesData[item].Id}>
-              <span className="attribute-name">{`${ReviewAttributesData[item].DimensionLabel}: `}</span>
-              <span className="attribute-value">{ReviewAttributesData[item].Value}</span>
+          {Object.keys(reviewAttributesData).map((item) => (
+            <div className="review-attributes-details" key={reviewAttributesData[item].Id}>
+              <span className="attribute-name">{`${reviewAttributesData[item].DimensionLabel}: `}</span>
+              <span className="attribute-value">{reviewAttributesData[item].Value}</span>
             </div>
           ))}
         </div>
