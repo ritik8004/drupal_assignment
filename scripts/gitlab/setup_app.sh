59674afe06ca3186d8ee5be12c312a7b0583f99c
DELBOSC <sylvain.delbosc@acquia.com>
2021-02-16 13:13:34 +0100

    Detect Gitlab CI

--- scripts/gitlab/setup_app.sh
+++ scripts/gitlab/setup_app.sh
@@ -3,7 +3,8 @@
 set -ev
 
 cd docroot
-drush @${drush.aliases.ci} sql-connect
+drush sa
+drush sql-connect
 vendor/bin/blt setup --define drush.alias='${drush.aliases.ci}' --environment=ci --no-interaction --ansi --verbose
 
 set +v
