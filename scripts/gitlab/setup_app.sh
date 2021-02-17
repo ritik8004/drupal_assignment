45b7887ce72a940cb5fb1e8a5ecbc24eb222aae0
DELBOSC <sylvain.delbosc@acquia.com>
2021-02-17 07:44:54 +0100

    Detect Gitlab CI

--- scripts/gitlab/setup_app.sh
+++ scripts/gitlab/setup_app.sh
@@ -2,6 +2,8 @@
 
 set -ev
 
+mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --host=mysql -e "show databases"
+mysql -u drupal8 -h mysql -pdrupal8 -P 3306 -e "show databases"
 mysql -u drupal -h mysql -pdrupal -P 3306 -e "show databases"
 vendor/bin/blt setup --define drush.alias='${drush.aliases.ci}' --environment=ci --no-interaction --ansi --verbose
 
