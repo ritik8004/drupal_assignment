69fa60c713682925ac47f0f04d832eda2ccc5908
DELBOSC <sylvain.delbosc@acquia.com>
2021-02-17 10:55:47 +0100

    Detect Gitlab CI

--- scripts/gitlab/setup_env.sh
+++ scripts/gitlab/setup_env.sh
@@ -18,4 +18,7 @@ echo "CREATE USER 'drupal'@'%' IDENTIFIED BY 'drupal';" | mysql --user=root --pa
 echo "GRANT ALL ON drupal.* TO 'drupal'@'%';" | mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --host=mysql
 echo "FLUSH PRIVILEGES;" | mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --host=mysql
 
+apt install memcached
+netstat -taunpe | grep LISTEN
+
 set +v
