89d03c9e7857a4ed20a5270476bd58520e010971
Mohammad Rashid <mohammad.rashid@alshaya.com>
2021-02-24 07:03:08 +0000

    CORE-27837: Added param comment and change variable in camel case.

--- docroot/modules/react/alshaya_bazaar_voice/js/rating/components/widgets/InlineRating.js
+++ docroot/modules/react/alshaya_bazaar_voice/js/rating/components/widgets/InlineRating.js
@@ -4,30 +4,30 @@ import DisplayStar from '../stars/DisplayStar';
 import smoothScrollTo from '../../../utilities/smoothScroll';
 
 const InlineRating = ({
-  ReviewsData,
+  reviewsData,
 }) => (
   <div className="inline-rating">
-    { Object.keys(ReviewsData).map((item) => (
+    { Object.keys(reviewsData).map((item) => (
       <div className="aggregate-rating" key={item} itemProp="aggregateRating" itemScope="" itemType="">
         <div className="empty-stars">
           <DisplayStar
-            starPercentage={ReviewsData[item].ReviewStatistics.AverageOverallRating}
+            starPercentage={reviewsData[item].ReviewStatistics.AverageOverallRating}
           />
           <div className="histogram-data">
             <div className="histogram-title">
-              {ReviewsData[item].ReviewStatistics.TotalReviewCount}
+              {reviewsData[item].ReviewStatistics.TotalReviewCount}
               {' '}
               {Drupal.t('reviews')}
             </div>
             <RatingSummary
-              HistogramData={ReviewsData[item].ReviewStatistics.RatingDistribution}
-              TotalReviewCount={ReviewsData[item].ReviewStatistics.TotalReviewCount}
+              HistogramData={reviewsData[item].ReviewStatistics.RatingDistribution}
+              TotalReviewCount={reviewsData[item].ReviewStatistics.TotalReviewCount}
             />
           </div>
         </div>
         <span>
           (
-          <a onClick={(e) => smoothScrollTo(e, '#reviews-section')} href="#">{ReviewsData[item].ReviewStatistics.TotalReviewCount}</a>
+          <a onClick={(e) => smoothScrollTo(e, '#reviews-section')} href="#">{reviewsData[item].ReviewStatistics.TotalReviewCount}</a>
           )
         </span>
       </div>
