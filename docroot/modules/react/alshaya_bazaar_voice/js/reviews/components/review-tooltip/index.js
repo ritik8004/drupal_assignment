3c624f83faffc076262ca6e9c1af1b3c048b1af9
Sanket Jain <sanket.jain@pimocre.services>
2021-02-25 17:23:27 +0530

    CORE-27837: Breaking review tooltip components into child components.

--- docroot/modules/react/alshaya_bazaar_voice/js/reviews/components/review-tooltip/index.js
+++ docroot/modules/react/alshaya_bazaar_voice/js/reviews/components/review-tooltip/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import UserReviewsDetails from '../user-reviews-details';
+import UserPersonalDetails from '../user-personal-details';
 
 const ReviewTooltip = ({
   reviewTooltipData,
@@ -13,28 +15,15 @@ const ReviewTooltip = ({
           <div className="user-location">{reviewTooltipData.UserLocation}</div>
         </div>
         <div className="user-review-wrapper">
-          <div className="user-reviews-details">
-            <div className="review-count">
-              <div className="label">{Drupal.t('Review')}</div>
-              <div className="value">{reviewRelatedCount.TotalReviewCount}</div>
-            </div>
-            <div className="review-vote">
-              <div className="label">{Drupal.t('Vote')}</div>
-              <div className="value">{reviewRelatedCount.HelpfulVoteCount}</div>
-            </div>
-          </div>
-          <div className="user-personal-details">
-            <div className="user-attributes">
-              <span className="user-name">{`${reviewTooltipData.UserNickname}:`}</span>
-              <span className="user-attribute-value">{reviewContextData.Age.Value}</span>
-            </div>
-            {(reviewContextData.Gender !== undefined) ? (
-              <div className="user-attributes">
-                <span className="user-name">{`${reviewContextData.Gender.DimensionLabel}: `}</span>
-                <span className="user-attribute-value">{reviewContextData.Gender.Value}</span>
-              </div>
-            ) : null}
-          </div>
+          <UserReviewsDetails
+            totalReviewCount={reviewRelatedCount.TotalReviewCount}
+            helpfulVoteCount={reviewRelatedCount.HelpfulVoteCount}
+          />
+          <UserPersonalDetails
+            userNickname={reviewTooltipData.UserNickname}
+            userAgeValue={reviewContextData.Age.Value}
+            userGender={reviewContextData.Gender}
+          />
         </div>
       </div>
     );
