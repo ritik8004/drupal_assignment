c6f864ad799c81ba74eedeb9fa3da4c09372f2ea
Swarad <swarad07@gmail.com>
2021-03-15 15:31:25 +0530

    CORE-28318: Fixing redundant code.

--- docroot/modules/react/alshaya_spc/js/cart/components/postpay/postpay/index.js
+++ docroot/modules/react/alshaya_spc/js/cart/components/postpay/postpay/index.js
@@ -8,8 +8,7 @@ const PostpayCart = (props) => {
     && !(mobileOnly && window.innerWidth >= 768)) {
     return (
       <div
-        className={`${classNames} ${drupalSettings.postpay_widget_info.class} fadeInUp`}
-        style={{ animationDelay: '0.5s' }}
+        className={`${classNames} ${drupalSettings.postpay_widget_info.class}`}
         data-type={drupalSettings.postpay_widget_info['data-type']}
         data-amount={(amount * drupalSettings.postpay.currency_multiplier).toFixed(0)}
         data-currency={drupalSettings.postpay_widget_info['data-currency']}
