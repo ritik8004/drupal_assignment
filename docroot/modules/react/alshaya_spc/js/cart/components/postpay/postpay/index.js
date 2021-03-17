54fd1d526d6b913409a81eb829337549c43527c8
Mehul <mehul.shah@axelerant.com>
2021-03-17 17:57:01 +0530

    CORE-28218: Hide or show postpay content based on the config value.

--- docroot/modules/react/alshaya_spc/js/cart/components/postpay/postpay/index.js
+++ docroot/modules/react/alshaya_spc/js/cart/components/postpay/postpay/index.js
@@ -7,14 +7,16 @@ const PostpayCart = (props) => {
   if (isCartPage === true
     && !(mobileOnly && window.innerWidth >= 768)) {
     return (
-      <div
-        className={`${classNames} ${drupalSettings.postpay_widget_info.class}`}
-        data-type={drupalSettings.postpay_widget_info['data-type']}
-        data-amount={(amount * drupalSettings.postpay.currency_multiplier).toFixed(0)}
-        data-currency={drupalSettings.postpay_widget_info['data-currency']}
-        data-num-instalments={drupalSettings.postpay_widget_info['data-num-instalments']}
-        data-locale={drupalSettings.postpay_widget_info['data-locale']}
-      />
+      <div className={`${drupalSettings.postpay_widget_info.postpay_mode_class}`}>
+        <div
+          className={`${classNames} ${drupalSettings.postpay_widget_info.class}`}
+          data-type={drupalSettings.postpay_widget_info['data-type']}
+          data-amount={(amount * drupalSettings.postpay.currency_multiplier).toFixed(0)}
+          data-currency={drupalSettings.postpay_widget_info['data-currency']}
+          data-num-instalments={drupalSettings.postpay_widget_info['data-num-instalments']}
+          data-locale={drupalSettings.postpay_widget_info['data-locale']}
+        />
+      </div>
     );
   }
   return (null);
