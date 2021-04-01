65d3063f177afc750a63eccbd8bc1a2ad83cc496
Sanket Jain <sanket.jain@pimocre.services>
2021-04-01 17:16:32 +0530

    CORE-29021: Code refactoring as per PR feedback.

--- docroot/modules/react/alshaya_bazaar_voice/js/src/myaccount/components/user-reviews-desc/index.js
+++ docroot/modules/react/alshaya_bazaar_voice/js/src/myaccount/components/user-reviews-desc/index.js
@@ -1,35 +1,29 @@
 import React from 'react';
 import { getDate } from '../../../../../../js/utilities/dateUtility';
+import ConditionalView from '../../../common/components/conditional-view';
 import ReviewPhoto from '../../../reviews/components/review-photo';
 
-export default class UserReviewsDescription extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
+const UserReviewsDescription = ({
+  reviewsIndividualSummary,
+}) => {
+  if (reviewsIndividualSummary === null) {
+    return null;
   }
-
-  render() {
-    const {
-      reviewsIndividualSummary,
-    } = this.props;
-    if (reviewsIndividualSummary === null) {
-      return null;
-    }
-    const reviewDate = getDate(reviewsIndividualSummary.SubmissionTime);
-    return (
-      <div className="reviews-block">
-        <div className="review-title">{reviewsIndividualSummary.Title}</div>
-        <div className="review-date">{reviewDate}</div>
-        <div className="review-text">{reviewsIndividualSummary.ReviewText}</div>
-        <div className="review-photos">
-          {
-                (reviewsIndividualSummary.Photos && reviewsIndividualSummary.Photos.length > 0)
-                  ? <ReviewPhoto photoCollection={reviewsIndividualSummary.Photos} />
-                  : null
-            }
-        </div>
+  const reviewDate = getDate(reviewsIndividualSummary.SubmissionTime);
+  return (
+    <div className="reviews-block">
+      <div className="review-title">{reviewsIndividualSummary.Title}</div>
+      <div className="review-date">{reviewDate}</div>
+      <div className="review-text">{reviewsIndividualSummary.ReviewText}</div>
+      <div className="review-photos">
+        <ConditionalView condition={reviewsIndividualSummary.Photos
+          && reviewsIndividualSummary.Photos.length > 0}
+        >
+          <ReviewPhoto photoCollection={reviewsIndividualSummary.Photos} />
+        </ConditionalView>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default UserReviewsDescription;
--- docroot/modules/react/alshaya_bazaar_voice/js/src/myaccount/components/user-reviews-products/index.js
+++ docroot/modules/react/alshaya_bazaar_voice/js/src/myaccount/components/user-reviews-products/index.js
@@ -1,37 +1,30 @@
 import React from 'react';
 
-export default class UserReviewsProducts extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
+const UserReviewsProducts = ({
+  reviewsIndividualSummary,
+  reviewsProduct,
+}) => {
+  if (reviewsIndividualSummary === null && reviewsProduct === null) {
+    return null;
   }
-
-  render() {
-    const {
-      reviewsIndividualSummary,
-      reviewsProduct,
-    } = this.props;
-    if (reviewsIndividualSummary === null && reviewsProduct === null) {
-      return null;
-    }
-    return (
-      <div className="product-block">
-        <div className="product-image-block">
-          <img src={reviewsProduct[reviewsIndividualSummary.ProductId].ImageUrl} />
-        </div>
-        <div className="product-title">
-          <a href={reviewsProduct[reviewsIndividualSummary.ProductId].ProductPageUrl}>
-            <span>{reviewsProduct[reviewsIndividualSummary.ProductId].Name}</span>
-          </a>
-        </div>
-        <div className="product-item-code">
-          {Drupal.t('Item Code')}
-          {' '}
-          :
-          { reviewsProduct[reviewsIndividualSummary.ProductId].Id }
-        </div>
+  return (
+    <div className="product-block">
+      <div className="product-image-block">
+        <img src={reviewsProduct[reviewsIndividualSummary.ProductId].ImageUrl} />
       </div>
-    );
-  }
-}
+      <div className="product-title">
+        <a href={reviewsProduct[reviewsIndividualSummary.ProductId].ProductPageUrl}>
+          <span>{reviewsProduct[reviewsIndividualSummary.ProductId].Name}</span>
+        </a>
+      </div>
+      <div className="product-item-code">
+        {Drupal.t('Item Code')}
+        {' '}
+        :
+        { reviewsProduct[reviewsIndividualSummary.ProductId].Id }
+      </div>
+    </div>
+  );
+};
+
+export default UserReviewsProducts;
