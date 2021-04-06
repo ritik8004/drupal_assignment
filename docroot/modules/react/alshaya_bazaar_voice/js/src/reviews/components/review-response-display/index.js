225561d99f2c81cb05ac36ca20633c01923bfe16
Sanket Jain <sanket.jain@pimocre.services>
2021-04-06 15:35:49 +0530

    CORE-28829: Extra if condition removed.

--- docroot/modules/react/alshaya_bazaar_voice/js/src/reviews/components/review-response-display/index.js
+++ docroot/modules/react/alshaya_bazaar_voice/js/src/reviews/components/review-response-display/index.js
@@ -10,31 +10,29 @@ const ReviewResponseDisplay = ({
   }
   TimeAgo.addLocale(en);
   const timeAgo = new TimeAgo('en-US');
-  if (reviewResponses !== undefined && reviewResponses !== null) {
-    let keyIndex = 0;
-    const reviewResponseDisplay = Object.values(reviewResponses).map((responseObj) => {
-      if (responseObj.response !== null) {
-        return ([
-          <div className="response-submission-wrapper" key={keyIndex}>
-            <div className="response-submission-box">
-              <div className="response-user-details">
-                <span className="response-user-name">{responseObj.Name}</span>
-                <span className="response-submission-date">{timeAgo.format(new Date(responseObj.Date))}</span>
-              </div>
-              <div className="response-description">
-                <span className="response-description-text">{responseObj.Response}</span>
-              </div>
+  let keyIndex = 0;
+  const reviewResponseDisplay = Object.values(reviewResponses).map((responseObj) => {
+    if (responseObj.response !== null) {
+      return ([
+        <div className="response-submission-wrapper" key={keyIndex}>
+          <div className="response-submission-box">
+            <div className="response-user-details">
+              <span className="response-user-name">{responseObj.Name}</span>
+              <span className="response-submission-date">{timeAgo.format(new Date(responseObj.Date))}</span>
             </div>
-          </div>,
-        ]);
-      }
-      keyIndex += 1;
-      return '';
-    }, {});
-
-    if (reviewResponseDisplay && reviewResponseDisplay.length > 0) {
-      return reviewResponseDisplay;
+            <div className="response-description">
+              <span className="response-description-text">{responseObj.Response}</span>
+            </div>
+          </div>
+        </div>,
+      ]);
     }
+    keyIndex += 1;
+    return '';
+  }, {});
+
+  if (reviewResponseDisplay && reviewResponseDisplay.length > 0) {
+    return reviewResponseDisplay;
   }
   return (null);
 };
