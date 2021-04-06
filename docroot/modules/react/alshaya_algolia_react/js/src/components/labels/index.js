65d9fc0ca4ed6ed73b01272414bc392cb2e25ce5
Swarad <swarad07@gmail.com>
2021-04-06 18:41:28 +0530

    CORE-24976: Multiple lozanges fixes in CORE.

--- docroot/modules/react/alshaya_algolia_react/js/src/components/labels/index.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/components/labels/index.js
@@ -5,17 +5,60 @@ const Labels = ({ labels, sku }) => {
   if (typeof labels === 'undefined' || labels.length === 0) {
     return (null);
   }
-  const labelItems = labels.map(({ image, position }) => (
-    <div className={`label ${position}`} key={image.url}>
-      <ImageElement src={image.url} alt={image.alt} title={image.title} />
-    </div>
-  ));
 
-  return (
-    <div className="labels-container" data-type="plp" data-sku={sku} data-main-sku={sku}>
-      {labelItems}
-    </div>
-  );
+  if (!(labels && Array.isArray(labels) && labels.length)) { return null; }
+
+  const bifercatedLabels = labels.reduce((aggregator, item) => {
+    const currentAggregation = { ...aggregator };
+    const currentItem = { ...item };
+    currentItem.position = currentItem.position || 'top-left';
+    if (!(currentItem.position in currentAggregation)) {
+      currentAggregation[currentItem.position] = [];
+    }
+    currentAggregation[currentItem.position].push(currentItem);
+    return currentAggregation;
+  }, {});
+
+  const bifercatedLabelsList = Object.keys(bifercatedLabels);
+
+  if (bifercatedLabelsList && Array.isArray(bifercatedLabelsList) && bifercatedLabelsList.length) {
+    return (
+      <>
+        <div className="product-labels">
+          <div className="labels-wrapper" data-type="plp" data-sku={sku} data-main-sku={sku}>
+            {
+              bifercatedLabelsList.map((key) => (
+                <div className={`labels-container ${key}`} key={`${key}-label-container`}>
+                  <LabelItems
+                    bifercatedLabels={bifercatedLabels}
+                    directionKey={key}
+                  />
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </>
+    );
+  }
+  return null;
 };
+
+const LabelItems = ({ bifercatedLabels, directionKey }) => (
+  <>
+    {
+      bifercatedLabels[directionKey].map((labelItem) => (
+        // BE to provide and add a unique key here.
+        <div className="label" key={labelItem.image.url}>
+          <ImageElement
+            src={labelItem.image.url}
+            alt={labelItem.image.alt}
+            title={labelItem.image.title}
+          />
+        </div>
+      ))
+    }
+  </>
+);
 
 export default Labels;
--- docroot/modules/react/alshaya_spc/js/utilities/product-lozenges/index.js
+++ docroot/modules/react/alshaya_spc/js/utilities/product-lozenges/index.js
@@ -4,22 +4,61 @@ const ProductLozenges = ({ labels, sku }) => {
   if (typeof labels === 'undefined' || labels.length === 0) {
     return (null);
   }
-  const lozengesItems = labels.map(({ image, position, text }) => (
-    <div className={`label ${position}`} key={image}>
-      <img
-        src={image}
-        alt={text || ''}
-        title={text || ''}
-        loading="lazy"
-      />
-    </div>
-  ));
 
-  return (
-    <div className="labels-container product-lozenges-container" data-type="spc-recommended-products" data-sku={sku} data-main-sku={sku}>
-      {lozengesItems}
-    </div>
-  );
+  if (!(labels && Array.isArray(labels) && labels.length)) { return null; }
+
+  const bifercatedLabels = labels.reduce((aggregator, item) => {
+    const currentAggregation = { ...aggregator };
+    const currentItem = { ...item };
+    currentItem.position = currentItem.position || 'top-left';
+    if (!(currentItem.position in currentAggregation)) {
+      currentAggregation[currentItem.position] = [];
+    }
+    currentAggregation[currentItem.position].push(currentItem);
+    return currentAggregation;
+  }, {});
+
+  const bifercatedLabelsList = Object.keys(bifercatedLabels);
+
+  if (bifercatedLabelsList && Array.isArray(bifercatedLabelsList) && bifercatedLabelsList.length) {
+    return (
+      <>
+        <div className="product-labels">
+          <div className="labels-wrapper" data-type="plp" data-sku={sku} data-main-sku={sku}>
+            {
+              bifercatedLabelsList.map((key) => (
+                <div className={`labels-container ${key}`} key={`${key}-label-container`}>
+                  <LabelItems
+                    bifercatedLabels={bifercatedLabels}
+                    directionKey={key}
+                  />
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </>
+    );
+  }
+  return null;
 };
+
+const LabelItems = ({ bifercatedLabels, directionKey }) => (
+  <>
+    {
+      bifercatedLabels[directionKey].map((labelItem) => (
+        // BE to provide and add a unique key here.
+        <div className="label testing" key={labelItem.image}>
+          <img
+            src={labelItem.image}
+            alt={labelItem.text || ''}
+            title={labelItem.text || ''}
+            loading="lazy"
+          />
+        </div>
+      ))
+    }
+  </>
+);
 
 export default ProductLozenges;
