5e381b1123d14b633e8dd089637590d88b6e4e03
Ankit Vats <ankitvats@MacBook-Pro.local>
2021-04-09 11:35:32 +0530

    CORE-27390: Code optimisation for config and formatter.

--- docroot/modules/custom/alshaya_media/src/Plugin/Field/FieldFormatter/AlshayaOembedFormatter.php
+++ docroot/modules/custom/alshaya_media/src/Plugin/Field/FieldFormatter/AlshayaOembedFormatter.php
@@ -2,25 +2,14 @@
 
 namespace Drupal\alshaya_media\Plugin\Field\FieldFormatter;
 
-use Drupal\Core\Cache\CacheableMetadata;
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Field\FieldDefinitionInterface;
-use Drupal\Core\Field\FieldItemListInterface;
-use Drupal\Core\Field\FormatterBase;
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
-use Drupal\Core\Messenger\MessengerInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Url;
-use Drupal\media\Entity\MediaType;
-use Drupal\media\IFrameUrlHelper;
 use Drupal\media\OEmbed\Resource;
-use Drupal\media\OEmbed\ResourceException;
-use Drupal\media\OEmbed\ResourceFetcherInterface;
-use Drupal\media\OEmbed\UrlResolverInterface;
-use Drupal\media\Plugin\media\Source\OEmbedInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Component\Utility\Html;
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\media\OEmbed\ResourceException;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\media\Plugin\Field\FieldFormatter\OEmbedFormatter;
 
 /**
  * Plugin implementation of the 'alshaya oembed' formatter.
@@ -35,110 +24,7 @@ use Drupal\Component\Utility\Html;
  *   },
  * )
  */
-class AlshayaOembedFormatter extends FormatterBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * The messenger service.
-   *
-   * @var \Drupal\Core\Messenger\MessengerInterface
-   */
-  protected $messenger;
-
-  /**
-   * The oEmbed resource fetcher.
-   *
-   * @var \Drupal\media\OEmbed\ResourceFetcherInterface
-   */
-  protected $resourceFetcher;
-
-  /**
-   * The oEmbed URL resolver service.
-   *
-   * @var \Drupal\media\OEmbed\UrlResolverInterface
-   */
-  protected $urlResolver;
-
-  /**
-   * The logger service.
-   *
-   * @var \Psr\Log\LoggerInterface
-   */
-  protected $logger;
-
-  /**
-   * The media settings config.
-   *
-   * @var \Drupal\Core\Config\ImmutableConfig
-   */
-  protected $config;
-
-  /**
-   * The iFrame URL helper service.
-   *
-   * @var \Drupal\media\IFrameUrlHelper
-   */
-  protected $iFrameUrlHelper;
-
-  /**
-   * Constructs an OEmbedFormatter instance.
-   *
-   * @param string $plugin_id
-   *   The plugin ID for the formatter.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
-   *   The definition of the field to which the formatter is associated.
-   * @param array $settings
-   *   The formatter settings.
-   * @param string $label
-   *   The formatter label display setting.
-   * @param string $view_mode
-   *   The view mode.
-   * @param array $third_party_settings
-   *   Any third party settings.
-   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
-   *   The messenger service.
-   * @param \Drupal\media\OEmbed\ResourceFetcherInterface $resource_fetcher
-   *   The oEmbed resource fetcher service.
-   * @param \Drupal\media\OEmbed\UrlResolverInterface $url_resolver
-   *   The oEmbed URL resolver service.
-   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
-   *   The logger factory service.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The config factory service.
-   * @param \Drupal\media\IFrameUrlHelper $iframe_url_helper
-   *   The iFrame URL helper service.
-   */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, MessengerInterface $messenger, ResourceFetcherInterface $resource_fetcher, UrlResolverInterface $url_resolver, LoggerChannelFactoryInterface $logger_factory, ConfigFactoryInterface $config_factory, IFrameUrlHelper $iframe_url_helper) {
-    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
-    $this->messenger = $messenger;
-    $this->resourceFetcher = $resource_fetcher;
-    $this->urlResolver = $url_resolver;
-    $this->logger = $logger_factory->get('media');
-    $this->config = $config_factory->get('media.settings');
-    $this->iFrameUrlHelper = $iframe_url_helper;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $plugin_id,
-      $plugin_definition,
-      $configuration['field_definition'],
-      $configuration['settings'],
-      $configuration['label'],
-      $configuration['view_mode'],
-      $configuration['third_party_settings'],
-      $container->get('messenger'),
-      $container->get('media.oembed.resource_fetcher'),
-      $container->get('media.oembed.url_resolver'),
-      $container->get('logger.factory'),
-      $container->get('config.factory'),
-      $container->get('media.oembed.iframe_url_helper')
-    );
-  }
+class AlshayaOembedFormatter extends OEmbedFormatter {
 
   /**
    * {@inheritdoc}
@@ -176,22 +62,8 @@ class AlshayaOembedFormatter extends FormatterBase implements ContainerFactoryPl
         continue;
       }
 
-      if ($resource->getType() === Resource::TYPE_LINK) {
-        $element[$delta] = [
-          '#title' => $resource->getTitle(),
-          '#type' => 'link',
-          '#url' => Url::fromUri($value),
-        ];
-      }
-      elseif ($resource->getType() === Resource::TYPE_PHOTO) {
-        $element[$delta] = [
-          '#theme' => 'image',
-          '#uri' => $resource->getUrl()->toString(),
-          '#width' => $width ?: $resource->getWidth(),
-          '#height' => $height ?: $resource->getHeight(),
-        ];
-      }
-      else {
+      if ($resource->getType() === Resource::TYPE_VIDEO ||
+      $resource->getType() === Resource::TYPE_RICH) {
         $url = Url::fromRoute('media.oembed_iframe', [], [
           'query' => [
             'url' => $value,
@@ -312,26 +184,6 @@ class AlshayaOembedFormatter extends FormatterBase implements ContainerFactoryPl
     ]);
 
     return $summary;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function isApplicable(FieldDefinitionInterface $field_definition) {
-    if ($field_definition->getTargetEntityTypeId() !== 'media') {
-      return FALSE;
-    }
-
-    if (parent::isApplicable($field_definition)) {
-      $media_type = $field_definition->getTargetBundle();
-
-      if ($media_type) {
-        $media_type = MediaType::load($media_type);
-        return $media_type && $media_type->getSource() instanceof OEmbedInterface;
-      }
-    }
-
-    return FALSE;
   }
 
 }
