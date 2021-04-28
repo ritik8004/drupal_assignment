12c6bb202beaec1b150ae1e2db2fc5f9fdd527fb
Mehul <mehul.shah@axelerant.com>
2021-04-28 12:59:36 +0530

    CORE-29497: Used react hooks to load widget.

--- docroot/modules/react/alshaya_spc/js/cart/components/postpay/postpay/index.js
+++ docroot/modules/react/alshaya_spc/js/cart/components/postpay/postpay/index.js
@@ -1,34 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-class PostpayCart extends React.Component {
-  componentDidMount() {
-    const { pageType } = this.props;
+const PostpayCart = (props) => {
+  const {
+    amount, classNames, mobileOnly, pageType,
+  } = props;
+
+  useEffect(() => {
     if (pageType === 'pdp') {
       window.postpay.ui.refresh();
     }
-  }
+  });
 
-  render() {
-    const {
-      amount, classNames, mobileOnly, pageType,
-    } = this.props;
-    if ((pageType === 'cart' && !(mobileOnly && window.innerWidth >= 768))
-      || pageType === 'pdp') {
-      return (
-        <div className={`postpay ${drupalSettings.postpay_widget_info.postpay_mode_class}`}>
-          <div
-            className={`${classNames} ${drupalSettings.postpay_widget_info.class}`}
-            data-type={drupalSettings.postpay_widget_info['data-type']}
-            data-amount={(amount * drupalSettings.postpay.currency_multiplier).toFixed(0)}
-            data-currency={drupalSettings.postpay_widget_info['data-currency']}
-            data-num-instalments={drupalSettings.postpay_widget_info['data-num-instalments']}
-            data-locale={drupalSettings.postpay_widget_info['data-locale']}
-          />
-        </div>
-      );
-    }
-    return (null);
+  if ((pageType === 'cart' && !(mobileOnly && window.innerWidth >= 768))
+    || pageType === 'pdp') {
+    return (
+      <div className={`postpay ${drupalSettings.postpay_widget_info.postpay_mode_class}`}>
+        <div
+          className={`${classNames} ${drupalSettings.postpay_widget_info.class}`}
+          data-type={drupalSettings.postpay_widget_info['data-type']}
+          data-amount={(amount * drupalSettings.postpay.currency_multiplier).toFixed(0)}
+          data-currency={drupalSettings.postpay_widget_info['data-currency']}
+          data-num-instalments={drupalSettings.postpay_widget_info['data-num-instalments']}
+          data-locale={drupalSettings.postpay_widget_info['data-locale']}
+        />
+      </div>
+    );
   }
-}
+  return (null);
+};
 
 export default PostpayCart;
