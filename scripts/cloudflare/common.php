e238ecf3ab6fce8c1b36774a8c57d724e85cc6ce
Nikunj Kotecha <nikunj.kotecha@acquia.com>
2021-05-10 07:43:15 +0530

    CORE-29313: Add utility code to clear CF cache for urls with language prefixes.

---
+++ scripts/cloudflare/common.php
@@ -0,0 +1,82 @@
+<?php
+// @codingStandardsIgnoreFile
+
+if (file_exists(__DIR__ . '/settings.php')) {
+  require_once 'settings.php';
+}
+else {
+  $home = isset($_ENV['AH_SITE_ENVIRONMENT']) ? $_SERVER['HOME'] : '/home/vagrant';
+  require_once $home . '/alshaya_cloudflare_settings.php';
+}
+
+function get_zone_for_domain(string $domain) {
+  // Remove the first level.
+  $domain_to_check = explode('.', $domain);
+  array_shift($domain_to_check);
+  $domain_to_check = implode('.', $domain_to_check);
+
+  $data = [
+    'page' => 1,
+    'per_page' => 20,
+    'name' => $domain_to_check,
+  ];
+
+  $api_url = 'https://api.cloudflare.com/client/v4/zones';
+  $api_url .= '?' . http_build_query($data);
+
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $api_url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+
+  $headers = [];
+  $headers[] = 'Content-Type: application/json';
+  $headers[] = 'Authorization: Bearer ' . $GLOBALS['api_key'];
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+  $result = curl_exec($ch);
+  if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+    return '';
+  }
+  curl_close($ch);
+
+  $result = json_decode($result, TRUE);
+  foreach ($result['result'] ?? [] as $row) {
+    return $row['id'];
+  }
+
+  return '';
+}
+
+function clear_cache_for_domain(string $zone, string $domain) {
+  $data = [
+    'prefixes' => [
+      $domain . '/en',
+      $domain . '/ar',
+    ],
+  ];
+
+  $api_url = 'https://api.cloudflare.com/client/v4/zones/' . $zone . '/purge_cache';
+
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $api_url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+  $headers = [];
+  $headers[] = 'Content-Type: application/json';
+  $headers[] = 'Authorization: Bearer ' . $GLOBALS['api_key'];
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+  $result = curl_exec($ch);
+  if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+    return '';
+  }
+  curl_close($ch);
+
+  return $result;
+}
