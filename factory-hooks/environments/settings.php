db7d539bf20b064290ecde541122a860f8a83565
joshirohit100 <joshi.rohit100@gmail.com>
2021-05-11 13:03:13 +0530

    CORE-29805: Correct the default settings value.

--- factory-hooks/environments/settings.php
+++ factory-hooks/environments/settings.php
@@ -345,10 +345,10 @@ function alshaya_get_additional_settings($site_code, $country_code, $env) {
           'dynamic_yield.settings' => [
             'section_id' => '9877876',
           ],
-          'live' => [
-            'dynamic_yield.settings' => [
-              'section_id' => '9877879',
-            ],
+        ],
+        'live' => [
+          'dynamic_yield.settings' => [
+            'section_id' => '9877879',
           ],
         ],
       ],
@@ -399,6 +399,9 @@ function alshaya_get_additional_settings($site_code, $country_code, $env) {
   }
   if (isset($mapping[$site_code][$country_code]['default'])) {
     $settings = array_replace_recursive($settings, $mapping[$site_code][$country_code]['default']);
+  }
+  if (isset($mapping[$site_code]['default'][$env])) {
+    $settings = array_replace_recursive($settings, $mapping[$site_code]['default'][$env]);
   }
   if (isset($mapping[$site_code][$country_code][$env])) {
     $settings = array_replace_recursive($settings, $mapping[$site_code][$country_code][$env]);
