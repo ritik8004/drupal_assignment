148c6a06d1f56329dd3832cf76c900cd97f343fd
vivekpan27 <vivek.panicker@innoraft.com>
2021-05-20 12:53:05 +0000

    CORE-29684: Added specific utility function for add to cart.

--- docroot/modules/react/alshaya_spc/assets/js/add_to_cart.js
+++ docroot/modules/react/alshaya_spc/assets/js/add_to_cart.js
@@ -97,7 +97,7 @@
             }
 
             // Post to ajax for cart update/create.
-            window.commerceBackend.updateCart(post_data)
+            window.commerceBackend.addToCart(post_data)
               .then (function (responseData) {
                 const response = responseData.data;
                 // If there any error we throw from middleware.
--- docroot/modules/react/alshaya_spc/js/backend/v1/cart.js
+++ docroot/modules/react/alshaya_spc/js/backend/v1/cart.js
@@ -4,6 +4,17 @@ drupalSettings = window.drupalSettings;
 window.commerceBackend = window.commerceBackend || {};
 
 /**
+ * Calls the update cart middleware API.
+ *
+ * @param {object} data
+ *   The data object to send in the API call.
+ *
+ * @returns {Promise}
+ *   A promise object.
+ */
+const updateCart = (data) => callMiddlewareApi('cart/update', 'POST', JSON.stringify(data));
+
+/**
  * Check if user is anonymous and without cart.
  *
  * @returns bool
@@ -27,7 +38,7 @@ window.commerceBackend.getCart = () => callMiddlewareApi('cart/get', 'GET');
 window.commerceBackend.restoreCart = () => callMiddlewareApi('cart/restore', 'GET');
 
 /**
- * Calls the cart update API.
+ * Adds item to the cart and returns the cart.
  *
  * @param {object} data
  *   The data object to send in the API call.
@@ -35,4 +46,4 @@ window.commerceBackend.restoreCart = () => callMiddlewareApi('cart/restore', 'GE
  * @returns {Promise}
  *   A promise object.
  */
-window.commerceBackend.updateCart = (data) => callMiddlewareApi('cart/update', 'POST', JSON.stringify(data));
+window.commerceBackend.addToCart = (data) => updateCart(data);
