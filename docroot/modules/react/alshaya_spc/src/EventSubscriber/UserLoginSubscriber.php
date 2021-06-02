7d53aee46d6f5a778ddff186f851355e5dd8f7c9
Marcelo Vani <marcelo.vani@acquia.com>
2021-06-02 13:24:30 +0100

    CORE-29594: Moved code to spc helper.

--- docroot/modules/react/alshaya_spc/src/EventSubscriber/UserLoginSubscriber.php
+++ docroot/modules/react/alshaya_spc/src/EventSubscriber/UserLoginSubscriber.php
@@ -3,10 +3,8 @@
 namespace Drupal\alshaya_spc\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Drupal\Core\Config\ConfigFactory;
+use Drupal\alshaya_spc\Helper\AlshayaSpcHelper;
 use Drupal\alshaya_spc\Event\UserLoginEvent;
-use Drupal\alshaya_spc\Helper\AlshayaSpcCustomerHelper;
 
 /**
  * Hooks into the user login event to perform authentication with Magento API.
@@ -16,44 +14,22 @@ use Drupal\alshaya_spc\Helper\AlshayaSpcCustomerHelper;
 class UserLoginSubscriber implements EventSubscriberInterface {
 
   /**
-   * SPC customer helper.
-   *
-   * @var \Drupal\alshaya_spc\Helper\AlshayaSpcCustomerHelper
-   */
-  protected $spcCustomerHelper;
-
-  /**
    * Configuration Factory.
    *
-   * @var \Drupal\Core\Config\ConfigFactory
+   * @var \Drupal\alshaya_spc\Helper\AlshayaSpcHelper
    */
-  protected $configFactory;
-
-  /**
-   * The session.
-   *
-   * @var \Symfony\Component\HttpFoundation\Session\SessionInterface
-   */
-  protected $session;
+  protected $alshayaSpcHelper;
 
   /**
    * AlshayaSpcCustomerController constructor.
    *
-   * @param \Drupal\alshaya_spc\Helper\AlshayaSpcCustomerHelper $spc_customer_helper
-   *   SPC customer helper.
-   * @param \Drupal\Core\Config\ConfigFactory $configFactory
-   *   Config factory manager.
-   * @param \Symfony\Component\HttpFoundation\Session\SessionInterface $session
-   *   The session.
+   * @param \Drupal\alshaya_spc\Helper\AlshayaSpcHelper $alshaya_spc_helper
+   *   SPC Helper.
    */
   public function __construct(
-    AlshayaSpcCustomerHelper $spc_customer_helper,
-    ConfigFactory $configFactory,
-    SessionInterface $session
+    alshayaSpcHelper $alshaya_spc_helper
   ) {
-    $this->spcCustomerHelper = $spc_customer_helper;
-    $this->configFactory = $configFactory;
-    $this->session = $session;
+    $this->alshayaSpcHelper = $alshaya_spc_helper;
   }
 
   /**
@@ -72,22 +48,8 @@ class UserLoginSubscriber implements EventSubscriberInterface {
    *   The event.
    */
   public function onUserLogin(UserLoginEvent $event) {
-    $version = $this->configFactory->get('alshaya_acm.cart_config')->get('version');
-    // Check if its V2.
-    if ($version == 2) {
-      // Get customer email.
-      $email = $event->account->getEmail();
-
-      // Login with Magento.
-      $response = $this->spcCustomerHelper->authenticateCustomerBySocialDetail($email);
-
-      // Store bearer token on secure cookie.
-      if (is_string($response)) {
-
-        // Store token in the session.
-        $this->session->set('alshaya_spc_token', json_decode($response));
-      }
-    }
+    $email = $event->account->getEmail();
+    $this->alshayaSpcHelper->authenticateCustomerBySocialDetail($email);
   }
 
 }
--- docroot/modules/react/alshaya_spc/src/Helper/AlshayaSpcHelper.php
+++ docroot/modules/react/alshaya_spc/src/Helper/AlshayaSpcHelper.php
@@ -3,6 +3,9 @@
 namespace Drupal\alshaya_spc\Helper;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Drupal\alshaya_spc\Helper\AlshayaSpcCustomerHelper;
+use Drupal\Core\Session\AccountInterface;
 
 /**
  * Class containing general helper methods for SPC.
@@ -17,13 +20,48 @@ class AlshayaSpcHelper {
   protected $configFactory;
 
   /**
+   * The session.
+   *
+   * @var \Symfony\Component\HttpFoundation\Session\SessionInterface
+   */
+  protected $session;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * SPC customer helper.
+   *
+   * @var \Drupal\alshaya_spc\Helper\AlshayaSpcCustomerHelper
+   */
+  protected $spcCustomerHelper;
+
+  /**
    * Constructor for AlshayaSpcHelper.
    *
    * @param Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory service.
+   * @param \Symfony\Component\HttpFoundation\Session\SessionInterface $session
+   *   The session.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current user.
+   * @param \Drupal\alshaya_spc\Helper\AlshayaSpcCustomerHelper $spc_customer_helper
+   *   SPC Helper.
    */
-  public function __construct(ConfigFactoryInterface $config_factory) {
+  public function __construct(
+    ConfigFactoryInterface $config_factory,
+    SessionInterface $session,
+    AccountInterface $current_user,
+    AlshayaSpcCustomerHelper $spc_customer_helper
+  ) {
     $this->configFactory = $config_factory;
+    $this->session = $session;
+    $this->currentUser = $current_user;
+    $this->spcCustomerHelper = $spc_customer_helper;
   }
 
   /**
@@ -36,4 +74,41 @@ class AlshayaSpcHelper {
     return $this->configFactory->get('alshaya_acm.cart_config')->get('version') ?? 1;
   }
 
+  /**
+   * Authenticate on Magento using email and stores in the session if successful.
+   *
+   * @param string $email
+   *   The email address.
+   */
+  public function authenticateCustomerBySocialDetail($email) {
+    if (!$this->currentUser->isAuthenticated()) {
+      return;
+    }
+
+    if ($this->getCommerceBackendVersion() == 2) {
+      // Login with Magento.
+      $response = $this->spcCustomerHelper->authenticateCustomerBySocialDetail($email);
+      if (is_string($response)) {
+        // Store bearer token on secure cookie.
+        $this->session->set('alshaya_spc_token', json_decode($response));
+      }
+    }
+  }
+
+  /**
+   * Gets the bearer token from session. If its not in the session, it retrieves from Magento.
+   *
+   * @return string|NULL $token
+   *   Then bearer token if available, otherwise NULL.
+   */
+  public function getBearerToken() {
+    $token = $this->session->get('alshaya_spc_token');
+    if (is_string($token)) {
+      return $token;
+    }
+    else {
+      $email = $this->currentUser->getEmail();
+      return $this->authenticateCustomerBySocialDetail($email);
+    }
+  }
 }
