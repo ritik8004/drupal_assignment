f16e67a552510f3901118527f7bfded13ab81d44
Ganesh  Devkate <ganeshdevkate@gmail.com>
2021-06-03 19:29:43 +0530

    CORE-30524: HFD secondary menu theming for mobile.

--- docroot/modules/custom/alshaya_secondary_main_menu/js/secondary_main_menu.js
+++ docroot/modules/custom/alshaya_secondary_main_menu/js/secondary_main_menu.js
@@ -6,11 +6,19 @@
   'use strict';
   Drupal.behaviors.alshayaSecondaryMainMenu = {
     attach: function (context) {
-      if ($('#block-alshayasecondarymainmenu').length) {
+      if ($('#block-alshayasecondarymainmenu').length && $('secondary-main-menu-wrapper').length == 0) {
         if ($(window).width() > 768) {
           $('#block-branding, #block-alshayasecondarymainmenu')
             .wrapAll('<div class="secondary-main-menu-wrapper"></div>');
           $('.secondary--main--menu').show();
+        } else {
+          $('#block-alshayamainmenu ul.menu--one__list').append($('#block-alshayasecondarymainmenu .secondary--main--menu'));
+          $('.secondary--main--menu').prepend('<li class="secondary-main-menu-header closed">' + Drupal.t('More') + ' </li>')
+          $('#block-alshayamainmenu .secondary--main--menu').show();
+          $('.secondary-main-menu-header').on('click', function () {
+            $('.secondary--main--menu > ul').toggle();
+            $('.secondary-main-menu-header').toggleClass('closed');
+          })
         }
       }
     }
