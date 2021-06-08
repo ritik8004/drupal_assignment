702190d3a1cf8dd6b6eca06939c036208b333658
vivekpan27 <vivek.panicker@innoraft.com>
2021-06-08 08:07:48 +0000

    CORE-30016: Added new setting for commerce backend version.

--- factory-hooks/post-settings-php/commerce_backend.php
+++ factory-hooks/post-settings-php/commerce_backend.php
@@ -9,3 +9,9 @@
  */
 
 $settings['middleware_auth'] = '5um6y5nxl3oqms9qw0jai36qkryrrocg';
+
+// v1 = backend is middleware, i.e. we call middleware APIs to perform commerce
+// operations.
+// v2 = backend is magento, i.e. we call magento APIs directly to perform
+// commerce operations.
+$settings['commerce_backend']['version'] = 'v1';
