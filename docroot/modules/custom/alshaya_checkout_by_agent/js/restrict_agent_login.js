64902b9a0718ec686f51421e675f810d9126ba31
Sonam Singh <sonam.singh@innoraft.com>
2021-06-09 12:21:07 +0530

    CORE-30629: Move code to check cookie and show message outside behaviours.

--- docroot/modules/custom/alshaya_checkout_by_agent/js/restrict_agent_login.js
+++ docroot/modules/custom/alshaya_checkout_by_agent/js/restrict_agent_login.js
@@ -12,19 +12,19 @@
           Drupal.smartAgent.logout('user/login');
         });
       }
-
-      // If agent location is not shared, then show message and clear smart agent cookie.
-      // We use `smart_agent_location_fetch_failed` cookie to check location is shared or not.
-      var locationFetchFailed = $.cookie('smart_agent_location_fetch_failed');
-
-      if (parseInt(locationFetchFailed) === 1) {
-        // Show a message to smart agent to share location and then log in.
-        $('.region__hero-content').append('<div class="location-not-shared-message messages messages--error">' + Drupal.t('Smart agent, we have not been able to confirm your location. Please enable location services and allow location tracking then try to log in again.') + '</div>');
-
-        // Clear smart agent location cookie.
-        $.removeCookie('smart_agent_location_fetch_failed', {path: '/'});
-      }
     }
   };
+
+  // If agent location is not shared, then show message and clear smart agent cookie.
+  // We use `smart_agent_location_fetch_failed` cookie to check location is shared or not.
+  var locationFetchFailed = $.cookie('smart_agent_location_fetch_failed');
+
+  if (parseInt(locationFetchFailed) === 1) {
+    // Show a message to smart agent to share location and then log in.
+    $('.region__hero-content').append('<div class="location-not-shared-message messages messages--error">' + Drupal.t('Smart agent, we have not been able to confirm your location. Please enable location services and allow location tracking then try to log in again.') + '</div>');
+
+    // Clear smart agent location cookie.
+    $.removeCookie('smart_agent_location_fetch_failed', {path: '/'});
+  }
 
 })(jQuery, Drupal);
