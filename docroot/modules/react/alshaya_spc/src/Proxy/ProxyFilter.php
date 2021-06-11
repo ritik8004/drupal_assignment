970f34c0d16d2b60e66a291740f8c5fa3e71f931
Marcelo Vani <marcelo.vani@acquia.com>
2021-06-11 09:17:22 +0100

    CORE-30816: Deal with query string arguments and safe url.

--- docroot/modules/react/alshaya_spc/src/Proxy/ProxyFilter.php
+++ docroot/modules/react/alshaya_spc/src/Proxy/ProxyFilter.php
@@ -6,6 +6,7 @@ use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Proxy\Filter\FilterInterface;
 use Laminas\Diactoros\Uri;
+use Drupal\Component\Utility\UrlHelper;
 
 /**
  * Updates the request Url.
@@ -36,8 +37,20 @@ class ProxyFilter implements FilterInterface {
    * @inheritdoc
    */
   public function __invoke(RequestInterface $request, ResponseInterface $response, callable $next) {
-    $request = $request->withUri(new Uri($this->url));
-    $response = $next($request, $response);
+    // Collect arguments from query string.
+    parse_str(parse_url($request->getUri()->getQuery(), PHP_URL_QUERY), $query);
+
+    // Build uri with query string arguments.
+    $uri = $this->url . '?' . http_build_query($query);
+
+    if (UrlHelper::isValid($uri)) {
+      // Do the request.
+      $request = $request->withUri(new Uri($uri));
+      $response = $next($request, $response);
+    }
+    else {
+      throw new \Exception('The Url is invalid and may contain malicious content.');
+    }
 
     return $response;
   }
