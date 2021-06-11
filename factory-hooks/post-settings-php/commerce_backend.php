917b61ee31df143f587d4d0f7fd51b5299cbac48
vivekpan27 <vivek.panicker@innoraft.com>
2021-06-11 05:23:46 +0000

    CORE-30016: Enabled middleware blocking for local by default.

--- factory-hooks/post-settings-php/commerce_backend.php
+++ factory-hooks/post-settings-php/commerce_backend.php
@@ -8,6 +8,11 @@
  * @see https://docs.acquia.com/site-factory/tiers/paas/workflow/hooks
  */
 
+$env = 'local';
+if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
+  $env = $_ENV['AH_SITE_ENVIRONMENT'];
+}
+
 $settings['middleware_auth'] = '5um6y5nxl3oqms9qw0jai36qkryrrocg';
 
 // 1 = backend is middleware, i.e. we call middleware APIs to perform commerce
@@ -19,3 +24,6 @@ $settings['commerce_backend']['version'] = 1;
 // Use this setting to toggle blocking and unblocking of calls to middleware
 // after making the switch to V2.
 $settings['commerce_backend']['block_middleware'] = 0;
+if ($env === 'local') {
+  $settings['commerce_backend']['block_middleware'] = 1;
+}
