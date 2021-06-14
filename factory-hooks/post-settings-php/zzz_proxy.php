9468cd8d8e6ce3e42453c34d07087632c9756ab8
Marcelo Vani <marcelo.vani@acquia.com>
2021-06-14 23:57:44 +0100

    CORE-30816: Proxy is only needed on V2.

--- factory-hooks/post-settings-php/zzz_proxy.php
+++ factory-hooks/post-settings-php/zzz_proxy.php
@@ -1,6 +1,6 @@
 <?php
 // @codingStandardsIgnoreFile
 
-if ($env === 'local' && isset($settings['alshaya_api.settings']['magento_host'])) {
+if ($env === 'local' && isset($settings['alshaya_api.settings']['magento_host']) && $settings['commerce_backend']['version'] == 2) {
   $settings['alshaya_api.settings']['magento_host'] = '/proxy/?url=' . $settings['alshaya_api.settings']['magento_host'];
 }
