9bde466c884e5b43605c788b41cd2741159a4168
Marcelo Vani <marcelo.vani@acquia.com>
2021-06-14 11:44:16 +0100

    CORE-30816: Changed the syntax.

--- factory-hooks/post-settings-php/zzz_proxy.php
+++ factory-hooks/post-settings-php/zzz_proxy.php
@@ -1,9 +1,6 @@
 <?php
 // @codingStandardsIgnoreFile
 
-if ($env === 'local') {
-  // Use proxy.
-  if (isset($settings['alshaya_api.settings']['magento_host'])) {
-    $settings['alshaya_api.settings']['magento_host'] = '/proxy/?url=' . $settings['alshaya_api.settings']['magento_host'];
-  }
+if ($env === 'local' && isset($settings['alshaya_api.settings']['magento_host'])) {
+  $settings['alshaya_api.settings']['magento_host'] = '/proxy/?url=' . $settings['alshaya_api.settings']['magento_host'];
 }
