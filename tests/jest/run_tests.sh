0008cf9b3c764061fb5647f3d195b37b94daed8b
Marcelo Vani <marcelo.vani@acquia.com>
2021-06-15 08:38:33 +0100

    CORE-30816: Only run tests after we change something.

--- tests/jest/run_tests.sh
+++ tests/jest/run_tests.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 cd ../../docroot/modules/react
-
-diff=$(git diff .)
-
+diff=$(git diff --name-only . | grep ".js")
 if [ ! -z "${diff}" ];
 then
-    npm test
+  npm test
 fi
