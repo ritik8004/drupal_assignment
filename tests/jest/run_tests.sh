275ab4ebdbb8e6267b73470baf951782c8d36c32
Marcelo Vani <marcelo.vani@acquia.com>
2021-06-15 09:35:28 +0100

    CORE-30816: Moved Unit tests to react-setup.sh.

--- tests/jest/run_tests.sh
+++ tests/jest/run_tests.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
-
-cd ../../docroot/modules/react
-diff=$(git diff --name-only . | grep ".js")
-if [ ! -z "${diff}" ];
-then
-  npm test
-fi
+cd ../../docroot/modules/react && npm test
