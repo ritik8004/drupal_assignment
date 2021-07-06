b3e06a977181bb6e77229705c7f4f1ae4ce50d86
Marcelo Vani <marcelo.vani@acquia.com>
2021-07-07 00:32:42 +0100

    CORE-31225: Added more validations.

--- docroot/modules/react/alshaya_spc/js/backend/v2/checkout.js
+++ docroot/modules/react/alshaya_spc/js/backend/v2/checkout.js
@@ -1122,6 +1122,75 @@ const isCartHasOosItem = (cartData) => {
 };
 
 /**
+ * Get address fields to validate from drupal settings.
+ *
+ * @return {object}
+ *   Fields to validate.
+ */
+const cartAddressFieldsToValidate = () => {
+  let addressFieldsToValidate = [];
+
+  // Get the address fields based on site/country code
+  // from the drupal settings.
+  const siteCountryCode = window.drupalSettings.cart.site_country_code;
+  const addressFields = window.drupalSettings.cart.address_fields;
+
+  // Use default value first if available.
+  if (!_.isUndefined(siteCountryCode.country_code)) {
+    const countryCode = siteCountryCode.country_code;
+    if (!_.isUndefined(addressFields.default[countryCode])) {
+      addressFieldsToValidate = addressFields.default[countryCode];
+    }
+    if (!_.isUndefined(siteCountryCode.site_code)) {
+      const siteCode = siteCountryCode.site_code;
+      // If brand specific value available/override.
+      if (!_.isUndefined(addressFields[siteCode])
+        && !_.isUndefined(addressFields[siteCode][countryCode])
+      ) {
+        addressFieldsToValidate = addressFields[siteCode][countryCode];
+      }
+    }
+  }
+  return addressFieldsToValidate;
+};
+
+/**
+ * Validates the extension attributes of the address of the cart.
+ *
+ * @param {object} data
+ *   Cart data.
+ *
+ * @return {bool}
+ *   FALSE if empty field value.
+ */
+const isAddressExtensionAttributesValid = (data) => {
+  let isValid = true;
+  // If there are address fields available for validation
+  // in drupal settings.
+  const addressFieldsToValidate = cartAddressFieldsToValidate();
+  if (!_.isEmpty(addressFieldsToValidate)) {
+    const cartAddressCustom = [];
+    // Prepare cart address field data.
+    data.shipping.address.custom_attributes.forEach((item) => {
+      cartAddressCustom[item.attribute_code] = item.value;
+    });
+
+    // Check each required field in custom attributes available in cart
+    // shipping address or not.
+    addressFieldsToValidate.forEach((field) => {
+      // If field not exists or empty.
+      if (_.isEmpty(cartAddressCustom[field])) {
+        const cartId = window.commerceBackend.getCartId();
+        logger.error(`Field: ${field} not available in cart shipping address. Cart id: ${cartId}`);
+      }
+      isValid = false;
+      return isValid;
+    });
+  }
+  return isValid;
+};
+
+/**
  * Finalises the payment on the cart.
  *
  * @param {object} data
@@ -1149,10 +1218,60 @@ const paymentFinalise = async (data) => {
 
     errorMessage = 'Cart contains some items which are not in stock.';
     errorCode = cartErrorCodes.cartHasOOSItem;
+  } else if (_.isUndefined(cartData.cart.shipping.method)
+    || _.isEmpty(cartData.cart.shipping.method)
+  ) {
+    // Check if shipping method is present else throw error.
+    isError = true;
+    const logData = JSON.stringify(cartData);
+    logger.error(`Error while finalizing payment. No shipping method available. Cart: ${logData}.`);
+    //
+  } else if (_.isUndefined(cartData.cart.shipping.address)
+    || _.isUndefined(cartData.cart.shipping.address.custom_attributes)
+    || _.isEmpty(cartData.cart.shipping.address.custom_attributes)
+  ) {
+    // If shipping address not have custom attributes.
+    isError = true;
+    const logData = JSON.stringify(cartData);
+    logger.error(`Error while finalizing payment. Shipping address not contains all info. Cart: ${logData}.`);
+    //
+  } else if (!isAddressExtensionAttributesValid(cartData)) {
+    // If address extension attributes doesn't contain all the required
+    // fields or required field value is empty, not process/place order.
+    isError = true;
+    const logData = JSON.stringify(cartData);
+    logger.error(`Error while finalizing payment. Shipping address not contains all required extension attributes. Cart: ${logData}.`);
+  } else if (_.isUndefined(cartData.cart.shipping.address.firstname)
+    || _.isUndefined(cartData.cart.shipping.address.lastname)
+  ) {
+    // If first/last name not available in shipping address.
+    isError = true;
+    const logData = JSON.stringify(cartData);
+    logger.error(`Error while finalizing payment. First name or Last name not available in cart for shipping address. Cart: ${logData}.`);
+  } else if (_.isUndefined(cartData.cart.billing_address.firstname)
+    || _.isUndefined(cartData.cart.billing_address.lastname)
+  ) {
+    // If first/last name not available in billing address.
+    isError = true;
+    const logData = JSON.stringify(cartData);
+    logger.error(`Error while finalizing payment. First name or Last name not available in cart for billing address. Cart: ${logData}.`);
   }
 
+  if (isError) {
+    return {
+      error: true,
+      error_code: errorCode,
+      error_message: errorMessage,
+    };
+  }
+
+  // const extension = {
+  //   attempted_payment: 1,
+  // };
+  logger.error(`${data}`);
+
   // aaa ---
-  logger.error(`${data}${isError}${errorCode}${errorMessage}`);
+  return null;
 };
 
 /**
