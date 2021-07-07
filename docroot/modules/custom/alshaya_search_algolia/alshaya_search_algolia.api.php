1d02b4b5282f2b5136c0155df45c90c46aea994d
hammad ghani <mohamed.ghani@alshaya.com>
2021-07-07 06:27:05 +0000

    CORE-31041: Disabled product not getting removed form listing page.

--- docroot/modules/custom/alshaya_search_algolia/alshaya_search_algolia.api.php
+++ docroot/modules/custom/alshaya_search_algolia/alshaya_search_algolia.api.php
@@ -5,6 +5,8 @@
  * Hooks specific to the alshaya_search_algolia module.
  */
 
+use Drupal\Core\Entity\EntityInterface;
+
 /**
  * @addtogroup hooks
  * @{
@@ -17,6 +19,34 @@
  *   Array of query parameters.
  */
 function hook_alshaya_product_list_exclude_attribute_alter(array &$excludedAttributes) {
+
+}
+
+/**
+ * Alter the list of attributes to excluded for ProductList Indexing.
+ *
+ * @param array $item_ids
+ *   Array of query parameters.
+ * @param object $index
+ *   Object of index.
+ * @param \Drupal\Core\Entity\EntityInterface $entity
+ *   EntityInterface of entity.
+ */
+function hook_alshaya_search_api_entity_delete_alter(array &$item_ids, $index, EntityInterface $entity) {
+
+}
+
+/**
+ * Alter the list of attributes to excluded for ProductList Indexing.
+ *
+ * @param array $item_ids
+ *   Array of query parameters.
+ * @param object $index
+ *   Object of index.
+ * @param string $id
+ *   String of query parameters.
+ */
+function hook_alshaya_search_api_track_items_alter(array &$item_ids, $index, $id) {
 
 }
 
