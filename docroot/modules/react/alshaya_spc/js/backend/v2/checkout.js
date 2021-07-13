55a41e6e0183851644d2f74de44715d2dc891fb3
Avinash Shukla <avinash.shukla@acquia.com>
2021-07-13 11:02:43 +0530

    CORE-30148: Added apple pay payment wrapper to new js file.

--- docroot/modules/react/alshaya_spc/js/backend/v2/checkout.js
+++ docroot/modules/react/alshaya_spc/js/backend/v2/checkout.js
@@ -26,7 +26,6 @@ import {
   getIp,
 } from './utility';
 import cartActions from '../../utilities/cart_actions';
-import { addPaymentMethodInCart } from '../../utilities/update_cart';
 
 window.commerceBackend = window.commerceBackend || {};
 
@@ -1098,7 +1097,6 @@ const paymentUpdate = async (data) => {
 
   const oldCart = await getCart();
   const cart = await updateCart(params);
-  console.log(cart);
   if (_.isEmpty(cart.data) || (!_.isUndefined(cart.data.error) && cart.data.error)) {
     const errorMessage = (cart.data.error_code > 600) ? 'Back-end system is down' : cart.data.error.error_message;
     const message = prepareOrderFailedMessage(oldCart, data, errorMessage, 'update cart', 'NA');
@@ -1671,54 +1669,6 @@ window.commerceBackend.placeOrder = async (data) => {
 
       return response;
     });
-};
-
-/**
- * Checkout.com Apple pay update payment method.
- *
- * @param {object} data
- *   The data object to send in the API call.
- *
- * @returns {Promise}
- *   A promise object.
- */
-window.commerceBackend.saveApplePayPayment = (data) => {
-  const paymentData = {
-    payment: {
-      method: 'checkout_com_applepay',
-      additional_data: {
-        data: data.paymentData.data,
-        ephemeralPublicKey: data.paymentData.header.ephemeralPublicKey,
-        publicKeyHash: data.paymentData.header.publicKeyHash,
-        transactionId: data.paymentData.header.transactionId,
-        signature: data.paymentData.signature,
-        version: data.paymentData.version,
-        paymentMethodDisplayName: data.paymentMethod.displayName,
-        paymentMethodNetwork: data.paymentMethod.network,
-        paymentMethodType: data.paymentMethod.type,
-      },
-    },
-  };
-  return addPaymentMethodInCart(cartActions.cartPaymentUpdate, paymentData).then((response) => {
-    if (!_.isEmpty(response.response_message)
-      && !_.isEmpty(response.response_message.status)
-      && response.response_message.status === 'success') {
-      return {
-        data: {
-          success: true,
-        },
-      };
-    }
-
-    return response;
-  }).catch((response) => {
-    logger.error('Error while finalizing payment. Error message: @message, Code: @code.', {
-      '@message': !_.isEmpty(response.error) ? response.error.message : response,
-      '@code': !_.isEmpty(response.error) ? response.error.error_code : '',
-    });
-
-    return response;
-  });
 };
 
 export {
