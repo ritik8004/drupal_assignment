b6719288135836fcdd2964fd2fc04889a31173cd
vivekpan27 <vivek.panicker@innoraft.com>
2021-07-21 12:18:54 +0000

    CORE-31124: Removed unnecessary settings file.

--- factory-hooks/post-settings-php/rcs.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @file
- * Defines the settings for the rcs replacements feature.
- */
-
-// Marks if we want to use rcs to generate pdp content.
-$settings['rcs_pdp_enabled'] = FALSE;
