6af91cc30b5e050bd7e4d063cbf0aaf0babe4061
Marcelo Vani <marcelo.vani@acquia.com>
2021-07-21 11:23:23 +0100

    CORE-30712: Restored smart agent code.

--- docroot/modules/react/alshaya_spc/js/utilities/api/requests.js
+++ docroot/modules/react/alshaya_spc/js/utilities/api/requests.js
@@ -1,3 +1,4 @@
+import Cookies from 'js-cookie';
 import {
   cartAvailableInStorage,
   redirectToCart,
@@ -20,14 +21,11 @@ export const fetchCartData = () => {
     return null;
   }
 
-  /**
-   * @todo Port to V2.
   // If reset_cart_storage cookie is set then remove cart from storage.
   if (Cookies.get('reset_cart_storage')) {
-    removeCartFromStorage();
+    window.commerceBackend.removeCartDataFromStorage();
     Cookies.remove('reset_cart_storage');
   }
-  */
 
   // Check if cart available in storage.
   let cart = cartAvailableInStorage();
