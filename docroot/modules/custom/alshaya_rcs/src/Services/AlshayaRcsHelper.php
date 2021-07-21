e896e8a328edecd0c5c82b4e2f9844f2bb982241
vivekpan27 <vivek.panicker@innoraft.com>
2021-07-21 12:18:03 +0000

    CORE-31124: Read from config to determine rcs pdp status.

--- docroot/modules/custom/alshaya_rcs/src/Services/AlshayaRcsHelper.php
+++ docroot/modules/custom/alshaya_rcs/src/Services/AlshayaRcsHelper.php
@@ -2,12 +2,31 @@
 
 namespace Drupal\alshaya_rcs\Services;
 
-use Drupal\Core\Site\Settings;
+use Drupal\Core\Config\ConfigFactoryInterface;
 
 /**
  * General Helper service for the Ashaya RCS feature.
  */
 class AlshayaRcsHelper {
+
+  /**
+   * Config factory service.
+   *
+   * @var Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructor for the AddToBagHelper service.
+   *
+   * @param Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory service.
+   */
+  public function __construct(
+    ConfigFactoryInterface $config_factory
+  ) {
+    $this->configFactory = $config_factory;
+  }
 
   /**
    * Detects if the RCS replacement on PDP is enabled or not.
@@ -16,7 +35,7 @@ class AlshayaRcsHelper {
    *   Whether rcs replacement on pdp is enabled or not.
    */
   public function isRcsPdpEnabled() {
-    return (bool) Settings::get('rcs_pdp_enabled');
+    return (bool) $this->configFactory->get('alshaya_rcs.settings')->get('rcs_pdp_enabled');
   }
 
 }
