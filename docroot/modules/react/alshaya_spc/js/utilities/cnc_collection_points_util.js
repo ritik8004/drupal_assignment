2c8ba210383ca1818f00d9940576f3d0ff4867ac
Sonam Singh <sonam.singh@innoraft.com>
2021-07-23 14:38:23 +0530

    CORE-32152: Add title and description in cnc section on checkout.

--- docroot/modules/react/alshaya_spc/js/utilities/cnc_collection_points_util.js
+++ docroot/modules/react/alshaya_spc/js/utilities/cnc_collection_points_util.js
@@ -1,7 +1,18 @@
-/* eslint-disable */
-// @todo: Will remove eslint disable once we add more utility methods to this file.
-
 /**
  * Helper to check if click and collect collection points is enabled.
  */
 export const collectionPointsEnabled = () => drupalSettings.cnc_collection_points_enabled || false;
+
+/**
+ * Helper to get click and collect section title.
+ */
+export const getCncSectionTitle = () => ((collectionPointsEnabled() === true)
+  ? Drupal.t('Collection Point')
+  : Drupal.t('Collection Store'));
+
+/**
+ * Helper to get click and collect section description.
+ */
+export const getCncSectionDescription = () => ((collectionPointsEnabled() === true)
+  ? Drupal.t('select your preferred collection point')
+  : Drupal.t('select your preferred collection store'));
