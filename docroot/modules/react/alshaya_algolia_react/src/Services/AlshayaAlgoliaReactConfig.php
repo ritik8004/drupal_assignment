b465de097708f0c867110d05b769a6d29c057318
Manisha Acheryya <manisha.acheryya@innoraft.com>
2021-07-23 05:21:34 +0000

    CORE-30932: Removed langcode.

--- docroot/modules/react/alshaya_algolia_react/src/Services/AlshayaAlgoliaReactConfig.php
+++ docroot/modules/react/alshaya_algolia_react/src/Services/AlshayaAlgoliaReactConfig.php
@@ -423,10 +423,9 @@ class AlshayaAlgoliaReactConfig implements AlshayaAlgoliaReactConfigInterface {
               && !$this->configFactory->get('alshaya_acm_product.settings')->get('enable_size_grouping_filter')) {
             $widget['type'] = 'checkbox';
           }
-          $langcode = $this->languageManager->getCurrentLanguage()->getId();
 
           $filter_facets[explode('.', $identifier)[0]] = [
-            'identifier' => $identifier . '.' . $langcode,
+            'identifier' => $identifier,
             'label' => $block->label(),
             'name' => $facet->getName(),
             'widget' => $widget,
