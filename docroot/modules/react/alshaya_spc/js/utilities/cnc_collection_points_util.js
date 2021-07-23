bf6b3fab4ca01ed312ee25e9c170d3015051b3cd
Sonam Singh <sonam.singh@innoraft.com>
2021-07-23 11:43:59 +0530

    CORE-32146: Fix js helper method structure.

--- docroot/modules/react/alshaya_spc/js/utilities/cnc_collection_points_util.js
+++ docroot/modules/react/alshaya_spc/js/utilities/cnc_collection_points_util.js
@@ -1,10 +1,7 @@
 /* eslint-disable */
-// @todo: Remove eslint disable once we add more utility methods to this file.
+// @todo: Will remove eslint disable once we add more utility methods to this file.
 
 /**
  * Helper to check if click and collect collection points is enabled.
  */
-export const cnc_collection_points_enabled = () => {
-  const cncCollectionPointsEnabled = drupalSettings.cnc_collection_points_enabled || false;
-  return cncCollectionPointsEnabled;
-};
+export const collectionPointsEnabled = () => drupalSettings.cnc_collection_points_enabled || false;
