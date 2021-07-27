6148859488b42c2fa7fd3f03ec5a1a252076cc46
Kunal Singh <skunal185@gmail.com>
2021-07-27 14:43:21 +0530

    CORE-31494: Added RCS PLP module and block.

---
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_plp/js/alshaya_rcs_plp_renderer-exports.es5.js
@@ -0,0 +1,12 @@
+// @codingStandardsIgnoreFile
+// This is because the linter is throwing errors where we use backticks here.
+// Once we enable webapack for the custom modules directory, we should look into
+// removing the above ignore line.
+exports.render = function render(
+  settings,
+  inputs,
+  entity,
+  innerHtml
+) {
+  // Not sure but thinking of rendering the react component from here.
+}
