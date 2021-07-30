8c6c007aeb93f8c5c3559510dd44e573f04ffba9
Kunal Singh <skunal185@gmail.com>
2021-07-30 16:30:08 +0530

    CORE-31494: Reattached drupal behaviour after react component is rendered.

--- docroot/modules/react/alshaya_algolia_react/js/src/PlpIndex.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/PlpIndex.js
@@ -4,7 +4,7 @@ import PlpApp from './plp/PlpApp';
 
 // Proceed only if element is present.
 const requiredData = jQuery('#alshaya-algolia-plp');
-if (requiredData.length) {
+if (requiredData.length && Object.keys(requiredData[0].dataset).length) {
   ReactDOM.render(
     <PlpApp dataAttribute={requiredData[0].dataset} />,
     document.querySelector('#alshaya-algolia-plp'),
--- docroot/modules/react/alshaya_algolia_react/js/src/PlpV2Index.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/PlpV2Index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PlpApp from './plp/PlpApp';
 
-(function PlpV2($, Drupal) {
+(function PlpV2($, Drupal, drupalSettings) {
   Drupal.behaviors.alshayaAlgoliaReactPLPV2Index = { // eslint-disable-line no-param-reassign
     attach: function process() {
       // Proceed only if RCS is processed.
       const requiredData = $('#rcs-ph-product_category_list > span');
-      if (requiredData.length) {
+      if (requiredData.length && Object.keys(requiredData[0].dataset).length) {
         ReactDOM.render(
           <PlpApp dataAttribute={requiredData[0].dataset} />,
           document.querySelector('#alshaya-algolia-plp'),
@@ -15,4 +15,9 @@ import PlpApp from './plp/PlpApp';
       }
     },
   };
+  // Re-attach all behaviors after some delay.
+  // @todo To check for a proper way to Re-attach the behaviors.
+  setTimeout(() => {
+    Drupal.attachBehaviors(document, drupalSettings);
+  }, 5000);
 }(jQuery, Drupal));
