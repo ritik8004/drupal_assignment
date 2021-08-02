0dcccdca888abc7eb8d7e3e814a5e6c5f1f1f569
Kunal Singh <skunal185@gmail.com>
2021-08-02 17:31:00 +0530

    CORE-32667: Added the LHN RCS Block and Render function.

--- docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_listing/js/alshaya_rcs_listing_renderer.es5.js
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_listing/js/alshaya_rcs_listing_renderer.es5.js
@@ -1,2 +1,3 @@
 global.renderRcsListing = require('./alshaya_rcs_listing_renderer-exports.es5.js');
 global.renderRcsCategoryBreadcrumb = require('./alshaya_rcs_listing_breadcrumb_renderer-exports.es5.js')
+global.renderRcsLhn = require('./alshaya_rcs_listing_lhn_renderer-exports.es5.js');
