dfb5bb920baa79844ce3c1c1a2e5392f5efd244a
Nikunj Kotecha <nikunj.kotecha@acquia.com>
2021-08-03 13:25:27 +0530

    CORE-31250: Make sure the token is set only for non-prod from code.

--- factory-hooks/post-settings-php/zz_logger_config.php
+++ factory-hooks/post-settings-php/zz_logger_config.php
@@ -15,5 +15,11 @@ $config['track_js.settings']['application'] = implode('-', [
   $settings['env_name'],
 ]);
 
-$config['datadog_js.settings']['token'] = 'pub9fea1ea3ed8eafec1fcd04e4a7eb2921';
 $config['datadog_js.settings']['application'] = 'datadoghq.eu';
+
+// Token for all the non-prod envs is added below.
+// We will need to set the same config on production with proper value.
+// This is anyways safe as it is public token visible in browser too.
+if ($settings['env_name'] !== 'live') {
+  $config['datadog_js.settings']['token'] = 'pub9fea1ea3ed8eafec1fcd04e4a7eb2921';
+}
