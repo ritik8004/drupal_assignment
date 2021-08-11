ed072b3d187c8686b2b9700b56d5fd4886493cd3
Sylvain DELBOSC <sylvain.delbosc@acquia.com>
2021-08-11 14:03:23 +0200

    Merge pull request #21453 from nikunjkotecha/CORE-30712
    [6.5.0] CORE-30712: Port smart agent feature to V2.

---
+++ docroot/modules/custom/alshaya_checkout_by_agent/src/Controller/AlshayaSmartAgentController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Drupal\alshaya_checkout_by_agent\Controller;
+
+use Drupal\alshaya_api\AlshayaApiWrapper;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Logger\LoggerChannelTrait;
+use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Controller for Smart Agent related callbacks.
+ */
+class AlshayaSmartAgentController extends ControllerBase {
+
+  use LoggerChannelTrait;
+
+  /**
+   * Alshaya API Wrapper service.
+   *
+   * @var \Drupal\alshaya_api\AlshayaApiWrapper
+   */
+  protected $apiWrapper;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\alshaya_api\AlshayaApiWrapper $api_wrapper
+   *   Alshaya API Wrapper service.
+   */
+  public function __construct(AlshayaApiWrapper $api_wrapper) {
+    $this->apiWrapper = $api_wrapper;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('alshaya_api.api')
+    );
+  }
+
+  /**
+   * Page Callback to resume cart shared by agent.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   HTTP Request.
+   *
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   *   Redirect response.
+   */
+  public function resume(Request $request) {
+    // In case of error, we redirect to cart page.
+    $redirect = new RedirectResponse(Url::fromRoute('acq_cart.cart')->toString(), 302);
+    $redirect->setMaxAge(0);
+    $redirect->headers->set('cache-control', 'must-revalidate, no-cache, no-store, private');
+
+    $content = $request->query->get('data');
+    if (empty($content)) {
+      return $redirect;
+    }
+
+    // Decrypt the data.
+    $data = $this->apiWrapper->getDecryptedSmartAgentData($content);
+
+    // Redirect to cart page if cart id or smart agent details is empty.
+    if (empty($data) || empty($data['success'])) {
+      $this->getLogger('AlshayaSmartAgentController')->warning('Error occurred while trying to decrypt the data, Response: @response.', [
+        '@response' => json_encode($data),
+      ]);
+
+      return $redirect;
+    }
+
+    if (alshaya_acm_customer_is_customer($this->currentUser())) {
+      // Associate the cart if customer logged in.
+      $user = $this->entityTypeManager()->getStorage('user')->load($this->currentUser()->id());
+      $customer_id = $user->get('acq_customer_id')->getString();
+
+      $this->getLogger('AlshayaSmartAgentController')->notice('Associating cart from Smart agent to Customer. Cart ID: @cart_id, Customer ID: @customer_id.', [
+        '@cart_id' => $data['cart_id'],
+        '@customer_id' => $customer_id,
+      ]);
+
+      $this->apiWrapper->associateCartToCustomer($data['cart_id'], $customer_id);
+    }
+    else {
+      // For guest users, set the guest cart id in cookie, we will read from
+      // there in JS to resume the cart.
+      setrawcookie('resume_cart_id', $data['masked_quote_id'], strtotime('+1 year'), '/');
+    }
+
+    return $redirect;
+  }
+
+}
