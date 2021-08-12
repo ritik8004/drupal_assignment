29b0128ff609b24025fb2fcf88eb10fc7691b1c1
Nikunj Kotecha <nikunj.kotecha@acquia.com>
2021-08-12 15:50:06 +0530

    CORE-33003: Load product status only on cart and checkout pages.

--- docroot/modules/react/alshaya_spc/js/cart.js
+++ docroot/modules/react/alshaya_spc/js/cart.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Cart from './cart/components/cart';
 
+// Store the page type for conditions later.
+window.spcPageType = 'cart';
+
 ReactDOM.render(
   <Cart />,
   document.getElementById('spc-cart'),
