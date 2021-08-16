7ae4b93247291fb1fb11018db4fc2f2d8b216488
Sonam Singh <sonam.singh@innoraft.com>
2021-08-16 21:17:43 +0530

    CORE-33062: Integrate benefit pay on mobile.

--- docroot/modules/react/alshaya_spc/js/checkout-confirmation/components/CompleteBenefitPayPayment/index.js
+++ docroot/modules/react/alshaya_spc/js/checkout-confirmation/components/CompleteBenefitPayPayment/index.js
@@ -1,68 +1,140 @@
 import React from 'react';
 import QRCode from 'react-qr-code';
+import hmacSHA256 from 'crypto-js/hmac-sha256';
+import Base64 from 'crypto-js/enc-base64';
 import DeviceView from '../../../common/components/device-view';
 import PriceElement from '../../../utilities/special-price/PriceElement';
 import BenefitPaySVG from '../../../svg-component/payment-method-svg/components/benefit-pay-svg';
 
-const CompleteBenefitPayPayment = (props) => {
-  const { payment, totals } = props;
+class CompleteBenefitPayPayment extends React.Component {
+  appId = 2264812781;
 
-  return (
-    <div className="benefit-pay-container">
-      <DeviceView device="mobile">
-        <button
-          type="button"
-          className="inapp-btn"
-        >
-          {Drupal.t('Pay using BenefitPay App')}
-        </button>
-        <div className="qr">
-          <div><span className="qr-label">{Drupal.t('or Scan to Pay')}</span></div>
-          <QRCode value={payment.qrData} size="130" />
-        </div>
-      </DeviceView>
-      <DeviceView device="above-mobile">
-        <div className="title">{Drupal.t('Please complete your payment by scanning the QR code.')}</div>
-        <div className="benefit-pay-wrapper">
-          <div className="benefit-pay-content">
-            <div className="benefit-pay-header">
-              <BenefitPaySVG />
-              <div className="title">{Drupal.t('BenefitPay')}</div>
-            </div>
-            <div className="qr-code-wrapper">
-              <div className="qr-left">
-                <span>{Drupal.t('Scan to Pay')}</span>
-                <QRCode value={payment.qrData} size="100" />
-              </div>
-              <div className="info-right">
-                <div>
-                  <span className="spc-label">
-                    {Drupal.t('Merchant')}
-                  </span>
-                  <span className="spc-value merchant-value">{Drupal.t('Alshaya')}</span>
-                </div>
-                <div>
-                  <span className="spc-label">
-                    {Drupal.t('Amount')}
-                  </span>
-                  <span className="spc-value">
-                    <PriceElement amount={totals.base_grand_total} format="string" />
-                  </span>
-                </div>
-                <div>
-                  <span className="spc-label">
-                    {Drupal.t('Reference number')}
-                  </span>
-                  <span className="spc-value">{payment.referenceNumber}</span>
-                </div>
-              </div>
-            </div>
-            <div className="benefit-pay-footer"><a href="/">{Drupal.t('How to pay using BenefitPay?')}</a></div>
+  merchantId = 4187951;
+
+  secretKey = 'vbgm3o5354c820vhrj0ld5wck693yipbabf43nq9m6avr';
+
+  // Create 'secure_hash' - the Hashing should be done by concatenating the
+  // request parameters as key value pairs, sorting them ascending by key and
+  // value, combining key and value with an =, then concatenating all the key-value
+  // pairs separated by a comma and finally performing a SHA-256 hash using the
+  // secret token with base64 encoding.
+  calcSecureHash = (data) => {
+    const benefitpayData = data;
+    const sortedValues = Object.entries(benefitpayData)
+      .filter((kv) => kv[1])
+      .map((kv) => `${kv[0]}="${kv[1]}"`)
+      .sort();
+    const hashStr = sortedValues.join(',');
+    const hash = hmacSHA256(hashStr, this.secretKey);
+    const hashInBase64 = Base64.stringify(hash);
+
+    benefitpayData.hashedString = this.secretKey;
+    benefitpayData.secure_hash = hashInBase64;
+
+    return benefitpayData;
+  }
+
+  prepareBenefitPayDetails = () => {
+    const { payment, totals } = this.props;
+
+    const data = {
+      transactionAmount: totals.base_grand_total.toFixed(3),
+      transactionCurrency: payment.currency,
+      referenceNumber: payment.referenceNumber,
+      merchantId: this.merchantId,
+      appId: this.appId,
+    };
+
+    return this.calcSecureHash(data);
+  }
+
+  buildBenefitPayMobileAppURL = () => {
+    const data = this.prepareBenefitPayDetails();
+    const { payment } = this.props;
+
+    const link = encodeURIComponent(
+      `https://benefit.bh/?inapp&app_id=${
+        data.appId
+      }&merchant_id=${
+        data.merchantId
+      }&qrdata=${
+        payment.qrData
+      }&referenceNumber=${
+        data.referenceNumber
+      }&hashedString=${
+        data.hashedString
+      }&amount=${
+        data.transactionAmount}`,
+    );
+
+    return `https://tbenefituser.page.link/?link=${link}`;
+  }
+
+  openBenefitPayApp = () => {
+    window.open(this.buildBenefitPayMobileAppURL(), '_blank');
+  };
+
+  render() {
+    const { payment, totals } = this.props;
+
+    return (
+      <div className="benefit-pay-container">
+        <DeviceView device="mobile">
+          <button
+            type="button"
+            className="inapp-btn"
+            onClick={() => this.openBenefitPayApp()}
+          >
+            {Drupal.t('Pay using BenefitPay App')}
+          </button>
+          <div className="qr">
+            <div><span className="qr-label">{Drupal.t('or Scan to Pay')}</span></div>
+            <QRCode value={payment.qrData} size="130" />
           </div>
-        </div>
-      </DeviceView>
-    </div>
-  );
-};
+        </DeviceView>
+        <DeviceView device="above-mobile">
+          <div className="title">{Drupal.t('Please complete your payment by scanning the QR code.')}</div>
+          <div className="benefit-pay-wrapper">
+            <div className="benefit-pay-content">
+              <div className="benefit-pay-header">
+                <BenefitPaySVG />
+                <div className="title">{Drupal.t('BenefitPay')}</div>
+              </div>
+              <div className="qr-code-wrapper">
+                <div className="qr-left">
+                  <span>{Drupal.t('Scan to Pay')}</span>
+                  <QRCode value={payment.qrData} size="100" />
+                </div>
+                <div className="info-right">
+                  <div>
+                    <span className="spc-label">
+                      {Drupal.t('Merchant')}
+                    </span>
+                    <span className="spc-value merchant-value">{Drupal.t('Alshaya')}</span>
+                  </div>
+                  <div>
+                    <span className="spc-label">
+                      {Drupal.t('Amount')}
+                    </span>
+                    <span className="spc-value">
+                      <PriceElement amount={totals.base_grand_total} format="string" />
+                    </span>
+                  </div>
+                  <div>
+                    <span className="spc-label">
+                      {Drupal.t('Reference number')}
+                    </span>
+                    <span className="spc-value">{payment.referenceNumber}</span>
+                  </div>
+                </div>
+              </div>
+              <div className="benefit-pay-footer"><a href="/">{Drupal.t('How to pay using BenefitPay?')}</a></div>
+            </div>
+          </div>
+        </DeviceView>
+      </div>
+    );
+  }
+}
 
 export default CompleteBenefitPayPayment;
