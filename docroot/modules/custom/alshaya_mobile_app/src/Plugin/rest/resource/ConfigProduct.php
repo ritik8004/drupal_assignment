4264a08efe9a44d262e9b723b977792f1b25b79c
Marcelo Vani <marcelo.vani@acquia.com>
2021-09-09 11:41:15 +0100

    CORE-31457: Created REST endpoing for product configs.

---
+++ docroot/modules/custom/alshaya_mobile_app/src/Plugin/rest/resource/ConfigProduct.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace Drupal\alshaya_mobile_app\Plugin\rest\resource;
+
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a resource to get product config.
+ *
+ * @RestResource(
+ *   id = "config_product",
+ *   label = @Translation("Product config"),
+ *   uri_paths = {
+ *     "canonical" = "/rest/v1/config/product"
+ *   }
+ * )
+ */
+class ConfigProduct extends ResourceBase {
+
+  /**
+   * The language manager.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
+   * Config Factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * ConfigProduct Resource constructor.
+   *
+   * @param array $configuration
+   *   Configuration array.
+   * @param string $plugin_id
+   *   Plugin id.
+   * @param mixed $plugin_definition
+   *   Plugin definition.
+   * @param array $serializer_formats
+   *   Serializer formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   Logger channel.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   *   The language manager.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   Config Factory.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    LanguageManagerInterface $language_manager,
+    ConfigFactoryInterface $config_factory
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+    $this->languageManager = $language_manager;
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('alshaya_acm_product'),
+      $container->get('language_manager'),
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * Lists configs to be sent as response.
+   *
+   * If you don't specify the individual keys, all items will be returned.
+   *
+   * @return array
+   *   The list of configs.
+   */
+  private function getList() {
+    return [
+      'alshaya_acm_product.settings' => [
+        'all_products_buyable',
+        'non_refundable_text',
+        'non_refundable_tooltip',
+        'same_day_delivery_text',
+        'same_day_delivery_sub_text',
+        'delivery_in_only_city_text',
+        'delivery_in_only_city_key',
+        'legal_notice_enabled',
+        'legal_notice_label',
+        'legal_notice_summary',
+      ],
+      'alshaya_acm_product.home_delivery',
+      'alshaya_click_collect.settings',
+      'acq_commerce.currency',
+    ];
+  }
+
+  /**
+   * Builds an array of cache tags based on config names.
+   *
+   * @return array
+   *   Cache tags.
+   */
+  private function getCacheTags() {
+    $tags = [];
+    foreach ($this->getList() as $key => $item) {
+      if (is_array($item)) {
+        $tags[] = 'config:' . $key;
+      }
+      else {
+        $tags[] = 'config:' . $item;
+      }
+    }
+
+    return $tags;
+  }
+
+  /**
+   * Returns product configuration.
+   *
+   * @return \Drupal\rest\ResourceResponse
+   *   The response containing configuration.
+   */
+  public function get() {
+    $data = [];
+    foreach ($this->getList() as $config => $name) {
+      if (is_array($name)) {
+        foreach ($name as $item) {
+          $data[$config][$item] = $this->getConfig($config, $item);
+        }
+      }
+      else {
+        $data[$name] = $this->getAllConfigs($name);
+      }
+    }
+
+    // Translate strings.
+    array_walk_recursive($data, function (&$value) {
+      if (is_string($value)) {
+        // @codingStandardsIgnoreStart
+        $value = new TranslatableMarkup($value);
+        // @codingStandardsIgnoreEnd
+      }
+    });
+
+    $response = new ResourceResponse($data);
+    $cacheableMetadata = $response->getCacheableMetadata();
+    $cacheableMetadata->addCacheTags($this->getCacheTags());
+    $cacheableMetadata->addCacheContexts(['url.query_args']);
+    $response->addCacheableDependency($cacheableMetadata);
+
+    return $response;
+  }
+
+  /**
+   * Get config for a given config name and key.
+   *
+   * @param string $name
+   *   The config name.
+   * @param string $key
+   *   The config key.
+   *
+   * @return array|mixed
+   *   The config.
+   */
+  private function getConfig($name, $key) {
+    return $this->configFactory->get($name)->get($key);
+  }
+
+  /**
+   * Get all items for a given config name.
+   *
+   * @param string $name
+   *   The config name.
+   *
+   * @return array|mixed
+   *   The config.
+   */
+  private function getAllConfigs($name) {
+    $configs = $this->configFactory->get($name)->getRawData();
+
+    // Remove unwanted item.
+    unset($configs['_core']);
+
+    return $configs;
+  }
+
+}
