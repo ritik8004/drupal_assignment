2b04d046000e42329acc6cb430631a5c9e3ea09a
Marcelo Vani <marcelo.vani@acquia.com>
2021-09-24 10:41:18 +0100

    CORE-34050: Moved handlebars package to main package.json. Js fixes.

--- docroot/modules/brands/alshaya_tbs/modules/alshaya_tbs_transac/js/alshaya_tbs_transac_product.js
+++ docroot/modules/brands/alshaya_tbs/modules/alshaya_tbs_transac/js/alshaya_tbs_transac_product.js
@@ -2,16 +2,13 @@
  * Listens to the 'alshayaRcsUpdateResults' event and updated the result object.
  */
 (function main($) {
-
-  $(document).ready(function ready() {
-    // Event listener to update the data layer object with the proper category
-    // data.
-    document.addEventListener('alshayaRcsUpdateResults', (e) => {
-      // Return if result is empty.
-      if (typeof e.detail.result === 'undefined' || e.detail.pageType !== 'product') {
-        return null;
-      }
-      // @todo add title/description overrides. See CORE-34431.
-    });
+  // Event listener to update the data layer object with the proper category
+  // data.
+  document.addEventListener('alshayaRcsUpdateResults', (e) => {
+    // Return if result is empty.
+    if (typeof e.detail.result === 'undefined' || e.detail.pageType !== 'product') {
+      return;
+    }
+    // @todo add title/description overrides. See CORE-34431.
   });
 })(jQuery);
