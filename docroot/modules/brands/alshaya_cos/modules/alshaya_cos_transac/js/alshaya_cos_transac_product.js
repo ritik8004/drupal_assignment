9b54bb811d2218310c0673c1c03bc95ef7a0891e
Marcelo Vani <marcelo.vani@acquia.com>
2021-09-29 11:08:46 +0100

    CORE-34549: Provide other fields to render description.

--- docroot/modules/brands/alshaya_cos/modules/alshaya_cos_transac/js/alshaya_cos_transac_product.js
+++ docroot/modules/brands/alshaya_cos/modules/alshaya_cos_transac/js/alshaya_cos_transac_product.js
@@ -12,15 +12,17 @@
 
     var data = e.detail.result;
 
-    // Title/description brand overrides. See CORE-34549.
-    var e.detail.result.description.html = data.description.html
-      + data.composition
-      + data.washing_instructions
-      + data.article_warning
-      + 'Make sure that your favourite item remain...'
-      + data.sku;
+    // Add extra data to product description. This will be rendered using handlebars templates.
+    data.description.show_product_detail_title = (data.composition || data.washing_instructions || data.article_warning);
+    data.description.composition = data.composition;
+    data.description.washing_instructions = data.washing_instructions;
+    data.description.article_warning = data.article_warning;
+    data.description.product_guide = ''; // @todo create a new config for this.
+    data.description.sku = data.sku;
+    e.detail.result.description = data.description;
 
-    var e.detail.result.short_description;
+    // Append text to short_description. This is will be trimmed if the description is longer than 160 characters.
+    e.detail.result.short_description = e.detail.result.description + ' ' + data.composition + ' ' + data.washing_instructions +  data.article_warning;
 
     // @todo add image brand overrides. See CORE-34424.
   });
