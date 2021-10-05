342ff80a5532600da62927bd6fb46d064385d443
ghani <mohamed.ghani@alshaya.com>
2021-10-05 06:45:37 +0000

    CORE-34031: New magazine layout with product gallery and content panel.

--- docroot/modules/custom/alshaya_pdp_layouts/src/Plugin/PdpLayout/MagazinePdpLayout.php
+++ docroot/modules/custom/alshaya_pdp_layouts/src/Plugin/PdpLayout/MagazinePdpLayout.php
@@ -23,7 +23,15 @@ class MagazinePdpLayout extends PdpLayoutBase {
    * {@inheritdoc}
    */
   public function getRenderArray(array &$variables) {
-    $variables['#attached']['library'][] = 'alshaya_product_zoom/magazine_gallery';
+    $cos_classic_gallery = \Drupal::config('alshaya_acm_product.settings')->get('cos_classic_gallery');
+    if ($cos_classic_gallery) {
+      // Classic gallery for cos PDP magazine layout.
+      $variables['#attached']['library'][] = 'alshaya_product_zoom/cloud_zoom_pdp_gallery';
+      $variables['#attached']['library'][] = 'alshaya_white_label/attribute';
+    }
+    else {
+      $variables['#attached']['library'][] = 'alshaya_product_zoom/magazine_gallery';
+    }
     $variables['#attached']['library'][] = 'alshaya_white_label/magazine_attribute';
     $variables['#attached']['library'][] = 'alshaya_white_label/magazine_socialSharepopup';
     $variables['#attached']['drupalSettings']['color_swatches_hover'] = TRUE;
