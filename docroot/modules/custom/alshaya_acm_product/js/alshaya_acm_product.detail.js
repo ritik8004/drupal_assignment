e5bb904c04b7cc96e08c011f449d40b5469a6a7f
Avinash <avinash.shukla@acquia.com>
2021-10-06 17:46:59 +0530

    CORE-34761: Refresh gallery price block limits etc on variant select.

--- docroot/modules/custom/alshaya_acm_product/js/alshaya_acm_product.detail.js
+++ docroot/modules/custom/alshaya_acm_product/js/alshaya_acm_product.detail.js
@@ -593,4 +593,15 @@
     return selectedSku;
   };
 
+  document.addEventListener('react-variant-select', function ({ detail }) {
+    const { variant } = detail;
+    $('.sku-base-form').trigger(
+      'variant-selected',
+      [
+        variant,
+        null
+      ]
+    );
+  });
+
 })(jQuery, Drupal, drupalSettings);
