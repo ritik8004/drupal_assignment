f6c1365fef63cbfa189c434c41dbeac58b3ddabd
Kunal Singh <skunal185@gmail.com>
2021-10-08 13:00:29 +0530

    CORE-34027: Moved v1 and v2 dynamic promotion into separate libraries.

---
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_promotion/js/alshaya_rcs_promotion_dynamic_label.js
@@ -0,0 +1,43 @@
+/**
+ * @file
+ * Alshaya RCS Promotions Dynamic Label Manager.
+ */
+
+window.dynamicPromotion = window.dynamicPromotion || {};
+
+const PromotionsDynamicLabelsUtil = {
+  apply: (cartData) => {
+    if (Object.values(cartData.items).length === 0) {
+      // Remove existing labels.
+      dispatchRcsCustomEvent('applyDynamicPromotions', {
+        cart_labels: null,
+        products_labels: null,
+      });
+
+      // No API call required.
+      return;
+    }
+
+    const productLabels = {};
+    Object.keys(cartData.items).forEach((key) => {
+      const response = Drupal.alshayaPromotions.getV2DynamicLabel(cartData.items[key].sku, cartData, 'api');
+      if (response && typeof response.data !== 'undefined') {
+        productLabels[key] = {
+          sku: cartData.items[key].sku,
+          labels: JSON.parse(response.data.dynamicPromotionLabel.label),
+        };
+      }
+    });
+    dispatchRcsCustomEvent('applyDynamicPromotions', {
+      products_labels: productLabels,
+      // @todo Change the cart_labels logic once the changes are done in
+      // Magento.
+      cart_labels: {
+        qualified: [],
+        next_eligible: [],
+      },
+    });
+  },
+};
+
+window.dynamicPromotion.apply = (cartData) => PromotionsDynamicLabelsUtil.apply(cartData);
---
+++ docroot/modules/react/alshaya_spc/js/promotions-dynamic-labels.js
@@ -0,0 +1,32 @@
+import Axios from 'axios';
+import dispatchCustomEvent from './utilities/events';
+
+window.dynamicPromotion = window.dynamicPromotion || {};
+
+const PromotionsDynamicLabelsUtil = {
+  apply: (cartData) => {
+    if (Object.values(cartData.items).length === 0) {
+      // Remove existing labels.
+      dispatchCustomEvent('applyDynamicPromotions', {
+        cart_labels: null,
+        products_labels: null,
+      });
+
+      // No API call required.
+      return;
+    }
+
+    let apiUrl = Drupal.url('promotions/dynamic-label-cart');
+    // We set cacheable=1 so it is always treated as anonymous user request.
+    apiUrl = `${apiUrl}?cacheable=1&context=web&${Drupal.alshayaSpc.getCartDataAsUrlQueryString(cartData)}`;
+
+    Axios.get(apiUrl).then((response) => {
+      if (response.data.cart_labels !== undefined
+        || response.data.products_labels !== undefined) {
+        dispatchCustomEvent('applyDynamicPromotions', response.data);
+      }
+    });
+  },
+};
+
+window.dynamicPromotion.apply = (cartData) => PromotionsDynamicLabelsUtil.apply(cartData);
