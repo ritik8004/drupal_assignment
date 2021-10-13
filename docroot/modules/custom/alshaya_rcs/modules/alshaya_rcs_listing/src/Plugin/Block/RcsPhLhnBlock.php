f7e0d64e36ec6dd0e10d40a2e7c7ef1337869bef
Marcelo Vani <marcelo.vani@acquia.com>
2021-10-13 09:38:21 +0100

    CORE-33045: Reverting changes to templates.

--- docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_listing/src/Plugin/Block/RcsPhLhnBlock.php
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_listing/src/Plugin/Block/RcsPhLhnBlock.php
@@ -89,7 +89,7 @@ class RcsPhLhnBlock extends BlockBase implements ContainerFactoryPluginInterface
       '#attributes' => [
         'id' => 'rcs-ph-lhn_block',
         'data-param-get-data' => 'false',
-        'class' => ['block-alshaya-category-lhn-block', 'rcs-loading'],
+        'class' => ['block-alshaya-category-lhn-block'],
         'data-param-entity-to-get' => 'navigation_menu',
       ],
     ];
