8ff6b4ae58c62d1032659568ff347a934fea37d1
vagrant <vagrant@local.alshaya.com>
2021-10-17 10:45:15 +0000

    CORE-35258: Slick slider counter for mobile in classic gallery.

--- docroot/modules/custom/alshaya_product_zoom/js/product_mobile_zoom.js
+++ docroot/modules/custom/alshaya_product_zoom/js/product_mobile_zoom.js
@@ -103,7 +103,7 @@
           var $status = $('.slider__counter');
           var $slickElement = $('#product-image-gallery-mobile');
           $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-            //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+            // currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
             var i = (currentSlide ? currentSlide : 0) + 1;
             $status.text(i + '/' + slick.slideCount);
           });
