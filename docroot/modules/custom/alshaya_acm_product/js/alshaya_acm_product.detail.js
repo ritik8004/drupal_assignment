5947393ac3909c6e65b16c28d2943886349cec86
Swarad Mokal <swarad07@gmail.com>
2021-10-20 10:40:42 +0530

    CORE-35003: Adding variables in logger.

--- docroot/modules/custom/alshaya_acm_product/js/alshaya_acm_product.detail.js
+++ docroot/modules/custom/alshaya_acm_product/js/alshaya_acm_product.detail.js
@@ -351,10 +351,10 @@
 
     if (typeof combinations[selectedCode][selectedValue] !== 'object' || combinations[selectedCode][selectedValue] === null) {
       Drupal.alshayaLogger('warning', 'Error occurred during attribute selection, sku: @sku, combinations: @combinations, selectedCode: @selectedCode, selectedValue: @selectedValue', {
-        'sku': sku,
-        'combinations': JSON.stringify(combinations),
-        'selectedCode': selectedCode,
-        'selectedValue': selectedValue,
+        '@sku': sku,
+        '@combinations': JSON.stringify(combinations),
+        '@selectedCode': selectedCode,
+        '@selectedValue': selectedValue,
       });
       return;
     }
