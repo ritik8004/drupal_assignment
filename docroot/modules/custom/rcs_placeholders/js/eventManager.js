ca2508a134e099ac01fcb3e054bf6b7f1c47e779
vivekpan27 <vivek.panicker@innoraft.com>
2021-10-22 11:10:04 +0530

    CORE-32871: Removed unnecessary code from event manager.

--- docroot/modules/custom/rcs_placeholders/js/eventManager.js
+++ docroot/modules/custom/rcs_placeholders/js/eventManager.js
@@ -3,22 +3,6 @@
  * Copyright (c) 2009, Howard Rauscher
  * Licensed under the MIT License
  */
-
-EventArg = function (name, data) {
-  this.name = name;
-  this.data = data;
-  this.cancelled = false;
-  this.removed = false;
-};
-EventArg.prototype = {
-  cancel: function () {
-    this.cancelled = true;
-  },
-  remove: function () {
-    this.removed = true;
-  },
-};
-
 function EventManager() {
   this.listeners = {};
 }
@@ -29,8 +13,8 @@ EventManager.prototype = {
     }
 
     // Initialize.
-    this.listeners[name] = typeof this.listeners[name] !== 'undefined' ? this.listeners[name] : {};
-    this.listeners[name][priority] = typeof this.listeners[name][priority] !== 'undefined' ? this.listeners[name][priority] : [];
+    this.listeners[name] = this.listeners[name] || {};
+    this.listeners[name][priority] = this.listeners[name][priority] || [];
 
     // Add the listener.
     this.listeners[name][priority].push(fn);
@@ -44,22 +28,10 @@ EventManager.prototype = {
 
     args = args || {};
     if (typeof listeners !== 'undefined') {
-      var data = {}, evt;
       var len = listeners.length;
 
       for (var i = len -1; i >= 0; i--) {
-        evt = new EventArg(name, data);
         listeners[i].call(window, args);
-        data = evt.data;
-
-        if (evt.removed) {
-          listeners.splice(i, 1);
-          len = listeners.length;
-          --i;
-        }
-        if (evt.cancelled) {
-          break;
-        }
       }
     }
     return args;
