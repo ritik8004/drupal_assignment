b40b3244494fdadbda67adaaeeaee2c51e106ffa
Kunal Singh <skunal185@gmail.com>
2021-10-28 16:41:31 +0530

    CORE-35432: Updated the user logger JS function to be more generic.

--- docroot/modules/custom/alshaya_user/js/user_action_logger.js
+++ docroot/modules/custom/alshaya_user/js/user_action_logger.js
@@ -1,61 +1,40 @@
-(function ($, Drupal) {
+(function ($, Drupal, drupalSettings) {
   'use strict';
+  // Track user activity around login/reset/register forms.
+  var userLogger = JSON.parse(localStorage.getItem('user_action_logger'));
+  if (userLogger && (drupalSettings.user.uid > 0 || userLogger.type === 'change-pwd-form')) {
+    Drupal.alshayaLogger(userLogger.level, userLogger.message, userLogger.context);
+    // Remove the local storage after logging the message.
+    localStorage.removeItem('user_action_logger');
+  }
 
   Drupal.behaviors.alshayaUserAction = {
-    attach: function (context, settings) {
-      // Track user activity around login/reset/register forms.
-      let userLogger = JSON.parse(localStorage.getItem('user_action_logger'));
-      if (userLogger && (settings.user.uid > 0 || userLogger.type === 'password_change')) {
-        Drupal.alshayaLogger(userLogger.level, userLogger.message);
-        // Remove the local storage after logging the message.
-        localStorage.removeItem('user_action_logger');
-      }
-
-      // User login via Form.
-      $("#user-login-form", context).on('submit', function (e) {
+    attach: function (context) {
+      // User action via Form.
+      $("#user-login-form, #user-register-form, #user-pass, #change-pwd-form", context).on('submit', function () {
         localStorage.setItem('user_action_logger', JSON.stringify({
           'level': 'debug',
-          'type': 'user_login',
-          'message': 'User performed login operation via Login form.',
-        }));
-      });
-      // User register via Form.
-      $("#user-register-form", context).on('submit', function (e) {
-        localStorage.setItem('user_action_logger', JSON.stringify({
-          'level': 'debug',
-          'type': 'user_register',
-          'message': 'User performed registeration and email verification operation via Register form.',
+          'type': $(this).attr('id'),
+          'message': 'Guest user attempted to submit in form @formId.',
+          'context': {
+            '@formId': $(this).attr('id'),
+          }
         }));
       });
       // User logout link action.
-      $(".signout-link", context).on('click', function (e) {
+      $(".signout-link", context).on('click', function () {
         Drupal.alshayaLogger('debug', 'User performed logout action');
       });
       // Social login action.
-      $('.auth-link').on('click', function (e) {
+      $('.auth-link').on('click', function () {
         localStorage.setItem('user_action_logger', JSON.stringify({
           'level': 'debug',
           'type': 'social_authentication',
           'message': 'User performed social authentication for Login.',
-        }));
-      });
-      // User password reset.
-      $("#user-pass", context).on('submit', function () {
-        localStorage.setItem('user_action_logger', JSON.stringify({
-          'level': 'debug',
-          'type': 'password_reset',
-          'message': 'User performed password reset.',
-        }));
-      });
-      // Password change.
-      $("#change-pwd-form").on('submit', function () {
-        localStorage.setItem('user_action_logger', JSON.stringify({
-          'level': 'debug',
-          'type': 'password_change',
-          'message': 'User performed password change action.',
+          'context': '',
         }));
       });
     }
   }
 
-})(jQuery, Drupal);
+})(jQuery, Drupal, drupalSettings);
