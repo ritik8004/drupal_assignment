4d015023cd54ea9e0885ac66d4ed744f60312116
Anurag Sharma <anuragsharmam@3197917.no-reply.drupal.org>
2021-10-29 13:39:09 +0530

    CORE-35589: Reduce code complexity.

--- docroot/modules/react/alshaya_sofa_sectional/js/sofa_sectional.js
+++ docroot/modules/react/alshaya_sofa_sectional/js/sofa_sectional.js
@@ -2,34 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SofaSectionalForm from './components/sofa-sectional';
 
-// Check if the PDP form element is exist and
-// data-sku is present, then render the react form.
-const pdpFormEelement = document.querySelector('.has-sofa-sectional-full-form .sku-base-form');
-if (pdpFormEelement) {
-  const { sku } = pdpFormEelement.dataset;
-  if (typeof sku !== 'undefined') {
-    ReactDOM.render(
-      <SofaSectionalForm sku={sku} />,
-      pdpFormEelement,
-    );
+/**
+ * Function to render Sofa and Section form
+ * for the given element selector.
+ *
+ * @param {string} elementSelector
+ *  Element selector for rendering form.
+ */
+const renderSofaSectionalForm = (elementSelector) => {
+  const selectedFormEelement = document.querySelector(elementSelector);
+  if (selectedFormEelement) {
+    // Get sku from forms data-sku attribute.
+    const { sku } = selectedFormEelement.dataset;
+    if (typeof sku !== 'undefined') {
+      ReactDOM.render(
+        <SofaSectionalForm sku={sku} />,
+        selectedFormEelement,
+      );
+    }
   }
-}
+};
 
 /**
  * Method to handle the modal on load event and render component.
  */
 const handleModalOnLoad = () => {
-  const modalFormEelement = document.querySelector('.has-sofa-sectional-modal-form .sku-base-form');
-  if (modalFormEelement) {
-    const { sku } = modalFormEelement.dataset;
-    if (typeof sku !== 'undefined') {
-      ReactDOM.render(
-        <SofaSectionalForm sku={sku} />,
-        modalFormEelement,
-      );
-    }
-  }
+  renderSofaSectionalForm('.has-sofa-sectional-modal-form .sku-base-form');
 };
+
+// Check if the PDP form element is exist and
+// data-sku is present, then render the react form.
+renderSofaSectionalForm('.has-sofa-sectional-full-form .sku-base-form');
 
 // Add modal load event listener to render new
 // sofa and sectional form whenever modal opens.
