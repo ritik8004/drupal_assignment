fb89087d947589bb81f4f673d68387e94dcb1c33
Sanket Jain <sanket.jain@pimocre.services>
2021-10-29 13:41:53 +0530

    CORE-34857: Minor code changes as per Pr feedback.

--- docroot/modules/custom/alshaya_color_split/js/alshaya_color_split.grouped_configurable.js
+++ docroot/modules/custom/alshaya_color_split/js/alshaya_color_split.grouped_configurable.js
@@ -37,12 +37,14 @@
         }
 
         // Update sameday and express delivery labels on variant change.
-        for (var option in variantInfo.delivery_options) {
-          $(node).find('.' + option).removeClass('active in-active');
-          $(node).find('.' + option).addClass(variantInfo.delivery_options[option].status);;
+        if (drupalSettings.expressDelivery !== 'undefined' && drupalSettings.expressDelivery.enabled) {
+          for (var option in variantInfo.delivery_options) {
+            $(node).find('.' + option).removeClass('active in-active');
+            $(node).find('.' + option).addClass(variantInfo.delivery_options[option].status);;
+          }
+          $(node).find('.express-delivery').removeClass('active in-active');
+          $(node).find('.express-delivery').addClass(variantInfo.express_delivery_class);
         }
-        $(node).find('.express-delivery').removeClass('active in-active');
-        $(node).find('.express-delivery').addClass(variantInfo.express_delivery_class);
 
         var productChanged = false;
         if ($(node).attr('data-vmode') === 'full') {
--- docroot/modules/custom/alshaya_color_split/js/alshaya_color_split.grouped_simple.js
+++ docroot/modules/custom/alshaya_color_split/js/alshaya_color_split.grouped_simple.js
@@ -33,12 +33,14 @@
         $(node).trigger('group-item-selected', [$(this).val()]);
 
         // Update sameday and express delivery labels on variant change.
-        for (var option in variantInfo.delivery_options) {
-          $(node).find('.' + option).removeClass('active in-active');
-          $(node).find('.' + option).addClass(variantInfo.delivery_options[option].status);;
+        if (drupalSettings.expressDelivery !== 'undefined' && drupalSettings.expressDelivery.enabled) {
+          for (var option in variantInfo.delivery_options) {
+            $(node).find('.' + option).removeClass('active in-active');
+            $(node).find('.' + option).addClass(variantInfo.delivery_options[option].status);;
+          }
+          $(node).find('.express-delivery').removeClass('active in-active');
+          $(node).find('.express-delivery').addClass(variantInfo.express_delivery_class);
         }
-        $(node).find('.express-delivery').removeClass('active in-active');
-        $(node).find('.express-delivery').addClass(variantInfo.express_delivery_class);
 
         if (viewMode === 'full' || viewMode === 'matchback' || viewMode === 'matchback_mobile') {
           $(node).find('.content--item-code .field__value').html($(this).val());
