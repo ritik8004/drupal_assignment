800cb72b0e3efc9af8598b569595693653ff2a29
Sonam Singh <sonam.singh@innoraft.com>
2021-11-08 13:09:08 +0530

    CORE-35522: Update empty transaction text.

--- docroot/modules/react/alshaya_aura_react/js/components/loyalty-club/loyalty-club-rewards-activity/empty-reward-activity.js
+++ docroot/modules/react/alshaya_aura_react/js/components/loyalty-club/loyalty-club-rewards-activity/empty-reward-activity.js
@@ -1,31 +1,15 @@
 import React from 'react';
-import AppStoreSVG
-  from '../../../../../alshaya_spc/js/svg-component/app-store-svg';
-import { getAuraConfig } from '../../../utilities/helper';
 
-const EmptyRewardActivity = () => {
-  const {
-    appStoreLink: appleAppStoreLink,
-    googlePlayLink: googlePlayStoreLink,
-    siteName,
-  } = getAuraConfig();
-
-  return (
-    <>
-      <div className="empty-reward-activity-content">
-        <span>{`${Drupal.t('You have no previous Aura transactions with')} `}</span>
-        <span className="highlight">{siteName}</span>
-        <span>{` ${Drupal.t('to display.')}`}</span>
-      </div>
-      <div>
-        {`${Drupal.t('To view rewards activity across all our brands, visit our Loyalty app')} `}
-        <div className="app-links">
-          <a href={appleAppStoreLink}><AppStoreSVG store="appstore" /></a>
-          <a href={googlePlayStoreLink}><AppStoreSVG store="playstore" /></a>
-        </div>
-      </div>
-    </>
-  );
-};
+const EmptyRewardActivity = () => (
+  <>
+    <div className="empty-reward-activity-content">
+      {Drupal.t('You currently have no Aura linked transactions.')}
+    </div>
+    <div>
+      {`${Drupal.t('To see your offers and rewards, visit your AURA MENA app or')} `}
+      <a href="aura-mena.com">aura-mena.com</a>
+    </div>
+  </>
+);
 
 export default EmptyRewardActivity;
