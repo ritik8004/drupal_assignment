4047c2b5f444336c64c0965326837d273ae1fca5
vagrant <vagrant@local.alshaya.com>
2021-11-15 11:34:21 +0000

    CORE-35831: Make feature enable disable.

---
+++ docroot/modules/react/alshaya_egift_card/src/Helper/EgiftCardHelper.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\alshaya_egift_card\Helper;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+
+/**
+ * Helper class for Egift Card.
+ *
+ * @package Drupal\alshaya_egift_card\Helper
+ */
+class EgiftCardHelper {
+
+  protected $configFactory;
+
+  /**
+   * The api helper object.
+   *
+   * @var Drupal\alshaya_egift_card\Helper\EgiftCardHelper
+   */
+
+    /**
+     * EgiftCardHelper constructor.
+     *
+     * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+     *   Config Factory service object.
+     */
+  public function __construct(
+    ConfigFactoryInterface $config_factory
+  ) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * Helper to check if EgiftCard is enabled.
+   *
+   * @return bool
+   *   TRUE/FALSE
+   */
+  public function isEgiftCardEnabled() {
+    return $this->configFactory->get('alshaya_egift_card.settings')->get('egift_card_enabled');
+  }
+}
