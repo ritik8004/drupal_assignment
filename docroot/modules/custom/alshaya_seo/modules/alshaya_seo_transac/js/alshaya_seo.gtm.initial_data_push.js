d067ded456bae9a9f6debfd17977d378fcc59a4f
koustubh dudhe <dudhe.koustubh@gmail.com>
2021-11-16 12:38:07 +0530

    CORE-18659: Change as per code review comment.

--- docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.initial_data_push.js
+++ docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.initial_data_push.js
@@ -4,16 +4,10 @@
  */
 
 (function ($, Drupal) {
-  'use strict';
-
-  Drupal.behaviors.initial_data_push = {
-    attach: function (context, settings) {
-      document.addEventListener('alterInitialData', (e) => {
-        let cartId = localStorage.getItem('cart_id');
-        if (cartId) {
-          e.detail.data().cart_id = cartId;
-        }
-      });
+  document.addEventListener('alterInitialDataLayerData', (e) => {
+    let cartId = localStorage.getItem('cart_id');
+    if (cartId) {
+      e.detail.data().cart_id = cartId;
     }
-  };
+  });
 })(jQuery, Drupal);
