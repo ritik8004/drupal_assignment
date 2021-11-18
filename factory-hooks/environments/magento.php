da0b188de7b1c5cf9f55dbae32a5b6e19aa57321
joshirohit100 <joshi.rohit100@gmail.com>
2021-11-18 10:50:24 +0530

    CORE-001: Adding ACM mapping for MU-QATAR for dev3.

--- factory-hooks/environments/magento.php
+++ factory-hooks/environments/magento.php
@@ -1478,6 +1478,30 @@ $magentos = [
       'store_id' => ['en' => 12, 'ar' => 11],
     ],
   ],
+  'mu_apc' => [
+    'url' => 'https://apc-7vc7xgy-szaftnexsfo4k.eu-3.magentosite.cloud',
+    'magento_secrets' => [
+      'consumer_key' => 'd8gfbetgjq5ivnqoqas85hdpi1zdplf1',
+      'consumer_secret' => 'gaxphnn61qgg5js9dsy2xrwlc70dehn5',
+      'access_token' => '214yfy8hauwdqu4b25ci573iefokdp85',
+      'access_token_secret' => 'z6jfn1akazvc86cpq5v1ymwi5ikcouhr',
+    ],
+    'kw' => [
+      'store_id' => ['en' => 1, 'ar' => 6],
+    ],
+    'sa' => [
+      'store_id' => ['en' => 2, 'ar' => 3],
+    ],
+    'ae' => [
+      'store_id' => ['en' => 4, 'ar' => 5],
+    ],
+    'eg' => [
+      'store_id' => ['en' => 8, 'ar' => 7],
+    ],
+    'qa' => [
+      'store_id' => ['en' => 12, 'ar' => 11],
+    ],
+  ],
   'mu_uat' => [
     'url' => 'https://muji-uat.store.alshaya.com',
     'magento_secrets' => [
