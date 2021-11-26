7e7e846e02e744c34776ec0bacb1d07b23767c23
Kunal Singh <skunal185@gmail.com>
2021-11-26 12:19:09 +0530

    CORE-34406: Enabled rcs category enrichment.

--- docroot/modules/custom/rcs_placeholders/src/Service/RcsPhPathProcessor.php
+++ docroot/modules/custom/rcs_placeholders/src/Service/RcsPhPathProcessor.php
@@ -135,7 +135,7 @@ class RcsPhPathProcessor implements InboundPathProcessorInterface {
 
     if (strpos($rcs_path_to_check, '/' . $category_prefix) === 0) {
       self::$entityType = 'category';
-      self::$entityPath = substr_replace($rcs_path_to_check, '', 0, strlen($category_prefix) + 1);
+      self::$entityPath = $rcs_path_to_check;
       self::$entityPathPrefix = $category_prefix;
 
       self::$processedPaths[$rcs_path_to_check] = '/taxonomy/term/' . $config->get('category.placeholder_tid');
