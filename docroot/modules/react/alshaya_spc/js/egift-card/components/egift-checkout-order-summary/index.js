82f65f53494f12c6c1b694a2c047a586a01e4a98
vagrant <vagrant@local.alshaya.com>
2021-12-09 08:53:07 +0000

    CORE-36013: Show pay with egift card on checkout page on total section.

---
+++ docroot/modules/react/alshaya_spc/js/egift-card/components/egift-checkout-order-summary/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import TotalLineItem from '../../../utilities/total-line-item';
+
+const EgiftCheckoutOrderSummary = (props) => {
+  const { totals } = props;
+
+  if (totals === undefined || totals === null) {
+    return null;
+  }
+
+  const { egiftRedeemedAmount, balancePayable } = totals;
+
+  if (egiftRedeemedAmount === undefined || balancePayable === undefined || egiftRedeemedAmount === 0) {
+    return null;
+  }
+
+  return (
+    <div className="egift-order-summary">
+      <TotalLineItem
+        name="paid-with-egift"
+        title={Drupal.t('Paid With eGfit card')}
+        value={egiftRedeemedAmount}
+      />
+        <TotalLineItem
+          name="balance-payable"
+          title={Drupal.t('Balance Payable')}
+          value={balancePayable}
+          showZeroValue
+        />
+    </div>
+  );
+};
+
+export default EgiftCheckoutOrderSummary;
