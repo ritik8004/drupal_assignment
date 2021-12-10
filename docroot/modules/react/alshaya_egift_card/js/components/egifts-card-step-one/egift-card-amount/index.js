9812d848bd22e473909036d425fc08cb5ead9345
Avinash <avinash.shukla@acquia.com>
2021-12-10 16:05:18 +0530

    CORE-36005: Added active select for amount items and fixed media image from api.

--- docroot/modules/react/alshaya_egift_card/js/components/egifts-card-step-one/egift-card-amount/index.js
+++ docroot/modules/react/alshaya_egift_card/js/components/egifts-card-step-one/egift-card-amount/index.js
@@ -11,11 +11,36 @@ const EgiftCardAmount = (props) => {
   // Get amounts that user can select from api response items.
   const amounts = selected.extension_attributes.hps_giftcard_amount;
 
+  const handleAmount = (e, amount) => {
+    // Remove any existing active class.
+    const amountElements = document.querySelectorAll('.item-amount');
+    [].forEach.call(amountElements, (el) => {
+      el.classList.remove('active');
+    });
+
+    // Empty open amount field and unlock
+    const openAmountInput = document.getElementById('open-amount');
+    if (openAmountInput !== null) {
+      openAmountInput.value = '';
+      openAmountInput.removeAttribute('disabled');
+      // Remove any error message from open amount.
+      document.getElementById('open-amount-error').innerHTML = '';
+    }
+
+    // Set target as element as active.
+    const element = e.target;
+    element.classList.add('active');
+
+    // Set amount for step 2.
+    handleAmountSelect(true, amount);
+  };
+
   // List all amounts.
   const listItems = amounts.map((amount) => (
     <li
-      onClick={() => handleAmountSelect(true, amount.value)}
       key={amount.value}
+      className="item-amount"
+      onClick={(e) => handleAmount(e, amount.value)}
     >
       {amount.value}
     </li>
