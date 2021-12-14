56571c10d7db3ce7c9a181a579c912ab87230f57
Anurag Sharma <anuragsharmam@3197917.no-reply.drupal.org>
2021-12-14 13:42:19 +0530

    CORE-36767: Open email popup with the given config text.

--- docroot/modules/react/alshaya_wishlist/js/components/wishlist-share/share-popup.js
+++ docroot/modules/react/alshaya_wishlist/js/components/wishlist-share/share-popup.js
@@ -11,17 +11,32 @@ export default class SharePopup extends React.Component {
     };
   }
 
+  /**
+   * Close the modal with the parent state change.
+   */
   closeModal = () => {
     const { closeWishlistModal } = this.props;
     // If user simply clicks on close, we pass false as response.
     closeWishlistModal(false);
   }
 
+  /**
+   * Handler to copy the share link on clipboard.
+   */
   copyShareLink = () => {
     // Set the copyLinkStatus to true when link is copied.
     // @todo: change the url with the actual share link.
     navigator.clipboard.writeText('https://www.google.com/');
     this.setState({ copyLinkStatus: true });
+  }
+
+  /**
+   * Handler to open the email client with share link.
+   */
+  emailClickHandler = (e) => {
+    // Open mailto with window location helper.
+    window.location = `mailto:?subject=${encodeURIComponent(drupalSettings.wishlist.config.shareEmailSubject) || ''}&body=${encodeURIComponent(drupalSettings.wishlist.config.shareEmailMessage) || ''}`;
+    e.preventDefault();
   }
 
   render() {
@@ -42,7 +57,7 @@ export default class SharePopup extends React.Component {
             >
               {Drupal.t('Close')}
             </a>
-            <div className="header">{Drupal.t('Share Your List')}</div>
+            <div className="header">{Drupal.t('Share Your List', {}, { context: 'wishlist' })}</div>
             <div className="content">
               {Drupal.t('Share all your faviourites with friends and family.')}
             </div>
@@ -50,6 +65,7 @@ export default class SharePopup extends React.Component {
               <button
                 type="button"
                 className="email-share-link"
+                onClick={(e) => this.emailClickHandler(e)}
               >
                 {Drupal.t('Email')}
               </button>
@@ -58,7 +74,7 @@ export default class SharePopup extends React.Component {
                   <span
                     className="copy-share-link link-copied"
                   >
-                    {Drupal.t('Link Copied')}
+                    {Drupal.t('Link Copied', {}, { context: 'wishlist' })}
                   </span>
                 )
                 : (
@@ -67,7 +83,7 @@ export default class SharePopup extends React.Component {
                     className="copy-share-link"
                     onClick={this.copyShareLink}
                   >
-                    {Drupal.t('Copy Link')}
+                    {Drupal.t('Copy Link', {}, { context: 'wishlist' })}
                   </button>
                 )}
             </div>
