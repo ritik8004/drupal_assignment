50ab7e4350157c95328ab95974978bec3bf0da6a
Sanket Jain <sanket.jain@pimocre.services>
2021-12-21 16:02:02 +0530

    CORE-36769: Added wishlist event listener for add to cart event.

---
+++ docroot/modules/react/alshaya_wishlist/assets/js/wishlist_add_to_cart.js
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * Contains utility function for wishlist add to cart.
+ */
+
+(function ($, Drupal, drupalSettings) {
+    'use strict';
+  
+    Drupal.behaviors.alshayaWishlistAddToCart = {
+      attach: function (context) {
+        // Trigger event for wishlist removal on product-add-to-cart-success.
+        $('.sku-base-form', context).once('wishlist-button-wrapper').on('product-add-to-cart-success', function (event) {
+					var productData = event.detail.productData;
+					if (productData && productData !== null) {
+						var wishlistAddToCartEvent = new CustomEvent('onProductAddToCart', {
+							bubbles: true,
+							detail: {
+								productData,
+							}
+						});
+						document.dispatchEvent(wishlistAddToCartEvent);
+					}
+        });
+      }
+    };
+  
+  })(jQuery, Drupal, drupalSettings);
+  
