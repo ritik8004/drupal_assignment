e2101d55eaa79e461b13c7e370c9b296a04c454f
Sanket Jain <sanket.jain@pimocre.services>
2021-12-21 16:27:04 +0530

    CORE-36769: Corrected extra lines in js file.

--- docroot/modules/react/alshaya_wishlist/assets/js/wishlist_add_to_cart.js
+++ docroot/modules/react/alshaya_wishlist/assets/js/wishlist_add_to_cart.js
@@ -4,12 +4,12 @@
  */
 
 (function ($, Drupal, drupalSettings) {
-    'use strict';
-  
-    Drupal.behaviors.alshayaWishlistAddToCart = {
-      attach: function (context) {
-        // Trigger event for wishlist removal on product-add-to-cart-success.
-        $('.sku-base-form', context).once('wishlist-button-wrapper').on('product-add-to-cart-success', function (event) {
+	'use strict';
+
+	Drupal.behaviors.alshayaWishlistAddToCart = {
+		attach: function (context) {
+		// Trigger event for wishlist removal on product-add-to-cart-success.
+		$('.sku-base-form', context).once('wishlist-button-wrapper').on('product-add-to-cart-success', function (event) {
 					var productData = event.detail.productData;
 					if (productData && productData !== null) {
 						var wishlistAddToCartEvent = new CustomEvent('onProductAddToCart', {
@@ -20,9 +20,8 @@
 						});
 						document.dispatchEvent(wishlistAddToCartEvent);
 					}
-        });
-      }
-    };
-  
-  })(jQuery, Drupal, drupalSettings);
-  
+		});
+		}
+	};
+
+})(jQuery, Drupal, drupalSettings);
