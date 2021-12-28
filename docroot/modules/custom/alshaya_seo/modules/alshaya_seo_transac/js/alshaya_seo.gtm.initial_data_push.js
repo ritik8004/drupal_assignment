93a8596daa813d28c56d275fcc78d1b6ed7882fc
koustubh dudhe <dudhe.koustubh@gmail.com>
2021-12-28 17:26:39 +0530

    CORE-18659: Remove unused params as per code review comments.

--- docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.initial_data_push.js
+++ docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.initial_data_push.js
@@ -3,11 +3,11 @@
  * Event Listener to alter datalayer.
  */
 
-(function ($, Drupal) {
+(function () {
   document.addEventListener('alterInitialDataLayerData', (e) => {
     let cartId = localStorage.getItem('cart_id');
     if (cartId) {
       e.detail.data().cart_id = cartId;
     }
   });
-})(jQuery, Drupal);
+})();
