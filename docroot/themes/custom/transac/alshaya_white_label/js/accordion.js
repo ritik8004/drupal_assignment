c873106029339cc63c733f14403f7b0a69f67a3a
Vasanth <vasanth.a@alshaya.com>
2021-12-31 10:57:32 +0000

    CORE-35917: Moved the function outside behaviours.

--- docroot/themes/custom/transac/alshaya_white_label/js/accordion.js
+++ docroot/themes/custom/transac/alshaya_white_label/js/accordion.js
@@ -23,19 +23,17 @@
   Drupal.behaviors.accordion = {
     attach: function (context, settings) {
       // Accordion for advance page category for mobile.
-      if ($('.c-accordion').once) {
-        $('.c-accordion').each(function () {
-          if ($(this).find('ul').length > 0) {
-            covertToAccordion($(this));
-          }
-          else {
-            $(this).addClass('empty-accordion-delivery-options');
-          }
-          // Add class on parent of c-accordion-delivery-options so we can hide
-          // the paragraph with margin in desktop.
-          $(this).parents('.c-promo__item').addClass('c-accordion-delivery-option-parent');
-        });
-      }
+      $('.c-accordion').once().each(function () {
+        if ($(this).find('ul').length > 0) {
+          covertToAccordion($(this));
+        }
+        else {
+          $(this).addClass('empty-accordion-delivery-options');
+        }
+        // Add class on parent of c-accordion-delivery-options so we can hide
+        // the paragraph with margin in desktop.
+        $(this).parents('.c-promo__item').addClass('c-accordion-delivery-option-parent');
+      });
     }
   };
 
