82eab294a5cafd4343337138d7d4157e20c73a2d
Rohit Joshi <joshi.rohit100@gmail.com>
2022-01-05 16:08:34 +0530

    Merge pull request #22681 from acquia-pso/develop-feature-wishlist
    [WIP] [DNM] CORE-36001: Merge WishList in Mainstream

---
+++ docroot/modules/react/alshaya_wishlist/js/wishlistheader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WishlistHeader from './components/wishlist-header';
+
+ReactDOM.render(
+  <WishlistHeader />,
+  document.getElementById('wishlist-header-wrapper'),
+);
