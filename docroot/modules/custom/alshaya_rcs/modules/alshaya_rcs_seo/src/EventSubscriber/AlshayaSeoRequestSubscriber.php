a2e6602d003962e6355875085cde886d0a76b747
vivekpan27 <vivek.panicker@innoraft.com>
2022-01-07 12:25:38 +0530

    CORE-37407: Used routematch object and set lower priority.

--- docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_seo/src/EventSubscriber/AlshayaSeoRequestSubscriber.php
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_seo/src/EventSubscriber/AlshayaSeoRequestSubscriber.php
@@ -2,11 +2,11 @@
 
 namespace Drupal\alshaya_rcs_seo\EventSubscriber;
 
+use Drupal\Core\Routing\RouteMatchInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 
 /**
  * Perform redirection on PLP if trailing slash not present.
@@ -14,34 +14,37 @@ use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 class AlshayaSeoRequestSubscriber implements EventSubscriberInterface {
 
   /**
-   * A router implementation which does not check access.
+   * Route matcher.
    *
-   * @var \Symfony\Component\Routing\Matcher\UrlMatcherInterface
+   * @var \Drupal\Core\Routing\RouteMatchInterface
    */
-  protected $accessUnawareRouter;
+  protected $routeMatch;
 
   /**
    * Constructs a AlshayaSeoRequestSubscriber object.
    *
-   * @param \Symfony\Component\Routing\Matcher\UrlMatcherInterface $accessUnawareRouter
-   *   A router implementation which does not check access.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   Route matcher.
    */
-  public function __construct(UrlMatcherInterface $accessUnawareRouter) {
-    $this->accessUnawareRouter = $accessUnawareRouter;
+  public function __construct(RouteMatchInterface $route_match) {
+    $this->routeMatch = $route_match;
   }
 
   /**
-   * Performs a redirect if trailing slash not present in PLP.
+   * Performs a redirect if trailing slash not present.
    *
    * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
    *   The Event to process.
    */
   public function onKernelRequestRedirect(GetResponseEvent $event) {
+    $routeName = $this->routeMatch->getRouteName();
     $request = $event->getRequest();
     $request_path = $request->getPathInfo();
 
-    $result = $this->accessUnawareRouter->match($request_path);
-    if ($result['_route'] && ($result['_route'] == 'entity.taxonomy_term.canonical' || $result['_route'] == 'alshaya_master.home')) {
+    if (in_array($routeName, [
+      'entity.taxonomy_term.canonical',
+      'alshaya_master.home',
+    ])) {
       if (substr($request_path, -1) != '/') {
         $request_uri = $request_path . '/';
         $response = new RedirectResponse($request_uri, 301);
@@ -55,7 +58,9 @@ class AlshayaSeoRequestSubscriber implements EventSubscriberInterface {
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
-    $events[KernelEvents::REQUEST][] = ['onKernelRequestRedirect', 100];
+    // If priority is made higher than this, then the route match object returns
+    // null.
+    $events[KernelEvents::REQUEST][] = ['onKernelRequestRedirect', 32];
     return $events;
   }
 
