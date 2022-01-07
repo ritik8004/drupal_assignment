987cc371b9d8e41d25048839c459a93b17befbed
vivekpan27 <vivek.panicker@innoraft.com>
2022-01-07 12:49:45 +0530

    CORE-29594: Removed comments.

--- docroot/modules/react/alshaya_spc/src/Helper/AlshayaSpcCustomerHelper.php
+++ docroot/modules/react/alshaya_spc/src/Helper/AlshayaSpcCustomerHelper.php
@@ -379,9 +379,6 @@ class AlshayaSpcCustomerHelper {
    *   The token or null.
    */
   public function getCustomerTokenBySocialDetail($mail) {
-    // @todo This function should only be responsible to return the token from
-    // session. Token creation will happen when the user authenticates either
-    // using user/pass or social login. See Ticket CORE-29594.
     $token = $this->getCustomerToken();
     if (empty($token) || !is_string($token)) {
       $token = json_decode($this->apiWrapper->getCustomerTokenBySocialDetail($mail));
