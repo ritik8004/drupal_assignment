8e7d31527b41dae455af0b89f24e9bfa85b3ff44
Anurag Sharma <anuragsharmam@3197917.no-reply.drupal.org>
2022-01-10 18:47:53 +0530

    CORE-36331: Removed unused file and added a react utility using alshaya local storage manager functions as a base.

--- docroot/modules/react/js/utilities/storage.js
+++ docroot/modules/react/js/utilities/storage.js
@@ -1,23 +1,52 @@
-export const setStorageInfo = (data, storageKey) => {
-  const dataToStore = (typeof data === 'object') ? JSON.stringify(data) : data;
-  localStorage.setItem(storageKey, dataToStore);
-};
+/**
+ * React utility functions for the alshaya local storage manager. Originally,
+ * functions are available in `alshaya_master/js/local_storage_manager.js`.
+ */
 
-export const removeStorageInfo = (storageKey) => {
-  localStorage.removeItem(storageKey);
-};
+/**
+   * Helper function to add an item into the local storage.
+   *
+   * @param {object|string} storageData
+   *  Data to store in the local storage.
+   * @param {string} storageKey
+   *  A string key to identify provided data in storage.
+   * @param {integer} expireAfterTime
+   *  Time, in seconds, after this data will be expired and clean.
+   *
+   * @returns {boolean}
+   *  true/false based on the action performed.
+   */
+export const setStorageInfo = (
+  storageData,
+  storageKey,
+  expireAfterTime = null,
+) => Drupal.getItemFromLocalStorage(
+  storageKey,
+  storageData,
+  expireAfterTime,
+);
 
-export const getStorageInfo = (storageKey) => {
-  const storageItem = localStorage.getItem(storageKey);
-  if (!storageItem) {
-    return null;
-  }
+/**
+ * Helper function to remove an item from the local storage.
+ *
+ * @param {string} storageKey
+ *  Local storage key to remove associated data.
+ *
+ * @returns {boolean}
+ *  true/false based on the action performed.
+ */
+export const removeStorageInfo = (storageKey) => Drupal.removeItemFromLocalStorage(storageKey);
 
-  try {
-    const storageItemArray = JSON.parse(storageItem);
-    // @TODO: Handle storage expiration.
-    return storageItemArray;
-  } catch (e) {
-    return storageItem;
-  }
-};
+/**
+ * Helper function to get data from the local storage for the
+ * provided key. This will removed the item from the storage
+ * as well if the data is expired.
+ *
+ * @param {string} storageKey
+ *  Local storage key to get associated data for.
+ *
+ * @returns {object|string}
+ *  Return null if no data available or is expired.
+ *  Return data object if available.
+ */
+export const getStorageInfo = (storageKey) => Drupal.getItemFromLocalStorage(storageKey);
