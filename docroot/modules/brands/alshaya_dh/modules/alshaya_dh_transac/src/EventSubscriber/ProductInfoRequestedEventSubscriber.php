7ba2e1d930d370a3270bd4116f4653f622921bc4
koustubh dudhe <dudhe.koustubh@gmail.com>
2022-01-21 18:37:53 +0530

    CORE-37606: Reused available methods for fetching descripition.

--- docroot/modules/brands/alshaya_dh/modules/alshaya_dh_transac/src/EventSubscriber/ProductInfoRequestedEventSubscriber.php
+++ docroot/modules/brands/alshaya_dh/modules/alshaya_dh_transac/src/EventSubscriber/ProductInfoRequestedEventSubscriber.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\alshaya_dh_transac\EventSubscriber;
 
-use Drupal\acq_sku\Entity\SKU;
 use Drupal\acq_sku\ProductInfoRequestedEvent;
 use Drupal\alshaya_acm_product\ProductHelper;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
@@ -74,7 +73,7 @@ class ProductInfoRequestedEventSubscriber implements EventSubscriberInterface {
    */
   public function processDescription(ProductInfoRequestedEvent $event) {
     $sku_entity = $event->getSku();
-    $description = $this->getDescription($sku_entity);
+    $description = _alshaya_dh_transac_get_product_description($sku_entity);
     $event->setValue($description['description']);
   }
 
@@ -86,7 +85,7 @@ class ProductInfoRequestedEventSubscriber implements EventSubscriberInterface {
    */
   public function processShortDescription(ProductInfoRequestedEvent $event) {
     $sku_entity = $event->getSku();
-    $description = $this->getDescription($sku_entity);
+    $description = _alshaya_dh_transac_get_product_description($sku_entity);
 
     if (!empty($description['description'][0]['value']['#markup'])) {
       // Get short description of given html.
@@ -101,36 +100,6 @@ class ProductInfoRequestedEventSubscriber implements EventSubscriberInterface {
       ];
       $event->setValue($short_desc);
     }
-  }
-
-  /**
-   * Prepare description and short description array for given sku.
-   *
-   * @param \Drupal\acq_sku\Entity\SKU $sku_entity
-   *   The sku entity.
-   *
-   * @return array
-   *   Return array of description and short description.
-   */
-  private function getDescription(SKU $sku_entity) {
-    $static = &drupal_static(__METHOD__, []);
-
-    if (!empty($static[$sku_entity->language()->getId()][$sku_entity->getSku()])) {
-      return $static[$sku_entity->language()->getId()][$sku_entity->getSku()];
-    }
-
-    $return['description'] = [];
-    $body = $sku_entity->get('attr_description')->getValue();
-    if (is_array($body) && !empty($body[0]['value'])) {
-      $return['description'][] = [
-        'value' => [
-          '#markup' => $body[0]['value'],
-        ],
-      ];
-    }
-
-    $static[$sku_entity->language()->getId()][$sku_entity->getSku()] = $return;
-    return $return;
   }
 
 }
