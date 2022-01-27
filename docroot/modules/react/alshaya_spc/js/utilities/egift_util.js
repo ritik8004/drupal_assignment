7794d54f782e59195493153977162803bb8aceeb
Hammad Ghani <mohamed.ghani@alshaya.com>
2022-01-27 14:44:06 +0000

    CORE-38115: Remove Aura redeemed points on adding egift prodcut in card after redemption.

--- docroot/modules/react/alshaya_spc/js/utilities/egift_util.js
+++ docroot/modules/react/alshaya_spc/js/utilities/egift_util.js
@@ -123,6 +123,7 @@ export const egiftFormElement = ({
  */
 export const updatePriceSummaryBlock = (refreshCart = null) => {
   // Fetch the fresh cart data and update the summary block.
+  showFullScreenLoader();
   const cartData = window.commerceBackend.getCart(true);
   if (cartData instanceof Promise) {
     cartData.then((data) => {
