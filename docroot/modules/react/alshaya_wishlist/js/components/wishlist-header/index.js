0328e8965cf99b8aaa1a8d72f5b5fc0f1361b748
Anurag Sharma <anuragsharmam@3197917.no-reply.drupal.org>
2022-01-28 19:32:26 +0530

    CORE-38132: Wishlist header icon active state on login.

--- docroot/modules/react/alshaya_wishlist/js/components/wishlist-header/index.js
+++ docroot/modules/react/alshaya_wishlist/js/components/wishlist-header/index.js
@@ -53,8 +53,8 @@ export default class WishlistHeader extends React.Component {
       // Remove wishlist info from local storage for guest users, as
       // we don't want this info to share with logged in users.
       addWishListInfoInStorage({}, guestUserStorageKey());
-
-      if (wishListDataOfGuestUser
+      if (typeof wishListDataOfGuestUser === 'object'
+        && Object.keys(wishListDataOfGuestUser).length > 0
         && hasValue(drupalSettings.wishlist.mergeWishlistForLoggedInUsers)) {
         // Merge wishlist information to the magento backend from local storage,
         // if wishlist data available in local storage and merging wishlist
