107ca9a7acd3ce13554d0219304156da82fe5c1b
Supreetam Laha <supreetam.laha@acquia.com>
2022-02-14 14:16:22 +0530

    CORE-27391: Fixing issues while attempting site setup.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -784,3 +784,11 @@ $settings['entity_update_batch_size'] = 50;
 # }
 require DRUPAL_ROOT . "/../vendor/acquia/blt/settings/blt.settings.php";
 $settings['install_profile'] = 'lightning';
+/**
+ * IMPORTANT.
+ *
+ * Do not include additional settings here. Instead, add them to settings
+ * included by `blt.settings.php`. See BLT's documentation for more detail.
+ *
+ * @link https://docs.acquia.com/blt/
+ */
