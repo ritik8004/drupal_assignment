eaf0427aff7b16d2f9669eb002ac3204dcb25f6f
Ghani <mohamed.ghani@alshaya.com>
2022-03-11 15:29:55 +0530

    CORE-39756: Text casing fix for Breadcrumb.

--- docroot/modules/react/alshaya_egift_card/src/Breadcrumb/AlshayaEgiftCardBreadcrumb.php
+++ docroot/modules/react/alshaya_egift_card/src/Breadcrumb/AlshayaEgiftCardBreadcrumb.php
@@ -30,8 +30,8 @@ class AlshayaEgiftCardBreadcrumb implements BreadcrumbBuilderInterface {
     $breadcrumb = new Breadcrumb();
     $breadcrumb->addLink(Link::createFromRoute($this->t('Home', [], ['context' => 'breadcrumb']), '<front>'));
     // @todo update route form config.
-    $breadcrumb->addLink(Link::createFromRoute($this->t('Egift Card', [], ['context' => 'breadcrumb']), '<front>'));
-    $breadcrumb->addLink(Link::createFromRoute($this->t('Buy eGift Card', [], ['context' => 'breadcrumb']), 'alshaya_egift_card.egift_card_purchase'));
+    $breadcrumb->addLink(Link::createFromRoute($this->t('eGift Card', [], ['context' => 'egift']), '<front>', [], ['attributes' => ['class' => ['egifts']]]));
+    $breadcrumb->addLink(Link::createFromRoute($this->t('Buy eGift Card', [], ['context' => 'egift']), 'alshaya_egift_card.egift_card_purchase', [], ['attributes' => ['class' => ['egifts']]]));
     $breadcrumb->addCacheableDependency(['url.path']);
     return $breadcrumb;
   }
