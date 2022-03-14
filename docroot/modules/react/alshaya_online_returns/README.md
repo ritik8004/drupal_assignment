7c91b9cc87827c00cfa03d5b0de45b8b136e325d
Sonam Singh <sonam.singh@innoraft.com>
2022-03-14 20:20:51 +0530

    DIG-4475: Create base module, utility, config and add is_returnable field to sku.

---
+++ docroot/modules/react/alshaya_online_returns/README.md
@@ -0,0 +1,6 @@
+# Module for Alshaya Online Returns.
+
+## Configurations
+
+* After `alshaya_online_returns module` is enabled, use config `online_returns_enabled` to enable/disable the feature.
+
---
+++ docroot/modules/react/alshaya_online_returns/src/Helper/OnlineReturnsHelper.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\alshaya_online_returns\Helper;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\acq_commerce\SKUInterface;
+
+/**
+ * Helper class for Online Returns.
+ *
+ * @package Drupal\alshaya_online_returns\Helper
+ */
+class OnlineReturnsHelper {
+
+  /**
+   * Config Factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * OnlineReturnsHelper constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   Config Factory service object.
+   */
+  public function __construct(
+    ConfigFactoryInterface $config_factory
+  ) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * Helper to check if aura is enabled.
+   *
+   * @return bool
+   *   TRUE/FALSE
+   */
+  public function isOnlineReturnsEnabled() {
+    return $this->configFactory->get('alshaya_online_returns.settings')->get('online_returns_enabled');
+  }
+
+  /**
+   * Wrapper function to check if SKU is returnable.
+   *
+   * @param \Drupal\acq_commerce\SKUInterface $sku_entity
+   *   SKU entity object.
+   *
+   * @return bool
+   *   SKU is returnable or not.
+   */
+  public function isSkuReturnable(SKUInterface $sku_entity) {
+    return (bool) $sku_entity->get('attr_is_returnable')->getString();
+  }
+
+}
