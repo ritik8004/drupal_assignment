155d69d39136882e3bc8b2c8b51074a1c4e661b3
Sonam Singh <sonam.singh@innoraft.com>
2022-03-15 20:13:36 +0530

    DIG-4475: Fix linting issues in js.

--- docroot/modules/react/js/utilities/onlineReturnsHelper.js
+++ docroot/modules/react/js/utilities/onlineReturnsHelper.js
@@ -2,12 +2,12 @@
  * Helper function to check if Online Returns is enabled.
  */
 const isOnlineReturnsEnabled = () => {
-	if (typeof drupalSettings.onlineReturns !== 'undefined'
-		&& typeof drupalSettings.onlineReturns.enabled !== 'undefined') {
-		return drupalSettings.onlineReturns.enabled;
-	}
+  if (typeof drupalSettings.onlineReturns !== 'undefined'
+    && typeof drupalSettings.onlineReturns.enabled !== 'undefined') {
+    return drupalSettings.onlineReturns.enabled;
+  }
 
-	return false;
+  return false;
 };
 
 export default isOnlineReturnsEnabled;
