c7520992ef818fdf7bb83e4bdc9e9caf71bc6ef7
anjali vijay <anjali.vijay@axelerant.com>
2022-03-21 18:26:44 +0530

    DIG-4476: Product ineligible message theming on magazine layout.

--- docroot/modules/react/alshaya_online_returns/js/pdp.js
+++ docroot/modules/react/alshaya_online_returns/js/pdp.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import OnlineReturnsPDP from './components/online-returns-pdp';
 import isOnlineReturnsEnabled from '../../js/utilities/onlineReturnsHelper';
 
-if (isOnlineReturnsEnabled() && document.querySelector('#online-returns-pdp')) {
-  ReactDOM.render(
-    <OnlineReturnsPDP />,
-    document.querySelector('#online-returns-pdp'),
-  );
+if (isOnlineReturnsEnabled()) {
+  if ((window.innerWidth < 768)
+    && document.querySelector('#online-returns-pdp-mobile')) {
+    ReactDOM.render(
+      <OnlineReturnsPDP />,
+      document.querySelector('#online-returns-pdp-mobile'),
+    );
+  } else if (document.querySelector('#online-returns-pdp-desktop')) {
+    ReactDOM.render(
+      <OnlineReturnsPDP />,
+      document.querySelector('#online-returns-pdp-desktop'),
+    );
+  }
 }
