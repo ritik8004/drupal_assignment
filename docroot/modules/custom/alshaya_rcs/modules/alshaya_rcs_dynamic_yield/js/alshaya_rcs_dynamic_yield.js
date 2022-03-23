74cd650552e643713e0d6db5dd088d913629e7ed
Kunal Singh <skunal185@gmail.com>
2022-03-23 10:05:46 +0530

    CORE-39834: Added a new module for RCS Dynamic Yield.

---
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_dynamic_yield/js/alshaya_rcs_dynamic_yield.js
@@ -0,0 +1,46 @@
+/**
+ * @file
+ * Push initial data to dynamic yield.
+ */
+
+(function ($, Drupal, drupalSettings) {
+  'use strict';
+
+  window.DY = window.DY || [];
+  var initialDyData = window.DY;
+  if (rcsPhGetPageType() === null) {
+    var alterInitialDYData = new CustomEvent('alterInitialDynamicYield', { detail: { data: () => initialDyData } });
+    document.dispatchEvent(alterInitialDYData);
+    window.DY = initialDyData;
+  }
+  else {
+    // Load initial dynamic yeild data after RCS page entity is loaded.
+    RcsEventManager.addListener('alshayaPageEntityLoaded', function (e) {
+      var alterInitialDyData = new CustomEvent('alterInitialDynamicYield', {
+        detail: {
+          data: () => initialDyData,
+          page_entity: e.detail.entity,
+          type: e.detail.pageType,
+        }
+      });
+      document.dispatchEvent(alterInitialDyData);
+      window.DY = initialDyData;
+    });
+  }
+
+  Drupal.behaviors.alshayaRcsDynamicYield = {
+    attach: function () {
+      if ($('body').hasClass('rcs-loaded') && drupalSettings.dynamicYield.sourceId) {
+        // Add DY dynamic and static script in header.
+        var script = document.createElement('script');
+        script.src = '//cdn-eu.dynamicyield.com/api/' + drupalSettings.dynamicYield.sourceId + '/api_dynamic.js';
+        document.head.appendChild(script);
+        // DY script for static script.
+        script = document.createElement('script');
+        script.src = '//cdn-eu.dynamicyield.com/api/' + drupalSettings.dynamicYield.sourceId + '/api_static.js';
+        document.head.appendChild(script);
+      }
+    }
+  };
+
+})(jQuery, Drupal, drupalSettings);
