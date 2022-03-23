06369d5170bafdbda2fa3551d0c53a5c5a1e406e
santhosh <santhosh@specbee.com>
2022-03-23 19:12:34 +0530

    CORE-38046: Updated the controller call and composer hash.

--- docroot/modules/react/alshaya_geolocation/src/AlshayaStoreUtility.php
+++ docroot/modules/react/alshaya_geolocation/src/AlshayaStoreUtility.php
@@ -29,14 +29,45 @@ class AlshayaStoreUtility {
   /**
    * Store finder list labels.
    */
-  public function storeLabels() {
+  public function storeLabels($transac = TRUE) {
     $labels = [];
     $config = $this->configFactory->getEditable('alshaya_stores_finder.settings');
     $labels['search_proximity_radius'] = $config->get('search_proximity_radius');
     $labels['store_list_label'] = $config->get('store_list_label');
     $labels['search_placeholder'] = $config->get('store_search_placeholder');
     $labels['load_more_item_limit'] = $config->get('load_more_item_limit');
+    if ($transac) {
+      $labels['apiUrl'] = '/alshaya-locations/stores-list';
+      return $labels;
+    }
+    $labels['apiUrl'] = '/alshaya-locations/local';
     return $labels;
+  }
+
+  /**
+   * Store finder list labels.
+   */
+  public function storeLibraries($transac = TRUE) {
+    $libraries = [];
+    if ($transac) {
+      $libraries = [
+        'alshaya_white_label/store_finder',
+        'alshaya_geolocation/marker-dropdown',
+      ];
+      return $libraries;
+    }
+    $libraries = [
+      'whitelabel/view-store-list-locator',
+      'whitelabel/views-unformatted-store-finder-glossary',
+      'whitelabel/block-store-finder-form',
+      'whitelabel/views-store-finder-glossary',
+      'whitelabel/field-store-open-hours',
+      'whitelabel/node-individual-store',
+      'whitelabel/block-footer-store',
+      'whitelabel/block-header-store',
+      'whitelabel/view-store-finder-list',
+    ];
+    return $libraries;
   }
 
 }
--- docroot/modules/react/alshaya_geolocation/src/Controller/AlshayaStoreFinderController.php
+++ docroot/modules/react/alshaya_geolocation/src/Controller/AlshayaStoreFinderController.php
@@ -39,30 +39,17 @@ class AlshayaStoreFinderController extends ControllerBase {
    * Store Finder controller.
    */
   public function store() {
-    $labels = $this->storeUtility->storeLabels();
-    // For transac.
-    $labels['apiUrl'] = '/alshaya-locations/stores-list';
-    $library = [
-      'alshaya_geolocation/alshaya-store-finder',
-      'alshaya_white_label/store_finder',
-      'alshaya_geolocation/marker-dropdown',
-    ];
     // For non-transac.
     if (drupal_get_profile() == 'alshaya_non_transac') {
-      $labels['apiUrl'] = '/alshaya-locations/local';
-      $library = [
-        'alshaya_geolocation/alshaya-store-finder',
-        'whitelabel/view-store-list-locator',
-        'whitelabel/views-unformatted-store-finder-glossary',
-        'whitelabel/block-store-finder-form',
-        'whitelabel/views-store-finder-glossary',
-        'whitelabel/field-store-open-hours',
-        'whitelabel/node-individual-store',
-        'whitelabel/block-footer-store',
-        'whitelabel/block-header-store',
-        'whitelabel/view-store-finder-list',
-      ];
+      $labels = $this->storeUtility->storeLabels(FALSE);
+      $libraries = $this->storeUtility->storeLibraries(FALSE);
     }
+    else {
+      // For transac.
+      $labels = $this->storeUtility->storeLabels();
+      $libraries = $this->storeUtility->storeLibraries();
+    }
+    $libraries[] = 'alshaya_geolocation/alshaya-store-finder';
     return [
       '#type' => 'markup',
       '#markup' => '<div id="store-finder-wrapper"></div>',
@@ -79,30 +66,17 @@ class AlshayaStoreFinderController extends ControllerBase {
    * Store finder list controller.
    */
   public function storeList() {
-    $labels = $this->storeUtility->storeLabels();
-    // For transac.
-    $labels['apiUrl'] = '/alshaya-locations/stores-list';
-    $library = [
-      'alshaya_geolocation/alshaya-store-finder-list',
-      'alshaya_white_label/store_finder',
-      'alshaya_geolocation/marker-dropdown',
-    ];
     // For non-transac.
     if (drupal_get_profile() == 'alshaya_non_transac') {
-      $labels['apiUrl'] = '/alshaya-locations/local';
-      $library = [
-        'alshaya_geolocation/alshaya-store-finder-list',
-        'whitelabel/view-store-list-locator',
-        'whitelabel/view-store-finder-list',
-        'whitelabel/field-store-open-hours',
-        'whitelabel/block-store-finder-form',
-        'whitelabel/views-unformatted-store-finder-glossary',
-        'whitelabel/views-store-finder-glossary',
-        'whitelabel/node-individual-store',
-        'whitelabel/block-footer-store',
-        'whitelabel/block-header-store',
-      ];
+      $labels = $this->storeUtility->storeLabels(FALSE);
+      $libraries = $this->storeUtility->storeLibraries(FALSE);
     }
+    else {
+      // For transac.
+      $labels = $this->storeUtility->storeLabels();
+      $libraries = $this->storeUtility->storeLibraries();
+    }
+    $libraries[] = 'alshaya_geolocation/alshaya-store-finder-list';
     return [
       '#type' => 'markup',
       '#markup' => '<div id="store-finder-list-wrapper"></div>',
