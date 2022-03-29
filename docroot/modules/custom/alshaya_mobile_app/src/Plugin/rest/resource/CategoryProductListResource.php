48b71e5515bf6a00fc2a750784ce5a75b78f1978
Prabhat Burnwal <prabhat.burnwal@innoraft.com>
2022-03-29 16:10:13 +0530

    CORE-30936: Redirection check is done for deeplink in getDeepLink method.

--- docroot/modules/custom/alshaya_mobile_app/src/Plugin/rest/resource/CategoryProductListResource.php
+++ docroot/modules/custom/alshaya_mobile_app/src/Plugin/rest/resource/CategoryProductListResource.php
@@ -284,11 +284,10 @@ class CategoryProductListResource extends ResourceBase {
       $sub_category_entity_list = $subcategory_list_view_value->_entity;
       $sub_category_entity = $this->entityRepository->getTranslationFromContext($sub_category_entity_list);
       $deeplink = $this->mobileAppUtility->getDeepLink($sub_category_entity);
-      $redirect_term_deeplink = $this->mobileAppUtility->getRedirectedTermDeeplink($sub_category_entity->id());
       $data[] = [
         'id' => $sub_category_entity->get('tid')->getValue()[0]['value'],
         'label' => $sub_category_entity->get('name')->getValue()[0]['value'],
-        'deeplink' => !empty($redirect_term_deeplink) ? $redirect_term_deeplink : $deeplink,
+        'deeplink' => $deeplink,
       ];
     }
     return $data;
