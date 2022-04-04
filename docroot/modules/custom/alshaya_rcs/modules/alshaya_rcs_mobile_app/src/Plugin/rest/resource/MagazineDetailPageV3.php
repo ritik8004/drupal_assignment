7f7115cf5412b2ce4633508be64bd6421c433620
Saurabh Pandit <saurabhpandit@Saurabhs-MacBook-Pro.local>
2022-04-04 14:45:13 +0530

    CORE-38628: Moved shop the story to separate function and overidden in V3.

--- docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_mobile_app/src/Plugin/rest/resource/MagazineDetailPageV3.php
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_mobile_app/src/Plugin/rest/resource/MagazineDetailPageV3.php
@@ -2,21 +2,11 @@
 
 namespace Drupal\alshaya_rcs_mobile_app\Plugin\rest\resource;
 
-use Drupal\rest\Plugin\ResourceBase;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Url;
-use Drupal\alshaya_mobile_app\Service\MobileAppUtility;
-use Drupal\Core\Cache\CacheableMetadata;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Drupal\rest\ResourceResponse;
 use Drupal\node\NodeInterface;
-use Drupal\Core\Render\RendererInterface;
-use Drupal\Core\Entity\EntityRepositoryInterface;
-use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\alshaya_mobile_app\Plugin\rest\resource\MagazineDetailPage;
 
 /**
- * Provides a resource to get deeplink.
+ * Provides a resource to get magazine detail deeplink.
  *
  * @RestResource(
  *   id = "magazine_detail_page_v3",
@@ -26,240 +16,24 @@ use Drupal\Core\Datetime\DateFormatterInterface;
  *   }
  * )
  */
-class MagazineDetailPageV3 extends ResourceBase {
+class MagazineDetailPageV3 extends MagazineDetailPage {
 
   /**
-   * Node bundle machine name.
-   */
-  const NODE_TYPE = 'magazine_article';
-
-  /**
-   * The core Render service.
+   * Get shop the story product skus.
    *
-   * @var \Drupal\Core\Render\RendererInterface
-   */
-  protected $renderer;
-  /**
-   * The mobile app utility service.
+   * @param \Drupal\node\NodeInterface $node
+   *   Magazine detail node.
    *
-   * @var \Drupal\alshaya_mobile_app\Service\MobileAppUtility
+   * @return array
+   *   Returns product skus for shop the story.
    */
-  protected $mobileAppUtility;
+  protected function getShopTheStory(NodeInterface $node) {
+    $skus = $node->get('field_magazine_shop_the_story')->getValue();
+    $shop_the_story['skus'] = array_map(function ($value) {
+      return $value['value'];
+    }, $skus);
 
-  /**
-   * The request stack service.
-   *
-   * @var \Symfony\Component\HttpFoundation\RequestStack
-   */
-  protected $requestStack;
-
-  /**
-   * The entity repository.
-   *
-   * @var \Drupal\Core\Entity\EntityRepositoryInterface
-   */
-  protected $entityRepository;
-
-  /**
-   * Date formatter service.
-   *
-   * @var \Drupal\Core\Datetime\DateFormatterInterface
-   */
-  protected $dateFormatter;
-
-  /**
-   * AdvancedPageResource constructor.
-   *
-   * @param array $configuration
-   *   Configuration array.
-   * @param string $plugin_id
-   *   Plugin id.
-   * @param mixed $plugin_definition
-   *   Plugin definition.
-   * @param array $serializer_formats
-   *   Serializer formats.
-   * @param \Psr\Log\LoggerInterface $logger
-   *   Logger channel.
-   * @param \Drupal\alshaya_mobile_app\Service\MobileAppUtility $mobile_app_utility
-   *   The mobile app utility service.
-   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
-   *   The request stack service.
-   * @param \Drupal\Core\Render\RendererInterface $renderer
-   *   Renderer interface.
-   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
-   *   The entity repository.
-   * @param \Drupal\Core\Datetime\DateFormatterInterface $date_formatter
-   *   Current time service.
-   */
-
-  /**
-   * {@inheritdoc}
-   */
-  public function __construct(
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
-    array $serializer_formats,
-    LoggerInterface $logger,
-    MobileAppUtility $mobile_app_utility,
-    RequestStack $request_stack,
-    RendererInterface $renderer,
-    EntityRepositoryInterface $entity_repository,
-    DateFormatterInterface $date_formatter
-  ) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
-    $this->mobileAppUtility = $mobile_app_utility;
-    $this->requestStack = $request_stack->getCurrentRequest();
-    $this->renderer = $renderer;
-    $this->entityRepository = $entity_repository;
-    $this->dateFormatter = $date_formatter;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->getParameter('serializer.formats'),
-      $container->get('logger.factory')->get('alshaya_mobile_app'),
-      $container->get('alshaya_mobile_app.utility'),
-      $container->get('request_stack'),
-      $container->get('renderer'),
-      $container->get('entity.repository'),
-      $container->get('date.formatter')
-    );
-  }
-
-  /**
-   * Responds to GET requests.
-   *
-   * @return \Drupal\rest\ResourceResponse
-   *   The response returns the deeplink.
-   */
-  public function get() {
-    // Path alias of magazine article.
-    $alias = $this->requestStack->query->get('url');
-    $node = $this->mobileAppUtility->getNodeFromAlias($alias, self::NODE_TYPE);
-    $res = NULL;
-
-    if (!$node instanceof NodeInterface) {
-      $this->mobileAppUtility->throwException();
-    }
-
-    if (!$node->isPublished()) {
-      $this->mobileAppUtility->throwException();
-    }
-    $node = $this->entityRepository->getTranslationFromContext($node);
-    // Get bubbleable metadata for CacheableDependency to avoid fatal error.
-    $node_url_obj = Url::fromRoute('entity.node.canonical', ['node' => $node->id()]);
-    $node_url = $node_url_obj->toString(TRUE);
-
-    $response_data = [
-      'id' => (int) $node->id(),
-      'name' => $node->label(),
-      'path' => $node_url->getGeneratedUrl(),
-    ];
-    if ($node->get('field_magazine_hero_image')->getValue()) {
-      $response_data['image'] = $this->mobileAppUtility->getImages($node, 'field_magazine_hero_image', 'magazine_article_hero');
-    }
-    if ($node->hasField('field_magazine_category') && $node->field_magazine_category) {
-      $magazine_category = $node->get('field_magazine_category')->referencedEntities();
-      foreach ($magazine_category as $magazine_category_value) {
-        $magazine_category_entity = $this->entityRepository->getTranslationFromContext($magazine_category_value);
-        $magazine_category_data['id'] = (int) $magazine_category_entity->id();
-        $magazine_category_data['name'] = $magazine_category_entity->getName();
-        $response_data['magazine_category'] = $magazine_category_data;
-      }
-    }
-    if ($node->get('field_magazine_slogan')->getValue()) {
-      $response_data['slogan'] = $node->field_magazine_slogan->getValue()[0]['value'];
-    }
-    if (!empty($node->field_magazine_date->getValue())) {
-      $magazine_date = $node->field_magazine_date->getValue()[0]['value'];
-      $response_data['date'] = $this->dateFormatter->format(strtotime($magazine_date), 'magazine_date');
-    }
-    if ($node->hasField('field_magazine_paragraphs') && !empty($node->field_magazine_paragraphs)) {
-      $magazine_paragraphs = $node->field_magazine_paragraphs->referencedEntities();
-      foreach ($magazine_paragraphs as $magazine_paragraphs_value) {
-        $item = $this->entityRepository->getTranslationFromContext($magazine_paragraphs_value);
-        if ($item->bundle() == 'title_textarea') {
-          $res['type'] = $item->bundle();
-          if ($item->get('field_body')->getValue()) {
-            $res['body'] = $item->get('field_body')->first()->getValue()['value'];
-          }
-          if (!empty($item->get('field_html_heading')->getValue())) {
-            $res['heading'] = $item->get('field_html_heading')->first()->getValue()['value'];
-          }
-        }
-        elseif ($item->bundle() == 'image_title_subtitle') {
-          $res['type'] = $item->bundle();
-          if ($item->get('field_banner')->getValue()) {
-            $res['image'] = $this->mobileAppUtility->getImages($item, 'field_banner');
-          }
-          if (!empty($item->get('field_link')->first())) {
-            $url = $item->get('field_link')->first()->getUrl();
-            $url_string = $url->toString(TRUE);
-            $res['link'] = $url_string->getGeneratedUrl();
-          }
-          if (!empty($item->get('field_sub_title')->getValue())) {
-            $res['sub_title'] = $item->get('field_sub_title')->getValue()[0]['value'];
-          }
-          if (!empty($item->get('field_title')->getValue())) {
-            $res['title'] = $item->get('field_title')->getValue()[0]['value'];
-          }
-        }
-        elseif ($item->bundle() == 'video' && $item->hasField('field_video') && !empty($item->field_video)) {
-          if (!empty($video = $item->field_video->referencedEntities())) {
-            $res['type'] = $item->bundle();
-            foreach ($video as $items) {
-              $res1['media_id'] = (int) $items->id();
-              if (!empty($items->field_media_oembed_video->getValue())) {
-                $res1['embed_video_url'] = $items->field_media_oembed_video->getValue()[0]['value'];
-              }
-              if (!empty($items->field_media_document->entity)) {
-                $res1['video_url'] = file_create_url($items->field_media_document->entity->getFileUri());
-              }
-              if (!empty($items->field_media_in_library->getValue())) {
-                $res1['media_library_flag'] = (int) $items->field_media_in_library->getValue()[0]['value'];
-              }
-              $res['media'] = $res1;
-              unset($res1);
-            }
-          }
-        }
-        if (!empty($res)) {
-          $response_data['magazine_paragraphs'][] = $res;
-          unset($res);
-        }
-      }
-    }
-    // Return product skus in shop the story.
-    if ($node->hasField('field_magazine_shop_the_story') && !empty($magazine_shop = $node->get('field_magazine_shop_the_story')->getValue())) {
-      $response_data['shop_the_story']['skus'] = array_map(function ($value) {
-        return $value['value'];
-      }, $magazine_shop);
-    }
-    $response = new ResourceResponse($response_data);
-    $response->addCacheableDependency($node);
-    foreach ($this->mobileAppUtility->getCacheableEntities() as $cacheable_entity) {
-      $response->addCacheableDependency($cacheable_entity);
-    }
-
-    $response->addCacheableDependency(CacheableMetadata::createFromRenderArray([
-      '#cache' => [
-        'contexts' => [
-          'url.query_args:url',
-        ],
-        'tags' => array_merge([
-          'node_view',
-          'paragraph_view',
-        ], $this->mobileAppUtility->getBlockCacheTags()),
-      ],
-    ]));
-    return $response;
+    return $shop_the_story;
   }
 
 }
