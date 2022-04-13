c54539b52bca63ee029227e7f34365082ece0797
nikunjkotecha <nikunj.kotecha@acquia.com>
2022-04-13 10:06:14 +0530

    CORE-32643: Update keys for captcha. Update readme. Add keys for FB login.

--- factory-hooks/post-settings-php/google_auth.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if (getenv('LANDO')) {
-  $config['social_auth_google.settings']['client_id'] = '847720644464-1bqotkjgsovehdtna2d63m706slkjaba.apps.googleusercontent.com';
-  $config['social_auth_google.settings']['client_secret'] = 'ERdHm6oEVXWdf_Xwp1-de_rf';
-}
