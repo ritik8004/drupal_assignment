b101ecd80271f36b4d3746d1ad9c08703bb23e70
anjali vijay <anjali.vijay@axelerant.com>
2022-04-14 18:20:07 +0530

    DIG-4489: Return request step-2 theming.

--- docroot/modules/react/alshaya_online_returns/js/return-request/components/refund-amount-wrapper/index.js
+++ docroot/modules/react/alshaya_online_returns/js/return-request/components/refund-amount-wrapper/index.js
@@ -7,7 +7,7 @@ const ReturnAmountWrapper = () => (
         { Drupal.t('Refund Amount', {}, { context: 'online_returns' }) }
       </div>
       <div className="refund-method-listing">
-        { Drupal.t('You will be notified of your refund details once we receive the items in the warehouse.', {}, { context: 'online_returns' }) }
+        { Drupal.t('Your refund amount will be notified to you through mail once we receive the items in warehouse', {}, { context: 'online_returns' }) }
       </div>
     </div>
   </>
