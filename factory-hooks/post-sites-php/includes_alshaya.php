23a4811606d9b51bd66a6d3eabf4f5e30c0aeb4e
nikunjkotecha <nikunj.kotecha@acquia.com>
2022-04-21 22:48:42 +0530

    CORE-40979: Executed blt acsf:init.

---
+++ factory-hooks/post-sites-php/includes_alshaya.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * ACSF post-sites-php hook.
+ *
+ * @see https://docs.acquia.com/site-factory/tiers/paas/workflow/hooks
+ */
+
+// Get the site's name from the first domain.
+global $_acsf_site_name;
+
+// Support cases like hmkw1 or mckw12.
+$_acsf_site_name = preg_replace('/\d/', '', $_acsf_site_name);
