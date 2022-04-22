5425c6613bb02e995a671992f6d469aeecc2de29
Anurag Sharma <anuragsharmam@3197917.no-reply.drupal.org>
2022-04-22 14:03:45 +0530

    CORE-39981: Updated the aura checkout strings.

--- docroot/modules/react/alshaya_spc/js/aura-loyalty/components/aura-checkout-rewards/components/aura-card/index.js
+++ docroot/modules/react/alshaya_spc/js/aura-loyalty/components/aura-checkout-rewards/components/aura-card/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import parse from 'html-react-parser';
+import { renderToString } from 'react-dom/server';
 import AuraHeaderIcon from '../../../../../../../alshaya_aura_react/js/svg-component/aura-header-icon';
 import getStringMessage from '../../../../../../../js/utilities/strings';
-import PointsString from '../../../utilities/points-string';
 import AuraFormRedeemPoints from '../../../aura-forms/aura-redeem-points';
-import PointsExpiryMessage from '../../../utilities/points-expiry-message';
 import { getAllAuraStatus } from '../../../../../../../alshaya_aura_react/js/utilities/helper';
 import ConditionalView from '../../../../../../../js/utilities/components/conditional-view';
 import { getTooltipPointsOnHoldMsg } from '../../../../../../../alshaya_aura_react/js/utilities/aura_utils';
 import ToolTip from '../../../../../utilities/tooltip';
+import { formatDate } from '../../../../../../../alshaya_aura_react/js/utilities/reward_activity_helper';
 
 export default class AuraLinkedCheckout extends React.Component {
   constructor(props) {
@@ -47,24 +48,32 @@ export default class AuraLinkedCheckout extends React.Component {
         <div className="redeem-aura-points">
           <div className={`redeem-aura-points-header-container ${activeClass}`} onClick={() => this.changeRedeemAuraAccordionStatus()}>
             <span>
-              { getStringMessage('redeem') }
+              {parse(parse(getStringMessage('redeem_aura_points_header', {
+                '@aura_icon': `<span class="join-aura">${renderToString(<AuraHeaderIcon />)}</span>`,
+              })))}
             </span>
-            <span className="join-aura"><AuraHeaderIcon /></span>
-            <span>{ getStringMessage('points') }</span>
             <span className="accordion-icon" />
           </div>
           <div className={`redeem-aura-points-content ${activeClass}`}>
             <div className="block-content">
               <div className="current-available-points">
-                <span className="spc-aura-redeem-text">{ getStringMessage('checkout_you_have') }</span>
-                <span className="spc-aura-highlight">
-                  <PointsString points={pointsInAccount} />
-                  ,
+                <span>
+                  {parse(parse(getStringMessage('checkout_you_have_pts', {
+                    '@pts': `<span class="spc-aura-highlight">${pointsInAccount}</span>`,
+                  })))}
                 </span>
               </div>
               <div className="points-expiring">
-                <PointsExpiryMessage points={expiringPoints} date={expiryDate} />
                 <ConditionalView condition={expiringPoints !== 0}>
+                  <div className="spc-aura-points-expiry-item">
+                    {parse(parse(getStringMessage(
+                      'checkout_point_expiry_with_date',
+                      {
+                        '@pts': pointsInAccount,
+                        '@date': `<b>${formatDate(expiryDate, 'DD-Mon-YYYY')}</b>`,
+                      },
+                    )))}
+                  </div>
                   {/* TO DO- Below tooltip should be replaced once we have tooltip content. */}
                   <ToolTip enable question>{ getTooltipPointsOnHoldMsg() }</ToolTip>
                 </ConditionalView>
@@ -83,9 +92,12 @@ export default class AuraLinkedCheckout extends React.Component {
           </div>
         </div>
         <div className="redeem-aura-footer">
-          <span className="before-text">{ getStringMessage('checkout_you_will_earn') }</span>
-          <span className="points-earned-with-purchase">{pointsToEarn}</span>
-          <span className="after-text">{ getStringMessage('checkout_aura_points_with_purchase') }</span>
+          {parse(parse(getStringMessage(
+            'aura_checkout_reward_points_to_earn',
+            {
+              '@pts': pointsToEarn,
+            },
+          )))}
         </div>
       </div>
     );
