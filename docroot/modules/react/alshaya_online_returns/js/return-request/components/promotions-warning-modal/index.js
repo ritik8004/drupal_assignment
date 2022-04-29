41e2974a7a0ba75a18d722e7bb8d98a2c3bd5fc4
Sonam Singh <sonam.singh@innoraft.com>
2022-04-29 15:49:47 +0530

    DIG-4498: Remove not required text and restructure component.

--- docroot/modules/react/alshaya_online_returns/js/return-request/components/promotions-warning-modal/index.js
+++ docroot/modules/react/alshaya_online_returns/js/return-request/components/promotions-warning-modal/index.js
@@ -6,29 +6,23 @@ const PromotionsWarningModal = ({
   handlePromotionContinue,
 }) => (
   <div className="promotions-warning-modal-wrapper">
-    <button type="button" className="close" onClick={() => closePromotionsWarningModal()}> close </button>
+    <button type="button" className="close" onClick={() => closePromotionsWarningModal()} />
     <div className="title">
       {Drupal.t('Selected Item is Promotional Item', {}, { context: 'online_returns' })}
     </div>
     <div className="description">
       <span>
-        {Drupal.t('To receive refund for a promotional items, all items related to the promotion has to be returned.', {}, { context: 'online_returns' })}
+        {Drupal.t('To receive refund for promotional items, all items related to the promotion has to be returned.', {}, { context: 'online_returns' })}
       </span>
       <span>
         {Drupal.t('Clicking continue will select all items in this promotion.', {}, { context: 'online_returns' })}
       </span>
     </div>
     <div className="cta-wrapper">
-      <button
-        type="button"
-        onClick={handlePromotionContinue}
-      >
+      <button type="button" onClick={handlePromotionContinue} >
         <span className="continue-button-label">{Drupal.t('Continue', {}, { context: 'online_returns' })}</span>
       </button>
-      <button
-        type="button"
-        onClick={handlePromotionDeselect}
-      >
+      <button type="button" onClick={handlePromotionDeselect} >
         <span className="deselect-button-label">{Drupal.t('Deselect this item', {}, { context: 'online_returns' })}</span>
       </button>
     </div>
