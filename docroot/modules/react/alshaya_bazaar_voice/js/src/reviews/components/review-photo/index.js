cc1fd9f9f2e34a6d68968c63d7c48c4ae0382bb8
Mohammad Rashid <mohammad.rashid@alshaya.com>
2022-05-05 10:48:18 +0000

    CORE-41012: Add condition in photo component and remove extra condition from desc component.

--- docroot/modules/react/alshaya_bazaar_voice/js/src/reviews/components/review-photo/index.js
+++ docroot/modules/react/alshaya_bazaar_voice/js/src/reviews/components/review-photo/index.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
+import { hasValue } from '../../../../../../js/utilities/conditionsUtility';
+import ConditionalView from '../../../common/components/conditional-view';
 
 const ReviewPhotos = ({
   photoCollection,
 }) => (
   <div className="thumbnail-img-block">
     {Object.keys(photoCollection).map((item) => (
-      <Popup
-        key={item}
-        trigger={(
-          <button type="button" className="thumbnail-img">
-            <img src={photoCollection[item].Sizes.large.Url} />
-          </button>
-        )}
-        position="bottom center"
-        closeOnDocumentClick={false}
-      >
-        {(close) => (
-          <div className="modal">
-            <button type="button" className="close" onClick={close} />
-            <div className="large-image">
+      <ConditionalView key={item} condition={hasValue(photoCollection[item].Sizes)}>
+        <Popup
+          key={item}
+          trigger={(
+            <button type="button" className="thumbnail-img">
               <img src={photoCollection[item].Sizes.large.Url} />
+            </button>
+          )}
+          position="bottom center"
+          closeOnDocumentClick={false}
+        >
+          {(close) => (
+            <div className="modal">
+              <button type="button" className="close" onClick={close} />
+              <div className="large-image">
+                <img src={photoCollection[item].Sizes.large.Url} />
+              </div>
             </div>
-          </div>
-        )}
-      </Popup>
+          )}
+        </Popup>
+      </ConditionalView>
     ))}
   </div>
 );
