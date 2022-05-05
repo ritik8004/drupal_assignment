7b20e9539ff3a8c1c2abc08885d659c2dc50edcd
Saurabh Pandit <saurabhpandit@Saurabhs-MacBook-Pro.local>
2022-05-05 13:35:45 +0530

    CORE-41104: Get alias from category slug in category migration rollback.

--- docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_main_menu/src/Service/AlshayaRcsCategoryDataMigration.php
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_main_menu/src/Service/AlshayaRcsCategoryDataMigration.php
@@ -236,12 +236,9 @@ class AlshayaRcsCategoryDataMigration {
           'path' => '/taxonomy/term/' . $tid,
         ]);
         $path_alias_storage->delete($aliases);
-        // Save term so that path alias is generated and can be used below.
-        $migrate_term->save();
         // Set commerce id if we are migrating product category.
         if ($vid == self::SOURCE_VOCABULARY_ID) {
-          $alias_entity = current($path_alias_storage->loadByProperties(['path' => '/taxonomy/term/' . $migrate_term->id()]));
-          $alias = ltrim($alias_entity->getAlias(), '/');
+          $alias = $source_term->get('field_category_slug')->getString();
           $commerce_id = $context['results']['commerce_ids'][$alias];
           $migrate_term->set('field_commerce_id', $commerce_id);
         }
@@ -326,13 +323,10 @@ class AlshayaRcsCategoryDataMigration {
       'path' => '/taxonomy/term/' . $tid,
     ]);
     $path_alias_storage->delete($aliases);
-    // Save term so that path alias is generated and can be used below.
-    $migrate_parent_term->save();
 
     // Set Commerce id for Product Category.
     if ($vid == self::SOURCE_VOCABULARY_ID) {
-      $alias_entity = current($path_alias_storage->loadByProperties(['path' => '/taxonomy/term/' . $migrate_parent_term->id()]));
-      $alias = ltrim($alias_entity->getAlias(), '/');
+      $alias = $source_parent_term->get('field_category_slug')->getString();
       $commerce_id = $results['commerce_ids'][$alias];
       $migrate_parent_term->set('field_commerce_id', $commerce_id);
     }
