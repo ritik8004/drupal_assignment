47eee95d938429638d7fef6dcc03449923918ea7
Saurabh Pandit <saurabhpandit@Saurabhs-MacBook-Pro.local>
2022-05-05 17:45:56 +0530

    CORE-40994: Changed rcs seo route subscriber class name.

--- docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_seo/src/EventSubscriber/AlshayaRcsSeoRouteSubscriber.php
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_seo/src/EventSubscriber/AlshayaRcsSeoRouteSubscriber.php
@@ -6,14 +6,15 @@ use Drupal\Core\Routing\RouteSubscriberBase;
 use Symfony\Component\Routing\RouteCollection;
 
 /**
- * Perform redirection for image sitemap to MDC urls.
+ * RCS SEO route subscriber class.
  */
-class AlshayaSitemapRouteSubscriber extends RouteSubscriberBase {
+class AlshayaRcsSeoRouteSubscriber extends RouteSubscriberBase {
 
   /**
    * {@inheritdoc}
    */
   public function alterRoutes(RouteCollection $collection) {
+    // Perform redirection for image sitemap to MDC urls.
     if ($route = $collection->get('alshaya_image_sitemap.alshaya_image_sitemap_get_url')) {
       $route->setDefault('_controller', '\Drupal\alshaya_rcs_seo\Controller\AlshayaRcsImageSitemapController::redirectImageSiteMap');
     }
