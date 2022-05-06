695800a5c39da0cb87ce9057f6d898af97bb8f0f
rupeshgharat <rupesh.gharat@qed42.com>
2022-05-06 18:32:52 +0530

    CORE-41136: email verfied aura popup.

--- docroot/modules/react/alshaya_spc/js/aura-loyalty/components/aura-forms/aura-unlinked-card/index.js
+++ docroot/modules/react/alshaya_spc/js/aura-loyalty/components/aura-forms/aura-unlinked-card/index.js
@@ -30,44 +30,51 @@ class AuraFormUnlinkedCard extends React.Component {
           <SectionTitle>
             {getStringMessage('link_card_header_logged_in')}
           </SectionTitle>
-          <button type="button" className="close" onClick={() => closeLinkOldCardModal()} />
+          <button
+            type="button"
+            className="close"
+            onClick={() => closeLinkOldCardModal()}
+          />
         </div>
-        <div className="description">
-          {getStringMessage('aura_link_unlinked_card_body_title', {
-            '!firstName': firstName,
-          })}
-          <b>{getStringMessage('aura_link_unlinked_card_body_sub_title')}</b>
-        </div>
-        <div className="spc-aura-unlink-card-form-content">
-          <div className="form-items">
-            <label>
-              {getStringMessage('aura_link_unlinked_card_field_title')}
-            </label>
-            <Cleave
-              name="spc-aura-link-card-input"
-              className="spc-aura-link-card-input"
-              options={{ creditCard: true }}
-              value={cardNumber}
-              disabled
-            />
-            <button
-              type="submit"
-              className="spc-aura-link-card-submit spc-aura-button"
-              onClick={() => this.linkYourCard(cardNumber)}
-            >
-              {Drupal.t('Submit')}
-            </button>
+        <div className="aura-modal-body">
+          <div className="description">
+            {getStringMessage('aura_link_unlinked_card_body_title', {
+              "!firstName": firstName,
+            })}
+            <b>{getStringMessage('aura_link_unlinked_card_body_sub_title')}</b>
           </div>
-          <div className="no-link-message">
-            <a onClick={() => openLinkCardModal()}>
-              {Drupal.t('Not you?')}
-            </a>
+          <div className="spc-aura-unlink-card-form-content">
+            <div className="form-items">
+              <label>
+                {getStringMessage('aura_link_unlinked_card_field_title')}
+              </label>
+              <Cleave
+                name="spc-aura-link-card-input"
+                className="spc-aura-link-card-input"
+                options={{ creditCard: true }}
+                value={cardNumber}
+                disabled
+              />
+            </div>
+            <div className="no-link-message">
+              <a href="#" onClick={() => openLinkCardModal()}>
+                {Drupal.t('Not you?')}
+              </a>
+            </div>
           </div>
         </div>
-        <div className="spc-aura-link-api-response-message" />
-        <div className="aura-modal-footer">
-          <div className="join-aura" onClick={() => openOTPModal()}>
-            {getStringMessage('aura_join_aura')}
+
+        <div className="aura-modal-form-actions">
+          <div
+            className="spc-aura-link-card-submit spc-aura-button"
+            onClick={() => this.linkYourCard(cardNumber)}
+          >
+            {Drupal.t('Submit')}
+          </div>
+          <div className="aura-modal-footer">
+            <div className="join-aura" onClick={() => openOTPModal()}>
+              {getStringMessage('aura_join_aura')}
+            </div>
           </div>
         </div>
       </div>
