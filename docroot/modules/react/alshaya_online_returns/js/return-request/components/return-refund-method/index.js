c4cb1bd385d14ef39d37b8244b601acdfada17bb
Marcelo Vani <marcelo.vani@acquia.com>
2022-05-10 14:49:53 +0100

    DIG-5565: Created a reusable component for card details.

--- docroot/modules/react/alshaya_online_returns/js/return-request/components/return-refund-method/index.js
+++ docroot/modules/react/alshaya_online_returns/js/return-request/components/return-refund-method/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import CardTypeSVG from '../../../../../alshaya_spc/js/svg-component/card-type-svg';
-import ConditionalView from '../../../../../js/utilities/components/conditional-view';
 import { hasValue } from '../../../../../js/utilities/conditionsUtility';
+import CardDetails from '../../../return-confirmation/components/card-details';
 
 const ReturnRefundMethod = ({
   paymentDetails,
@@ -19,30 +18,7 @@ const ReturnRefundMethod = ({
           <div className="method-listing-label">
             { Drupal.t('Your refund will be credited back to the following payment methods.', {}, { context: 'online_returns' }) }
           </div>
-          <div className="method-list-wrapper">
-            {Object.keys(paymentDetails).map((method) => (
-              <div key={method} className="method-wrapper">
-                <div className="card-icon">
-                  <CardTypeSVG type={paymentDetails[method].payment_type.toLowerCase()} class={`${paymentDetails[method].payment_type.toLowerCase()} is-active`} />
-                </div>
-                <div className="card-detail">
-                  <ConditionalView condition={hasValue(paymentDetails[method].card_type)}>
-                    <span className="payment-type bold-text">{ paymentDetails[method].card_type }</span>
-                  </ConditionalView>
-                  <ConditionalView condition={hasValue(paymentDetails[method].card_number)}>
-                    <span>
-                      {' '}
-                      { Drupal.t('Card ending in', {}, { context: 'online_returns' }) }
-                      {' '}
-                    </span>
-                    <span className="payment-info bold-text">
-                      { Drupal.t('@card_number', { '@card_number': paymentDetails[method].card_number }, {}, { context: 'online_returns' }) }
-                    </span>
-                  </ConditionalView>
-                </div>
-              </div>
-            ))}
-          </div>
+          <CardDetails paymentDetails={paymentDetails} />
           <div className="refund-message">
             { Drupal.t('Estimated refund in 3-5 business days after we receive the item', {}, { context: 'online_returns' }) }
           </div>
