b9654381857e3b069a2145cffd1b9273ad39312c
Mohammad Rashid <mohammad.rashid@alshaya.com>
2022-05-17 05:28:43 +0000

    CORE-41511: remove extra library.

--- docroot/modules/react/alshaya_hello_member/src/Plugin/Block/MyAccountsHelloMemberBlock.php
+++ docroot/modules/react/alshaya_hello_member/src/Plugin/Block/MyAccountsHelloMemberBlock.php
@@ -94,11 +94,6 @@ class MyAccountsHelloMemberBlock extends BlockBase implements ContainerFactoryPl
     return [
       '#theme' => 'my_accounts_hello_member_block',
       '#strings' => _alshaya_hello_member_static_strings(),
-      '#attached' => [
-        'library' => [
-          'alshaya_white_label/hello-member-myaccount',
-        ],
-      ],
     ];
   }
 
