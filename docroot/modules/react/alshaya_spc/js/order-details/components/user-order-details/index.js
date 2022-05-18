8c5cb2c990a94dbf05d5fbd7dfece905bd54d343
Marcelo Vani <marcelo.vani@acquia.com>
2022-05-18 14:53:19 +0100

    DIG-6807: Use the parse function to render html.

--- docroot/modules/react/alshaya_spc/js/order-details/components/user-order-details/index.js
+++ docroot/modules/react/alshaya_spc/js/order-details/components/user-order-details/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import parse from 'html-react-parser';
+import Aura from '../aura';
+import TotalItemCount from '../total-item-count';
+import CancelledItems from '../cancelled-items';
+import OnlineReturns from '../online-returns';
+import OnlineBooking from '../online-booking';
+import DeliveryDetailNotice from '../delivery-detail-notice';
+import DeliveryDetails from '../delivery-details';
+import OrderItems from '../order-items';
+import { hasValue } from '../../../../../js/utilities/conditionsUtility';
 import ConditionalView from '../../../../../js/utilities/components/conditional-view';
 import Aura from '../aura';
 import TotalItemCount from '../total-item-count';
