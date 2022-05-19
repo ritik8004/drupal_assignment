af7fcb6a72ac3db1ba3a241685853ec43222327c
Marcelo Vani <marcelo.vani@acquia.com>
2022-05-19 11:00:34 +0100

    DIG-6807: Refactoring online return components.

---
+++ docroot/modules/react/alshaya_spc/js/order-details/components/order-cancelled-items/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { hasValue } from '../../../../../js/utilities/conditionsUtility';
+import OrderItems from '../order-items';
+
+const OrderCancelledItems = (props) => {
+  const { order } = props;
+  if (hasValue(order.cancelled_items_count)) {
+    return null;
+  }
+
+  return (
+    <>
+      <div id="cancelled-items" className="order-item-row cancelled-items">
+        <div>
+          <div>{Drupal.t('Cancelled Items')}</div>
+        </div>
+      </div>
+      <OrderItems products={order.cancelled_products} cancelled />
+    </>
+  );
+};
+
+export default OrderCancelledItems;
