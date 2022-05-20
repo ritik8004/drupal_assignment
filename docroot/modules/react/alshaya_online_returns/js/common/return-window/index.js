1c1e94745c6618ed88376cd29ec88043d5f6cfff
Marcelo Vani <marcelo.vani@acquia.com>
2022-05-20 16:41:35 +0100

    DIG-6807: Create a class before html.

--- docroot/modules/react/alshaya_online_returns/js/common/return-window/index.js
+++ docroot/modules/react/alshaya_online_returns/js/common/return-window/index.js
@@ -3,9 +3,10 @@ import { hasValue } from '../../../../js/utilities/conditionsUtility';
 
 const ReturnWindow = (props) => {
   const { message, closed } = props;
+  const className = hasValue(closed) ? 'return-window-closed' : '';
 
   return (
-    <span className={`${hasValue(closed) ? 'return-window-closed' : ''}`}>
+    <span className={className}>
       {message}
     </span>
   );
