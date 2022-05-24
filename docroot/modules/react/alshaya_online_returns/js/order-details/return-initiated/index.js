c99b4915b268d35f57015a5341efb25594c5b25a
Sanket Jain <sanket.jain@pimocre.services>
2022-05-24 12:58:21 +0530

    DIG-6638: Removed looping for return initiated items.

--- docroot/modules/react/alshaya_online_returns/js/order-details/return-initiated/index.js
+++ docroot/modules/react/alshaya_online_returns/js/order-details/return-initiated/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ConditionalView from '../../../../js/utilities/components/conditional-view';
 import { hasValue } from '../../../../js/utilities/conditionsUtility';
 import { isReturnClosed } from '../../utilities/return_api_helper';
 import ProcessedItem from './processed-item';
@@ -10,19 +9,17 @@ const ReturnInitiated = ({
   if (!hasValue(returns)) {
     return null;
   }
+
+  const initiatedReturnItem = returns.find((returnItem) => !isReturnClosed(returnItem.returnInfo));
+
+  if (!hasValue(initiatedReturnItem)) {
+    return null;
+  }
   return (
-    <div className="return-processed-items">
-      <ConditionalView condition={hasValue(returns)}>
-        {returns.map((returnItem) => (
-          <div className="return-items" key={returnItem.returnInfo.increment_id}>
-            <ConditionalView condition={!isReturnClosed(returnItem.returnInfo)}>
-              <ProcessedItem
-                returnData={returnItem}
-              />
-            </ConditionalView>
-          </div>
-        ))}
-      </ConditionalView>
+    <div className="return-initiated-item">
+      <ProcessedItem
+        returnData={initiatedReturnItem}
+      />
     </div>
   );
 };
