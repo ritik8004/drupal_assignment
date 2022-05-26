d0ac86d84e6f3973d6b7e9e086eb15ad351e95e0
Sanket Jain <sanket.jain@pimocre.services>
2022-05-26 14:17:28 +0530

    DIG-8004: Reusable function to convert text to CSS class.

--- docroot/modules/react/alshaya_online_returns/js/order-details/return-initiated/processed-item/index.js
+++ docroot/modules/react/alshaya_online_returns/js/order-details/return-initiated/processed-item/index.js
@@ -57,7 +57,7 @@ class ProcessedItem extends React.Component {
     // @todo: Use utility function to trim status text for css class.
     // Refer DIG-8004 for more info.
     const returnStatus = returnData.returnInfo.extension_attributes.customer_status;
-    const returnStatusClass = returnStatus.replace(/\s+/g, '-').toLowerCase();
+    const returnStatusClass = Drupal.cleanCssIdentifier(returnStatus);
     return (
       <div key={returnData.returnInfo.increment_id} className="return-status-header">
         <div className="return-status-wrapper">
