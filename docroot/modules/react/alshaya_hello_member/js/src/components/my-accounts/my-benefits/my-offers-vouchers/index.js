50a78ed85ee90cc301b17cb39364cd6ab5d21686
Sanjib.dutta <sanjib.dutta@alshaya.com>
2022-05-26 19:37:44 +0300

    CORE-41507: [My account] My benefits & My points.

--- docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/my-benefits/my-offers-vouchers/index.js
+++ docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/my-benefits/my-offers-vouchers/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 
 const MyOffersAndVouchers = () => (
-  <div className="my-offers-vouchers-details">
-    <div className="image">
-      {/* display offers or vouchers image */}
+  <>
+    <div className="my-offers-vouchers-details">
+      <div className="image-container">
+        {/* display offers or vouchers image */}
+      </div>
+      <div className="voucher-wrapper">
+        <div className="title">
+          {/* display offers or vouchers title */}
+        </div>
+        <div className="info">
+          {/* display offers or vouchers info */}
+        </div>
+        <div className="expiry">
+          {/* display offers or vouchers expiry */}
+        </div>
+      </div>
     </div>
-    <div className="title">
-      {/* display offers or vouchers title */}
+    <div className="btn-wrapper">
+      <a>SHOW ALL</a>
     </div>
-    <div className="info">
-      {/* display offers or vouchers info */}
-    </div>
-    <div className="expiry">
-      {/* display offers or vouchers expiry */}
-    </div>
-  </div>
+  </>
 );
 
 export default MyOffersAndVouchers;
