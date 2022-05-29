1fa6c1985df71bc0e51498c00ead3c15db8823ab
Mohammad Rashid <mohammad.rashid@alshaya.com>
2022-05-29 07:46:19 +0000

    CORE-41503: Update components data with dynamic values.

--- docroot/modules/react/alshaya_hello_member/js/src/utilities.js
+++ docroot/modules/react/alshaya_hello_member/js/src/utilities.js
@@ -1,18 +1,14 @@
 /**
  * Get the memberId with required format.
  */
-const getFormatedMemberId = (memberId) => {
-  return memberId.replace(/(\d{4})(\d{4})(\d{4})(\d{4})/, '$1 $2 $3 $4');
-};
+const getFormatedMemberId = (memberId) => memberId.replace(/(\d{4})(\d{4})(\d{4})(\d{4})/, '$1 $2 $3 $4');
 
 /**
  * Get the full name.
  */
-const getFullName = (firsName, lastName) => {
-  return Drupal.t('Hi') + " " + firsName + " " + lastName;
-};
+const getFullName = (firsName, lastName) => `${Drupal.t('Hi')} ${firsName} ${lastName}`;
 
 export {
   getFormatedMemberId,
-  getFullName
+  getFullName,
 };
--- docroot/modules/react/js/utilities/helloMemberHelper.js
+++ docroot/modules/react/js/utilities/helloMemberHelper.js
@@ -25,7 +25,7 @@ export default function isHelloMemberEnabled() {
  * @returns {string}
  *   The api endpoint.
  */
- export const getApiEndpoint = (action) => {
+export const getApiEndpoint = (action) => {
   let endpoint = '';
   switch (action) {
     case 'helloMemberGetCustomerData':
@@ -56,7 +56,7 @@ export default function isHelloMemberEnabled() {
  * @returns {object}
  *   Returns the promise object.
  */
- export const callHelloMemberApi = (action, method, postData, bearerToken = true) => {
+export const callHelloMemberApi = (action, method, postData, bearerToken = true) => {
   const endpoint = getApiEndpoint(action);
   return callMagentoApi(endpoint, method, postData, bearerToken);
 };
