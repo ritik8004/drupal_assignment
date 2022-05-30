6e34a5231ab2a0d82be6bf1f6b055f52c0d0e782
Saurabh Pandit <saurabhpandit@Saurabhs-MacBook-Pro.local>
2022-05-30 16:54:57 +0530

    CORE-40520: Fixed merge conflict in hm rcs transac module. Fixed merge conflict with develop,

--- docroot/modules/brands/alshaya_hm/modules/alshaya_hm_transac_rcs/js/alshaya_hm_transac_rcs_product.js
+++ docroot/modules/brands/alshaya_hm/modules/alshaya_hm_transac_rcs/js/alshaya_hm_transac_rcs_product.js
@@ -13,15 +13,93 @@
 
     var data = e.detail.result;
 
+    // Attributes to be displayed on main page.
+    let main_attributes_code = {
+      'fit' : 'FIT',
+      'article_description' : 'ARTICLE DESCRIPTION',
+    };
+    let description_details = [];
+    for (var attributes_code in main_attributes_code) {
+      if (Drupal.hasValue(data[attributes_code])) {
+        // Attribute codes are comma seperated if they have multiple values.
+        let attr_values = data[attributes_code].split(",");
+        let labels = [];
+        for (let attr_value of attr_values) {
+          let label = window.commerceBackend.getAttributeValueLabel(attributes_code, attr_value);
+          labels.push(label);
+        }
+        description_details.push({
+          title: main_attributes_code[attributes_code],
+          data: labels,
+        });
+      }
+    };
+
+    // Additional attributes to be displayed on overlay.
+    let overlay_attributes_codes = {
+      'product_designer_collection' : 'DESIGNER COLLECTION',
+      'concept' : 'CONCEPT',
+      'product_collection' : 'COLLECTION',
+      'product_environment' : 'ENVIRONMENT',
+      'product_quality' : 'QUALITY',
+      'product_feature' : 'FEATURE',
+      'function' : 'FUNCTION',
+      'washing_instructions' : 'WASHING INSTRUCTION',
+      'dry_cleaning_instructions' : 'DRY CLEAN INSTRUCTION',
+      'style' : 'STYLE',
+      'clothing_style' : 'CLOTHING STYLE',
+      'collar_style' : 'COLLAR STYLE',
+      'neckline_style' : 'NECKLINE STYLE',
+      'accessories_style' : 'ACCESSORIES STYLE',
+      'footwear_style' : 'FOOTWEAR STYLE',
+      'fit' : 'FIT',
+      'descriptive_length' : 'DESCRIPTIVE LENGTH',
+      'garment_length' : 'GARMENT LENGTH',
+      'sleeve_length' : 'SLEEVE LENGTH',
+      'waist_rise' : 'WAIST RISE',
+      'heel_height' : 'HEEL HEIGHT',
+      'measurements_in_cm' : 'MEASURMENTS IN CM',
+      'color_name' : 'COLOR NAME',
+      'fragrance_name' : 'FRAGRANCE NAME',
+      'article_fragrance_description' : 'FRAGRANCE DESCRIPTION',
+      'article_pattern' : 'PATTERN',
+      'article_visual_description' : 'VISUAL DESCRIPTION',
+      'textual_print' : 'TEXTUAL PRINT',
+      'article_license_company' : 'LICENSE COMPANY',
+      'article_license_item' : 'LICENSE ITEM',
+    };
+    let overlay_properties = [];
+    for (var attributes_code in overlay_attributes_codes) {
+      if (Drupal.hasValue(data[attributes_code])) {
+        // Attribute codes are comma seperated if they have multiple values.
+        let attr_values = data[attributes_code].split(",");
+        let labels = [];
+        for (let attr_value of attr_values) {
+          let label = window.commerceBackend.getAttributeValueLabel(attributes_code, attr_value);
+          labels.push(label);
+        }
+        overlay_properties.push({
+          title: overlay_attributes_codes[attributes_code],
+          data: labels,
+        });
+      }
+    };
+
+    // Get composition attribute based on product type.
+    let composition = fetchCompositionAttribute(data);
+
     // Add extra data to product description.
     // This will be rendered using handlebars templates to add P tags and H3 titles.
     e.detail.result.description = {
       html: data.description.html,
-      composition: data.composition,
+      composition: composition,
       washing_instructions: data.washing_instructions,
       article_warning: data.article_warning,
       sku: data.sku,
       show_product_detail_title: (data.composition || data.washing_instructions || data.article_warning),
+      description_details: description_details,
+      title_name: data.title_name,
+      overlay_properties: overlay_properties,
     };
 
     // Append field values to short_description.
@@ -58,4 +136,16 @@
       }
     })
   });
+
+    /**
+   * Fetch composition attribute for pdp.
+   */
+  var fetchCompositionAttribute = function(entity) {
+    if (entity.type_id == 'configurable') {
+    return entity.variants[0].product.composition;
+  }
+  else {
+    return entity.composition;
+  }
+};
 })();
