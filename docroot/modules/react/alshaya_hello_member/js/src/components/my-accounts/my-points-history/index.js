8fb8413de749934c8545b92725374576b1b1feb0
Sanjib.dutta <sanjib.dutta@alshaya.com>
2022-05-30 16:00:11 +0300

    CORE-41488: [My account] Hello membership points history.

--- docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/my-points-history/index.js
+++ docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/my-points-history/index.js
@@ -18,9 +18,23 @@ class MyPointsHistory extends React.Component {
     }
 
     return (
-      <div className="my-points-history-wrapper">
-        {/* Display points history */}
-      </div>
+      <>
+        <div className="my-points-history-wrapper">
+          <div className="purchase-store">
+            <p className="history-dark-title">Instore Purchase</p>
+            <p className="history-light-title">Lead Trinity</p>
+          </div>
+          <div className="points-date">02/12/2021</div>
+          <div className="points-earned">
+            <p className="history-light-title">Points earned</p>
+            <p>124</p>
+          </div>
+          <div className="voucher-redeem">
+            <p className="history-light-title">Voucher redeemed</p>
+            <p>2</p>
+          </div>
+        </div>
+      </>
     );
   }
 }
