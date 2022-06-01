6de085d61c2314fa0a9e0710ad6250b3cc3f040b
Sanjib.dutta <sanjib.dutta@alshaya.com>
2022-06-01 15:52:26 +0300

    CORE-41488: remooving voucher redeem as per comment.

--- docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/my-points-history/index.js
+++ docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/my-points-history/index.js
@@ -24,17 +24,13 @@ class MyPointsHistory extends React.Component {
         <div className="my-points-history-wrapper">
           <div className="history-points-row">
             <div className="purchase-store">
-              <p className="history-dark-title">{/* Instore Purchase */}</p>
-              <p className="history-light-title">{/* Lead Trinity */}</p>
+              <p className="history-dark-title"> Instore Purchase </p>
+              <p className="history-light-title"> Lead Trinity </p>
             </div>
-            <div className="points-date">{/* date 02/12/2021 */}</div>
+            <div className="points-date"> date 02/12/2021 </div>
             <div className="points-earned">
-              <p className="history-light-title">{/* Points earned */}</p>
-              <p>{/* points eg:124 */}</p>
-            </div>
-            <div className="voucher-redeem">
-              <p className="history-light-title">{/* Voucher redeemed */}</p>
-              <p>{/* no of voucher 2 */}</p>
+              <p className="history-light-title"> Points earned </p>
+              <p> points eg:124 </p>
             </div>
           </div>
         </div>
