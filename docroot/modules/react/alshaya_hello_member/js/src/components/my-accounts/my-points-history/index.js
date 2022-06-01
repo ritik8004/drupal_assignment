aa37057170d1bfc389df476a8fd80dc576f56913
Sanjib.dutta <sanjib.dutta@alshaya.com>
2022-06-01 12:26:55 +0300

    CORE-41488: [My account] Hello membership points history -- accordion and expiry block added.

--- docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/my-points-history/index.js
+++ docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/my-points-history/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import MembershipExpiry from './membership-expiry';
+import MembershipExpiryPoints from './membership-expiry-points';
 
 class MyPointsHistory extends React.Component {
   constructor(props) {
@@ -20,20 +22,24 @@ class MyPointsHistory extends React.Component {
     return (
       <>
         <div className="my-points-history-wrapper">
-          <div className="purchase-store">
-            <p className="history-dark-title">Instore Purchase</p>
-            <p className="history-light-title">Lead Trinity</p>
-          </div>
-          <div className="points-date">02/12/2021</div>
-          <div className="points-earned">
-            <p className="history-light-title">Points earned</p>
-            <p>124</p>
-          </div>
-          <div className="voucher-redeem">
-            <p className="history-light-title">Voucher redeemed</p>
-            <p>2</p>
+          <div className="history-points-row">
+            <div className="purchase-store">
+              <p className="history-dark-title">{/* Instore Purchase */}</p>
+              <p className="history-light-title">{/* Lead Trinity */}</p>
+            </div>
+            <div className="points-date">{/* date 02/12/2021 */}</div>
+            <div className="points-earned">
+              <p className="history-light-title">{/* Points earned */}</p>
+              <p>{/* points eg:124 */}</p>
+            </div>
+            <div className="voucher-redeem">
+              <p className="history-light-title">{/* Voucher redeemed */}</p>
+              <p>{/* no of voucher 2 */}</p>
+            </div>
           </div>
         </div>
+        <MembershipExpiry />
+        <MembershipExpiryPoints />
       </>
     );
   }
