305f8c74dbab4a4b86cdbb95bb029c07f70d6996
nikunjkotecha <nikunj.kotecha@acquia.com>
2022-06-02 10:50:19 +0530

    CORE-42033: Refactored code and reduced cache tags on all pages. Fixed translation context.

--- docroot/modules/react/js/utilities/onlineReturnsHelper.js
+++ docroot/modules/react/js/utilities/onlineReturnsHelper.js
@@ -3,13 +3,6 @@ import { hasValue } from './conditionsUtility';
 /**
  * Helper function to check if Online Returns is enabled.
  */
-const isOnlineReturnsEnabled = () => {
-  if (hasValue(drupalSettings.onlineReturns)
-    && hasValue(drupalSettings.onlineReturns.enabled)) {
-    return drupalSettings.onlineReturns.enabled;
-  }
-
-  return false;
-};
+const isOnlineReturnsEnabled = () => hasValue(drupalSettings.onlineReturns);
 
 export default isOnlineReturnsEnabled;
