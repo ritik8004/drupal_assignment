596d4d77fa8ba0956cd47310f6c4f7f209f20600
nikunjkotecha <nikunj.kotecha@acquia.com>
2022-06-02 11:56:49 +0530

    CORE-42037: Remove duplicate condition for feature status.

--- docroot/modules/react/alshaya_online_returns/js/my-accounts.js
+++ docroot/modules/react/alshaya_online_returns/js/my-accounts.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import OnlineReturnsEligibility from './my-account/online-returns-eligibility';
-import { isOnlineReturnsEnabled } from '../../js/utilities/onlineReturnsHelper';
 
 const returnWindowPlaceholder = document.querySelector('#online-returns-eligibility-window');
 
@@ -9,7 +8,7 @@ if (returnWindowPlaceholder) {
   const orderId = returnWindowPlaceholder.closest('.recent__orders--body').getAttribute('data-order-id');
   const selector = document.querySelector(`*[data-order-id="${orderId}"] #online-returns-eligibility-window`);
 
-  if (isOnlineReturnsEnabled() && selector) {
+  if (selector) {
     ReactDOM.render(
       <OnlineReturnsEligibility orderId={orderId} />,
       selector,
