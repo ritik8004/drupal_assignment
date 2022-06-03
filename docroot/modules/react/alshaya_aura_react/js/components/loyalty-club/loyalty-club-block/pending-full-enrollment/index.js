e607dac83945c440e28a9461641680540a989a87
koustubh dudhe <dudhe.koustubh@gmail.com>
2022-06-03 14:23:58 +0530

    CORE-42110: Content changes as per requirement.

--- docroot/modules/react/alshaya_aura_react/js/components/loyalty-club/loyalty-club-block/pending-full-enrollment/index.js
+++ docroot/modules/react/alshaya_aura_react/js/components/loyalty-club/loyalty-club-block/pending-full-enrollment/index.js
@@ -13,10 +13,10 @@ const AuraMyAccountPendingFullEnrollment = () => {
     <div className="aura-pending-full-enrollment-wrapper fadeInUp">
       <div className="pending-full-enrollment-description">
         <div className="title">
-          {Drupal.t('Congrats! You are now part of the exclusive AURA club. You’ll now earn points as you shop online and in stores.')}
+          {Drupal.t('Congrats! You are now part of the exclusive Aura club. You’ll now earn points as you shop online and in stores.')}
         </div>
         <div className="description">
-          {Drupal.t('To use your points online, please download the AURA app and provide us with a few more details.')}
+          {Drupal.t('To use your points online, please download the Aura MENA app and provide us with a few more details.')}
         </div>
         <div className="app-links">
           <a
