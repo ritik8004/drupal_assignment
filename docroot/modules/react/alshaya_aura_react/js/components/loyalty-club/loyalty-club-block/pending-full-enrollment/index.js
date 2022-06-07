aafb4f39f724fdce239bf033552e95998214a133
koustubh dudhe <dudhe.koustubh@gmail.com>
2022-06-07 11:16:59 +0530

    CORE-42110: Update translationss for my aura banner.

--- docroot/modules/react/alshaya_aura_react/js/components/loyalty-club/loyalty-club-block/pending-full-enrollment/index.js
+++ docroot/modules/react/alshaya_aura_react/js/components/loyalty-club/loyalty-club-block/pending-full-enrollment/index.js
@@ -13,10 +13,10 @@ const AuraMyAccountPendingFullEnrollment = () => {
     <div className="aura-pending-full-enrollment-wrapper fadeInUp">
       <div className="pending-full-enrollment-description">
         <div className="title">
-          {Drupal.t('Congrats! You are now part of the exclusive Aura club. You’ll now earn points as you shop online and in stores.', {}, { context: 'aura' })}
+          {Drupal.t('Congratulations! You are now part of Aura, the loyalty experience personalised for you. You can now earn points when you shop online or instore.', {}, { context: 'aura' })}
         </div>
         <div className="description">
-          {Drupal.t('To use your points online, please download the Aura MENA app and provide us with a few more details.', {}, { context: 'aura' })}
+          {Drupal.t('To use your points online, complete your registration on the Aura MENA app.', {}, { context: 'aura' })}
         </div>
         <div className="app-links">
           <a
