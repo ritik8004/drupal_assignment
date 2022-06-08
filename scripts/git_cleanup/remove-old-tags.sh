330d983c75a9e99f3a2ed1ffd3d49e925eccdddb
vbouchet <vincent.bouchet.31@gmail.com>
2022-06-08 21:23:40 +0200

    CORE-000: Improve the tag cleanup script.

--- scripts/git_cleanup/remove-old-tags.sh
+++ scripts/git_cleanup/remove-old-tags.sh
@@ -8,16 +8,23 @@ bltDir="$scriptDir/../../blt"
 repos=`grep -Ei '@svn-5975.enterprise-g1.hosting.acquia.com' ${bltDir}/blt.yml | sed -r "s/'//g" | sed -r "s/- //g"`
 
 for repo in $repos ; do
+  echo "Processing repo $repo."
   i=0
   to_delete=""
+  to_keep="WELCOME"
 
   # Get the release tags and keep the last ones (using nb_to_keep).
-  refs=$(git ls-remote -t --refs $repo | grep -o -E "refs/tags/.*$" | grep -o -E "refs/tags/[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+-build$" | sort -r -t '/' -k 3 -V)
+  refs=$(git ls-remote -t --refs $repo | grep -o -E "refs/tags/.*$" | grep -o -E "refs/tags/[0-9]+\.[0-9]+\.[0-9]+-build$" | sort -r -t '/' -k 3 -V)
   for ref in $refs ; do
     tag=$(echo $ref | cut -d'/' -f3)
 
     if (( $i < $nb_to_keep )) ; then
       echo "Keeping $tag"
+      if [ "$to_keep" = "" ] ; then
+        to_keep="$tag"
+      else
+        to_keep+=" $tag"
+      fi
       ((i++))
     else
       echo "Marking $tag as to be deleted"
@@ -29,37 +36,26 @@ for repo in $repos ; do
     fi
   done
 
-  t1=$(git ls-remote -t --refs $repo)
-  echo "T1"
-  echo "$t1"
-
-  t2=$(git ls-remote -t --refs $repo | grep -o -E "refs/tags/.*$")
-  echo "T2"
-  echo "$t2"
-
-  t3=$(git ls-remote -t --refs $repo | grep -o -E "refs/tags/.*$" | grep -o -vE "refs/tags/[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+-build$")
-  echo "T3"
-  echo "$t3"
-
-  t4=$(git ls-remote -t --refs $repo | grep -o -E "refs/tags/.*$" | grep -o -vE "refs/tags/[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+-build$" | grep -o -vE "refs/tags/WELCOME")
-  echo "T4"
-  echo "$t4"
-
-  t5=$(git ls-remote -t --refs $repo | grep -o -E "refs/tags/.*$" | grep -o -vE "refs/tags/[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+-build$" | grep -o -vE "refs/tags/WELCOME" | sort -r -t '/' -k 3 -V)
-  echo "T5"
-  echo "$t5"
-
-  # Get the other tags except the "WELCOME" one.
-  refs=$(git ls-remote -t --refs $repo | grep -o -E "refs/tags/.*$" | grep -o -vE "refs/tags/[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+-build$" | grep -o -vE "refs/tags/WELCOME" | sort -r -t '/' -k 3 -V)
-  echo "$refs"
+  # Get the all tags and compare against the to_keep list.
+  # grep -o -vE behaves in a different way on BSD and GNU systems. Instead of building a complex solution to
+  # workaround, simply compare the list of tags with the list of tags we know we want to keep.
+  refs=$(git ls-remote -t --refs $repo | grep -o -E "refs/tags/.*$" | sort -r -t '/' -k 3 -V)
   for ref in $refs ; do
     tag=$(echo $ref | cut -d'/' -f3)
 
-    echo "Marking $tag as to be deleted"
-    if [ "$to_delete" = "" ] ; then
-      to_delete="$tag"
-    else
-      to_delete+=" $tag"
+    for keep_tag in $to_keep ; do
+      if [ "$tag" = "$keep_tag" ] ; then
+        break
+      fi
+    done
+
+    if [ "$tag" != "$keep_tag" ] ; then
+      echo "Marking $tag as to be deleted"
+      if [ "$to_delete" = "" ] ; then
+        to_delete="$tag"
+      else
+        to_delete+=" $tag"
+      fi
     fi
   done
   echo
