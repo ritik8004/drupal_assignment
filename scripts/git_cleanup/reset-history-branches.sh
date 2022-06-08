6a19cca04597cf7146bdda765bc5e8a29a733665
vbouchet <vincent.bouchet.31@gmail.com>
2022-06-08 16:40:38 +0200

    CORE-000: Update the script to reset branches history.

--- scripts/git_cleanup/reset-history-branches.sh
+++ scripts/git_cleanup/reset-history-branches.sh
@@ -11,14 +11,22 @@ repo1=`echo ${repos} | cut -d" " -f1`
 
 # Clone the first repo in a temporary directory.
 rm -Rf /tmp/alshaya_acquia_git
+echo "Cloning $repo1 into /tmp/alshaya_acquia_git."
 git clone $repo1 /tmp/alshaya_acquia_git
 cd /tmp/alshaya_acquia_git || exit
 
 # Add the other repos as git remotes.
 for repo in $repos ; do
   repo_name=$(echo $repo | cut -d '@' -f1)
+  echo "Adding $repo as $repo_name remote."
   git remote add $repo_name $repo
 done
+
+echo "Settings origin as the default remote."
+git config checkout.defaultRemote origin
+
+echo "Fetching all remotes."
+git pull --all
 
 # Fetch the list of branches we want to reset.
 refs=$(git ls-remote -h $repo1 | grep -o -E "refs/heads/.*-build$")
@@ -27,10 +35,9 @@ refs+=" distro"
 # Reset the history of each branch and push to all repos.
 for ref in $refs ; do
   ref_name=$(echo $ref | cut -d '/' -f3)
-  echo $ref_name
+  echo "Processing branch $ref_name."
 
-  git checkout $ref_name
-  git pull --all
+  git checkout origin/$ref_name
   git checkout --orphan $ref_name-tmp
   git add .
   git commit -m "Starting fresh orphan branch for $ref_name" --quiet
@@ -40,6 +47,7 @@ for ref in $refs ; do
   # Push the reset branch to all the remotes.
   for repo in $repos ; do
     repo_name=$(echo $repo | cut -d '@' -f1)
+    echo "Pushing new $ref_name to $repo_name"
     git push -f $repo_name $ref_name
   done
 done
