10f9fe6ec9693f1088b9159c91772c12318c5311
Sanjib.dutta <sanjib.dutta@alshaya.com>
2022-06-09 18:57:27 +0300

    CORE-41507: [My account] My benefits & My points -- slider implemented for voucher and points.

--- docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/my-benefits/my-offers-vouchers/index.js
+++ docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/my-benefits/my-offers-vouchers/index.js
@@ -1,39 +1,51 @@
-import React, { useState } from 'react';
-import getStringMessage from '../../../../../../../js/utilities/strings';
-import ConditionalView from '../../../../../../../js/utilities/components/conditional-view';
+import React from 'react';
+import Slider from 'react-slick';
 
 const MyOffersAndVouchers = ({ myBenefitsList }) => {
-  const showMoreLimit = 3;
-  const [expanded, setExpanded] = useState(false);
-  const dataForDisplay = expanded ? myBenefitsList : myBenefitsList.slice(0, showMoreLimit);
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    adaptiveHeight: true,
+    lazyLoad: true,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          swipeToSlide: true,
+        },
+      },
+    ],
+  };
 
   return (
     <div className="my-benefits-wrapper">
-      {dataForDisplay.map((data) => (
-        <div className="my-offers-vouchers-details" key={data.id}>
-          <div className="image-container">
-            <img src={data.image} />
+      <Slider {...settings}>
+        {myBenefitsList.map((data) => (
+          <div className="my-offers-vouchers-details" key={data.id}>
+            <div className="image-container">
+              <img src={data.image} />
+            </div>
+            <div className="voucher-wrapper">
+              <div className="title">
+                {data.name}
+              </div>
+              <div className="info">
+                {data.description}
+              </div>
+              <div className="expiry">
+                {data.end_date}
+              </div>
+            </div>
           </div>
-          <div className="voucher-wrapper">
-            <div className="title">
-              {data.name}
-            </div>
-            <div className="info">
-              {data.description}
-            </div>
-            <div className="expiry">
-              {data.end_date}
-            </div>
-          </div>
-        </div>
-      ))}
-      <ConditionalView condition={myBenefitsList.length > showMoreLimit}>
-        <div className="btn-wrapper">
-          <a onClick={() => setExpanded(!expanded)}>
-            {expanded ? getStringMessage('show_less') : getStringMessage('show_all')}
-          </a>
-        </div>
-      </ConditionalView>
+        ))}
+      </Slider>
     </div>
   );
 };
