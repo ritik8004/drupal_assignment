50548ebd06c0fb02210561294d7f01c76fabbcbf
Milind Kagdelwar <milind.kagdelwar@acquia.com>
2022-06-22 17:58:19 +0530

    CORE-42087: Updated condition for date and ict component.

--- docroot/modules/react/alshaya_spc/js/checkout/components/online-booking/ict-delivery-information/index.js
+++ docroot/modules/react/alshaya_spc/js/checkout/components/online-booking/ict-delivery-information/index.js
@@ -4,8 +4,14 @@ import moment from 'moment';
 const IctDeliveryInformation = (props) => {
   const { deliveryMethod, date } = props;
 
+  // Check if delivery method in HD and CNC.
   if (deliveryMethod !== 'home_delivery'
     && deliveryMethod !== 'click_and_collect') {
+    return null;
+  }
+
+  // Check if the date is empty.
+  if (!date) {
     return null;
   }
 
@@ -16,14 +22,12 @@ const IctDeliveryInformation = (props) => {
 
   // return the common markup.
   return (
-    <>
-      <div className={`ict-delivery-info_${deliveryMethod}`}>
-        {`${deliveryText} 
-        ${moment(date).format('Do')} 
-        ${moment(date).locale(drupalSettings.path.currentLanguage).format('MMM')} 
-        ${moment().format('YYYY')}`}
-      </div>
-    </>
+    <div className={`ict-delivery-info_${deliveryMethod}`}>
+      {`${deliveryText}
+       ${moment(date).format('Do')} 
+       ${moment(date).locale(drupalSettings.path.currentLanguage).format('MMM')} 
+       ${moment().format('YYYY')}`}
+    </div>
   );
 };
 export default IctDeliveryInformation;
