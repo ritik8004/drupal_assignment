fb4d011c13f3f1425cfdcf5a5f3eb749b23d0efc
Hammad Ghani <mohamed.ghani@alshaya.com>
2022-06-22 16:41:07 +0530

    CORE-41887: [My account] My details.

---
+++ docroot/modules/react/alshaya_hello_member/js/src/components/send-otp-popup/index.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import Popup from 'reactjs-popup';
+import { sendOtp, verifyOtp } from '../../../../../js/utilities/otp_helper';
+import getStringMessage from '../../../../../js/utilities/strings';
+
+class SendOtpPopup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openModal: false,
+    };
+  }
+
+  // Open Modal.
+  openModal = (e) => {
+    const responseData = sendOtp(
+      `${drupalSettings.alshaya_mobile_prefix.slice(1)}${document.getElementById('edit-field-mobile-number-0-mobile').value}`,
+      'reg',
+    );
+    if (responseData instanceof Promise) {
+      responseData.then((result) => {
+        if (result.status !== undefined) {
+          this.setState({
+            openModal: true,
+          });
+        }
+      });
+    }
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  // Close Modal.
+  closeModal = () => {
+    this.setState({
+      openModal: false,
+    });
+  };
+
+  // Resend OTP.
+  resendOtp = () => {
+    const responseData = sendOtp(
+      `${drupalSettings.alshaya_mobile_prefix.slice(1)}${document.getElementById('edit-field-mobile-number-0-mobile').value}`,
+      'reg',
+    );
+    if (responseData instanceof Promise) {
+      responseData.then((result) => {
+        if (result.status !== undefined) {
+          this.setState({
+            openModal: true,
+          });
+        }
+      });
+    }
+  };
+
+  // Verify OTP
+  verifyOtp = () => {
+    const responseData = verifyOtp(
+      `${document.getElementById('edit-field-mobile-number-0-mobile').value}`,
+      `${document.getElementsByName('OTP')[0].value}`,
+      'reg',
+      `${drupalSettings.alshaya_mobile_prefix.slice(1)}`,
+    );
+    if (responseData instanceof Promise) {
+      responseData.then((result) => {
+        if (result.data.status !== undefined) {
+          this.setState({
+            openModal: false,
+          });
+        }
+        // If successfully verified make the otp verified checkbox seleted.
+        document.getElementById('edit-field-verified-otp-value').checked = true;
+      });
+    }
+  };
+
+  render() {
+    const { openModal } = this.state;
+    const buttonName = Drupal.t('Send OTP', {}, { context: 'hello_member' });
+    const button = React.createElement('button', { type: 'submit', onClick: this.openModal }, buttonName);
+    return (
+      <>
+        { button }
+        <Popup
+          open={openModal}
+          className="hello_member_otp_popup"
+          closeOnDocumentClick={false}
+          closeOnEscape={false}
+        >
+          <div className="hello-member-otp-popup-form">
+            <a className="close-modal" onClick={(e) => this.closeModal(e)} />
+            { getStringMessage('sent_otp_message') }
+            { document.getElementById('edit-field-mobile-number-0-mobile').value }
+            <input
+              type="text"
+              id="OTP"
+              name="OTP"
+              className="OTP"
+              maxLength="6"
+            />
+          </div>
+          <div className="hello-member-modal-form-actions">
+            <div className="hello-member-otp-submit-description">
+              { getStringMessage('resend_otp_desc') }
+              <div className="hello-member-modal-form-resend-otp" onClick={() => this.resendOtp()}>{ getStringMessage('resend_code_label') }</div>
+            </div>
+            <div className="hello-member-modal-form-submit" onClick={() => this.verifyOtp()}>{ getStringMessage('verify_label') }</div>
+          </div>
+        </Popup>
+      </>
+    );
+  }
+}
+
+export default SendOtpPopup;
---
+++ docroot/modules/react/alshaya_hello_member/js/src/send-otp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SendOtpPopup from './components/send-otp-popup';
+
+ReactDOM.render(
+  <SendOtpPopup />,
+  document.getElementById('hello-member-send-otp'),
+);
