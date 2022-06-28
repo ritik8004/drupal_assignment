6f982138f3bdae7ca22969da47b379a370498adb
Mohammad Rashid <mohdrashid987305@gmail.com>
2022-06-28 18:46:59 +0530

    CORE-42577: Refactoring done based on new structure in api response.

--- docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/my-points-history/index.js
+++ docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/my-points-history/index.js
@@ -76,7 +76,6 @@ class MyPointsHistory extends React.Component {
 
     return (
       <>
-        <MemberPointsSummary />
         <div className="my-points-history-wrapper">
           {pointsHistoryData.map((data) => (
             <div className="history-points-row" key={data.trn_no}>
@@ -101,6 +100,7 @@ class MyPointsHistory extends React.Component {
             </div>
           </ConditionalView>
         </div>
+        <MemberPointsSummary />
       </>
     );
   }
