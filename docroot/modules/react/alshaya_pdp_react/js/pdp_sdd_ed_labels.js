2d9609a3752d8f32de0f3d2b65e3b99c3b14a696
Avinash <avinash.shukla@acquia.com>
2022-07-04 17:40:40 +0530

    CORE-42470: Moving component to alshaya spc module.

--- docroot/modules/react/alshaya_pdp_react/js/pdp_sdd_ed_labels.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PdpSddEd from './pdp-layout/components/pdp-sdd-ed';
-
-ReactDOM.render(
-  <PdpSddEd deliveryOptions={drupalSettings.deliveryOptions} />,
-  document.getElementById('sdd-ed-labels'),
-);
