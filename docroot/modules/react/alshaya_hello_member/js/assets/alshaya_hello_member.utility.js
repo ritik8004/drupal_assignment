7b9f47a057e790d51919463dfb31c7f4099f59c9
www-data <www-data@bce13d18b279>
2022-07-07 19:10:59 +0000

    CORE-42861: MyAccount Add more & Get more is not working as expected.

---
+++ docroot/modules/react/alshaya_hello_member/js/assets/alshaya_hello_member.utility.js
@@ -0,0 +1,16 @@
+(function ($,Drupal) {
+  'use strict';
+
+  Drupal.behaviors.alshayaHelloMemberProfile = {
+    attach: function (context) {
+      $('input#edit-field-mobile-number-0-mobile'').change(function(){
+        if( $('input#edit-field-mobile-number-0-mobile').val() !== ''
+          && ($('input#edit-field-mobile-number-0-mobile').attr('value') !== $('input#edit-field-mobile-number-0-mobile').val())) {
+          $('#hello-member-send-otp .btn-wrapper').removeClass('in-active');
+          $('#edit-submit').addClass('in-active');
+        }
+      });
+    },
+  };
+
+})(jQuery,Drupal);
