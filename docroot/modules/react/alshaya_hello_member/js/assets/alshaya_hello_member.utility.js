c34fab20899a082b190732788d8c07f30bd75a84
www-data <www-data@bce13d18b279>
2022-07-07 19:30:23 +0000

    CORE-42861: MyAccount Add more & Get more is not working as expected.

--- docroot/modules/react/alshaya_hello_member/js/assets/alshaya_hello_member.utility.js
+++ docroot/modules/react/alshaya_hello_member/js/assets/alshaya_hello_member.utility.js
@@ -4,6 +4,7 @@
   Drupal.behaviors.alshayaHelloMemberProfile = {
     attach: function (context) {
       $('input#edit-field-mobile-number-0-mobile').change(function(){
+        // On changing phone number in my account disable submit till otp is verified.
         if( $('input#edit-field-mobile-number-0-mobile').val() !== ''
           && ($('input#edit-field-mobile-number-0-mobile').attr('value') !== $('input#edit-field-mobile-number-0-mobile').val())) {
           $('#hello-member-send-otp .btn-wrapper').removeClass('in-active');
