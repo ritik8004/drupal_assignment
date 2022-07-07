1f71a9995506e4a8fea6445a795ad7fb195ddc85
Kunal Singh <skunal185@gmail.com>
2022-07-08 01:56:04 +0530

    DIG-10120: Added the GTM event for return item select.

--- docroot/modules/react/alshaya_online_returns/js/utilities/online_returns_util.js
+++ docroot/modules/react/alshaya_online_returns/js/utilities/online_returns_util.js
@@ -1,5 +1,6 @@
 import moment from 'moment-timezone';
 import { hasValue } from '../../../js/utilities/conditionsUtility';
+import { getDeliveryAddress } from './return_request_util';
 
 /**
  * Helper function to check if order is eligible for return.
@@ -257,6 +258,83 @@ function getPrintLabelStatus(returnData) {
   return false;
 }
 
+/**
+ * Utility function to get order GTM info.
+ */
+function getOrderGtmInfo() {
+  if (hasValue(drupalSettings.returnInfo)
+    && hasValue(drupalSettings.returnInfo.orderDetails)
+    && hasValue(drupalSettings.returnInfo.orderDetails['#gtm_info'])) {
+    return drupalSettings.returnInfo.orderDetails['#gtm_info'];
+  }
+
+  return '';
+}
+
+/**
+ * Get the product array of all the selected items.
+ *
+ * @param {array} itemsSelected
+ *   An array of all the selected products for return.
+ *
+ * @returns {array}
+ *   An array containing sku info of all the selected products.
+ */
+function getProductGtmInfo(itemsSelected) {
+  // Build the product SKU object for all the selected items.
+  const skuProduct = [];
+  itemsSelected.forEach((item) => {
+    const GtmInfo = getOrderGtmInfo();
+    if (GtmInfo && hasValue(GtmInfo.products) && hasValue(GtmInfo.products[item.sku])) {
+      // Push the return reason for individual item.
+      GtmInfo.products[item.sku].reason = item.reason;
+      skuProduct.push(GtmInfo.products[item.sku]);
+    }
+  });
+
+  return skuProduct;
+}
+
+/**
+ * Get the parepared order object for the return GTM.
+ *
+ * @returns {object}
+ *   The return order GTM object.
+ */
+function getPreparedOrderGtm(eventType) {
+  const gtmInfo = getOrderGtmInfo();
+  const {
+    transactionId,
+    deliveryOption,
+    firstTimeTransaction,
+  } = gtmInfo.general;
+
+  // Get delivery address info.
+  const deliveryInfo = getDeliveryAddress(getOrderDetails());
+
+  // Prepare the Return order object.
+  const returnOrder = {
+    orderTransactionId: transactionId,
+    orderType: deliveryOption,
+    orderFirstTimeTransaction: firstTimeTransaction,
+    orderDeliveryCity: deliveryInfo.area_parent_display,
+    orderDeliveryArea: deliveryInfo.administrative_area_display,
+    returnType: 'online',
+  };
+
+  // @todo to update the info in the further GA tickets.
+  // Add returned & refunded info for selected events.
+  if (eventType !== 'item_confirmed') {
+    returnOrder.refundAmount = '';
+    returnOrder.refundMethods = '';
+    returnOrder.returnId = '';
+    // @Todo To add the firstTimeReturn info when available.
+    returnOrder.firstTimeReturn = '';
+  }
+
+  return returnOrder;
+}
+
 export {
   isReturnEligible,
   getReturnExpiration,
@@ -276,4 +354,7 @@ export {
   getReturnWindowEligibleDateMessage,
   getPrintLabelStatus,
   getCancelButtonStatus,
+  getOrderGtmInfo,
+  getPreparedOrderGtm,
+  getProductGtmInfo,
 };
