fc52a0b4b16ef3e73bcc6a4d4c354c44e5ee3912
Saurabh Pandit <saurabhpandit@Saurabhs-MacBook-Pro.local>
2022-07-15 12:08:04 +0530

    CORE-42886: Added child component to display price ranges.

--- docroot/modules/react/alshaya_algolia_react/js/src/components/price/PriceElement.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/components/price/PriceElement.js
@@ -1,27 +1,36 @@
 import React from 'react';
+import { hasValue } from '../../../../../js/utilities/conditionsUtility';
+
+const PriceItem = ({ amount, maxAmount }) => {
+  const { decimalPoints } = drupalSettings.reactTeaserView.price;
+
+  if (hasValue(maxAmount)) {
+    return (
+      <span key="amount" className="price-amount">
+        {Number(amount).toFixed(decimalPoints)}
+        <span className="min-max-separator">-</span>
+        {Number(maxAmount).toFixed(decimalPoints)}
+      </span>
+    );
+  } else {
+    return (
+      <span key="amount" className="price-amount">
+      {amount.toFixed(decimalPoints)}
+      </span>
+    );
+  }
+};
 
 const PriceElement = ({ amount, maxAmount }) => {
   if (typeof amount === 'undefined') {
     return (null);
   }
-  const { decimalPoints } = drupalSettings.reactTeaserView.price;
-  const priceItem = (typeof maxAmount !== 'undefined')
-    ? (
-      <span key="amount" className="price-amount">
-        {amount.toFixed(decimalPoints)}
-        <span className="min-max-separator">-</span>
-        {maxAmount.toFixed(decimalPoints)}
-      </span>
-    )
-    : (
-      <span key="amount" className="price-amount">{amount.toFixed(decimalPoints)}</span>
-    );
 
   const priceParts = [
     (
       <span key="currency" className="price-currency suffix">{drupalSettings.reactTeaserView.price.currency}</span>
     ),
-    (priceItem),
+    (<PriceItem key="price-item" amount={amount} maxAmount={maxAmount} />),
   ];
 
   return (
