e558b701c10d4347d713cf7ef347058c2cf1dac9
Sasikumar <sasikumar.k@alshaya.com>
2022-07-23 18:31:39 +0530

    CORE-43587: Update force loyalty popup content.

--- docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/membership-popup/index.js
+++ docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/membership-popup/index.js
@@ -7,7 +7,7 @@ class MembershipPopup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isModelOpen: false,
+      isModelOpen: true,
     };
   }
 
@@ -47,8 +47,7 @@ class MembershipPopup extends React.Component {
           closeOnEscape={false}
         >
           <div className="hello-member-popup-form">
-            <a className="close-modal" onClick={(e) => this.closeModal(e)} />
-            <MembershipInfo />
+            <MembershipInfo close={this.closeModal.bind(this)}/>
           </div>
         </Popup>
       </>
