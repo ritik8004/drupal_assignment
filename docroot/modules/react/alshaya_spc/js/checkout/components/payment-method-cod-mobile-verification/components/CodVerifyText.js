98505ec11f3ab28de3242f6abe87d9204b7e6ce8
Avinash <avinash.shukla@acquia.com>
2022-07-28 17:03:50 +0530

    CO-87: Fixed translation string format.

--- docroot/modules/react/alshaya_spc/js/checkout/components/payment-method-cod-mobile-verification/components/CodVerifyText.js
+++ docroot/modules/react/alshaya_spc/js/checkout/components/payment-method-cod-mobile-verification/components/CodVerifyText.js
@@ -3,8 +3,8 @@ import React from 'react';
 const CodVerifyText = ({ mobileNumber, otpLength }) => (
   <div className="cod-mobile-verify-text">
     <span>
-      {Drupal.t('Enter the @otp_length-digit OTP code sent to @mobileNumber', {
-        '@mobileNumber': mobileNumber,
+      {Drupal.t('Enter the @otp_length-digit OTP code sent to @mobile_number', {
+        '@mobile_number': mobileNumber,
         '@otp_length': otpLength,
       }, { context: 'cod_mobile_verification' })}
     </span>
