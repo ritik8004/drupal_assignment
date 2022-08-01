89f585eecb4c1e30ef3644aab8804e66caf2eb25
vivekpan27 <vivek.panicker@innoraft.com>
2022-08-01 19:33:57 +0530

    CORE-43689: Used render array instead of markup and added attribiutes.

--- docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_seo/src/Controller/AlshayaRcsSeoController.php
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_seo/src/Controller/AlshayaRcsSeoController.php
@@ -3,6 +3,7 @@
 namespace Drupal\alshaya_rcs_seo\Controller;
 
 use Drupal\alshaya_acm_product_category\ProductCategoryTree;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -12,20 +13,33 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class AlshayaRcsSeoController extends ControllerBase {
 
   /**
-   * Product Category Tree service object.
+   * Product Category Tree.
    *
    * @var \Drupal\alshaya_acm_product_category\ProductCategoryTree
    */
   protected $productCategoryTree;
 
   /**
+   * Config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
    * AlshayaRcsSeoController constructor.
    *
    * @param \Drupal\alshaya_acm_product_category\ProductCategoryTree $product_category_tree
-   *   Product Category Tree service object.
+   *   Product Category Tree.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *  Config factory.
    */
-  public function __construct(ProductCategoryTree $product_category_tree) {
+  public function __construct(
+    ProductCategoryTree $product_category_tree,
+    ConfigFactoryInterface $config_factory
+  ) {
     $this->productCategoryTree = $product_category_tree;
+    $this->configFactory = $config_factory;
   }
 
   /**
@@ -33,7 +47,8 @@ class AlshayaRcsSeoController extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('alshaya_acm_product_category.product_category_tree')
+      $container->get('alshaya_acm_product_category.product_category_tree'),
+      $container->get('config.factory')
     );
   }
 
@@ -42,7 +57,14 @@ class AlshayaRcsSeoController extends ControllerBase {
    */
   public function siteMap() {
     return [
-      '#markup' => '<div id="rcs-ph-sitemap"></div>',
+      '#type' => 'container',
+      '#attributes' => [
+        'id' => 'rcs-ph-sitemap',
+        'data-param-get-data' => 'false',
+        'class' => ['block-rcs-ph-sitemap-block'],
+        'data-param-entity-to-get' => 'navigation_menu',
+        'data-param-category_id' => $this->configFactory->get('alshaya_rcs_main_menu.settings')->get('root_category'),
+      ],
       '#attached' => [
         'library' => [
           'alshaya_white_label/sitemap',
