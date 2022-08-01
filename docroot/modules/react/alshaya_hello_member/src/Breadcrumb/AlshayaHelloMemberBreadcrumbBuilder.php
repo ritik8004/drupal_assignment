5a707b4d7029b3819137457e72a5e9abb83b61d1
Mohammad Rashid <mohdrashid987305@gmail.com>
2022-08-01 15:14:44 +0300

    CORE-43886: Fix translations issue.

--- docroot/modules/react/alshaya_hello_member/src/Breadcrumb/AlshayaHelloMemberBreadcrumbBuilder.php
+++ docroot/modules/react/alshaya_hello_member/src/Breadcrumb/AlshayaHelloMemberBreadcrumbBuilder.php
@@ -61,12 +61,11 @@ class AlshayaHelloMemberBreadcrumbBuilder implements BreadcrumbBuilderInterface 
     $user_id = $this->currentUser->id();
     $type = $this->currentRequest->get('type');
     $code = $this->currentRequest->get('code');
-    $benefit_type = ($type === 'coupon') ? 'Coupon' : 'Offer';
+    $benefit_type = ($type === 'coupon') ? $this->t('Coupon', [], ['context' => 'hello_member']) : $this->t('Offer', [], ['context' => 'hello_member']);
 
     $breadcrumb->addLink(Link::createFromRoute($this->t('My Account'), 'entity.user.canonical', ['user' => $user_id]));
-    $breadcrumb->addLink(Link::createFromRoute($this->t('@benefit_type',
-      ['@benefit_type' => $benefit_type],
-      ['context' => 'hello_member']),
+    $breadcrumb->addLink(Link::createFromRoute(
+      $benefit_type,
       'alshaya_hello_member.hello_member_benefits_page',
       ['user' => $user_id, 'type' => $type, 'code' => $code],
       ['attributes' => ['class' => ['hello-member-brdcrb-nav']]]
