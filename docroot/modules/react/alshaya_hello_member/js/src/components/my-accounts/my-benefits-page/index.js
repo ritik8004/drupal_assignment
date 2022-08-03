4f040fe290b63cb635dbf7c2f4d4b2024321c8d9
harshita <harshita.shukla@alshaya.com>
2022-08-03 09:35:19 +0000

    CORE-43375: QR code update for benefits on my account.

--- docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/my-benefits-page/index.js
+++ docroot/modules/react/alshaya_hello_member/js/src/components/my-accounts/my-benefits-page/index.js
@@ -17,6 +17,7 @@ class MyBenefitsPage extends React.Component {
       wait: false,
       myBenefit: null,
       codeId: null,
+      couponId: null,
       voucherType: null,
     };
   }
@@ -34,7 +35,8 @@ class MyBenefitsPage extends React.Component {
           this.setState({
             myBenefit: response.data.coupons[0],
             wait: true,
-            codeId: `${response.data.coupons[0].type}|${response.data.coupons[0].code}`,
+            codeId: response.data.coupons[0].code,
+            couponId: `${response.data.coupons[0].type}|${response.data.coupons[0].code}`,
             voucherType: response.data.coupons[0].type,
           });
           removeFullScreenLoader();
@@ -67,7 +69,7 @@ class MyBenefitsPage extends React.Component {
 
   render() {
     const {
-      wait, myBenefit, codeId, voucherType,
+      wait, myBenefit, codeId, couponId, voucherType,
     } = this.state;
 
     if (!wait) {
@@ -107,7 +109,7 @@ class MyBenefitsPage extends React.Component {
           <QrCodeDisplay
             memberId={myBenefit.member_identifier}
             qrCodeTitle={qrCodeTitle}
-            codeId={codeId}
+            codeId={couponId || codeId}
             width={79}
           />
           <AddBenefitsToCart
