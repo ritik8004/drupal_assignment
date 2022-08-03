1df8fbf11ae842942c39571360d264d56412c899
Supreetam Laha <supreetam.laha@acquia.com>
2022-08-04 03:39:58 +0530

    CORE-43057: Handling different payment method scenarios for checkout S4 event.

--- docroot/modules/react/alshaya_spc/js/checkout/components/payment-method-apple-pay/index.js
+++ docroot/modules/react/alshaya_spc/js/checkout/components/payment-method-apple-pay/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ApplePay from '../../../utilities/apple_pay';
+import dispatchCustomEvent from '../../../utilities/events';
 
 class PaymentMethodApplePay extends React.Component {
   componentDidMount = () => {
@@ -8,6 +9,10 @@ class PaymentMethodApplePay extends React.Component {
 
   validateBeforePlaceOrder = () => {
     const { cart } = this.props;
+    // To add the custom event for the checkout step 4.
+    dispatchCustomEvent('orderValidated', {
+      cart: cart.cart,
+    });
     ApplePay.startPayment(cart.cart.totals.base_grand_total);
     return false;
   };
