5d1087a7602eb2813073c9b3555213614979bc13
Marcelo Vani <marcelo.vani@acquia.com>
2022-08-05 11:50:40 +0100

    CORE-40521: Removed else statement.

--- docroot/modules/custom/alshaya_pims/modules/alshaya_pims_assets/js/alshaya_assets_swatch.js
+++ docroot/modules/custom/alshaya_pims/modules/alshaya_pims_assets/js/alshaya_assets_swatch.js
@@ -10,15 +10,14 @@
         if (variant.product.swatch_data.swatch_type === 'image') {
           try {
             const data = JSON.parse(variant.product.assets_swatch);
-            if (Drupal.hasValue(data[0].url)) {
-              e.detail.colorOptionsList = Object.assign(e.detail.colorOptionsList, {
-                display_value: '<img src="' + data[0].url + '">',
-                swatch_type: data[0].image_type,
-              });
-            }
-            else {
+            if (!Drupal.hasValue(data[0].url)) {
               throw new Error('Empty url.');
             }
+
+            e.detail.colorOptionsList = Object.assign(e.detail.colorOptionsList, {
+              display_value: '<img src="' + data[0].url + '">',
+              swatch_type: data[0].image_type,
+            });
           }
           catch (e) {
             Drupal.alshayaLogger('warning', 'Invalid swatch asset data for sku @sku. @message', {
