f8320df60b85c3fd811a02d72834edeed8394c45
saurabh-axl <saurabh.pandit@axelerant.com>
2022-08-12 14:58:39 +0530

    CORE-43615: Moved RCS PLP mobile menu into handlebar template.

--- docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_listing/js/alshaya_rcs_listing_renderer.es5.js
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_listing/js/alshaya_rcs_listing_renderer.es5.js
@@ -1,4 +1,4 @@
 global.renderRcsListing = require('./alshaya_rcs_listing_renderer-exports.es5.js');
 global.renderRcsBreadcrumb = require('./alshaya_rcs_listing_renderer_breadcrumb-exports.es5');
-global.renderRcsPlpMobileMenu = require('./alshaya_rcs_listing_mobile_menu_renderer.es5');
+global.plpMobileMenuProcessor = require('./alshaya_rcs_listing_mobile_menu_processor-exports.es5');
 global.lhnProcessor = require('./alshaya_rcs_listing_lhn_processor-exports.es5.js');
