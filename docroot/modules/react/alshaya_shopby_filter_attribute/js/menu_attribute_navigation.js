d3b89c8a0e596cafdcc87214a9890205b915d97c
Anurag Sharma <anuragsharmam@3197917.no-reply.drupal.org>
2022-08-18 14:24:11 +0530

    CORE-42242: Attach react component only if feature is enabled and attribute is available.

--- docroot/modules/react/alshaya_shopby_filter_attribute/js/menu_attribute_navigation.js
+++ docroot/modules/react/alshaya_shopby_filter_attribute/js/menu_attribute_navigation.js
@@ -6,13 +6,20 @@ import AttrNavigation from './components/attr_navigation';
  * Function to render Attribute navigation in main menu.
  */
 const renderAttributeNavigation = () => {
-  const attributeNavSelector = document.querySelectorAll('[data-nav-attr="size_shoe_eu"]');
+  // Return if filter attribute is undefined or empty.
+  if (typeof drupalSettings.shopByFilterAttribute.menuFilterAttributes === 'undefined'
+    || (typeof drupalSettings.shopByFilterAttribute.menuFilterAttributes !== 'undefined'
+    && drupalSettings.shopByFilterAttribute.menuFilterAttributes === '')) {
+    return;
+  }
+
+  const attributeNavSelector = document.querySelectorAll(`[data-nav-attr="${drupalSettings.shopByFilterAttribute.menuFilterAttributes}"]`);
   const attributeNavSelectorList = [].slice.call(attributeNavSelector);
 
   attributeNavSelectorList.forEach((attrNavElement) => {
     ReactDOM.render(
       <AttrNavigation
-        attr="size_shoe_eu"
+        attr={drupalSettings.shopByFilterAttribute.menuFilterAttributes}
         element={attrNavElement}
       />,
       attrNavElement,
@@ -20,6 +27,11 @@ const renderAttributeNavigation = () => {
   });
 };
 
-
-// Add modal load event listener to render shop by filters menu item.
-window.addEventListener('load', renderAttributeNavigation);
+// Check if config exist and feature is enabled and bind the page load event
+// to render the attribute navigation component.
+if (typeof drupalSettings.shopByFilterAttribute !== 'undefined'
+  && typeof drupalSettings.shopByFilterAttribute.enabled !== 'undefined'
+  && drupalSettings.shopByFilterAttribute.enabled) {
+  // Add modal load event listener to render shop by filters menu item.
+  window.addEventListener('load', renderAttributeNavigation);
+}
