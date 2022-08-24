8285dc369ec169ab64d895b60d85b517b62849d6
www-data <anjali.vijay@axelerant.com>
2022-08-24 08:45:19 +0000

    DIG-11744: PDP return eligibility notification theming.

--- docroot/modules/react/alshaya_online_returns/js/pdp/components/eligible-for-return/index.js
+++ docroot/modules/react/alshaya_online_returns/js/pdp/components/eligible-for-return/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const EligibleForReturn = () => (
-  <span>
+  <span className="return-eligibility-pdp">
     {Drupal.t('Free online returns within 14 days', {}, { context: 'online_returns' })}
   </span>
 );
