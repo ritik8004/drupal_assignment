3eeded17af5f39ccf25a66144cfb5015b203d014
Hammad Ghani <mohamed.ghani@alshaya.com>
2022-08-25 09:15:51 +0000

    CORE-43742: PR review comments.

--- docroot/modules/react/alshaya_hello_member/js/src/become-hello-member.js
+++ docroot/modules/react/alshaya_hello_member/js/src/become-hello-member.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import BecomeHelloMember from '../../../js/utilities/components/become-hello-member';
 
-Drupal.behaviors.alshayaHelloMemberBecomeHelloMember = {
-  attach: function alshayaHelloMemberBecomeHelloMember() {
+Drupal.behaviors.alshayaHelloMemberMyPointsHistoryBehavior = {
+  attach: function alshayaHelloMemberMyPointsHistory() {
     const querySelector = document.querySelector('#hello-member-become-hello-member-block');
     if (querySelector) {
-      jQuery('#hello-member-become-hello-member-block').once('init-react').each(() => {
-        ReactDOM.render(
-          <BecomeHelloMember />,
-          querySelector,
-        );
-      });
+      ReactDOM.render(
+        <BecomeHelloMember />,
+        querySelector,
+      );
     }
   },
 };
