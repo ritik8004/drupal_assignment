aaaaf52ca79a73d3c4d6841ebc0546777a0fa4c8
Kunal Singh <skunal185@gmail.com>
2022-08-25 15:32:34 +0530

    CORE-43796: Added the try catch block for JS redirect.

--- docroot/modules/custom/alshaya_i18n/src/EventSubscriber/AlshayaI18nRequestSubscriber.php
+++ docroot/modules/custom/alshaya_i18n/src/EventSubscriber/AlshayaI18nRequestSubscriber.php
@@ -86,10 +86,15 @@ class AlshayaI18nRequestSubscriber implements EventSubscriberInterface {
           var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
           if (match) return match[2];
         }
-        const languages = $languages;
+        var languages = $languages;
+        var default_lang = '" . $this->config->get('default_langcode') . "';
         var preferred_lang = window.getCookie('alshaya_lang');
-        if (!languages.includes(preferred_lang)) {
-          preferred_lang = '" . $this->config->get('default_langcode') . "';
+        try {
+          if (!languages.includes(preferred_lang)) {
+            preferred_lang = default_lang;
+          }
+        } catch (err) {
+          preferred_lang = default_lang;
         }
         window.location = '/' + preferred_lang + '/';";
 
