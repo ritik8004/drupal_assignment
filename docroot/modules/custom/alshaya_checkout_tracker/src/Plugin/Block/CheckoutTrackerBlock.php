44e49cabffed97675fe31285231dbe2a16f218d6
harshita <harshita.shukla@alshaya.com>
2022-09-02 11:26:01 +0000

    CORE-44605: Implementation Approach for checkout progress tracker.

--- docroot/modules/custom/alshaya_checkout_tracker/src/Plugin/Block/CheckoutTrackerBlock.php
+++ docroot/modules/custom/alshaya_checkout_tracker/src/Plugin/Block/CheckoutTrackerBlock.php
@@ -3,7 +3,7 @@
 namespace Drupal\alshaya_checkout_tracker\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Url;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -17,6 +17,13 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * )
  */
 class CheckoutTrackerBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
 
   /**
    * Stores the configuration factory.
@@ -33,12 +40,15 @@ class CheckoutTrackerBlock extends BlockBase implements ContainerFactoryPluginIn
    * @param string $plugin_id
    *   The plugin_id for the plugin instance.
    * @param mixed $plugin_definition
-   *   The plugin implementation definition.
+   *   Private temp store service.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The current route match.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The factory for configuration objects.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteMatchInterface $route_match, ConfigFactoryInterface $config_factory) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->routeMatch = $route_match;
     $this->configFactory = $config_factory;
   }
 
@@ -50,6 +60,7 @@ class CheckoutTrackerBlock extends BlockBase implements ContainerFactoryPluginIn
       $configuration,
       $plugin_id,
       $plugin_definition,
+      $container->get('current_route_match'),
       $container->get('config.factory')
     );
   }
@@ -58,22 +69,44 @@ class CheckoutTrackerBlock extends BlockBase implements ContainerFactoryPluginIn
    * Implements \Drupal\block\BlockBase::build().
    */
   public function build() {
+    $route_name = $this->routeMatch->getRouteName();
     $bag = NULL;
+    $signin = NULL;
     $checkout = NULL;
-    $current_path = Url::fromRoute('<current>');
-    if (str_contains($current_path, '/cart')) {
+    $confirmation = NULL;
+
+    if ($route_name == 'acq_cart.cart') {
+      $bag = 'current';
+      $signin = '';
       $checkout = '';
-      $bag = 'completed';
+      $confirmation = '';
     }
-    if (str_contains($current_path, '/checkout')) {
+    if ($route_name == 'alshaya_spc.checkout.login') {
+      $bag = 'completed';
+      $signin = 'current';
+      $checkout = '';
+      $confirmation = '';
+    }
+    if ($route_name == 'alshaya_spc.checkout') {
+      $bag = 'completed';
+      $signin = 'completed';
+      $checkout = 'current';
+      $confirmation = '';
+    }
+    if ($route_name == 'alshaya_spc.checkout.confirmation') {
+      $bag = 'completed';
+      $signin = 'completed';
       $checkout = 'completed';
-      $bag = 'completed';
+      $confirmation = 'current';
     }
+
     return [
       '#theme' => 'checkout_tracker_block',
       '#steps' => [
         'bagclass' => $bag,
+        'signinclass' => $signin,
         'checkoutclass' => $checkout,
+        'deliveryclass' => $confirmation,
       ],
     ];
   }
