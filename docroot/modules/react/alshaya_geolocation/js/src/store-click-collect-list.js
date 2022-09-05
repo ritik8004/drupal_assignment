1f685adc2c2c61ffb720b543ffc0e743410f7e55
subhadeep-bhandari-acquia <subhadeep.bhandari@acquia.com>
2022-09-05 13:41:49 +0530

    CORE-43724: Implementation to disable click and collect.

--- docroot/modules/react/alshaya_geolocation/js/src/store-click-collect-list.js
+++ docroot/modules/react/alshaya_geolocation/js/src/store-click-collect-list.js
@@ -4,17 +4,23 @@ import { StoreClickCollectList } from './alshaya-store-click-collect-list';
 
 let componentRendered = false;
 
-Drupal.behaviors.alshayaGeolocationCncRcsBehavior = {
-  attach: function alshayaGeolocationCnCRcs() {
-    const node = jQuery('.entity--type-node').not('[data-sku *= "#"]');
-    if (!node.length || (node.length && componentRendered)) {
-      return;
-    }
+(function storeClickCollectListMain(drupalSettings) {
+  Drupal.behaviors.alshayaGeolocationCncRcsBehavior = {
+    attach: function alshayaGeolocationCnCRcs() {
+      if (drupalSettings.storeLabels.state !== 'enabled') {
+        return;
+      }
 
-    componentRendered = true;
-    ReactDOM.render(
-      <StoreClickCollectList />,
-      document.getElementById('pdp-store-click-collect-list'),
-    );
-  },
-};
+      const node = jQuery('.entity--type-node').not('[data-sku *= "#"]');
+      if (!node.length || (node.length && componentRendered)) {
+        return;
+      }
+      componentRendered = true;
+      // Click & Collect section will render only if it is enabled.
+      ReactDOM.render(
+        <StoreClickCollectList />,
+        document.getElementById('pdp-store-click-collect-list'),
+      );
+    },
+  };
+}(drupalSettings));
