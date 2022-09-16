1f6b4f7c1ffac7caa17e7946d0ee603155a0bce4
koustubh dudhe <dudhe.koustubh@gmail.com>
2022-09-16 10:37:19 +0530

    CORE-42678: Store authenticated user's cart id in localstorage temporarily.

--- docroot/modules/react/alshaya_spc/js/backend/v2/common.js
+++ docroot/modules/react/alshaya_spc/js/backend/v2/common.js
@@ -124,6 +124,8 @@ window.commerceBackend.setCartDataInStorage = (data) => {
     cartInfo,
     parseInt(drupalSettings.alshaya_spc.cart_storage_expiration, 10) * 60,
   );
+  // Remove 'user_cart_id' as cart_data localstorage is available.
+  Drupal.removeItemFromLocalStorage('user_cart_id');
 };
 
 /**
@@ -759,7 +761,16 @@ const associateCartToCustomer = async (guestCartId) => {
   StaticStorage.clear();
 
   // Reload cart.
-  await getCart(true);
+  const cartData = await getCart(true);
+  if (hasValue(cartData)
+    && hasValue(cartData.data)
+    && hasValue(cartData.data.cart)
+    && hasValue(cartData.data.cart.id)
+    && isUserAuthenticated()
+  ) {
+    // store authenticated user cart id in 'user_cart_id';
+    Drupal.addItemInLocalStorage('user_cart_id', cartData.data.cart.id);
+  }
 };
 
 /**
