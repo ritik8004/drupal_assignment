8d1e9601b14fb577be0b48866fc3e3b78d4f0bb5
vivekpan27 <vivek.panicker@innoraft.com>
2022-09-19 14:49:50 +0530

    CORE-42472: Reverted code for rendering delivery labels.

--- docroot/modules/react/alshaya_spc/js/pdp_sdd_ed_labels.js
+++ docroot/modules/react/alshaya_spc/js/pdp_sdd_ed_labels.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PdpSddEd from '../../js/utilities/components/pdp-sdd-ed';
-import { hasValue } from '../../js/utilities/conditionsUtility';
 
 /**
  * Show PDP labels on default and magazine layout.
@@ -25,12 +24,7 @@ const renderPdpDeliveryLabels = () => {
 // Set pdp labels for mobile view on magazine layout
 // after product zoom gallery is loaded in order
 // to have the wrapper loaded in dom.
-if (hasValue(drupalSettings.alshayaRcs)) {
-  document.addEventListener('onSkuBaseFormLoad', renderPdpDeliveryLabels, false);
-} else {
-  document.addEventListener('productGalleryLoaded', renderPdpDeliveryLabels, false);
-}
-
+document.addEventListener('productGalleryLoaded', renderPdpDeliveryLabels, false);
 
 // Set pdp labels on load for default layout.
 renderPdpDeliveryLabels();
