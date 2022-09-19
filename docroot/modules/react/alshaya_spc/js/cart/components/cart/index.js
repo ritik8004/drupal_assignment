68bee92a9f2e5eb74594d61bebfe81ae9499f8d5
Anurag Sharma <anuragsharmam@3197917.no-reply.drupal.org>
2022-09-19 10:39:19 +0530

    CORE-44989: Updated the file name of component for better read.

--- docroot/modules/react/alshaya_spc/js/cart/components/cart/index.js
+++ docroot/modules/react/alshaya_spc/js/cart/components/cart/index.js
@@ -43,7 +43,7 @@ import { isUserAuthenticated } from '../../../backend/v2/utility';
 import { applyHelloMemberLoyalty } from '../../../hello-member-loyalty/components/hello-member-checkout-rewards/utilities/loyalty_helper';
 import { isOnlineReturnsCartBannerEnabled } from '../../../../../js/utilities/onlineReturnsHelper';
 import OnlineReturnsCartBanner from '../../../../../alshaya_online_returns/js/cart/online-returns-cart-banner';
-import PaymentMethodsLogos from '../payment-methods';
+import PaymentMethodsLogos from '../payment-methods-logos';
 
 export default class Cart extends React.Component {
   constructor(props) {
