81e2fb834b46938cab3ffd7281b2c50560222608
Avinash <avinash.shukla@acquia.com>
2022-09-20 10:01:03 +0530

    CORE-45615: Fixed static call and private method in cod plugin.

--- docroot/modules/react/alshaya_spc/src/Plugin/SpcPaymentMethod/CashOnDelivery.php
+++ docroot/modules/react/alshaya_spc/src/Plugin/SpcPaymentMethod/CashOnDelivery.php
@@ -3,9 +3,6 @@
 namespace Drupal\alshaya_spc\Plugin\SpcPaymentMethod;
 
 use Drupal\alshaya_spc\AlshayaSpcPaymentMethodPluginBase;
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * COD payment method for SPC.
@@ -16,49 +13,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   hasForm = false
  * )
  */
-class CashOnDelivery extends AlshayaSpcPaymentMethodPluginBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * Config factory.
-   *
-   * @var \Drupal\Core\Config\ConfigFactoryInterface
-   */
-  protected ConfigFactoryInterface $configFactory;
-
-  /**
-   * CashOnDelivery constructor.
-   *
-   * @param array $configuration
-   *   Plugin configuration array.
-   * @param string $plugin_id
-   *   Plugin id.
-   * @param mixed $plugin_definition
-   *   Plugin definition.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   Configuration factory.
-   */
-  public function __construct(array $configuration,
-                              $plugin_id,
-                              $plugin_definition,
-                              ConfigFactoryInterface $config_factory) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->configFactory = $config_factory;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container,
-                                array $configuration,
-                                $plugin_id,
-                                $plugin_definition): static {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('config.factory')
-    );
-  }
+class CashOnDelivery extends AlshayaSpcPaymentMethodPluginBase {
 
   /**
    * {@inheritdoc}
@@ -67,7 +22,7 @@ class CashOnDelivery extends AlshayaSpcPaymentMethodPluginBase implements Contai
     $build['#strings'] = array_merge($build['#strings'], self::getCodSurchargeStrings());
 
     // Get COD payment method mobile verification settings.
-    $cod_mobile_verification = self::getCodMobileVerificationSettings();
+    $cod_mobile_verification = $this->getCodMobileVerificationSettings();
     if ($cod_mobile_verification) {
       $build['#attached']['drupalSettings']['codMobileVerification'] = $cod_mobile_verification;
       $build['#attached']['library'][] = 'alshaya_white_label/checkout-cod-mobile-verification';
@@ -108,8 +63,8 @@ class CashOnDelivery extends AlshayaSpcPaymentMethodPluginBase implements Contai
    * @return bool
    *   Configuration value.
    */
-  public function getCodMobileVerificationSettings(): bool {
-    return $this->configFactory->get('alshaya_spc.settings')->get('cod_mobile_verification');
+  private function getCodMobileVerificationSettings(): bool {
+    return \Drupal::config('alshaya_spc.settings')->get('cod_mobile_verification');
   }
 
 }
