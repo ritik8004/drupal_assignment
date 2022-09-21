bbba5e881ec9d0c46aeec2a28cda862c4103a703
Anurag Sharma <anuragsharmam@3197917.no-reply.drupal.org>
2022-09-21 16:40:18 +0530

    CORE-45760: Added cache tags for config and removed unused code.

--- docroot/modules/custom/alshaya_tamara/js/tamara_checkout.js
+++ docroot/modules/custom/alshaya_tamara/js/tamara_checkout.js
@@ -5,8 +5,6 @@
     window.TamaraInstallmentPlan.init({
       lang: drupalSettings.path.currentLanguage,
       currency: drupalSettings.alshaya_spc.currency_config.currency_code,
-      // @todo: need to check with public key.
-      // publicKey: drupalSettings.tamara.publicKey,
     });
 
     // Render the installment plan widget.
