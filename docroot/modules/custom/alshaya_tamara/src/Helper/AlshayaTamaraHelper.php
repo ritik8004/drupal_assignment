13165f65401d1bff850b612253baa9e55cb1c788
Anurag Sharma <anuragsharmam@3197917.no-reply.drupal.org>
2022-09-23 13:07:19 +0530

    CORE-45760: Fix the PR feedbacks.

--- docroot/modules/custom/alshaya_tamara/src/Helper/AlshayaTamaraHelper.php
+++ docroot/modules/custom/alshaya_tamara/src/Helper/AlshayaTamaraHelper.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\alshaya_tamara\Helper;
 
-use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Config\ConfigFactoryInterface;
 
 /**
@@ -31,18 +30,11 @@ class AlshayaTamaraHelper {
 
   /**
    * Check if the Tamara payment method is enabled on Drupal (not excluded).
-   *
-   * @param array $build
-   *   Build.
    */
-  public function isTamaraEnabled(array &$build) {
+  public function isTamaraEnabled() {
     // Return FALSE if tamara payment method is excluded from the Checkout page.
     $config = $this->configFactory->get('alshaya_acm_checkout.settings');
     $excludedPaymentMethods = $config->get('exclude_payment_methods');
-
-    CacheableMetadata::createFromRenderArray($build)
-      ->addCacheableDependency($config)
-      ->applyTo($build);
 
     if (in_array('tamara', array_filter($excludedPaymentMethods))) {
       return FALSE;
