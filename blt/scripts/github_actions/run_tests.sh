31e8b949114606f7f0e15f7bcb1d0176f6e9f48a
joshirohit100 <joshi.rohit100@gmail.com>
2022-09-30 14:00:02 +0530

    CORE-46347: Not run drupal install all time.

--- blt/scripts/github_actions/run_tests.sh
+++ blt/scripts/github_actions/run_tests.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+# Get all changed files.
+CHANGED_FILES="$1"
+
+# Test drupal installation only when a new module is added or
+# change in config files or
+# install hook is added (change in install file) or
+# change in services.yml as these could impact the installation process
+INSTALL_DRUPAL="NO";
+if [[ ${CHANGED_FILES} =~ ".info.yml" || ${CHANGED_FILES} =~ ".services.yml" || ${CHANGED_FILES} =~ "config/install" || ${CHANGED_FILES} =~ "config/optional" || ${CHANGED_FILES} =~ ".install" ]]
+then
+  INSTALL_DRUPAL="YES"
+fi
+
 # Run tests.
 blt validate:composer
 blt validate:php
@@ -9,6 +22,14 @@ blt validate:phpcs
 blt validate:yaml
 blt validate:twig
 blt validate:acsf
+
+if [ ${INSTALL_DRUPAL} == "NO" ]
+then
+   echo "Drupal installation is skipped as not required..."
+   exit;
+fi
+
+echo "Installing Drupal..."
 
 # Setup site.
 blt setup:settings --define environment=ci --no-interaction --verbose
