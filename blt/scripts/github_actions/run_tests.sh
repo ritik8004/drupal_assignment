432b605bdb9209496623b670df9ed81edbccab04
joshirohit100 <joshi.rohit100@gmail.com>
2022-09-30 14:46:17 +0530

    CORE-46347: Adding checks for composer / twig etc as well.

--- blt/scripts/github_actions/run_tests.sh
+++ blt/scripts/github_actions/run_tests.sh
@@ -5,41 +5,61 @@ set -e
 # Get all changed files.
 CHANGED_FILES="$1"
 
-# Test drupal installation only when a new module is added or
-# change in config files or
-# install hook is added (change in install file) or
-# change in services.yml as these could impact the installation process
-INSTALL_DRUPAL="NO";
-if [[ ${CHANGED_FILES} =~ ".info.yml" || ${CHANGED_FILES} =~ ".services.yml" || ${CHANGED_FILES} =~ "config/install" || ${CHANGED_FILES} =~ "config/optional" || ${CHANGED_FILES} =~ ".install" ]]
+# Run tests.
+
+# Composer check
+if [[ ${CHANGED_FILES} =~ "composer.lock" || ${CHANGED_FILES} =~ "composer.json" ]]
 then
-  INSTALL_DRUPAL="YES"
+  blt validate:composer
+else
+  echo "Skipping validate:composer checks as there is no change in Composer."
 fi
 
-# Run tests.
-blt validate:composer
-blt validate:php
-blt validate:phpcs
-blt validate:yaml
-blt validate:twig
+# PHP check
+if [[ ${CHANGED_FILES} =~ ".php" || ${CHANGED_FILES} =~ ".theme" || ${CHANGED_FILES} =~ ".module" || ${CHANGED_FILES} =~ ".inc" || ${CHANGED_FILES} =~ ".install"]]
+then
+  blt validate:php
+  blt validate:phpcs
+else
+  echo "Skipping validate:php checks as there is no change in PHP files."
+fi
+
+# Yaml check
+if [[ ${CHANGED_FILES} =~ ".yml" || ${CHANGED_FILES} =~ ".yaml" ]]
+then
+  blt validate:yaml
+else
+  echo "Skipping validate:yaml checks as there is no change in YAML files."
+fi
+
+# Twig check
+if [[ ${CHANGED_FILES} =~ ".twig" ]]
+then
+  blt validate:twig
+else
+  echo "Skipping validate:twig checks as there is no change in Twig files."
+fi
+
 blt validate:acsf
 
-if [ ${INSTALL_DRUPAL} == "NO" ]
+# Drupal installtion check
+if [[ ${CHANGED_FILES} =~ ".info.yml" || ${CHANGED_FILES} =~ ".services.yml" || ${CHANGED_FILES} =~ "config/install" || ${CHANGED_FILES} =~ "config/optional" || ${CHANGED_FILES} =~ ".install" ]]
 then
-   echo "Drupal installation is skipped as not required..."
-   exit;
+  # Setup site.
+  blt setup:settings --define environment=ci --no-interaction --verbose
+  blt setup:hash-salt --define environment=ci --no-interaction --verbose
+  blt setup:drupal:install --define environment=ci --no-interaction --verbose
+
+  cd "$GITHUB_WORKSPACE/docroot"
+
+  # Setup brand.
+  drush @self --uri=default alshaya-post-drupal-install --brand_module="alshaya_vs" --country_code="ae"
+
+  drush status
+  drush pml
+
+else
+  echo "Drupal installation is skipped as not required..."
 fi
 
-echo "Installing Drupal..."
-
-# Setup site.
-blt setup:settings --define environment=ci --no-interaction --verbose
-blt setup:hash-salt --define environment=ci --no-interaction --verbose
-blt setup:drupal:install --define environment=ci --no-interaction --verbose
-
-cd "$GITHUB_WORKSPACE/docroot"
-
-# Setup brand.
-drush @self --uri=default alshaya-post-drupal-install --brand_module="alshaya_vs" --country_code="ae"
-
-drush status
-drush pml
+exit;
