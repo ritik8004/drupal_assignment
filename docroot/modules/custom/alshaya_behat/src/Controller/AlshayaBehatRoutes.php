90c311a8246c68fd882bbff36c8a294af55ddbf1
vivekpan27 <vivek.panicker@innoraft.com>
2022-09-30 14:22:35 +0530

    CORE-45762: Created and used utility function to get the working node.

--- docroot/modules/custom/alshaya_behat/src/Controller/AlshayaBehatRoutes.php
+++ docroot/modules/custom/alshaya_behat/src/Controller/AlshayaBehatRoutes.php
@@ -2,14 +2,12 @@
 
 namespace Drupal\alshaya_behat\Controller;
 
-use Drupal\acq_sku\Entity\SKU;
-use Drupal\acq_sku\StockManager;
-use Drupal\alshaya_acm_product\SkuManager;
 use Drupal\alshaya_behat\Service\AlshayaBehatHelper;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Http\RequestStack;
 use Drupal\Core\Site\Settings;
+use Drupal\node\NodeInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
@@ -59,8 +57,6 @@ class AlshayaBehatRoutes extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('acq_sku.stock_manager'),
-      $container->get('alshaya_acm_product.skumanager'),
       $container->get('request_stack'),
       $container->get('alshaya_behat.helper')
     );
@@ -69,23 +65,15 @@ class AlshayaBehatRoutes extends ControllerBase {
   /**
    * Constructor for AlshayaBehatRoutes.
    *
-   * @param \Drupal\acq_sku\StockManager $stock_manager
-   *   Stock manager.
-   * @param \Drupal\alshaya_acm_product\SkuManager $sku_manager
-   *   Sku manager.
    * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
    *   Request stack.
    * @param \Drupal\alshaya_behat\Service\AlshayaBehatHelper $alshaya_behat
    *   Alshaya behat.
    */
   public function __construct(
-    StockManager $stock_manager,
-    SkuManager $sku_manager,
     RequestStack $request_stack,
     AlshayaBehatHelper $alshaya_behat
   ) {
-    $this->stockManager = $stock_manager;
-    $this->skuManager = $sku_manager;
     $this->request = $request_stack->getCurrentRequest();
     $this->alshayaBehat = $alshaya_behat;
   }
@@ -114,50 +102,10 @@ class AlshayaBehatRoutes extends ControllerBase {
    * @throws \Symfony\Component\HttpKernel\Exception\BadRequestHttpException
    */
   public function firstInStockProduct() {
-    $page = 0;
-
-    while (TRUE) {
-      // Query the database to fetch in-stock products.
-      $skus = $this->alshayaBehat->getSkus($page, self::SKUS_LIMIT);
-      if (empty($skus)) {
-        break;
-      }
-
-      foreach ($skus as $sku) {
-        // Load the SKU.
-        $main_sku = SKU::loadFromSku($sku);
-        if ($this->stockManager->isProductInStock($main_sku)) {
-          // SKU might be configurable. So fetch the parent.
-          $parent_sku = $this->skuManager->getParentSkuBySku($main_sku);
-          if ($parent_sku) {
-            if ($this->stockManager->isProductInStock($parent_sku)) {
-              $main_sku = $parent_sku;
-            }
-            else {
-              continue;
-            }
-          }
-        }
-        else {
-          continue;
-        }
-
-        // Fetch the display node for the SKU.
-        $node = $this->skuManager->getDisplayNode($main_sku);
-        if (is_null($node)) {
-          continue;
-        }
-        // Request the node and check if there is any error when loading the
-        // node.
-        // If there is an error we check the next sku.
-        if (!$this->alshayaBehat->isNodePageLoading($node)) {
-          continue;
-        }
-        // Redirect to the node page.
-        return new RedirectResponse($node->toUrl()->toString());
-      }
-
-      $page++;
+    $node = $this->alshayaBehat->getWorkingProduct();
+    if ($node instanceof NodeInterface) {
+      // Redirect to the node page.
+      return new RedirectResponse($node->toUrl()->toString());
     }
 
     // If no SKU is found which is in stock, then redirect to 400 page.
@@ -173,50 +121,10 @@ class AlshayaBehatRoutes extends ControllerBase {
    * @throws \Symfony\Component\HttpKernel\Exception\BadRequestHttpException
    */
   public function firstOosProduct() {
-    $page = 0;
-
-    while (TRUE) {
-      // Query the database to fetch out of stock products.
-      $skus = $this->alshayaBehat->getSkus($page, self::SKUS_LIMIT, TRUE);
-      if (empty($skus)) {
-        break;
-      }
-
-      foreach ($skus as $sku) {
-        // Load the sku.
-        $main_sku = SKU::loadFromSku($sku);
-        if (!$this->stockManager->isProductInStock($main_sku)) {
-          // SKU might be configurable. So fetch the parent.
-          $parent_sku = $this->skuManager->getParentSkuBySku($main_sku);
-          if ($parent_sku) {
-            if (!$this->stockManager->isProductInStock($parent_sku)) {
-              $main_sku = $parent_sku;
-            }
-            else {
-              continue;
-            }
-          }
-        }
-        else {
-          continue;
-        }
-
-        // Fetch the display node for the sku.
-        $node = $this->skuManager->getDisplayNode($main_sku);
-        if (is_null($node)) {
-          continue;
-        }
-        // Request the node and check if there is any error when loading the
-        // node.
-        // If there is an error we check the next sku.
-        if (!$this->alshayaBehat->isNodePageLoading($node)) {
-          continue;
-        }
-        // Redirect to the node page.
-        return new RedirectResponse($node->toUrl()->toString());
-      }
-
-      $page++;
+    $node = $this->alshayaBehat->getWorkingProduct(TRUE);
+    if ($node instanceof NodeInterface) {
+      // Redirect to the node page.
+      return new RedirectResponse($node->toUrl()->toString());
     }
 
     // If no SKU is found which is OOS, then redirect to 400 page.
--- docroot/modules/custom/alshaya_behat/src/Service/AlshayaBehatHelper.php
+++ docroot/modules/custom/alshaya_behat/src/Service/AlshayaBehatHelper.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\alshaya_behat\Service;
 
+use Drupal\acq_sku\Entity\SKU;
+use Drupal\acq_sku\StockManager;
+use Drupal\alshaya_acm_product\SkuManager;
 use Drupal\Core\Database\Connection;
 use Drupal\node\NodeInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,19 +31,55 @@ class AlshayaBehatHelper {
   protected $httpKernel;
 
   /**
+   * Stock Manager.
+   *
+   * @var \Drupal\acq_sku\StockManager
+   */
+  private $stockManager;
+
+  /**
+   * Sku manager.
+   *
+   * @var \Drupal\alshaya_acm_product\SkuManager
+   */
+  private $skuManager;
+
+  /**
+   * Request.
+   *
+   * @var \Symfony\Component\HttpFoundation\Request
+   */
+  protected $request;
+
+  /**
+   * Number of skus to fetch.
+   *
+   * @var int
+   */
+  protected const SKUS_LIMIT = 10;
+
+  /**
    * Constructor for Handlebars Service.
    *
    * @param \Drupal\Core\Database\Connection $connection
    *   Connection.
    * @param \Symfony\Component\HttpKernel\HttpKernel $http_kernel
    *   Http kernel.
+   * @param \Drupal\acq_sku\StockManager $stock_manager
+   *   Stock manager.
+   * @param \Drupal\alshaya_acm_product\SkuManager $sku_manager
+   *   Sku manager.
    */
   public function __construct(
     Connection $connection,
-    HttpKernel $http_kernel
+    HttpKernel $http_kernel,
+    StockManager $stock_manager,
+    SkuManager $sku_manager,
   ) {
     $this->database = $connection;
     $this->httpKernel = $http_kernel;
+    $this->stockManager = $stock_manager;
+    $this->skuManager = $sku_manager;
   }
 
   /**
@@ -52,7 +91,7 @@ class AlshayaBehatHelper {
    * @return bool
    *   TRUE if node loads successfully else false.
    */
-  public function isNodePageLoading(NodeInterface $node) {
+  private function isNodePageLoading(NodeInterface $node) {
     $request = Request::create('/node/' . $node->id());
     $request_success = TRUE;
 
@@ -79,7 +118,7 @@ class AlshayaBehatHelper {
    * @return array
    *   Array of SKU values.
    */
-  public function getSkus($page, $limit, $oos = FALSE) {
+  private function getSkus($page, $limit, $oos = FALSE) {
     // Query the database to fetch in-stock products.
     $query = $this->database->select('node__field_skus', 'nfs');
     $query->leftJoin('acq_sku_stock', 'stock', 'stock.sku = nfs.field_skus_value');
@@ -95,6 +134,66 @@ class AlshayaBehatHelper {
     $query->range($page * $limit, $limit);
 
     return $query->distinct()->execute()->fetchCol();
+  }
+
+  /**
+   * Get a working OOS/In stock product.
+   *
+   * @param bool $oos
+   *   If OOS product is required, set this to TRUE.
+   *
+   * @return \Drupal\node\NodeInterface|null
+   *   The node object else null.
+   */
+  public function getWorkingProduct($oos = FALSE) {
+    $page = 0;
+    while (TRUE) {
+      // Query the database to fetch in-stock products.
+      $skus = $this->getSkus($page, self::SKUS_LIMIT, $oos);
+      if (empty($skus)) {
+        break;
+      }
+
+      foreach ($skus as $sku) {
+        // Load the SKU.
+        $main_sku = SKU::loadFromSku($sku);
+        $is_product_in_stock = $this->stockManager->isProductInStock($main_sku);
+        $condition = $oos === FALSE ? $is_product_in_stock : !$is_product_in_stock;
+        if ($condition) {
+          // SKU might be configurable. So fetch the parent.
+          $parent_sku = $this->skuManager->getParentSkuBySku($main_sku);
+          if ($parent_sku) {
+            $is_product_in_stock = $this->stockManager->isProductInStock($parent_sku);
+            $condition = $oos === FALSE ? $is_product_in_stock : !$is_product_in_stock;
+
+            if ($condition) {
+              $main_sku = $parent_sku;
+            }
+            else {
+              continue;
+            }
+          }
+        }
+        else {
+          continue;
+        }
+
+        // Fetch the display node for the SKU.
+        $node = $this->skuManager->getDisplayNode($main_sku);
+        if (is_null($node)) {
+          continue;
+        }
+        // Request the node and check if there is any error when loading the
+        // node.
+        // If there is an error we check the next sku.
+        if (!$this->isNodePageLoading($node)) {
+          continue;
+        }
+        return $node;
+      }
+
+      $page++;
+    }
   }
 
 }
