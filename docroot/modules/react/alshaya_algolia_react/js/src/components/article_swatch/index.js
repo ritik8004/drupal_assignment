aadaded2bda699b46d6a96fb92d6acd9fa87ad5f
Mohammad Rashid <mohammad.rashid@alshaya.com>
2022-10-02 21:55:13 +0300

    CORE-46389: Add component to display swatches for color skus in listing pages.

---
+++ docroot/modules/react/alshaya_algolia_react/js/src/components/article_swatch/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const ArticleSwatches = ({ articleSwatches, url }) => {
+  if (typeof articleSwatches === 'undefined') {
+    return null;
+  }
+
+  const limit = drupalSettings.reactTeaserView.swatches.swatchPlpLimit;
+  const totalNoOfSwatches = articleSwatches.length;
+  const diff = totalNoOfSwatches - limit;
+  let swatchMoreText = '';
+
+  if (diff > 0) {
+    swatchMoreText = ` +${diff}`;
+  }
+
+  return (
+    <div className="article-swatch-wrapper">
+      <div className="swatches">
+        {articleSwatches.map(
+          (swatch) => (
+            <button
+              type="button"
+              className="article-swatch"
+              key={swatch.article_sku_code}
+              style={{ backgroundColor: swatch.rgb_color }}
+            />
+          ),
+        )}
+        <a className="more-color-swatch" href={url}>{swatchMoreText}</a>
+      </div>
+    </div>
+  );
+};
+
+export default ArticleSwatches;
