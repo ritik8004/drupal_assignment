d63a916a21c3bfa9bfa67cc9a66e6d291e8cbd62
Marcelo Vani <marcelo.vani@acquia.com>
2022-10-05 18:37:29 +0100

    CORE-45188: Added code and test for loader.

--- docroot/modules/react/alshaya_spc/js/utilities/checkout_util.js
+++ docroot/modules/react/alshaya_spc/js/utilities/checkout_util.js
@@ -82,29 +82,80 @@ export const controlPlaceOrderCTA = (status) => {
   }
 };
 
+const fullLoaderClasses = ['ajax-progress', 'fullscreen-loader'];
+
 /**
  * Place ajax full screen loader.
+ * @todo Remove duplicated code that is already implemented
+ * @see docroot/modules/react/js/utilities/showRemoveFullScreenLoader.js
+ *
+ * @param {string} contextClass
+ *   The loader contextual class.
  */
-export const showFullScreenLoader = () => {
-  const loaderDivExisting = document.getElementsByClassName('ajax-progress-fullscreen');
-  if (loaderDivExisting.length > 0) {
+export const showFullScreenLoader = (contextualClass = '') => {
+  let classes = fullLoaderClasses;
+  let loaderDiv = document.querySelector('.fullscreen-loader');
+  if (typeof contextualClass === 'string' && contextualClass !== '') {
+    if (loaderDiv) {
+      if (loaderDiv.classList.contains(contextualClass)) {
+        // Loader already contains this contextual class.
+        return;
+      }
+      // Add contextual class to existing loader.
+      loaderDiv.classList.add(contextualClass);
+    } else {
+      // Append contextual class to the list of classes.
+      classes = fullLoaderClasses.concat([contextualClass]);
+    }
+  } else if (loaderDiv) {
+    // Loader already loaded.
     return;
   }
 
   controlAddressFormCTA('disable');
-  const loaderDiv = document.createElement('div');
-  loaderDiv.className = 'ajax-progress ajax-progress-fullscreen';
+
+  // Create a div with the list of classes.
+  loaderDiv = document.createElement('div');
+  loaderDiv.className = classes.join(' ');
   document.body.appendChild(loaderDiv);
 };
 
 /**
  * Remove ajax loader.
+ * @todo Remove duplicated code that is already implemented
+ * @see docroot/modules/react/js/utilities/removeFullScreenLoader.js
+ *
+ * @param {string} context
+ *   The loader context.
  */
-export const removeFullScreenLoader = () => {
-  const loaderDiv = document.getElementsByClassName('ajax-progress-fullscreen');
-  while (loaderDiv.length > 0) {
-    loaderDiv[0].parentNode.removeChild(loaderDiv[0]);
+export const removeFullScreenLoader = (contextualClass = '') => {
+  let loaderDiv = null;
+  if (typeof contextualClass === 'string' && contextualClass !== '') {
+    // Check if there is a loader with the contextual class.
+    loaderDiv = document.querySelector(`.${contextualClass}`);
+    if (loaderDiv) {
+      // Remove the contextual class.
+      loaderDiv.classList.remove(contextualClass);
+    }
+  } else {
+    // Populate loader div.
+    loaderDiv = document.querySelector('.fullscreen-loader');
   }
+
+  // Loader div is not on the page.
+  if (!loaderDiv) {
+    return;
+  }
+
+  // Check if there are still contextual classes.
+  if (loaderDiv.classList.length > fullLoaderClasses.length) {
+    // There are still contextual classes to be removed before we can delete it.
+    return;
+  }
+
+  // Remove loader completely.
+  loaderDiv.remove();
+
   controlAddressFormCTA('enable');
 };
 
