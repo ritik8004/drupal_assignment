dbf260f8b2f79efbd2acd40027680e67a4166284
Anurag Sharma <anuragsharmam@3197917.no-reply.drupal.org>
2022-10-07 11:56:10 +0530

    CORE-46586: Add sucess and fail urls if payment method is tamara.

--- docroot/modules/react/alshaya_spc/js/backend/v2/checkout.js
+++ docroot/modules/react/alshaya_spc/js/backend/v2/checkout.js
@@ -1381,6 +1381,17 @@ const isPostpayPaymentMethod = (paymentMethod) => paymentMethod.indexOf('postpay
 const isTabbyPaymentMethod = (paymentMethod) => paymentMethod.indexOf('tabby', 0) !== -1;
 
 /**
+ * Checks if tamara payment method.
+ *
+ * @param {string} paymentMethod
+ *   Payment method code.
+ *
+ * @return {bool}
+ *   TRUE if payment methods from tamara
+ */
+const isTamaraPaymentMethod = (paymentMethod) => paymentMethod.indexOf('tamara', 0) !== -1;
+
+/**
  * Prepare message to log when API fail after payment successful.
  *
  * @param {array} cart
@@ -1579,7 +1590,8 @@ const paymentUpdate = async (data) => {
   // If upapi payment method (payment method via checkout.com).
   if (isUpapiPaymentMethod(paymentData.method)
     || isPostpayPaymentMethod(paymentData.method)
-    || isTabbyPaymentMethod(paymentData.method)) {
+    || isTabbyPaymentMethod(paymentData.method)
+    || isTamaraPaymentMethod(paymentData.method)) {
     // Add success and fail redirect url to additional data.
     params.payment.additional_data.successUrl = `${window.location.origin}${Drupal.url('spc/payment-callback/success')}`;
     params.payment.additional_data.failUrl = `${window.location.origin}${Drupal.url(`spc/payment-callback/${paymentData.method}/error`)}`;
