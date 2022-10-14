bc0d7e45a0d4ebe38d4a12c7f600cadecc056369
vivekpan27 <vivek.panicker@innoraft.com>
2022-10-14 16:12:41 +0530

    CORE-47071: Added services for V2 and V3 Department page.

---
+++ docroot/modules/custom/alshaya_advanced_page/src/Service/AlshayaDepartmentPageHelper.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\alshaya_advanced_page\Service;
+
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\node\Entity\Node;
+
+/**
+ * Helper class for Department pages.
+ */
+class AlshayaDepartmentPageHelper {
+
+  /**
+   * Database.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * Route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Constructor for AlshayaDepartmentPageHelper.
+   *
+   * @param \Drupal\Core\Database\Connection $connection
+   *   Database connection.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   Route match.
+   */
+  public function __construct(
+    Connection $connection,
+    RouteMatchInterface $route_match
+  ) {
+    $this->database = $connection;
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * Helper function to fetch list of advanced department pages.
+   *
+   * @return array
+   *   Nids of department pages keyed by term ids.
+   */
+  public function getDepartmentPages() {
+    static $department_pages;
+
+    // We cache the nid-tid relationship for a single page request.
+    if (empty($department_pages)) {
+      $query = $this->database->select('node__field_product_category', 'nfpc');
+      $query->addField('nfpc', 'field_product_category_target_id', 'tid');
+      $query->addField('nfpc', 'entity_id', 'nid');
+      $department_pages = $query->execute()->fetchAllKeyed();
+    }
+
+    return $department_pages;
+  }
+
+  /**
+   * Get the department node object based on current route.
+   *
+   * @return mixed|false
+   *   Return Node object if department page, else FALSE.
+   */
+  public function getDepartmentPageNode() {
+    $result = &drupal_static(__FUNCTION__);
+    // Return if the static cache is set.
+    if (isset($result)) {
+      return $result;
+    }
+
+    $result = FALSE;
+    $node = $this->route_match->getParameter('node');
+
+    // Load the processed parameter.
+    if ($node) {
+      // Check if the current node type is department_page. For revision related
+      // pages $node will have nid.
+      if ($node instanceof Node && $node->bundle() == 'advanced_page' && $node->get('field_use_as_department_page')->value == 1) {
+        $result = $node;
+      }
+    }
+
+    return $result;
+  }
+
+}
