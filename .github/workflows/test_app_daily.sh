4efd2a085d7763f6fc6ca752a273da15969e9fb5
nikunjkotecha <nikunj.kotecha@acquia.com>
2022-10-17 18:57:27 +0530

    CORE-27168: Add daily cron job to test behat scenarios.

---
+++ .github/workflows/test_app_daily.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -ev
+
+cd ${GITHUB_WORKSPACE}/tests/behat
+composer install -n
+cd bin; npm install
+cd ..
+./behat-build.sh --rebuild=TRUE
+bin/behat --profile=hm-kw-uat-en-desktop --format pretty --tags=”@contact-us”
+
+set +v
