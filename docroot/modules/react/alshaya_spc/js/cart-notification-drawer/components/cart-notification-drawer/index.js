a90c3ce9425d0511b73869ca275fdab6edd4164e
Sanket Jain <sanket.jain@alshaya.com>
2022-10-17 10:55:34 +0530

    CORE-47101: Fix cart notification issue for pdp.

--- docroot/modules/react/alshaya_spc/js/cart-notification-drawer/components/cart-notification-drawer/index.js
+++ docroot/modules/react/alshaya_spc/js/cart-notification-drawer/components/cart-notification-drawer/index.js
@@ -1,69 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { hasValue } from '../../../../../js/utilities/conditionsUtility';
 import CartNotificationDrawerContent from '../cart-notification-drawer-content';
 import CartNotificationDrawerPopupContent from '../utilities/cart-notification-drawer-popup-content';
 
-class CartNotificationDrawer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cartDrawerOpen: true,
-    };
+const CartNotificationDrawer = ({ productData }) => {
+  if (!hasValue(productData)) {
+    return null;
   }
 
-  componentDidUpdate(prevProps) {
-    const { cartDrawerOpen } = this.props;
-    if (prevProps.cartDrawerOpen !== cartDrawerOpen) {
-      this.refreshState();
-    }
+  const [open, setCartDrawerState] = useState(true);
+
+  useEffect(() => {
+    setCartDrawerState(true);
+  });
+
+  const closeModal = () => {
+    document.querySelector('body').classList.remove('overlay-cart-drawer');
+    setCartDrawerState(false);
+  };
+
+  if (!open) {
+    return null;
   }
 
-  refreshState = () => {
-    this.setState({
-      cartDrawerOpen: true,
-    });
-  }
-
-  getPanelContent = () => {
-    const { productData } = this.props;
-    if (hasValue(productData) && hasValue(productData.productInfo)) {
+  const getPanelContent = (product) => {
+    if (hasValue(product) && hasValue(product.productInfo)) {
       return (
         <CartNotificationDrawerContent
-          productData={productData.productInfo}
-          closeModal={() => this.closeModal()}
+          productData={product.productInfo}
+          closeModal={() => closeModal()}
           overlayClass="overlay-cart"
         />
       );
     }
     return null;
+  };
+
+  const panelContent = getPanelContent(productData);
+
+  if (!hasValue(panelContent)) {
+    return null;
   }
 
-  closeModal = () => {
-    document.querySelector('body').classList.remove('overlay-cart-drawer');
-    this.setState({
-      cartDrawerOpen: false,
-    });
-  }
-
-  render() {
-    const { cartDrawerOpen } = this.state;
-    if (!cartDrawerOpen) {
-      return null;
-    }
-
-    const panelContent = this.getPanelContent();
-    if (!hasValue(panelContent)) {
-      return null;
-    }
-
-    return (
-      <div className="cart-notification-drawer">
-        <CartNotificationDrawerPopupContent>
-          {panelContent}
-        </CartNotificationDrawerPopupContent>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="cart-notification-drawer">
+      <CartNotificationDrawerPopupContent>
+        {panelContent}
+      </CartNotificationDrawerPopupContent>
+    </div>
+  );
+};
 
 export default CartNotificationDrawer;
