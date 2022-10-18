c014117d414ff169430f0dd692ab8f51bbabe931
nikunjkotecha <nikunj.kotecha@acquia.com>
2022-10-18 12:40:38 +0530

    CORE-27168: Update name to mention it is for smoke tests.

--- blt/scripts/github_actions/test_smoke.sh
+++ blt/scripts/github_actions/test_smoke.sh
@@ -7,6 +7,7 @@ composer install -n
 cd bin; npm install
 cd ..
 ./behat-build.sh --rebuild=TRUE
+# @todo change below to execute smoke tests on all sites.
 bin/behat --profile=hm-kw-uat-en-desktop --format pretty --tags=”@contact-us”
 
 set +v
