ceb1af6643dfe4e899148ed98050ad08a2d7a077
vivekpan27 <vivek.panicker@innoraft.com>
2022-10-18 14:51:44 +0530

    CORE-47135: Added custom JS sharethis file and loaded sharethis JS from it.

---
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_product/js/alshaya_rcs_sharethis.js
@@ -0,0 +1,39 @@
+/**
+ * @file
+ * This file contains most of the code for the configuration page.
+ */
+
+(function alshayaRcsSharethis($, drupalSettings) {
+  'use strict';
+  Drupal.behaviors.shareThis = {
+    attach: function alshayaRcsShareThisBehavior(context) {
+      // Check if we are displaying sharethis in the page.
+      if ($('.sharethis-container').length === 0) {
+        return;
+      }
+      // Check if the PDP rendering is complete.
+      var node = $('.entity--type-node', context).not('[data-sku *= "#"]');
+      var $context = $(context);
+      if ($context && $context.hasClass('entity--type-node')){
+        node = $context;
+      }
+      if (node.length === 0) {
+        return;
+      }
+      var skuBaseForm = $('.sku-base-form', node);
+      if (skuBaseForm.length === 0) {
+        return;
+      }
+      // Start loading sharethis button js.
+      Drupal.loadShareThis(false);
+      // Execute the code to start sharethis. Wait for a second so that the
+      // sharethis JS is loaded.
+      setTimeout(function executeSharethisJs() {
+        if (typeof stLight !== 'undefined') {
+          stLight.options(drupalSettings.sharethis);
+        }
+        stButtons.locateElements();
+      }, 1000);
+    }
+  };
+})(jQuery, drupalSettings);
