897e0fadcc92f82c281d30cc348095f14233126f
Marcelo Vani <marcelo.vani@acquia.com>
2022-10-18 12:18:46 +0100

    CORE-000: Removed jquery from globals.js.

--- docroot/modules/react/js/__tests__/globals.js
+++ docroot/modules/react/js/__tests__/globals.js
@@ -1,7 +1,4 @@
 import Drupal from '../../../../core/misc/drupal.es6';
-import jQuery from '../../../../core/assets/vendor/jquery/jquery';
-
-global['jQuery'] = global['$'] = jQuery;
 
 export default {
   Drupal,
