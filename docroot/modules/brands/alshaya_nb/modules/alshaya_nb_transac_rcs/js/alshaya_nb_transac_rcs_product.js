7ea5b6acf0607f9a824424a124523f41637d1310
Mohammad Rashid <mohammad.rashid@alshaya.com>
2022-10-22 09:40:56 +0300

    CORE-47525: PDP readiness for NB using classic layout.

--- docroot/modules/brands/alshaya_nb/modules/alshaya_nb_transac_rcs/js/alshaya_nb_transac_rcs_product.js
+++ docroot/modules/brands/alshaya_nb/modules/alshaya_nb_transac_rcs/js/alshaya_nb_transac_rcs_product.js
@@ -1,9 +1,10 @@
 /**
  * Listens to the 'rcsUpdateResults' event and updated the result object.
  */
-(function main(RcsEventManager) {
-  // Event listener to update the product data object.
-  RcsEventManager.addListener('rcsUpdateResults', function updateProductData(e) {
+ (function main() {
+  // Event listener to update the data layer object with the proper product
+  // data.
+  RcsEventManager.addListener('rcsUpdateResults', (e) => {
     // Return if result is empty.
     if (typeof e.detail.result === 'undefined' || e.detail.pageType !== 'product') {
       return;
@@ -12,14 +13,24 @@
     var data = e.detail.result;
 
     // Add extra data to product description.
-    var description = data.description.html;
+    // This will be rendered using handlebars templates to add P tags and H3 titles.
     e.detail.result.description = {
-      html: description,
+      html: data.description.html,
+      primary_material: data.primary_material,
+      technologies: data.technologies,
+      feature_bullets: data.feature_bullets,
+      green_leaf_notice: data.green_leaf_notice,
       sku: data.sku,
     };
 
     // Append field values to short_description.
     // The text will be trimmed if the description is longer than 160 characters.
-    e.detail.result.short_description = { html: data.description.html };
+    var short_description = { html: data.description.html };
+    short_description.html += (data.primary_material) ? '' + data.primary_material : '';
+    short_description.html += (data.technologies) ? '' + data.technologies : '';
+    short_description.html += (data.feature_bullets) ? '' + data.feature_bullets : '';
+    short_description.html += (data.green_leaf_notice) ? '' + data.green_leaf_notice : '';
+    short_description.html += (data.weight) ? '' + data.weight : '';
+    e.detail.result.short_description = short_description;
   });
-})(RcsEventManager);
+})();
