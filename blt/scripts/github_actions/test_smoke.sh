dcacaac3b9b5a1a70887818b8652646728f2b6d3
nikunjkotecha <nikunj.kotecha@acquia.com>
2022-10-28 00:45:50 +0530

    CORE-27168: Use Actions for chrome and chromedriver.

--- blt/scripts/github_actions/test_smoke.sh
+++ blt/scripts/github_actions/test_smoke.sh
@@ -5,14 +5,6 @@ set -ev
 cd ${GITHUB_WORKSPACE}/tests/behat
 composer install -n
 
-cd bin
-npm install
-
-cd ..
-
-java -Dwebdriver.chrome.driver=$CHROMEWEBDRIVER/chromedriver -jar vendor/se/selenium-server-standalone/bin/selenium-server-standalone.jar &
-sleep 3
-
 ./behat-build.sh --rebuild=TRUE --site=hm-kw-uat-en
 
 # @todo change below to execute smoke tests on all sites.
