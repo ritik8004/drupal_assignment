ac3cc6aec291e87fe72f6ca4b2acbaea02633efa
nikunjkotecha <nikunj.kotecha@acquia.com>
2022-10-27 18:25:58 +0530

    CORE-27168: Support selenium driver for Behat.

--- blt/scripts/github_actions/test_smoke.sh
+++ blt/scripts/github_actions/test_smoke.sh
@@ -4,8 +4,14 @@ set -ev
 
 cd ${GITHUB_WORKSPACE}/tests/behat
 composer install -n
-cd bin; npm install
+
+cd bin
+npm install
+wget --max-redirect=1 https://goo.gl/s4o9Vx -O selenium.jar
+java -jar selenium.jar &
+
 cd ..
+
 ./behat-build.sh --rebuild=TRUE
 # @todo change below to execute smoke tests on all sites.
 bin/behat --profile=hm-kw-uat-en-desktop --format pretty --tags="@contact-us"
