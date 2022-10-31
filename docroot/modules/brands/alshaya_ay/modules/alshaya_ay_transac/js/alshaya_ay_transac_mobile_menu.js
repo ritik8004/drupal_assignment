8f6a91730da7feecc8da90748fe526e48cf47d9f
Abdeali Waseef <wabeali@gmail.com>
2022-10-31 13:30:18 +0530

    CORE-47679: WIP Mobile search implementation improvements.

---
+++ docroot/modules/brands/alshaya_ay/modules/alshaya_ay_transac/js/alshaya_ay_transac_mobile_menu.js
@@ -0,0 +1,41 @@
+/**
+ * AY Mobile Menu.
+ */
+
+(function ($, Drupal) {
+  Drupal.behaviors.ayMainMenu = {
+    attach: function (context, settings) {
+      var menu = $('.main--menu');
+      var searchBlock = $('.block-alshaya-algolia-react-autocomplete');
+
+      if(window.innerWidth < 1025 && menu.length && searchBlock.length) {
+        var menuOpenButton = $('a.mobile--menu');
+        var menuCloseButton = $('.mobile--close');
+        var searchInput = searchBlock.find('input');
+        var searchBackButton = searchBlock.find('.algolia-search-back-icon');
+        var searchClearButton = searchBlock.find('.algolia-search-cleartext-icon');
+  
+        // Closing the search if user opens the menu while search is focused.
+        $(menuOpenButton).once('menu-open').click(function() {
+          if(searchBlock.hasClass('focused')) {
+            searchBackButton.click();
+          } else if (searchBlock.hasClass('clear-icon')) {
+            // When the user access page where search is already initiated,
+            // the search block has a .clear-icon class but is not focused.
+            searchClearButton.click();
+            searchBackButton.click();
+            searchInput.blur();
+          }
+        });
+  
+        // Closing the menu if user searches while menu is open.
+        $(searchBlock).once('search-block').click(function() {
+          if(menu.hasClass('menu--active')) {
+            window.scrollTo(0, 0);
+            menuCloseButton.click();
+          }
+        });
+      }
+    }
+  }
+})(jQuery, Drupal);
