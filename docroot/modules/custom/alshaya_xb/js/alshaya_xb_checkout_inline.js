854c340a7e5988f50acb7ed7753efccc4c293ec5
Marcelo Vani <marcelo.vani@acquia.com>
2022-11-01 18:41:18 +0000

    CORE-45572: Using file_get_contents to load inline script.

---
+++ docroot/modules/custom/alshaya_xb/js/alshaya_xb_checkout_inline.js
@@ -0,0 +1,19 @@
+var glegem = glegem || function () {
+  (window["glegem"].q = window["glegem"].q || []).push(arguments)
+}
+
+glegem("OnCheckoutStepLoaded", function (data) {
+  switch (data.StepId) {
+    case data.Steps.LOADED:
+      // Push data to datalayer.
+      Drupal.alshayaXbCheckoutGaPush(data, 2);
+      break;
+
+    case data.Steps.CONFIRMATION:
+      // Push data to datalayer.
+      Drupal.alshayaXbCheckoutGaPush(data, 4);
+      // Clear local storage.
+      window.commerceBackend.removeCartDataFromStorage(true);
+      break;
+  }
+});
