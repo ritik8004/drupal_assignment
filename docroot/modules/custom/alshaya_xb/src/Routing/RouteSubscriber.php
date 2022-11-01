d4f533045f5aaa32951babf97628b492bcd94f31
Marcelo Vani <marcelo.vani@acquia.com>
2022-11-01 18:53:18 +0000

    CORE-45572: Replaced variable assignments in conditions with null safe operators.

--- docroot/modules/custom/alshaya_xb/src/Routing/RouteSubscriber.php
+++ docroot/modules/custom/alshaya_xb/src/Routing/RouteSubscriber.php
@@ -18,20 +18,19 @@ class RouteSubscriber extends RouteSubscriberBase {
    */
   protected function alterRoutes(RouteCollection $collection) {
     // Return empty checkout page.
-    if ($route = $collection->get('alshaya_spc.checkout')) {
-      $route->setDefault('_controller', '\Drupal\alshaya_xb\Controller\CheckoutController::checkoutPage');
-    }
+    $route = $collection->get('alshaya_spc.checkout');
+    $route?->setDefault('_controller', '\Drupal\alshaya_xb\Controller\CheckoutController::checkoutPage');
+
     // Altering the access for profile.user_page.multiple route.
-    if ($route = $collection->get('profile.user_page.multiple')) {
-      $route->setRequirement('_access', 'FALSE');
-    }
+    $route = $collection->get('profile.user_page.multiple');
+    $route?->setRequirement('_access', 'FALSE');
+
     // Altering the access for store_finder routes.
-    if ($route = $collection->get('alshaya_geolocation.store_finder')) {
-      $route->setRequirement('_access', 'FALSE');
-    }
-    if ($route = $collection->get('alshaya_geolocation.store_finder_list')) {
-      $route->setRequirement('_access', 'FALSE');
-    }
+    $route = $collection->get('alshaya_geolocation.store_finder');
+    $route?->setRequirement('_access', 'FALSE');
+
+    $route = $collection->get('alshaya_geolocation.store_finder_list');
+    $route?->setRequirement('_access', 'FALSE');
   }
 
 }
