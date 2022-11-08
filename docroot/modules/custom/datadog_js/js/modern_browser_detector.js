86158053daf50ea82b89eb49fdc409b4ad26048e
Ashish Singh <ashish.singh@axelerant.com>
2022-11-08 17:52:57 +0530

    CORE-35004: Fixed 2 space indentaion.

--- docroot/modules/custom/datadog_js/js/modern_browser_detector.js
+++ docroot/modules/custom/datadog_js/js/modern_browser_detector.js
@@ -2,32 +2,32 @@
  * Detect browser is modern or not and add variable to window object
  */
 (function (window) {
-    window.isModernBrowser = false;
-    try {
-        () => { };
+	window.isModernBrowser = false;
+	try {
+		() => { };
 
-        // Class support
-        class __ES6Test { }
+		// Class support
+		class __ES6Test { }
 
-        // Object initializer property and method shorthands
-        let a = true;
-        let b = {
-            a,
-            c() {
-                return true;
-            },
-            d: [1, 2, 3],
-        };
-        const g = true;
+		// Object initializer property and method shorthands
+		let a = true;
+		let b = {
+			a,
+			c() {
+				return true;
+			},
+			d: [1, 2, 3],
+		};
+	  const g = true;
 
-        // Object destructuring
-        let { c, d } = b;
+		// Object destructuring
+		let { c, d } = b;
 
-        // Spread operator
-        let e = [...d, 4];
+		// Spread operator
+		let e = [...d, 4];
 
-        window.isModernBrowser = true;
-    } catch (error) {
-        window.isModernBrowser = false;
-    }
+		window.isModernBrowser = true;
+	} catch (error) {
+		window.isModernBrowser = false;
+	}
 }(window));
