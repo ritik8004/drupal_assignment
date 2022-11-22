4de095d9ff5481928a832c361c6c55e466860db2
vivekpan27 <vivek.panicker@innoraft.com>
2022-11-22 13:45:56 +0530

    CORE-47339: Added category subscriber for rcs path processor event.

---
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_listing/src/EventSubscriber/AlshayaRcsPhListingPathProcessorEventSubscriber.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Drupal\alshaya_rcs_listing\EventSubscriber;
+
+use Drupal\alshaya_rcs\Service\AlshayaRcsEnrichmentHelper;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\rcs_placeholders\Event\RcsPhPathProcessorEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Provides a path processor subscriber for rcs categories.
+ */
+class AlshayaRcsPhListingPathProcessorEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Enrichment helper.
+   *
+   * @var \Drupal\alshaya_rcs\Service\AlshayaRcsEnrichmentHelper
+   */
+  protected $enrichmentHelper;
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs an AlshayaRcsPhListingPathProcessorEventSubscriber object.
+   */
+  public function __construct(
+    AlshayaRcsEnrichmentHelper $enrichment_helper,
+    ConfigFactoryInterface $config_factory
+  ) {
+    $this->enrichmentHelper = $enrichment_helper;
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      RcsPhPathProcessorEvent::EVENT_NAME => [
+        ['onPathProcess'],
+      ],
+    ];
+  }
+
+  /**
+   * Processes list page path.
+   *
+   * @param \Drupal\rcs_placeholders\Event\RcsPhPathProcessorEvent $event
+   *   Event object.
+   */
+  public function onPathProcess(RcsPhPathProcessorEvent $event): void {
+    $data = $event->getData();
+    if (empty($data['path']) || empty($data['full_path'])) {
+      return;
+    }
+
+    $path = $data['path'];
+    $full_path = $data['full_path'];
+    $config = $this->configFactory->get('rcs_placeholders.settings');
+    $category_prefix = $config->get('category.path_prefix');
+
+    if (!str_starts_with($path, '/' . $category_prefix)) {
+      return;
+    }
+
+    $category = $config->get('category.enrichment')
+      ? $this->enrichmentHelper->getEnrichedEntity('category', $path)
+      : NULL;
+    $entityData = NULL;
+    $processed_paths = '/taxonomy/term/' . $config->get('category.placeholder_tid');
+
+    if (isset($category)) {
+      $entityData = $category->toArray();
+      $processed_paths = '/taxonomy/term/' . $category->id();
+    }
+
+    $event->setData([
+      'entityType' => 'category',
+      'entityPath' => substr_replace($path, '', 0, strlen($category_prefix) + 1),
+      'entityPathPrefix' => $category_prefix,
+      'entityFullPath' => $full_path,
+      'processedPaths' => $processed_paths,
+      'entityData' => $entityData,
+    ]);
+
+    $event->stopPropagation();
+  }
+
+}
