2b7df9c389a42a19f33d889471fd4c4c4f5d6c12
vivekpan27 <vivek.panicker@innoraft.com>
2022-12-01 18:30:17 +0530

    CORE-48562: Added name to the function.

--- docroot/modules/react/alshaya_matchback/js/matchbackAddToBag.js
+++ docroot/modules/react/alshaya_matchback/js/matchbackAddToBag.js
@@ -1,27 +1,31 @@
-import { createConfigurableDrawer } from "../../js/utilities/addToBagHelper";
-import { hasValue } from "../../js/utilities/conditionsUtility";
-import MatchbackAddToBag from "./add_to_bag";
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { createConfigurableDrawer } from '../../js/utilities/addToBagHelper';
+import { hasValue } from '../../js/utilities/conditionsUtility';
+import MatchbackAddToBag from './add_to_bag';
 
-
-(function($, Drupal) {
-  Drupal.behaviors.alshayaMatchbackBehavior = {
-    attach: function attach(context, settings) {
+(function alshayaMatchbackAddToBag($, Drupal) {
+  Drupal.behaviors.alshayaMatchbackBehavior = { // eslint-disable-line no-param-reassign
+    attach: function attach(context) {
+      // We only want to proceed when the AJAX call to fetch matchback items
+      // is done and Drupal behaviors is called.
       if (hasValue(context.classList) && !context.classList.contains('crossell-title')) {
         return;
       }
       // Add the drawer warpper markup to the dom.
       createConfigurableDrawer(true);
       // Now add the Add to Bag button for each carousel item.
-      var matchbackMobileElements = $('.matchback-add-to-bag');
-      matchbackMobileElements.each(function eachElement(i, obj) {
-        var $parent = $($(this).parents('article[data-vmode="matchback_mobile"]')[0]);
+      const matchbackMobileElements = $('.matchback-add-to-bag');
+      matchbackMobileElements.each(function eachElement() {
+        const $parent = $($(this).parents('article[data-vmode="matchback_mobile"]')[0]);
         ReactDOM.render(
-        <MatchbackAddToBag
-          sku={$parent.attr('data-sku')}
-          url={$parent.find('.full-prod-link').attr('href')}
-        />
-        ,this);
+          <MatchbackAddToBag
+            sku={$parent.attr('data-sku')}
+            url={$parent.find('.full-prod-link').attr('href')}
+          />,
+          this,
+        );
       });
-    }
-  }
-})(jQuery, Drupal);
+    },
+  };
+}(jQuery, Drupal));
