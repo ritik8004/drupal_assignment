35ecd89616a70df594fe383ffb7cb05d528a8c5a
Sanket Jain <sanket.jain@alshaya.com>
2022-12-02 11:54:13 +0530

    CORE-48577: Removed dependency from newsletter module.

--- docroot/modules/custom/alshaya_newsletter/js/alshaya_newsletter.js
+++ docroot/modules/custom/alshaya_newsletter/js/alshaya_newsletter.js
@@ -94,7 +94,9 @@
 
         if (data.message === 'success') {
           // Tracking newsletter in gtm.
-          Drupal.alshaya_seo_gtm_push_lead_type('footer');
+          if (Drupal.alshaya_seo_gtm_push_lead_type !== undefined) {
+            Drupal.alshaya_seo_gtm_push_lead_type('footer');
+          }
           $('#alshaya-newsletter-subscribe .form-type-email input').val('');
           setTimeout(Drupal.clearNewsletterForm, parseInt(data.interval));
         }
