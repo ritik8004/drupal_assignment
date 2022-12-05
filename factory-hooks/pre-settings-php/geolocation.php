58e322f761a2b150c619403471f5ebad09c475d9
santhosh <santhosh@specbee.com>
2022-12-05 11:50:35 +0530

    CORE-43734: Changing the config key for geolocation configuation.

--- factory-hooks/pre-settings-php/geolocation.php
+++ factory-hooks/pre-settings-php/geolocation.php
@@ -8,9 +8,9 @@
  */
 
 // Set google map api key.
-$settings['geolocation.settings']['google_map_api_key'] = 'AIzaSyCfNUqK2pIA8TTJusCyLGiypSbnxBe8fJ8';
+$settings['geolocation_google_maps.settings']['google_map_api_key'] = 'AIzaSyCfNUqK2pIA8TTJusCyLGiypSbnxBe8fJ8';
 
 // Set the key to use in Lando env directly in $config.
 if (getenv('LANDO')) {
-  $config['geolocation.settings']['google_map_api_key'] = 'AIzaSyBL9faHw5s_vO1sUalcbQv05dzce_71fUY';
+  $config['geolocation_google_maps.settings']['google_map_api_key'] = 'AIzaSyBL9faHw5s_vO1sUalcbQv05dzce_71fUY';
 }
