6e48de7472603574cdf99a73d664a23f5971c5d1
vivekpan27 <vivek.panicker@innoraft.com>
2022-12-09 16:37:23 +0530

    CORE-48562: Removed unnecessary extra component.

--- docroot/modules/react/alshaya_matchback/js/matchbackAddToBag.js
+++ docroot/modules/react/alshaya_matchback/js/matchbackAddToBag.js
@@ -2,17 +2,17 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import { createConfigurableDrawer } from '../../js/utilities/addToBagHelper';
 import { hasValue } from '../../js/utilities/conditionsUtility';
-import MatchbackAddToBag from './add_to_bag';
+import AddToBagContainer from '../../js/utilities/components/addtobag-container';
 
 (function alshayaMatchbackAddToBag($, Drupal) {
-  Drupal.behaviors.alshayaMatchbackBehavior = { // eslint-disable-line no-param-reassign
+  Drupal.behaviors.alshayaMatchbackAddToBagBehavior = { // eslint-disable-line no-param-reassign
     attach: function attach(context) {
       // We only want to proceed when the AJAX call to fetch matchback items
       // is done and Drupal behaviors is called.
       if (hasValue(context.classList) && !context.classList.contains('crossell-title')) {
         return;
       }
-      // Add the drawer warpper markup to the dom.
+      // Add the drawer warpper markup to the DOM.
       createConfigurableDrawer(true);
       // Now add the Add to Bag button for each carousel item.
       const matchbackMobileElements = $('.matchback-add-to-bag');
@@ -23,7 +23,7 @@ import MatchbackAddToBag from './add_to_bag';
         stock = hasValue(stock) ? stock : '0';
 
         ReactDOM.render(
-          <MatchbackAddToBag
+          <AddToBagContainer
             sku={$parent.attr('data-sku')}
             url={$parent.find('.full-prod-link').attr('href')}
             stockQty={stock}
