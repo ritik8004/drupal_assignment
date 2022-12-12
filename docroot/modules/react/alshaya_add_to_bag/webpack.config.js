985cbec21331bdc08956b803dd9080af216878ba
subhadeep-bhandari-acquia <subhadeep.bhandari@acquia.com>
2022-12-12 13:12:16 +0530

    CORE-47229: Changes made to reduce react js file build time.

--- docroot/modules/react/alshaya_add_to_bag/webpack.config.js
+++ docroot/modules/react/alshaya_add_to_bag/webpack.config.js
@@ -30,14 +30,6 @@ var config = {
           rootMode: "upward",
         }
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(woff|woff2|ttf|otf|eot|svg|gif)$/,
-        use: ['file-loader'],
-      }
     ],
   },
 };
--- docroot/modules/react/alshaya_spc/webpack.config.js
+++ docroot/modules/react/alshaya_spc/webpack.config.js
@@ -48,15 +48,6 @@ var config = {
               'plugins': ['@babel/plugin-proposal-class-properties']}]
         }
       },
-      // Added below rules for parsing react datepicker css in components.
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(woff|woff2|ttf|otf|eot|svg|gif)$/,
-        use: ['file-loader'],
-      },
     ]
   }
 };
