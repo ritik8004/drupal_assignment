4d061ab2b26d911ad7a52cbb2d5c5fda8865bd7e
Kunal Singh <skunal185@gmail.com>
2022-12-23 18:33:21 +0530

    CORE-50437: Updated the check for category id.

--- docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_product/js/alshaya_rcs_product_renderer_breadcrumb-exports.es5.js
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_product/js/alshaya_rcs_product_renderer_breadcrumb-exports.es5.js
@@ -46,17 +46,16 @@ exports.normalize = function normalize(
   });
 
   // Detect the root category.
-  let rootCategoryId = null;
   Object.keys(categories).some(function (i) {
     const c = categories[i];
     // Top level categories will not have breadcrumbs and the level is always 2.
     if (c.level === 2 && c.breadcrumbs === null) {
-      return rootCategoryId = c.id;
+      return c.id;
     } else {
       // If we could not find a top level category, we get it from the breadcrumb
       // on the next category.
       if (Array.isArray(c.breadcrumbs) && typeof c.breadcrumbs[0].category_id !== 'undefined') {
-        return rootCategoryId = c.breadcrumbs[0].category_id;
+        return c.breadcrumbs[0].category_id;
       }
     }
   });
@@ -67,8 +66,7 @@ exports.normalize = function normalize(
   let deepestCategoryId = data.breadcrumb_category_id;
   Object.keys(categories).forEach(function (i) {
     // Check if the first category in the breadcrumb is the same as the root category.
-    if (Drupal.hasValue(categories[i])
-      && typeof categories[i].id !== 'undefined'
+    if (Drupal.hasValue(categories[i].id)
       && categories[i].id === parseInt(deepestCategoryId, 10)
     ) {
       deepestCategory = categories[i];
