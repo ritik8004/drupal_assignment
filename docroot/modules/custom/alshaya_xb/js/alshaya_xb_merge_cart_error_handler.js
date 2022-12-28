5183a2e9f1da7df04bdeba30339fea1d5f423560
avinash.shukla <avinash.shukla@acquia.com>
2022-12-28 13:54:18 +0530

    CORE-50445: Removed block for error container to use the existing container.

--- docroot/modules/custom/alshaya_xb/js/alshaya_xb_merge_cart_error_handler.js
+++ docroot/modules/custom/alshaya_xb/js/alshaya_xb_merge_cart_error_handler.js
@@ -6,10 +6,14 @@
 (function ($, Drupal) {
   Drupal.behaviors.alshayaXbMergeCartErrorhandler = {
     attach: function (context) {
+      // Display error received from MDC on cart merge for XB.
       document.addEventListener('mergeCartError',  (e) => {
         var data = e.detail;
         if (typeof data.error_message !== 'undefined') {
-          $('.errors-container').html('<div class="error">' + data.error_message + '</div>');
+          var wrapper = $('[data-drupal-messages-fallback]');
+          // Show error message from event data.
+          wrapper.html('<div class="errors-container"><div class="error">' + data.error_message + '</div></div>');
+          wrapper.removeClass('hidden');
         }
       });
     }
