8f56006f54335c0cb1dc78e4015091b538b5f423
Kunal Singh <skunal185@gmail.com>
2023-01-03 04:10:50 +0530

    CORE-32004: Updated the fixed free gift promotion with default one.

--- docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_product/js/alshaya_rcs_product.pdp.rcs.js
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_product/js/alshaya_rcs_product.pdp.rcs.js
@@ -173,10 +173,10 @@ window.commerceBackend = window.commerceBackend || {};
     const freeGiftPromotion = mainProduct.free_gift_promotion;
     if (freeGiftPromotion.length > 0
       // We support displaying only one free gift promotion for now.
-      && freeGiftPromotion[3].total_items > 0) {
+      && freeGiftPromotion[0].total_items > 0) {
       // Get the first free gift product info. In case there are multiple free
       // gift items, then we will load the product info during modal view.
-      var giftItemSku = freeGiftPromotion[3].gifts[0].sku;
+      var giftItemSku = freeGiftPromotion[0].gifts[0].sku;
       var freeGiftProduct = await globalThis.rcsPhCommerceBackend.getData('product_by_sku', { sku: giftItemSku });
 
       if (Drupal.hasValue(freeGiftProduct.sku)) {
