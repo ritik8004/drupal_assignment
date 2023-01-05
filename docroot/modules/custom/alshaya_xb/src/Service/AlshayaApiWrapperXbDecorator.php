b8fc4daf7fa6fece4206c604c5bdc69e5474475c
avinash.shukla <avinash.shukla@acquia.com>
2023-01-05 13:00:03 +0530

    CORE-50557: Fixed removed constructor using parent in service yml.

--- docroot/modules/custom/alshaya_xb/src/Service/AlshayaApiWrapperXbDecorator.php
+++ docroot/modules/custom/alshaya_xb/src/Service/AlshayaApiWrapperXbDecorator.php
@@ -3,16 +3,6 @@
 namespace Drupal\alshaya_xb\Service;
 
 use Drupal\alshaya_api\AlshayaApiWrapper;
-use Drupal\alshaya_api\Helper\MagentoApiHelper;
-use Drupal\Component\Datetime\TimeInterface;
-use Drupal\Core\Cache\CacheBackendInterface;
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Extension\ModuleHandlerInterface;
-use Drupal\Core\File\FileSystemInterface;
-use Drupal\Core\Language\LanguageManagerInterface;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
-use Drupal\Core\Session\AccountInterface;
-use Drupal\Core\State\StateInterface;
 
 /**
  * Class AlshayaApiWrapperXbDecorator decorates AlshayaApiWrapper.
@@ -24,60 +14,16 @@ class AlshayaApiWrapperXbDecorator extends AlshayaApiWrapper {
    *
    * @var \Drupal\alshaya_xb\Service\DomainConfigOverrides
    */
-  protected $configOverrides;
+  protected $domainConfig;
 
   /**
-   * Constructs a new AlshayaApiWrapperXbDecorator object.
+   * Set domain config overrides service.
    *
-   * @param \Drupal\alshaya_xb\Service\DomainConfigOverrides $config_overrides
+   * @param \Drupal\alshaya_xb\Service\DomainConfigOverrides $domain_config
    *   Domain config overrides.
-   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
-   *   The language manager.
-   * @param \Drupal\Component\Datetime\TimeInterface $date_time
-   *   The date time service.
-   * @param \Drupal\Core\Cache\CacheBackendInterface $cache
-   *   Cache Backend object for "cache.data".
-   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
-   *   LoggerFactory object.
-   * @param \Drupal\Core\State\StateInterface $state
-   *   The state factory.
-   * @param \Drupal\Core\File\FileSystemInterface $fileSystem
-   *   The filesystem service.
-   * @param \Drupal\alshaya_api\Helper\MagentoApiHelper $mdc_helper
-   *   The magento api helper.
-   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
-   *   The module handler.
-   * @param \Drupal\Core\Session\AccountInterface $current_user
-   *   Current user object.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   Config factory.
    */
-  public function __construct(
-    DomainConfigOverrides $config_overrides,
-    LanguageManagerInterface $language_manager,
-    TimeInterface $date_time,
-    CacheBackendInterface $cache,
-    LoggerChannelFactoryInterface $logger_factory,
-    StateInterface $state,
-    FileSystemInterface $fileSystem,
-    MagentoApiHelper $mdc_helper,
-    ModuleHandlerInterface $module_handler,
-    AccountInterface $current_user,
-    ConfigFactoryInterface $config_factory
-  ) {
-    $this->configOverrides = $config_overrides;
-    parent::__construct(
-      $language_manager,
-      $date_time,
-      $cache,
-      $logger_factory,
-      $state,
-      $fileSystem,
-      $mdc_helper,
-      $module_handler,
-      $current_user,
-      $config_factory
-    );
+  public function setDomainConfigOverrides(DomainConfigOverrides $domain_config) {
+    $this->domainConfig = $domain_config;
   }
 
   /**
@@ -85,13 +31,13 @@ class AlshayaApiWrapperXbDecorator extends AlshayaApiWrapper {
    */
   public function getCustomerAddressForm() {
     // Get domain overrides.
-    $configOverrides = $this->configOverrides->getConfigByDomain();
+    $configOverrides = $this->domainConfig->getConfigByDomain();
 
     // Get country code from domain overrides, if not available then
     // use site level country code.
-    $country_code = $configOverrides['code'] ?? strtoupper(_alshaya_custom_get_site_level_country_code());
+    $country_code = $configOverrides['code'] ?? _alshaya_custom_get_site_level_country_code();
 
-    return $this->getCustomerAddressFormByCountryCode($country_code);
+    return $this->getCustomerAddressFormByCountryCode(strtoupper($country_code));
   }
 
 }
