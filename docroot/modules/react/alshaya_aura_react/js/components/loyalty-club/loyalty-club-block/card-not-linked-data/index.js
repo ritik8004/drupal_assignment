47cb45d83d15c820a697e96fe73a67bfc813e710
avinash.shukla <avinash.shukla@acquia.com>
2023-01-09 18:08:38 +0530

    CORE-50185: Aura banner updated for my account and my aura.

--- docroot/modules/react/alshaya_aura_react/js/components/loyalty-club/loyalty-club-block/card-not-linked-data/index.js
+++ docroot/modules/react/alshaya_aura_react/js/components/loyalty-club/loyalty-club-block/card-not-linked-data/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cleave from 'cleave.js/react';
 import ConditionalView
   from '../../../../../../alshaya_spc/js/common/components/conditional-view';
@@ -8,6 +8,67 @@ import { getNotYouLabel } from '../../../../utilities/aura_utils';
 
 const AuraMyAccountOldCardFound = (props) => {
   const { cardNumber, notYouFailed } = props;
+
+  const [wrapperClass, setWrapperClass] = useState('hide');
+
+  const onClickArrowHead = () => {
+    if (wrapperClass === 'hide') {
+      setWrapperClass('show');
+    } else {
+      setWrapperClass('hide');
+    }
+  };
+
+  if (drupalSettings.path.currentPath === 'user/loyalty-club') {
+    return (
+      <div className="aura-myaccount-no-linked-card-wrapper old-card-found fadeInUp">
+        <div className="aura-logo">
+          <ConditionalView condition={window.innerWidth > 1024}>
+            <AuraLogo stacked="vertical" />
+          </ConditionalView>
+          <ConditionalView condition={window.innerWidth < 1025}>
+            <AuraLogo stacked="horizontal" />
+          </ConditionalView>
+        </div>
+        <div className="aura-myaccount-no-linked-card-description old-card-found">
+          <div className="header">
+            <span className="bold">{ drupalSettings.userDetails.userName }</span>
+            { Drupal.t('an Aura loyalty account no. @card_number is associated with your email adress. It just takes one click to link.', {
+              '@card_number': cardNumber,
+            }, { context: 'aura' })}
+            <span className="bold">{Drupal.t('Do you want to link now?')}</span>
+          </div>
+          <div className="card-number-wrapper">
+            <div className="link-card-wrapper">
+              <div className="link-card-loader-placeholder" />
+              <div
+                className="link-your-card"
+                onClick={() => {
+                  Drupal.alshayaSeoGtmPushAuraEventData({ action: 'AURA_EVENT_ACTION_LINK_YOUR_CARD', label: 'initiated' });
+                  handleLinkYourCard(cardNumber);
+                }}
+              >
+                { Drupal.t('Link your card', {}, { context: 'aura' }) }
+              </div>
+            </div>
+            <div className="not-you-wrapper">
+              <div className="not-you-loader-placeholder" />
+              <div className="error-placeholder" />
+              <div
+                className="not-you"
+                onClick={() => {
+                  handleNotYou(cardNumber);
+                  Drupal.alshayaSeoGtmPushAuraEventData({ action: 'AURA_EVENT_ACTION_SIGN_IN_NOT_YOU', label: 'initiated' });
+                }}
+              >
+                { getNotYouLabel(notYouFailed) }
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="aura-myaccount-no-linked-card-wrapper old-card-found fadeInUp">
@@ -21,10 +82,16 @@ const AuraMyAccountOldCardFound = (props) => {
       </div>
       <div className="aura-myaccount-no-linked-card-description old-card-found">
         <div className="header">
-          { Drupal.t('An Aura card is already associated with your email address. Link your card in just one click.') }
+          { Drupal.t('An Aura loyalty card is associated with your email adress. It just a takes one click to link.', {}, { context: 'aura' })}
           <span className="bold">{Drupal.t('Do you want to link now?')}</span>
+          <button type="button" onClick={onClickArrowHead}>
+            <span className={`arrowHead ${wrapperClass}`}>&nbsp;</span>
+          </button>
         </div>
-        <div className="card-number-wrapper">
+        <div className={`card-number-wrapper ${wrapperClass}`}>
+          <div className="card-number-label">
+            { Drupal.t('Aura membership number', {}, { context: 'aura' }) }
+          </div>
           <Cleave
             name="aura-my-account-link-card"
             className="aura-my-account-link-card"
@@ -32,29 +99,22 @@ const AuraMyAccountOldCardFound = (props) => {
             value={cardNumber}
             options={{ blocks: [4, 4, 4, 4] }}
           />
-          <div className="link-card-wrapper">
-            <div className="link-card-loader-placeholder" />
-            <div
-              className="link-your-card"
-              onClick={() => {
-                Drupal.alshayaSeoGtmPushAuraEventData({ action: 'AURA_EVENT_ACTION_LINK_YOUR_CARD', label: 'initiated' });
-                handleLinkYourCard(cardNumber);
-              }}
-            >
-              { Drupal.t('Link your account') }
-            </div>
-          </div>
           <div className="not-you-wrapper">
             <div className="not-you-loader-placeholder" />
             <div className="error-placeholder" />
             <div
               className="not-you"
+            >
+              { getNotYouLabel(notYouFailed) }
+            </div>
+            <div
+              className="link-your-card"
               onClick={() => {
                 handleNotYou(cardNumber);
                 Drupal.alshayaSeoGtmPushAuraEventData({ action: 'AURA_EVENT_ACTION_SIGN_IN_NOT_YOU', label: 'initiated' });
               }}
             >
-              { getNotYouLabel(notYouFailed) }
+              { Drupal.t('Link your card', {}, { context: 'aura' }) }
             </div>
           </div>
         </div>
