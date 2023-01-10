29ffe65bc59d31bee3ebbdb78c4a1acedaa5fff3
Nikita Jain <nikita@axelerant.com>
2023-01-10 21:42:23 +0530

    CORE-45944: Added other envs.

--- tests/behat/src/Process/AlshayaFeatureProcess.php
+++ tests/behat/src/Process/AlshayaFeatureProcess.php
@@ -54,7 +54,7 @@ class AlshayaFeatureProcess {
     else {
       $this->suiteLocators[] = $this->sourcePath . DIRECTORY_SEPARATOR . 'common' . DIRECTORY_SEPARATOR . 'oldPDP';
     }
-    if (in_array($environment[2], ['dev3', 'local', 'dev', 'dev2', 'qa', 'uat'])) {
+    if (in_array($environment[2], ['dev3', 'local', 'dev', 'dev2', 'qa', 'qa2', 'test', 'uat'])) {
       if ($environment[0] == 'bp' || $environment[0] == 'hm') {
         $this->suiteLocators[] = $this->sourcePath . DIRECTORY_SEPARATOR . 'common' . DIRECTORY_SEPARATOR . 'boots';
       }
