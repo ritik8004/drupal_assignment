86a2e3adeb5ed15a8eaaf9cab009ba66f79fd503
avinash.shukla <avinash.shukla@acquia.com>
2023-01-11 10:37:12 +0530

    CORE-50185: Fixed link your card and not you button.

--- docroot/modules/react/alshaya_aura_react/js/components/loyalty-club/loyalty-club-block/card-not-linked-data/index.js
+++ docroot/modules/react/alshaya_aura_react/js/components/loyalty-club/loyalty-club-block/card-not-linked-data/index.js
@@ -104,14 +104,18 @@ const AuraMyAccountOldCardFound = (props) => {
             <div className="error-placeholder" />
             <div
               className="not-you"
+              onClick={() => {
+                handleNotYou(cardNumber);
+                Drupal.alshayaSeoGtmPushAuraEventData({ action: 'AURA_EVENT_ACTION_SIGN_IN_NOT_YOU', label: 'initiated' });
+              }}
             >
               { getNotYouLabel(notYouFailed) }
             </div>
             <div
               className="link-your-card"
               onClick={() => {
-                handleNotYou(cardNumber);
-                Drupal.alshayaSeoGtmPushAuraEventData({ action: 'AURA_EVENT_ACTION_SIGN_IN_NOT_YOU', label: 'initiated' });
+                Drupal.alshayaSeoGtmPushAuraEventData({ action: 'AURA_EVENT_ACTION_LINK_YOUR_CARD', label: 'initiated' });
+                handleLinkYourCard(cardNumber);
               }}
             >
               { Drupal.t('Link your card', {}, { context: 'aura' }) }
