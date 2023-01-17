0de85cd4cf3612bdcf279ac6654ace9b551e1535
Ashish Singh <ashish.singh@axelerant.com>
2023-01-17 17:39:47 +0530

    CORE-47150: Svgs moved to white label from westelm.

--- docroot/modules/react/alshaya_spc/js/svg-component/payment-method-svg/index.js
+++ docroot/modules/react/alshaya_spc/js/svg-component/payment-method-svg/index.js
@@ -5,7 +5,7 @@ const PaymentMethodIcon = (props) => {
   const { methodName, methodLabel, context } = props;
   if (methodName === 'banktransfer') {
     return (
-      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_westelm/imgs/icons/payment-options-icons/bank-transfer.svg" />
+      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_white_label/imgs/cards/payment-options-icons/bank-transfer.svg" />
     );
   }
 
@@ -15,48 +15,48 @@ const PaymentMethodIcon = (props) => {
     if (context && context === 'cart') {
       return (
         <>
-          <img className="payment-method-icon" src="/themes/custom/transac/alshaya_westelm/imgs/icons/payment-options-icons/visa.svg" />
-          <img className="payment-method-icon" src="/themes/custom/transac/alshaya_westelm/imgs/icons/payment-options-icons/mastercard.svg" />
+          <img className="payment-method-icon" src="/themes/custom/transac/alshaya_white_label/imgs/cards/payment-options-icons/visa.svg" />
+          <img className="payment-method-icon" src="/themes/custom/transac/alshaya_white_label/imgs/cards/payment-options-icons/mastercard.svg" />
         </>
       );
     }
 
     return (
-      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_westelm/imgs/icons/payment-options-icons/card.svg" />
+      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_white_label/imgs/cards/payment-options-icons/card.svg" />
     );
   }
   if (methodName === 'checkout_com_upapi_knet') {
     return (
-      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_westelm/imgs/icons/payment-options-icons/knet.svg" />
+      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_white_label/imgs/cards/payment-options-icons/knet.svg" />
     );
   }
   if (methodName === 'checkout_com_applepay' || methodName === 'checkout_com_upapi_applepay') {
     return (
-      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_westelm/imgs/icons/payment-options-icons/apple-pay.svg" />
+      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_white_label/imgs/cards/payment-options-icons/apple-pay.svg" />
     );
   }
   if (methodName === 'checkout_com_upapi_qpay') {
     return (
-      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_westelm/imgs/icons/payment-options-icons/naps.svg" />
+      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_white_label/imgs/cards/payment-options-icons/naps.svg" />
     );
   }
 
   if (methodName === 'postpay') {
     return (
       // SVG had font issue and JPG not available so used png.
-      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_westelm/imgs/icons/payment-options-icons/post-pay.png" />
+      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_white_label/imgs/cards/payment-options-icons/post-pay.png" />
     );
   }
 
   if (methodName === 'tabby') {
     return (
-      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_westelm/imgs/icons/payment-options-icons/tabby.svg" alt={methodLabel} />
+      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_white_label/imgs/cards/payment-options-icons/tabby.svg" alt={methodLabel} />
     );
   }
 
   if (methodName === 'checkout_com_upapi_fawry') {
     return (
-      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_westelm/imgs/icons/payment-options-icons/fawry-pay.svg" />
+      <img className="payment-method-icon" src="/themes/custom/transac/alshaya_white_label/imgs/cards/payment-options-icons/fawry-pay.svg" />
     );
   }
 
@@ -72,7 +72,7 @@ const PaymentMethodIcon = (props) => {
   }
 
   return (
-    <img className="payment-method-icon" src="/themes/custom/transac/alshaya_westelm/imgs/icons/payment-options-icons/cash.svg" />
+    <img className="payment-method-icon" src="/themes/custom/transac/alshaya_white_label/imgs/cards/payment-options-icons/cash.svg" />
   );
 };
 
