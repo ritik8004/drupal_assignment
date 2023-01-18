b6521f1184c1094a0dc78664b0f7897b70262772
Sanket Jain <sanket.jain@alshaya.com>
2023-01-18 16:28:27 +0530

    CORE-48243: Fix super category active link issue on store finder.

--- docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_super_category/src/Service/AlshayaRcsSuperCategoryManager.php
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_super_category/src/Service/AlshayaRcsSuperCategoryManager.php
@@ -5,8 +5,68 @@ namespace Drupal\alshaya_rcs_super_category\Service;
 use Drupal\alshaya_super_category\AlshayaSuperCategoryManager;
 use Drupal\node\NodeInterface;
 use Drupal\taxonomy\TermInterface;
+use Drupal\rcs_placeholders\Service\RcsPhPathProcessor;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\alshaya_config\AlshayaConfigManager;
+use Drupal\alshaya_acm_product_category\ProductCategoryTreeInterface;
+use Drupal\Core\Path\CurrentPathStack;
+use Drupal\rcs_placeholders\Service\RcsPhEnrichmentHelper;
 
 class AlshayaRcsSuperCategoryManager extends AlshayaSuperCategoryManager {
+
+  /**
+   * The current path.
+   *
+   * @var \Drupal\Core\Path\CurrentPathStack
+   */
+  protected $currentPath;
+
+  /**
+   * Enrichment helper.
+   *
+   * @var \Drupal\rcs_placeholders\Service\RcsPhEnrichmentHelper
+   */
+  protected $enrichmentHelper;
+
+  /**
+   * AlshayaSuperCategoryManager constructor.
+   *
+   * @param \Drupal\alshaya_acm_product_category\ProductCategoryTreeInterface $product_category_tree
+   *   Product category tree.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   Config factory.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Entity type manager.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   Module handler.
+   * @param \Drupal\alshaya_config\AlshayaConfigManager $alshaya_config_manager
+   *   Alshaya config manager.
+   * @param \Drupal\Core\Path\CurrentPathStack $current_path
+   *   The current path.
+   * @param \Drupal\rcs_placeholders\Service\RcsPhEnrichmentHelper $enrichment_helper
+   *   Enrichment helper.
+   */
+  public function __construct(
+    ProductCategoryTreeInterface $product_category_tree,
+    ConfigFactoryInterface $config_factory,
+    EntityTypeManagerInterface $entity_type_manager,
+    ModuleHandlerInterface $module_handler,
+    AlshayaConfigManager $alshaya_config_manager,
+    CurrentPathStack $current_path,
+    RcsPhEnrichmentHelper $enrichment_helper
+  ) {
+    parent::__construct(
+      $product_category_tree,
+      $config_factory,
+      $entity_type_manager,
+      $module_handler,
+      $alshaya_config_manager
+    );
+    $this->currentPath = $current_path;
+    $this->enrichmentHelper = $enrichment_helper;
+  }
 
   /**
    * {@inheritdoc}
@@ -55,8 +115,14 @@ class AlshayaRcsSuperCategoryManager extends AlshayaSuperCategoryManager {
     $term = $this->productCategoryTree->getCategoryTermFromRoute();
 
     if (empty($term)) {
-      $categories = $this->productCategoryTree->getCategoryRootTerms();
-      if ($categories) {
+      // Get enriched entity by current path.
+      // Fallback logic to get first term.
+      // If current path matches the term overridden path, we return that term.
+      $term = $this->enrichmentHelper->getEnrichedEntityByPath('category', 'internal:'. $this->currentPath->getPath());
+     // ksm($term->id());
+      if ($term === NULL) {
+        ksm("dssdf");
+        $categories = $this->productCategoryTree->getCategoryRootTerms();
         $category = reset($categories);
         $term = $this->entityTypeManager->getStorage('taxonomy_term')->load($category['id']);
       }
