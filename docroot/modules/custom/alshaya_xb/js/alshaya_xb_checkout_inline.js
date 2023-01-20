9be9e0952ada29d0722c9e7451a3fe6d5b6580fb
avinash.shukla <avinash.shukla@acquia.com>
2023-01-20 16:38:44 +0530

    CORE-46275: Log globale data to verify shipping and billing details.

--- docroot/modules/custom/alshaya_xb/js/alshaya_xb_checkout_inline.js
+++ docroot/modules/custom/alshaya_xb/js/alshaya_xb_checkout_inline.js
@@ -3,8 +3,9 @@ var glegem = glegem || function () {
 };
 
 glegem("OnClientEvent", function (source, data) {
-  console.log(GEMerchantUtils.ClientEvents.BILLING_DETAILS_COMPLETED);
   console.log(source, data);
+  console.log(GEMerchantUtils.ClientEvents.BILLING_DETAILS_COMPLETED);
+  console.log(GEMerchantUtils.ClientEvents.SHIPPING_DETAILS_COMPLETED);
 });
 
 glegem("OnCheckoutStepLoaded", function (data) {
