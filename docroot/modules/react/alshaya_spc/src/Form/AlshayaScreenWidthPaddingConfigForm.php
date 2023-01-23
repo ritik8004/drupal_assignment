dc8dae3fe2fafea95590c0abf5355f0d0635ff65
gulfam-alshaya <gulfam.mirza@alshaya.com>
2023-01-23 17:06:31 +0530

    CORE-51476: Dev Create a Config Form alshaya spc screen width padding PR.

--- docroot/modules/react/alshaya_spc/src/Form/AlshayaScreenWidthPaddingConfigForm.php
+++ docroot/modules/react/alshaya_spc/src/Form/AlshayaScreenWidthPaddingConfigForm.php
@@ -41,6 +41,18 @@ class AlshayaScreenWidthPaddingConfigForm extends ConfigFormBase {
       '#description' => $this->t('Set the padding if screen Width 320.'),
       '#default_value' => $screen_width_padding_config->get('screen_width_320'),
     ];
+    $form['alshaya_screen_width_padding']['screen_width_360'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Enter the padding for Screen Width 360'),
+      '#description' => $this->t('Set the padding if screen Width 360.'),
+      '#default_value' => $screen_width_padding_config->get('screen_width_360'),
+    ];
+    $form['alshaya_screen_width_padding']['screen_width_375'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Enter the padding for Screen Width 375'),
+      '#description' => $this->t('Set the padding if screen Width 375.'),
+      '#default_value' => $screen_width_padding_config->get('screen_width_375'),
+    ];
     $form['alshaya_screen_width_padding']['screen_width_480'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Enter the padding for Screen Width 480'),
@@ -53,17 +65,35 @@ class AlshayaScreenWidthPaddingConfigForm extends ConfigFormBase {
       '#description' => $this->t('Set the padding if screen Width 720.'),
       '#default_value' => $screen_width_padding_config->get('screen_width_720'),
     ];
+    $form['alshaya_screen_width_padding']['screen_width_768'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Enter the padding for Screen Width 768'),
+      '#description' => $this->t('Set the padding if screen Width 768.'),
+      '#default_value' => $screen_width_padding_config->get('screen_width_768'),
+    ];
     $form['alshaya_screen_width_padding']['screen_width_1024'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Enter the padding for Screen Width 1024'),
       '#description' => $this->t('Set the padding if screen Width 1024.'),
       '#default_value' => $screen_width_padding_config->get('screen_width_1024'),
     ];
+    $form['alshaya_screen_width_padding']['screen_width_1280'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Enter the padding for Screen Width 1280'),
+      '#description' => $this->t('Set the padding if screen Width 1280.'),
+      '#default_value' => $screen_width_padding_config->get('screen_width_1280'),
+    ];
     $form['alshaya_screen_width_padding']['screen_width_1680'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Enter the padding for Screen Width 1680'),
       '#description' => $this->t('Set the padding if screen Width 1680.'),
       '#default_value' => $screen_width_padding_config->get('screen_width_1680'),
+    ];
+    $form['alshaya_screen_width_padding']['screen_width_1920'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Enter the padding for Screen Width 1920'),
+      '#description' => $this->t('Set the padding if screen Width 1920.'),
+      '#default_value' => $screen_width_padding_config->get('screen_width_1920'),
     ];
     $form['alshaya_screen_width_padding']['screen_width_2560'] = [
       '#type' => 'textfield',
@@ -82,10 +112,15 @@ class AlshayaScreenWidthPaddingConfigForm extends ConfigFormBase {
     $this->config('alshaya_spc.screen_width_padding')
       ->set('screen_width_padding_status', $form_state->getValue('screen_width_padding_status'))
       ->set('screen_width_320', $form_state->getValue('screen_width_320'))
+      ->set('screen_width_360', $form_state->getValue('screen_width_360'))
+      ->set('screen_width_375', $form_state->getValue('screen_width_375'))
       ->set('screen_width_480', $form_state->getValue('screen_width_480'))
       ->set('screen_width_720', $form_state->getValue('screen_width_720'))
+      ->set('screen_width_768', $form_state->getValue('screen_width_768'))
       ->set('screen_width_1024', $form_state->getValue('screen_width_1024'))
+      ->set('screen_width_1280', $form_state->getValue('screen_width_1280'))
       ->set('screen_width_1680', $form_state->getValue('screen_width_1680'))
+      ->set('screen_width_1920', $form_state->getValue('screen_width_1920'))
       ->set('screen_width_2560', $form_state->getValue('screen_width_2560'))
       ->save();
     parent::submitForm($form, $form_state);
