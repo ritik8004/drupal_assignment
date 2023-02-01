8b98256b02bf58bde61446bcc1b43a6f03090e1f
vivekpan27 <vivek.panicker@innoraft.com>
2023-02-01 11:59:25 +0530

    CORE-51847: Determined stock status for config products based on status of children.

--- docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_product/js/alshaya_rcs_pdp_magazine_v2.js
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_product/js/alshaya_rcs_pdp_magazine_v2.js
@@ -7,6 +7,7 @@
   const staticDataStore = {
     pdpPromotion: [],
   };
+  const inStock = 'IN_STOCK';
 
   // Call event after entity load and process product data.
   RcsEventManager.addListener('alshayaPageEntityLoaded', async function pageEntityLoaded(e) {
@@ -85,6 +86,32 @@
   }
 
   /**
+   * Get stock status for the product.
+   *
+   * @param {Object} product
+   *   Product object.
+   */
+  function getProductStockStatus(product) {
+    var status = false;
+    if (product.type_id === 'configurable'
+      && Drupal.hasValue(product.variants)
+    ) {
+      product.variants.some(function eachVariant(variant) {
+        if (variant.product.stock_status === inStock) {
+          status = true;
+          return true;
+        }
+        return false;
+      });
+    }
+    else if (product.type_id === 'simple' && product.stock_status === inStock) {
+      status = true;
+    }
+
+    return status;
+  }
+
+  /**
    * Process product data as per maganize v2 data format.
    *
    * @param {object} product
@@ -103,7 +130,7 @@
       is_product_buyable: product.is_buyable,
       shortDesc: Drupal.hasValue(product.short_description) ? product.short_description.html : '',
       stockQty: product.stock_data.qty,
-      stockStatus: product.stock_status === 'IN_STOCK',
+      stockStatus: getProductStockStatus(product),
       title: {'label': product.name},
       rawGallery: updateGallery(product, product.name),
       additionalAttributes: Object.keys(product.description.additional_attributes).length > 0 ? product.description.additional_attributes : {},
