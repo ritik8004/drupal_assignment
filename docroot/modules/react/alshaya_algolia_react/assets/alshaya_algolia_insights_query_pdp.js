b13526696bf522582535849c0df55ed005f02bff
gulfam-alshaya <gulfam.mirza@alshaya.com>
2023-02-13 18:49:44 +0530

    CORE-52271: Handle for the V2 - Magzine and Default Layout.

--- docroot/modules/react/alshaya_algolia_react/assets/alshaya_algolia_insights_query_pdp.js
+++ docroot/modules/react/alshaya_algolia_react/assets/alshaya_algolia_insights_query_pdp.js
@@ -2,7 +2,7 @@
  * @file
  * PLP All Filters Panel & Facets JS file.
  */
-(function ($, Drupal) {
+(function ($, Drupal, drupalSettings) {
 
     /**
    * Helper function get the algolia_search_clicks value.
@@ -11,13 +11,12 @@
    */
     Drupal.fetchSkuAlgoliaInsightsClickData = function () {
         var algoliaClicks = Drupal.getItemFromLocalStorage('algolia_search_clicks');
-        var sku= $('#data-insights-query-id').attr('data-sku');
+        var sku= Object.keys(drupalSettings.productInfo);
         if (algoliaClicks && algoliaClicks[sku] !== undefined  && algoliaClicks[sku] !== null
           && typeof algoliaClicks[sku] !== 'string') {
         var dataInsightsQueryId = algoliaClicks[sku]['query-id'];
-        $('#data-insights-query-id').attr('data-insights-query-id',dataInsightsQueryId);
+        $('.data-insights-query-class').attr('data-insights-query-id',dataInsightsQueryId);
       }
-      
     };
     Drupal.fetchSkuAlgoliaInsightsClickData();
-})(jQuery, Drupal);
+})(jQuery, Drupal, drupalSettings);
