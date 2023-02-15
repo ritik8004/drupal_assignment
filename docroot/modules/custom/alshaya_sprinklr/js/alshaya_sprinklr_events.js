0af6357d3c4650ce3cba2f8dfe2f3cc081ef69c9
manish-3108 <manish.sharma@innoraft.com>
2023-02-15 10:37:53 +0530

    CORE-52529: Updated comments as per PR feedback.

--- docroot/modules/custom/alshaya_sprinklr/js/alshaya_sprinklr_events.js
+++ docroot/modules/custom/alshaya_sprinklr/js/alshaya_sprinklr_events.js
@@ -4,22 +4,36 @@
  */
 
 (function (drupalSettings) {
-  // Auto open the sprinklr chatbot when user logs in
-  // this is done only once when user logs in via the
-  // link provided by chatbot.
+  // Drupal Settings 'updateConversationContext' variable is
+  // used to determine if user is visiting the page right
+  // after the login using 'update_conversation_context' cookie
+  // for normal login and 'sprinklr_social_login' cookie for
+  // social login check 'AlshayaSprinklrEventSubscriber'.
+  // Once this variable is added in drupalSettings the cookies are
+  // deleted check 'alshaya_sprinklr_page_attachments_alter'.
   if (typeof drupalSettings.sprinklr.updateConversationContext !== 'undefined'
     && drupalSettings.sprinklr.updateConversationContext === true) {
-    // Trigger chatbot 'openExistingConversation' event
-    // to auto open the chatbot when user logs in.
-    // Add a delay of 3s to fire this event because
-    // the chatbot is not loaded immediately and we don't
+    // Trigger chatbot 'updateConversationContext' &
+    // 'openExistingConversation' event to auto open the chatbot
+    // when user logs in. Add a delay of 4s to fire this event
+    // because the chatbot is not loaded immediately and we don't
     // have any event to check that as of now.
     // Also, triggering this event does not return proper
     // response so we can't determine if it was successful
-    // or not. Hence, we are using setTimeout with 3s delay.
+    // or not. Hence, we are using setTimeout with 4s delay.
+    // @todo need to check with sprinklr team to figure out
+    // a better approach to handle these events in future.
     setTimeout(() => {
+      // Trigger updateConversationContext SDK to let
+      // sprinklr chatbot know that the anonymous user
+      // has now logged in.
+      sprChat('updateConversationContext', {
+        context: typeof drupalSettings.sprinklr.clientContext !== 'undefined'
+          ? drupalSettings.sprinklr.clientContext
+          : {},
+      });
       // Open the sprinklr chatbot.
       sprChat('openExistingConversation');
-    }, 3000);
+    }, 4000);
   }
 })(drupalSettings)
