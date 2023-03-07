98ce30acbce166d38ba931fe7eafc83f8e630102
root <shubhangi.johri@alshaya.com>
2023-03-07 08:00:41 +0000

    CORE-53411: Restricting shoeAi to PDP and PLP.

--- docroot/modules/custom/alshaya_shoeai/js/alshaya_shoeai.js
+++ docroot/modules/custom/alshaya_shoeai/js/alshaya_shoeai.js
@@ -5,11 +5,12 @@
 (function (drupalSettings) {  
   let shoeAi = drupalSettings.shoeai;
   if (shoeAi.status != null && shoeAi.status == 'enabled') {
+    let language = drupalSettings.path.currentLanguage;
     let script = document.createElement('script');
     script.src = 'https://shoesize.me/assets/plugin/loader.js';
     script.type = 'text/javascript';
     script.async = true;  
-    script.text = '{shopID:"' + shoeAi.shopId + '", locale: "' + drupalSettings.path.currentLanguage + '", scale: "eu", zeroHash: "' + shoeAi.zeroHash + '"}';
+    script.text = '{shopID:"' + shoeAi.shopId + '", locale: "' + language + '", scale: "eu", zeroHash: "' + shoeAi.zeroHash + '"}';
     document.head.appendChild(script);
   }
 })(drupalSettings);
