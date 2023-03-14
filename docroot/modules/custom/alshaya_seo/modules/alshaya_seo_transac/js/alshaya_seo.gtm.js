cc1d0e55513ace75becbb031d2791ba125f23044
root <root@c520d997474c>
2023-03-14 08:36:03 +0000

    CORE-45078: Fix for gtm condition.

--- docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.js
+++ docroot/modules/custom/alshaya_seo/modules/alshaya_seo_transac/js/alshaya_seo.gtm.js
@@ -916,7 +916,7 @@
           }
 
           // IF listName contains placeholder remove it.
-          if (productData.list.indexOf('match back') === '-1') {
+          if (productData.list.indexOf('match back') === -1) {
             productData.list = listName.replace('PDP-placeholder', referrerData.pageType);
           }
         }
