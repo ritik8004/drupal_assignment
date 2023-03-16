81673118b995cdd4773a854ee53b0bd3908b8f13
root <root@c520d997474c>
2023-03-16 21:19:58 +0000

    CORE-45077: Update for product click.

--- docroot/modules/react/alshaya_matchback/assets/js/alshaya_matchback.gtm.js
+++ docroot/modules/react/alshaya_matchback/assets/js/alshaya_matchback.gtm.js
@@ -1,13 +1,13 @@
 (function alshayaMatchback($, Drupal, drupalSettings) {
   Drupal.behaviors.alshayaMatchbackGtmBehavior = {
     attach: function alshayaMatchbackBehavior(context) {
-      $('article[data-vmode="matchback"]', context)
+      $('article[data-vmode="matchback"] a.full-prod-link', context)
         .once('matchback-click-handler-attached')
         .on('click', function onMatchbackClick() {
           Drupal.alshaya_seo_gtm_push_product_clicks(
             $(this).closest('article'),
             drupalSettings.gtm.currency,
-            $('body').attr('gtm-list-name').replace('PDP-placeholder', 'match back'),
+            $('body').attr('gtm-list-name').replace('PDP-placeholder', 'match backs'),
           );
         });
     },
