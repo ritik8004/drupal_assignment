b79f0cb685825668d865d9c239786eba89dc4bf7
vbouchet <vincent.bouchet.31@gmail.com>
2023-03-17 09:45:32 +0100

    CORE-50305: Fix @here mention in Slack message.

--- scripts/deployment/post_to_slack.sh
+++ scripts/deployment/post_to_slack.sh
@@ -5,5 +5,5 @@ message="$1"
 FILE=$HOME/slack_settings
 if [ -f $FILE ]; then
   . $HOME/slack_settings
-  curl -X POST --data-urlencode "payload={\"username\": \"${AH_SITE_NAME}\", \"text\": \"@here ${message} - ${AH_SITE_NAME}\", \"icon_emoji\": \":acquiacloud:\"}" $SLACK_WEBHOOK_URL -s > /dev/null
+  curl -X POST --data-urlencode "payload={\"username\": \"${AH_SITE_NAME}\", \"text\": \"<!here> ${message} - ${AH_SITE_NAME}\", \"icon_emoji\": \":acquiacloud:\"}" $SLACK_WEBHOOK_URL -s > /dev/null
 fi
