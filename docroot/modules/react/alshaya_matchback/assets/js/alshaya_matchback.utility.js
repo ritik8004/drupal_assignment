2fea4e613bbc34741021773789e24a84868e49e2
Rahul Rasgon <rahulrasgon@gmail.com>
2023-03-23 10:38:05 +0530

    CORE-53072: Fixed multiple product detail view data Layer events firing.

--- docroot/modules/react/alshaya_matchback/assets/js/alshaya_matchback.utility.js
+++
@@ -1,9 +0,0 @@
-(function alshayaMatchbackUtility(Drupal, $) {
-  // Push to GTM when add to bag product drawer is opened.
-  document.addEventListener('drawerOpenEvent', function onDrawerOpen(e) {
-    var $element = $(e.detail.triggerButtonElement).closest('article.entity--type-node');
-    if ($element.length) {
-      Drupal.alshayaSeoGtmPushProductDetailView($element);
-    }
-  });
-})(Drupal, jQuery);
