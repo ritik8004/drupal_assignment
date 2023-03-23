fa26d6b454cfcf5f29906c3083d6b2ca0aed5328
Kunal Singh <skunal185@gmail.com>
2023-03-23 18:13:29 +0530

    CORE-51626: Removed the unwanted function arguments. (#28243)

--- docroot/modules/custom/alshaya_stores_finder/src/Controller/AccessCheckController.php
+++ docroot/modules/custom/alshaya_stores_finder/src/Controller/AccessCheckController.php
@@ -3,7 +3,6 @@
 namespace Drupal\alshaya_stores_finder\Controller;
 
 use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Controller\ControllerBase;
 
 /**
@@ -14,13 +13,10 @@ class AccessCheckController extends ControllerBase {
   /**
    * Checks access for a specific request.
    *
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   Run access checks for this account.
-   *
    * @return \Drupal\Core\Access\AccessResultInterface
    *   The access result.
    */
-  public function accessCheck(AccountInterface $account) {
+  public function accessCheck() {
     // Allow access to the routes only if the store finder status is set.
     return AccessResult::allowedIf($this->config('alshaya_stores_finder.settings')->get('stores_finder_page_status'));
   }
