dfba5040e8665050ea6f9bb6c3983032d6d703a3
Hammad Ghani <78860474+hammad-ghani@users.noreply.github.com>
2023-03-27 03:08:35 -0400

    Merge pull request #28242 from saurabh-axl/CORE-51591
    CORE-51591: Added missing item in purchase success GA datalayer.

--- docroot/modules/custom/alshaya_xb/js/alshaya_xb_checkout_seo.js
+++ docroot/modules/custom/alshaya_xb/js/alshaya_xb_checkout_seo.js
@@ -125,6 +125,7 @@
     let productSku = [];
     let productStyleCode = [];
     let discountAmount = 0;
+    let firstTimeTransaction = null;
     if (geData.details.ProductInformation) {
       Object.entries(geData.details.ProductInformation).forEach(function (productItem) {
         const product = productItem[1];
@@ -145,6 +146,7 @@
         productStyleCode.push(product.ProductGroupCode);
         cartItemsCount = parseInt(product.Quantity) + cartItemsCount;
         discountAmount = product.ProductPrices.MerchantTransaction.DiscountedPrice;
+        firstTimeTransaction = Drupal.getProductMetadata(product, 'firstTimeTransaction');
       });
     }
 
@@ -161,7 +163,7 @@
       "discountAmount": discountAmount,
       "transactionId": geData.OrderId,
       "globaleId": geData.OrderId,
-      "firstTimeTransaction": null, // @todo We need to expose this via drupalSettings.
+      "firstTimeTransaction": firstTimeTransaction || true,
       "privilegesCardNumber": null, // @todo We need to ask Global-e to get this information.
       "loyaltyType": null, // @todo We need to ask Global-e to get this information.
       "rewardType": null, // @todo We need to ask Global-e to get this information.
