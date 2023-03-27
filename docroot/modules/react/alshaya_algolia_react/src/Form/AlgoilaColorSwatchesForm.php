ed1f9a65d12e483df44954721c21cb5447989972
gulfam-alshaya <gulfam.mirza@alshaya.com>
2023-03-27 17:36:51 +0530

    CORE-54413: dev fix the PR comment.

--- docroot/modules/react/alshaya_algolia_react/src/Form/AlgoilaColorSwatchesForm.php
+++ docroot/modules/react/alshaya_algolia_react/src/Form/AlgoilaColorSwatchesForm.php
@@ -38,11 +38,11 @@ class AlgoilaColorSwatchesForm extends ConfigFormBase {
       '#tree' => FALSE,
       '#open' => TRUE,
     ];
-    $form['plp_swatch_config']['enable_plp_color_swatch_slider'] = [
+    $form['plp_swatch_config']['enable_listing_page_color_swatch_slider'] = [
       '#type' => 'radios',
       '#title' => $this->t('Enable / Disable Swatch Slider'),
       '#options' => [1 => $this->t('Enabled'), 0 => $this->t('Disabled')],
-      '#default_value' => $config->get('enable_plp_color_swatch_slider') === FALSE ? 0 : 1,
+      '#default_value' => $config->get('enable_listing_page_color_swatch_slider') === FALSE ? 0 : 1,
     ];
     $form['plp_swatch_config']['swatch_type'] = [
       '#type' => 'select',
@@ -53,22 +53,22 @@ class AlgoilaColorSwatchesForm extends ConfigFormBase {
       '#title' => $this->t('Select Swatch Type'),
       '#default_value' => $config->get('swatch_type'),
     ];
-    $form['plp_swatch_config']['no_of_swatches_plp'] = [
+    $form['plp_swatch_config']['no_of_swatches_desktop'] = [
       '#type' => 'number',
       '#min' => '1',
       '#max' => '6',
       '#step' => '1',
       '#title' => $this->t('Set the swatch limit for PLP view'),
-      '#default_value' => $config->get('no_of_swatches_plp'),
+      '#default_value' => $config->get('no_of_swatches_desktop'),
       '#description' => $this->t('Max number swatches to display upfront in PLP followed by carousel.'),
     ];
-    $form['plp_swatch_config']['no_of_swatches_plp_mobile'] = [
+    $form['plp_swatch_config']['no_of_swatches_mobile'] = [
       '#type' => 'number',
       '#min' => '1',
       '#max' => '4',
       '#step' => '1',
       '#title' => $this->t('Set the swatch limit for PLP on mobile view'),
-      '#default_value' => $config->get('no_of_swatches_plp_mobile'),
+      '#default_value' => $config->get('no_of_swatches_mobile'),
       '#description' => $this->t('Max number swatches to display upfront in PLP on mobile view followed by carousel.'),
     ];
 
@@ -80,10 +80,10 @@ class AlgoilaColorSwatchesForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->configFactory->getEditable('alshaya_algolia_react.color_swatches')
-      ->set('enable_plp_color_swatch_slider', $form_state->getValue('enable_plp_color_swatch_slider'))
+      ->set('enable_listing_page_color_swatch_slider', $form_state->getValue('enable_listing_page_color_swatch_slider'))
       ->set('swatch_type', $form_state->getValue('swatch_type'))
-      ->set('no_of_swatches_plp', (int) $form_state->getValue('no_of_swatches_plp'))
-      ->set('no_of_swatches_plp_mobile', (int) $form_state->getValue('no_of_swatches_plp_mobile'))
+      ->set('no_of_swatches_desktop', (int) $form_state->getValue('no_of_swatches_desktop'))
+      ->set('no_of_swatches_mobile', (int) $form_state->getValue('no_of_swatches_mobile'))
       ->save();
 
     parent::submitForm($form, $form_state);
