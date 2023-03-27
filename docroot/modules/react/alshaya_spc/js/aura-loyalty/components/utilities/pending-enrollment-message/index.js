c3eda63557bcbeb62a39acbf0863300f8afb0b3d
nithish <nithish@qburst.com>
2023-03-27 10:31:33 +0000

    CORE-39994: Updated the message for AURA linked QE user on order confirmation page.

--- docroot/modules/react/alshaya_spc/js/aura-loyalty/components/utilities/pending-enrollment-message/index.js
+++ docroot/modules/react/alshaya_spc/js/aura-loyalty/components/utilities/pending-enrollment-message/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import parse from 'html-react-parser';
 import AuraAppLinks from '../aura-app-links';
+import { isMobile } from '../../../../../../js/utilities/display';
 
 const PendingEnrollmentMessage = () => {
-  const message = Drupal.t('To use your points online, please download the Aura MENA app and provide us with a few more details.', {}, { context: 'aura' });
-
+  if (isMobile()) {
+    return (
+      <div className="spc-aura-pending-enrollment-message-wrapper">
+        <div className="spc-aura-pending-enrollment-message">
+          <div className="message">
+            {Drupal.t('To use your points online, please download the Aura MENA app and provide us with a few more details.', {}, { context: 'aura' })}
+          </div>
+          <AuraAppLinks />
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="spc-aura-pending-enrollment-message-wrapper">
       <div className="spc-aura-pending-enrollment-message">
         <div className="message">
-          {message}
+          { parse(Drupal.t('To use your points online, please download the Aura MENA app available both on <strong>App Store</strong> and <strong>Play Store</strong>.', {}, { context: 'aura' })) }
         </div>
-        <AuraAppLinks />
       </div>
     </div>
   );
