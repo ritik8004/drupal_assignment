247cf7bc4d7ef8c7e4a499f69aebb28180084cca
vivekpan27 <vivek.panicker@innoraft.com>
2023-03-28 18:07:49 +0530

    CORE-51803: Added default values outside if statement.

--- docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_bazaar_voice/js/alshaya_rcs_bazaar_voice.utility.js
+++ docroot/modules/custom/alshaya_rcs/modules/alshaya_rcs_bazaar_voice/js/alshaya_rcs_bazaar_voice.utility.js
@@ -238,20 +238,19 @@
       type: 'GET',
       dataType: 'json',
     }).then(function getData(response, status, xhr) {
+      staticStorage.writeReviewFormHiddenFields = [];
+      var data = {
+        data: [],
+        status: xhr.status,
+      };
+
       if (Drupal.hasValue(response) && Drupal.hasValue(response[0])) {
-        staticStorage.writeReviewFormHiddenFields = response[0].hide_fields_write_review;
-        response = response[0].write_review_form;
-        var data = {
-          data: Object.values(response),
-          status: xhr.status,
-        };
-      }
-      else {
-        staticStorage.writeReviewFormHiddenFields = [];
-        var data = {
-          data: [],
-          status: xhr.status,
-        };
+        staticStorage.writeReviewFormHiddenFields = Array.isArray(response[0].hide_fields_write_review)
+          ? response[0].hide_fields_write_review
+          : [];
+        data.data = typeof response[0].write_review_form === "object"
+          ? Object.values(response[0].write_review_form)
+          : [];
       }
 
       var event = new CustomEvent('showMessage', {
