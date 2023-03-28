cd1b4323f1d0e77f7b65ddd95b58038064bee877
nithish <nithish@qburst.com>
2023-03-28 06:29:22 +0000

    CORE-39994: Comments addressed on updating the message for AURA linked QE user on order confirmation page in web view.

--- docroot/modules/react/alshaya_spc/js/aura-loyalty/components/utilities/pending-enrollment-message/index.js
+++ docroot/modules/react/alshaya_spc/js/aura-loyalty/components/utilities/pending-enrollment-message/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import parse from 'html-react-parser';
 import AuraAppLinks from '../aura-app-links';
 import { isMobile } from '../../../../../../js/utilities/display';
-import ConditionalView from '../../../../../../js/utilities/components/conditional-view';
 
 const PendingEnrollmentMessage = () => {
-  const message = isMobile() ? Drupal.t('To use your points online, please download the Aura MENA app and provide us with a few more details.', {}, { context: 'aura' }) : parse(Drupal.t('To use your points online, please download the Aura MENA app available both on <strong>App Store</strong> and <strong>Play Store</strong>.', {}, { context: 'aura' }));
+  const message = isMobile()
+    ? Drupal.t('To use your points online, please download the Aura MENA app and provide us with a few more details.', {}, { context: 'aura' })
+    : parse(Drupal.t('To use your points online, please download the Aura MENA app available both on <strong>App Store</strong> and <strong>Play Store</strong>.', {}, { context: 'aura' }));
 
   return (
     <div className="spc-aura-pending-enrollment-message-wrapper">
@@ -13,9 +14,7 @@ const PendingEnrollmentMessage = () => {
         <div className="message">
           { message }
         </div>
-        <ConditionalView condition={isMobile()}>
-          <AuraAppLinks />
-        </ConditionalView>
+        { isMobile() && <AuraAppLinks /> }
       </div>
     </div>
   );
