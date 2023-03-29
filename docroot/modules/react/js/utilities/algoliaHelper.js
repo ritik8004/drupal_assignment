ae62522fa5dbd002f8a50fbdf858198d46235b19
lokeshwari-alshaya <lokeshwari.gautam@alshaya.com>
2023-03-29 07:41:04 +0000

    CORE-55013: Removing Extra code.

--- docroot/modules/react/js/utilities/algoliaHelper.js
+++ docroot/modules/react/js/utilities/algoliaHelper.js
@@ -1,6 +1,5 @@
 import algoliasearch from 'algoliasearch/lite';
 
-// Adding _useRequestCache parameter to avoid duplicate requests on Facet filters and sort orders.
 export const searchClient = algoliasearch(
   drupalSettings.algoliaSearch.application_id,
   drupalSettings.algoliaSearch.api_key,
