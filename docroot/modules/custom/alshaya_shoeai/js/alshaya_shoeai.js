84142fe55eb211e4c1d6cddfe35efb7a49fba796
shubhangi-alshaya <123928489+shubhangi-alshaya@users.noreply.github.com>
2023-04-03 16:36:49 +0530

    Core 54854 (#28342)
    * CORE-54854: Update for shoe recommendation on PDP.
    
    * CORE-54854: Update for indentation and commits.
    
    * Update docroot/modules/custom/alshaya_shoeai/js/alshaya_shoeai.js
    
    Co-authored-by: Nilesh Lohar <nilesh.lohar@acquia.com>
    
    * Update docroot/modules/custom/alshaya_shoeai/js/alshaya_shoeai.js
    
    Co-authored-by: Nilesh Lohar <nilesh.lohar@acquia.com>
    
    * Update docroot/modules/custom/alshaya_shoeai/js/alshaya_shoeai.js
    
    Co-authored-by: Nilesh Lohar <nilesh.lohar@acquia.com>
    
    * CORE-54854: Update for recommended size in PDP.
    
    ---------
    
    Co-authored-by: root <root@072e52585360>
    Co-authored-by: Nilesh Lohar <nilesh.lohar@acquia.com>

--- docroot/modules/custom/alshaya_shoeai/js/alshaya_shoeai.js
+++ docroot/modules/custom/alshaya_shoeai/js/alshaya_shoeai.js
@@ -2,6 +2,7 @@
  * @file
  * Contains Alshaya ShoeAI functionality.
  */
+
 (function (drupalSettings) {  
   let shoeAi = drupalSettings.shoeai;
   if (shoeAiStatus(shoeAi)) {
@@ -10,8 +11,12 @@
     script.src = 'https://shoesize.me/assets/plugin/loader.js';
     script.type = 'text/javascript';
     script.async = true;  
-    script.text = '{shopID:"' + shoeAi.shopId + '", locale: "' +
-     language + '", scale: "eu", zeroHash: "' + shoeAi.zeroHash + '"}';
+    // newRecommendation callback is only for PDP to set the default size as per recommendation from ShoeAI.
+    let newRecommendation = function(recommendation){
+      shoe_size_recommendation(recommendation)
+    };
+    script.text = '{shopID:"' + shoeAi.shopId + '", locale:"' +
+     language + '",scale: "eu", zeroHash:"' + shoeAi.zeroHash + '",newRecommendation:' + newRecommendation + '}';
     document.body.appendChild(script);
   }
 })(drupalSettings);
@@ -21,5 +26,20 @@ function shoeAiStatus(shoeAi) {
     return true;
   } else {
     return false;
+  }
+}
+
+// Helper function for getting recommended shoesize from shoeai and select the size in PDP if available.
+window.shoe_size_recommendation = (recommendation) => {
+  const recommendedSize = recommendation.size['eu'] ? recommendation.size['eu'].replace('.0','') : null;
+  // work only if recommendedSize is not null.
+  if (recommendedSize) {
+    const sizes = document.querySelectorAll('.magv2-select-list-name');
+    for (const size of sizes) {
+      if (size.innerText == recommendedSize) {
+        size.click();
+        return;
+      }
+    }
   }
 }
