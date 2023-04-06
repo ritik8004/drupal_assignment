3dc1057ffd36cf23d59fa1ee0aa6c571d7d80e8a
Milind Kagdelwar <milind.kagdelwar@acquia.com>
2023-04-06 11:30:16 +0530

    CORE-55291: Use metatag graphQL query in hook or Event in Drupal.

--- docroot/modules/custom/rcs_placeholders/src/Service/RcsPhPathPrefixHelper.php
+++ docroot/modules/custom/rcs_placeholders/src/Service/RcsPhPathPrefixHelper.php
@@ -3,6 +3,8 @@
 namespace Drupal\rcs_placeholders\Service;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Service provides helper functions for the rcs path prefix.
@@ -17,13 +19,23 @@ class RcsPhPathPrefixHelper {
   protected $configFactory;
 
   /**
+   * Request object.
+   *
+   * @var null|\Symfony\Component\HttpFoundation\Request
+   */
+  protected $requestStack;
+
+  /**
    * Constructs a new RcsPhPathPrefixHelper instance.
    *
    * @param \Drupal\Core\Entity\ConfigFactoryInterface $config_factory
    *   Config Factory service.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   Request stock service object.
    */
-  public function __construct(ConfigFactoryInterface $config_factory) {
+  public function __construct(ConfigFactoryInterface $config_factory, RequestStack $request_stack) {
     $this->configFactory = $config_factory;
+    $this->requestStack = $request_stack;
   }
 
   /**
@@ -42,6 +54,26 @@ class RcsPhPathPrefixHelper {
       }
     }
     return $prefixes;
+  }
+
+  /**
+   * Returns requested path prefixes type.
+   *
+   * @return string
+   *   Prefix type which is configured from URL.
+   */
+  public function getRcsPathPageType(): string {
+    $page_type = '';
+    $request_uri = $this->requestStack->getRequestUri();
+    $rcs_config = $this->configFactory->get('rcs_placeholders.settings');
+    $settings = $rcs_config->getRawData();
+    foreach ($settings as $type => $value) {
+      if (!empty($value['path_prefix']) && strpos(strtolower($request_uri), (string) $type) > -1) {
+        $page_type = $type;
+        break;
+      }
+    }
+    return $page_type;
   }
 
 }
