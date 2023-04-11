cc4a85891f0263568d12e1e3384619dab6f16da5
shubhangi-alshaya <123928489+shubhangi-alshaya@users.noreply.github.com>
2023-04-11 15:08:58 +0530

    CORE-55080: Update for purchase tracking script tag. (#28404)
    * CORE-55080: Update for purchase tracking script on cart page.
    
    * CORE-55080: Update for purchase tracking script on cart page.
    
    * CORE-55080: Update for purchase tracking script on cart page.
    
    * CORE-55080: Update for purchase tracking script on cart page.
    
    * CORE-55080: Update for removing code from react components.
    
    * CORE-55080: Update for code in module.
    
    * CORE-55080: Update for helper function.
    
    * CORE-55080: Update for helper function.
    
    * Update docroot/modules/custom/alshaya_shoeai/alshaya_shoeai.module
    
    Co-authored-by: Nilesh Lohar <nilesh.lohar@acquia.com>
    
    * Update docroot/modules/custom/alshaya_shoeai/js/alshaya_shoeai.js
    
    Co-authored-by: Nilesh Lohar <nilesh.lohar@acquia.com>
    
    * CORE-55080: Update for helper function.
    
    <shubhangi-alshaya@shubhangi.johri@alshaya.com>
    
    * Update alshaya_shoeai.js
    
    * Update alshaya_shoeai.module
    
    * CORE-55080: Update for code as per comments.
    
    * CORE-55080: Update for code as per comments.
    
    ---------
    
    Co-authored-by: root <root@072e52585360>
    Co-authored-by: Nilesh Lohar <nilesh.lohar@acquia.com>

--- docroot/modules/custom/alshaya_shoeai/js/alshaya_shoeai.js
+++ docroot/modules/custom/alshaya_shoeai/js/alshaya_shoeai.js
@@ -22,6 +22,14 @@
     script.text = '{shopID:"' + shoeAi.shopId + '", locale:"' +
      language + '", scale: "eu", zeroHash:"' + shoeAi.zeroHash + '", newRecommendation:' + newRecommendation +', inCart:' + inCart + '}';
     document.body.appendChild(script);
+    // Initialize shoeai purchase tracking script on cart page.
+    document.addEventListener('updateCartItemData', function (e) {
+      initialiseShoeSizeShoppingCart(e, 'updateCartItemData');
+    });
+    // Initialize shoeai purchase tracking script in PLP,SLP and PDP.
+    document.addEventListener('product-add-to-cart-success', function (e) {
+      initialiseShoeSizeShoppingCart(e, 'addToCartPlp');
+    });
   }
 })(drupalSettings);
 
@@ -53,15 +61,84 @@ window.shoe_size_add_to_cart = (recommendation) => {
 
 // Helper function for getting recommended shoesize from shoeai and select the size in PDP if available.
 window.shoe_size_recommendation = (recommendation) => {
-  const recommendedSize = recommendation.size['eu'] ? recommendation.size['eu'].replace('.0','') : null;
-  // work only if recommendedSize is not null.
-  if (recommendedSize) {
-    const sizes = document.querySelectorAll('.magv2-select-list-name');
-    for (const size of sizes) {
-      if (size.innerText == recommendedSize) {
-        size.click();
-        return;
-      }
+  if (recommendation.size) {
+    const recommendedSize = recommendation.size['eu'] ? recommendation.size['eu'].replace('.0','') : null;
+    // work only if recommendedSize is not null.
+    if (recommendedSize) {
+      const sizes = document.querySelectorAll('.magv2-select-list-name');
+      for (const size of sizes) {
+        if (size.innerText == recommendedSize) {
+          size.click();
+          return;
+        }
+      }    
     }
   }
 }
+
+// Helper function for initialising setShoeSizeShoppingCart function.
+function initialiseShoeSizeShoppingCart(e, event) {
+  const localCart = window.commerceBackend.getCartDataFromStorage();
+  var items = localCart.cart.items;
+  var totals = localCart.cart.totals.items;
+  var updated_sku = '';
+  var updated_qty = '';
+  if (event == 'updateCartItemData') {
+    // Updated_sku is required to know which item is updated.
+    updated_sku = e.detail.data.item.sku;
+    // Updated_qty is required as items and totals dont have correct quantity.
+    updated_qty = e.detail.data.qty;
+  }
+  setShoeSizeShoppingCart(totals, items, updated_sku, updated_qty);
+};
+
+/*
+ * Helper function for populating
+ * ShoeSizeShoppingCart variable with cart item
+ * and call shoeai ext. script.
+ */
+function setShoeSizeShoppingCart(totals, items, updated_sku, updated_qty) {
+  // Using data.totals.items as it has information related to size of shoes.
+  const products = Object.values(totals);
+  // Products do not have skus hence need to take from data.items.
+  const skus = Object.keys(items);
+  if (skus.length && products.length) {
+    const purchase = [];
+    const { shopId, scale } = drupalSettings.shoeai;
+    let currency = '';
+    if (drupalSettings.alshaya_spc && drupalSettings.alshaya_spc.currency_config.currency_code) {
+      currency = drupalSettings.alshaya_spc.currency_config.currency_code;
+    }
+    let i = 0;
+    // Create purchase object with all products in cart.
+    for (i; i < products.length; i++) {
+      purchase[i] = {};
+      purchase[i].shoeID = skus[i];
+      purchase[i].price = products[i].price_incl_tax ? products[i].price_incl_tax : products[i].price;
+      if (skus[i] == updated_sku) {
+        purchase[i].quantity = updated_qty;
+      } else {
+        purchase[i].quantity = products[i].qty;
+      }
+      purchase[i].scale = scale;
+      purchase[i].currency = currency;
+      // fetch the size.
+      const options = JSON.parse(products[i].options)
+        ? JSON.parse(products[i].options)[0] : [];
+      if (options) {
+        purchase[i].size = options.value;
+      }
+    }
+    // Setting the variable ShoeSizeShoppingCart and calling shoeai script.
+    if (purchase.length && shopId) {
+      window.ShoeSizeShoppingCart = {};
+      window.ShoeSizeShoppingCart.shopID = shopId;
+      window.ShoeSizeShoppingCart.purchases = purchase;
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://shoesize.me/plugin/cart.js?shopid=' + encodeURIComponent(window.ShoeSizeShoppingCart.shopID)
+        + '&sid=' + Math.round(new Date().getTime() / 1000);
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    }
+  }
+};
