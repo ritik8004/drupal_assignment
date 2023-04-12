b626ee4a422a813d84a5649478ba2d0ffa0a9f64
lokeshwari-alshaya <lokeshwari.gautam@alshaya.com>
2023-04-12 11:43:21 +0000

    CORE-54949: Remove single value factes.

--- docroot/modules/react/alshaya_algolia_react/js/src/components/filters/index.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/components/filters/index.js
@@ -15,7 +15,17 @@ const Filters = ({ indexName, pageType, ...props }) => {
       const activeFilters = [];
       filters.forEach((element) => {
         const children = element.getElementsByTagName('ul')[0];
-        if (typeof children !== 'undefined' && children.querySelector('li') === null) {
+        const exclude = drupalSettings.algoliaSearch.exclude_render_single_result_facets.split(',');
+        // Do not show facets that have a single value if the render_single_result_facets is false.
+        // Make sure that we donot exclude the facets like price, color etc.
+        if (!drupalSettings.algoliaSearch.render_single_result_facets) {
+          if ((!exclude.includes(element.getAttribute('id')) && children.childElementCount <= 1)) {
+            element.classList.add('hide-facet-block');
+          } else {
+            element.classList.remove('hide-facet-block');
+            activeFilters.push(element);
+          }
+        } else if (typeof children !== 'undefined' && children.querySelector('li') === null) {
           element.classList.add('hide-facet-block');
         } else {
           activeFilters.push(element);
