a91c09e277253922b2f5f0905c3f4fceb920f6ee
Saurabh <saurabh@Apples-MacBook-Pro.local>
2023-04-12 10:35:03 +0530

    CORE-55527: Removed irrelevant changes from datalayer error log patch.

--- docroot/modules/react/alshaya_spc/js/utilities/checkout_util.js
+++ docroot/modules/react/alshaya_spc/js/utilities/checkout_util.js
@@ -192,12 +192,12 @@ export const placeOrder = (paymentMethod) => {
             return;
           }
 
-          logger.debug('Before orderPlaced event on url: @url.', {
+          logger.notice('Before orderPlaced event on url: @url.', {
             '@url': response.data.redirectUrl,
           });
           // Dispatch an event after order is placed before redirecting to confirmation page.
           dispatchCustomEvent('orderPlaced', true);
-          logger.debug('After orderPlaced event on url: @url.', {
+          logger.notice('After orderPlaced event on url: @url.', {
             '@url': response.data.redirectUrl,
           });
           // This here possibly means that we are redirecting to confirmation page.
