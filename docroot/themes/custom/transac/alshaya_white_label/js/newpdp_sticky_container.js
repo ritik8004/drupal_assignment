e5848ec79c6c7ae1f3cbb227fcfeff8a8ad79dd4
www-data <anjali.vijay@axelerant.com>
2023-04-13 07:59:18 +0000

    CORE-55065: PR feedback fixes.

--- docroot/themes/custom/transac/alshaya_white_label/js/newpdp_sticky_container.js
+++ docroot/themes/custom/transac/alshaya_white_label/js/newpdp_sticky_container.js
@@ -3,14 +3,18 @@
  * NewPDP sticky container.
  */
 
-(function ($, Drupal) {
+(function (Drupal) {
   Drupal.behaviors.newpdpStickyContainer = {
     attach: function () {
       var root = document.querySelector(':root');
-      var galleryWidth = document.querySelector('.magv2-content') && document.querySelector('.magv2-content').offsetWidth;
-      var sidebarWidth = document.querySelector('.magv2-sidebar') && document.querySelector('.magv2-sidebar').offsetWidth;
-      var totalWidth = galleryWidth + sidebarWidth + 'px';
-      root.style.setProperty('--dynamic-container-width', totalWidth);
+      var newPdpGallery = document.querySelector('.magv2-content');
+      var newPdpSidebar = document.querySelector('.magv2-sidebar');
+      var galleryWidth = newPdpGallery && newPdpGallery.offsetWidth;
+      var sidebarWidth = newPdpSidebar && newPdpSidebar.offsetWidth;
+      if (galleryWidth && sidebarWidth) {
+        var totalWidth = galleryWidth + sidebarWidth + 'px';
+        root.style.setProperty('--dynamic-container-width', totalWidth);
+      }
     }
   };
-})(jQuery, Drupal);
+})(Drupal);
