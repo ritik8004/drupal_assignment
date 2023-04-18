5a6d55069638fe475f936efcd779e14897f2d049
Rahul Rasgon <rahulrasgon@gmail.com>
2023-04-18 17:04:21 +0530

    CORE-54414: Fixed react component issue when no color swatches data available.

--- docroot/modules/react/alshaya_algolia_react/js/src/components/teaser/index.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/components/teaser/index.js
@@ -307,7 +307,7 @@ const Teaser = ({
               />
             </div>
             )}
-          {(hasValue(attribute.attr_article_swatches) && showColorSwatchSlider)
+          {(hasValue(attribute.attr_article_swatches[0]) && showColorSwatchSlider)
             ? (
               <ArticleSwatches
                 sku={sku}
