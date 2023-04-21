4db5aacd667afaf9f45c199216bdf642e63b7281
Supreetam Laha <supreetam.laha@acquia.com>
2023-04-21 14:14:15 +0530

    CORE-34314: Addressing PR feedbacks for mainstream merge.

--- docroot/modules/react/alshaya_spc/assets/js/free_gift_cart_util.js
+++ docroot/modules/react/alshaya_spc/assets/js/free_gift_cart_util.js
@@ -24,7 +24,7 @@ window.commerceBackend = window.commerceBackend || {};
           .configurables[key].attribute_id;
         // Skipping the psudo attributes.
         if (
-          drupalSettings.psudo_attribute === undefined
+          !Drupal.hasValue(drupalSettings.psudo_attribute)
           || drupalSettings.psudo_attribute === optionId
         ) {
           return;
