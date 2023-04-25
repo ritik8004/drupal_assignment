ff4663029828ac1909d0c4653fa47e7226beae3f
vivekpan27 <vivek.panicker@innoraft.com>
2023-04-25 18:19:00 +0530

    CORE-55953: Fetched color facet values from color label attribute.

--- docroot/modules/react/alshaya_algolia_react/js/src/components/algolia/widgets/DynamicWidgets.js
+++ docroot/modules/react/alshaya_algolia_react/js/src/components/algolia/widgets/DynamicWidgets.js
@@ -42,9 +42,9 @@ function DynamicWidgets({
     buildFacets(userData);
   }
 
-  const Fallback = hasValue(fallbackComponent) ? function fallback() {
-    return null;
-  } : fallbackComponent;
+  const Fallback = !hasValue(fallbackComponent)
+    ? function fallback() { return null; }
+    : fallbackComponent;
 
   const widgets = new Map();
 
