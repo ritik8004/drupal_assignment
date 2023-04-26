46b3b724b867254ea2b0eeabb83698d9209ad332
Saurabh <saurabh@Apples-MacBook-Pro.local>
2023-04-26 13:52:51 +0530

    CORE-54049: Added comments in swatch click js file.

--- docroot/modules/brands/alshaya_nb/modules/alshaya_nb_transac_rcs/js/plp-swatch-click.js
+++ docroot/modules/brands/alshaya_nb/modules/alshaya_nb_transac_rcs/js/plp-swatch-click.js
@@ -1,6 +1,6 @@
 /**
  * @file
- * PLP Swatch Hover js file.
+ * PLP Swatch Click js file.
  */
 
 /* global debounce */
@@ -8,17 +8,19 @@
 (function ($, Drupal) {
 
   /**
-   * Adding the hover effect to colour swatches on plp.
+   * Adding the click effect to image swatches on plp.
    */
    function onSwatchClickUpdateMainImage() {
     $('.product-plp-detail-wrapper .swatches').find('.swatch-image').on('click', debounce(function (e) {
       e.preventDefault();
       var productUrl = $(this).find('img').attr('data-sku-image');
+      // Replace image url on click.
       $(this).closest('.c-products__item').find('.alshaya_search_mainimage > img').attr('src', productUrl);
       var skuId = $(this).find('img').attr('data-sku-id');
       var url = $(this).closest('.c-products__item').find('a').attr('href');
       url = url.split('?');
       url = url[0] + '?selected=' + skuId;
+      // Replace product url on click.
       $(this).closest('.c-products__item').find('a.product-selected-url').attr('href', url);
     }, 100));
   }
