4370a4c352c8f3c36ab0d4a961f59ca829775e54
Abdeali Waseef <wabeali@gmail.com>
2023-05-04 18:20:48 +0530

    CORE-54492: Adding GTM event for size guide click on magazine v2 for v3 sites.

--- docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-cart/size-guide/index.js
+++ docroot/modules/react/alshaya_pdp_react/js/pdp-layout/components/pdp-cart/size-guide/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import parse from 'html-react-parser';
 import { hasValue } from '../../../../../../js/utilities/conditionsUtility';
 
-const SizeGuide = ({ attrId }) => {
+const SizeGuide = ({ attrId, context }) => {
+  const sizeGuideRef = useRef(null);
+
   // Get size guide as per v3 architecture.
   const sizeGuide = window.commerceBackend.getSizeGuideSettings();
+
+  useEffect(() => {
+    const sizeGuideLink = sizeGuideRef.current.querySelector('.size-guide-link');
+    if (sizeGuideLink && context) {
+      sizeGuideLink.addEventListener('click', () => {
+        Drupal.alshayaSeoGtmPushSizeGuideEvents('open', context);
+      });
+    }
+  }, []);
 
   if (hasValue(sizeGuide)) {
     // If the current attr matches the size attribute.
     if (sizeGuide.attributes.indexOf(attrId) !== -1) {
       return (
-        <div className="size-guide">
+        <div className="size-guide" ref={sizeGuideRef}>
           {parse(sizeGuide.link)}
         </div>
       );
