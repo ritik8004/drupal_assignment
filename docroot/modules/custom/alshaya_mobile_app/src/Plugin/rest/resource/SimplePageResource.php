04f3fa4119a273a6ec063c00e35148f7a0221307
DDEV User <nobody@example.com>
2023-05-15 10:48:22 +0000

    CORE-56771: Comments addressed on Add data for updated timestamp date and add a new field version_number for static page api response.

--- docroot/modules/custom/alshaya_mobile_app/src/Plugin/rest/resource/SimplePageResource.php
+++ docroot/modules/custom/alshaya_mobile_app/src/Plugin/rest/resource/SimplePageResource.php
@@ -175,8 +175,8 @@ class SimplePageResource extends ResourceBase {
       'id' => (int) $node->id(),
       'name' => $node->label(),
       'path' => $node_url->getGeneratedUrl(),
-      'updated' => $node->get('changed')->value,
-      'version' => $node->get('field_version')->value,
+      'updated' => $node->get('changed')->getString(),
+      'version' => $node->get('field_version')->getString(),
       'deeplink' => $this->mobileAppUtility->getDeepLink($node),
       'html' => !empty($node->get('body')->first())
       ? $this->mobileAppUtility->convertRelativeUrlsToAbsolute($node->get('body')->first()->getValue()['value'])
