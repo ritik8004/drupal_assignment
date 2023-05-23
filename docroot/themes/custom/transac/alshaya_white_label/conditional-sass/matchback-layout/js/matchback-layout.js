ea704ff667c74d7d8d6d44e5276ca810c47d2c91
Murtaza Alvi <smurtazama@gmail.com>
2023-05-24 00:11:53 +0530

    CORE-57013: Crossell slider dots on mulltiple lines.

--- docroot/themes/custom/transac/alshaya_white_label/conditional-sass/matchback-layout/js/matchback-layout.js
+++ docroot/themes/custom/transac/alshaya_white_label/conditional-sass/matchback-layout/js/matchback-layout.js
@@ -55,6 +55,82 @@
       ocObject.once('product-matchback-carousel').slick(options);
 
     }
+
+    var slickSlider = $('.machbackSlider');
+    var transformXIntervalNext = -15;
+    var transformXIntervalPrev = 15;
+    let dots, totalDots, visibleWidth, dotWidth, visibleItemsToShow, maxDots;
+
+    // Limit the number of dots for each slider.
+    $(slickSlider).each(function () {
+      $(this).find('.slick-dots').once().wrap("<div class='slick-dots__container'></div>");
+      dots = $(".machbackSlider .slick-dots li");
+      totalDots = dots.length;
+
+      if (totalDots === 0) return;
+
+      // Calculate the number of dots to be shown based on the width.
+      visibleWidth = $(".slick-dots__container .slick-dots").width();
+      dotWidth = $(dots[0]).outerWidth() + 10;
+      visibleItemsToShow = Math.floor(visibleWidth / dotWidth);
+      maxDots = visibleItemsToShow;
+
+      var transformCount = 0;
+
+      $(this).find('.slick-dots li').each(function (index) {
+        $(this).addClass('dot-index-' + index);
+      });
+
+      function setBoundries(slick, state) {
+        if (state === 'default') {
+          slick.find('.slick-dots li').eq(visibleItemsToShow).addClass('n-small-1');
+        }
+      }
+
+      $(this).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        var totalCount = $(this).find('.slick-dots li').length;
+
+        if (totalCount > maxDots) {
+          if (nextSlide > currentSlide) {
+            if ($(this).find('.slick-dots .dot-index-' + nextSlide).hasClass('n-small-1')) {
+              if (!$(this).find('.slick-dots li:last-child').hasClass('n-small-1')) {
+                transformCount = transformCount + transformXIntervalNext;
+                $(this).find('.slick-dots .dot-index-' + nextSlide).removeClass('n-small-1');
+
+                var nextSlidePlusOne = nextSlide + 1;
+                $(this).find('.slick-dots .dot-index-' + nextSlidePlusOne).addClass('n-small-1');
+                $(this).find('.slick-dots').css('transform', 'translateX(' + transformCount + 'px)');
+
+                var pPointer = nextSlide - (visibleItemsToShow - 1);
+                var pPointerMinusOne = pPointer - 1;
+                $(this).find('.slick-dots li').eq(pPointerMinusOne).removeClass('p-small-1');
+                $(this).find('.slick-dots li').eq(pPointer).addClass('p-small-1');
+              }
+            }
+          }
+          else {
+            if ($(this).find('.slick-dots .dot-index-' + nextSlide).hasClass('p-small-1')) {
+              if (!$(this).find('.slick-dots li:first-child').hasClass('p-small-1')) {
+                transformCount = transformCount + transformXIntervalPrev;
+                $(this).find('.slick-dots .dot-index-' + nextSlide).removeClass('p-small-1');
+
+                var nextSlidePlusOne = nextSlide - 1;
+                $(this).find('.slick-dots .dot-index-' + nextSlidePlusOne).addClass('p-small-1');
+                $(this).find('.slick-dots').css('transform', 'translateX(' + transformCount + 'px)');
+
+                var nPointer = currentSlide + (visibleItemsToShow - 1);
+                var nPointerMinusOne = nPointer - 1;
+                $(this).find('.slick-dots li').eq(nPointer).removeClass('n-small-1');
+                $(this).find('.slick-dots li').eq(nPointerMinusOne).addClass('n-small-1');
+              }
+            }
+          }
+        }
+      });
+
+      $(this).find('.slick-dots').css('transform', 'translateX(0)');
+      setBoundries($(this),'default');
+    });
   }
 
   // Call matchbackSlider() to apply slick and instagram dots.
