7eed0df1e63af6ff90197482b74cb41ecd63b76c
Murtaza Alvi <smurtazama@gmail.com>
2023-05-24 14:13:52 +0530

    CORE-57013: Crossell slider dots on mulltiple lines code optimization.

--- docroot/themes/custom/transac/alshaya_white_label/conditional-sass/matchback-layout/js/matchback-layout.js
+++ docroot/themes/custom/transac/alshaya_white_label/conditional-sass/matchback-layout/js/matchback-layout.js
@@ -56,28 +56,29 @@
 
     }
 
-    var slickSlider = $('.machbackSlider');
-    var transformXIntervalNext = -15;
-    var transformXIntervalPrev = 15;
-    let dots, totalDots, visibleWidth, dotWidth, visibleItemsToShow, maxDots;
+    let $slickSlider = $('.machbackSlider');
+    let transformXIntervalNext = -15;
+    let transformXIntervalPrev = 15;
+    let totalDots, visibleWidth, dotWidth, visibleItemsToShow, maxDots;
 
     // Limit the number of dots for each slider.
-    $(slickSlider).each(function () {
-      $(this).find('.slick-dots').once().wrap("<div class='slick-dots__container'></div>");
-      dots = $(".machbackSlider .slick-dots li");
-      totalDots = dots.length;
+    $($slickSlider).each(function () {
+      let $dotsWrapper = $(this).find('.slick-dots');
+      $dotsWrapper.once().wrap("<div class='slick-dots__container'></div>");
+      let $dots = $(this).find('.slick-dots li');
+      totalDots = $dots.length;
 
       if (totalDots === 0) return;
 
       // Calculate the number of dots to be shown based on the width.
       visibleWidth = $(".slick-dots__container .slick-dots").width();
-      dotWidth = $(dots[0]).outerWidth() + 10;
+      dotWidth = $($dots[0]).outerWidth() + 10;
       visibleItemsToShow = Math.floor(visibleWidth / dotWidth);
       maxDots = visibleItemsToShow;
 
-      var transformCount = 0;
+      let transformCount = 0;
 
-      $(this).find('.slick-dots li').each(function (index) {
+      $dots.each(function (index) {
         $(this).addClass('dot-index-' + index);
       });
 
@@ -88,53 +89,54 @@
       }
 
       $(this).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-        var totalCount = $(this).find('.slick-dots li').length;
+        let totalCount = $dots.length;
+        let $nextSlideElement = $dotsWrapper.find('.dot-index-' + nextSlide);
 
         if (totalCount > maxDots) {
           if (nextSlide > currentSlide) {
-            if ($(this).find('.slick-dots .dot-index-' + nextSlide).hasClass('n-small-1')) {
+            if ($nextSlideElement.hasClass('n-small-1')) {
               if (!$(this).find('.slick-dots li:last-child').hasClass('n-small-1')) {
                 transformCount = transformCount + transformXIntervalNext;
-                $(this).find('.slick-dots .dot-index-' + nextSlide).removeClass('n-small-1');
+                $nextSlideElement.removeClass('n-small-1');
 
-                var nextSlidePlusOne = nextSlide + 1;
+                let nextSlidePlusOne = nextSlide + 1;
                 $(this).find('.slick-dots .dot-index-' + nextSlidePlusOne).addClass('n-small-1');
-                $(this).find('.slick-dots').css('transform', 'translateX(' + transformCount + 'px)');
+                $dotsWrapper.css('transform', 'translateX(' + transformCount + 'px)');
                 if (isRTL()) {
-                  $(this).find('.slick-dots').css('transform', 'translateX(' + Math.abs(transformCount) + 'px)');
+                  $dotsWrapper.css('transform', 'translateX(' + Math.abs(transformCount) + 'px)');
                 }
 
-                var pPointer = nextSlide - (visibleItemsToShow - 1);
-                var pPointerMinusOne = pPointer - 1;
-                $(this).find('.slick-dots li').eq(pPointerMinusOne).removeClass('p-small-1');
-                $(this).find('.slick-dots li').eq(pPointer).addClass('p-small-1');
+                let pPointer = nextSlide - (visibleItemsToShow - 1);
+                let pPointerMinusOne = pPointer - 1;
+                $dots.eq(pPointerMinusOne).removeClass('p-small-1');
+                $dots.eq(pPointer).addClass('p-small-1');
               }
             }
           }
           else {
-            if ($(this).find('.slick-dots .dot-index-' + nextSlide).hasClass('p-small-1')) {
+            if ($nextSlideElement.hasClass('p-small-1')) {
               if (!$(this).find('.slick-dots li:first-child').hasClass('p-small-1')) {
                 transformCount = transformCount + transformXIntervalPrev;
-                $(this).find('.slick-dots .dot-index-' + nextSlide).removeClass('p-small-1');
+                $nextSlideElement.removeClass('p-small-1');
 
-                var nextSlidePlusOne = nextSlide - 1;
+                let nextSlidePlusOne = nextSlide - 1;
                 $(this).find('.slick-dots .dot-index-' + nextSlidePlusOne).addClass('p-small-1');
-                $(this).find('.slick-dots').css('transform', 'translateX(' + transformCount + 'px)');
+                $dotsWrapper.css('transform', 'translateX(' + transformCount + 'px)');
                 if (isRTL()) {
-                  $(this).find('.slick-dots').css('transform', 'translateX(' + Math.abs(transformCount) + 'px)');
+                  $dotsWrapper.css('transform', 'translateX(' + Math.abs(transformCount) + 'px)');
                 }
 
-                var nPointer = currentSlide + (visibleItemsToShow - 1);
-                var nPointerMinusOne = nPointer - 1;
-                $(this).find('.slick-dots li').eq(nPointer).removeClass('n-small-1');
-                $(this).find('.slick-dots li').eq(nPointerMinusOne).addClass('n-small-1');
+                let nPointer = currentSlide + (visibleItemsToShow - 1);
+                let nPointerMinusOne = nPointer - 1;
+                $dots.eq(nPointer).removeClass('n-small-1');
+                $dots.eq(nPointerMinusOne).addClass('n-small-1');
               }
             }
           }
         }
       });
 
-      $(this).find('.slick-dots').css('transform', 'translateX(0)');
+      $dotsWrapper.css('transform', 'translateX(0)');
       setBoundries($(this),'default');
     });
   }
